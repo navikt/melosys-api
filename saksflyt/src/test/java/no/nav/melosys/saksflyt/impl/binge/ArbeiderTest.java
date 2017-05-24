1ba7d0256973eef6476dcf217dd3053fc5550291
Farjam Movafagh <farjam.movafagh@nav.no>
2017-05-24 12:23:49 +0200

    FIX: Fjernet warning i testkode

--- saksflyt/src/test/java/no/nav/melosys/saksflyt/impl/binge/ArbeiderTest.java
+++ saksflyt/src/test/java/no/nav/melosys/saksflyt/impl/binge/ArbeiderTest.java
@@ -13,7 +13,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.util.ReflectionTestUtils;
 
