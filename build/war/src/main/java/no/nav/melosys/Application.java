31393cb8d6cf233d287ab1a571f288c36002e61e
Francois Allix <francois.allix@nav.no>
2017-06-15 12:43:14 +0200

    Saksflyt blir en del av applikasjonen

--- build/war/src/main/java/no/nav/melosys/Application.java
+++ build/war/src/main/java/no/nav/melosys/Application.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:saksflyt.properties")
 public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) throws Exception {
