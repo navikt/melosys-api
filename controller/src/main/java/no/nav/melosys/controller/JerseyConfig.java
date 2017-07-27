a7067703c77320231ca4fc17b1600a8ee7f74117
Francois Allix <francois.allix@nav.no>
2017-07-27 15:24:30 +0200

    Flyttet JerseyConfig slik at det blir sammen med rest tjenestene

--- controller/src/main/java/no/nav/melosys/controller/JerseyConfig.java
+++ controller/src/main/java/no/nav/melosys/controller/JerseyConfig.java
@@ -4,13 +4,11 @@ import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.context.annotation.Configuration;
 
 import no.nav.melosys.controller.BehandlingRestTjeneste;
-import no.nav.melosys.controller.JaxRsEndpoint;
 
 @Configuration
 public class JerseyConfig extends ResourceConfig {
 
     public JerseyConfig() {
-        register(JaxRsEndpoint.class);
         register(BehandlingRestTjeneste.class);
     }
 }
