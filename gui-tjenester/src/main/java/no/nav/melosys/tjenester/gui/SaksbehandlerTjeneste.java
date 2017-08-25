ff4ff27b7a9823c526bf01e634c9a95a4141b0d5
Francois Allix <francois.allix@nav.no>
2017-08-25 13:27:57 +0200

    Melosys kommer ikke til å bruke Foreldrepengers loginmodul

--- gui-tjenester/src/main/java/no/nav/melosys/tjenester/gui/SaksbehandlerTjeneste.java
+++ gui-tjenester/src/main/java/no/nav/melosys/tjenester/gui/SaksbehandlerTjeneste.java
@@ -8,8 +8,6 @@ import javax.ws.rs.core.MediaType;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import no.nav.melosys.tjenester.gui.dto.InnloggetBrukerDto;
-import no.nav.vedtak.felles.integrasjon.ldap.LdapBrukeroppslag;
-import no.nav.vedtak.sikkerhet.context.SubjectHandler;
 
 @Api(tags = {"saksbehandler"})
 @Path("/saksbehandler")
@@ -20,11 +18,10 @@ public class SaksbehandlerTjeneste {
     @ApiOperation(value = "Returnerer fullt navn for ident",
             notes = ("Ident hentes fra sikkerhetskonteksten som er tilgjengelig etter innlogging."))
     public InnloggetBrukerDto innloggetBruker() {
-        String ident = SubjectHandler.getSubjectHandler().getUid();
-        LdapBrukeroppslag ldap = new LdapBrukeroppslag();
-        String navn = ldap.finnNavnPåBruker(ident);
-        return new InnloggetBrukerDto(ident, navn);
-        //return new InnloggetBrukerDto("Z991001", "F_Z991001 E_Z991001");
+        // TODO Implementere LDAP oppslag
+        //String ident = SubjectHandler.getSubjectHandler().getUid();
+
+       return new InnloggetBrukerDto("Z991001", "F_Z991001 E_Z991001");
     }
 
 }
