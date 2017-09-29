45d463f91af5f320d6cd973473a606c01d37ed3c
Francois Allix <francois.allix@nav.no>
2017-09-29 15:49:15 +0200

    EESSI2-256

--- integrasjon/src/main/java/no/nav/melosys/integrasjon/tps/TpsService.java
+++ integrasjon/src/main/java/no/nav/melosys/integrasjon/tps/TpsService.java
@@ -1,14 +1,23 @@
 package no.nav.melosys.integrasjon.tps;
 
+import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Optional;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import no.nav.melosys.domain.Saksopplysning;
+import no.nav.melosys.domain.SaksopplysningKilde;
+import no.nav.melosys.domain.SaksopplysningType;
+import no.nav.melosys.domain.dokument.DokumentFactory;
 import no.nav.melosys.integrasjon.tps.aktoer.AktorConsumer;
 import no.nav.melosys.integrasjon.tps.person.PersonConsumer;
 import no.nav.tjeneste.virksomhet.aktoer.v2.binding.HentAktoerIdForIdentPersonIkkeFunnet;
@@ -30,14 +39,29 @@ public class TpsService implements TpsFasade {
 
     private static final Logger log = LoggerFactory.getLogger(TpsService.class);
 
+    private static final String PERSON_VERSJON = "3.0";
+
     private AktorConsumer aktorConsumer;
 
     private PersonConsumer personConsumer;
 
+    private DokumentFactory dokumentFactory;
+
+    private final Marshaller marshaller;
+
     @Autowired
-    public TpsService(AktorConsumer aktorConsumer, PersonConsumer personConsumer) {
+    public TpsService(AktorConsumer aktorConsumer, PersonConsumer personConsumer, DokumentFactory dokumentFactory) {
         this.aktorConsumer = aktorConsumer;
         this.personConsumer = personConsumer;
+        this.dokumentFactory = dokumentFactory;
+
+        try {
+            JAXBContext jaxbContext = JAXBContext.newInstance(no.nav.tjeneste.virksomhet.person.v3.HentPersonResponse.class);
+            marshaller = jaxbContext.createMarshaller();
+        } catch (JAXBException e) {
+            log.error("", e);
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
@@ -72,19 +96,51 @@ public class TpsService implements TpsFasade {
     }
 
     @Override
-    public HentPersonResponse hentPerson(String ident) throws HentPersonPersonIkkeFunnet, HentPersonSikkerhetsbegrensning {
-        return hentPerson(ident, null);
+    public Saksopplysning hentPerson(String ident, Collection<Informasjonsbehov> behov) throws HentPersonPersonIkkeFunnet, HentPersonSikkerhetsbegrensning {
+        HentPersonRequest request = new HentPersonRequest();
+        NorskIdent norskIdent = new NorskIdent();
+        norskIdent.setIdent(ident);
+
+        PersonIdent personIdent = new PersonIdent();
+        personIdent.setIdent(norskIdent);
+
+        request.setAktoer(personIdent);
+        if (behov != null) {
+            request.getInformasjonsbehov().addAll(behov);
+        }
+
+        // Kall til TPS
+        HentPersonResponse response = personConsumer.hentPerson(request);
+
+        // Response -> xml
+        StringWriter xmlWriter = new StringWriter();
+        try {
+            no.nav.tjeneste.virksomhet.person.v3.HentPersonResponse xmlRoot = new no.nav.tjeneste.virksomhet.person.v3.HentPersonResponse();
+            xmlRoot.setResponse(response);
+            marshaller.marshal(xmlRoot, xmlWriter);
+        } catch (JAXBException e) {
+            log.error("", e);
+            throw new RuntimeException(e);
+        }
+
+        Saksopplysning saksopplysning = new Saksopplysning();
+        saksopplysning.setDokumentXml(xmlWriter.toString());
+        saksopplysning.setKilde(SaksopplysningKilde.TPS);
+        saksopplysning.setType(SaksopplysningType.PERSONOPPLYSNING);
+        saksopplysning.setVersjon(PERSON_VERSJON);
+
+        // xml -> java objekter
+        dokumentFactory.lagDokument(saksopplysning);
+
+        return saksopplysning;
     }
 
     @Override
+    @Deprecated
     public HentPersonResponse hentPersonMedAdresse(String ident) throws HentPersonPersonIkkeFunnet, HentPersonSikkerhetsbegrensning {
         Collection<Informasjonsbehov> behov = new ArrayList<>();
         behov.add(Informasjonsbehov.ADRESSE);
-        return hentPerson(ident, behov);
-    }
 
-    @Override
-    public HentPersonResponse hentPerson(String ident, Collection<Informasjonsbehov> behov) throws HentPersonPersonIkkeFunnet, HentPersonSikkerhetsbegrensning {
         HentPersonRequest request = new HentPersonRequest();
         NorskIdent norskIdent = new NorskIdent();
         norskIdent.setIdent(ident);
