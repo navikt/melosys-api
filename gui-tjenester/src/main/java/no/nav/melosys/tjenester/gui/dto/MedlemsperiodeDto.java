51e886c7480e429f5168c62d96c65b23fd8a35be
Eivind K. Nilsbakken <eivind.nilsbakken@nav.no>
2017-10-16 15:44:53 +0200

    Lag Medlemsperiode DTO og joda->java time converter

---
+++ gui-tjenester/src/main/java/no/nav/melosys/tjenester/gui/dto/MedlemsperiodeDto.java
@@ -0,0 +1,81 @@
+package no.nav.melosys.tjenester.gui.dto;
+
+import java.time.LocalDate;
+
+import no.nav.melosys.tjenester.gui.dto.util.DtoUtils;
+import no.nav.tjeneste.virksomhet.medlemskap.v2.informasjon.Medlemsperiode;
+
+public class MedlemsperiodeDto {
+
+    private PeriodeDto periode;
+
+    private String type;
+
+    private String status;
+
+    private String grunnlagstype;
+
+    private String land;
+
+    private String trygdedekning;
+
+    public static MedlemsperiodeDto toDto(Medlemsperiode m) {
+        MedlemsperiodeDto medlemsperiodeDto = new MedlemsperiodeDto();
+
+        LocalDate fom = DtoUtils.tilLocalDate(m.getFraOgMed());
+        LocalDate tom = DtoUtils.tilLocalDate(m.getTilOgMed());
+        medlemsperiodeDto.setPeriode(new PeriodeDto(fom, tom));
+
+        //m.getType().
+
+        return medlemsperiodeDto;
+    }
+
+    public PeriodeDto getPeriode() {
+        return periode;
+    }
+
+    public void setPeriode(PeriodeDto periode) {
+        this.periode = periode;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getGrunnlagstype() {
+        return grunnlagstype;
+    }
+
+    public void setGrunnlagstype(String grunnlagstype) {
+        this.grunnlagstype = grunnlagstype;
+    }
+
+    public String getLand() {
+        return land;
+    }
+
+    public void setLand(String land) {
+        this.land = land;
+    }
+
+    public String getTrygdedekning() {
+        return trygdedekning;
+    }
+
+    public void setTrygdedekning(String trygdedekning) {
+        this.trygdedekning = trygdedekning;
+    }
+}
--- gui-tjenester/src/main/java/no/nav/melosys/tjenester/gui/dto/util/DtoUtils.java
+++ gui-tjenester/src/main/java/no/nav/melosys/tjenester/gui/dto/util/DtoUtils.java
@@ -2,10 +2,14 @@ package no.nav.melosys.tjenester.gui.dto.util;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 import javax.xml.datatype.XMLGregorianCalendar;
 
 public final class DtoUtils {
+
+    private static final String ISO_DATE_PATTERN = "yyyy-MM-dd";
+    private static final DateTimeFormatter ISO_DATE_FORMATTER = DateTimeFormatter.ofPattern(ISO_DATE_PATTERN);
 
     private DtoUtils() {
     }
@@ -15,6 +19,11 @@ public final class DtoUtils {
             return null;
         }
         return xmlCal.toGregorianCalendar().toZonedDateTime().toLocalDate();
+    }
+
+    public static LocalDate tilLocalDate(org.joda.time.LocalDate jodaLocalDate) {
+        String dateStr = jodaLocalDate.toString(ISO_DATE_PATTERN);
+        return LocalDate.parse(dateStr, ISO_DATE_FORMATTER);
     }
 
     public static LocalDateTime tilLocalDateTime(XMLGregorianCalendar tidspunkt) {
