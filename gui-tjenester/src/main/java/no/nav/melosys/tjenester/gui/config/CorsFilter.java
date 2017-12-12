137a29611d771c2b0d7cc6d615f2e52623e2018f
Francois Allix <francois.allix@nav.no>
2017-12-12 11:44:59 +0100

    Fjernet DELETE og PUT fra CorsFilter da disse metoder brukes ikke

--- gui-tjenester/src/main/java/no/nav/melosys/tjenester/gui/config/CorsFilter.java
+++ gui-tjenester/src/main/java/no/nav/melosys/tjenester/gui/config/CorsFilter.java
@@ -1,12 +1,11 @@
 package no.nav.melosys.tjenester.gui.config;
 
-import java.io.IOException;
-
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
 import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.ext.Provider;
+import java.io.IOException;
 
 @Provider
 public class CorsFilter implements ContainerResponseFilter {
@@ -16,7 +15,7 @@ public class CorsFilter implements ContainerResponseFilter {
         MultivaluedMap<String, Object> headers = responseContext.getHeaders();
 
         headers.add("Access-Control-Allow-Origin", "*");
-        headers.add("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, OPTIONS");
+        headers.add("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
         headers.add("Access-Control-Allow-Headers", "Origin, Content-Type, Accept");
     }
 }
