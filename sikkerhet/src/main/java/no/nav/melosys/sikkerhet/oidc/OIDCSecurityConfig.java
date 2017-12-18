1699e177f3b320c6ea431de1ded8fba6d05bb0c0
Francois Allix <francois.allix@nav.no>
2017-12-18 16:53:50 +0100

    MELOSYS-719 CORS fjernes

--- sikkerhet/src/main/java/no/nav/melosys/sikkerhet/oidc/OIDCSecurityConfig.java
+++ sikkerhet/src/main/java/no/nav/melosys/sikkerhet/oidc/OIDCSecurityConfig.java
@@ -2,8 +2,6 @@ package no.nav.melosys.sikkerhet.oidc;
 
 import org.mitre.openid.connect.client.OIDCAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -13,11 +11,6 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 import org.springframework.security.web.authentication.preauth.AbstractPreAuthenticatedProcessingFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
 
 /**
  * Spring security {@code @Configuration}
@@ -26,9 +19,6 @@ import java.util.Arrays;
 @EnableWebSecurity
 @Import(MITREidConfig.class)
 public class OIDCSecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Value("${config.cors.allowOrigin}")
-    private String allowOrigin;
 
     private OIDCAuthenticationFilter filter;
 
@@ -47,30 +37,12 @@ public class OIDCSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
-        http.cors().and()
-                .addFilterAfter(new OAuth2ClientContextFilter(), AbstractPreAuthenticatedProcessingFilter.class)
-                .addFilterAfter(filter, OAuth2ClientContextFilter.class)
-                .addFilterAfter(refreshingFilter, OIDCAuthenticationFilter.class)
-                .authorizeRequests().anyRequest().authenticated()
-                .and().csrf().disable()
-                .httpBasic().authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/openid_connect_login"));
-    }
-
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration config = new CorsConfiguration();
-        config.addAllowedOrigin(allowOrigin);
-        config.setAllowedMethods(Arrays.asList("GET", "POST"));
-        config.setAllowCredentials(true);
-        config.setMaxAge(3600L);
-        config.addAllowedHeader("Accept");
-        config.addAllowedHeader("Accept-Charset");
-        config.addAllowedHeader("Authorization");
-        config.addAllowedHeader("Content-type");
-        config.addAllowedHeader("Origin");
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", config);
-        return source;
+        http.addFilterAfter(new OAuth2ClientContextFilter(), AbstractPreAuthenticatedProcessingFilter.class)
+            .addFilterAfter(filter, OAuth2ClientContextFilter.class)
+            .addFilterAfter(refreshingFilter, OIDCAuthenticationFilter.class)
+            .authorizeRequests().anyRequest().authenticated()
+            .and().csrf().disable()
+            .httpBasic().authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/openid_connect_login"));
     }
 
 }
