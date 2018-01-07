ed617b6df5404edd65214e34e0d7d95d3cccfa4a
Eivind K. Nilsbakken <eivind.nilsbakken@nav.no>
2018-01-07 13:00:50 +0100

    MELOSYS-722: Legg til hjelpemetoder for å støtte artikkel 12

--- domain/src/main/java/no/nav/melosys/domain/dokument/felles/Landkode.java
+++ domain/src/main/java/no/nav/melosys/domain/dokument/felles/Landkode.java
@@ -1,6 +1,19 @@
 package no.nav.melosys.domain.dokument.felles;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Landkode {
+    final String ISLAND = "ISL";
+    final String LIECHTENSTEIN = "LIE";
+    final String LUXEMBOURG = "LUX";
+    final String NEDERLAND = "NLD";
+    final String NORGE = "NOR";
+    final String SVEITS = "SWZ";
+
+    // TODO: Sjekk om vi skal ta hensyn til Sveits og Svalbard
+    final Set<String> EØS = new HashSet<>(Arrays.asList(ISLAND, LIECHTENSTEIN, NORGE));
 
     private String kode;
 
@@ -19,5 +32,28 @@ public class Landkode {
     public void setKode(String kode) {
         this.kode = kode;
     }
-    
+
+    public boolean erEØS() {
+        return EØS.contains(kode);
+    }
+
+    public boolean erSveits() {
+        return SVEITS.equals(kode);
+    }
+
+    public boolean erStatsløs() {
+        return kode == null;
+    }
+
+    public boolean erDekketAvNordiskKonvensjonOmTrygd() {
+        return true;
+    }
+
+    public boolean erNederland() {
+        return NEDERLAND.equals(kode);
+    }
+
+    public boolean erLuxembourg() {
+        return NEDERLAND.equals(kode);
+    }
 }
