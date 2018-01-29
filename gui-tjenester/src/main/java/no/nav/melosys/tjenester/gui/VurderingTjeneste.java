ccbe6a151d5d77841771406e0cb14c6340eb3a38
Farjam Movafagh <farjam.movafagh@nav.no>
2018-01-29 13:34:10 +0100

    MELOSYS-577: Fikset kompileringsfeil etter rebase

--- gui-tjenester/src/main/java/no/nav/melosys/tjenester/gui/VurderingTjeneste.java
+++ gui-tjenester/src/main/java/no/nav/melosys/tjenester/gui/VurderingTjeneste.java
@@ -1,14 +1,21 @@
 package no.nav.melosys.tjenester.gui;
 
+import static no.nav.melosys.regler.api.lovvalg.rep.Argument.*;
+
 import java.util.ArrayList;
 import java.util.HashMap;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
+import org.springframework.web.context.WebApplicationContext;
+
 import io.swagger.annotations.Api;
-import no.nav.melosys.regler.api.lovvalg.rep.Argument;
 import no.nav.melosys.regler.api.lovvalg.rep.Artikkel;
 import no.nav.melosys.regler.api.lovvalg.rep.Betingelse;
 import no.nav.melosys.regler.api.lovvalg.rep.FastsettLovvalgReply;
@@ -16,10 +23,6 @@ import no.nav.melosys.regler.api.lovvalg.rep.Lovvalgsbestemmelse;
 import no.nav.melosys.regler.api.lovvalg.rep.Resultat;
 import no.nav.melosys.service.RegelmodulService;
 import no.nav.melosys.tjenester.gui.dto.LovvalgDto;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Scope;
-import org.springframework.stereotype.Service;
-import org.springframework.web.context.WebApplicationContext;
 
 @Api(tags = {"vurdering"})
 @Path("/vurdering")
@@ -68,22 +71,38 @@ public class VurderingTjeneste extends RestTjeneste {
 
         lovvalgsbestemmelse.betingelser = new ArrayList<>();
         Betingelse betingelse = new Betingelse();
-        betingelse.argument = Argument.A1_12_1_VIRKSOMHET_I_UTSENDERLAND;
+        betingelse.argument = ARBEIDSPLASSEN_I_UTLANDET_DEKKES_AV_EF_883_2004;
+        betingelse.krav = "Arbeidsplassen i utlandet er på et sted som dekkes av EF 883/2004 er sann";
         betingelse.resultat = Resultat.OPPFYLT;
         lovvalgsbestemmelse.betingelser.add(betingelse);
 
         betingelse = new Betingelse();
-        betingelse.argument = Argument.A1_12_1_SENDES_TIL_ANNEN_MEDLEMSSTAT;
+        betingelse.argument = BRUKER_HAR_NORSK_ARBEIDSGIVER;
+        betingelse.krav = "Bruker har norsk arbeidsgiver er sann";
         betingelse.resultat = Resultat.OPPFYLT;
         lovvalgsbestemmelse.betingelser.add(betingelse);
 
         betingelse = new Betingelse();
-        betingelse.argument = Argument.A1_12_1_UTENLANDSOPPHOLDET_ER_IKKE_OVER_24_MND;
+        betingelse.argument = ANTALL_ARBEIDSGIVERE_I_SØKNADSPERIODEN;
+        betingelse.krav = "Antall arbeidsgivere i søknadsperioden er lik 1";
         betingelse.resultat = Resultat.OPPFYLT;
         lovvalgsbestemmelse.betingelser.add(betingelse);
 
         betingelse = new Betingelse();
-        betingelse.argument = Argument.A1_12_1_SKAL_ERSTATTE_ANNEN_PERSON;
+        betingelse.argument = HOVEDARBEIDSFORHOLDET_VARER_I_HELE_SØKNADSPERIODEN;
+        betingelse.krav = "Hovedarbeidsforholdet varer i hele søknadsperioden er sann";
+        betingelse.resultat = Resultat.OPPFYLT;
+        lovvalgsbestemmelse.betingelser.add(betingelse);
+
+        betingelse = new Betingelse();
+        betingelse.argument = LENGDE_MND_UTENLANDSOPPHOLD;
+        betingelse.krav = "Antall måneder utenlandsoppholdet varer er mindre enn eller lik 24";
+        betingelse.resultat = Resultat.OPPFYLT;
+        lovvalgsbestemmelse.betingelser.add(betingelse);
+
+        betingelse = new Betingelse();
+        betingelse.argument = BRUKER_ER_MEDLEM_AV_FTR_MÅNEDEN_FØR_PERIODESTART;
+        betingelse.krav = "Bruker er medlem av FTR måneden før periodestart er sann";
         betingelse.resultat = Resultat.OPPFYLT;
         lovvalgsbestemmelse.betingelser.add(betingelse);
 
