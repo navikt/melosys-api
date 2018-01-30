f6816ba2fb5e6f0ab549de33874c7b82c79eefc8
Francois Allix <francois.allix@nav.no>
2018-01-30 14:24:46 +0100

    MELOSYS-826MELOSYS-826 Mocking av /faktaavklaring

--- gui-tjenester/src/main/java/no/nav/melosys/tjenester/gui/FaktaavklaringTjeneste.java
+++ gui-tjenester/src/main/java/no/nav/melosys/tjenester/gui/FaktaavklaringTjeneste.java
@@ -1,16 +1,16 @@
 package no.nav.melosys.tjenester.gui;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import org.springframework.context.annotation.Scope;
-import org.springframework.stereotype.Service;
-import org.springframework.web.context.WebApplicationContext;
-
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
+import org.springframework.web.context.WebApplicationContext;
 
 @Api(tags = {"faktaavklaring"})
 @Path("/faktaavklaring")
@@ -64,18 +64,17 @@ public class FaktaavklaringTjeneste extends RestTjeneste {
 
     @GET
     @Path("{behandlingID}")
-    @ApiOperation(value = "Utfører faktaavklaring på en gitt søknad")
-    public Response utførFaktaavklaring(@PathParam("behandlingID") long behandlingID) {
+    @ApiOperation(value = "Henter faktaavklaring for en gitt søknad")
+    public Response hentFaktaavklaring(@PathParam("behandlingID") long behandlingID) {
         // TODO Mock. Venter på avklaringer
         return Response.ok().entity(jsonFaktaAvklaring).build();
     }
 
     @POST
     @Path("{behandlingID}")
-    @ApiOperation(value = "Utfører faktaavklaring på en gitt søknad")
-    public Response utførFaktaavklaring(@PathParam("behandlingID") long behandlingID, String behandling) {
+    @ApiOperation(value = "Lagrer faktaavklaring for en gitt søknad")
+    public Response utførFaktaavklaring(@PathParam("behandlingID") long behandlingID, String body) {
         // TODO Mock. Venter på avklaringer
-        //return Response.ok().entity(behandling).build();
         return Response.ok().entity(jsonFaktaAvklaring).build();
     }
 }
