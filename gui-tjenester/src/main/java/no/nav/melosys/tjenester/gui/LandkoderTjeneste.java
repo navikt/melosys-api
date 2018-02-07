d200f1bdafa9ea8785018c2abc78f52c0d00ed1e
Francois Allix <francois.allix@nav.no>
2018-02-07 17:39:20 +0100

    Fikset mock /landkoder

--- gui-tjenester/src/main/java/no/nav/melosys/tjenester/gui/LandkoderTjeneste.java
+++ gui-tjenester/src/main/java/no/nav/melosys/tjenester/gui/LandkoderTjeneste.java
@@ -1,26 +1,27 @@
 package no.nav.melosys.tjenester.gui;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
 import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
-
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-import java.io.*;
-import java.nio.file.Files;
 
 @Api(tags = {"landkoder"})
 @Path("/landkoder")
 @Service
-@Scope(value= WebApplicationContext.SCOPE_REQUEST)
-@Transactional
+@Scope(value= WebApplicationContext.SCOPE_APPLICATION)
 public class LandkoderTjeneste extends RestTjeneste {
 
     private String jsonLandkoder;
@@ -28,7 +29,19 @@ public class LandkoderTjeneste extends RestTjeneste {
     @Autowired
     public LandkoderTjeneste(ResourceLoader resourceLoader) throws IOException {
         Resource resource = resourceLoader.getResource("classpath:landkoder.json");
-        jsonLandkoder = new String(Files.readAllBytes(resource.getFile().toPath()));
+
+        InputStream inputStream = resource.getInputStream();
+
+        StringBuilder stringBuilder = new StringBuilder();
+        try (Reader reader = new BufferedReader(new InputStreamReader
+                (inputStream, Charset.forName(StandardCharsets.UTF_8.name())))) {
+            int c = 0;
+            while ((c = reader.read()) != -1) {
+                stringBuilder.append((char) c);
+            }
+        }
+
+        jsonLandkoder = stringBuilder.toString();
     }
 
     @GET
