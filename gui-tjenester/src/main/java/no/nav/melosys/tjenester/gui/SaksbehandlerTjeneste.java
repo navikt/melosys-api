ffe2875aadaf1a8a2c8a4f4e182b3a42398bea0b
Francois Allix <francois.allix@nav.no>
2018-02-14 12:31:25 +0100

    MELOSYS-890 LDAP integrasjon

--- gui-tjenester/src/main/java/no/nav/melosys/tjenester/gui/SaksbehandlerTjeneste.java
+++ gui-tjenester/src/main/java/no/nav/melosys/tjenester/gui/SaksbehandlerTjeneste.java
@@ -1,15 +1,21 @@
 package no.nav.melosys.tjenester.gui;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import no.nav.melosys.integrasjon.felles.exception.IntegrasjonException;
+import no.nav.melosys.integrasjon.felles.exception.TekniskException;
+import no.nav.melosys.integrasjon.ldap.LdapBruker;
+import no.nav.melosys.integrasjon.ldap.LdapBrukeroppslag;
 import no.nav.melosys.sikkerhet.context.SpringSubjectHandler;
 import no.nav.melosys.tjenester.gui.dto.InnloggetBrukerDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 import org.springframework.web.context.WebApplicationContext;
-
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
 
 @Api(tags = {"saksbehandler"})
 @Path("/saksbehandler")
@@ -17,14 +23,23 @@ import javax.ws.rs.Path;
 @Scope(value= WebApplicationContext.SCOPE_REQUEST)
 public class SaksbehandlerTjeneste extends RestTjeneste {
 
+    private static final Logger log = LoggerFactory.getLogger(SaksbehandlerTjeneste.class);
+
     @GET
     @ApiOperation(value = "Returnerer fullt navn for ident",
             notes = ("Ident hentes fra sikkerhetskonteksten som er tilgjengelig etter innlogging."))
     public InnloggetBrukerDto innloggetBruker() {
-        // TODO Implementere LDAP oppslag
         String ident = SpringSubjectHandler.getUserID();
 
-       return new InnloggetBrukerDto(ident, "_TODO_");
+        LdapBruker ldapBruker;
+        try {
+            ldapBruker = new LdapBrukeroppslag().hentBrukerinformasjon(ident);
+        } catch (IntegrasjonException | TekniskException e) {
+            log.warn("", e);
+            throw e;
+        }
+
+       return new InnloggetBrukerDto(ident, ldapBruker.getDisplayName());
     }
 
 }
---
+++ integrasjon/src/main/java/no/nav/melosys/integrasjon/ldap/LdapInnlogging.java
@@ -0,0 +1,42 @@
+package no.nav.melosys.integrasjon.ldap;
+
+import java.util.Hashtable;
+import javax.naming.Context;
+import javax.naming.NamingException;
+import javax.naming.ldap.InitialLdapContext;
+import javax.naming.ldap.LdapContext;
+
+import no.nav.melosys.integrasjon.felles.exception.IntegrasjonException;
+import no.nav.melosys.integrasjon.felles.exception.TekniskException;
+
+public class LdapInnlogging {
+
+    private LdapInnlogging() {
+    }
+
+    public static LdapContext lagLdapContext() {
+        String url = getRequiredProperty("ldap.url");
+        String user = getRequiredProperty("ldap.username") + "@" + getRequiredProperty("ldap.domain");
+
+        Hashtable<String, Object> environment = new Hashtable<>(); //NOSONAR //metodeparameter krever Hashtable
+        environment.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
+        environment.put(Context.SECURITY_AUTHENTICATION, "simple");
+        environment.put(Context.SECURITY_CREDENTIALS, getRequiredProperty("ldap.password"));
+        environment.put(Context.SECURITY_PRINCIPAL, user);
+        environment.put(Context.PROVIDER_URL, url);
+        try {
+            return new InitialLdapContext(environment, null);
+        } catch (NamingException e) {
+            throw new IntegrasjonException("Klarte ikke å koble til LDAP på URL " + url);
+        }
+    }
+
+    static String getRequiredProperty(String navn) {
+        String verdi = System.getProperty(navn);
+        if (verdi == null || verdi.isEmpty()) {
+            throw new TekniskException("Klarte ikke koble til LDAP da påkrevd system property " + navn + " ikke er satt");
+        }
+        return verdi;
+
+    }
+}
