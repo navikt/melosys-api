3bd3969f2d417e4a029037e6a4354150b871c4a7
Francois Allix <francois.allix@nav.no>
2018-04-04 16:23:24 +0200

    Internt kodeverk som kreves i frontend.

---
+++ app/src/main/resources/db/migration/melosysDB/V1.0_10__LANDKODER.sql
@@ -0,0 +1,36 @@
+CREATE TABLE landkoder (
+    kode    VARCHAR2(99)  NOT NULL,
+    navn    VARCHAR2(99)  NOT NULL,
+    CONSTRAINT pk_landkoder PRIMARY KEY (kode)
+);
+
+INSERT INTO landkoder (kode, navn) VALUES ('BE', 'Belgia');
+INSERT INTO landkoder (kode, navn) VALUES ('BG', 'Bulgaria');
+INSERT INTO landkoder (kode, navn) VALUES ('DK', 'Danmark');
+INSERT INTO landkoder (kode, navn) VALUES ('EE', 'Estland');
+INSERT INTO landkoder (kode, navn) VALUES ('FI', 'Finland');
+INSERT INTO landkoder (kode, navn) VALUES ('FR', 'Frankrike');
+INSERT INTO landkoder (kode, navn) VALUES ('GR', 'Hellas');
+INSERT INTO landkoder (kode, navn) VALUES ('IE', 'Irland');
+INSERT INTO landkoder (kode, navn) VALUES ('IS', 'Island');
+INSERT INTO landkoder (kode, navn) VALUES ('IT', 'Italia');
+INSERT INTO landkoder (kode, navn) VALUES ('HR', 'Kroatia');
+INSERT INTO landkoder (kode, navn) VALUES ('CY', 'Kypros');
+INSERT INTO landkoder (kode, navn) VALUES ('LV', 'Latvia');
+INSERT INTO landkoder (kode, navn) VALUES ('LI', 'Liechtenstein');
+INSERT INTO landkoder (kode, navn) VALUES ('LT', 'Litauen');
+INSERT INTO landkoder (kode, navn) VALUES ('LU', 'Luxembourg');
+INSERT INTO landkoder (kode, navn) VALUES ('MT', 'Malta');
+INSERT INTO landkoder (kode, navn) VALUES ('NL', 'Nederland');
+INSERT INTO landkoder (kode, navn) VALUES ('NO', 'Norge');
+INSERT INTO landkoder (kode, navn) VALUES ('PL', 'Polen');
+INSERT INTO landkoder (kode, navn) VALUES ('PT', 'Portugal');
+INSERT INTO landkoder (kode, navn) VALUES ('RO', 'Romania');
+INSERT INTO landkoder (kode, navn) VALUES ('SK', 'Slovakia');
+INSERT INTO landkoder (kode, navn) VALUES ('SI', 'Slovenia');
+INSERT INTO landkoder (kode, navn) VALUES ('ES', 'Spania');
+INSERT INTO landkoder (kode, navn) VALUES ('GB', 'Storbritannia');
+INSERT INTO landkoder (kode, navn) VALUES ('SE', 'Sverige');
+INSERT INTO landkoder (kode, navn) VALUES ('DE', 'Tyskland');
+INSERT INTO landkoder (kode, navn) VALUES ('HU', 'Ungarn');
+INSERT INTO landkoder (kode, navn) VALUES ('AT', 'Østerrike');
---
+++ frontend-api/src/main/java/no/nav/melosys/tjenester/gui/dto/KodeverkDto.java
@@ -0,0 +1,42 @@
+package no.nav.melosys.tjenester.gui.dto;
+
+import java.util.List;
+
+public class KodeverkDto {
+    private List<KodeverdiDto> behandlingstyper;
+    private List<KodeverdiDto> behandlingsstatus;
+    private List<KodeverdiDto> landkoder;
+    private List<KodeverdiDto> sakstyper;
+
+    public List<KodeverdiDto> getBehandlingstyper() {
+        return behandlingstyper;
+    }
+
+    public void setBehandlingstyper(List<KodeverdiDto> behandlingstyper) {
+        this.behandlingstyper = behandlingstyper;
+    }
+
+    public List<KodeverdiDto> getBehandlingsstatus() {
+        return behandlingsstatus;
+    }
+
+    public void setBehandlingsstatus(List<KodeverdiDto> behandlingsstatus) {
+        this.behandlingsstatus = behandlingsstatus;
+    }
+
+    public List<KodeverdiDto> getLandkoder() {
+        return landkoder;
+    }
+
+    public void setLandkoder(List<KodeverdiDto> landkoder) {
+        this.landkoder = landkoder;
+    }
+
+    public List<KodeverdiDto> getSakstyper() {
+        return sakstyper;
+    }
+
+    public void setSakstyper(List<KodeverdiDto> sakstyper) {
+        this.sakstyper = sakstyper;
+    }
+}
