2de98ecd06b70eb0c4fa14452638731a0dbc0091
Ankur Tade <Ankur.Tade@nav.no>
2018-04-12 15:38:17 +0200

    MELOSYS-1123 : Erstattet sjekk fra Objects.isNull med "null =="

--- integrasjon/src/main/java/no/nav/melosys/integrasjon/felles/mdc/CallIdOutInterceptor.java
+++ integrasjon/src/main/java/no/nav/melosys/integrasjon/felles/mdc/CallIdOutInterceptor.java
@@ -1,7 +1,6 @@
 package no.nav.melosys.integrasjon.felles.mdc;
 
 import java.util.List;
-import java.util.Objects;
 import javax.xml.bind.JAXBException;
 
 import org.apache.cxf.binding.soap.SoapHeader;
@@ -30,11 +29,10 @@ public class CallIdOutInterceptor extends AbstractPhaseInterceptor<Message> {
     public void handleMessage(Message message) {
         //Call Id egentlig settes i MDCFilter og Forespørsel til gui tjeneste kaller MDCFilter. Gjennom initialisering av Melosys App, KodeVerkService henter kodeverk med bruk av Kodeverk Web Service og legger inn i cache. I det tilfelle MDCFilter blir ikke kallet fordi at det er ikke noen forespørsel fra gui tjeneste lag. Så vi må sette Call Id eksplisitt.
         //TODO: vi trenger bedre løsning enn bare sette Call Id fordi det kan påvirker alle tjenester siden alle bruker denne interceptor men det er veldig liten sannsynlighet at MDFilter klarer ikke å sette Call Id.
-        if (Objects.isNull(MDCOperations.getFromMDC(MDC_CALL_ID))) {
+        if (null == MDCOperations.getFromMDC(MDC_CALL_ID)) {
             putToMDC(MDC_CALL_ID, generateCallId());
         }
         String callId = MDCOperations.getFromMDC(MDC_CALL_ID);
-
         SoapMessage soapMessage;
         if (message instanceof SoapMessage) {
             soapMessage = (SoapMessage) message;
