99974ff925246f05b0bacb97b40cd2590b3275ad
Ankur Tade <Ankur.Tade@nav.no>
2018-04-13 10:43:55 +0200

    Melosys-1123: Fikset kode etter forslager i PR.

--- integrasjon/src/main/java/no/nav/melosys/integrasjon/felles/mdc/CallIdOutInterceptor.java
+++ integrasjon/src/main/java/no/nav/melosys/integrasjon/felles/mdc/CallIdOutInterceptor.java
@@ -14,6 +14,7 @@ import org.apache.cxf.phase.Phase;
 
 import static no.nav.melosys.integrasjon.felles.mdc.MDCOperations.MDC_CALL_ID;
 import static no.nav.melosys.integrasjon.felles.mdc.MDCOperations.generateCallId;
+import static no.nav.melosys.integrasjon.felles.mdc.MDCOperations.getFromMDC;
 import static no.nav.melosys.integrasjon.felles.mdc.MDCOperations.putToMDC;
 
 /**
@@ -27,12 +28,18 @@ public class CallIdOutInterceptor extends AbstractPhaseInterceptor<Message> {
 
     @Override
     public void handleMessage(Message message) {
-        //Call Id egentlig settes i MDCFilter og Forespørsel til gui tjeneste kaller MDCFilter. Gjennom initialisering av Melosys App, KodeVerkService henter kodeverk med bruk av Kodeverk Web Service og legger inn i cache. I det tilfelle MDCFilter blir ikke kallet fordi at det er ikke noen forespørsel fra gui tjeneste lag. Så vi må sette Call Id eksplisitt.
+        /*Call Id egentlig settes i MDCFilter og Forespørsel til gui tjeneste kaller MDCFilter.
+         *Gjennom initialisering av Melosys App, KodeVerkService henter kodeverk med bruk av Kodeverk Web Service og
+         * legger inn i cache. I det tilfelle MDCFilter blir ikke kallet fordi at det er ikke noen forespørsel
+         * fra gui tjeneste lag. Så vi må sette Call Id eksplisitt.
+         */
         //TODO: vi trenger bedre løsning enn bare sette Call Id fordi det kan påvirker alle tjenester siden alle bruker denne interceptor men det er veldig liten sannsynlighet at MDFilter klarer ikke å sette Call Id.
-        if (null == MDCOperations.getFromMDC(MDC_CALL_ID)) {
-            putToMDC(MDC_CALL_ID, generateCallId());
+        String callId = getFromMDC(MDC_CALL_ID);
+        if (callId == null) {
+            callId = generateCallId();
+            putToMDC(MDC_CALL_ID, callId);
         }
-        String callId = MDCOperations.getFromMDC(MDC_CALL_ID);
+
         SoapMessage soapMessage;
         if (message instanceof SoapMessage) {
             soapMessage = (SoapMessage) message;
