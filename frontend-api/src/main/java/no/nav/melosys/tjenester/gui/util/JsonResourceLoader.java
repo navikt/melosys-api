0699f00361e056dc3b8e835871f0348bb2fe2f7d
Bjølseth, Are <are.bjolseth@nav.no>
2018-05-28 16:22:34 +0200

    Added class

---
+++ frontend-api/src/main/java/no/nav/melosys/tjenester/gui/util/JsonResourceLoader.java
@@ -0,0 +1,33 @@
+package no.nav.melosys.tjenester.gui.util;
+
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+public class JsonResourceLoader {
+    public static String load(ResourceLoader resourceLoader, String filename) throws IOException {
+        String json;
+
+        Resource resource = resourceLoader.getResource("classpath:"+filename);
+
+        InputStream inputStream = resource.getInputStream();
+
+        StringBuilder stringBuilder = new StringBuilder();
+        try (Reader reader = new BufferedReader(new InputStreamReader
+                (inputStream, Charset.forName(StandardCharsets.UTF_8.name())))) {
+            int c = 0;
+            while ((c = reader.read()) != -1) {
+                stringBuilder.append((char) c);
+            }
+        }
+        json = stringBuilder.toString();
+        return json;
+    }
+}
