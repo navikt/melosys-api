673635f6ca458b10d505ddbec4168efa9f7df4bc
Francois Allix <francois.allix@nav.no>
2018-07-26 11:54:48 +0200

    MELOSYS-1398: Micrometer + Prometheus konfig

---
+++ config/src/main/java/no/nav/melosys/metrics/MetricsConfig.java
@@ -0,0 +1,31 @@
+package no.nav.melosys.metrics;
+
+import javax.annotation.PostConstruct;
+
+import io.micrometer.spring.autoconfigure.MeterRegistryCustomizer;
+import io.prometheus.client.CollectorRegistry;
+import io.prometheus.client.hotspot.DefaultExports;
+import io.prometheus.client.spring.boot.EnableSpringBootMetricsCollector;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableSpringBootMetricsCollector
+public class MetricsConfig {
+
+    @Bean
+    MeterRegistryCustomizer registryCustomizer() {
+        return registry -> registry.config().commonTags("team", "melosys");
+    }
+
+    // Denne gjør at Micrometer og Prometheus sin simpleclient bruker samme registry
+    @Bean
+    CollectorRegistry prometheusCollector() {
+        return CollectorRegistry.defaultRegistry;
+    }
+
+    @PostConstruct
+    public void prometheusHotspotConfig() {
+        DefaultExports.initialize();
+    }
+}
