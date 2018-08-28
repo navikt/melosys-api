3bb3c5ad74f51d4c8c7fac974f376514b86c5cc7
Francois Allix <francois.allix@nav.no>
2018-08-28 14:34:00 +0200

    JSON skjemaer lagres nå under /schema

--- frontend-api/src/test/java/no/nav/melosys/tjenester/gui/JsonSchemaTest.java
+++ frontend-api/src/test/java/no/nav/melosys/tjenester/gui/JsonSchemaTest.java
@@ -10,7 +10,7 @@ import org.springframework.core.io.DefaultResourceLoader;
 
 public abstract class JsonSchemaTest {
 
-    private static final String ROOT = "scripts/schema/";
+    private static final String ROOT = "schema/";
 
     public abstract String schemaNavn();
 
