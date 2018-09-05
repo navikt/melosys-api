a32aacafb01f07af0cbbbd2053d89fbba7ba4bc0
Francois Allix <francois.allix@nav.no>
2018-09-05 14:55:45 +0200

    MELOSYS-1291: Bruk av historisk statsborgerskap ved søknader tilbake i tid

--- saksflyt/src/test/java/no/nav/melosys/saksflyt/agent/jfr/VurderInngangsvilkaarTest.java
+++ saksflyt/src/test/java/no/nav/melosys/saksflyt/agent/jfr/VurderInngangsvilkaarTest.java
@@ -2,10 +2,14 @@ package no.nav.melosys.saksflyt.agent.jfr;
 
 import java.time.LocalDate;
 import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 import no.nav.melosys.domain.*;
 import no.nav.melosys.domain.dokument.felles.Land;
 import no.nav.melosys.domain.dokument.person.PersonDokument;
+import no.nav.melosys.domain.dokument.person.PersonhistorikkDokument;
+import no.nav.melosys.domain.dokument.person.StatsborgerskapPeriode;
 import no.nav.melosys.regler.api.lovvalg.rep.Feilmelding;
 import no.nav.melosys.regler.api.lovvalg.rep.Kategori;
 import no.nav.melosys.regler.api.lovvalg.rep.VurderInngangsvilkaarReply;
@@ -85,6 +89,50 @@ public class VurderInngangsvilkaarTest {
         assertEquals(2, p.getHendelser().size());
         assertNull(p.getBehandling().getFagsak().getType());
         assertEquals(ProsessSteg.FEILET_MASKINELT, p.getSteg());
+    }
+
+    @Test
+    public void utfoerStegMedHistorikkStatsborgerskap() {
+        // Sett opp input...
+        Prosessinstans p = new Prosessinstans();
+        p.setBehandling(new Behandling());
+        p.getBehandling().setFagsak(new Fagsak());
+        PersonDokument pDok = new PersonDokument();
+        pDok.statsborgerskap = new Land("NOR");
+        Saksopplysning sopp = new Saksopplysning();
+        sopp.setType(SaksopplysningType.PERSONOPPLYSNING);
+        sopp.setDokument(pDok);
+
+        Saksopplysning historiskSopp = new Saksopplysning();
+        historiskSopp.setType(SaksopplysningType.PERSONHISTORIKK);
+        PersonhistorikkDokument personhistorikkDokument = new PersonhistorikkDokument();
+        historiskSopp.setDokument(personhistorikkDokument);
+        StatsborgerskapPeriode statsborgerskapPeriode = new StatsborgerskapPeriode();
+        statsborgerskapPeriode.statsborgerskap = new Land("IRL");
+        personhistorikkDokument.statsborgerskapListe.add(statsborgerskapPeriode);
+
+        Set<Saksopplysning> saksopplysninger = new HashSet<>();
+        saksopplysninger.add(sopp);
+        saksopplysninger.add(historiskSopp);
+
+        p.getBehandling().setSaksopplysninger(saksopplysninger);
+        p.setData(ProsessDataKey.LAND, Collections.singletonList(Landkoder.CH.getKode()));
+        p.setData(ProsessDataKey.SØKNADSPERIODE, new PeriodeDto(LocalDate.now().minusMonths(6), null));
+
+        // Sett opp regelmodulService til å alltid returnere EØS og ingen feil.
+        VurderInngangsvilkaarReply res = new VurderInngangsvilkaarReply();
+        res.feilmeldinger = Collections.emptyList();
+        res.kvalifisererForEf883_2004 = true;
+        when(regelmodulService.vurderInngangsvilkår(any(), any(), any())).thenReturn(res);
+        when(behandlingRepository.findOne(any())).thenReturn(p.getBehandling());
+
+        agent.utførSteg(p);
+
+        verify(fagsakRepository, times(1)).save(any(Fagsak.class));
+
+        assertNull(p.getHendelser());
+        assertEquals(FagsakType.EU_EØS, p.getBehandling().getFagsak().getType());
+        assertEquals(ProsessSteg.HENT_ARBF_OPPL, p.getSteg());
     }
 
     public static Prosessinstans lagProsessinstans() {
