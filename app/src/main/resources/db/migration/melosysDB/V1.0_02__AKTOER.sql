d2ccb6668da41a7c9bf6890d89dec207596dcbef
Francois Allix <francois.allix@nav.no>
2018-09-17 10:53:30 +0200

    Kodeverk for roller i Aktoer

--- app/src/main/resources/db/migration/melosysDB/V1.0_02__AKTOER.sql
+++ app/src/main/resources/db/migration/melosysDB/V1.0_02__AKTOER.sql
@@ -14,8 +14,9 @@ CREATE TABLE rolle_type (
     CONSTRAINT pk_rolle PRIMARY KEY (kode)
 );
 
-INSERT INTO rolle_type (kode, navn) VALUES ('BRUKER', 'Bruker');
-INSERT INTO rolle_type (kode, navn) VALUES ('ARBGIV', 'Arbeidsgiver');
-INSERT INTO rolle_type (kode, navn) VALUES ('REPRES', 'Representant ');
+INSERT INTO rolle_type (kode, navn) VALUES ('BRUKER', 'Personen som avklaringen lovvalg eller medlemskap gjelder for.');
+INSERT INTO rolle_type (kode, navn) VALUES ('ARBEIDSGIVER', 'Arbeidsgiver som sender bruker for arbeid eller oppdrag i utlandet.');
+INSERT INTO rolle_type (kode, navn) VALUES ('REPRESENTANT', 'Aktøren representerer bruker og/eller arbeidsgiver is saken.');
+INSERT INTO rolle_type (kode, navn) VALUES ('MYNDIGHET', 'Myndigheten det sendes til og/eller mottas dokumentasjon fra i saken.');
 
 ALTER TABLE aktoer ADD CONSTRAINT fk_aktoer_rolle FOREIGN KEY (rolle) REFERENCES rolle_type;
