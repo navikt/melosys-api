b2003bacb7e33655d4431e955d33cd3a30be7a20
Ankur Tade <Ankur.Tade@nav.no>
2018-10-15 16:38:35 +0200

    Slettet method hente FagsakbyGsak nummer som ikke i bruk

--- repository/src/main/java/no/nav/melosys/repository/FagsakRepository.java
+++ repository/src/main/java/no/nav/melosys/repository/FagsakRepository.java
@@ -12,8 +12,6 @@ public interface FagsakRepository extends CrudRepository<Fagsak, Long> {
 
     Fagsak findBySaksnummer(String saksnummer);
 
-    Fagsak findByGsakSaksnummer(Long gsakSakID);
-
     @Query("select f from Fagsak f, Aktoer a where a.fagsak = f and a.rolle = :rolle  and a.aktørId = :id") //$NON-NLS-1$
     List<Fagsak> findByRolleAndAktør(@Param("rolle") RolleType rolle, @Param("id") String aktørID);
 
