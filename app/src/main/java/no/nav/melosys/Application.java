54c28319fa68007528613042f29de699e24acf1a
Francois Allix <francois.allix@nav.no>
2018-11-02 13:26:43 +0100

    Fjernet SpringBootServletInitializer siden Melosys ikke leveres som war-fil

--- app/src/main/java/no/nav/melosys/Application.java
+++ app/src/main/java/no/nav/melosys/Application.java
@@ -2,9 +2,7 @@ package no.nav.melosys;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Controller;
@@ -17,15 +15,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @PropertySource(value = "classpath:service.properties", encoding = "utf-8")
 @SpringBootApplication
 @Controller
-public class Application extends SpringBootServletInitializer {
+public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(Application.class);
     }
 
     @Bean
