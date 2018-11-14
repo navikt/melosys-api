d5578f45f890ade94dc90331c9a52f37cc61e190
Francois Allix <francois.allix@nav.no>
2018-11-14 13:54:40 +0100

    Fjernet warning
    Encountered Java type [no.nav.melosys.domain.bestemmelse.Lovvalg Bestemmelse] for which we could not locate a Java TypeDescriptor and which does not appear to implement equals and/or hashCode. This can lead to significant performance problems when performing equality/dirty checking involving this Java type. Consider registering a custom JavaTypeDescriptor or at least implementing equals/hashCode.

--- domain/src/main/java/no/nav/melosys/domain/bestemmelse/LovvalgBestemmelse.java
+++ domain/src/main/java/no/nav/melosys/domain/bestemmelse/LovvalgBestemmelse.java
@@ -5,4 +5,10 @@ import no.nav.melosys.domain.Kodeverk;
 public interface LovvalgBestemmelse extends Kodeverk {
 
     String name();
+
+    // Til Hibernate https://hibernate.atlassian.net/browse/HHH-10858
+    boolean equals(Object other);
+
+    // Til Hibernate https://hibernate.atlassian.net/browse/HHH-10858
+    int hashCode();
 }
