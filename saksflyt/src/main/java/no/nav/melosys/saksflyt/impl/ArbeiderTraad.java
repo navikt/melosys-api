79f9b870596020e7996401b45c276e046f07e929
Francois Allix <francois.allix@nav.no>
2018-11-15 14:03:51 +0100

    Detaljerer logging i ArbeiderTraad

--- saksflyt/src/main/java/no/nav/melosys/saksflyt/impl/ArbeiderTraad.java
+++ saksflyt/src/main/java/no/nav/melosys/saksflyt/impl/ArbeiderTraad.java
@@ -37,7 +37,7 @@ public class ArbeiderTraad extends Thread {
     @SuppressWarnings("unused")
     private ArbeiderTraad() {} // Skal ikke håndteres av Spring
     
-    public ArbeiderTraad(Binge binge, ProsessinstansRepository prosessinstansRepo, List<StegBehandler> stegBehandlere, long oppholdMellomSteg) {
+    ArbeiderTraad(Binge binge, ProsessinstansRepository prosessinstansRepo, List<StegBehandler> stegBehandlere, long oppholdMellomSteg) {
         this.binge = binge;
         this.prosessinstansRepo = prosessinstansRepo;
         this.stegBehandlere = stegBehandlere;
@@ -83,7 +83,8 @@ public class ArbeiderTraad extends Thread {
                     }
                 } catch (RuntimeException e) {
                     // Vi er her hvis en StegBehandler kastet en Exception 
-                    logger.error("Ubehandlet Exception. Aktiv stegBehandler: {}, Prosessinstans som kanskje må ryddes opp i: {}", aktivStegBehandler.getClass().getName(), aktivProsessinstans.getId(), e);
+                    logger.error("Ubehandlet Exception! Arbeider stopper. Aktiv stegBehandler var: {}.", aktivStegBehandler.getClass().getSimpleName());
+                    logger.error("Prosessinstans som kanskje må ryddes opp i: {}", aktivProsessinstans.getId(), e);
                     setAktivPiTilFeilet();
                     return;
                 }
@@ -94,7 +95,7 @@ public class ArbeiderTraad extends Thread {
     private void setAktivPiTilFeilet() {
         try {
             aktivProsessinstans.setSteg(ProsessSteg.FEILET_MASKINELT);
-            prosessinstansRepo.save(aktivProsessinstans); // Kan resultere i DataAccessException, som kastes videre og merfører at tråden stoppes.
+            prosessinstansRepo.save(aktivProsessinstans);
             logger.error("Prosessinstans {} ble satt til feilet", aktivProsessinstans.getId());
         } catch (RuntimeException e) {
             logger.error("Kunne ikke sette prosessinstans {} til feilet", aktivProsessinstans.getId(), e);
