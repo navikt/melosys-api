8d4bfd980b2922d9caad46c6121e67c10e35ab18
Francois Allix <francois.allix@nav.no>
2018-11-19 11:03:03 +0100

    Oppfriskning: feilet instanser er ikke aktive

--- repository/src/main/java/no/nav/melosys/repository/ProsessinstansRepository.java
+++ repository/src/main/java/no/nav/melosys/repository/ProsessinstansRepository.java
@@ -2,11 +2,12 @@ package no.nav.melosys.repository;
 
 import java.util.Optional;
 
+import no.nav.melosys.domain.ProsessSteg;
 import no.nav.melosys.domain.ProsessType;
 import no.nav.melosys.domain.Prosessinstans;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ProsessinstansRepository extends CrudRepository<Prosessinstans, Long> {
-    Optional<Prosessinstans> findByStegIsNotNullAndBehandling_Id(long id);
+    Optional<Prosessinstans> findByStegIsNotNullAndStegIsNotAndBehandling_Id(ProsessSteg prosessSteg, long id);
     Optional<Prosessinstans> findByStegIsNotNullAndTypeAndBehandling_Id(ProsessType prosessType, long id);
 }
