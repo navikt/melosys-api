02293915f21ba6154c47f702b14675fba0e3accb
Eivind K. Nilsbakken <eivind.nilsbakken@nav.no>
2018-12-10 11:12:59 +0100

    MELOSYS-1868: Legg til index på behandling_id i tidligere_medlemsperiode-tabell

--- app/src/main/resources/db/migration/melosysDB/V1.0_12__TIDLIGERE_MEDLEMSPERIODE.sql
+++ app/src/main/resources/db/migration/melosysDB/V1.0_12__TIDLIGERE_MEDLEMSPERIODE.sql
@@ -1,6 +1,8 @@
 CREATE TABLE tidligere_medlemsperiode (
     behandling_id   NUMBER(19) NOT NULL,
     periode_id      NUMBER(19) NOT NULL,
-    CONSTRAINT medlemsperiode_behandling_pk PRIMARY KEY (behandling_id, periode_id),
-    CONSTRAINT medlemsperiode_behandling_fk FOREIGN KEY (behandling_id) REFERENCES behandling
+    CONSTRAINT pk_medlemsperiode_behandling PRIMARY KEY (behandling_id, periode_id),
+    CONSTRAINT fk_medlemsperiode_behandling FOREIGN KEY (behandling_id) REFERENCES behandling
 );
+
+CREATE INDEX idx_medlemsperiode_behandling ON tidligere_medlemsperiode (behandling_id);
