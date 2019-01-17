f8b69af2894d6c44c5d051627d804dfe11004bb3
Bjørnar Askevold <bjornar.askevold@nav.no>
2019-01-17 16:52:03 +0100

    Rettet avvik i dto mellom frontend/backend

--- service/src/main/java/no/nav/melosys/service/dokument/brev/BrevbestillingDto.java
+++ service/src/main/java/no/nav/melosys/service/dokument/brev/BrevbestillingDto.java
@@ -1,5 +1,6 @@
 package no.nav.melosys.service.dokument.brev;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import no.nav.melosys.domain.RolleType;
 
 public class BrevbestillingDto {
@@ -8,5 +9,6 @@ public class BrevbestillingDto {
 
     public String fritekst;
 
+    @JsonProperty("begrunnelse")
     public String begrunnelseKode;
 }
