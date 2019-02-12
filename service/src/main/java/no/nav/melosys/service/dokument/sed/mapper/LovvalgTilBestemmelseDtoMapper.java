50e8820509870b6ad62814ba71fe85e1e301d97e
Andreas Strand <andreas.strand@nav.no>
2019-02-12 10:27:14 +0100

    MELOSYS-2210 Gjort om LovvalgTilBestemmelseDtoMapper til Map. Også lagt til test

--- service/src/main/java/no/nav/melosys/service/dokument/sed/mapper/LovvalgTilBestemmelseDtoMapper.java
+++ service/src/main/java/no/nav/melosys/service/dokument/sed/mapper/LovvalgTilBestemmelseDtoMapper.java
@@ -1,5 +1,8 @@
 package no.nav.melosys.service.dokument.sed.mapper;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import no.nav.melosys.domain.bestemmelse.LovvalgBestemmelse;
 import no.nav.melosys.domain.bestemmelse.LovvalgBestemmelse_883_2004;
 import no.nav.melosys.domain.bestemmelse.LovvalgBestemmelse_987_2009;
@@ -7,68 +10,33 @@ import no.nav.melosys.integrasjon.eessi.dto.Bestemmelse;
 
 public class LovvalgTilBestemmelseDtoMapper {
 
+    private static final Map<LovvalgBestemmelse, Bestemmelse> mapper = new HashMap<>();
+
+    static {
+        mapper.put(LovvalgBestemmelse_883_2004.FO_883_2004_ART11_1, Bestemmelse.ART_11_1);
+        mapper.put(LovvalgBestemmelse_883_2004.FO_883_2004_ART11_3A, Bestemmelse.ART_11_3_a);
+        mapper.put(LovvalgBestemmelse_883_2004.FO_883_2004_ART11_3B, Bestemmelse.ART_11_3_b);
+        mapper.put(LovvalgBestemmelse_883_2004.FO_883_2004_ART11_3C, Bestemmelse.ART_11_3_c);
+        mapper.put(LovvalgBestemmelse_883_2004.FO_883_2004_ART11_3D, Bestemmelse.ART_11_3_d);
+        mapper.put(LovvalgBestemmelse_883_2004.FO_883_2004_ART11_3E, Bestemmelse.ART_11_3_e);
+        mapper.put(LovvalgBestemmelse_883_2004.FO_883_2004_ART12_1, Bestemmelse.ART_12_1);
+        mapper.put(LovvalgBestemmelse_883_2004.FO_883_2004_ART12_2, Bestemmelse.ART_12_2);
+        mapper.put(LovvalgBestemmelse_883_2004.FO_883_2004_ART13_1A, Bestemmelse.ART_13_1_a);
+        mapper.put(LovvalgBestemmelse_883_2004.FO_883_2004_ART13_1B1, Bestemmelse.ART_13_1_b_1);
+        mapper.put(LovvalgBestemmelse_883_2004.FO_883_2004_ART13_1B2, Bestemmelse.ART_13_1_b_2);
+        mapper.put(LovvalgBestemmelse_883_2004.FO_883_2004_ART13_1B3, Bestemmelse.ART_13_1_b_3);
+        mapper.put(LovvalgBestemmelse_883_2004.FO_883_2004_ART13_1B4, Bestemmelse.ART_13_1_b_4);
+        mapper.put(LovvalgBestemmelse_883_2004.FO_883_2004_ART13_2A, Bestemmelse.ART_13_2_a);
+        mapper.put(LovvalgBestemmelse_883_2004.FO_883_2004_ART13_2B, Bestemmelse.ART_13_2_b);
+        mapper.put(LovvalgBestemmelse_987_2009.FO_987_2009_ART14_11, Bestemmelse.ART_14_11);
+        mapper.put(LovvalgBestemmelse_883_2004.FO_883_2004_ART16_1, Bestemmelse.ART_16_1);
+        mapper.put(LovvalgBestemmelse_883_2004.FO_883_2004_ART16_2, Bestemmelse.ART_16_2);
+    }
 
     public static Bestemmelse mapMelosysLovvalgTilBestemmelseDto(LovvalgBestemmelse lovvalgBestemmelse) {
-        if (lovvalgBestemmelse instanceof LovvalgBestemmelse_883_2004) {
-            return mapLovvalgBestemmelse_883_2004TilBestemmelseDto((LovvalgBestemmelse_883_2004) lovvalgBestemmelse);
-        } else if (lovvalgBestemmelse instanceof LovvalgBestemmelse_987_2009) {
-            return mapLovvalgBestemmelse_987_2009TilBestemmelseDto((LovvalgBestemmelse_987_2009) lovvalgBestemmelse);
-        }
 
-        throw new RuntimeException("Støtte for kode: " + lovvalgBestemmelse.getKode() + " er ikke implementert");
-    }
-
-    private static Bestemmelse mapLovvalgBestemmelse_987_2009TilBestemmelseDto(LovvalgBestemmelse_987_2009 lovvalgBestemmelse) {
-        switch (lovvalgBestemmelse) {
-            case FO_987_2009_ART14_11:
-                return Bestemmelse.ART_14_11;
-        }
-
-        throw new RuntimeException("Støtte for kode: " + lovvalgBestemmelse.getKode() + " er ikke implementert");
-    }
-
-    private static Bestemmelse mapLovvalgBestemmelse_883_2004TilBestemmelseDto(LovvalgBestemmelse_883_2004 lovvalgBestemmelse) {
-        switch (lovvalgBestemmelse) {
-            case FO_883_2004_ART11_1:
-                return Bestemmelse.ART_11_1;
-            case FO_883_2004_ART11_3A:
-                return Bestemmelse.ART_11_3_a;
-            case FO_883_2004_ART11_3B:
-                return Bestemmelse.ART_11_3_b;
-            case FO_883_2004_ART11_3C:
-                return Bestemmelse.ART_11_3_c;
-            case FO_883_2004_ART11_3D:
-                return Bestemmelse.ART_11_3_d;
-            case FO_883_2004_ART11_3E:
-                return Bestemmelse.ART_11_3_e;
-            case FO_883_2004_ART11_4_2:
-                return Bestemmelse.ART_11_4_2;
-            case FO_883_2004_ART12_1:
-                return Bestemmelse.ART_12_1;
-            case FO_883_2004_ART12_2:
-                return Bestemmelse.ART_12_2;
-            case FO_883_2004_ART13_1A:
-                return Bestemmelse.ART_13_1_a;
-            case FO_883_2004_ART13_1B1:
-                return Bestemmelse.ART_13_1_b_1;
-            case FO_883_2004_ART13_1B2:
-                return Bestemmelse.ART_13_1_b_2;
-            case FO_883_2004_ART13_1B3:
-                return Bestemmelse.ART_13_1_b_3;
-            case FO_883_2004_ART13_1B4:
-                return Bestemmelse.ART_13_1_b_4;
-            case FO_883_2004_ART13_2A:
-                return Bestemmelse.ART_13_2_a;
-            case FO_883_2004_ART13_2B:
-                return Bestemmelse.ART_13_2_b;
-            case FO_883_2004_ART13_3:
-                return Bestemmelse.ART_13_3;
-            case FO_883_2004_ART13_4:
-                return Bestemmelse.ART_13_4;
-            case FO_883_2004_ART16_1:
-                return Bestemmelse.ART_16_1;
-            case FO_883_2004_ART16_2:
-                return Bestemmelse.ART_16_2;
+        if (lovvalgBestemmelse != null && mapper.containsKey(lovvalgBestemmelse)) {
+            return mapper.get(lovvalgBestemmelse);
         }
 
         throw new RuntimeException("Støtte for kode: " + lovvalgBestemmelse.getKode() + " er ikke implementert");
---
+++ service/src/test/java/no/nav/melosys/service/dokument/sed/mapper/LovvalgTilBestemmelseDtoMapperTest.java
@@ -0,0 +1,41 @@
+package no.nav.melosys.service.dokument.sed.mapper;
+
+import no.nav.melosys.domain.bestemmelse.LovvalgBestemmelse;
+import no.nav.melosys.domain.bestemmelse.LovvalgBestemmelse_883_2004;
+import no.nav.melosys.integrasjon.eessi.dto.Bestemmelse;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class LovvalgTilBestemmelseDtoMapperTest {
+
+    @Test
+    public void map12_1TilBestemmelseDto_forventKorrektBestemmelse() {
+        LovvalgBestemmelse lovvalgBestemmelse = LovvalgBestemmelse_883_2004.FO_883_2004_ART12_1;
+        Bestemmelse resultat = LovvalgTilBestemmelseDtoMapper.mapMelosysLovvalgTilBestemmelseDto(lovvalgBestemmelse);
+
+        assertThat(resultat, is(Bestemmelse.ART_12_1));
+    }
+    @Test
+    public void map12_2TilBestemmelseDto_forventKorrektBestemmelse() {
+        LovvalgBestemmelse lovvalgBestemmelse = LovvalgBestemmelse_883_2004.FO_883_2004_ART12_2;
+        Bestemmelse resultat = LovvalgTilBestemmelseDtoMapper.mapMelosysLovvalgTilBestemmelseDto(lovvalgBestemmelse);
+
+        assertThat(resultat, is(Bestemmelse.ART_12_2));
+    }
+
+    @Test
+    public void map16_2TilBestemmelseDto_forventKorrektBestemmelse() {
+        LovvalgBestemmelse lovvalgBestemmelse = LovvalgBestemmelse_883_2004.FO_883_2004_ART16_2;
+        Bestemmelse resultat = LovvalgTilBestemmelseDtoMapper.mapMelosysLovvalgTilBestemmelseDto(lovvalgBestemmelse);
+
+        assertThat(resultat, is(Bestemmelse.ART_16_2));
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void mapNullTilBestemmelseDto_forventException() {
+        LovvalgTilBestemmelseDtoMapper.mapMelosysLovvalgTilBestemmelseDto(null);
+    }
+
+}
