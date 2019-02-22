ff999948837fe8d39eeda7aca55bc16fab278057
Sigbjørn Myhre <sigbjorn.myhre@nav.no>
2019-02-22 08:42:39 +0100

    Bruker rett kodeverksklasse for mapping av vilkaarsbegrunnelse

--- service/src/main/java/no/nav/melosys/service/dokument/sed/mapper/VilkaarsresultatTilBegrunnelseMapper.java
+++ service/src/main/java/no/nav/melosys/service/dokument/sed/mapper/VilkaarsresultatTilBegrunnelseMapper.java
@@ -1,25 +1,52 @@
 package no.nav.melosys.service.dokument.sed.mapper;
 
-import java.util.Set;
+import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
 
 import no.nav.melosys.domain.VilkaarBegrunnelse;
 import no.nav.melosys.domain.Vilkaarsresultat;
-import no.nav.melosys.domain.kodeverk.Art16_1_Avslag__Begrunnelser;
+import no.nav.melosys.domain.kodeverk.Art12_1_Begrunnelser;
+import no.nav.melosys.domain.kodeverk.Art16_1_Anmodning_Begrunnelser;
 
 public class VilkaarsresultatTilBegrunnelseMapper {
 
     public static String mapVilkaarsresultatTilBegrunnelseString(Vilkaarsresultat vilkaarsresultat) {
 
-        Set<String> begrunnelser = vilkaarsresultat.getBegrunnelser().stream()
+        List<String> begrunnelser = vilkaarsresultat.getBegrunnelser().stream()
             .map(VilkaarBegrunnelse::getKode)
-            .map(kode -> Art16_1_Avslag__Begrunnelser.valueOf(kode).getBeskrivelse())
-            .collect(Collectors.toSet());
+            .map(VilkaarsresultatTilBegrunnelseMapper::getBeskrivelse)
+            .collect(Collectors.toList());
 
-        if (begrunnelser.contains(Art16_1_Avslag__Begrunnelser.SAERLIG_AVSLAGSGRUNN.getBeskrivelse())) {
-            return vilkaarsresultat.getBegrunnelseFritekst();
-        }
+        // Særlig grunn blir byttet ut med verdi i fritekst-felt
+        begrunnelser.replaceAll(begrunnelse -> {
+            if (begrunnelse.equals(Art16_1_Anmodning_Begrunnelser.SAERLIG_GRUNN.getBeskrivelse())) {
+                return vilkaarsresultat.getBegrunnelseFritekst();
+            }
+
+            return begrunnelse;
+        });
 
         return String.join("\n", begrunnelser);
+    }
+
+    private static String getBeskrivelse(String kode) {
+        if (isArt12_1_Begrunnelse(kode)) {
+            return Art12_1_Begrunnelser.valueOf(kode).getBeskrivelse();
+        } else if (isArt16_1_Anmodning_Begrunnelse(kode)) {
+            return Art16_1_Anmodning_Begrunnelser.valueOf(kode).getBeskrivelse();
+        } else {
+            return kode;
+        }
+    }
+
+    private static boolean isArt12_1_Begrunnelse(String kode) {
+        return Arrays.stream(Art12_1_Begrunnelser.values())
+            .anyMatch(begrunnelse -> begrunnelse.getKode().equals(kode));
+    }
+
+    private static boolean isArt16_1_Anmodning_Begrunnelse(String kode) {
+        return Arrays.stream(Art16_1_Anmodning_Begrunnelser.values())
+            .anyMatch(begrunnelse -> begrunnelse.getKode().equals(kode));
     }
 }
--- service/src/test/java/no/nav/melosys/service/dokument/sed/mapper/VilkaarsresultatTilBegrunnelseMapperTest.java
+++ service/src/test/java/no/nav/melosys/service/dokument/sed/mapper/VilkaarsresultatTilBegrunnelseMapperTest.java
@@ -12,9 +12,9 @@ import static org.junit.Assert.assertThat;
 public class VilkaarsresultatTilBegrunnelseMapperTest {
 
     @Test
-    public void mapVilkaarsresultatTilBegrunnelseString_forventSøktForSentBeskrivelse() {
+    public void mapVilkaarsresultatTilBegrunnelseString_medArt12_1_forventUtsendelseOver24MndBeskrivelse() {
         VilkaarBegrunnelse vilkaarBegrunnelse = new VilkaarBegrunnelse();
-        vilkaarBegrunnelse.setKode("SOEKT_FOR_SENT");
+        vilkaarBegrunnelse.setKode("UTSENDELSE_OVER_24_MN");
 
         Set<VilkaarBegrunnelse> vilkaarBegrunnelser = new HashSet<>();
         vilkaarBegrunnelser.add(vilkaarBegrunnelse);
@@ -23,56 +23,71 @@ public class VilkaarsresultatTilBegrunnelseMapperTest {
         vilkaarsresultat.setBegrunnelser(vilkaarBegrunnelser);
 
         assertThat(VilkaarsresultatTilBegrunnelseMapper.mapVilkaarsresultatTilBegrunnelseString(vilkaarsresultat),
-                is("Søkt mer enn ett år etter perioden startet"));
+            is("Utsendelseperioden overskrider 24 måneder"));
+    }
+
+    @Test
+    public void mapVilkaarsresultatTilBegrunnelseString_medArt16_1_forventUtsendelseMellom24MndOg5AarBeskrivelse() {
+        VilkaarBegrunnelse vilkaarBegrunnelse = new VilkaarBegrunnelse();
+        vilkaarBegrunnelse.setKode("UTSENDELSE_MELLOM_24_MN_OG_5_AAR");
+
+        Set<VilkaarBegrunnelse> vilkaarBegrunnelser = new HashSet<>();
+        vilkaarBegrunnelser.add(vilkaarBegrunnelse);
+
+        Vilkaarsresultat vilkaarsresultat = new Vilkaarsresultat();
+        vilkaarsresultat.setBegrunnelser(vilkaarBegrunnelser);
+
+        assertThat(VilkaarsresultatTilBegrunnelseMapper.mapVilkaarsresultatTilBegrunnelseString(vilkaarsresultat),
+                is("Utsendelseperioden er mellom 2 og 5 år"));
     }
 
     @Test
     public void mapVilkaarsresultatTilBegrunnelseString_forventFritekst() {
         VilkaarBegrunnelse vilkaarBegrunnelse = new VilkaarBegrunnelse();
-        vilkaarBegrunnelse.setKode("SAERLIG_AVSLAGSGRUNN");
+        vilkaarBegrunnelse.setKode("SAERLIG_GRUNN");
 
         Set<VilkaarBegrunnelse> vilkaarBegrunnelser = new HashSet<>();
         vilkaarBegrunnelser.add(vilkaarBegrunnelse);
 
         Vilkaarsresultat vilkaarsresultat = new Vilkaarsresultat();
         vilkaarsresultat.setBegrunnelser(vilkaarBegrunnelser);
-        vilkaarsresultat.setBegrunnelseFritekst("Fritekst som beskriver avslagsgrunn");
+        vilkaarsresultat.setBegrunnelseFritekst("Fritekst som beskriver anmodning om unntak");
 
         assertThat(VilkaarsresultatTilBegrunnelseMapper.mapVilkaarsresultatTilBegrunnelseString(vilkaarsresultat),
-            is("Fritekst som beskriver avslagsgrunn"));
+            is("Fritekst som beskriver anmodning om unntak"));
     }
 
     @Test
     public void mapVilkaarsresultatTilBegrunnelseString_medFlereBegrunnelser_forventSammensattBegrunnelse() {
-        VilkaarBegrunnelse vilkaarBegrunnelseForSent = new VilkaarBegrunnelse();
-        vilkaarBegrunnelseForSent.setKode("SOEKT_FOR_SENT");
-        VilkaarBegrunnelse vilkaarBegrunnelseErstatter = new VilkaarBegrunnelse();
-        vilkaarBegrunnelseErstatter.setKode("ERSTATTER_EN_ANNEN_SAMLET_OVER_5_AAR");
+        VilkaarBegrunnelse vilkaarBegrunnelseUtsendelse = new VilkaarBegrunnelse();
+        vilkaarBegrunnelseUtsendelse.setKode("UTSENDELSE_OVER_24_MN");
+        VilkaarBegrunnelse vilkaarBegrunnelseIdeellOrganisasjon = new VilkaarBegrunnelse();
+        vilkaarBegrunnelseIdeellOrganisasjon.setKode("IDEELL_ORGANISASJON_IKKE_VESENTLIG_VIRK");
 
         Set<VilkaarBegrunnelse> vilkaarBegrunnelser = new HashSet<>();
-        vilkaarBegrunnelser.add(vilkaarBegrunnelseForSent);
-        vilkaarBegrunnelser.add(vilkaarBegrunnelseErstatter);
+        vilkaarBegrunnelser.add(vilkaarBegrunnelseUtsendelse);
+        vilkaarBegrunnelser.add(vilkaarBegrunnelseIdeellOrganisasjon);
 
         Vilkaarsresultat vilkaarsresultat = new Vilkaarsresultat();
         vilkaarsresultat.setBegrunnelser(vilkaarBegrunnelser);
         vilkaarsresultat.setBegrunnelseFritekst("Fritekst som ikke skal vises");
 
         assertThat(VilkaarsresultatTilBegrunnelseMapper.mapVilkaarsresultatTilBegrunnelseString(vilkaarsresultat),
-            is("Erstatter en annen utsendt person, samlet periode mer enn 5 år\nSøkt mer enn ett år etter perioden startet"));
+            is("Utsendelseperioden overskrider 24 måneder\nArbeider for ideell organisasjon som ikke har vesentlig virksomhet i Norge"));
     }
 
     @Test
-    public void mapVilkaarsresultatTilBegrunnelseString_medFlereBegrunnelserOgFritekst_forventFritekst() {
-        VilkaarBegrunnelse vilkaarBegrunnelseForSent = new VilkaarBegrunnelse();
-        vilkaarBegrunnelseForSent.setKode("SOEKT_FOR_SENT");
-        VilkaarBegrunnelse vilkaarBegrunnelseErstatter = new VilkaarBegrunnelse();
-        vilkaarBegrunnelseErstatter.setKode("ERSTATTER_EN_ANNEN_SAMLET_OVER_5_AAR");
+    public void mapVilkaarsresultatTilBegrunnelseString_medFlereBegrunnelserOgFritekst_forventSammensattBegrunnelseOgFritekst() {
+        VilkaarBegrunnelse vilkaarBegrunnelseUtsendelse = new VilkaarBegrunnelse();
+        vilkaarBegrunnelseUtsendelse.setKode("UTSENDELSE_OVER_24_MN");
+        VilkaarBegrunnelse vilkaarBegrunnelseIdeellOrganisasjon = new VilkaarBegrunnelse();
+        vilkaarBegrunnelseIdeellOrganisasjon.setKode("IDEELL_ORGANISASJON_IKKE_VESENTLIG_VIRK");
         VilkaarBegrunnelse vilkaarBegrunnelseSærlig = new VilkaarBegrunnelse();
-        vilkaarBegrunnelseSærlig.setKode("SAERLIG_AVSLAGSGRUNN");
+        vilkaarBegrunnelseSærlig.setKode("SAERLIG_GRUNN");
 
         Set<VilkaarBegrunnelse> vilkaarBegrunnelser = new HashSet<>();
-        vilkaarBegrunnelser.add(vilkaarBegrunnelseForSent);
-        vilkaarBegrunnelser.add(vilkaarBegrunnelseErstatter);
+        vilkaarBegrunnelser.add(vilkaarBegrunnelseUtsendelse);
+        vilkaarBegrunnelser.add(vilkaarBegrunnelseIdeellOrganisasjon);
         vilkaarBegrunnelser.add(vilkaarBegrunnelseSærlig);
 
         Vilkaarsresultat vilkaarsresultat = new Vilkaarsresultat();
@@ -80,6 +95,21 @@ public class VilkaarsresultatTilBegrunnelseMapperTest {
         vilkaarsresultat.setBegrunnelseFritekst("Fritekst som skal vises");
 
         assertThat(VilkaarsresultatTilBegrunnelseMapper.mapVilkaarsresultatTilBegrunnelseString(vilkaarsresultat),
-            is("Fritekst som skal vises"));
+            is("Utsendelseperioden overskrider 24 måneder\nFritekst som skal vises\nArbeider for ideell organisasjon som ikke har vesentlig virksomhet i Norge"));
+    }
+
+    @Test
+    public void mapVilkaarsresultatTilBegrunnelseString_medKodeSomIkkeFinnes_forventSammeKode() {
+        VilkaarBegrunnelse vilkaarBegrunnelse= new VilkaarBegrunnelse();
+        vilkaarBegrunnelse.setKode("EN_KODE_SOM_IKKE_FINNES_I_KODEVERK");
+
+        Set<VilkaarBegrunnelse> vilkaarBegrunnelser = new HashSet<>();
+        vilkaarBegrunnelser.add(vilkaarBegrunnelse);
+
+        Vilkaarsresultat vilkaarsresultat = new Vilkaarsresultat();
+        vilkaarsresultat.setBegrunnelser(vilkaarBegrunnelser);
+
+        assertThat(VilkaarsresultatTilBegrunnelseMapper.mapVilkaarsresultatTilBegrunnelseString(vilkaarsresultat),
+            is("EN_KODE_SOM_IKKE_FINNES_I_KODEVERK"));
     }
 }
