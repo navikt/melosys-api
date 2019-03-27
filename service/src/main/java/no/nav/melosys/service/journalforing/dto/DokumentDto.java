42ce4318aedb97a78c67b94949563f4f8a39e615
Andreas Strand <andreas.strand@nav.no>
2019-03-27 21:20:16 +0100

    Setter og Getter for DokumentDto

--- service/src/main/java/no/nav/melosys/service/journalforing/dto/DokumentDto.java
+++ service/src/main/java/no/nav/melosys/service/journalforing/dto/DokumentDto.java
@@ -1,14 +1,22 @@
 package no.nav.melosys.service.journalforing.dto;
 
 public class DokumentDto {
-    public String dokumentID;
-    public String tittel;
+    private String dokumentID;
+    private String tittel;
 
-    public DokumentDto() {
+    public String getDokumentID() {
+        return dokumentID;
     }
 
-    public DokumentDto(String tittel) {
-        this.dokumentID = null;
+    public void setDokumentID(String dokumentID) {
+        this.dokumentID = dokumentID;
+    }
+
+    public String getTittel() {
+        return tittel;
+    }
+
+    public void setTittel(String tittel) {
         this.tittel = tittel;
     }
 }
