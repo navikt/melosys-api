9df5d5c88a64c046f59469241d8a6c3eaa5e340d
rgraneru <roar.granevang@nav.no>
2019-04-02 13:37:07 +0200

    MELOSYS-2444 omdøping skalTilordnes til isSkalTilordnes

--- service/src/main/java/no/nav/melosys/service/saksflyt/ProsessinstansService.java
+++ service/src/main/java/no/nav/melosys/service/saksflyt/ProsessinstansService.java
@@ -57,7 +57,7 @@ public class ProsessinstansService {
         prosessinstans.setData(ProsessDataKey.AVSENDER_ID, journalfoeringDto.getAvsenderID());
         prosessinstans.setData(ProsessDataKey.AVSENDER_NAVN, journalfoeringDto.getAvsenderNavn());
         prosessinstans.setData(ProsessDataKey.HOVEDDOKUMENT_TITTEL, journalfoeringDto.getHoveddokumentTittel());
-        prosessinstans.setData(ProsessDataKey.SKAL_TILORDNES, journalfoeringDto.skalTilordnes());
+        prosessinstans.setData(ProsessDataKey.SKAL_TILORDNES, journalfoeringDto.isSkalTilordnes());
 
         if (!CollectionUtils.isEmpty(journalfoeringDto.getVedlegg())) {
             prosessinstans.setData(ProsessDataKey.VEDLEGG_TITTEL_LISTE, journalfoeringDto.getVedlegg().stream().map(DokumentDto::getTittel).collect(Collectors.toList()));
