489119cd148cdaef7986add90d842745be33c330
rgraneru <roar.granevang@nav.no>
2019-05-02 09:01:08 +0200

    MELOSYS-2526 Syntaksfix

--- frontend-api/src/test/java/no/nav/melosys/tjenester/gui/util/NumericStringRandomizer.java
+++ frontend-api/src/test/java/no/nav/melosys/tjenester/gui/util/NumericStringRandomizer.java
@@ -4,14 +4,14 @@ import io.github.benas.randombeans.api.Randomizer;
 import org.apache.commons.lang3.RandomStringUtils;
 
 public class NumericStringRandomizer implements Randomizer<String> {
-    private final int lenght;
+    private final int length;
 
     public NumericStringRandomizer(int length) {
-        this.lenght = length;
+        this.length = length;
     }
 
     @Override
     public String getRandomValue() {
-        return RandomStringUtils.randomNumeric(lenght);
+        return RandomStringUtils.randomNumeric(length);
     }
 }
