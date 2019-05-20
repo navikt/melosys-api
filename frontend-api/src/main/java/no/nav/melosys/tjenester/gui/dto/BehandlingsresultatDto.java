2e035c5dfbdd42a65707d6f48a76b1bca19b00be
Andreas Strand <andreas.strand@nav.no>
2019-05-20 10:26:59 +0200

    MELOSYS-2657 Endring i struktur av behanldingsresultat

--- frontend-api/src/main/java/no/nav/melosys/tjenester/gui/dto/BehandlingsresultatDto.java
+++ frontend-api/src/main/java/no/nav/melosys/tjenester/gui/dto/BehandlingsresultatDto.java
@@ -1,14 +1,41 @@
 package no.nav.melosys.tjenester.gui.dto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import no.nav.melosys.domain.Behandlingsresultat;
-import no.nav.melosys.domain.kodeverk.Henleggelsesgrunner;
+import no.nav.melosys.domain.BehandlingsresultatBegrunnelse;
 
 public class BehandlingsresultatDto {
-    public Henleggelsesgrunner henleggelsegrunnKode;
-    public String henleggelseFritekst;
 
-    public BehandlingsresultatDto(Behandlingsresultat behandlingsresultat) {
-        henleggelsegrunnKode = behandlingsresultat.getHenleggelsesgrunn();
-        henleggelseFritekst = behandlingsresultat.getHenleggelseFritekst();
+    private List<String> begrunnelser;
+    private String begrunnelseFritekst;
+
+    public List<String> getBegrunnelser() {
+        return begrunnelser;
+    }
+
+    public void setBegrunnelser(List<String> begrunnelser) {
+        this.begrunnelser = begrunnelser;
+    }
+
+    public String getBegrunnelseFritekst() {
+        return begrunnelseFritekst;
+    }
+
+    public void setBegrunnelseFritekst(String begrunnelseFritekst) {
+        this.begrunnelseFritekst = begrunnelseFritekst;
+    }
+
+    public static BehandlingsresultatDto av(Behandlingsresultat behandlingsresultat) {
+        BehandlingsresultatDto dto = new BehandlingsresultatDto();
+        dto.setBegrunnelseFritekst(behandlingsresultat.getBegrunnelseFritekst());
+        dto.setBegrunnelser(new ArrayList<>());
+
+        behandlingsresultat.getBehandlingsresultatBegrunnelser().stream()
+            .map(BehandlingsresultatBegrunnelse::getKode)
+            .forEach(dto.getBegrunnelser()::add);
+
+        return dto;
     }
 }
