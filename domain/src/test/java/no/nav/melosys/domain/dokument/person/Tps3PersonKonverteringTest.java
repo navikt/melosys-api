498f8a4d07ab89d6c5d236ef206d725d275a06cd
Francois Allix <francois.allix@nav.no>
2019-05-31 10:30:42 +0200

    Konverterer midlertidige adresser fra TPS (MELOSYS-2716)

--- domain/src/test/java/no/nav/melosys/domain/dokument/person/Tps3PersonKonverteringTest.java
+++ domain/src/test/java/no/nav/melosys/domain/dokument/person/Tps3PersonKonverteringTest.java
@@ -1,47 +1,41 @@
 package no.nav.melosys.domain.dokument.person;
 
-import java.io.*;
+import java.io.IOException;
+import java.io.InputStream;
 import java.time.LocalDate;
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.Unmarshaller;
-import javax.xml.bind.helpers.DefaultValidationEventHandler;
-import javax.xml.transform.Result;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.stream.StreamResult;
-import javax.xml.transform.stream.StreamSource;
+import java.util.Objects;
 
+import no.nav.melosys.domain.Saksopplysning;
+import no.nav.melosys.domain.SaksopplysningType;
+import no.nav.melosys.domain.dokument.DokumentFactory;
+import no.nav.melosys.domain.dokument.KonverteringTest;
+import no.nav.melosys.domain.dokument.XsltTemplatesFactory;
+import no.nav.melosys.domain.dokument.jaxb.JaxbConfig;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
 
-public class Tps3PersonKonverteringTest {
-
-    public static final String TPS_PERSON_3_0_MOCK = "person/tps_person_3.0_mock.xml";
-    public static final String TPS_PERSON_3_0_XSLT = "tps/person_3.0.xslt";
+public class Tps3PersonKonverteringTest implements KonverteringTest {
+    private static final String TPS_PERSON_3_0_MOCK = "person/tps_person_3.0_mock.xml";
     private static final String TPS_PERSON_MED_FAMILIERELASJONER = "person/88888888886.xml";
+
+    private static DokumentFactory factory;
+
+    @BeforeClass
+    public static void setUp() {
+        Jaxb2Marshaller marshaller = new JaxbConfig().jaxb2Marshaller();
+        XsltTemplatesFactory xsltTemplatesFactory = new XsltTemplatesFactory();
+        factory = new DokumentFactory(marshaller, xsltTemplatesFactory);
+    }
 
     @Test
     public void testTpsTilDomene() throws Exception {
-        File inputXml = new File(getClass().getClassLoader().getResource(TPS_PERSON_3_0_MOCK).getFile());
-        File xslt = new File(getClass().getClassLoader().getResource(TPS_PERSON_3_0_XSLT).getFile());
+        Saksopplysning test = getSaksopplysning(TPS_PERSON_3_0_MOCK);
 
-        // Transformer...
-        Transformer t = TransformerFactory.newInstance().newTransformer(new StreamSource(xslt));
-        StreamSource xmlSource = new StreamSource(inputXml);
-        ByteArrayOutputStream baos = new ByteArrayOutputStream(99999);
-        Result outputTarget = new StreamResult(baos);
-        t.transform(xmlSource, outputTarget);
-        baos.flush();
-        byte[] xmlBytes = baos.toByteArray();
-
-        // Unmarshal...
-        ByteArrayInputStream bais = new ByteArrayInputStream(xmlBytes);
-        JAXBContext ctx = JAXBContext.newInstance(PersonDokument.class);
-        Unmarshaller um = ctx.createUnmarshaller();
-        um.setEventHandler(new DefaultValidationEventHandler());
-        PersonDokument p2 = (PersonDokument) um.unmarshal(bais);
-
+        PersonDokument p2 = (PersonDokument) test.getDokument();
         // Verifiser...
         assertEquals("MILI", p2.diskresjonskode.getKode());
         assertEquals(LocalDate.of(2011, 11, 11), p2.fødselsdato);
@@ -51,22 +45,9 @@ public class Tps3PersonKonverteringTest {
 
     @Test
     public void testFamilierelasjoner() throws Exception {
-        StreamSource xmlSource = new StreamSource(getClass().getClassLoader().getResourceAsStream(TPS_PERSON_MED_FAMILIERELASJONER));
-        StreamSource streamSource = new StreamSource(getClass().getClassLoader().getResourceAsStream(TPS_PERSON_3_0_XSLT));
+        Saksopplysning test = getSaksopplysning(TPS_PERSON_MED_FAMILIERELASJONER);
 
-        Transformer transformer = TransformerFactory.newInstance().newTransformer(streamSource);
-        StringWriter writer = new StringWriter();
-        Result outputTarget = new StreamResult(writer);
-        transformer.transform(xmlSource, outputTarget);
-        writer.flush();
-
-        // Unmarshal...
-        StringReader reader = new StringReader(writer.toString());
-        JAXBContext ctx = JAXBContext.newInstance(PersonDokument.class);
-        Unmarshaller um = ctx.createUnmarshaller();
-        um.setEventHandler(new DefaultValidationEventHandler());
-        PersonDokument dokument = (PersonDokument) um.unmarshal(reader);
-
+        PersonDokument dokument = (PersonDokument) test.getDokument();
         // Verifiser...
         assertThat(dokument).isNotNull();
         assertThat(dokument.familiemedlemmer).isNotEmpty();
@@ -75,7 +56,7 @@ public class Tps3PersonKonverteringTest {
     @Test
     public void testPostadresse() throws Exception {
         final String kilde = "person/midlertidig_postadresse_utland.xml";
-        PersonDokument dokument = getPersonDokument(kilde);
+        PersonDokument dokument = (PersonDokument) getSaksopplysning(kilde).getDokument();
 
         assertThat(dokument).isNotNull();
         assertThat(dokument.postadresse).isNotNull();
@@ -85,32 +66,21 @@ public class Tps3PersonKonverteringTest {
     @Test
     public void testMidlertidigPostadresseNorge() throws Exception {
         final String kilde = "person/midlertidig_postadresse_norge.xml";
-        PersonDokument dokument = getPersonDokument(kilde);
+        //final String kilde = "person/MEL-156.xml";
+        PersonDokument dokument = (PersonDokument) getSaksopplysning(kilde).getDokument();
 
         assertThat(dokument).isNotNull();
         assertThat(dokument.midlertidigPostadresse).isNotNull();
 
         MidlertidigPostadresseNorge midlertidigPostadresseNorge = (MidlertidigPostadresseNorge) dokument.midlertidigPostadresse;
-        assertThat(midlertidigPostadresseNorge.gateadresse.getGatenummer()).isNull();
+        assertThat(midlertidigPostadresseNorge.gateadresse.getGatenummer()).isEqualTo(29);
         assertThat(midlertidigPostadresseNorge.gateadresse.getHusnummer()).isEqualTo(7);
     }
 
-    private PersonDokument getPersonDokument(String kilde) throws Exception {
-        StreamSource xmlSource = new StreamSource(getClass().getClassLoader().getResourceAsStream(kilde));
-        StreamSource streamSource = new StreamSource(getClass().getClassLoader().getResourceAsStream(TPS_PERSON_3_0_XSLT));
-
-        Transformer transformer = TransformerFactory.newInstance().newTransformer(streamSource);
-        StringWriter writer = new StringWriter();
-        Result outputTarget = new StreamResult(writer);
-        transformer.transform(xmlSource, outputTarget);
-        writer.flush();
-
-        // Unmarshal...
-        StringReader reader = new StringReader(writer.toString());
-        JAXBContext ctx = JAXBContext.newInstance(PersonDokument.class);
-        Unmarshaller unmarshaller = ctx.createUnmarshaller();
-        unmarshaller.setEventHandler(new DefaultValidationEventHandler());
-        return (PersonDokument) unmarshaller.unmarshal(reader);
+    @Override
+    public Saksopplysning getSaksopplysning(String ressurs) throws IOException {
+        final InputStream kilde = getClass().getClassLoader().getResourceAsStream(ressurs);
+        Objects.requireNonNull(kilde);
+        return konverter(kilde, factory, SaksopplysningType.PERSONOPPLYSNING, "3.0");
     }
-
 }
