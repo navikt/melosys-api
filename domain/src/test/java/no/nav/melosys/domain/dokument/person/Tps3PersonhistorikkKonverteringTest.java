01397bfaf75326abb8fca4f14b522b8f52125354
Francois Allix <francois.allix@nav.no>
2019-07-22 13:58:50 +0200

    Henting av personhistorikk feiler fordi tilleggsadresse er ikke mappet (MELOSYS-2867)
    - Fjerner tilsynelatende ekte personer fra testdata.

--- domain/src/test/java/no/nav/melosys/domain/dokument/person/Tps3PersonhistorikkKonverteringTest.java
+++ domain/src/test/java/no/nav/melosys/domain/dokument/person/Tps3PersonhistorikkKonverteringTest.java
@@ -1,14 +1,13 @@
 package no.nav.melosys.domain.dokument.person;
 
-import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.nio.charset.Charset;
-import java.util.stream.Collectors;
+import java.util.Objects;
 
 import no.nav.melosys.domain.Saksopplysning;
 import no.nav.melosys.domain.SaksopplysningType;
 import no.nav.melosys.domain.dokument.DokumentFactory;
+import no.nav.melosys.domain.dokument.KonverteringTest;
 import no.nav.melosys.domain.dokument.XsltTemplatesFactory;
 import no.nav.melosys.domain.dokument.jaxb.JaxbConfig;
 import org.junit.Before;
@@ -17,9 +16,9 @@ import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class Tps3PersonhistorikkKonverteringTest {
+public class Tps3PersonhistorikkKonverteringTest implements KonverteringTest {
 
-    private static final String XML_PATH = "person/FJERNET_historikk.xml";
+    private static final String XML_PATH = "person/person_historikk.xml";
 
     private DokumentFactory factory;
 
@@ -32,49 +31,42 @@ public class Tps3PersonhistorikkKonverteringTest {
 
     @Test
     public void testKonverteringPersonhistorikk() throws Exception {
-        final InputStream kilde = getClass().getClassLoader().getResourceAsStream(XML_PATH);
+        PersonhistorikkDokument dokument = (PersonhistorikkDokument) getSaksopplysning(XML_PATH).getDokument();
 
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(kilde, Charset.forName("UTF-8")))) {
-            Saksopplysning saksopplysning = new Saksopplysning();
+        assertThat(dokument).isNotNull();
+        assertThat(dokument.statsborgerskapListe).isNotNull();
+        assertThat(dokument.bostedsadressePeriodeListe).isNotNull();
+        assertThat(dokument.postadressePeriodeListe).isNotNull();
+        assertThat(dokument.midlertidigAdressePeriodeListe).isNotNull();
 
-            String xmlStr = reader.lines().collect(Collectors.joining(System.lineSeparator()));
-
-            saksopplysning.setDokumentXml(xmlStr);
-            saksopplysning.setType(SaksopplysningType.PERSHIST);
-            saksopplysning.setVersjon("3.4");
-
-            factory.lagDokument(saksopplysning);
-
-            PersonhistorikkDokument dokument = (PersonhistorikkDokument) saksopplysning.getDokument();
-
-            assertThat(dokument).isNotNull();
-            assertThat(dokument.statsborgerskapListe).isNotNull();
-            assertThat(dokument.bostedsadressePeriodeListe).isNotNull();
-            assertThat(dokument.postadressePeriodeListe).isNotNull();
-            assertThat(dokument.midlertidigAdressePeriodeListe).isNotNull();
-
-            for (StatsborgerskapPeriode statsborgerskapPeriode : dokument.statsborgerskapListe) {
-                assertThat(statsborgerskapPeriode.endringstidspunkt).isNotNull();
-                assertThat(statsborgerskapPeriode.statsborgerskap).isNotNull();
-            }
-
-            for (BostedsadressePeriode bostedsadressePeriode : dokument.bostedsadressePeriodeListe) {
-                assertThat(bostedsadressePeriode.endringstidspunkt).isNotNull();
-                assertThat(bostedsadressePeriode.periode).isNotNull();
-                assertThat(bostedsadressePeriode.bostedsadresse.getLand()).isNotNull();
-            }
-
-            for (PostadressePeriode postadressePeriode : dokument.postadressePeriodeListe) {
-                assertThat(postadressePeriode.endringstidspunkt).isNotNull();
-                assertThat(postadressePeriode.periode).isNotNull();
-                assertThat(postadressePeriode.postadresse.land).isNotNull();
-            }
-
-            for (MidlertidigPostadresse midlertidigPostadresse : dokument.midlertidigAdressePeriodeListe) {
-                assertThat(midlertidigPostadresse.endringstidspunkt).isNotNull();
-                assertThat(midlertidigPostadresse.postleveringsPeriode).isNotNull();
-                assertThat(midlertidigPostadresse.land).isNotNull();
-            }
+        for (StatsborgerskapPeriode statsborgerskapPeriode : dokument.statsborgerskapListe) {
+            assertThat(statsborgerskapPeriode.endringstidspunkt).isNotNull();
+            assertThat(statsborgerskapPeriode.statsborgerskap).isNotNull();
         }
+
+        for (BostedsadressePeriode bostedsadressePeriode : dokument.bostedsadressePeriodeListe) {
+            assertThat(bostedsadressePeriode.endringstidspunkt).isNotNull();
+            assertThat(bostedsadressePeriode.periode).isNotNull();
+            assertThat(bostedsadressePeriode.bostedsadresse.getLand()).isNotNull();
+        }
+
+        for (PostadressePeriode postadressePeriode : dokument.postadressePeriodeListe) {
+            assertThat(postadressePeriode.endringstidspunkt).isNotNull();
+            assertThat(postadressePeriode.periode).isNotNull();
+            assertThat(postadressePeriode.postadresse.land).isNotNull();
+        }
+
+        for (MidlertidigPostadresse midlertidigPostadresse : dokument.midlertidigAdressePeriodeListe) {
+            assertThat(midlertidigPostadresse.endringstidspunkt).isNotNull();
+            assertThat(midlertidigPostadresse.postleveringsPeriode).isNotNull();
+            assertThat(midlertidigPostadresse.land).isNotNull();
+        }
+    }
+
+    @Override
+    public Saksopplysning getSaksopplysning(String ressurs) throws IOException {
+        final InputStream kilde = getClass().getClassLoader().getResourceAsStream(ressurs);
+        Objects.requireNonNull(kilde);
+        return konverter(kilde, factory, SaksopplysningType.PERSHIST, "3.4");
     }
 }
