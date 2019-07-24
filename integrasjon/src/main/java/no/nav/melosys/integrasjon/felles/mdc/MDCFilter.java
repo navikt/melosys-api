0e01b0e339c69dcc6a0f30021d3efb5d8fd438b6
Francois Allix <francois.allix@nav.no>
2019-07-24 14:30:29 +0200

    Linting

--- integrasjon/src/main/java/no/nav/melosys/integrasjon/felles/mdc/MDCFilter.java
+++ integrasjon/src/main/java/no/nav/melosys/integrasjon/felles/mdc/MDCFilter.java
@@ -2,41 +2,32 @@ package no.nav.melosys.integrasjon.felles.mdc;
 
 
 import java.io.IOException;
-
-import javax.servlet.Filter;
 import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import no.nav.melosys.sikkerhet.context.SubjectHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 @WebFilter(urlPatterns = "/*")
-public class MDCFilter implements Filter {
-
+public class MDCFilter extends OncePerRequestFilter {
     private static final Logger LOGGER = LoggerFactory.getLogger(MDCFilter.class);
-
     private static final String SYSTEMBRUKER = "srvmelosys";
 
     @Override
-    public void init(FilterConfig filterConfig) {
-
-    }
-
-    @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+    public void doFilterInternal(HttpServletRequest servletRequest, HttpServletResponse servletResponse, FilterChain filterChain)
+        throws IOException, ServletException {
         try {
             final String userId = SubjectHandler.getInstance().getUserID();
-            final String consumerId = SYSTEMBRUKER;
             final String callId = MDCOperations.generateCallId();
 
             MDCOperations.putToMDC(MDCOperations.MDC_CALL_ID, callId);
             MDCOperations.putToMDC(MDCOperations.MDC_USER_ID, userId != null ? userId : "");
-            MDCOperations.putToMDC(MDCOperations.MDC_CONSUMER_ID, consumerId);
+            MDCOperations.putToMDC(MDCOperations.MDC_CONSUMER_ID, SYSTEMBRUKER);
             LOGGER.debug("Set MDC values");
 
             filterChain.doFilter(servletRequest, servletResponse);
@@ -48,10 +39,4 @@ public class MDCFilter implements Filter {
             LOGGER.debug("Cleared MDC values");
         }
     }
-
-    @Override
-    public void destroy() {
-
-    }
-
 }
