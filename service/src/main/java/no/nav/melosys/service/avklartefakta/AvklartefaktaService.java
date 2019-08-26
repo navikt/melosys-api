ae98624b83dbba38066664a4bdff4107a4e2e268
Bjørnar Askevold <bjornar.askevold@nav.no>
2019-08-26 12:25:53 +0200

    Endret avklartMaritimtArbeid til å kun opprettes via konstruktør Oppdatering av testdata

--- service/src/main/java/no/nav/melosys/service/avklartefakta/AvklartefaktaService.java
+++ service/src/main/java/no/nav/melosys/service/avklartefakta/AvklartefaktaService.java
@@ -110,12 +110,14 @@ public class AvklartefaktaService {
         Set<Avklartefakta> maritimeAvklartefakta =
             avklarteFaktaRepository.findAllByBehandlingsresultatIdAndTypeIn(behandlingsid, maritimeFaktatyper);
 
-        HashMap<String, AvklartMaritimtArbeid> maritimeFaktaGruppert = new HashMap<>();
-        maritimeAvklartefakta.forEach(avklartefakta ->
-            maritimeFaktaGruppert.computeIfAbsent(avklartefakta.getSubjekt(), v -> new AvklartMaritimtArbeid(avklartefakta.getSubjekt()))
-                .leggTilFakta(avklartefakta)
-        );
-        return maritimeFaktaGruppert;
+        Map<String, List<Avklartefakta>> maritimtArbeidGruppert = grupperForSubjekt(maritimeAvklartefakta);
+        return maritimtArbeidGruppert.entrySet().stream()
+            .collect(Collectors.toMap(Map.Entry::getKey, AvklartMaritimtArbeid::av));
+    }
+
+    private Map<String, List<Avklartefakta>> grupperForSubjekt(Collection<Avklartefakta> avklartefakta) {
+        return avklartefakta.stream()
+            .collect(Collectors.groupingBy(Avklartefakta::getSubjekt, Collectors.toList()));
     }
 
     public Optional<Avklartefakta> hentVurderingUnntakPeriode(long behandlingsid) {
