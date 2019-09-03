adbaebf3b546414c6f905c1887244363b5950f5c
Francois Allix <francois.allix@nav.no>
2019-09-03 11:24:51 +0200

    Metrikker for feilet prosessinstanser

--- config/src/main/java/no/nav/melosys/metrics/MetrikkerNavn.java
+++ config/src/main/java/no/nav/melosys/metrics/MetrikkerNavn.java
@@ -7,6 +7,5 @@ public final class MetrikkerNavn {
     public static final String BEHANDLINGER_OPPRETTET = METRIKKER_NAMESPACE + "behandlinger.opprettet";
     public static final String BEHANDLINGER_AVSLUTTET = METRIKKER_NAMESPACE + "behandlinger.avsluttet";
     public static final String PROSESSINSTANSER_OPPRETTET = METRIKKER_NAMESPACE + "prosessinstanser.opprettet";
-    public static final String PROSESSINSTANSER_AVSLUTTET = METRIKKER_NAMESPACE + "prosessinstanser.avsluttet";
-    public static final String PROSESSINSTANSER_FEILET = METRIKKER_NAMESPACE + "prosessinstanser.feilet";
+    public static final String PROSESSINSTANSER = METRIKKER_NAMESPACE + "prosessinstanser.";
 }
