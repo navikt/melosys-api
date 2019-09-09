339ec374b2848b547069cabdbed23b067915bb45
Sigbjørn Myhre <sigbjorn.myhre@nav.no>
2019-09-09 16:14:45 +0200

    Bruker MockRestServiceServer i EessiConsumerTest

--- integrasjon/src/test/java/no/nav/melosys/integrasjon/eessi/EessiConsumerImplTest.java
+++ integrasjon/src/test/java/no/nav/melosys/integrasjon/eessi/EessiConsumerImplTest.java
@@ -6,52 +6,35 @@ import no.nav.melosys.integrasjon.eessi.dto.SedDataDto;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Captor;
-import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.client.MockRestServiceServer;
 import org.springframework.web.client.RestTemplate;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
 
 @RunWith(MockitoJUnitRunner.class)
 public class EessiConsumerImplTest {
-
-    @Mock
-    private RestTemplate restTemplate;
-
     private EessiConsumer eessiConsumer;
-
-    @Captor
-    private ArgumentCaptor<HttpEntity> httpEntityCaptor;
-    @Captor
-    private ArgumentCaptor<ParameterizedTypeReference> parameterizedTypeReferenceCaptor;
+    private MockRestServiceServer server;
 
     @Before
     public void setup() {
+        RestTemplate restTemplate = new RestTemplateBuilder().build();
         eessiConsumer = new EessiConsumerImpl(restTemplate);
+        server = MockRestServiceServer.createServer(restTemplate);
     }
 
     @Test
     public void genererSedForhåndsvisning() throws MelosysException {
         final byte[] PDF = "pdf".getBytes();
-        when(restTemplate.exchange(anyString(), any(HttpMethod.class), any(HttpEntity.class), any(ParameterizedTypeReference.class)))
-            .thenReturn(new ResponseEntity(PDF, HttpStatus.OK));
+        server.expect(requestTo("/sed/A001/pdf"))
+            .andRespond(withSuccess(PDF, MediaType.APPLICATION_PDF));
 
-        SedDataDto sedDataDto = new SedDataDto();
-        byte[] pdf = eessiConsumer.genererSedForhåndsvisning(sedDataDto, SedType.A001);
-
-        verify(restTemplate).exchange(eq("/sed/A001/pdf"), eq(HttpMethod.POST), httpEntityCaptor.capture(), parameterizedTypeReferenceCaptor.capture());
+        byte[] pdf = eessiConsumer.genererSedForhåndsvisning(new SedDataDto(), SedType.A001);
         assertThat(pdf).isEqualTo(PDF);
-        assertThat(httpEntityCaptor.getValue().getBody()).isEqualTo(sedDataDto);
-        assertThat(parameterizedTypeReferenceCaptor.getValue().getType().getTypeName()).isEqualTo(byte[].class.getSimpleName());
     }
 }
