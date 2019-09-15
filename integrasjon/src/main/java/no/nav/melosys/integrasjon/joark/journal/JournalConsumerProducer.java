f03444fe624360cfc9dc1efcfab35b6fd5ac9c2d
Andreas Strand <andreas.strand@nav.no>
2019-09-15 22:42:13 +0200

    Oppretter JoarkSystemService, for å kunne hente journalpost-dokumenter med system-kontekst

--- integrasjon/src/main/java/no/nav/melosys/integrasjon/joark/journal/JournalConsumerProducer.java
+++ integrasjon/src/main/java/no/nav/melosys/integrasjon/joark/journal/JournalConsumerProducer.java
@@ -4,8 +4,10 @@ import no.nav.melosys.sikkerhet.sts.NAVSTSClient;
 import no.nav.melosys.sikkerhet.sts.StsConfigurationUtil;
 import no.nav.tjeneste.virksomhet.journal.v3.JournalV3;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class JournalConsumerProducer {
@@ -17,8 +19,16 @@ public class JournalConsumerProducer {
     }
 
     @Bean
+    @Primary
     public JournalConsumer journalConsumer() {
         JournalV3 port = wrapWithSts(consumerConfig.getPort(), NAVSTSClient.StsClientType.SECURITYCONTEXT_TIL_SAML);
+        return new JournalConsumerImpl(port);
+    }
+
+    @Bean
+    @Qualifier("system")
+    public JournalConsumer journalConsumerSystem() {
+        JournalV3 port = wrapWithSts(consumerConfig.getPort(), NAVSTSClient.StsClientType.SYSTEM_SAML);
         return new JournalConsumerImpl(port);
     }
 
