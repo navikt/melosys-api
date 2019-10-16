cffaeb136be24bcc194dd18bbf078cd277f35bec
Francois Allix <francois.allix@nav.no>
2019-10-16 11:09:10 +0200

    Fjerner unødvendig endepunkt /inngangsvilkaar

--- frontend-api/src/main/java/no/nav/melosys/tjenester/gui/config/JerseyConfig.java
+++ frontend-api/src/main/java/no/nav/melosys/tjenester/gui/config/JerseyConfig.java
@@ -35,7 +35,6 @@ public class JerseyConfig extends ResourceConfig {
         register(DokumentTjeneste.class);
         register(FagsakTjeneste.class);
         register(FrontendLoggingTjeneste.class);
-        register(InngangTjeneste.class);
         register(JournalfoeringTjeneste.class);
         register(KontaktopplysningTjeneste.class);
         register(LovvalgsperiodeTjeneste.class);
