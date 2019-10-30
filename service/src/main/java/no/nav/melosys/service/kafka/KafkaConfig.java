c80a0e55dec6eefdc4d9a4c384b577087b18e8c7
Andreas Strand <andreas.strand@nav.no>
2019-10-30 14:42:32 +0100

    Fjerner AUTO_COMMIT_INTERVAL_MS_CONFIG da den ikke har noen effekt da ENABLE_AUTO_COMMIT er skrudd av

--- service/src/main/java/no/nav/melosys/service/kafka/KafkaConfig.java
+++ service/src/main/java/no/nav/melosys/service/kafka/KafkaConfig.java
@@ -48,7 +48,6 @@ public class KafkaConfig {
         props.put(ConsumerConfig.GROUP_ID_CONFIG, kafkaGroupId);
         props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-        props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, 100);
         props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, 15000);
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
