4736739ce89cc4fc5067ba7f643ab928a5da1a45
Francois Allix <francois.allix@nav.no>
2019-11-05 15:17:53 +0100

    Flyttet prosess* i domene til egen mappe "saksflyt"

--- repository/src/main/java/no/nav/melosys/repository/ProsessinstansRepository.java
+++ repository/src/main/java/no/nav/melosys/repository/ProsessinstansRepository.java
@@ -11,7 +11,7 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface ProsessinstansRepository extends JpaRepository<Prosessinstans, Long> {
     @Query("SELECT NEW no.nav.melosys.repository.ProsessinstansAntall(p.type, p.steg, COUNT(p)) FROM Prosessinstans p "
-        + "WHERE p.steg <> no.nav.melosys.domain.ProsessSteg.FERDIG GROUP BY p.type, p.steg")
+        + "WHERE p.steg <> no.nav.melosys.domain.saksflyt.ProsessSteg.FERDIG GROUP BY p.type, p.steg")
     List<ProsessinstansAntall> antallAktiveOgFeiletPerTypeOgSteg();
     Optional<Prosessinstans> findByBehandling_IdAndStegIsNotAndStegIsNot(long id, ProsessSteg prosessSteg1, ProsessSteg prosessSteg2);
     Optional<Prosessinstans> findByTypeAndBehandling_IdAndStegIsNotAndStegIsNot(ProsessType prosessType, long id, ProsessSteg prosessSteg1, ProsessSteg prosessSteg2);
