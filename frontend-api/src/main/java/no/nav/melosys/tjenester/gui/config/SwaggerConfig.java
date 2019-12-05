e2e26e4bbf46d10c0360a226480881ce08e11fee
Andreas Strand <andreas.strand@nav.no>
2019-12-05 20:59:41 +0100

    ApiInfo for Swagger

--- frontend-api/src/main/java/no/nav/melosys/tjenester/gui/config/SwaggerConfig.java
+++ frontend-api/src/main/java/no/nav/melosys/tjenester/gui/config/SwaggerConfig.java
@@ -1,10 +1,14 @@
 package no.nav.melosys.tjenester.gui.config;
 
+import java.util.Collections;
+
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -16,11 +20,24 @@ public class SwaggerConfig {
 
     @Bean
     public Docket api() {
+        //TODO: SecurityScheme/SecurityContext
         return new Docket(DocumentationType.SWAGGER_2)
+            .apiInfo(apiInfo())
             .select()
             .apis(RequestHandlerSelectors.basePackage("no.nav.melosys.tjenester.gui"))
             .paths(PathSelectors.any())
             .build()
             .enable(true);
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+            "Melosys API",
+            "Frontend API for Melosys",
+            "1",
+            null,
+            new Contact("teammelosys", null, null),
+            null, null, Collections.emptyList()
+        );
     }
 }
