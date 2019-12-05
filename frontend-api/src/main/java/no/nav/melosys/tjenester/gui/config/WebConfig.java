f539ac5c08dfd363746d676cbfc429cf3e9c1932
Andreas Strand <andreas.strand@nav.no>
2019-12-05 20:45:07 +0100

    Prefix /api for spring-controllere

---
+++ frontend-api/src/main/java/no/nav/melosys/tjenester/gui/config/WebConfig.java
@@ -0,0 +1,23 @@
+package no.nav.melosys.tjenester.gui.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    private static final String API_PREFIX = "/api";
+    private static final String FRONTEND_API_TJENESTER = "no.nav.melosys.tjenester.gui";
+
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        configurer.addPathPrefix(API_PREFIX, WebConfig::erFrontendApiTjeneste);
+    }
+
+    private static boolean erFrontendApiTjeneste(Class clazz) {
+        return clazz.getPackageName().equals(FRONTEND_API_TJENESTER)
+            && clazz.isAnnotationPresent(RestController.class);
+    }
+}
