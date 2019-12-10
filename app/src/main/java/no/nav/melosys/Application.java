94e01f2d3d52417d4d9b63043d1e1e9811b54cd0
Andreas Strand <andreas.strand@nav.no>
2019-12-10 12:22:39 +0100

    Fjerner frontendlogger avhengighet

--- app/src/main/java/no/nav/melosys/Application.java
+++ app/src/main/java/no/nav/melosys/Application.java
@@ -4,12 +4,9 @@ import no.nav.security.token.support.spring.api.EnableJwtTokenValidation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @ServletComponentScan("no.nav.melosys.integrasjon.felles")
 @PropertySource(value = "classpath:saksflyt.properties", encoding = "utf-8")
@@ -21,16 +18,6 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-
-    @Bean
-    public WebMvcConfigurer dispatcherServletConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addResourceHandlers(ResourceHandlerRegistry registry) {
-                registry.addResourceHandler("/frontendlogger/**").addResourceLocations("classpath:/frontendlogger/");
-            }
-        };
     }
 
     @GetMapping(path = { "/journalforing/**", "/sok/**" })
