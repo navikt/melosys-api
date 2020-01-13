535dd2e60b68a522ed28aa44a0f31a78b6ed08cb
Francois Allix <francois.allix@nav.no>
2020-01-13 12:32:53 +0100

    Opprydding

--- integrasjon/src/test/java/no/nav/melosys/integrasjon/test/TpsTestData.java
+++ integrasjon/src/test/java/no/nav/melosys/integrasjon/test/TpsTestData.java
@@ -3,10 +3,10 @@ package no.nav.melosys.integrasjon.test;
 public class TpsTestData {
 
     // Konstanter for standardbrukere (kan refereres eksternt)
-    public static final long STD_KVINNE_AKTØR_ID = FJERNET67L;
+    public static final long STD_KVINNE_AKTØR_ID = 9999999999999L;
     public static final String STD_KVINNE_FNR = "88888888888";
     public static final String STD_KVINNE_ETTERNAVN = "KVINNE_ETTERNAVNSEN";
 
     public static final String STD_FNR = "88888888884";
-    public static final long STD_AKTØR_ID = FJERNET71L;
+    public static final long STD_AKTØR_ID = 8888888888888L;
 }
