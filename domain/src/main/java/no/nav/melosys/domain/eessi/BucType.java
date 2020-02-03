c94ca7bc8aab4b6808a97a4581d5c0b2b0180fc7
Sigbjørn Myhre <sigbjorn.myhre@nav.no>
2020-02-03 16:40:51 +0100

    Legger til sed- og buc-typer for alle h-bucer

--- domain/src/main/java/no/nav/melosys/domain/eessi/BucType.java
+++ domain/src/main/java/no/nav/melosys/domain/eessi/BucType.java
@@ -7,5 +7,18 @@ public enum BucType {
     LA_BUC_04,
     LA_BUC_05,
     LA_BUC_06,
-    H_BUC_02a
+
+    H_BUC_01,
+    H_BUC_02a,
+    H_BUC_02b,
+    H_BUC_02c,
+    H_BUC_03a,
+    H_BUC_03b,
+    H_BUC_04,
+    H_BUC_05,
+    H_BUC_06,
+    H_BUC_07,
+    H_BUC_08,
+    H_BUC_09,
+    H_BUC_10,
 }
