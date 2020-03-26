dab8699cedcf83ea5764c0027677fc942ac9c967
Francois Allix <francois.allix@nav.no>
2020-03-26 13:49:20 +0100

    Utvider equals og hashcode for Avklartefakta med fakta siden subjekt kan være ubrukt
    - Enhetstester for henting av arbeidsland og bostedsland

--- service/src/test/java/no/nav/melosys/service/avklartefakta/AvklartefaktaServiceTest.java
+++ service/src/test/java/no/nav/melosys/service/avklartefakta/AvklartefaktaServiceTest.java
@@ -1,11 +1,13 @@
 package no.nav.melosys.service.avklartefakta;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 import no.nav.melosys.domain.Behandlingsresultat;
 import no.nav.melosys.domain.avklartefakta.Avklartefakta;
 import no.nav.melosys.domain.avklartefakta.AvklartefaktaRegistrering;
 import no.nav.melosys.domain.kodeverk.Avklartefaktatyper;
+import no.nav.melosys.domain.kodeverk.Landkoder;
 import no.nav.melosys.domain.kodeverk.Maritimtyper;
 import no.nav.melosys.domain.kodeverk.yrker.Yrkesgrupper;
 import no.nav.melosys.exception.IkkeFunnetException;
@@ -17,7 +19,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
@@ -27,18 +28,14 @@ import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class AvklartefaktaServiceTest {
-
-    @InjectMocks
-    private AvklartefaktaService avklartefaktaService;
-
     @Mock
     private AvklarteFaktaRepository avklarteFaktaRepository;
-
     @Mock
     private BehandlingsresultatRepository behandlingsresultatRepository;
-
     @Mock
-    AvklartefaktaDtoKonverterer avklartefaktaDtoKonverterer;
+    private AvklartefaktaDtoKonverterer avklartefaktaDtoKonverterer;
+
+    private AvklartefaktaService avklartefaktaService;
 
     @Captor
     private ArgumentCaptor<Avklartefakta> captor;
@@ -50,41 +47,27 @@ public class AvklartefaktaServiceTest {
 
     @Test
     public void hentAvklartefakta() {
-        String referanse = "Referanse";
-        String subjektID = "SubjektID";
-        String fakta = "NO";
-        Avklartefaktatyper type = Avklartefaktatyper.ARBEIDSLAND;
-        String begrunnelsekode = "Begrunnelse";
-        String begrunnelsefritekst = "Fritekst";
-
-        Avklartefakta avklartefakta = new Avklartefakta();
-        avklartefakta.setReferanse(referanse);
-        avklartefakta.setSubjekt(subjektID);
-        avklartefakta.setFakta(fakta);
-        avklartefakta.setType(type);
-        avklartefakta.setBegrunnelseFritekst(begrunnelsefritekst);
-
-        AvklartefaktaRegistrering registrering = new AvklartefaktaRegistrering();
-        registrering.setAvklartefakta(avklartefakta);
-        registrering.setBegrunnelseKode(begrunnelsekode);
-        avklartefakta.setRegistreringer(new HashSet<>(Arrays.asList(registrering)));
-        Set<Avklartefakta> avklartefaktaSet = new HashSet<>(Arrays.asList(avklartefakta));
+        Avklartefakta avklartefakta = lagAvklartefakta(Avklartefaktatyper.ARBEIDSLAND, "NO", "TRUE");
+        Set<Avklartefakta> avklartefaktaSet = new HashSet<>(List.of(avklartefakta));
 
         when(avklarteFaktaRepository.findByBehandlingsresultatId(anyLong())).thenReturn(avklartefaktaSet);
 
-        AvklartefaktaDto dto = avklartefaktaService.hentAlleAvklarteFakta(1L).stream().findFirst().get();
+        Optional<AvklartefaktaDto> dtoOpt = avklartefaktaService.hentAlleAvklarteFakta(1L).stream()
+            .findFirst();
+        assertThat(dtoOpt).isPresent();
 
-        assertEquals(referanse, dto.getReferanse());
-        assertEquals(subjektID, dto.getSubjektID());
-        assertEquals(Arrays.asList(fakta), dto.getFakta());
-        assertEquals(type, dto.getAvklartefaktaType());
-        assertEquals(Arrays.asList(begrunnelsekode), dto.getBegrunnelseKoder());
-        assertEquals(begrunnelsefritekst, dto.getBegrunnelseFritekst());
+        AvklartefaktaDto dto = dtoOpt.get();
+        assertEquals(avklartefakta.getReferanse(), dto.getReferanse());
+        assertEquals(avklartefakta.getSubjekt(), dto.getSubjektID());
+        assertEquals(List.of(avklartefakta.getFakta()), dto.getFakta());
+        assertEquals(avklartefakta.getType(), dto.getAvklartefaktaType());
+        assertEquals(avklartefakta.getRegistreringer().stream().map(AvklartefaktaRegistrering::getBegrunnelseKode)
+            .collect(Collectors.toList()), dto.getBegrunnelseKoder());
+        assertEquals(avklartefakta.getBegrunnelseFritekst(), dto.getBegrunnelseFritekst());
     }
 
     @Test
     public void lagreAvklarteFakta() throws IkkeFunnetException {
-
         Behandlingsresultat behandlingsresultat = new Behandlingsresultat();
         when(behandlingsresultatRepository.findById(anyLong())).thenReturn(Optional.of(behandlingsresultat));
         Avklartefakta avklartefakta = new Avklartefakta();
@@ -96,14 +79,32 @@ public class AvklartefaktaServiceTest {
         verify(avklarteFaktaRepository).flush();
         verify(avklartefaktaDtoKonverterer).opprettAvklartefaktaFraDto(any(), any());
         verify(avklarteFaktaRepository).saveAll(any());
+    }
 
+    @Test
+    public void hentAlleAvklarteArbeidsland() {
+        when(avklarteFaktaRepository.findAllByBehandlingsresultatIdAndType(anyLong(), eq(Avklartefaktatyper.ARBEIDSLAND)))
+            .thenReturn(Set.of(lagAvklartefakta(Avklartefaktatyper.ARBEIDSLAND, null, "NO"),
+                lagAvklartefakta(Avklartefaktatyper.ARBEIDSLAND, null, "SE")));
+
+        Set<Landkoder> landkoder = avklartefaktaService.hentAlleAvklarteArbeidsland(1L);
+        assertThat(landkoder).containsExactlyInAnyOrder(Landkoder.NO, Landkoder.SE);
+    }
+
+    @Test
+    public void hentBostedsland() {
+        when(avklarteFaktaRepository.findAllByBehandlingsresultatIdAndType(anyLong(), eq(Avklartefaktatyper.BOSTEDSLAND)))
+            .thenReturn(Set.of(lagAvklartefakta(Avklartefaktatyper.BOSTEDSLAND, null, "NO")));
+
+        Optional<Landkoder> landkoder = avklartefaktaService.hentBostedland(1L);
+        assertThat(landkoder).isPresent().get().isEqualTo(Landkoder.NO);
     }
 
     @Test
     public void hentYrkesgruppe_forventerOrdinær() throws TekniskException {
         Avklartefakta avklartefakta = new Avklartefakta();
         avklartefakta.setFakta("ORDINAER");
-        Optional<Avklartefakta> avklartefaktaSet = Optional.ofNullable(avklartefakta);
+        Optional<Avklartefakta> avklartefaktaSet = Optional.of(avklartefakta);
         when(avklarteFaktaRepository.findByBehandlingsresultatIdAndType(anyLong(), any())).thenReturn(avklartefaktaSet);
 
         Optional<Yrkesgrupper> yrkesgruppeType = avklartefaktaService.finnYrkesGruppe(1L);
@@ -114,7 +115,7 @@ public class AvklartefaktaServiceTest {
     public void hentYrkesgruppe_forventerFlyende() throws TekniskException {
         Avklartefakta avklartefakta = new Avklartefakta();
         avklartefakta.setFakta("YRKESAKTIV_FLYVENDE");
-        Optional<Avklartefakta> avklartefaktaSet = Optional.ofNullable(avklartefakta);
+        Optional<Avklartefakta> avklartefaktaSet = Optional.of(avklartefakta);
         when(avklarteFaktaRepository.findByBehandlingsresultatIdAndType(anyLong(), any())).thenReturn(avklartefaktaSet);
 
         Optional<Yrkesgrupper> yrkesgruppeType = avklartefaktaService.finnYrkesGruppe(1L);
@@ -125,7 +126,7 @@ public class AvklartefaktaServiceTest {
     public void hentYrkesgruppe_forventerSokkelSkip() throws TekniskException {
         Avklartefakta avklartefakta = new Avklartefakta();
         avklartefakta.setFakta("SOKKEL_ELLER_SKIP");
-        Optional<Avklartefakta> avklartefaktaFraDb = Optional.ofNullable(avklartefakta);
+        Optional<Avklartefakta> avklartefaktaFraDb = Optional.of(avklartefakta);
         when(avklarteFaktaRepository.findByBehandlingsresultatIdAndType(anyLong(), any())).thenReturn(avklartefaktaFraDb);
 
         Optional<Yrkesgrupper> yrkesgruppeType = avklartefaktaService.finnYrkesGruppe(1L);
@@ -136,7 +137,7 @@ public class AvklartefaktaServiceTest {
     public void hentYrkesgruppe_utenYrkesgruppe_forventerFeil() throws TekniskException {
         Avklartefakta avklartefakta = new Avklartefakta();
         avklartefakta.setFakta("IKKE_YRKESAKTIV");
-        Optional<Avklartefakta> avklartefaktaFraDb = Optional.ofNullable(avklartefakta);
+        Optional<Avklartefakta> avklartefaktaFraDb = Optional.of(avklartefakta);
         when(avklarteFaktaRepository.findByBehandlingsresultatIdAndType(anyLong(), any())).thenReturn(avklartefaktaFraDb);
 
         avklartefaktaService.finnYrkesGruppe(1L);
@@ -165,7 +166,7 @@ public class AvklartefaktaServiceTest {
     public void hentMaritimType_medSokkelTekst_foventerSokkelType() {
         Avklartefakta avklartefakta = new Avklartefakta();
         avklartefakta.setFakta("SOKKEL");
-        Optional<Avklartefakta> avklartefaktaFraDb = Optional.ofNullable(avklartefakta);
+        Optional<Avklartefakta> avklartefaktaFraDb = Optional.of(avklartefakta);
         when(avklarteFaktaRepository.findByBehandlingsresultatIdAndType(anyLong(), any())).thenReturn(avklartefaktaFraDb);
 
         Optional<Maritimtyper> maritimType = avklartefaktaService.hentMaritimType(1L);
@@ -176,7 +177,7 @@ public class AvklartefaktaServiceTest {
     public void hentMaritimType_medSkipTekst_foventerSkipType() {
         Avklartefakta avklartefakta = new Avklartefakta();
         avklartefakta.setFakta("SKIP");
-        Optional<Avklartefakta> avklartefaktaFraDb = Optional.ofNullable(avklartefakta);
+        Optional<Avklartefakta> avklartefaktaFraDb = Optional.of(avklartefakta);
         when(avklarteFaktaRepository.findByBehandlingsresultatIdAndType(anyLong(), any())).thenReturn(avklartefaktaFraDb);
 
         Optional<Maritimtyper> maritimType = avklartefaktaService.hentMaritimType(1L);
@@ -221,7 +222,8 @@ public class AvklartefaktaServiceTest {
         avklartefakta.setFakta("TRUE");
         avklartefakta.setSubjekt(orgnr1);
 
-        when(avklarteFaktaRepository.findByBehandlingsresultatIdAndTypeAndFakta(anyLong(), any(), eq("TRUE"))).thenReturn(new HashSet<>(Arrays.asList(avklartefakta)));
+        when(avklarteFaktaRepository.findByBehandlingsresultatIdAndTypeAndFakta(anyLong(), any(), eq("TRUE")))
+            .thenReturn(new HashSet<>(List.of(avklartefakta)));
 
         Set<String> avklarteOrgnumre = avklartefaktaService.hentAvklarteOrgnrOgUuid(1L);
         assertThat(avklarteOrgnumre).containsOnly(orgnr1);
@@ -243,5 +245,24 @@ public class AvklartefaktaServiceTest {
 
         AvklartefaktaRegistrering registrering = capturedAvklarteFakta.getRegistreringer().iterator().next();
         assertThat(registrering.getBegrunnelseKode()).isEqualTo("kode");
+    }
+
+    private static Avklartefakta lagAvklartefakta(Avklartefaktatyper type, String subjektID, String fakta) {
+        String referanse = "Referanse";
+        String begrunnelsekode = "Begrunnelse";
+        String begrunnelsefritekst = "Fritekst";
+
+        Avklartefakta avklartefakta = new Avklartefakta();
+        avklartefakta.setReferanse(referanse);
+        avklartefakta.setSubjekt(subjektID);
+        avklartefakta.setFakta(fakta);
+        avklartefakta.setType(type);
+        avklartefakta.setBegrunnelseFritekst(begrunnelsefritekst);
+
+        AvklartefaktaRegistrering registrering = new AvklartefaktaRegistrering();
+        registrering.setAvklartefakta(avklartefakta);
+        registrering.setBegrunnelseKode(begrunnelsekode);
+        avklartefakta.setRegistreringer(new HashSet<>(List.of(registrering)));
+        return avklartefakta;
     }
 }
