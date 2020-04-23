87874a1aa67caabe952ae350fab71076974a5e25
Eivind Nilsbakken <eivind.nilsbakken@nav.no>
2020-04-23 10:29:40 +0200

    Hent periode fra SED hvis ikke søknad

--- service/src/main/java/no/nav/melosys/service/OppfriskSaksopplysningerService.java
+++ service/src/main/java/no/nav/melosys/service/OppfriskSaksopplysningerService.java
@@ -3,10 +3,10 @@ package no.nav.melosys.service;
 import no.nav.melosys.domain.Behandling;
 import no.nav.melosys.domain.Fagsak;
 import no.nav.melosys.domain.behandlingsgrunnlag.BehandlingsgrunnlagData;
-import no.nav.melosys.domain.dokument.soeknad.Periode;
+import no.nav.melosys.domain.dokument.sed.SedDokument;
 import no.nav.melosys.domain.kodeverk.Sakstyper;
 import no.nav.melosys.domain.kodeverk.behandlinger.Behandlingstema;
-import no.nav.melosys.domain.util.BehandlingsgrunnlagUtils;
+import no.nav.melosys.domain.util.SaksopplysningerUtils;
 import no.nav.melosys.exception.MelosysException;
 import no.nav.melosys.exception.TekniskException;
 import no.nav.melosys.integrasjon.tps.TpsFasade;
@@ -19,6 +19,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
 
 @Service
 public class OppfriskSaksopplysningerService {
@@ -56,26 +58,55 @@ public class OppfriskSaksopplysningerService {
 
         String aktørID = behandling.getFagsak().hentBruker().getAktørId();
         String brukerID = tpsFasade.hentIdentForAktørId(aktørID);
-        BehandlingsgrunnlagData grunnlagData = behandling.getBehandlingsgrunnlag().getBehandlingsgrunnlagdata();
-        Periode grunnlagPeriode = BehandlingsgrunnlagUtils.hentPeriode(grunnlagData);
+
+        BehandlingsgrunnlagData grunnlagData = null;
+        LocalDate fom = null, tom = null;
+
+        if (harBehandlingsgrunnlagSøknad(behandling.getTema())) {
+            grunnlagData = behandling.getBehandlingsgrunnlag().getBehandlingsgrunnlagdata();
+            fom = grunnlagData.periode.getFom();
+            tom = grunnlagData.periode.getTom();
+        }
+        if (harSedOpplysninger(behandling.getTema())) {
+            SedDokument sedDokument = SaksopplysningerUtils.hentSedDokument(behandling);
+            fom = sedDokument.getLovvalgsperiode().getFom();
+            tom = sedDokument.getLovvalgsperiode().getTom();
+        }
 
         RegisteropplysningerRequest registeropplysningerRequest = RegisteropplysningerRequest.builder()
             .behandlingID(behandlingID)
             .saksopplysningTyper(utledSaksopplysningTyper(behandling.getTema()))
             .fnr(brukerID)
-            .fom(grunnlagPeriode.getFom())
-            .tom(grunnlagPeriode.getTom())
+            .fom(fom)
+            .tom(tom)
             .build();
 
         registeropplysningerService.hentOgLagreOpplysninger(registeropplysningerRequest);
         behandlingsresultatService.tømBehandlingsresultat(behandlingID);
 
         Fagsak fagsak = behandling.getFagsak();
-        if (!Sakstyper.EU_EOS.equals(fagsak.getType())) {
-            fagsak.setType(regelmodulService.kvalifisererForEf883_2004(behandlingID, grunnlagData.soeknadsland, grunnlagPeriode)
+        if (grunnlagData != null && !Sakstyper.EU_EOS.equals(fagsak.getType())) {
+            fagsak.setType(regelmodulService.kvalifisererForEf883_2004(behandlingID, grunnlagData.soeknadsland, grunnlagData.periode)
                 ? Sakstyper.EU_EOS : Sakstyper.UKJENT);
             fagsakService.lagre(fagsak);
         }
+    }
+
+    private boolean harBehandlingsgrunnlagSøknad(Behandlingstema tema) {
+        return tema == Behandlingstema.UTSENDT_ARBEIDSTAKER
+            || tema == Behandlingstema.UTSENDT_SELVSTENDIG
+            || tema == Behandlingstema.ARBEID_FLERE_LAND
+            || tema == Behandlingstema.IKKE_YRKESAKTIV
+            || tema == Behandlingstema.ARBEID_ETT_LAND_ØVRIG
+            || tema == Behandlingstema.ARBEID_NORGE_BOSATT_ANNET_LAND;
+    }
+
+    private boolean harSedOpplysninger(Behandlingstema tema) {
+        return tema == Behandlingstema.REGISTRERING_UNNTAK_NORSK_TRYGD_UTSTASJONERING
+            || tema == Behandlingstema.REGISTRERING_UNNTAK_NORSK_TRYGD_ØVRIGE
+            || tema == Behandlingstema.ANMODNING_OM_UNNTAK_HOVEDREGEL
+            || tema == Behandlingstema.BESLUTNING_LOVVALG_NORGE
+            || tema == Behandlingstema.BESLUTNING_LOVVALG_ANNET_LAND;
     }
 
     private RegisteropplysningerRequest.SaksopplysningTyper utledSaksopplysningTyper(Behandlingstema behandlingstema) throws TekniskException {
