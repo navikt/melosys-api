162b27191e1bbcaf92ca807b1f1d0d4d31799915
Sigbjørn Myhre <sigbjorn.myhre@nav.no>
2020-05-29 15:10:07 +0200

    QA MELOSYS-3760

--- integrasjon/src/test/java/no/nav/melosys/integrasjon/sakogbehandling/behandlingstatus/BehandlingStatusMapperTest.java
+++ integrasjon/src/test/java/no/nav/melosys/integrasjon/sakogbehandling/behandlingstatus/BehandlingStatusMapperTest.java
@@ -1,8 +1,7 @@
 package no.nav.melosys.integrasjon.sakogbehandling.behandlingstatus;
 
-import java.time.LocalDate;
-import java.util.List;
-import javax.xml.datatype.XMLGregorianCalendar;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 
 import no.nav.melding.virksomhet.behandlingsstatus.hendelsehandterer.v1.hendelseshandtererbehandlingsstatus.Aktoer;
 import no.nav.melding.virksomhet.behandlingsstatus.hendelsehandterer.v1.hendelseshandtererbehandlingsstatus.Applikasjoner;
@@ -10,9 +9,11 @@ import no.nav.melding.virksomhet.behandlingsstatus.hendelsehandterer.v1.hendelse
 import no.nav.melosys.domain.Fagsystem;
 import no.nav.melosys.domain.kodeverk.behandlinger.Behandlingstema;
 import no.nav.melosys.integrasjon.Konstanter;
+import no.nav.melosys.integrasjon.KonverteringsUtils;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.within;
 
 public class BehandlingStatusMapperTest {
     @Test
@@ -33,16 +34,8 @@ public class BehandlingStatusMapperTest {
         assertThat(behandlingStatusMapper.getHendelsesprodusentREF()).extracting(Applikasjoner::getValue).isEqualTo(Fagsystem.MELOSYS.getKode());
         assertThat(behandlingStatusMapper.getAnsvarligEnhetREF()).isEqualTo(Integer.toString(Konstanter.MELOSYS_ENHET_ID));
 
-        final var nå = LocalDate.now();
-        assertThat(behandlingStatusMapper.getHendelsesTidspunkt())
-            .extracting(
-                XMLGregorianCalendar::getDay,
-                XMLGregorianCalendar::getMonth,
-                XMLGregorianCalendar::getYear)
-            .isEqualTo(List.of(
-                nå.getDayOfMonth(),
-                nå.getMonthValue(),
-                nå.getYear()));
+        assertThat(KonverteringsUtils.xmlGregorianCalendarToInstant(behandlingStatusMapper.getHendelsesTidspunkt()))
+            .isCloseTo(Instant.now(), within(1, ChronoUnit.MINUTES));
     }
 }
 
