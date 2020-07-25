3f231db7cffaf7f97b5c61fb39c6f74b5d4bda07
Francois Allix <francois.allix@nav.no>
2020-07-25 21:12:33 +0200

    Spring retry for integrasjoner som feiler mest

--- app/src/main/java/no/nav/melosys/Application.java
+++ app/src/main/java/no/nav/melosys/Application.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @SpringBootApplication
 @Controller
 @EnableJwtTokenValidation(ignore={"org.springframework", "springfox.documentation"})
+@EnableRetry
 public class Application {
 
     public static void main(String[] args) {
