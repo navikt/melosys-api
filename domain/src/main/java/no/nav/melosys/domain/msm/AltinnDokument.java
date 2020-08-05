1d5d8901188fa46d6672e28fb67a60eb558c939b
Andreas Strand <andreas.strand@nav.no>
2020-08-05 23:16:13 +0200

    Integrasjon mot endepunkt for å hente dokumenter i pdf tilknyttet altinn-søknad

---
+++ domain/src/main/java/no/nav/melosys/domain/msm/AltinnDokument.java
@@ -0,0 +1,49 @@
+package no.nav.melosys.domain.msm;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public final class AltinnDokument {
+    private final String soknadID;
+    private final String dokumentID;
+    private final String tittel;
+    private final AltinnDokumentType dokumentType;
+    private final String innhold;
+
+
+    public AltinnDokument(
+        @JsonProperty("soknadID") String soknadID,
+        @JsonProperty("dokumentID") String dokumentID,
+        @JsonProperty("tittel") String tittel,
+        @JsonProperty("dokumentType") AltinnDokumentType dokumentType,
+        @JsonProperty("innhold") String innhold) {
+        this.soknadID = soknadID;
+        this.dokumentID = dokumentID;
+        this.tittel = tittel;
+        this.dokumentType = dokumentType;
+        this.innhold = innhold;
+    }
+
+    public String getSoknadID() {
+        return soknadID;
+    }
+
+    public String getDokumentID() {
+        return dokumentID;
+    }
+
+    public String getTittel() {
+        return tittel;
+    }
+
+    public AltinnDokumentType getDokumentType() {
+        return dokumentType;
+    }
+
+    public String getInnhold() {
+        return innhold;
+    }
+
+    public enum AltinnDokumentType {
+        SOKNAD, FULLMAKT
+    }
+}
--- integrasjon/src/main/java/no/nav/melosys/integrasjon/altinn/SoknadMottakConsumerImpl.java
+++ integrasjon/src/main/java/no/nav/melosys/integrasjon/altinn/SoknadMottakConsumerImpl.java
@@ -1,7 +1,9 @@
 package no.nav.melosys.integrasjon.altinn;
 
+import java.util.Collection;
 import java.util.Collections;
 
+import no.nav.melosys.domain.msm.AltinnDokument;
 import no.nav.melosys.soknad_altinn.MedlemskapArbeidEOSM;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,10 +13,14 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 public class SoknadMottakConsumerImpl implements SoknadMottakConsumer {
 
     private static final Logger log = LoggerFactory.getLogger(SoknadMottakConsumerImpl.class);
+
+    private static final String SOKNADER_PATH = "soknader";
+    private static final String DOKUMENTER_PATH = "dokumenter";
 
     private final RestTemplate restTemplate;
 
@@ -26,15 +32,28 @@ public class SoknadMottakConsumerImpl implements SoknadMottakConsumer {
     public MedlemskapArbeidEOSM hentSøknad(String søknadID) {
         log.info("Henter søknad med ID {}", søknadID);
 
-        return restTemplate.exchange(String.format("/soknader/%s", søknadID), HttpMethod.GET,
-            new HttpEntity<>(getXmltypeHeaders()), new ParameterizedTypeReference<MedlemskapArbeidEOSM>() {
-            }
-        ).getBody();
+        String url = UriComponentsBuilder.fromPath(SOKNADER_PATH)
+            .pathSegment(søknadID)
+            .toUriString();
+
+        return restTemplate.exchange(url, HttpMethod.GET, new HttpEntity<>(getHeaders(MediaType.APPLICATION_XML)),
+            new ParameterizedTypeReference<MedlemskapArbeidEOSM>() {}).getBody();
     }
 
-    private HttpHeaders getXmltypeHeaders() {
+    @Override
+    public Collection<AltinnDokument> hentDokumenter(String søknadID) {
+        log.info("Henter dokumenter tilknyttet altinn-søknad {}", søknadID);
+        String url = UriComponentsBuilder.fromPath(SOKNADER_PATH)
+            .pathSegment(søknadID, DOKUMENTER_PATH)
+            .toUriString();
+
+        return restTemplate.exchange(url, HttpMethod.GET, new HttpEntity<>(getHeaders(MediaType.APPLICATION_JSON)),
+            new ParameterizedTypeReference<Collection<AltinnDokument>>() {}).getBody();
+    }
+
+    private HttpHeaders getHeaders(MediaType mediaType) {
         HttpHeaders headers = new HttpHeaders();
-        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_XML));
+        headers.setAccept(Collections.singletonList(mediaType));
         return headers;
     }
 }
