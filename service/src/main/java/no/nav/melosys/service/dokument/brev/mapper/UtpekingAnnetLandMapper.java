52c40fcd91d3ff1fd1ca880e280ba66914b48979
Francois Allix <francois.allix@nav.no>
2020-08-05 14:03:49 +0200

    Vedtaksperiode i utpekingsbrev

--- service/src/main/java/no/nav/melosys/service/dokument/brev/mapper/UtpekingAnnetLandMapper.java
+++ service/src/main/java/no/nav/melosys/service/dokument/brev/mapper/UtpekingAnnetLandMapper.java
@@ -2,9 +2,11 @@ package no.nav.melosys.service.dokument.brev.mapper;
 
 import javax.xml.bind.JAXBElement;
 import javax.xml.bind.JAXBException;
+import javax.xml.datatype.DatatypeConfigurationException;
 
 import no.nav.dok.melosysbrev._000168.BrevdataType;
 import no.nav.dok.melosysbrev._000168.Fag;
+import no.nav.dok.melosysbrev._000168.LovvalgsperiodeType;
 import no.nav.dok.melosysbrev._000168.ObjectFactory;
 import no.nav.dok.melosysbrev.felles.melosys_felles.FellesType;
 import no.nav.dok.melosysbrev.felles.melosys_felles.LovvalgsbestemmelseKode;
@@ -13,9 +15,12 @@ import no.nav.dok.melosysbrev.felles.melosys_felles.TilleggsbestemmelseKode;
 import no.nav.melosys.domain.Behandling;
 import no.nav.melosys.domain.Behandlingsresultat;
 import no.nav.melosys.domain.Utpekingsperiode;
+import no.nav.melosys.exception.TekniskException;
 import no.nav.melosys.service.dokument.brev.BrevData;
 import no.nav.melosys.service.dokument.brev.BrevDataUtpekingAnnetLand;
 import org.xml.sax.SAXException;
+
+import static no.nav.melosys.service.dokument.brev.mapper.felles.BrevMapperUtils.convertToXMLGregorianCalendarRemoveTimezone;
 
 public final class UtpekingAnnetLandMapper implements BrevDataMapper {
     private static final String XSD_LOCATION = "melosysbrev/melosys_000168.xsd";
@@ -25,17 +30,18 @@ public final class UtpekingAnnetLandMapper implements BrevDataMapper {
                                 MelosysNAVFelles navFelles,
                                 Behandling behandling,
                                 Behandlingsresultat resultat,
-                                BrevData brevData) throws JAXBException, SAXException {
+                                BrevData brevData) throws JAXBException, SAXException, TekniskException {
         Fag fag = mapFag((BrevDataUtpekingAnnetLand)brevData);
         JAXBElement<BrevdataType> brevdataTypeJAXBElement = mapintoBrevdataType(fellesType, navFelles, fag);
         return JaxbHelper.marshalAndValidate(brevdataTypeJAXBElement, XSD_LOCATION);
     }
 
-    Fag mapFag(BrevDataUtpekingAnnetLand brevDataUtpekingAnnetLand) {
+    Fag mapFag(BrevDataUtpekingAnnetLand brevDataUtpekingAnnetLand) throws TekniskException {
         Fag fag = new Fag();
         Utpekingsperiode utpekingsperiode = brevDataUtpekingAnnetLand.utpekingsperiode;
 
         fag.setLovvalgsland(utpekingsperiode.getLovvalgsland().getBeskrivelse());
+        fag.setLovvalgsperiode(lagLovvalgsperiodeType(utpekingsperiode));
         fag.setLovvalgsbestemmelse(LovvalgsbestemmelseKode.fromValue(utpekingsperiode.getBestemmelse().getKode()));
         if (utpekingsperiode.getTilleggsbestemmelse() != null) {
             fag.setTilleggsbestemmelse(TilleggsbestemmelseKode.fromValue(utpekingsperiode.getTilleggsbestemmelse().getKode()));
@@ -43,6 +49,18 @@ public final class UtpekingAnnetLandMapper implements BrevDataMapper {
         fag.setFritekst(brevDataUtpekingAnnetLand.fritekst);
 
         return fag;
+    }
+
+    private static LovvalgsperiodeType lagLovvalgsperiodeType(Utpekingsperiode utpekingsperiode)
+        throws TekniskException {
+        LovvalgsperiodeType lovvalgsperiodeType = new LovvalgsperiodeType();
+        try {
+            lovvalgsperiodeType.setFomDato(convertToXMLGregorianCalendarRemoveTimezone(utpekingsperiode.getFom()));
+            lovvalgsperiodeType.setTomDato(convertToXMLGregorianCalendarRemoveTimezone(utpekingsperiode.getTom()));
+        } catch (DatatypeConfigurationException e) {
+            throw new TekniskException(e);
+        }
+        return lovvalgsperiodeType;
     }
 
     @SuppressWarnings("Duplicates")
