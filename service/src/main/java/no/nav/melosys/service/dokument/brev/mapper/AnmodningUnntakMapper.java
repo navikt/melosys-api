baa6b60d17f76f930871f424b30cc8fa956e3185
Sigbjørn Myhre <sigbjorn.myhre@nav.no>
2020-08-19 14:21:33 +0200

    Legger til unntak fra bestemmelse i orienteringsbrev for anmodning om unntak

--- service/src/main/java/no/nav/melosys/service/dokument/brev/mapper/AnmodningUnntakMapper.java
+++ service/src/main/java/no/nav/melosys/service/dokument/brev/mapper/AnmodningUnntakMapper.java
@@ -5,10 +5,7 @@ import javax.xml.bind.JAXBElement;
 import javax.xml.bind.JAXBException;
 import javax.xml.datatype.DatatypeConfigurationException;
 
-import no.nav.dok.melosysbrev._000084.BrevdataType;
-import no.nav.dok.melosysbrev._000084.Fag;
-import no.nav.dok.melosysbrev._000084.LovvalgsperiodeType;
-import no.nav.dok.melosysbrev._000084.ObjectFactory;
+import no.nav.dok.melosysbrev._000084.*;
 import no.nav.dok.melosysbrev.felles.melosys_felles.FellesType;
 import no.nav.dok.melosysbrev.felles.melosys_felles.InngangsvilkaarBegrunnelseKode;
 import no.nav.dok.melosysbrev.felles.melosys_felles.MelosysNAVFelles;
@@ -17,7 +14,9 @@ import no.nav.melosys.domain.Anmodningsperiode;
 import no.nav.melosys.domain.Behandling;
 import no.nav.melosys.domain.Behandlingsresultat;
 import no.nav.melosys.domain.VilkaarBegrunnelse;
+import no.nav.melosys.domain.kodeverk.LovvalgBestemmelse;
 import no.nav.melosys.domain.kodeverk.Sakstyper;
+import no.nav.melosys.domain.kodeverk.lovvalgsbestemmelser.Lovvalgbestemmelser_883_2004;
 import no.nav.melosys.exception.TekniskException;
 import no.nav.melosys.service.dokument.brev.BrevData;
 import no.nav.melosys.service.dokument.brev.BrevDataAnmodningUnntak;
@@ -79,7 +78,45 @@ public class AnmodningUnntakMapper implements BrevDataMapper {
 
         fag.setBegrunnelseFritekst(brevData.fritekst);
 
+        Anmodningsperiode anmodningsperiode = resultat.hentValidertAnmodningsperiode();
+        if (anmodningsperiode.erUnntakFraArtikkel13()) {
+            fag.setBestemmelseDetSoekesUnntakFra(tilBestemmelseBrev(anmodningsperiode.getUnntakFraBestemmelse()));
+        }
+
         return fag;
+    }
+
+    private static BestemmelseDetSoekesUnntakFraKode tilBestemmelseBrev(LovvalgBestemmelse bestemmelse) {
+        if (bestemmelse instanceof Lovvalgbestemmelser_883_2004) {
+            switch ((Lovvalgbestemmelser_883_2004) bestemmelse) {
+                case FO_883_2004_ART13_1A:
+                    return BestemmelseDetSoekesUnntakFraKode.FO_883_2004_ART_13_1_A;
+                case FO_883_2004_ART13_1B1:
+                    return BestemmelseDetSoekesUnntakFraKode.FO_883_2004_ART_13_1_B_1;
+                case FO_883_2004_ART13_1B2:
+                    return BestemmelseDetSoekesUnntakFraKode.FO_883_2004_ART_13_1_B_2;
+                case FO_883_2004_ART13_1B3:
+                    return BestemmelseDetSoekesUnntakFraKode.FO_883_2004_ART_13_1_B_3;
+                case FO_883_2004_ART13_1B4:
+                    return BestemmelseDetSoekesUnntakFraKode.FO_883_2004_ART_13_1_B_4;
+                case FO_883_2004_ART13_2A:
+                    return BestemmelseDetSoekesUnntakFraKode.FO_883_2004_ART_13_2_A;
+                case FO_883_2004_ART13_2B:
+                    return BestemmelseDetSoekesUnntakFraKode.FO_883_2004_ART_13_2_B;
+                case FO_883_2004_ART13_3:
+                    return BestemmelseDetSoekesUnntakFraKode.FO_883_2004_ART_13_3;
+                case FO_883_2004_ART13_4:
+                    return BestemmelseDetSoekesUnntakFraKode.FO_883_2004_ART_13_4;
+                default:
+                    throw new UnsupportedOperationException(
+                        String.format("BestemmelseDetSoekesUnntakFraKode støtter ikke %s", bestemmelse)
+                    );
+            }
+        }
+
+        throw new UnsupportedOperationException(
+            String.format("Kan ikke konvertere bestemmelse av type %s til BestemmelseDetSoekesUnntakFraKode", bestemmelse)
+        );
     }
 
     LovvalgsperiodeType lagLovvalgsperiodeType(Behandlingsresultat resultat) {
