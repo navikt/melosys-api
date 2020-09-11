8faed072e03aad8da286da49c96e8b39e5fad56f
Andreas Strand <andreas.strand@nav.no>
2020-09-11 15:57:45 +0200

    Definerer flyt ANMODNING_OM_UNNTAK og sletter steg AOU_VALIDERING

--- saksflyt/src/main/java/no/nav/melosys/saksflyt/prosessflyt/ProsessflytDefinisjon.java
+++ saksflyt/src/main/java/no/nav/melosys/saksflyt/prosessflyt/ProsessflytDefinisjon.java
@@ -6,7 +6,7 @@ import java.util.Optional;
 
 import no.nav.melosys.domain.saksflyt.ProsessType;
 
-import static no.nav.melosys.domain.saksflyt.ProsessSteg.MANGELBREV;
+import static no.nav.melosys.domain.saksflyt.ProsessSteg.*;
 
 public final class ProsessflytDefinisjon {
 
@@ -19,6 +19,18 @@ public final class ProsessflytDefinisjon {
             ProsessType.MANGELBREV,
             new ProsessFlyt(ProsessType.MANGELBREV,
                 MANGELBREV
+            )
+        );
+
+        PROSESS_FLYT_MAP.put(
+            ProsessType.ANMODNING_OM_UNNTAK,
+            new ProsessFlyt(ProsessType.ANMODNING_OM_UNNTAK,
+                AOU_OPPDATER_RESULTAT,
+                AOU_AVKLAR_MYNDIGHET,
+                AOU_OPPDATER_MEDL,
+                AOU_SEND_BREV,
+                AOU_SEND_SED,
+                AOU_OPPDATER_OPPGAVE
             )
         );
     }
