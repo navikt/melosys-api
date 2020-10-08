c4d545357122740f6487fd11bdd16e146f667c03
Janita Marielle Johansen <janita.marielle.johansen@nav.no>
2020-10-08 13:58:47 +0200

    Skrevet tester

--- service/src/main/java/no/nav/melosys/service/behandling/EndreBehandlingstemaService.java
+++ service/src/main/java/no/nav/melosys/service/behandling/EndreBehandlingstemaService.java
@@ -6,6 +6,7 @@ import no.nav.melosys.repository.BehandlingRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import static no.nav.melosys.domain.kodeverk.behandlinger.Behandlingstema.*;
@@ -14,9 +15,9 @@ import static no.nav.melosys.domain.kodeverk.behandlinger.Behandlingstema.*;
 public class EndreBehandlingstemaService {
     private final BehandlingRepository behandlingRepository;
 
-    private static final List<Behandlingstema> tillateBehandlingstemaForSoknad = Arrays.asList(UTSENDT_ARBEIDSTAKER, UTSENDT_SELVSTENDIG, ARBEID_ETT_LAND_ØVRIG, IKKE_YRKESAKTIV, ARBEID_FLERE_LAND,
+    private static final List<Behandlingstema> muligeBehandlingstemaForSoknad = Arrays.asList(UTSENDT_ARBEIDSTAKER, UTSENDT_SELVSTENDIG, ARBEID_ETT_LAND_ØVRIG, IKKE_YRKESAKTIV, ARBEID_FLERE_LAND,
         ARBEID_NORGE_BOSATT_ANNET_LAND);
-    private static final List<Behandlingstema> tillateBehandlingstemaForSED = Arrays.asList(ØVRIGE_SED_MED, ØVRIGE_SED_UFM, TRYGDETID);
+    private static final List<Behandlingstema> muligeBehandlingstemaForSED = Arrays.asList(ØVRIGE_SED_MED, ØVRIGE_SED_UFM, TRYGDETID);
 
     public EndreBehandlingstemaService(BehandlingRepository behandlingRepository) {
         this.behandlingRepository = behandlingRepository;
@@ -24,21 +25,21 @@ public class EndreBehandlingstemaService {
 
 
     public List<Behandlingstema> hentMuligeBehandlingstema(Behandling behandling) {
-        if (tillateBehandlingstemaForSoknad.contains(behandling.getTema())) {
-            return tillateBehandlingstemaForSoknad;
-        } else if (tillateBehandlingstemaForSED.contains(behandling.getTema())) {
-            return tillateBehandlingstemaForSED;
+        if (muligeBehandlingstemaForSoknad.contains(behandling.getTema())) {
+            return muligeBehandlingstemaForSoknad;
+        } else if (muligeBehandlingstemaForSED.contains(behandling.getTema())) {
+            return muligeBehandlingstemaForSED;
         } else {
-            return null;
+            return Collections.emptyList();
         }
     }
 
     public Behandling endreBehandlingstemaTilBehandling(Behandling behandling, Behandlingstema nyttTema) {
-        if (tillateBehandlingstemaForSoknad.contains(behandling.getTema()) && tillateBehandlingstemaForSoknad.contains(nyttTema)) {
+        if (muligeBehandlingstemaForSoknad.contains(behandling.getTema()) && muligeBehandlingstemaForSoknad.contains(nyttTema)) {
             behandling.setTema(nyttTema);
             return behandlingRepository.save(behandling);
 
-        } else if (tillateBehandlingstemaForSED.contains(behandling.getTema()) && tillateBehandlingstemaForSED.contains(nyttTema)) {
+        } else if (muligeBehandlingstemaForSED.contains(behandling.getTema()) && muligeBehandlingstemaForSED.contains(nyttTema)) {
             behandling.setTema(nyttTema);
             return behandlingRepository.save(behandling);
 
--- service/src/test/java/no/nav/melosys/service/behandling/EndreBehandlingstemaServiceTest.java
+++ service/src/test/java/no/nav/melosys/service/behandling/EndreBehandlingstemaServiceTest.java
@@ -1,4 +1,100 @@
 package no.nav.melosys.service.behandling;
 
+import static no.nav.melosys.domain.kodeverk.behandlinger.Behandlingstema.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+
+import java.util.Arrays;
+import java.util.List;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import no.nav.melosys.domain.Behandling;
+import no.nav.melosys.domain.kodeverk.behandlinger.Behandlingstema;
+import no.nav.melosys.repository.BehandlingRepository;
+
+@RunWith(MockitoJUnitRunner.class)
 public class EndreBehandlingstemaServiceTest {
+
+    @Mock
+    private BehandlingRepository behandlingRepository;
+
+    private EndreBehandlingstemaService endreBehandlingstemaService;
+
+    @Captor
+    private ArgumentCaptor<Behandling> behandlingArgumentCaptor;
+
+    @Before
+    public void setUp(){
+        endreBehandlingstemaService = spy(new EndreBehandlingstemaService(behandlingRepository));
+    }
+
+    @Test
+    public void hentMuligeBehandlingstemaForSoknad() {
+        Behandling behandling = new Behandling();
+        behandling.setTema(ARBEID_FLERE_LAND);
+
+        List<Behandlingstema> muligeBehandlingstema = endreBehandlingstemaService.hentMuligeBehandlingstema(behandling);
+        assertThat(Arrays.asList(UTSENDT_ARBEIDSTAKER, UTSENDT_SELVSTENDIG, ARBEID_ETT_LAND_ØVRIG, IKKE_YRKESAKTIV, ARBEID_FLERE_LAND,
+            ARBEID_NORGE_BOSATT_ANNET_LAND)).isEqualTo(muligeBehandlingstema);
+    }
+
+    @Test
+    public void hentMuligeBehandlingstemaForSED() {
+        Behandling behandling = new Behandling();
+        behandling.setTema(ØVRIGE_SED_MED);
+
+        List<Behandlingstema> muligeBehandlingstema = endreBehandlingstemaService.hentMuligeBehandlingstema(behandling);
+        assertThat(Arrays.asList(ØVRIGE_SED_MED, ØVRIGE_SED_UFM, TRYGDETID)).isEqualTo(muligeBehandlingstema);
+    }
+
+    @Test
+    public void hentMuligeBehandlingstemaUgyldig() {
+        Behandling behandling = new Behandling();
+        behandling.setTema(REGISTRERING_UNNTAK_NORSK_TRYGD_UTSTASJONERING);
+
+        List<Behandlingstema> muligeBehandlingstema = endreBehandlingstemaService.hentMuligeBehandlingstema(behandling);
+        assertThat(muligeBehandlingstema.size()).isZero();
+    }
+
+    @Test
+    public void endreBehandlingstemaSoknad() {
+        Behandling behandling = new Behandling();
+        behandling.setId(11L);
+        behandling.setTema(ARBEID_FLERE_LAND);
+
+        endreBehandlingstemaService.endreBehandlingstemaTilBehandling(behandling, UTSENDT_ARBEIDSTAKER);
+        verify(behandlingRepository).save(behandlingArgumentCaptor.capture());
+        assertThat(behandlingArgumentCaptor.getValue().getTema()).isEqualTo(UTSENDT_ARBEIDSTAKER);
+        assertThat(behandlingArgumentCaptor.getValue().getId()).isEqualTo(11L);
+    }
+
+    @Test
+    public void endreBehandlingstemaSED() {
+        Behandling behandling = new Behandling();
+        behandling.setId(11L);
+        behandling.setTema(TRYGDETID);
+
+        endreBehandlingstemaService.endreBehandlingstemaTilBehandling(behandling, ØVRIGE_SED_MED);
+        verify(behandlingRepository).save(behandlingArgumentCaptor.capture());
+        assertThat(behandlingArgumentCaptor.getValue().getTema()).isEqualTo(ØVRIGE_SED_MED);
+        assertThat(behandlingArgumentCaptor.getValue().getId()).isEqualTo(11L);
+    }
+
+    @Test
+    public void endreBehandlingstemaUgyldig() {
+        Behandling behandling = new Behandling();
+        behandling.setId(11L);
+        behandling.setTema(ARBEID_FLERE_LAND);
+
+        endreBehandlingstemaService.endreBehandlingstemaTilBehandling(behandling, ØVRIGE_SED_MED);
+        verify(behandlingRepository, never()).save(behandlingArgumentCaptor.capture());
+    }
 }
