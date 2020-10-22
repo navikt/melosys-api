1beb2ac5ed5748bfb1ce9f456711145d2543f011
Andreas Strand <andreas.strand@nav.no>
2020-10-22 15:53:50 +0200

    Prosessinstans.sistFullførtSteg kan være null

--- saksflyt/src/main/java/no/nav/melosys/saksflyt/kontroll/dto/HentProsessinstansDto.java
+++ saksflyt/src/main/java/no/nav/melosys/saksflyt/kontroll/dto/HentProsessinstansDto.java
@@ -1,21 +1,25 @@
 package no.nav.melosys.saksflyt.kontroll.dto;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 import java.util.UUID;
 
+import no.nav.melosys.domain.saksflyt.ProsessSteg;
 import no.nav.melosys.domain.saksflyt.Prosessinstans;
 
 public class HentProsessinstansDto {
     private final UUID id;
     private final String prosessType;
-    private final String sistFullførtSteg;
     private final LocalDateTime endretDato;
+    private final String sistFullførtSteg;
 
     public HentProsessinstansDto(Prosessinstans prosessinstans) {
         this.id = prosessinstans.getId();
         this.prosessType = prosessinstans.getType().getKode();
-        this.sistFullførtSteg = prosessinstans.getSistFullførtSteg().getKode();
         this.endretDato = prosessinstans.getEndretDato();
+        this.sistFullførtSteg = Optional.ofNullable(prosessinstans.getSistFullførtSteg())
+            .map(ProsessSteg::getKode)
+            .orElse(null);
     }
 
     public UUID getId() {
@@ -26,11 +30,11 @@ public class HentProsessinstansDto {
         return prosessType;
     }
 
-    public String getSistFullførtSteg() {
-        return sistFullførtSteg;
-    }
-
     public LocalDateTime getEndretDato() {
         return endretDato;
+    }
+
+    public String getSistFullførtSteg() {
+        return sistFullførtSteg;
     }
 }
