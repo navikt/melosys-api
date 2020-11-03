5030f5735ba67d0e5fe269d2f28ce41bae7a3c3c
Andreas Strand <andreas.strand@nav.no>
2020-11-03 12:36:14 +0100

    Fikser feature-toggle endepunkt

--- frontend-api/src/main/java/no/nav/melosys/tjenester/gui/FeatureToggleTjeneste.java
+++ frontend-api/src/main/java/no/nav/melosys/tjenester/gui/FeatureToggleTjeneste.java
@@ -1,18 +1,23 @@
 package no.nav.melosys.tjenester.gui;
 
+import java.util.Collection;
 import java.util.Map;
-import java.util.Set;
 import java.util.stream.Collectors;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import no.finn.unleash.Unleash;
 import no.nav.security.token.support.core.api.Protected;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Protected
 @RestController
+@RequestMapping("/featuretoggle")
+@Api(tags = {"featuretoggle"})
 public class FeatureToggleTjeneste {
 
     private final Unleash unleash;
@@ -21,8 +26,9 @@ public class FeatureToggleTjeneste {
         this.unleash = unleash;
     }
 
-    @PostMapping
-    public ResponseEntity<Map<String, Boolean>> hentFeatureToggles(@RequestBody Set<String> features) {
+    @GetMapping
+    @ApiOperation(value = "Returnerer om oppgitte feature-toggles er aktiv")
+    public ResponseEntity<Map<String, Boolean>> hentFeatureToggles(@RequestParam Collection<String> features) {
         return ResponseEntity.ok(features.stream().collect(Collectors.toMap(f -> f, unleash::isEnabled)));
     }
 }
