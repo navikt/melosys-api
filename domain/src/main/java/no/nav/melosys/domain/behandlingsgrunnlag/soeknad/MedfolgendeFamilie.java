1ac4ab2db75ae5f3e1cbe62fb1bbba30f6be6b89
Eivind Nilsbakken <eivind.nilsbakken@nav.no>
2020-11-12 10:46:54 +0100

    Bruk oppdatert JSON-skjema for medfølgende familie

--- domain/src/main/java/no/nav/melosys/domain/behandlingsgrunnlag/soeknad/MedfolgendeFamilie.java
+++ domain/src/main/java/no/nav/melosys/domain/behandlingsgrunnlag/soeknad/MedfolgendeFamilie.java
@@ -3,7 +3,7 @@ package no.nav.melosys.domain.behandlingsgrunnlag.soeknad;
 public class MedfolgendeFamilie {
     public String fnr;
     public String navn;
-    public Relasjon relasjon;
+    public Relasjon relasjonsrolle;
 
     public enum Relasjon {
         BARN
@@ -12,7 +12,7 @@ public class MedfolgendeFamilie {
     public static MedfolgendeFamilie tilBarnFraFnr(String fnr) {
         MedfolgendeFamilie medfolgendeFamilie = new MedfolgendeFamilie();
         medfolgendeFamilie.fnr = fnr;
-        medfolgendeFamilie.relasjon = Relasjon.BARN;
+        medfolgendeFamilie.relasjonsrolle = Relasjon.BARN;
         return medfolgendeFamilie;
     }
 }
