3d6440dd09c0564c66134a3df471b74cea1d0b62
Andreas Strand <andreas.strand@nav.no>
2020-11-25 11:02:11 +0100

    Legger til ny mappe i modul "domain", "kodeverk". Mapper entiteter i kodeverk-endepunkt til KodeDto. Nytt enum VilkaarSpørsmål

--- frontend-api/src/main/java/no/nav/melosys/tjenester/gui/kodeverk/MelosysInterntKodeverkTjeneste.java
+++ frontend-api/src/main/java/no/nav/melosys/tjenester/gui/kodeverk/MelosysInterntKodeverkTjeneste.java
@@ -1,12 +1,19 @@
 package no.nav.melosys.tjenester.gui.kodeverk;
 
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import no.nav.melosys.domain.kodeverk.Kodeverk;
+import no.nav.melosys.domain.kodeverk.Trygdedekninger;
+import no.nav.melosys.domain.kodeverk.VilkaarSpørsmål;
+import no.nav.melosys.service.kodeverk.KodeDto;
 import no.nav.melosys.service.medlemskapsperiode.MedlemskapsperiodeService;
-import no.nav.melosys.tjenester.gui.dto.FolketrygdenKoderDto;
 import no.nav.security.token.support.core.api.Protected;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,8 +28,6 @@ import org.springframework.web.context.WebApplicationContext;
 @Scope(value = WebApplicationContext.SCOPE_REQUEST)
 public class MelosysInterntKodeverkTjeneste {
 
-    private static final Logger log = LoggerFactory.getLogger(MelosysInterntKodeverkTjeneste.class);
-
     private final MedlemskapsperiodeService medlemskapsperiodeService;
 
     public MelosysInterntKodeverkTjeneste(MedlemskapsperiodeService medlemskapsperiodeService) {
@@ -31,8 +36,20 @@ public class MelosysInterntKodeverkTjeneste {
 
     @GetMapping("/folketrygden")
     @ApiOperation(value = "Henter koder fra internt kodeverk til saksbehandling av folketrygden-saker")
-    public ResponseEntity<FolketrygdenKoderDto> hentKoderTilFolketrygden() {
-        log.info("Henter koder fra internt kodeverk til saksbehandling av folketrygden-saker.");
-        return ResponseEntity.ok(new FolketrygdenKoderDto(medlemskapsperiodeService.hentGyldigeTrygdedekninger()));
+    public ResponseEntity<Map<String, Collection<KodeDto>>> hentKoderTilFolketrygden() {
+        Map<String, Collection<KodeDto>> kodeverdier = new HashMap<>();
+        kodeverdier.put(Trygdedekninger.class.getSimpleName(), tilKodeDto(medlemskapsperiodeService.hentGyldigeTrygdedekninger()));
+        kodeverdier.put(VilkaarSpørsmål.class.getSimpleName(), tilKodeDto(VilkaarSpørsmål.values()));
+        return ResponseEntity.ok(kodeverdier);
     }
+
+    private <T extends Kodeverk> Collection<KodeDto> tilKodeDto(Collection<T> kodeverk) {
+        return tilKodeDto(kodeverk.toArray(kodeverk.toArray(new Kodeverk[0])));
+    }
+
+    private Collection<KodeDto> tilKodeDto(Kodeverk... kodeverk) {
+        return Stream.of(kodeverk).map(k -> new KodeDto(k.getKode(), k.getBeskrivelse())).collect(Collectors.toSet());
+    }
+
+
 }
