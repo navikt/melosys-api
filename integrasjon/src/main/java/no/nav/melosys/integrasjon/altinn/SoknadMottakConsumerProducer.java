b22097c4a9ebd62e45032cc016aaeda6b659a218
Eivind K. Nilsbakken <eivind.nilsbakken@nav.no>
2020-12-01 21:29:25 +0100

    Konfigurer XmlMapper i mottak for elektronisk søknad

--- integrasjon/src/main/java/no/nav/melosys/integrasjon/altinn/SoknadMottakConsumerProducer.java
+++ integrasjon/src/main/java/no/nav/melosys/integrasjon/altinn/SoknadMottakConsumerProducer.java
@@ -2,11 +2,14 @@ package no.nav.melosys.integrasjon.altinn;
 
 import java.util.Collections;
 
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import com.fasterxml.jackson.module.jaxb.JaxbAnnotationModule;
 import no.nav.melosys.integrasjon.felles.SystemContextClientRequestInterceptor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.xml.MappingJackson2XmlHttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -22,6 +25,9 @@ public class SoknadMottakConsumerProducer {
     public SoknadMottakConsumer soknadMottakConsumer(SystemContextClientRequestInterceptor interceptor) {
         RestTemplate restTemplate = new RestTemplateBuilder().rootUri(url).build();
         restTemplate.setInterceptors(Collections.singletonList(interceptor));
+        restTemplate.getMessageConverters().add(0, new MappingJackson2XmlHttpMessageConverter(
+            new XmlMapper().registerModule(new JaxbAnnotationModule())
+        ));
         return new SoknadMottakConsumerImpl(restTemplate);
     }
 }
