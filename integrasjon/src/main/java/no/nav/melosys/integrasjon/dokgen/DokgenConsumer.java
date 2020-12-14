7fdad5cdd18578e666dd3601e07b8e7d4a739c8f
Paul M. Ø. Lunde <paul.magne.lunde@nav.no>
2020-12-14 12:22:46 +0100

    Justering av consumer

--- integrasjon/src/main/java/no/nav/melosys/integrasjon/dokgen/DokgenConsumer.java
+++ integrasjon/src/main/java/no/nav/melosys/integrasjon/dokgen/DokgenConsumer.java
@@ -4,11 +4,8 @@ import no.nav.melosys.integrasjon.dokgen.dto.DokgenDto;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
-
-import static java.lang.String.format;
 
 @Component
 public class DokgenConsumer {
@@ -22,13 +19,13 @@ public class DokgenConsumer {
 
     public byte[] lagPdf(String malNavn, DokgenDto dokgenDto, boolean bestillKopi) {
         log.info("Produserer PDF i melosys-dokgen. Mal: {}", malNavn);
-        String lagPdfUri = format("/mal/%s/lag-pdf", malNavn);
+        String lagPdfUri = "/mal/{malNavn}/lag-pdf";
 
         return webClient.post().uri(uriBuilder ->
             uriBuilder
                 .path(lagPdfUri)
                 .queryParam("somKopi", bestillKopi)
-                .build())
+                .build(malNavn))
             .bodyValue(dokgenDto)
             .retrieve()
             .bodyToMono(byte[].class)
