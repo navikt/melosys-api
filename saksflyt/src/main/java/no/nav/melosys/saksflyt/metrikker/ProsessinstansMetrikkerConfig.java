053940c4f65bf6b0d6f5ed25d87f92b44ac3f2b5
Francois Allix <francois.allix@nav.no>
2021-01-04 18:00:31 +0100

    Opprett metrikker på en mer idiomatisk måte

---
+++ saksflyt/src/main/java/no/nav/melosys/saksflyt/metrikker/ProsessinstansMetrikkerConfig.java
@@ -0,0 +1,27 @@
+package no.nav.melosys.saksflyt.metrikker;
+
+import io.micrometer.core.instrument.Gauge;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.binder.MeterBinder;
+import no.nav.melosys.domain.saksflyt.ProsessType;
+import no.nav.melosys.metrics.MetrikkerNavn;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ProsessinstansMetrikkerConfig {
+    @Bean
+    public MeterBinder ProsessinstansMetrikker(MeterRegistry meterRegistry,
+                                               ProsessinstansStatusCache statusCache) {
+        return registry -> {
+            for (ProsessType prosessType : ProsessType.values()) {
+                String gaugeNavn = MetrikkerNavn.PROSESSINSTANSER + prosessType.getKode().toLowerCase() + ".feilet";
+                Gauge.builder(
+                    gaugeNavn,
+                    statusCache,
+                    type -> statusCache.antallProsessinstanserFeilet(prosessType)
+                ).register(meterRegistry);
+            }
+        };
+    }
+}
