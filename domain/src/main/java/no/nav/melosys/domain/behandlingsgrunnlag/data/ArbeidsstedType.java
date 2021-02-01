a5795102caca48f8d50c6fad238300f24d0ecde8
Francois Allix <francois.allix@nav.no>
2021-02-01 10:50:47 +0100

    Arbeidssteder fra elektroniske søknader

---
+++ domain/src/main/java/no/nav/melosys/domain/behandlingsgrunnlag/data/ArbeidsstedType.java
@@ -0,0 +1,8 @@
+package no.nav.melosys.domain.behandlingsgrunnlag.data;
+
+public enum ArbeidsstedType {
+    LAND,
+    OFFSHORE,
+    SKIPSFART,
+    LUFTFART
+}
--- domain/src/main/java/no/nav/melosys/domain/behandlingsgrunnlag/data/LuftfartBase.java
+++ domain/src/main/java/no/nav/melosys/domain/behandlingsgrunnlag/data/LuftfartBase.java
@@ -4,4 +4,13 @@ public class LuftfartBase {
     public String hjemmebaseNavn;
     public String hjemmebaseLand;
     public String typeFlyvninger;
+
+    public LuftfartBase() {
+    }
+
+    public LuftfartBase(String hjemmebaseNavn, String hjemmebaseLand, String typeFlyvninger) {
+        this.hjemmebaseNavn = hjemmebaseNavn;
+        this.hjemmebaseLand = hjemmebaseLand;
+        this.typeFlyvninger = typeFlyvninger;
+    }
 }
