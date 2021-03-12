06b1c69415dcf3650b995bb9c98cd94ae82eaa7c
Andreas Strand <andreas.strand@nav.no>
2021-03-12 16:32:22 +0100

    Justerer delay med spring-retry drastisk ned for at tråder ikke skal blokkere hverandre i sakflyt

--- integrasjon/src/main/java/no/nav/melosys/integrasjon/inntk/InntektFasade.java
+++ integrasjon/src/main/java/no/nav/melosys/integrasjon/inntk/InntektFasade.java
@@ -13,6 +13,6 @@ public interface InntektFasade {
     @Retryable(
         value = {IntegrasjonException.class},
         maxAttempts = 3,
-        backoff = @Backoff(delay = 120000, maxDelay = 480000, multiplier = 2))
+        backoff = @Backoff(delay = 1_000, multiplier = 1.5))
     Saksopplysning hentInntektListe(String personID, YearMonth fom, YearMonth tom) throws IntegrasjonException, FunksjonellException;
 }
