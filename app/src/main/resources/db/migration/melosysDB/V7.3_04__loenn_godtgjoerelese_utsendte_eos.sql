20f316d2c17d7e64e908cc3673fce1ed673b8e35
Francois Allix <francois.allix@nav.no>
2021-03-18 09:44:31 +0100

    En PL/SQL blokk per migrering

--- app/src/main/resources/db/migration/melosysDB/V7.3_04__loenn_godtgjoerelese_utsendte_eos.sql
+++ app/src/main/resources/db/migration/melosysDB/V7.3_04__loenn_godtgjoerelese_utsendte_eos.sql
@@ -1,19 +1,4 @@
 DECLARE
-    CURSOR bgs IS SELECT * FROM BEHANDLINGSGRUNNLAG WHERE "TYPE" != 'SØKNAD_A1_UTSENDTE_ARBEIDSTAKERE_EØS' FOR UPDATE;
-    jsonData JSON_OBJECT_T;
-    oppdatertJsonData BEHANDLINGSGRUNNLAG.DATA%TYPE;
-BEGIN
-    FOR bg IN bgs
-        LOOP
-            jsonData := TREAT(JSON_ELEMENT_T.parse(bg.data) AS JSON_OBJECT_T);
-            jsonData.remove('arbeidsinntekt');
-            jsonData.put_Null('loennOgGodtgjoerelse');
-            oppdatertJsonData := jsonData.stringify;
-            UPDATE BEHANDLINGSGRUNNLAG SET DATA = oppdatertJsonData WHERE CURRENT OF bgs;
-        END LOOP;
-END;
-
-DECLARE
     CURSOR bgs IS SELECT * FROM BEHANDLINGSGRUNNLAG WHERE "TYPE" = 'SØKNAD_A1_UTSENDTE_ARBEIDSTAKERE_EØS' FOR UPDATE;
     jsonData JSON_OBJECT_T;
     oppdatertJsonData BEHANDLINGSGRUNNLAG.DATA%TYPE;
@@ -39,3 +24,4 @@ BEGIN
             UPDATE BEHANDLINGSGRUNNLAG SET DATA = oppdatertJsonData WHERE CURRENT OF bgs;
         END LOOP;
 END;
+/
