cef003ca4ad3303fda19c1f19c723618231274fe
JohanSakshaug <johan.sakshaug@nav.no>
2021-03-26 18:10:59 +0100

    sett juridiskArbeidsgiverNorge.erOffentligVirksomhet til null for alle behandlingsgrunnlag

---
+++ app/src/main/resources/db/migration/melosysDB/V7.3_05__juridisk_arbeidsgiver_norge_offentlig_virksomhet_null.sql
@@ -0,0 +1,19 @@
+DECLARE
+    CURSOR bgs IS SELECT * FROM BEHANDLINGSGRUNNLAG FOR UPDATE;
+    jsonData JSON_OBJECT_T;
+    juridiskArbeidsgiverNorge JSON_OBJECT_T;
+    oppdatertJsonData BEHANDLINGSGRUNNLAG.DATA%TYPE;
+BEGIN
+FOR bg IN bgs
+        LOOP
+            jsonData := TREAT(JSON_ELEMENT_T.parse(bg.data) AS JSON_OBJECT_T);
+
+            juridiskArbeidsgiverNorge := jsonData.GET_OBJECT('juridiskArbeidsgiverNorge');
+            juridiskArbeidsgiverNorge.put_null('erOffentligVirksomhet');
+            jsonData.put('juridiskArbeidsgiverNorge', juridiskArbeidsgiverNorge);
+
+            oppdatertJsonData := jsonData.stringify;
+            UPDATE BEHANDLINGSGRUNNLAG SET DATA = oppdatertJsonData WHERE CURRENT OF bgs;
+        END LOOP;
+END;
+/
