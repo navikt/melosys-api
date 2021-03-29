301f72b6427f44fda495806ac723d9ba7212d8ed
janitamarielle <janita.marielle.johansen@nav.no>
2021-03-29 08:51:17 +0200

    PR-kommentarer, egendefinert -> orgnrSettesAvSaksbehandler

--- frontend-api/src/main/java/no/nav/melosys/tjenester/gui/BrevbestillingTjeneste.java
+++ frontend-api/src/main/java/no/nav/melosys/tjenester/gui/BrevbestillingTjeneste.java
@@ -159,7 +159,7 @@ public class BrevbestillingTjeneste {
                         new MottakerDto.Builder()
                             .medType("Annen organisasjon")
                             .medRolle(hovedMottaker)
-                            .egendefinert()
+                            .orgnrSettesAvSaksbehandler()
                             .build()
                     );
 
