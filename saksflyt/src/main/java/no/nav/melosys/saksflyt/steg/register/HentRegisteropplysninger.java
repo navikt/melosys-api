d5c6fae4c229c8b92daba3f868561fe043c0e561
Andreas Strand <andreas.strand@nav.no>
2021-04-20 13:13:13 +0200

    Fikser noen qualifiers for PersondataFasade i saksflyt som ikke ble oppdaget før nå, da gamle kall mot aktørregister kun bruker system-kontekst

--- saksflyt/src/main/java/no/nav/melosys/saksflyt/steg/register/HentRegisteropplysninger.java
+++ saksflyt/src/main/java/no/nav/melosys/saksflyt/steg/register/HentRegisteropplysninger.java
@@ -5,14 +5,15 @@ import no.nav.melosys.domain.kodeverk.Sakstyper;
 import no.nav.melosys.domain.saksflyt.ProsessSteg;
 import no.nav.melosys.domain.saksflyt.Prosessinstans;
 import no.nav.melosys.exception.MelosysException;
-import no.nav.melosys.service.persondata.PersondataFasade;
 import no.nav.melosys.saksflyt.steg.StegBehandler;
 import no.nav.melosys.service.behandling.BehandlingService;
+import no.nav.melosys.service.persondata.PersondataFasade;
 import no.nav.melosys.service.registeropplysninger.RegisteropplysningerRequest;
 import no.nav.melosys.service.registeropplysninger.RegisteropplysningerService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import static no.nav.melosys.domain.saksflyt.ProsessSteg.HENT_REGISTEROPPLYSNINGER;
@@ -28,7 +29,9 @@ public class HentRegisteropplysninger implements StegBehandler {
     private final PersondataFasade persondataFasade;
 
     @Autowired
-    public HentRegisteropplysninger(RegisteropplysningerService registeropplysningerService, BehandlingService behandlingService, PersondataFasade persondataFasade) {
+    public HentRegisteropplysninger(RegisteropplysningerService registeropplysningerService,
+                                    BehandlingService behandlingService,
+                                    @Qualifier("system") PersondataFasade persondataFasade) {
         this.registeropplysningerService = registeropplysningerService;
         this.behandlingService = behandlingService;
         this.persondataFasade = persondataFasade;
