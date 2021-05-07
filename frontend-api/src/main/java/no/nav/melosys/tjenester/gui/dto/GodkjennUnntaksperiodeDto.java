28de2f76dbe4b7b2ddd83ae57ff0e60e41799375
martonSkjevelandNav <marton.skjeveland@nav.no>
2021-05-07 12:46:46 +0200

    Fjernet test + oppdatert schema + lagt på jsonfelter

--- frontend-api/src/main/java/no/nav/melosys/tjenester/gui/dto/GodkjennUnntaksperiodeDto.java
+++ frontend-api/src/main/java/no/nav/melosys/tjenester/gui/dto/GodkjennUnntaksperiodeDto.java
@@ -1,11 +1,16 @@
 package no.nav.melosys.tjenester.gui.dto;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class GodkjennUnntaksperiodeDto {
 
     private final boolean varsleUtland;
     private final String fritekst;
 
-    public GodkjennUnntaksperiodeDto(boolean varsleUtland, String fritekst) {
+    @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
+    public GodkjennUnntaksperiodeDto(@JsonProperty("varsleUtland") boolean varsleUtland,
+                                     @JsonProperty("fritekst") String fritekst) {
         this.varsleUtland = varsleUtland;
         this.fritekst = fritekst;
     }
