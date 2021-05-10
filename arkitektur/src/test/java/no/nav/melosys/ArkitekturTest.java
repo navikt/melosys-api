d4d0afecb9398b593dc48717339e19e815189c08
Andreas Strand <andreas.strand@nav.no>
2021-05-10 21:40:52 +0200

    Tester at Mockito lenient ikke legges til på nye tester

--- arkitektur/src/test/java/no/nav/melosys/ArkitekturTest.java
+++ arkitektur/src/test/java/no/nav/melosys/ArkitekturTest.java
@@ -1,9 +1,17 @@
 package no.nav.melosys;
 
+import com.tngtech.archunit.core.domain.JavaClass;
 import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchCondition;
 import com.tngtech.archunit.lang.ArchRule;
+import com.tngtech.archunit.lang.ConditionEvents;
+import com.tngtech.archunit.lang.SimpleConditionEvent;
+import com.tngtech.archunit.library.freeze.FreezingArchRule;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
 
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
 import static com.tngtech.archunit.library.Architectures.layeredArchitecture;
 
 @AnalyzeClasses(packages = "no.nav.melosys")
@@ -19,4 +27,24 @@ class ArkitekturTest {
         .whereLayer("Service").mayOnlyBeAccessedByLayers("Controller")
         .whereLayer("Integrations").mayOnlyBeAccessedByLayers("Service")
         .whereLayer("Persistence").mayOnlyBeAccessedByLayers("Service");
+
+
+    private static final ArchCondition<JavaClass> notBeUsingStrictnessLenient =
+        new ArchCondition<>("not be using lenient strictness") {
+            @Override
+            public void check(JavaClass javaClass, ConditionEvents conditionEvents) {
+                var mockitoSettings = javaClass.tryGetAnnotationOfType(MockitoSettings.class);
+                if (mockitoSettings.isPresent() && mockitoSettings.get().strictness() == Strictness.LENIENT) {
+                    var error = String.format(
+                        "Test-class %s is using @MockitoSettings with LENIENT strictness", javaClass.getSimpleName()
+                    );
+                    conditionEvents.add(SimpleConditionEvent.violated(javaClass, error));
+                }
+            }
+        };
+
+    @ArchTest
+    static final ArchRule do_not_use_lenient_strictness_on_tests = FreezingArchRule.freeze(
+        classes().that().areAnnotatedWith(MockitoSettings.class).should(notBeUsingStrictnessLenient)
+    );
 }
