4ab4c0bd2ac2aaf5764edee4cc518474bbd4f2f1
Marton Skjæveland <56435736+martonSkjevelandNav@users.noreply.github.com>
2021-06-08 12:32:50 +0200

    Producer for fattetvedtak på kafka og test av validering (#594)
    * Skjelett for fattetvedtak på kafka og test av validering
    
    * Lagtt til pool for injecting av kafkaprops
    
    * La til ny kafka-cofig for aiven
    
    * Endret miljøvariabel fra POOL til KAFKA_POOL
    
    * Lagt til kafka-miljøvariabler fra NAIS
    
    * Smårefaktoreringer etter pr
    
    * Endret configs for å støtte lokal kjøring

--- service/src/main/java/no/nav/melosys/service/vedtak/publisering/FattetVedtakProducer.java
+++ service/src/main/java/no/nav/melosys/service/vedtak/publisering/FattetVedtakProducer.java
@@ -1,32 +1,62 @@
 package no.nav.melosys.service.vedtak.publisering;
 
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
+import no.nav.melosys.exception.IntegrasjonException;
 import no.nav.melosys.service.JsonSchemaValidator;
 import no.nav.melosys.service.vedtak.publisering.dto.FattetVedtak;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Component;
+import org.springframework.util.concurrent.ListenableFuture;
 
 @Component
 public class FattetVedtakProducer {
+
     private static final Logger log = LoggerFactory.getLogger(FattetVedtakProducer.class);
     private static final String FATTET_VEDTAK_SCHEMA = "fattet-vedtak-schema.json";
 
+    private final KafkaTemplate<String, FattetVedtak> kafkaTemplate;
     private final ObjectMapper objectMapper;
+    private final String topicName;
 
     @Autowired
-    public FattetVedtakProducer(ObjectMapper objectMapper) {
+    public FattetVedtakProducer(@Qualifier("fattetVedtak") KafkaTemplate kafkaTemplate,
+                                @Value("${kafka.fattetvedtak.topic}") String topicName,
+                                ObjectMapper objectMapper) {
+        this.kafkaTemplate = kafkaTemplate;
         this.objectMapper = objectMapper;
+        this.topicName = topicName;
     }
 
-    public void publiserMelding(FattetVedtak fattetVedtak) {
-        validerMelding(fattetVedtak);
-        //TODO Implementer med Aiven oppsett
+    public FattetVedtak produserMelding(FattetVedtak fattetVedtak) {
+        valider(fattetVedtak);
+        ListenableFuture<SendResult<String, FattetVedtak>> future = kafkaTemplate.send(topicName, fattetVedtak);
+
+        try {
+            SendResult<String, FattetVedtak> res = future.get(15L, TimeUnit.SECONDS);
+            log.info("Melding sendt på topic {} for sak {}. Record.value: {}, offset: {}",
+                topicName, res.getProducerRecord().value().sak().saksnummer(), res.getProducerRecord().value(), res.getRecordMetadata().offset()
+            );
+            return res.getProducerRecord().value();
+        } catch (ExecutionException | TimeoutException e) {
+            throw new IntegrasjonException("Kunne ikke sende melding om fattet vedtak for sak " + fattetVedtak.sak().saksnummer(), e);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new IntegrasjonException("Avbrutt ved sending av melding om fattet vedtak");
+        }
     }
 
-    private void validerMelding(FattetVedtak fattetVedtak) {
+    private void valider(FattetVedtak fattetVedtak) {
         new JsonSchemaValidator(objectMapper).valider(fattetVedtak, FATTET_VEDTAK_SCHEMA);
     }
 }
