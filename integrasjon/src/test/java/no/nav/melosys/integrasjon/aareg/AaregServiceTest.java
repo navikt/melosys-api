7332a24c17435c60191b149c9f32ecb5088a0ce0
RuneLind <rulind@gmail.com>
2021-06-23 23:45:53 +0200

    Flytt aareg rest api test til AaregServiceTest

--- integrasjon/src/test/java/no/nav/melosys/integrasjon/aareg/AaregServiceTest.java
+++ integrasjon/src/test/java/no/nav/melosys/integrasjon/aareg/AaregServiceTest.java
@@ -1,11 +1,15 @@
 package no.nav.melosys.integrasjon.aareg;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.github.tomakehurst.wiremock.WireMockServer;
 import no.finn.unleash.FakeUnleash;
+import no.finn.unleash.Unleash;
 import no.nav.melosys.domain.Saksopplysning;
 import no.nav.melosys.domain.dokument.DokumentFactory;
 import no.nav.melosys.domain.dokument.XsltTemplatesFactory;
+import no.nav.melosys.domain.dokument.arbeidsforhold.Arbeidsforhold;
 import no.nav.melosys.domain.dokument.arbeidsforhold.ArbeidsforholdDokument;
 import no.nav.melosys.domain.dokument.jaxb.JaxbConfig;
 import no.nav.melosys.exception.SikkerhetsbegrensningException;
@@ -14,12 +18,19 @@ import no.nav.melosys.integrasjon.aareg.arbeidsforhold.ArbeidsforholdMock;
 import no.nav.melosys.integrasjon.aareg.arbeidsforhold.ArbeidsforholdRestConsumer;
 import no.nav.tjeneste.virksomhet.arbeidsforhold.v3.binding.HentArbeidsforholdHistorikkSikkerhetsbegrensning;
 import no.nav.tjeneste.virksomhet.arbeidsforhold.v3.meldinger.HentArbeidsforholdHistorikkRequest;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
 import static org.assertj.core.api.Assertions.catchThrowable;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.ArgumentMatchers.argThat;
@@ -29,20 +40,61 @@ import static org.mockito.Mockito.when;
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class AaregServiceTest {
     private static final Long SIKKERHETSBEGRENSET_ID = 1L;
+    private static final String NAV_PERSONIDENT = "12345678990";
 
     private AaregService aaregService;
     private Jaxb2Marshaller marshaller;
     private ObjectMapper objectMapper = new ObjectMapper();
 
+    private WireMockServer wireMockServer;
+    private ArbeidsforholdRestConsumer restConsumer;
+    private FakeUnleash fakeUnleash = new FakeUnleash();
+
     @BeforeAll
     public void setupBeforeAll() {
+        marshaller = JaxbConfig.jaxb2Marshaller();
         objectMapper.registerModule(new JavaTimeModule());
+        wireMockServer = new WireMockServer(wireMockConfig().dynamicPort());
+        wireMockServer.start();
+
+        WebClient webClient = WebClient.builder()
+            .baseUrl("http://localhost:" + wireMockServer.port())
+            .build();
+        restConsumer = new ArbeidsforholdRestConsumer(webClient);
+        aaregService = lagAaregService(new ArbeidsforholdMock(), fakeUnleash, restConsumer);
     }
 
     @BeforeEach
     public void setUp() {
-        marshaller = JaxbConfig.jaxb2Marshaller();
-        aaregService = lagAaregService(new ArbeidsforholdMock());
+        fakeUnleash.disable("melosys.aareg.rest");
+    }
+
+    @Test
+    public void getArbeidsforholdDokumentFromRestService() throws JsonProcessingException {
+        wireMockServer.stubFor(get(urlPathEqualTo("/"))
+            .withHeader("Nav-Personident", equalTo(NAV_PERSONIDENT))
+            .withQueryParam("regelverk", equalTo("A_ORDNINGEN"))
+            .withQueryParam("ansettelsesperiodeFom", equalTo("2014-07-01"))
+            .withQueryParam("ansettelsesperiodeTom", equalTo("2015-12-31"))
+            .willReturn(aResponse()
+                .withStatus(200)
+                .withHeader("Content-Type", "application/json")
+                .withBody(responsBody)
+            )
+        );
+        fakeUnleash.enable("melosys.aareg.rest");
+        Saksopplysning saksopplysning = aaregService.finnArbeidsforholdPrArbeidstaker(
+            NAV_PERSONIDENT,
+            LocalDate.of(2014, 7, 1),
+            LocalDate.of(2015, 12, 31));
+        ArbeidsforholdDokument arbeidsforholdDokument = (ArbeidsforholdDokument) saksopplysning.getDokument();
+
+        List<Arbeidsforhold> arbeidsforhold = arbeidsforholdDokument.getArbeidsforhold();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        String result = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(arbeidsforhold);
+        Assertions.assertThat(result).isEqualTo(expectedResult);
     }
 
     @Test
@@ -73,7 +125,7 @@ public class AaregServiceTest {
     @Test
     public void hentSikkerhetsbegrensetArbeidsforholdHistorikkKasterUnntak() throws Exception {
         ArbeidsforholdConsumer arbeidsforholdConsumer = mockArbeidsforholdConsumer();
-        AaregService instans = lagAaregService(arbeidsforholdConsumer);
+        AaregService instans = lagAaregService(arbeidsforholdConsumer, new FakeUnleash(), null);
         Throwable unntak = catchThrowable(() -> instans.hentArbeidsforholdHistorikk(SIKKERHETSBEGRENSET_ID));
         assertThat(unntak).isInstanceOf(SikkerhetsbegrensningException.class)
             .hasMessageContaining("oppslag av arbeidsforhold");
@@ -89,10 +141,11 @@ public class AaregServiceTest {
         return arbeidsforholdConsumer;
     }
 
-    private AaregService lagAaregService(ArbeidsforholdConsumer arbeidsforholdConsumer) {
+    private AaregService lagAaregService(ArbeidsforholdConsumer arbeidsforholdConsumer,
+                                         Unleash unleash,
+                                         ArbeidsforholdRestConsumer arbeidsforholdRestConsumer) {
         DokumentFactory dokumentFactory = new DokumentFactory(marshaller, new XsltTemplatesFactory());
-        FakeUnleash unleash = new FakeUnleash();
-        return new AaregService(arbeidsforholdConsumer, dokumentFactory, new ArbeidsforholdRestConsumer(null), unleash);
+        return new AaregService(arbeidsforholdConsumer, dokumentFactory, arbeidsforholdRestConsumer, unleash);
     }
 
     private final String expectedSoapApiResult = """
@@ -145,4 +198,229 @@ public class AaregServiceTest {
           "Aordning" : true,
           "timerTimelonnet" : [ ]
         } ]""";
+
+    private final String expectedResult = """
+        [ {
+          "arbeidsforholdID" : "abc-321",
+          "arbeidsforholdIDnav" : 123456,
+          "ansettelsesPeriode" : {
+            "fom" : [ 2014, 7, 1 ],
+            "tom" : [ 2015, 12, 31 ]
+          },
+          "arbeidsforholdstype" : "ordinaertArbeidsforhold",
+          "arbeidsavtaler" : [ {
+            "arbeidstidsordning" : {
+              "kode" : "ikkeSkift"
+            },
+            "avloenningstype" : "",
+            "yrke" : {
+              "kode" : "2130123",
+              "term" : ""
+            },
+            "gyldighetsperiode" : {
+              "fom" : [ 2014, 7, 1 ],
+              "tom" : [ 2015, 12, 31 ]
+            },
+            "avtaltArbeidstimerPerUke" : 37.5,
+            "stillingsprosent" : 49.5,
+            "sisteLoennsendringsdato" : [ 2014, 7, 1 ],
+            "beregnetAntallTimerPrUke" : 26.5,
+            "endringsdatoStillingsprosent" : [ 2014, 7, 1 ],
+            "skipsregister" : null,
+            "skipstype" : null,
+            "maritimArbeidsavtale" : false,
+            "beregnetStillingsprosent" : 0.7,
+            "antallTimerGammeltAa" : null,
+            "fartsomraade" : null
+          } ],
+          "permisjonOgPermittering" : [ {
+            "permisjonsId" : "123-xyz",
+            "permisjonsPeriode" : {
+              "fom" : [ 2014, 7, 1 ],
+              "tom" : [ 2015, 12, 31 ]
+            },
+            "permisjonsprosent" : 50.5,
+            "permisjonOgPermittering" : "permisjonMedForeldrepenger"
+          } ],
+          "utenlandsopphold" : [ {
+            "periode" : {
+              "fom" : [ 2014, 7, 1 ],
+              "tom" : [ 2015, 12, 31 ]
+            },
+            "land" : "JPN",
+            "rapporteringsAarMaaned" : [ 2017, 12 ]
+          } ],
+          "arbeidsgivertype" : "ORGANISASJON",
+          "arbeidsgiverID" : "991609407",
+          "arbeidstakerID" : "31126700000",
+          "opplysningspliktigtype" : "ORGANISASJON",
+          "opplysningspliktigID" : "991609407",
+          "opprettelsestidspunkt" : 1537265549.000000000,
+          "sistBekreftet" : 1537355431.000000000,
+          "Aordning" : false,
+          "timerTimelonnet" : [ {
+            "antallTimer" : 37.5,
+            "timelonnetPeriode" : {
+              "fom" : [ 2014, 7, 1 ],
+              "tom" : [ 2015, 12, 31 ]
+            },
+            "rapporteringsAarMaaned" : [ 2018, 5 ]
+          } ]
+        } ]""";
+
+    private final String responsBody = """
+        [
+          {
+            "ansettelsesperiode": {
+              "bruksperiode": {
+                "fom": "2015-01-06T21:44:04.748",
+                "tom": "2015-12-06T19:45:04"
+              },
+              "periode": {
+                "fom": "2014-07-01",
+                "tom": "2015-12-31"
+              },
+              "sluttaarsak": "arbeidstakerHarSagtOppSelv",
+              "sporingsinformasjon": {
+                "endretAv": "Z990693",
+                "endretKilde": "AAREG",
+                "endretKildereferanse": "referanse-fra-kilde",
+                "endretTidspunkt": "2018-09-19T12:11:20.79",
+                "opprettetAv": "srvappserver",
+                "opprettetKilde": "EDAG",
+                "opprettetKildereferanse": "22a26849-aeef-4b81-9174-e238c11e1081",
+                "opprettetTidspunkt": "2018-09-19T12:10:58.059"
+              },
+              "varslingskode": "ERKONK"
+            },
+            "antallTimerForTimeloennet": [
+              {
+                "antallTimer": 37.5,
+                "periode": {
+                  "fom": "2014-07-01",
+                  "tom": "2015-12-31"
+                },
+                "rapporteringsperiode": "2018-05",
+                "sporingsinformasjon": {
+                  "endretAv": "Z990693",
+                  "endretKilde": "AAREG",
+                  "endretKildereferanse": "referanse-fra-kilde",
+                  "endretTidspunkt": "2018-09-19T12:11:20.79",
+                  "opprettetAv": "srvappserver",
+                  "opprettetKilde": "EDAG",
+                  "opprettetKildereferanse": "22a26849-aeef-4b81-9174-e238c11e1081",
+                  "opprettetTidspunkt": "2018-09-19T12:10:58.059"
+                }
+              }
+            ],
+            "arbeidsavtaler": [
+              {
+                "ansettelsesform": "fast",
+                "antallTimerPrUke": 37.5,
+                "arbeidstidsordning": "ikkeSkift",
+                "beregnetAntallTimerPrUke": 26.5,
+                "bruksperiode": {
+                  "fom": "2015-01-06T21:44:04.748",
+                  "tom": "2015-12-06T19:45:04"
+                },
+                "gyldighetsperiode": {
+                  "fom": "2014-07-01",
+                  "tom": "2015-12-31"
+                },
+                "sistLoennsendring": "2014-07-01",
+                "sistStillingsendring": "2014-07-01",
+                "sporingsinformasjon": {
+                  "endretAv": "Z990693",
+                  "endretKilde": "AAREG",
+                  "endretKildereferanse": "referanse-fra-kilde",
+                  "endretTidspunkt": "2018-09-19T12:11:20.79",
+                  "opprettetAv": "srvappserver",
+                  "opprettetKilde": "EDAG",
+                  "opprettetKildereferanse": "22a26849-aeef-4b81-9174-e238c11e1081",
+                  "opprettetTidspunkt": "2018-09-19T12:10:58.059"
+                },
+                "stillingsprosent": 49.5,
+                "type": "Forenklet",
+                "yrke": "2130123"
+              }
+            ],
+            "arbeidsforholdId": "abc-321",
+            "arbeidsgiver": {
+              "type": "Organisasjon",
+              "organisasjonsnummer": "991609407"
+            },
+            "arbeidstaker": {
+              "aktoerId": "1234567890",
+              "offentligIdent": "31126700000"
+            },
+            "innrapportertEtterAOrdningen": false,
+            "navArbeidsforholdId": 123456,
+            "opplysningspliktig": {
+              "type": "Organisasjon",
+              "organisasjonsnummer": "991609407"
+            },
+            "permisjonPermitteringer": [
+              {
+                "periode": {
+                  "fom": "2014-07-01",
+                  "tom": "2015-12-31"
+                },
+                "permisjonPermitteringId": "123-xyz",
+                "prosent": 50.5,
+                "sporingsinformasjon": {
+                  "endretAv": "Z990693",
+                  "endretKilde": "AAREG",
+                  "endretKildereferanse": "referanse-fra-kilde",
+                  "endretTidspunkt": "2018-09-19T12:11:20.79",
+                  "opprettetAv": "srvappserver",
+                  "opprettetKilde": "EDAG",
+                  "opprettetKildereferanse": "22a26849-aeef-4b81-9174-e238c11e1081",
+                  "opprettetTidspunkt": "2018-09-19T12:10:58.059"
+                },
+                "type": "permisjonMedForeldrepenger",
+                "varslingskode": "string"
+              }
+            ],
+            "registrert": "2018-09-18T11:12:29",
+            "sistBekreftet": "2018-09-19T12:10:31",
+            "sporingsinformasjon": {
+              "endretAv": "Z990693",
+              "endretKilde": "AAREG",
+              "endretKildereferanse": "referanse-fra-kilde",
+              "endretTidspunkt": "2018-09-19T12:11:20.79",
+              "opprettetAv": "srvappserver",
+              "opprettetKilde": "EDAG",
+              "opprettetKildereferanse": "22a26849-aeef-4b81-9174-e238c11e1081",
+              "opprettetTidspunkt": "2018-09-19T12:10:58.059"
+            },
+            "type": "ordinaertArbeidsforhold",
+            "utenlandsopphold": [
+              {
+                "landkode": "JPN",
+                "periode": {
+                  "fom": "2014-07-01",
+                  "tom": "2015-12-31"
+                },
+                "rapporteringsperiode": "2017-12",
+                "sporingsinformasjon": {
+                  "endretAv": "Z990693",
+                  "endretKilde": "AAREG",
+                  "endretKildereferanse": "referanse-fra-kilde",
+                  "endretTidspunkt": "2018-09-19T12:11:20.79",
+                  "opprettetAv": "srvappserver",
+                  "opprettetKilde": "EDAG",
+                  "opprettetKildereferanse": "22a26849-aeef-4b81-9174-e238c11e1081",
+                  "opprettetTidspunkt": "2018-09-19T12:10:58.059"
+                }
+              }
+            ],
+            "varsler": [
+              {
+                "entitet": "ANSETTELSESPERIODE",
+                "varslingskode": "string"
+              }
+            ]
+          }
+        ]
+        """;
 }
