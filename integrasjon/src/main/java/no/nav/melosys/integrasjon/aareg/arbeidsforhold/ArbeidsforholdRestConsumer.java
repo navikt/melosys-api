8a17c65a7fa854ed481873293f4fd4104b51ab92
RuneLind <rulind@gmail.com>
2021-06-28 10:43:22 +0200

    Fjern ulogisk variable navn og unødvendig null check

--- integrasjon/src/main/java/no/nav/melosys/integrasjon/aareg/arbeidsforhold/ArbeidsforholdRestConsumer.java
+++ integrasjon/src/main/java/no/nav/melosys/integrasjon/aareg/arbeidsforhold/ArbeidsforholdRestConsumer.java
@@ -4,8 +4,6 @@ import no.nav.melosys.integrasjon.felles.RestConsumer;
 import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.WebClient;
 
-import static java.util.Objects.requireNonNull;
-
 public class ArbeidsforholdRestConsumer implements RestConsumer {
 
     private final WebClient webClient;
@@ -15,25 +13,22 @@ public class ArbeidsforholdRestConsumer implements RestConsumer {
     }
 
     public ArbeidsforholdResponse finnArbeidsforholdPrArbeidstaker(String fnr, ArbeidsforholdQuery arbeidsforholdQuery) {
-        ArbeidsforholdResponse.Arbeidsforhold[] arbeidsforholdResponse = hentArbeidsforhold(fnr, arbeidsforholdQuery);
-        return new ArbeidsforholdResponse(arbeidsforholdResponse);
+        return new ArbeidsforholdResponse(hentArbeidsforhold(fnr, arbeidsforholdQuery));
     }
 
     private ArbeidsforholdResponse.Arbeidsforhold[] hentArbeidsforhold(String fnr, ArbeidsforholdQuery arbeidsforholdQuery) {
-        return requireNonNull( // TODO: sjekk om tjenesten kan returnere null;
-            webClient.get().uri("", uriBuilder ->
-                uriBuilder
-                    .queryParam("regelverk", arbeidsforholdQuery.getRegelverk())
-                    .queryParamIfPresent("arbeidsforholdType", arbeidsforholdQuery.getArbeidsforholdType())
-                    .queryParamIfPresent("ansettelsesperiodeFom", arbeidsforholdQuery.getAnsettelsesperiodeFom())
-                    .queryParamIfPresent("ansettelsesperiodeTom", arbeidsforholdQuery.getAnsettelsesperiodeTom())
-                    // TODO: sjekk om metriks får url uten detaljer så det funker i grafana
-                    .build())
-                .header("Nav-Personident", fnr)
-                .accept(MediaType.APPLICATION_JSON)
-                .retrieve()
-                .bodyToMono(ArbeidsforholdResponse.Arbeidsforhold[].class)
-                .block()
-        );
+        return webClient.get().uri("", uriBuilder ->
+            uriBuilder
+                .queryParam("regelverk", arbeidsforholdQuery.getRegelverk())
+                .queryParamIfPresent("arbeidsforholdType", arbeidsforholdQuery.getArbeidsforholdType())
+                .queryParamIfPresent("ansettelsesperiodeFom", arbeidsforholdQuery.getAnsettelsesperiodeFom())
+                .queryParamIfPresent("ansettelsesperiodeTom", arbeidsforholdQuery.getAnsettelsesperiodeTom())
+                // TODO: sjekk om metriks får url uten detaljer så det funker i grafana
+                .build())
+            .header("Nav-Personident", fnr)
+            .accept(MediaType.APPLICATION_JSON)
+            .retrieve()
+            .bodyToMono(ArbeidsforholdResponse.Arbeidsforhold[].class)
+            .block();
     }
 }
