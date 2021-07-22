ec51f9a10a94a16f523c838d22ae364195e63b61
Francois Allix <francois.allix@nav.no>
2021-07-22 11:24:16 +0200

    MelosysModule er til frontend-api
    Forhindrer autoconfig av MelosysModule for default ObjectMapper
    og bruker MelosysModule i API

--- frontend-api/src/main/java/no/nav/melosys/tjenester/gui/config/WebConfig.java
+++ frontend-api/src/main/java/no/nav/melosys/tjenester/gui/config/WebConfig.java
@@ -1,7 +1,21 @@
 package no.nav.melosys.tjenester.gui.config;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import no.nav.melosys.service.kodeverk.KodeverkService;
+import no.nav.melosys.tjenester.gui.config.jackson.MelosysModule;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
@@ -9,16 +23,36 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
     private static final String API_PREFIX = "/api";
     private static final String FRONTEND_API_TJENESTER = "no.nav.melosys.tjenester.gui";
+    private final KodeverkService kodeverkService;
+
+    @Autowired
+    public WebConfig(KodeverkService kodeverkService) {
+        this.kodeverkService = kodeverkService;
+    }
+
+    @Override
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.removeAll(converters.stream()
+            .filter(MappingJackson2HttpMessageConverter.class::isInstance).collect(Collectors.toSet()));
+        converters.add(new MappingJackson2HttpMessageConverter(apiObjectMapper()));
+    }
+
+    private ObjectMapper apiObjectMapper() {
+        return Jackson2ObjectMapperBuilder.json()
+            .modules(new JavaTimeModule(), new MelosysModule(kodeverkService))
+            .featuresToEnable(MapperFeature.DEFAULT_VIEW_INCLUSION)
+            .featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+            .build();
+    }
 
     @Override
     public void configurePathMatch(PathMatchConfigurer configurer) {
         configurer.addPathPrefix(API_PREFIX, WebConfig::erFrontendApiRestTjeneste);
     }
 
-    private static boolean erFrontendApiRestTjeneste(Class clazz) {
+    private static boolean erFrontendApiRestTjeneste(Class<?> clazz) {
         return clazz.getPackageName().startsWith(FRONTEND_API_TJENESTER)
             && clazz.isAnnotationPresent(RestController.class);
     }
