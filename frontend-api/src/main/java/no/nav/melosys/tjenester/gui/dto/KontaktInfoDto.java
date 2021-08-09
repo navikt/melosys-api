41020b671cdba0f753e80addd458e395fe33a2ad
JohanSakshaug <johan.sakshaug@nav.no>
2021-08-09 10:36:52 +0200

    Fiks jackson-error: InvalidDefinitionException: Cannot construct instance of `no.nav.melosys.tjenester.gui.dto.KontaktInfoDto` (no Creators, like default constructor, exist): cannot deserialize from Object value (no delegate- or property-based Creator)

--- frontend-api/src/main/java/no/nav/melosys/tjenester/gui/dto/KontaktInfoDto.java
+++ frontend-api/src/main/java/no/nav/melosys/tjenester/gui/dto/KontaktInfoDto.java
@@ -1,5 +1,7 @@
 package no.nav.melosys.tjenester.gui.dto;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import no.nav.melosys.domain.Kontaktopplysning;
 
 public class KontaktInfoDto {
@@ -7,7 +9,10 @@ public class KontaktInfoDto {
     private String kontaktorgnr;
     private String kontakttelefon;
 
-    public KontaktInfoDto(String kontaktnavn, String kontaktorgnr, String kontakttelefon) {
+    @JsonCreator
+    public KontaktInfoDto(@JsonProperty("kontaktnavn") String kontaktnavn,
+                          @JsonProperty("kontaktorgnr") String kontaktorgnr,
+                          @JsonProperty("kontakttelefon") String kontakttelefon) {
         this.kontaktnavn = kontaktnavn;
         this.kontaktorgnr = kontaktorgnr;
         this.kontakttelefon = kontakttelefon;
