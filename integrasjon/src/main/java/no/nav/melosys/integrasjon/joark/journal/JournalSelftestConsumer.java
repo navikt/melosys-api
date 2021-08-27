bd0f23bcfa29e8ceb24e31396fbd860f888f37bb
Andreas Strand <andreas.strand@nav.no>
2021-08-27 09:24:37 +0200

    Fjerner integrasjoner som er erstattet av SAF

--- integrasjon/src/main/java/no/nav/melosys/integrasjon/joark/journal/JournalSelftestConsumer.java
+++
@@ -1,6 +0,0 @@
-package no.nav.melosys.integrasjon.joark.journal;
-
-import no.nav.melosys.integrasjon.felles.SelftestConsumer;
-
-public interface JournalSelftestConsumer extends SelftestConsumer {
-}
