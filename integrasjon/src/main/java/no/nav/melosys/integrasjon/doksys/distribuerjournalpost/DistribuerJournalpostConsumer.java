cf2e9f68491d98eb0718eb58d9d84fdbc1fa389f
JohanSakshaug <johan.sakshaug@nav.no>
2021-09-06 13:28:20 +0200

    DistribuerjournalpostConsumer exchange()

--- integrasjon/src/main/java/no/nav/melosys/integrasjon/doksys/distribuerjournalpost/DistribuerJournalpostConsumer.java
+++ integrasjon/src/main/java/no/nav/melosys/integrasjon/doksys/distribuerjournalpost/DistribuerJournalpostConsumer.java
@@ -1,21 +1,33 @@
 package no.nav.melosys.integrasjon.doksys.distribuerjournalpost;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import no.nav.melosys.exception.IntegrasjonException;
+import no.nav.melosys.exception.SikkerhetsbegrensningException;
 import no.nav.melosys.integrasjon.doksys.distribuerjournalpost.dto.DistribuerJournalpostRequest;
 import no.nav.melosys.integrasjon.doksys.distribuerjournalpost.dto.DistribuerJournalpostResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
+import org.springframework.util.StringUtils;
+import org.springframework.web.client.HttpStatusCodeException;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
 
 public class DistribuerJournalpostConsumer {
     private static final Logger log = LoggerFactory.getLogger(DistribuerJournalpostConsumer.class);
 
     private final RestTemplate restTemplate;
+    private final ObjectMapper objectMapper;
 
     public DistribuerJournalpostConsumer(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
+        this.objectMapper = new ObjectMapper();
     }
 
     public DistribuerJournalpostResponse distribuerJournalpost(DistribuerJournalpostRequest request) {
@@ -25,6 +37,35 @@ public class DistribuerJournalpostConsumer {
         headers.add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
         headers.add(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE);
 
-        return restTemplate.postForObject("", new HttpEntity<>(request, headers), DistribuerJournalpostResponse.class);
+        return exchange("", HttpMethod.POST, new HttpEntity<>(request, headers));
+    }
+
+    private DistribuerJournalpostResponse exchange(String uri, HttpMethod method, HttpEntity<?> entity) {
+        try {
+            return restTemplate.exchange(uri, method, entity, DistribuerJournalpostResponse.class).getBody();
+        } catch (HttpStatusCodeException ex) {
+            String feilmelding = hentFeilmelding(ex);
+            switch (ex.getStatusCode()) {
+                case UNAUTHORIZED:
+                case FORBIDDEN:
+                    throw new SikkerhetsbegrensningException(feilmelding, ex);
+                default:
+                    throw new IntegrasjonException(feilmelding, ex);
+            }
+        } catch (RestClientException ex) {
+            throw new IntegrasjonException("Ukjent feil mot distribuerjournalpost", ex);
+        }
+    }
+
+    private String hentFeilmelding(HttpStatusCodeException e) {
+        String feilmelding = e.getResponseBodyAsString();
+        if (StringUtils.isEmpty(feilmelding)) return e.getMessage();
+        try {
+            JsonNode json = objectMapper.readTree(feilmelding).path("message");
+            return json.isMissingNode() ? e.getMessage() : json.toString();
+        } catch (IOException ex) {
+            log.warn("Kunne ikke lese feilmelding fra response", ex);
+            return feilmelding;
+        }
     }
 }
