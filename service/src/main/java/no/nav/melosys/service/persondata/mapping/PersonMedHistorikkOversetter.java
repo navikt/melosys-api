cde01da89850ef21d7d3b9594bf0907581d76206
Francois Allix <32903762+frans-a@users.noreply.github.com>
2021-09-27 12:50:40 +0200

    Sortering trengs ikke her

--- service/src/main/java/no/nav/melosys/service/persondata/mapping/PersonMedHistorikkOversetter.java
+++ service/src/main/java/no/nav/melosys/service/persondata/mapping/PersonMedHistorikkOversetter.java
@@ -1,6 +1,7 @@
 package no.nav.melosys.service.persondata.mapping;
 
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import no.nav.melosys.integrasjon.pdl.dto.person.Person;
 import no.nav.melosys.service.kodeverk.KodeverkService;
@@ -11,7 +12,6 @@ import no.nav.melosys.service.persondata.mapping.adresse.OppholdsadresseOversett
 
 public final class PersonMedHistorikkOversetter {
     private PersonMedHistorikkOversetter() {
-        throw new IllegalStateException("Ikke ment å bli instantiert");
     }
 
     public static PersonMedHistorikk oversett(Person person, KodeverkService kodeverkService) {
@@ -22,17 +22,19 @@ public final class PersonMedHistorikkOversetter {
         return new PersonMedHistorikk(
             person.bostedsadresse().stream()
                 .map(a -> BostedsadresseOversetter.oversett(a, kodeverkService))
-                .flatMap(Optional::stream).toList(),
+                .flatMap(Optional::stream).collect(Collectors.toUnmodifiableSet()),
             DoedsfallOversetter.oversett(person.doedsfall()),
             FoedselOversetter.oversett(person.foedsel()),
             FolkeregisteridentOversetter.oversett(person.folkeregisteridentifikator()),
             FolkeregisterpersonstatusOversetter.oversett(person.folkeregisterpersonstatus()),
             KjoennOversetter.oversett(person.kjoenn()),
             person.kontaktadresse().stream().map(k -> KontaktadresseOversetter.oversett(k, kodeverkService))
-                .toList(),
+                .collect(Collectors.toUnmodifiableSet()),
             NavnOversetter.oversett(person.navn()),
-            person.oppholdsadresse().stream().map(o -> OppholdsadresseOversetter.oversett(o, kodeverkService)).toList(),
-            person.sivilstand().stream().map(SivilstandOversetter::oversett).toList(),
-            person.statsborgerskap().stream().map(StatsborgerskapOversetter::oversett).toList());
+            person.oppholdsadresse().stream().map(o -> OppholdsadresseOversetter.oversett(o, kodeverkService))
+                .collect(Collectors.toUnmodifiableSet()),
+            person.sivilstand().stream().map(SivilstandOversetter::oversett).collect(Collectors.toUnmodifiableSet()),
+            person.statsborgerskap().stream().map(StatsborgerskapOversetter::oversett)
+                .collect(Collectors.toUnmodifiableSet()));
     }
 }
