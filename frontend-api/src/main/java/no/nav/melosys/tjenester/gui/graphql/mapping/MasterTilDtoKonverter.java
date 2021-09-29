96d472a5a7a5f862c0ab55c7ae0ca059284e66b9
JohanSakshaug <johan.sakshaug@nav.no>
2021-09-29 14:23:01 +0200

    følg java-konvensjon

--- frontend-api/src/main/java/no/nav/melosys/tjenester/gui/graphql/mapping/MasterTilDtoKonverter.java
+++ frontend-api/src/main/java/no/nav/melosys/tjenester/gui/graphql/mapping/MasterTilDtoKonverter.java
@@ -1,13 +1,13 @@
 package no.nav.melosys.tjenester.gui.graphql.mapping;
 
 public final class MasterTilDtoKonverter {
+    final static String PDL = "PDL";
+    final static String NAV_PDL = "NAV (PDL)";
+
     private MasterTilDtoKonverter() {
     }
 
     public static String tilDto(String master) {
-        final String PDL = "PDL";
-        final String NAV_PDL = "NAV (PDL)";
-
         if (PDL.equals(master)) return NAV_PDL;
 
         return master;
