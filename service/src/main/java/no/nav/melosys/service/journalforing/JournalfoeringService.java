a9753d12f1c67a7743e3f66d1a34827cf1f010a2
martonSkjevelandNav <marton.skjeveland@nav.no>
2021-10-06 09:14:35 +0200

    Formattering

--- service/src/main/java/no/nav/melosys/service/journalforing/JournalfoeringService.java
+++ service/src/main/java/no/nav/melosys/service/journalforing/JournalfoeringService.java
@@ -297,8 +297,7 @@ public class JournalfoeringService {
             boolean erUkjenteEllerAlleEosLand = journalfoeringDto.getFagsak().getLand().erUkjenteEllerAlleEosLand();
             if (erUkjenteEllerAlleEosLand && antallLand != 0) {
                 throw new FunksjonellException(String.format("Det kan ikke være noen land for behandlingstema %s om ukjenteEllerAlleEosLand er valgt", behandlingstemaKode));
-            }
-            else if (antallLand < 2) {
+            } else if (antallLand < 2) {
                 throw new FunksjonellException(String.format("Det er påkrevd med to eller flere land for behandlingstema %s om ikke ukjenteEllerAlleEosLand er valgt", behandlingstemaKode));
             }
         } else if (Behandling.erBehandlingAvSøknadUtsendtArbeidstaker(behandlingstemaKode) && antallLand != 1) {
