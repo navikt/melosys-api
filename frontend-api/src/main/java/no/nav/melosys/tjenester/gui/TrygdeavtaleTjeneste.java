03f1d6955854e0f17365e9ca493e86ac6d979c6d
RuneLind <rulind@gmail.com>
2021-10-14 15:58:52 +0200

    Legg til medfølgende famile fra mel-TA
    Og legg til integrasjontest for TrygdeavtaleTjeneste. TrygdeavtaleTjenesteIT kjører nå uten feil :) Forsatt WIP

--- frontend-api/src/main/java/no/nav/melosys/tjenester/gui/TrygdeavtaleTjeneste.java
+++ frontend-api/src/main/java/no/nav/melosys/tjenester/gui/TrygdeavtaleTjeneste.java
@@ -1,15 +1,22 @@
 package no.nav.melosys.tjenester.gui;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Set;
 
 import io.swagger.annotations.Api;
 import no.nav.melosys.domain.behandlingsgrunnlag.Behandlingsgrunnlag;
 import no.nav.melosys.domain.behandlingsgrunnlag.SoeknadFtrl;
 import no.nav.melosys.domain.behandlingsgrunnlag.data.Periode;
+import no.nav.melosys.exception.TekniskException;
 import no.nav.melosys.service.TrygdeavtaleService;
+import no.nav.melosys.service.avklartefakta.AvklarteMedfolgendeFamilieService;
+import no.nav.melosys.service.avklartefakta.AvklartefaktaService;
 import no.nav.melosys.service.behandling.BehandlingService;
 import no.nav.melosys.service.behandlingsgrunnlag.BehandlingsgrunnlagService;
 import no.nav.melosys.service.tilgang.Aksesskontroll;
+import no.nav.melosys.tjenester.gui.dto.LagreMedfolgendeFamilieDto;
+import no.nav.melosys.tjenester.gui.dto.MedfolgendeFamilieDto;
 import no.nav.melosys.tjenester.gui.dto.trygdeavtale.TrygdeavtaleInfoDto;
 import no.nav.melosys.tjenester.gui.dto.trygdeavtale.TrygdeAvtaleDataForVedtakDto;
 import no.nav.security.token.support.core.api.Protected;
@@ -29,15 +36,21 @@ public class TrygdeavtaleTjeneste {
     private final BehandlingService behandlingService;
     private final Aksesskontroll aksesskontroll;
     private final BehandlingsgrunnlagService behandlingsgrunnlagService;
+    private final AvklartefaktaService avklartefaktaService;
+    private final AvklarteMedfolgendeFamilieService avklarteMedfolgendeFamilieService;
 
     public TrygdeavtaleTjeneste(TrygdeavtaleService trygdeavtaleService,
                                 BehandlingService behandlingService,
                                 Aksesskontroll aksesskontroll,
-                                BehandlingsgrunnlagService behandlingsgrunnlagService) {
+                                BehandlingsgrunnlagService behandlingsgrunnlagService,
+                                AvklartefaktaService avklartefaktaService,
+                                AvklarteMedfolgendeFamilieService avklarteMedfolgendeFamilieService) {
         this.trygdeavtaleService = trygdeavtaleService;
         this.behandlingService = behandlingService;
         this.aksesskontroll = aksesskontroll;
         this.behandlingsgrunnlagService = behandlingsgrunnlagService;
+        this.avklartefaktaService = avklartefaktaService;
+        this.avklarteMedfolgendeFamilieService = avklarteMedfolgendeFamilieService;
     }
 
     @GetMapping("{behandlingID}")
@@ -57,27 +70,39 @@ public class TrygdeavtaleTjeneste {
 
     @PostMapping("{behandlingID}")
     public void overforDataForVedtak(
-        @PathVariable("behandlingID") long behandlingsid,
+        @PathVariable("behandlingID") long behandlingsId,
         @RequestBody TrygdeAvtaleDataForVedtakDto trygdeAvtaleDataForVedtakDto) {
 
-        Behandlingsgrunnlag behandlingsgrunnlag = behandlingsgrunnlagService.hentBehandlingsgrunnlag(behandlingsid);
-        SoeknadFtrl behandlingsgrunnlagdata = (SoeknadFtrl) behandlingsgrunnlag.getBehandlingsgrunnlagdata();
+        // TODO: Flytt dette ut til en egen klasse
+        oppdaterBehandlingsgrunnlag(behandlingsId, trygdeAvtaleDataForVedtakDto);
 
-        behandlingsgrunnlagdata.periode = new Periode(
-            trygdeAvtaleDataForVedtakDto.fom(),
-            trygdeAvtaleDataForVedtakDto.tom()
-        );
-
-        behandlingsgrunnlagdata.soeknadsland.landkoder.addAll(trygdeAvtaleDataForVedtakDto.land());
+        var familie = new ArrayList<>(trygdeAvtaleDataForVedtakDto.barn());
+        familie.add(trygdeAvtaleDataForVedtakDto.ektefelle());
+        LagreMedfolgendeFamilieDto lagreMedfolgendeFamilieDto = new LagreMedfolgendeFamilieDto(Set.copyOf(familie));
+        avklarteMedfolgendeFamilieService.lagreMedfolgendeFamilieSomAvklartefakta(behandlingsId, lagreMedfolgendeFamilieDto.til());
 
         // TODO:
         // behandlinger/1/medlemskapsperioder/bestemmelser - trenger ikke
         // behandlinger/1/trygdeavgift/grunnlag - trenger ikke
         // vilkaar/1 - bestemlese vilkår, kanskje lovvalgsperiode
-        // avklartefakta/1/medfolgendeFamilie - begynne med denne
-
+        // avklartefakta/1/medfolgendeFamilie - begynne med denne - Done
         // sjekk lagring av virksomheter
+    }
 
+    private void oppdaterBehandlingsgrunnlag(long behandlingsId, TrygdeAvtaleDataForVedtakDto trygdeAvtaleDataForVedtakDto) {
+        // Periode og land skal alltid være det sammne - men er en sanity sjekk for nå
+        Behandlingsgrunnlag behandlingsgrunnlag = behandlingsgrunnlagService.hentBehandlingsgrunnlag(behandlingsId);
+        SoeknadFtrl behandlingsgrunnlagdata = (SoeknadFtrl) behandlingsgrunnlag.getBehandlingsgrunnlagdata();
+
+        Periode periode = behandlingsgrunnlagdata.periode;
+        assert periode.getFom().equals(trygdeAvtaleDataForVedtakDto.fom());
+        assert periode.getTom().equals(trygdeAvtaleDataForVedtakDto.tom());
+
+        for (var land : trygdeAvtaleDataForVedtakDto.land()) {
+            if (!behandlingsgrunnlagdata.soeknadsland.landkoder.contains(land))
+                throw new TekniskException("Forventet " + land + " i behandlingsgrunnlagdata soeknadsland.landkoder");
+        }
+        // Ser ikke ut som vi trenger og oppdagtere dette nå
         behandlingsgrunnlagService.oppdaterBehandlingsgrunnlag(behandlingsgrunnlag);
     }
 }
---
+++ integrasjonstest/src/test/java/no/nav/melosys/integrasjonstest/saksflyt/TrygdeavtaleTjenesteIT.java
@@ -0,0 +1,146 @@
+package no.nav.melosys.integrasjonstest.saksflyt;
+
+import no.nav.melosys.domain.Behandling;
+import no.nav.melosys.domain.arkiv.Journalpost;
+import no.nav.melosys.domain.behandlingsgrunnlag.Behandlingsgrunnlag;
+import no.nav.melosys.domain.behandlingsgrunnlag.SoeknadFtrl;
+import no.nav.melosys.domain.behandlingsgrunnlag.data.MedfolgendeFamilie;
+import no.nav.melosys.domain.behandlingsgrunnlag.data.Periode;
+import no.nav.melosys.domain.kodeverk.Avsendertyper;
+import no.nav.melosys.domain.kodeverk.begrunnelser.folketrygdloven.Medfolgende_barn_begrunnelser_ftrl;
+import no.nav.melosys.domain.kodeverk.begrunnelser.folketrygdloven.Medfolgende_ektefelle_samboer_begrunnelser_ftrl;
+import no.nav.melosys.service.TrygdeavtaleService;
+import no.nav.melosys.service.avklartefakta.AvklarteMedfolgendeFamilieService;
+import no.nav.melosys.service.avklartefakta.AvklartefaktaService;
+import no.nav.melosys.service.behandling.BehandlingService;
+import no.nav.melosys.service.behandlingsgrunnlag.BehandlingsgrunnlagService;
+import no.nav.melosys.service.felles.dto.SoeknadslandDto;
+import no.nav.melosys.service.journalforing.JournalfoeringService;
+import no.nav.melosys.service.journalforing.dto.DokumentDto;
+import no.nav.melosys.service.journalforing.dto.FagsakDto;
+import no.nav.melosys.service.journalforing.dto.JournalfoeringOpprettDto;
+import no.nav.melosys.service.journalforing.dto.PeriodeDto;
+import no.nav.melosys.service.tilgang.Aksesskontroll;
+import no.nav.melosys.tjenester.gui.TrygdeavtaleTjeneste;
+import no.nav.melosys.tjenester.gui.dto.FagsakSokDto;
+import no.nav.melosys.tjenester.gui.dto.trygdeavtale.TrygdeAvtaleDataForVedtakDto;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(properties = "spring.profiles.active:local-mock")
+class TrygdeavtaleTjenesteIT {
+
+    @Autowired
+    private TrygdeavtaleService trygdeavtaleService;
+    @Autowired
+    private BehandlingService behandlingService;
+    @Autowired
+    private Aksesskontroll aksesskontroll;
+    @Autowired
+    private BehandlingsgrunnlagService behandlingsgrunnlagService;
+    @Autowired
+    private AvklartefaktaService avklartefaktaService;
+    @Autowired
+    private AvklarteMedfolgendeFamilieService avklarteMedfolgendeFamilieService;
+    @Autowired
+    private JournalfoeringService journalfoeringService;
+
+    private TrygdeavtaleTjeneste trygdeavtaleTjeneste;
+
+    @BeforeEach
+    void setup() {
+        trygdeavtaleTjeneste = new TrygdeavtaleTjeneste(
+            trygdeavtaleService,
+            behandlingService,
+            aksesskontroll,
+            behandlingsgrunnlagService,
+            avklartefaktaService,
+            avklarteMedfolgendeFamilieService);
+
+        // TODO: bruk http://localhost:8083/testdata/jfr-oppgave til å lage oppgave og kjør så opprettOgJournalfør
+        Journalpost journalpost = journalfoeringService.hentJournalpost("526345");
+        if (!journalpost.isErFerdigstilt()) {
+            System.out.println("opprettOgJournalfør");
+            JournalfoeringOpprettDto journalfoeringDto = lagJournalfoeringOpprettDto();
+            journalfoeringService.opprettOgJournalfør(journalfoeringDto);
+        }
+
+        // kan slå opp i behandling for å finne behandling id fra INITIERENDE_JOURNALPOST_ID
+        Behandling behandling = behandlingService.hentBehandling(2L);
+        behandlingService.endreBehandlingsstatusFraOpprettetTilUnderBehandling(behandling);
+        Behandlingsgrunnlag behandlingsgrunnlag = behandling.getBehandlingsgrunnlag();
+
+        SoeknadFtrl behandlingsgrunnlagdata = (SoeknadFtrl) behandlingsgrunnlag.getBehandlingsgrunnlagdata();
+        behandlingsgrunnlagdata.periode = new Periode(
+            LocalDate.of(2021, 1, 1),
+            LocalDate.of(2021, 2, 1)
+        );
+        behandlingsgrunnlagdata.soeknadsland.landkoder = List.of("GB");
+        List<MedfolgendeFamilie> medfolgendeFamilie = new ArrayList<>();
+        medfolgendeFamilie.add(MedfolgendeFamilie.tilMedfolgendeFamilie(
+            "0bad5c70-8a3f-4fc7-9031-d3aebd6b68de", "fnr", "role",
+            MedfolgendeFamilie.Relasjonsrolle.BARN
+        ));
+        medfolgendeFamilie.add(MedfolgendeFamilie.tilMedfolgendeFamilie(
+            "1212121212121-4fc7-9031-ab34332121ff", "fnr", "role",
+            MedfolgendeFamilie.Relasjonsrolle.EKTEFELLE_SAMBOER
+        ));
+        behandlingsgrunnlagdata.personOpplysninger.medfolgendeFamilie = medfolgendeFamilie;
+
+        behandlingsgrunnlagService.oppdaterBehandlingsgrunnlag(behandlingsgrunnlag);
+
+        String jsonData = behandlingsgrunnlag.getJsonData();
+        System.out.println(jsonData);
+    }
+
+    private JournalfoeringOpprettDto lagJournalfoeringOpprettDto() {
+        JournalfoeringOpprettDto journalfoeringDto = new JournalfoeringOpprettDto();
+        journalfoeringDto.setAvsenderID("30056928150");
+        journalfoeringDto.setAvsenderNavn("KARAFFEL TRIVIELL");
+        journalfoeringDto.setBrukerID("30056928150");
+        journalfoeringDto.setAvsenderType(Avsendertyper.PERSON);
+        DokumentDto dokumentDto = new DokumentDto("85937", "Søknad om A1 for utsendte arbeidstakere i EØS/Sveits");
+        journalfoeringDto.setHoveddokument(dokumentDto);
+        journalfoeringDto.setJournalpostID("526345");
+        journalfoeringDto.setOppgaveID("1");
+        journalfoeringDto.setSkalTilordnes(true);
+        journalfoeringDto.setMottattDato(LocalDate.of(2021, 10, 13));
+        journalfoeringDto.setBehandlingstemaKode("TRYGDEAVTALE_UK");
+        FagsakDto fagsak = new FagsakDto();
+        fagsak.setSakstype("TRYGDEAVTALE");
+        fagsak.setSoknadsperiode(new PeriodeDto());
+        fagsak.setLand(new SoeknadslandDto());
+        journalfoeringDto.setFagsak(fagsak);
+        return journalfoeringDto;
+    }
+
+    @Test
+    void test() {
+        TrygdeAvtaleDataForVedtakDto trygdeAvtaleDataForVedtakDto = new TrygdeAvtaleDataForVedtakDto.Builder()
+            .fom(LocalDate.of(2021, 1, 1))
+            .tom(LocalDate.of(2021, 2, 1))
+            .land(List.of("GB"))
+            .virksomheter(List.of("11111111111"))
+            .vedtak("JA_FATTE_VEDTAK")
+            .innvilgelse("JA")
+            .bestemmelse("UK_ART6_1")
+            .addBarn("0bad5c70-8a3f-4fc7-9031-d3aebd6b68de",
+                false, Medfolgende_barn_begrunnelser_ftrl.OVER_18_AR.getKode(),
+                "begrunnelse barn")
+            .ektefelle("1212121212121-4fc7-9031-ab34332121ff",
+                false,  Medfolgende_ektefelle_samboer_begrunnelser_ftrl.EGEN_INNTEKT.getKode(),
+                "begrunnelse samboer")
+            .build();
+
+        trygdeavtaleTjeneste.overforDataForVedtak(2L, trygdeAvtaleDataForVedtakDto);
+    }
+}
