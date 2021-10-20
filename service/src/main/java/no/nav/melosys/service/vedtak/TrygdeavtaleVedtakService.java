7dd95d4d250de6c7df954f5be885e743e3942bdc
RuneLind <rulind@gmail.com>
2021-10-20 09:03:50 +0200

    Oppdater TrygdeavtaleVedtakService og FattTrygdeavtaleVedtakRequest
    Og legg til TrygdeavtaleVedtakServiceTest

--- service/src/main/java/no/nav/melosys/service/vedtak/TrygdeavtaleVedtakService.java
+++ service/src/main/java/no/nav/melosys/service/vedtak/TrygdeavtaleVedtakService.java
@@ -2,16 +2,20 @@ package no.nav.melosys.service.vedtak;
 
 import no.nav.melosys.domain.Behandling;
 import no.nav.melosys.domain.Tema;
+import no.nav.melosys.domain.kodeverk.Aktoersroller;
 import no.nav.melosys.domain.kodeverk.Landkoder;
 import no.nav.melosys.domain.kodeverk.Oppgavetyper;
 import no.nav.melosys.domain.kodeverk.Saksstatuser;
 import no.nav.melosys.domain.kodeverk.behandlinger.Behandlingsstatus;
+import no.nav.melosys.domain.kodeverk.brev.Produserbaredokumenter;
 import no.nav.melosys.domain.oppgave.Oppgave;
 import no.nav.melosys.exception.FunksjonellException;
 import no.nav.melosys.exception.IkkeFunnetException;
 import no.nav.melosys.integrasjon.oppgave.OppgaveOppdatering;
 import no.nav.melosys.service.behandling.BehandlingService;
 import no.nav.melosys.service.behandling.BehandlingsresultatService;
+import no.nav.melosys.service.dokument.DokgenService;
+import no.nav.melosys.service.dokument.brev.BrevbestillingRequest;
 import no.nav.melosys.service.oppgave.OppgaveService;
 import no.nav.melosys.service.saksflyt.ProsessinstansService;
 import org.slf4j.Logger;
@@ -32,16 +36,19 @@ public class TrygdeavtaleVedtakService {
     private final BehandlingService behandlingService;
     private final ProsessinstansService prosessinstansService;
     private final OppgaveService oppgaveService;
+    private final DokgenService dokgenService;
 
     @Autowired
     public TrygdeavtaleVedtakService(BehandlingsresultatService behandlingsresultatService,
                                      BehandlingService behandlingService,
                                      ProsessinstansService prosessinstansService,
-                                     OppgaveService oppgaveService) {
+                                     OppgaveService oppgaveService,
+                                     DokgenService dokgenService) {
         this.behandlingsresultatService = behandlingsresultatService;
         this.behandlingService = behandlingService;
         this.prosessinstansService = prosessinstansService;
         this.oppgaveService = oppgaveService;
+        this.dokgenService = dokgenService;
     }
 
     public void fattVedtak(Behandling behandling, FattTrygdeavtaleVedtakRequest request) {
@@ -61,7 +68,21 @@ public class TrygdeavtaleVedtakService {
         behandlingService.lagre(behandling);
 
         prosessinstansService.opprettProsessinstansIverksettVedtak(behandling, request);
+        dokgenService.produserOgDistribuerBrev(behandlingID, lagBrevbestilling(request));
         oppgaveService.ferdigstillOppgaveMedSaksnummer(saksnummer);
+    }
+
+    private BrevbestillingRequest lagBrevbestilling(FattTrygdeavtaleVedtakRequest request) {
+        return new BrevbestillingRequest.Builder()
+            .medProduserbardokument(Produserbaredokumenter.INNVILGELSE_FOLKETRYGDLOVEN_2_8)
+            .medMottaker(Aktoersroller.BRUKER)
+            .medKopiMottakere(request.getKopiMottakere())
+            .medInnledningFritekst(request.getFritekstInnledning())
+            .medBegrunnelseFritekst(request.getFritekstBegrunnelse())
+            .medEktefelleFritekst(request.getFritekstEktefelle())
+            .medBarnFritekst(request.getFritekstBarn())
+            .medBestillersId(request.getBestillersId())
+            .build();
     }
 
     private void oppdaterBehandlingsresultat(long behandlingID, FattTrygdeavtaleVedtakRequest request) throws IkkeFunnetException {
