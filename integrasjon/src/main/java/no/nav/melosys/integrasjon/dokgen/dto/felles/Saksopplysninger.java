8dfaeb484cc99e17d867daf2f77c91b3202700d2
martonSkjevelandNav <marton.skjeveland@nav.no>
2021-11-11 11:38:07 +0100

    Endret rekkefølge på verdier lik dokgen

--- integrasjon/src/main/java/no/nav/melosys/integrasjon/dokgen/dto/felles/Saksopplysninger.java
+++ integrasjon/src/main/java/no/nav/melosys/integrasjon/dokgen/dto/felles/Saksopplysninger.java
@@ -3,15 +3,15 @@ package no.nav.melosys.integrasjon.dokgen.dto.felles;
 import no.nav.melosys.domain.brev.DokgenBrevbestilling;
 
 public record Saksopplysninger(
-    String fnr,
     String saksnummer,
-    String navnBruker
+    String navnBruker,
+    String fnr
 ) {
     public static Saksopplysninger av(DokgenBrevbestilling brevbestilling) {
         return new Saksopplysninger(
-            brevbestilling.getPersondokument().hentFolkeregisterident(),
             brevbestilling.getBehandling().getFagsak().getSaksnummer(),
-            brevbestilling.getPersondokument().getSammensattNavn()
+            brevbestilling.getPersondokument().getSammensattNavn(),
+            brevbestilling.getPersondokument().hentFolkeregisterident()
         );
     }
 }
