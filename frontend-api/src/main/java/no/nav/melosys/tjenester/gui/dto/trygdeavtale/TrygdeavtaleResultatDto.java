340d4794959c21c575763dac7a978466a9ca3339
Rune Lind <rulind@gmail.com>
2021-11-11 16:12:44 +0100

    Fjern vedtak og innvilgelse fra TrygdeavtaleResultatDto

--- frontend-api/src/main/java/no/nav/melosys/tjenester/gui/dto/trygdeavtale/TrygdeavtaleResultatDto.java
+++ frontend-api/src/main/java/no/nav/melosys/tjenester/gui/dto/trygdeavtale/TrygdeavtaleResultatDto.java
@@ -7,8 +7,6 @@ import java.util.List;
 
 public record TrygdeavtaleResultatDto(
     List<String> virksomheter,
-    String vedtak,
-    String innvilgelse,
     String bestemmelse,
     List<MedfolgendeFamilieDto> barn,
     MedfolgendeFamilieDto ektefelle
@@ -16,24 +14,12 @@ public record TrygdeavtaleResultatDto(
     // Mulig å bytte ut med https://github.com/Randgalt/record-builder
     public static class Builder {
         private List<String> virksomheter;
-        private String vedtak;
-        private String innvilgelse;
         private String bestemmelse;
         private final List<MedfolgendeFamilieDto> barn = new ArrayList<>();
         private MedfolgendeFamilieDto ektefelle;
 
         public Builder virksomheter(List<String> virksomheter) {
             this.virksomheter = virksomheter;
-            return this;
-        }
-
-        public Builder innvilgelse(String innvilgelse) {
-            this.innvilgelse = innvilgelse;
-            return this;
-        }
-
-        public Builder vedtak(String vedtak) {
-            this.vedtak = vedtak;
             return this;
         }
 
@@ -55,8 +41,6 @@ public record TrygdeavtaleResultatDto(
         public TrygdeavtaleResultatDto build() {
             return new TrygdeavtaleResultatDto(
                 virksomheter,
-                vedtak,
-                innvilgelse,
                 bestemmelse,
                 barn,
                 ektefelle
