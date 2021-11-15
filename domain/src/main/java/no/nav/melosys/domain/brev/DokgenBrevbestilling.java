1ae5160390d7493a3cbebe77f90c5f4641c7bd6a
Rune Lind <rulind@gmail.com>
2021-11-15 12:49:22 +0100

    PR fix - Fjern dupplisering av InnvilgelseBrevbestilling

--- domain/src/main/java/no/nav/melosys/domain/brev/DokgenBrevbestilling.java
+++ domain/src/main/java/no/nav/melosys/domain/brev/DokgenBrevbestilling.java
@@ -18,7 +18,6 @@ import no.nav.melosys.domain.person.Persondata;
         @JsonSubTypes.Type(value = MangelbrevBrevbestilling.class),
         @JsonSubTypes.Type(value = InnvilgelseBrevbestilling.class),
         @JsonSubTypes.Type(value = AttestStorbritanniaBrevbestilling.class),
-        @JsonSubTypes.Type(value = InnvilgelseBrevbestilling.class),
         @JsonSubTypes.Type(value = FritekstbrevBrevbestilling.class)
     }
 )
