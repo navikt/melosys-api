8b8c0121babdd6f6f360212714120181d49e3cea
Rune Lind <rulind@gmail.com>
2021-11-17 16:52:06 +0100

    Fjern motaker som legges til base klasse i DokgenMalMapper

--- integrasjon/src/main/java/no/nav/melosys/integrasjon/dokgen/dto/innvilgelsestorbritannia/InnvilgelseUK.java
+++ integrasjon/src/main/java/no/nav/melosys/integrasjon/dokgen/dto/innvilgelsestorbritannia/InnvilgelseUK.java
@@ -4,21 +4,14 @@ import no.nav.melosys.domain.brev.InnvilgelseBrevbestilling;
 import no.nav.melosys.domain.kodeverk.lovvalgsbestemmelser.Lovvalgbestemmelser_trygdeavtale_uk;
 import no.nav.melosys.integrasjon.dokgen.dto.DokgenDto;
 import no.nav.melosys.integrasjon.dokgen.dto.felles.Innvilgelse;
-import no.nav.melosys.integrasjon.dokgen.dto.felles.Mottaker;
 
 public class InnvilgelseUK extends DokgenDto {
 
-    private final Mottaker mottaker;
     private final Innvilgelse innvilgelse;
     private final Lovvalgbestemmelser_trygdeavtale_uk artikkel;
     private final Soknad soknad;
     private final Familie familie;
     private final boolean virksomhetArbeidsgiverSkalHaKopi;
-
-    @Override
-    public Mottaker getMottaker() {
-        return mottaker;
-    }
 
     public Innvilgelse getInnvilgelse() {
         return innvilgelse;
@@ -42,17 +35,14 @@ public class InnvilgelseUK extends DokgenDto {
 
     public InnvilgelseUK(Builder builder) {
         super(builder.brevbestilling);
-        this.mottaker = builder.mottaker;
         this.innvilgelse = Innvilgelse.av(builder.brevbestilling);
         this.artikkel = builder.artikkel;
         this.soknad = builder.soknad;
         this.familie = builder.familie;
         this.virksomhetArbeidsgiverSkalHaKopi = builder.virksomhetArbeidsgiverSkalHaKopi;
-
     }
 
-    static public class Builder {
-        private Mottaker mottaker;
+    public static class Builder {
         private Innvilgelse innvilgelse;
         private Lovvalgbestemmelser_trygdeavtale_uk artikkel;
         private Soknad soknad;
@@ -62,11 +52,6 @@ public class InnvilgelseUK extends DokgenDto {
 
         public Builder(InnvilgelseBrevbestilling brevbestilling) {
             this.brevbestilling = brevbestilling;
-        }
-
-        public Builder mottaker(Mottaker mottaker) {
-            this.mottaker = mottaker;
-            return this;
         }
 
         public Builder innvilgelse(Innvilgelse innvilgelse) {
