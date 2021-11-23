e5472c6ea0d54ad27b3f6253ad1b95d7590d6e18
janitamarielle <janita.marielle.johansen@nav.no>
2021-11-23 10:34:25 +0100

    PR-kommentar, test med disabled mer klart enn kommentert ut

--- service/src/test/java/no/nav/melosys/service/vedtak/TrygdeavtaleVedtakServiceTest.java
+++ service/src/test/java/no/nav/melosys/service/vedtak/TrygdeavtaleVedtakServiceTest.java
@@ -5,7 +5,6 @@ import java.util.List;
 import no.nav.melosys.domain.Behandling;
 import no.nav.melosys.domain.Behandlingsresultat;
 import no.nav.melosys.domain.Fagsak;
-import no.nav.melosys.domain.kodeverk.Aktoersroller;
 import no.nav.melosys.domain.kodeverk.Landkoder;
 import no.nav.melosys.domain.kodeverk.Vedtakstyper;
 import no.nav.melosys.service.behandling.BehandlingService;
@@ -19,6 +18,7 @@ import no.nav.melosys.sikkerhet.context.SpringSubjectHandler;
 import no.nav.melosys.sikkerhet.context.SubjectHandler;
 import no.nav.melosys.sikkerhet.context.TestSubjectHandler;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
@@ -26,9 +26,11 @@ import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static no.nav.melosys.domain.kodeverk.Aktoersroller.*;
 import static no.nav.melosys.domain.kodeverk.Saksstatuser.MEDLEMSKAP_AVKLART;
 import static no.nav.melosys.domain.kodeverk.behandlinger.Behandlingsresultattyper.MEDLEM_I_FOLKETRYGDEN;
 import static no.nav.melosys.domain.kodeverk.behandlinger.Behandlingsstatus.IVERKSETTER_VEDTAK;
+import static no.nav.melosys.domain.kodeverk.brev.Produserbaredokumenter.ATTEST_NO_UK_1;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.verify;
@@ -72,7 +74,7 @@ class TrygdeavtaleVedtakServiceTest {
     }
 
     @Test
-    void fattVedtak_førstegangsvedtak_fatterVedtak() {
+    void fattVedtak_førstegangsvedtakUtenBrev_fatterVedtak() {
         Behandlingsresultat behandlingsresultat = new Behandlingsresultat();
         when(behandlingsresultatService.hentBehandlingsresultat(anyLong())).thenReturn(behandlingsresultat);
 
@@ -83,7 +85,31 @@ class TrygdeavtaleVedtakServiceTest {
         verify(behandlingService).lagre(behandlingCaptor.capture());
         verify(prosessinstansService).opprettProsessinstansIverksettVedtakTrygdeavtale(any(Behandling.class), eq(request));
         verify(oppgaveService).ferdigstillOppgaveMedSaksnummer(SAKSNUMMER);
-//        verify(dokgenService).produserOgDistribuerBrev(anyLong(), brevbestillingRequestCaptor.capture());
+
+        Behandlingsresultat lagretBehandlingsresultat = behandlingsresultatCaptor.getValue();
+        assertThat(lagretBehandlingsresultat)
+            .extracting("type", "begrunnelseFritekst", "fastsattAvLand")
+            .containsExactly(MEDLEM_I_FOLKETRYGDEN, "Begrunnelse", Landkoder.NO);
+
+        Behandling lagretBehandling = behandlingCaptor.getValue();
+        assertThat(lagretBehandling.getStatus()).isEqualTo(IVERKSETTER_VEDTAK);
+        assertThat(lagretBehandling.getFagsak().getStatus()).isEqualTo(MEDLEMSKAP_AVKLART);
+    }
+
+    @Test
+    @Disabled("Denne er disabled frem til dokgen attest-bestilling er ok")
+    void fattVedtak_kallerDokgen_fatterVedtak() {
+        Behandlingsresultat behandlingsresultat = new Behandlingsresultat();
+        when(behandlingsresultatService.hentBehandlingsresultat(anyLong())).thenReturn(behandlingsresultat);
+
+        FattTrygdeavtaleVedtakRequest request = lagFattVedtakRequest();
+        trygdeavtaleVedtakService.fattVedtak(lagBehandling(), request);
+
+        verify(behandlingsresultatService).lagre(behandlingsresultatCaptor.capture());
+        verify(behandlingService).lagre(behandlingCaptor.capture());
+        verify(prosessinstansService).opprettProsessinstansIverksettVedtakTrygdeavtale(any(Behandling.class), eq(request));
+        verify(oppgaveService).ferdigstillOppgaveMedSaksnummer(SAKSNUMMER);
+        verify(dokgenService).produserOgDistribuerBrev(anyLong(), brevbestillingRequestCaptor.capture());
 
         Behandlingsresultat lagretBehandlingsresultat = behandlingsresultatCaptor.getValue();
         assertThat(lagretBehandlingsresultat)
@@ -94,14 +120,14 @@ class TrygdeavtaleVedtakServiceTest {
         assertThat(lagretBehandling.getStatus()).isEqualTo(IVERKSETTER_VEDTAK);
         assertThat(lagretBehandling.getFagsak().getStatus()).isEqualTo(MEDLEMSKAP_AVKLART);
 
-//        BrevbestillingRequest brevbestillingRequest = brevbestillingRequestCaptor.getValue();
-//        assertThat(brevbestillingRequest)
-//            .extracting("produserbardokument", "bestillersId", "mottaker", "innledningFritekst",
-//                "begrunnelseFritekst", "ektefelleFritekst", "barnFritekst")
-//            .containsExactly(ATTEST_NO_UK_1, "Z990007", BRUKER, "Innledning",
-//                "Begrunnelse", "Ektefelle omfattet", "Barn omfattet");
-//        assertThat(brevbestillingRequest.getKopiMottakere().size()).isEqualTo(1);
-//        assertThat(brevbestillingRequest.getKopiMottakere().get(0).getRolle()).isEqualTo(ARBEIDSGIVER);
+        BrevbestillingRequest brevbestillingRequest = brevbestillingRequestCaptor.getValue();
+        assertThat(brevbestillingRequest)
+            .extracting("produserbardokument", "bestillersId", "mottaker", "innledningFritekst",
+                "begrunnelseFritekst", "ektefelleFritekst", "barnFritekst")
+            .containsExactly(ATTEST_NO_UK_1, "Z990007", BRUKER, "Innledning",
+                "Begrunnelse", "Ektefelle omfattet", "Barn omfattet");
+        assertThat(brevbestillingRequest.getKopiMottakere().size()).isEqualTo(1);
+        assertThat(brevbestillingRequest.getKopiMottakere().get(0).getRolle()).isEqualTo(ARBEIDSGIVER);
     }
 
     private FattTrygdeavtaleVedtakRequest lagFattVedtakRequest() {
@@ -112,7 +138,7 @@ class TrygdeavtaleVedtakServiceTest {
             .medFritekstBegrunnelse("Begrunnelse")
             .medFritekstEktefelle("Ektefelle omfattet")
             .medFritekstBarn("Barn omfattet")
-            .medKopiMottakere(List.of(new KopiMottaker(Aktoersroller.ARBEIDSGIVER, "987654321", null)))
+            .medKopiMottakere(List.of(new KopiMottaker(ARBEIDSGIVER, "987654321", null)))
             .medBestillersId(SubjectHandler.getInstance().getUserID())
             .build();
     }
