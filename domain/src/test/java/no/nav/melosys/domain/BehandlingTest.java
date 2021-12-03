2c344e19b120ceee26f8fc0d6c9a1b1c0331b481
Eirik Bergande <eirik.bergande@nav.no>
2021-12-03 12:22:00 +0100

    PDL skal ikke lagres hvis det eksisterer tps eller pdl fra før (#889)
    PDL skal ikke lagres hvis det eksisterer tps eller pdl fra før

--- domain/src/test/java/no/nav/melosys/domain/BehandlingTest.java
+++ domain/src/test/java/no/nav/melosys/domain/BehandlingTest.java
@@ -3,6 +3,7 @@ package no.nav.melosys.domain;
 import no.nav.melosys.domain.kodeverk.behandlinger.Behandlingsstatus;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -24,24 +25,26 @@ class BehandlingTest {
     }
 
     @Test
-    void saksopplysningEksisterer_eksisterer_true() {
-        Saksopplysning saksopplysning = new Saksopplysning();
-        saksopplysning.setType(SaksopplysningType.PDL_PERSOPL);
+    void saksopplysningerEksistererIkke_eksisterer_false() {
+        Saksopplysning saksopplysning1 = new Saksopplysning();
+        saksopplysning1.setType(SaksopplysningType.PERSHIST);
+        Saksopplysning saksopplysning2 = new Saksopplysning();
+        saksopplysning1.setType(SaksopplysningType.PERSOPL);
 
         Behandling behandling = new Behandling();
-        behandling.setSaksopplysninger(Set.of(saksopplysning));
+        behandling.setSaksopplysninger(Set.of(saksopplysning1, saksopplysning2));
 
-        assertThat(behandling.saksopplysningEksisterer(SaksopplysningType.PDL_PERSOPL)).isTrue();
+        assertThat(behandling.saksopplysningerEksistererIkke(List.of(SaksopplysningType.PDL_PERSOPL, SaksopplysningType.PERSOPL))).isFalse();
     }
 
     @Test
-    void saksopplysningEksisterer_eksistererIkke_false() {
+    void saksopplysningerEksistererIkke_eksistererIkke_true() {
         Saksopplysning saksopplysning = new Saksopplysning();
         saksopplysning.setType(SaksopplysningType.PDL_PERSOPL);
 
         Behandling behandling = new Behandling();
         behandling.setSaksopplysninger(Set.of(saksopplysning));
 
-        assertThat(behandling.saksopplysningEksisterer(SaksopplysningType.PDL_PERS_SAKS)).isFalse();
+        assertThat(behandling.saksopplysningerEksistererIkke(List.of(SaksopplysningType.PDL_PERS_SAKS, SaksopplysningType.PERSHIST))).isTrue();
     }
 }
