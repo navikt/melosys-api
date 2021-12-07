53ff0c4202dde6f9cddc1b81ebc6a6a4a676d961
Marton Skjæveland <56435736+martonSkjevelandNav@users.noreply.github.com>
2021-12-07 15:58:53 +0100

    Revert "Revert "Utvidet personstatus fra PDL med historikk (#870)" (#891)" (#893)
    Legger tilbake endringen
    This reverts commit 1fa6c22a2470842d74acc14e10154c97274c100a.

--- integrasjon/src/main/java/no/nav/melosys/integrasjon/pdl/dto/person/Folkeregisterpersonstatus.java
+++ integrasjon/src/main/java/no/nav/melosys/integrasjon/pdl/dto/person/Folkeregisterpersonstatus.java
@@ -1,7 +1,13 @@
 package no.nav.melosys.integrasjon.pdl.dto.person;
 
+import no.nav.melosys.integrasjon.pdl.dto.Folkeregistermetadata;
+import no.nav.melosys.integrasjon.pdl.dto.HarFolkeregistermetadata;
 import no.nav.melosys.integrasjon.pdl.dto.HarMetadata;
 import no.nav.melosys.integrasjon.pdl.dto.Metadata;
 
-public record Folkeregisterpersonstatus(String status, Metadata metadata) implements HarMetadata {
+public record Folkeregisterpersonstatus(
+    String status,
+    Metadata metadata,
+    Folkeregistermetadata folkeregistermetadata
+) implements HarMetadata, HarFolkeregistermetadata {
 }
