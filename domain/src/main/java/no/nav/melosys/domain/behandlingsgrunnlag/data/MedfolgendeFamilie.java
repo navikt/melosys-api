b730504121d22f84be293175cf6a1c97b63d7228
Rune Lind <rulind@gmail.com>
2021-12-08 12:00:11 +0100

    Parse dato fra støttetede formater
    https://jira.adeo.no/browse/MELOSYS-4730

--- domain/src/main/java/no/nav/melosys/domain/behandlingsgrunnlag/data/MedfolgendeFamilie.java
+++ domain/src/main/java/no/nav/melosys/domain/behandlingsgrunnlag/data/MedfolgendeFamilie.java
@@ -1,10 +1,15 @@
 package no.nav.melosys.domain.behandlingsgrunnlag.data;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Objects;
 import java.util.UUID;
+import java.util.stream.Stream;
 
 import no.nav.commons.foedselsnummer.FoedselsNr;
 import no.nav.melosys.exception.FunksjonellException;
+import no.nav.melosys.exception.TekniskException;
 
 public class MedfolgendeFamilie {
     private String uuid;
@@ -64,7 +69,7 @@ public class MedfolgendeFamilie {
     }
 
     public IdentType utledIdentType() {
-        if (fnr.length() < 11 && fnr.contains(".")) {
+        if (fnr.length() < 11) {
             return IdentType.DATO;
         }
 
@@ -77,6 +82,38 @@ public class MedfolgendeFamilie {
     }
 
     public LocalDate datoFraFnr() {
-        return LocalDate.parse(fnr); // TODO: støtt lovlige formater https://jira.adeo.no/browse/MELOSYS-4730#
+        if (utledIdentType() != IdentType.DATO) {
+            throw new TekniskException("Kan bare parse dato når IdentType er DATO");
+        }
+        LocalDate happyCase = finnFørsteMatch(Stream.of(
+            "ddMMyyyy", "dd.MM.yyyy", "dd/MM/yyyy", "dd-MM-yyyy"
+        ));
+        if (happyCase != null) return happyCase;
+
+        LocalDate dateMedUklartÅrhundre = finnFørsteMatch(Stream.of(
+            "ddMMyy", "dd.MM.yy", "dd/MM/yy", "dd-MM-yy"
+        ));
+        if (dateMedUklartÅrhundre == null) return null;
+
+        if (dateMedUklartÅrhundre.isAfter(LocalDate.now())) {
+            // om en person har en dato frem i tiden velger vi å plassere han på 1900 tallet
+            return dateMedUklartÅrhundre.minusYears(100);
+        }
+        return dateMedUklartÅrhundre;
+    }
+
+    private LocalDate finnFørsteMatch(Stream<String> stream) {
+        return stream.map(this::tryParseFnr)
+            .filter(Objects::nonNull)
+            .findFirst()
+            .orElse(null);
+    }
+
+    private LocalDate tryParseFnr(String pattern) {
+        try {
+            return LocalDate.parse(fnr, DateTimeFormatter.ofPattern(pattern));
+        } catch (DateTimeParseException e) {
+            return null;
+        }
     }
 }
---
+++ domain/src/test/java/no/nav/melosys/domain/behandlingsgrunnlag/data/MedfolgendeFamilieTest.java
@@ -0,0 +1,68 @@
+package no.nav.melosys.domain.behandlingsgrunnlag.data;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.stream.Stream;
+
+import no.nav.melosys.exception.TekniskException;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
+
+class MedfolgendeFamilieTest {
+
+    public static final String NAVN = "Doffen Duck";
+    public static final String GYLDIG_FNR = "01108049800";
+
+    @Test
+    void datoFraFnr_gyldigFnr_kastException() {
+        MedfolgendeFamilie medfolgendeFamilie = MedfolgendeFamilie.tilBarnFraFnrOgNavn(GYLDIG_FNR, NAVN);
+        assertThatExceptionOfType(TekniskException.class)
+            .isThrownBy(medfolgendeFamilie::datoFraFnr)
+            .withMessageContaining("Kan bare parse dato når IdentType er DATO");
+    }
+
+    @ParameterizedTest()
+    @MethodSource("gyldigeDatoer")
+    void datoFraFnr_parseDatoMedGyldigFormat_returnerDato(String dato, LocalDate expected) {
+        MedfolgendeFamilie medfolgendeFamilie = MedfolgendeFamilie.tilBarnFraFnrOgNavn(dato, NAVN);
+        assertThat(medfolgendeFamilie.datoFraFnr()).isEqualTo(expected);
+    }
+
+    @ParameterizedTest()
+    @MethodSource("århundreOvergangDatoer")
+    void datoFraFnr_ÅrhundreOvergangDatoer_returnerDato(String dato, LocalDate expected) {
+        MedfolgendeFamilie medfolgendeFamilie = MedfolgendeFamilie.tilBarnFraFnrOgNavn(dato, NAVN);
+        assertThat(medfolgendeFamilie.datoFraFnr()).isEqualTo(expected);
+        System.out.println(medfolgendeFamilie.datoFraFnr());
+    }
+
+    private static List<Arguments> gyldigeDatoer() {
+        return Stream.of(
+            "01021980",
+            "010280",
+            "01.02.80",
+            "01.02.1980",
+            "01/02/80",
+            "01/02/1980",
+            "01-02-80",
+            "01-02-1980"
+        ).map(s -> Arguments.of(s, LocalDate.of(1980, 2, 1))).toList();
+    }
+
+    private static List<Arguments> århundreOvergangDatoer() {
+        LocalDate now = LocalDate.now();
+        LocalDate bruk2000 = now;
+        LocalDate bruk1900 = now.plusDays(1);
+        return List.of(
+            Arguments.of((bruk2000.format(DateTimeFormatter.ofPattern("dd.MM.YY"))), bruk2000),
+            Arguments.of((bruk1900.format(DateTimeFormatter.ofPattern("dd.MM.YY"))), bruk1900.minusYears(100)),
+            Arguments.of(("01.02.21"), LocalDate.of(2021, 2, 1))
+        );
+    }
+}
