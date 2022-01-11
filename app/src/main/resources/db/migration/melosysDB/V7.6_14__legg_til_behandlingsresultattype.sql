310fc81a0e1b696ea12355d912f06b2eccbff142
Isa Salem Hame <isa.salem.hame@nav.no>
2022-01-11 05:51:00 +0100

    Legger til nye behandlingsresultattyper i databasen
    MELOSYS_4815

---
+++ app/src/main/resources/db/migration/melosysDB/V7.6_14__legg_til_behandlingsresultattype.sql
@@ -0,0 +1,2 @@
+INSERT INTO behandlingsresultat_type (kode, navn) VALUES ('AVSLUTTET_UTEN_ENDRING', 'Ferdigbehandlet');
+INSERT INTO behandlingsresultat_type (kode, navn) VALUES ('HENLEGGELSE_BORTFALT', 'Kan ikke behandles i Melosys');
