53c80fa4d6a669640e048e24e4b6cabf34e640db
Marton Skjæveland <56435736+martonSkjevelandNav@users.noreply.github.com>
2022-01-18 15:47:04 +0100

    Henter myndighet.navn fra utenlandske myndigheter tabell (#956)
    * Henter myndighet.navn fra utenlandske myndigheter tabell
    
    * Flyttet logikk for henting med institusjonskode til service

--- saksflyt/src/main/java/no/nav/melosys/saksflyt/steg/brev/OpprettJournalforBrev.java
+++ saksflyt/src/main/java/no/nav/melosys/saksflyt/steg/brev/OpprettJournalforBrev.java
@@ -15,6 +15,7 @@ import no.nav.melosys.exception.FunksjonellException;
 import no.nav.melosys.integrasjon.ereg.EregFasade;
 import no.nav.melosys.integrasjon.joark.JoarkFasade;
 import no.nav.melosys.saksflyt.steg.StegBehandler;
+import no.nav.melosys.service.aktoer.UtenlandskMyndighetService;
 import no.nav.melosys.service.behandling.BehandlingService;
 import no.nav.melosys.service.dokument.DokgenService;
 import no.nav.melosys.service.dokument.DokumentproduksjonsInfo;
@@ -36,19 +37,21 @@ public class OpprettJournalforBrev implements StegBehandler {
 
     private final BehandlingService behandlingService;
     private final DokgenService dokgenService;
+    private final UtenlandskMyndighetService utenlandskMyndighetService;
     private final JoarkFasade joarkFasade;
     private final PersondataFasade persondataFasade;
     private final EregFasade eregFasade;
     private final Unleash unleash;
 
     @Autowired
-    public OpprettJournalforBrev(BehandlingService behandlingService, DokgenService dokgenService,
+    public OpprettJournalforBrev(BehandlingService behandlingService, DokgenService dokgenService, UtenlandskMyndighetService utenlandskMyndighetService,
                                  @Qualifier("system") JoarkFasade joarkFasade,
                                  @Qualifier("system") PersondataFasade persondataFasade,
                                  @Qualifier("system") EregFasade eregFasade,
                                  Unleash unleash) {
         this.behandlingService = behandlingService;
         this.dokgenService = dokgenService;
+        this.utenlandskMyndighetService = utenlandskMyndighetService;
         this.joarkFasade = joarkFasade;
         this.persondataFasade = persondataFasade;
         this.eregFasade = eregFasade;
@@ -87,8 +90,8 @@ public class OpprettJournalforBrev implements StegBehandler {
 
         if (!isEmpty(institusjonsid)) {
             mottaker.setInstitusjonId(institusjonsid);
-        }
-        else if (!isEmpty(orgnr)) {
+            sammensattNavn = utenlandskMyndighetService.hentUtenlandskMyndighetForInstitusjonID(institusjonsid).navn;
+        } else if (!isEmpty(orgnr)) {
             mottaker.setOrgnr(orgnr);
         } else {
             mottaker.setAktørId(aktørId);
