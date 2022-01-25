e809cbfe081ab94ca436f262980e94b9d01bf86a
martonSkjevelandNav <marton.skjeveland@nav.no>
2022-01-25 14:55:20 +0100

    Legger til den oppdaterte rollen i ROLLE_TYPE

--- app/src/main/resources/db/migration/melosysDB/V7.6_16__oppdater_aktoerrolle_trygdemyndighet_.sql
+++ app/src/main/resources/db/migration/melosysDB/V7.6_16__oppdater_aktoerrolle_trygdemyndighet_.sql
@@ -1,3 +1,6 @@
+INSERT INTO ROLLE_TYPE (kode, navn)
+VALUES ('TRYGDEMYNDIGHET', 'Trygdemyndigheten det sendes til og/eller mottas dokumentasjon fra i saken.');
+
 UPDATE AKTOER
 SET ROLLE = 'TRYGDEMYNDIGHET'
 WHERE ROLLE = 'MYNDIGHET';
