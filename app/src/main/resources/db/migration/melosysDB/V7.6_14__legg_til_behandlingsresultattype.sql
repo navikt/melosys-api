97e5a223f72cae3c96e9ed1b1e2f7e6458f5fb55
Isa Salem Hame <isa.salem.hame@nav.no>
2022-01-28 02:40:40 +0100

    Gjør endringer i tekst for behandlingsresultattype
    MELOSYS-4478

--- app/src/main/resources/db/migration/melosysDB/V7.6_14__legg_til_behandlingsresultattype.sql
+++ app/src/main/resources/db/migration/melosysDB/V7.6_14__legg_til_behandlingsresultattype.sql
@@ -1,2 +1,2 @@
-INSERT INTO behandlingsresultat_type (kode, navn) VALUES ('AVSLUTTET_UTEN_ENDRING', 'Ferdigbehandlet');
+INSERT INTO behandlingsresultat_type (kode, navn) VALUES ('AVSLUTTET_UTEN_ENDRING', 'Ferdigbehandlet uten nytt vedtak');
 INSERT INTO behandlingsresultat_type (kode, navn) VALUES ('HENLEGGELSE_BORTFALT', 'Kan ikke behandles i Melosys');
