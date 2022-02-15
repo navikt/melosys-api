c8f4c1e712edd4cd0fad562c6e3c0f2d6e70abae
martonSkjevelandNav <marton.skjeveland@nav.no>
2022-02-15 12:43:51 +0100

    Konsistent konstantnavn i test

--- domain/src/test/java/no/nav/melosys/domain/person/NavnTest.java
+++ domain/src/test/java/no/nav/melosys/domain/person/NavnTest.java
@@ -6,16 +6,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class NavnTest {
 
-    private static final String NAVN_STANDARD = "Per Olav Mellomnavn Etternavn";
+    private static final String NAVN_ETTERNAVN_SIST = "Per Olav Mellomnavn Etternavn";
     private static final String NAVN_ETTERNAVN_FØRST = "Etternavn, Per Olav Mellomnavn";
 
     @Test
     public void navnEtternavnFørst() {
-        assertThat(Navn.navnEtternavnFørst(NAVN_STANDARD)).isEqualTo(NAVN_ETTERNAVN_FØRST);
+        assertThat(Navn.navnEtternavnFørst(NAVN_ETTERNAVN_SIST)).isEqualTo(NAVN_ETTERNAVN_FØRST);
     }
 
     @Test
     void navnEtternavnSist() {
-        assertThat(Navn.navnEtternavnSist(NAVN_ETTERNAVN_FØRST)).isEqualTo(NAVN_STANDARD);
+        assertThat(Navn.navnEtternavnSist(NAVN_ETTERNAVN_FØRST)).isEqualTo(NAVN_ETTERNAVN_SIST);
     }
 }
