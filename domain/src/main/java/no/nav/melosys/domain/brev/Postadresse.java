8a685a495b55eac9cc6b815394cb756fa532b9ec
Isa Salem Hame <isa.salem.hame@nav.no>
2022-02-17 12:21:32 +0100

    Endrer rekkefølge på variable i klassen Postadresse
    MELOSYS-4815

--- domain/src/main/java/no/nav/melosys/domain/brev/Postadresse.java
+++ domain/src/main/java/no/nav/melosys/domain/brev/Postadresse.java
@@ -10,14 +10,14 @@ import static java.util.Arrays.asList;
 import static no.nav.melosys.domain.adresse.Adresse.sammenslå;
 
 public record Postadresse(
+    String coAdressenavn,
     String adresselinje1,
     String adresselinje2,
     String adresselinje3,
     String adresselinje4,
     String postnr,
     String poststed,
-    String landkode,
-    String coAdressenavn
+    String landkode
 ) {
     public List<String> adresselinjer() {
         List<String> adresselinjer = new ArrayList<>();
@@ -35,24 +35,26 @@ public record Postadresse(
 
     public static Postadresse lagPostadresse(StrukturertAdresse strukturertAdresse, String coAdressenavn) {
         return new Postadresse(
+            coAdressenavn,
             sammenslå(strukturertAdresse.getGatenavn(), strukturertAdresse.getHusnummerEtasjeLeilighet()),
             strukturertAdresse.getPostboks(),
             strukturertAdresse.getRegion(),
             null,
             strukturertAdresse.getPostnummer(),
             strukturertAdresse.getPoststed(),
-            strukturertAdresse.getLandkode(),
-            coAdressenavn);
+            strukturertAdresse.getLandkode());
     }
 
     public static Postadresse lagPostadresse(SemistrukturertAdresse semistrukturertAdresse, String coAdressenavn) {
-        return new Postadresse(semistrukturertAdresse.adresselinje1(),
+        return new Postadresse(
+            coAdressenavn,
+            semistrukturertAdresse.adresselinje1(),
             semistrukturertAdresse.adresselinje2(),
             semistrukturertAdresse.adresselinje3(),
             null,
             semistrukturertAdresse.postnr(),
             semistrukturertAdresse.poststed(),
-            semistrukturertAdresse.landkode(),
-            coAdressenavn);
+            semistrukturertAdresse.landkode()
+            );
     }
 }
