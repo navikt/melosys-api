810cb0186450f101c0a234bc000b5f5bb4551604
Rune Lind <rulind@gmail.com>
2022-02-21 13:56:09 +0100

    Nojira service med db komptest (#1012)
    Komponent test på BehandlingsresultatService uten å laste full application context
    Om man vil kjøre uten Testcontainers kan man kommentere ut arv til OracleTestContainerBase fra DataJpaTestBase

---
+++ integrasjonstest/src/test/kotlin/no/nav/melosys/itest/OracleTestContainerBase.kt
@@ -0,0 +1,23 @@
+package no.nav.melosys.itest
+
+import org.springframework.test.context.DynamicPropertyRegistry
+import org.springframework.test.context.DynamicPropertySource
+import org.testcontainers.containers.OracleContainer
+import org.testcontainers.junit.jupiter.Container
+import org.testcontainers.junit.jupiter.Testcontainers
+
+@Testcontainers
+open class OracleTestContainerBase {
+    companion object {
+        @Container
+        var DB = OracleContainer()
+
+        @DynamicPropertySource
+        @JvmStatic
+        fun oracleProperties(registry: DynamicPropertyRegistry) {
+            registry.add("spring.datasource.url") { DB.jdbcUrl }
+            registry.add("spring.datasource.password") { DB.password }
+            registry.add("spring.datasource.username") { DB.username }
+        }
+    }
+}
