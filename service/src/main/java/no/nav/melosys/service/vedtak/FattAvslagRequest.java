47b9de978f171078f935c55be4faa643a9ae627e
RuneLind <rulind@gmail.com>
2022-02-23 15:24:46 +0100

    Arv FattAvslagRequest fra FattVedtakRequest

--- service/src/main/java/no/nav/melosys/service/vedtak/FattAvslagRequest.java
+++ service/src/main/java/no/nav/melosys/service/vedtak/FattAvslagRequest.java
@@ -1,36 +1,20 @@
 package no.nav.melosys.service.vedtak;
 
 
-import no.nav.melosys.domain.kodeverk.Vedtakstyper;
-import no.nav.melosys.domain.kodeverk.behandlinger.Behandlingsresultattyper;
-
-public class FattAvslagRequest {
+public class FattAvslagRequest extends FattVedtakRequest {
     private final String fritekst;
-    private final Behandlingsresultattyper behandlingsresultattype;
-    private final Vedtakstyper vedtakstype;
 
     private FattAvslagRequest(Builder builder) {
+        super(builder);
         this.fritekst = builder.fritekst;
-        this.behandlingsresultattype = builder.behandlingsresultattype;
-        this.vedtakstype = builder.vedtakstype;
     }
 
     public String getFritekst() {
         return fritekst;
     }
 
-    public Behandlingsresultattyper getBehandlingsresultattype() {
-        return behandlingsresultattype;
-    }
-
-    public Vedtakstyper getVedtakstype() {
-        return vedtakstype;
-    }
-
-    public static class Builder {
+    public static class Builder extends FattVedtakRequest.Builder<Builder> {
         private String fritekst;
-        private Behandlingsresultattyper behandlingsresultattype;
-        private Vedtakstyper vedtakstype;
 
         public Builder getThis() {
             return this;
@@ -43,16 +27,6 @@ public class FattAvslagRequest {
 
         public FattAvslagRequest build() {
             return new FattAvslagRequest(this);
-        }
-
-        public Builder medBehandlingsresultattype(Behandlingsresultattyper behandlingsresultattype) {
-            this.behandlingsresultattype = behandlingsresultattype;
-            return this;
-        }
-
-        public Builder medVedtakstype(Vedtakstyper vedtakstype) {
-            this.vedtakstype = vedtakstype;
-            return this;
         }
     }
 }
