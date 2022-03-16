8530cb2f3e695f18d060e925536544cd9f6770b3
Ivan Skodje <ivan@tybo.no>
2022-03-16 09:07:46 +0100

    Automatisk import rekkefølge-fiks via editorconfig

--- frontend-api/src/main/java/no/nav/melosys/tjenester/gui/BehandlingTjeneste.java
+++ frontend-api/src/main/java/no/nav/melosys/tjenester/gui/BehandlingTjeneste.java
@@ -1,5 +1,7 @@
 package no.nav.melosys.tjenester.gui;
 
+import java.util.Collection;
+
 import com.fasterxml.jackson.annotation.JsonView;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -24,8 +26,6 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.WebApplicationContext;
-
-import java.util.Collection;
 
 @Protected
 @RestController
