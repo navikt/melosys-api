6163891a30d4c3a13913b9b1fe535e8dd42eac84
RuneLind <rulind@gmail.com>
2022-03-26 12:24:19 +0100

    Tester med WireMockServer for å sjekke headers i AaregServiceIT
    Work in progress

--- integrasjonstest/src/test/kotlin/no/nav/melosys/itest/AaregServiceIT.kt
+++ integrasjonstest/src/test/kotlin/no/nav/melosys/itest/AaregServiceIT.kt
@@ -55,26 +55,9 @@ internal class AaregServiceIT(
 ) {
 
     companion object {
-        var wireMockServer: WireMockServer = WireMockServer(WireMockConfiguration.wireMockConfig().port(8180)).apply { start() }
-
-//        @JvmStatic
-//        @BeforeAll
-//        fun beforeAll() {
-//            println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-//            wireMockServer.start()
-//        }
+        var wireMockServer: WireMockServer =
+            WireMockServer(WireMockConfiguration.wireMockConfig().port(8180)).apply { start() }
     }
-
-//    @BeforeAll
-//    internal fun beforeAll() {
-//        println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-//        wireMockServer.start()
-//    }
-//
-//    @AfterAll
-//    internal  fun afterAll() {
-//        wireMockServer.stop()
-//    }
 
     @BeforeEach
     fun setup() {
@@ -109,17 +92,30 @@ internal class AaregServiceIT(
                 )
             )
 
-        // TODO: bruk MockWebServer siden dette ikke bruker RestTemplateBuilder
-//        server.expect(requestTo("?regelverk=A_ORDNINGEN&ansettelsesperiodeFom=2022-03-24&ansettelsesperiodeTom=2022-03-24"))
-//            .andRespond(withSuccess("{  }", MediaType.APPLICATION_JSON))
+        wireMockServer.stubFor(
+            WireMock.get("/?regelverk=A_ORDNINGEN&ansettelsesperiodeFom=2022-03-26&ansettelsesperiodeTom=2022-03-27")
+                .withHeader("Nav-Personident", WireMock.equalTo("121"))
+                .withHeader("Authorization", WireMock.equalTo("Bearer Bearer --token--"))
+                .withQueryParam("regelverk", WireMock.equalTo("A_ORDNINGEN"))
+                .withQueryParam("ansettelsesperiodeFom", WireMock.equalTo("2022-03-26"))
+                .withQueryParam("ansettelsesperiodeTom", WireMock.equalTo("2022-03-27")).willReturn(
+                    WireMock.aResponse()
+                        .withStatus(200)
+                        .withHeader("Content-Type", "application/json")
+                        .withBody("[]")
+                )
+        )
 
-        aaregService.finnArbeidsforholdPrArbeidstaker("121", LocalDate.now(), LocalDate.now())
+        val start = LocalDate.of(2022, 3, 26)
+        val stop = LocalDate.of(2022, 3, 27)
+        aaregService.finnArbeidsforholdPrArbeidstaker("121", start, stop)
+
+
         ThreadLocalAccessInfo.afterCompletion("request")
     }
 
     @Test
     fun testRequestFromProsess() {
-
         val uuid = UUID.randomUUID()
         ThreadLocalAccessInfo.beforExecuteProcess(uuid, "prossesSteg")
 
@@ -133,22 +129,22 @@ internal class AaregServiceIT(
             )
 
         wireMockServer.stubFor(
-            WireMock.get("/?regelverk=A_ORDNINGEN&ansettelsesperiodeFom=2022-03-26&ansettelsesperiodeTom=2022-03-26")
+            WireMock.get("/?regelverk=A_ORDNINGEN&ansettelsesperiodeFom=2022-03-26&ansettelsesperiodeTom=2022-03-27")
                 .withHeader("Nav-Personident", WireMock.equalTo("121"))
                 .withHeader("Authorization", WireMock.equalTo("Bearer Bearer --token--"))
                 .withQueryParam("regelverk", WireMock.equalTo("A_ORDNINGEN"))
                 .withQueryParam("ansettelsesperiodeFom", WireMock.equalTo("2022-03-26"))
-                .withQueryParam("ansettelsesperiodeTom", WireMock.equalTo("2022-03-26")).willReturn(
+                .withQueryParam("ansettelsesperiodeTom", WireMock.equalTo("2022-03-27")).willReturn(
                     WireMock.aResponse()
                         .withStatus(200)
                         .withHeader("Content-Type", "application/json")
-                        .withBody(responsAaregRestBody)
-                ))
+                        .withBody("[]")
+                )
+        )
 
-//        server.expect(requestTo("dummy?regelverk=A_ORDNINGEN&ansettelsesperiodeFom=2022-03-24&ansettelsesperiodeTom=2022-03-24"))
-//            .andRespond(withSuccess("{  }", MediaType.APPLICATION_JSON))
-
-        aaregService.finnArbeidsforholdPrArbeidstaker("121", LocalDate.now(), LocalDate.now())
+        val start = LocalDate.of(2022, 3, 26)
+        val stop = LocalDate.of(2022, 3, 27)
+        aaregService.finnArbeidsforholdPrArbeidstaker("121", start, stop)
         ThreadLocalAccessInfo.afterExecuteProcess(uuid)
     }
 
@@ -159,15 +155,6 @@ internal class AaregServiceIT(
         fun kodeOppslag(): KodeOppslag {
             return KodeOppslagImpl();
         }
-
-//        @Bean
-//        fun aConfig(): ArbeidsforholdRestConsumerConfig {
-////            return ArbeidsforholdRestConsumerConfig("http://localhost:${wireMockServer.port()}")
-//            println("######################")
-//            println(wireMockServer.port())
-//            println("######################")
-//            return ArbeidsforholdRestConsumerConfig("http://localhost:${wireMockServer.port()}")
-//        }
     }
 
     class KodeOppslagImpl : KodeOppslag {
@@ -198,159 +185,4 @@ internal class AaregServiceIT(
             return "Z990007"
         }
     }
-
-    private val responsAaregRestBody: String = """
-        [
-          {
-            "ansettelsesperiode": {
-              "bruksperiode": {
-                "fom": "2015-01-06T21:44:04.748",
-                "tom": "2015-12-06T19:45:04"
-              },
-              "periode": {
-                "fom": "2014-07-01"
-              },
-              "sluttaarsak": "arbeidstakerHarSagtOppSelv",
-              "sporingsinformasjon": {
-                "endretAv": "Z990693",
-                "endretKilde": "AAREG",
-                "endretKildereferanse": "referanse-fra-kilde",
-                "endretTidspunkt": "2018-09-19T12:11:20.79",
-                "opprettetAv": "srvappserver",
-                "opprettetKilde": "EDAG",
-                "opprettetKildereferanse": "22a26849-aeef-4b81-9174-e238c11e1081",
-                "opprettetTidspunkt": "2018-09-19T12:10:58.059"
-              },
-              "varslingskode": "ERKONK"
-            },
-            "antallTimerForTimeloennet": [
-              {
-                "antallTimer": 37.5,
-                "periode": {
-                  "fom": "2014-07-01",
-                  "tom": "2015-12-31"
-                },
-                "rapporteringsperiode": "2018-05",
-                "sporingsinformasjon": {
-                  "endretAv": "Z990693",
-                  "endretKilde": "AAREG",
-                  "endretKildereferanse": "referanse-fra-kilde",
-                  "endretTidspunkt": "2018-09-19T12:11:20.79",
-                  "opprettetAv": "srvappserver",
-                  "opprettetKilde": "EDAG",
-                  "opprettetKildereferanse": "22a26849-aeef-4b81-9174-e238c11e1081",
-                  "opprettetTidspunkt": "2018-09-19T12:10:58.059"
-                }
-              }
-            ],
-            "arbeidsavtaler": [
-              {
-                "ansettelsesform": "fast",
-                "antallTimerPrUke": 37.5,
-                "arbeidstidsordning": "ikkeSkift",
-                "beregnetAntallTimerPrUke": 26.5,
-                "bruksperiode": {
-                  "fom": "2015-01-06T21:44:04.748",
-                  "tom": "2015-12-06T19:45:04"
-                },
-                "gyldighetsperiode": {
-                  "fom": "2014-07-01",
-                  "tom": "2015-12-31"
-                },
-                "sistLoennsendring": "2014-07-01",
-                "sistStillingsendring": "2014-07-01",
-                "sporingsinformasjon": {
-                  "endretAv": "Z990693",
-                  "endretKilde": "AAREG",
-                  "endretKildereferanse": "referanse-fra-kilde",
-                  "endretTidspunkt": "2018-09-19T12:11:20.79",
-                  "opprettetAv": "srvappserver",
-                  "opprettetKilde": "EDAG",
-                  "opprettetKildereferanse": "22a26849-aeef-4b81-9174-e238c11e1081",
-                  "opprettetTidspunkt": "2018-09-19T12:10:58.059"
-                },
-                "stillingsprosent": 49.5,
-                "type": "Forenklet",
-                "yrke": "2130123"
-              }
-            ],
-            "arbeidsforholdId": "abc-321",
-            "arbeidsgiver": {
-              "type": "Organisasjon",
-              "organisasjonsnummer": "991609407"
-            },
-            "arbeidstaker": {
-              "aktoerId": "1234567890",
-              "offentligIdent": "31126700000"
-            },
-            "innrapportertEtterAOrdningen": false,
-            "navArbeidsforholdId": 123456,
-            "opplysningspliktig": {
-              "type": "Organisasjon",
-              "organisasjonsnummer": "991609407"
-            },
-            "permisjonPermitteringer": [
-              {
-                "periode": {
-                  "fom": "2014-07-01",
-                  "tom": "2015-12-31"
-                },
-                "permisjonPermitteringId": "123-xyz",
-                "prosent": 50.5,
-                "sporingsinformasjon": {
-                  "endretAv": "Z990693",
-                  "endretKilde": "AAREG",
-                  "endretKildereferanse": "referanse-fra-kilde",
-                  "endretTidspunkt": "2018-09-19T12:11:20.79",
-                  "opprettetAv": "srvappserver",
-                  "opprettetKilde": "EDAG",
-                  "opprettetKildereferanse": "22a26849-aeef-4b81-9174-e238c11e1081",
-                  "opprettetTidspunkt": "2018-09-19T12:10:58.059"
-                },
-                "type": "permisjonMedForeldrepenger",
-                "varslingskode": "string"
-              }
-            ],
-            "registrert": "2018-09-18T11:12:29",
-            "sistBekreftet": "2018-09-19T12:10:31",
-            "sporingsinformasjon": {
-              "endretAv": "Z990693",
-              "endretKilde": "AAREG",
-              "endretKildereferanse": "referanse-fra-kilde",
-              "endretTidspunkt": "2018-09-19T12:11:20.79",
-              "opprettetAv": "srvappserver",
-              "opprettetKilde": "EDAG",
-              "opprettetKildereferanse": "22a26849-aeef-4b81-9174-e238c11e1081",
-              "opprettetTidspunkt": "2018-09-19T12:10:58.059"
-            },
-            "type": "ordinaertArbeidsforhold",
-            "utenlandsopphold": [
-              {
-                "landkode": "JPN",
-                "periode": {
-                  "fom": "2014-07-01",
-                  "tom": "2015-12-31"
-                },
-                "rapporteringsperiode": "2017-12",
-                "sporingsinformasjon": {
-                  "endretAv": "Z990693",
-                  "endretKilde": "AAREG",
-                  "endretKildereferanse": "referanse-fra-kilde",
-                  "endretTidspunkt": "2018-09-19T12:11:20.79",
-                  "opprettetAv": "srvappserver",
-                  "opprettetKilde": "EDAG",
-                  "opprettetKildereferanse": "22a26849-aeef-4b81-9174-e238c11e1081",
-                  "opprettetTidspunkt": "2018-09-19T12:10:58.059"
-                }
-              }
-            ],
-            "varsler": [
-              {
-                "entitet": "ANSETTELSESPERIODE",
-                "varslingskode": "string"
-              }
-            ]
-          }
-        ]
-        """;
 }
