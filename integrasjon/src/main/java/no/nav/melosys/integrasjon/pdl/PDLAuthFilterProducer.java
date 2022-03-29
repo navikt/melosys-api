c67219d64dc2baefe43ba084dac5848c1a9a642f
RuneLind <rulind@gmail.com>
2022-03-29 12:22:08 +0200

    Legg til PDLConsumer token test

--- integrasjon/src/main/java/no/nav/melosys/integrasjon/pdl/PDLAuthFilterProducer.java
+++ integrasjon/src/main/java/no/nav/melosys/integrasjon/pdl/PDLAuthFilterProducer.java
@@ -1,36 +1,58 @@
 package no.nav.melosys.integrasjon.pdl;
 
+import no.finn.unleash.Unleash;
 import no.nav.melosys.sikkerhet.context.ThreadLocalAccessInfo;
 import no.nav.melosys.integrasjon.reststs.RestStsClient;
 import no.nav.melosys.sikkerhet.context.SubjectHandler;
+import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.function.Supplier;
 
 @Configuration
 public class PDLAuthFilterProducer {
     private static final Logger log = LoggerFactory.getLogger(PDLAuthFilterProducer.class);
 
+    private final Unleash unleash;
+
+    public PDLAuthFilterProducer(Unleash unleash) {
+        this.unleash = unleash;
+    }
+
     @Bean
     @Qualifier("system")
     public PDLAuthFilter pdlSystemAuthFilter(RestStsClient restStsClient) {
-        if (ThreadLocalAccessInfo.isFrontendCall()) { // Debug only
-            ThreadLocalAccessInfo.warnFrontendCall(this, "PDLAuthFilterProducer");
-            log.warn("Blir kalt fra forntend\n{}", ThreadLocalAccessInfo.getInfo());
+        if (unleash.isEnabled("melosys.auto.token")) {
+            return autoAuthFilter(restStsClient);
         }
-
         return new PDLAuthFilter(restStsClient, restStsClient::bearerToken);
     }
 
     @Bean
     @Qualifier("saksbehandler")
     public PDLAuthFilter pdlSaksbehandlerAuthFilter(RestStsClient restStsClient) {
-        if (ThreadLocalAccessInfo.isProcessCall()) { // Debug only
-            ThreadLocalAccessInfo.warnFrontendCall(this, "PDLAuthFilterProducer");
-            log.warn("Blir kalt fra prosess\n{}", ThreadLocalAccessInfo.getInfo());
+        if (unleash.isEnabled("melosys.auto.token")) {
+            return autoAuthFilter(restStsClient);
         }
         return new PDLAuthFilter(restStsClient, () -> "Bearer " + SubjectHandler.getInstance().getOidcTokenString());
     }
+
+    @Bean
+    public PDLAuthFilter autoAuthFilter(RestStsClient restStsClient) {
+        return new PDLAuthFilter(restStsClient, getStringSupplier(restStsClient));
+    }
+
+    private Supplier<String> getStringSupplier(RestStsClient restStsClient) {
+        if (ThreadLocalAccessInfo.isProcessCall()) {
+            // Funker ikke med å bruke ThreadLocalAccessInfo.isProcessCall siden dette skjer under wirering
+            return restStsClient::bearerToken;
+        }
+        return () -> "Bearer " + SubjectHandler.getInstance().getOidcTokenString();
+    }
+
 }
---
+++ integrasjonstest/src/test/kotlin/no/nav/melosys/itest/token/PDLConsumerMedUserTokenIT.kt
@@ -0,0 +1,140 @@
+package no.nav.melosys.itest.token
+
+import com.github.tomakehurst.wiremock.WireMockServer
+import com.github.tomakehurst.wiremock.client.WireMock
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration
+import no.finn.unleash.FakeUnleash
+import no.finn.unleash.Unleash
+import no.nav.melosys.integrasjon.felles.EnvironmentHandler
+import no.nav.melosys.integrasjon.pdl.*
+import no.nav.melosys.integrasjon.reststs.RestStsClient
+import no.nav.melosys.integrasjon.reststs.StsRestTemplateProducer
+import no.nav.melosys.sikkerhet.context.SpringSubjectHandler
+import no.nav.melosys.sikkerhet.context.SubjectHandler
+import no.nav.melosys.sikkerhet.context.ThreadLocalAccessInfo
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import org.mockito.Mockito
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.autoconfigure.web.reactive.function.client.WebClientAutoConfiguration
+import org.springframework.boot.test.autoconfigure.web.client.RestClientTest
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.context.annotation.Bean
+import org.springframework.http.MediaType
+import org.springframework.mock.env.MockEnvironment
+import org.springframework.test.web.client.MockRestServiceServer
+import org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo
+import org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess
+
+@RestClientTest(
+    value = [
+        PDLConsumerImpl::class,
+        PDLConsumerProducer::class,
+        PDLAuthFilter::class,
+        PDLAuthFilterProducer::class,
+        StsRestTemplateProducer::class,
+        RestStsClient::class,
+        WebClientAutoConfiguration::class
+    ],
+    properties = ["spring.profiles.active:itest-aareg"]
+)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+internal class PDLConsumerMedUserTokenIT(
+    @Autowired private val server: MockRestServiceServer,
+    @Autowired private val pdlConsumer: PDLConsumer,
+    @Value("\${mockserver.port}") private val mockPort: Int
+) {
+    var wireMockServer: WireMockServer = WireMockServer(WireMockConfiguration.wireMockConfig().port(mockPort))
+
+    @BeforeAll
+    fun beforeAll() {
+        wireMockServer.start()
+    }
+
+    @AfterAll
+    fun afterAll() {
+        wireMockServer.stop()
+    }
+
+    @TestConfiguration
+    class TestConfig {
+        val unleash = FakeUnleash()
+
+        @Bean
+        fun unleash(): Unleash {
+            unleash.enableAll()
+            return unleash
+        }
+    }
+
+    class TestSubjectHandler : SubjectHandler() {
+        override fun getOidcTokenString(): String {
+            return "--token-from-user--"
+        }
+
+        override fun getUserID(): String {
+            return "Z990007"
+        }
+    }
+
+    @BeforeEach
+    fun setup() {
+        wireMockServer.start()
+        val environment = Mockito.spy(MockEnvironment())
+        environment.setProperty("systemuser.username", "test")
+        environment.setProperty("systemuser.password", "test")
+        EnvironmentHandler(environment)
+    }
+
+    @Test
+    fun testRequestFraWeb() {
+        ThreadLocalAccessInfo.preHandle("request")
+
+        SpringSubjectHandler.set(TestSubjectHandler())
+        server.expect(requestTo("/?grant_type=client_credentials&scope=openid"))
+            .andRespond(
+                withSuccess(
+                    "{ \"access_token\": \"--token-from-service--\", \"expires_in\": \"123\" }",
+                    MediaType.APPLICATION_JSON
+                )
+            )
+
+        wireMockServer.stubFor(
+            WireMock.post("/graphql")
+                .withHeader("Authorization", WireMock.equalTo("Bearer --token-from-user--"))
+                .withHeader("Nav-Consumer-Token", WireMock.equalTo("Bearer --token-from-service--"))
+                .willReturn(
+                    WireMock.aResponse()
+                        .withStatus(200)
+                        .withHeader("Content-Type", "application/json")
+                        .withBody(mockData)
+                )
+        )
+
+        pdlConsumer.hentIdenter("99026522600")
+
+        ThreadLocalAccessInfo.afterCompletion("request")
+    }
+
+    private val mockData = """{
+          "data": {
+            "hentIdenter": {
+              "identer": [
+                {
+                  "ident": "99026522600",
+                  "gruppe": "FOLKEREGISTERIDENT"
+                },
+                {
+                  "ident": "9834873315250",
+                  "gruppe": "AKTORID"
+                }
+              ]
+            }
+          }
+        }
+        """
+}
