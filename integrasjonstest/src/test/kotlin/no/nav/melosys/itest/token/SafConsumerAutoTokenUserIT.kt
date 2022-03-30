454fa5aa2008dd59b5d4273115383b4b7be54327
RuneLind <rulind@gmail.com>
2022-03-30 14:33:11 +0200

    Legg til SafConsumer User/System auto token tests

---
+++ integrasjonstest/src/test/kotlin/no/nav/melosys/itest/token/SafConsumerAutoTokenUserIT.kt
@@ -0,0 +1,63 @@
+package no.nav.melosys.itest.token
+
+import com.github.tomakehurst.wiremock.client.WireMock
+import com.github.tomakehurst.wiremock.matching.StringValuePattern
+import no.finn.unleash.FakeUnleash
+import no.finn.unleash.Unleash
+import no.nav.melosys.integrasjon.joark.saf.SafConsumer
+import no.nav.melosys.sikkerhet.context.SpringSubjectHandler
+import no.nav.melosys.sikkerhet.context.ThreadLocalAccessInfo
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.context.annotation.Bean
+import org.springframework.test.web.client.MockRestServiceServer
+import java.util.*
+
+class SafConsumerAutoTokenUserIT(
+    @Autowired private val safConsumer: SafConsumer,
+    @Autowired server: MockRestServiceServer,
+    @Value("\${mockserver.port}") mockPort: Int,
+) : SafConsumerTestBase(server, mockPort) {
+
+    @TestConfiguration
+    class TestConfig {
+        @Bean
+        fun unleash(): Unleash {
+            return FakeUnleash().apply { enable("melosys.auto.token") }
+        }
+    }
+
+    @Test
+    fun authorizationSkalKommeFraProsess() {
+        val uuid = UUID.randomUUID()
+        ThreadLocalAccessInfo.beforExecuteProcess(uuid, "prossesSteg")
+
+        verifyHeaders(
+            mapOf<String, StringValuePattern>(
+                Pair("Authorization", WireMock.equalTo("Bearer --token-from-service--")),
+                Pair("Nav-Consumer-Id", WireMock.equalTo("melosys"))
+            )
+        )
+        safConsumer.hentDokument("1", "1")
+
+        ThreadLocalAccessInfo.afterExecuteProcess(uuid)
+    }
+
+    @Test
+    fun authorizationSkalKommeFraWeb() {
+        SpringSubjectHandler.set(TestSubjectHandler())
+        ThreadLocalAccessInfo.preHandle("request")
+
+        verifyHeaders(
+            mapOf<String, StringValuePattern>(
+                Pair("Authorization", WireMock.equalTo("Bearer --token-from-user--")),
+                Pair("Nav-Consumer-Id", WireMock.equalTo("melosys"))
+            )
+        )
+        safConsumer.hentDokument("1", "1")
+
+        ThreadLocalAccessInfo.afterCompletion("request")
+    }
+}
