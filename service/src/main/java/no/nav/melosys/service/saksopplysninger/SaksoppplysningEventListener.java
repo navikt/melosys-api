9e0a6caa45a7b99f1bb222333dc49fb400763520
Francois Allix <32903762+frans-a@users.noreply.github.com>
2022-03-30 21:27:28 +0200

    Lagre bare familie hvis avklart

--- service/src/main/java/no/nav/melosys/service/saksopplysninger/SaksoppplysningEventListener.java
+++ service/src/main/java/no/nav/melosys/service/saksopplysninger/SaksoppplysningEventListener.java
@@ -3,7 +3,6 @@ package no.nav.melosys.service.saksopplysninger;
 
 import java.util.List;
 
-import no.finn.unleash.Unleash;
 import no.nav.melosys.domain.Behandling;
 import no.nav.melosys.domain.BehandlingEndretStatusEvent;
 import no.nav.melosys.domain.SaksopplysningType;
@@ -12,6 +11,7 @@ import no.nav.melosys.domain.person.Informasjonsbehov;
 import no.nav.melosys.domain.person.PersonMedHistorikk;
 import no.nav.melosys.domain.person.Persondata;
 import no.nav.melosys.service.SaksopplysningerService;
+import no.nav.melosys.service.avklartefakta.AvklartefaktaService;
 import no.nav.melosys.service.behandling.BehandlingService;
 import no.nav.melosys.service.persondata.PersondataFasade;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -25,35 +25,41 @@ public class SaksoppplysningEventListener {
     private final SaksopplysningerService saksopplysningerService;
     private final BehandlingService behandlingService;
     private final PersondataFasade persondataFasade;
-    private final Unleash unleash;
+    private final AvklartefaktaService avklartefaktaService;
 
     public SaksoppplysningEventListener(SaksopplysningerService saksopplysningerService,
                                         BehandlingService behandlingService,
                                         @Qualifier("system") PersondataFasade persondataFasade,
-                                        Unleash unleash) {
+                                        AvklartefaktaService avklartefaktaService) {
         this.saksopplysningerService = saksopplysningerService;
         this.behandlingService = behandlingService;
         this.persondataFasade = persondataFasade;
-        this.unleash = unleash;
+        this.avklartefaktaService = avklartefaktaService;
     }
 
     @EventListener
     @Transactional
     public void lagrePersonopplysninger(BehandlingEndretStatusEvent event) {
-        if (unleash.isEnabled("melosys.pdl.aktiv")) {
-            if (List.of(Behandlingsstatus.AVSLUTTET, Behandlingsstatus.IVERKSETTER_VEDTAK, Behandlingsstatus.MIDLERTIDIG_LOVVALGSBESLUTNING).contains(event.getBehandlingsstatus())) {
-                Behandling behandling = behandlingService.hentBehandlingMedSaksopplysninger(event.getBehandling().getId());
+        if (List.of(Behandlingsstatus.AVSLUTTET, Behandlingsstatus.IVERKSETTER_VEDTAK, Behandlingsstatus.MIDLERTIDIG_LOVVALGSBESLUTNING).contains(event.getBehandlingsstatus())) {
+            Behandling behandling = behandlingService.hentBehandlingMedSaksopplysninger(event.getBehandling().getId());
 
-                if (behandling.manglerSaksopplysningerAvType(List.of(SaksopplysningType.PDL_PERSOPL))) {
-                    Persondata persondata = persondataFasade.hentPerson(behandling.getFagsak().hentAktørID(), Informasjonsbehov.MED_FAMILIERELASJONER);
-                    saksopplysningerService.lagrePersonopplysninger(behandling, persondata);
-                }
-
-                if (behandling.manglerSaksopplysningerAvType(List.of(SaksopplysningType.PDL_PERS_SAKS))) {
-                    PersonMedHistorikk personMedHistorikk = persondataFasade.hentPersonMedHistorikk(behandling.getFagsak().hentAktørID());
-                    saksopplysningerService.lagrePersonMedHistorikk(behandling, personMedHistorikk);
-                }
+            if (behandling.manglerSaksopplysningerAvType(List.of(SaksopplysningType.PDL_PERSOPL))) {
+                saksopplysningerService.lagrePersonopplysninger(behandling, hentPersondata(behandling));
             }
+
+            if (behandling.manglerSaksopplysningerAvType(List.of(SaksopplysningType.PDL_PERS_SAKS))) {
+                PersonMedHistorikk personMedHistorikk = persondataFasade.hentPersonMedHistorikk(behandling.getFagsak().hentAktørID());
+                saksopplysningerService.lagrePersonMedHistorikk(behandling, personMedHistorikk);
+            }
+        }
+    }
+
+    private Persondata hentPersondata(Behandling behandling) {
+        if (avklartefaktaService.hentAvklarteMedfølgendeBarn(behandling.getId()).finnes()
+            || avklartefaktaService.hentAvklarteMedfølgendeEktefelle(behandling.getId()).finnes()) {
+            return persondataFasade.hentPerson(behandling.getFagsak().hentAktørID(), Informasjonsbehov.MED_FAMILIERELASJONER);
+        } else {
+            return persondataFasade.hentPerson(behandling.getFagsak().hentAktørID());
         }
     }
 }
