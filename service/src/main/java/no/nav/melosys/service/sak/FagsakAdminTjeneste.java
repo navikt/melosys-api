670d4ddc7da78bb2872fa6ba70549775482f4854
Isa Salem Hame <wanghattan@gmail.com>
2022-05-04 12:14:35 +0200

    Legger til logging i alle admin-kall

--- service/src/main/java/no/nav/melosys/service/sak/FagsakAdminTjeneste.java
+++ service/src/main/java/no/nav/melosys/service/sak/FagsakAdminTjeneste.java
@@ -3,6 +3,8 @@ package no.nav.melosys.service.sak;
 import no.nav.melosys.domain.Fagsak;
 import no.nav.melosys.service.AdminTjeneste;
 import no.nav.security.token.support.core.api.Unprotected;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,22 +14,22 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/admin/fagsaker")
 public class FagsakAdminTjeneste implements AdminTjeneste {
     private final String apiKey;
-    private final FagsakService fagsakService;
     private final HenleggFagsakService henleggFagsakService;
+    private static final Logger log = LoggerFactory.getLogger(FagsakAdminTjeneste.class);
 
-    public FagsakAdminTjeneste(@Value("${Melosys-admin.apikey}") String apiKey, FagsakService fagsakService,
-                               HenleggFagsakService henleggFagsakService) {
+    public FagsakAdminTjeneste(@Value("${Melosys-admin.apikey}") String apiKey, HenleggFagsakService henleggFagsakService) {
         this.apiKey = apiKey;
-        this.fagsakService = fagsakService;
         this.henleggFagsakService = henleggFagsakService;
     }
 
     @PutMapping("/{saksnummer}/henlegg-bortfalt")
     public ResponseEntity<Void> henleggFagsakSomBortfalt(@PathVariable String saksnummer,
-                                              @RequestHeader(API_KEY_HEADER) String apiKey) {
+                                                         @RequestHeader(API_KEY_HEADER) String apiKey) {
         validerApikey(apiKey);
 
+        log.info("Forsøker å henlegge sak {}", saksnummer);
         henleggFagsakService.henleggSakEllerBehandlingSomBortfalt(saksnummer);
+
         return ResponseEntity.noContent().build();
     }
 
