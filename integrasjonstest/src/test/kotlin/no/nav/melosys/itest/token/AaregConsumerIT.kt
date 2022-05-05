7cbff0884ad8b785c6e81405201d7b7c968cbd6c
RuneLind <rulind@gmail.com>
2022-05-05 15:07:01 +0200

    Fjern AaregConsumerTestBase

--- integrasjonstest/src/test/kotlin/no/nav/melosys/itest/token/AaregConsumerIT.kt
+++ integrasjonstest/src/test/kotlin/no/nav/melosys/itest/token/AaregConsumerIT.kt
@@ -2,18 +2,37 @@ package no.nav.melosys.itest.token
 
 import com.github.tomakehurst.wiremock.client.WireMock
 import com.github.tomakehurst.wiremock.matching.StringValuePattern
+import no.nav.melosys.integrasjon.aareg.arbeidsforhold.ArbeidsforholdContextExchangeFilter
+import no.nav.melosys.integrasjon.aareg.arbeidsforhold.ArbeidsforholdQuery
 import no.nav.melosys.integrasjon.aareg.arbeidsforhold.ArbeidsforholdRestConsumer
+import no.nav.melosys.integrasjon.aareg.arbeidsforhold.ArbeidsforholdRestConsumerConfig
+import no.nav.melosys.integrasjon.reststs.RestStsClient
+import no.nav.melosys.integrasjon.reststs.StsRestTemplateProducer
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.autoconfigure.web.reactive.function.client.WebClientAutoConfiguration
+import org.springframework.boot.test.autoconfigure.web.client.RestClientTest
 import org.springframework.test.web.client.MockRestServiceServer
 
+@RestClientTest(
+    value = [
+        StsRestTemplateProducer::class,
+        RestStsClient::class,
+        WebClientAutoConfiguration::class,
+
+        ArbeidsforholdRestConsumer::class,
+        ArbeidsforholdRestConsumerConfig::class,
+        ArbeidsforholdContextExchangeFilter::class,
+    ],
+    properties = ["spring.profiles.active:itest-token"]
+)
 class AaregConsumerIT(
     @Autowired private val arbeidsforholdRestConsumer: ArbeidsforholdRestConsumer,
     @Autowired private val server: MockRestServiceServer,
     @Value("\${mockserver.port}") mockPort: Int,
-) : AaregConsumerTestBase(server, arbeidsforholdRestConsumer, mockPort) {
+) : ConsumerTestBase<String>(server, mockPort) {
 
     @Test
     fun authorizationSkalKommeFraBruker() {
@@ -55,5 +74,14 @@ class AaregConsumerIT(
         executeErrorFromServer { error ->
             assertThat(error).startsWith("Henting av arbeidsforhold fra Aareg feilet")
         }
+    }
+
+    override fun getMockData(): String {
+        return "[]"
+    }
+
+    override fun executeRequest() {
+        val build = ArbeidsforholdQuery.Builder().build()
+        arbeidsforholdRestConsumer.finnArbeidsforholdPrArbeidstaker("121", build)
     }
 }
