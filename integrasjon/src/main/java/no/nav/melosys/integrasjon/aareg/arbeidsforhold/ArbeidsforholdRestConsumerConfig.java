de4127a5d71182a88f5b2aa7037608fa90848ed4
RuneLind <rulind@gmail.com>
2022-05-05 10:24:28 +0200

    Bruk WebClientConfig.errorFilter over alt
    Flytt test og gjøre dette i spring context. Og legg flere tester

--- integrasjon/src/main/java/no/nav/melosys/integrasjon/aareg/arbeidsforhold/ArbeidsforholdRestConsumerConfig.java
+++ integrasjon/src/main/java/no/nav/melosys/integrasjon/aareg/arbeidsforhold/ArbeidsforholdRestConsumerConfig.java
@@ -1,14 +1,11 @@
 package no.nav.melosys.integrasjon.aareg.arbeidsforhold;
 
 
-import no.nav.melosys.exception.TekniskException;
 import no.nav.melosys.integrasjon.felles.WebClientConfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
 
 @Configuration
 public class ArbeidsforholdRestConsumerConfig implements WebClientConfig {
@@ -24,19 +21,7 @@ public class ArbeidsforholdRestConsumerConfig implements WebClientConfig {
         return new ArbeidsforholdRestConsumer(webClientBuilder
             .baseUrl(url)
             .filter(systemContextExchangeFilter)
-            .filter(errorFilter())
+            .filter(errorFilter("Henting av arbeidsforhold fra Aareg feilet"))
             .build());
-    }
-
-    public static ExchangeFilterFunction errorFilter() {
-        return ExchangeFilterFunction.ofResponseProcessor(response -> {
-            if (response.statusCode().isError()) {
-                return response.bodyToMono(String.class)
-                    .defaultIfEmpty(response.statusCode().getReasonPhrase())
-                    .flatMap(errorBody -> Mono.error(new TekniskException(
-                        String.format("Henting av arbeidsforhold fra Aareg feilet. %s - %s", response.statusCode(), errorBody))));
-            }
-            return Mono.just(response);
-        });
     }
 }
