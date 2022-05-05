0bf9838f4858c538f98a836d24473266c20baf93
RuneLind <rulind@gmail.com>
2022-05-05 15:03:28 +0200

    Fjern PDLConsumerTestBase

--- integrasjonstest/src/test/kotlin/no/nav/melosys/itest/token/PDLConsumerIT.kt
+++ integrasjonstest/src/test/kotlin/no/nav/melosys/itest/token/PDLConsumerIT.kt
@@ -1,19 +1,37 @@
 package no.nav.melosys.itest.token
 
+import com.github.tomakehurst.wiremock.client.MappingBuilder
 import com.github.tomakehurst.wiremock.client.WireMock
 import com.github.tomakehurst.wiremock.matching.StringValuePattern
-import no.nav.melosys.integrasjon.pdl.PDLConsumer
+import no.nav.melosys.integrasjon.pdl.*
+import no.nav.melosys.integrasjon.reststs.RestStsClient
+import no.nav.melosys.integrasjon.reststs.StsRestTemplateProducer
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.autoconfigure.web.reactive.function.client.WebClientAutoConfiguration
+import org.springframework.boot.test.autoconfigure.web.client.RestClientTest
 import org.springframework.test.web.client.MockRestServiceServer
 
+@RestClientTest(
+    value = [
+        StsRestTemplateProducer::class,
+        RestStsClient::class,
+        WebClientAutoConfiguration::class,
+
+        PDLConsumerImpl::class,
+        PDLConsumerProducer::class,
+        PDLAuthFilter::class,
+        PDLAuthFilterProducer::class,
+    ],
+    properties = ["spring.profiles.active:itest-token"]
+)
 class PDLConsumerIT(
     @Autowired private val pdlConsumer: PDLConsumer,
     @Autowired server: MockRestServiceServer,
     @Value("\${mockserver.port}") mockPort: Int,
-) : PDLConsumerTestBase(server, mockPort, pdlConsumer) {
+) : ConsumerTestBase<String>(server, mockPort) {
 
     @Test
     fun authorizationSkalKommeFraSystem() {
@@ -56,4 +74,33 @@ class PDLConsumerIT(
             Assertions.assertThat(error).startsWith("Kall mot PDL feilet.")
         }
     }
+
+    override fun createWireMock(): MappingBuilder {
+        return WireMock.post("/graphql")
+    }
+
+    override fun getMockData(): String {
+        return """{
+          "data": {
+            "hentIdenter": {
+              "identer": [
+                {
+                  "ident": "99026522600",
+                  "gruppe": "FOLKEREGISTERIDENT"
+                },
+                {
+                  "ident": "9834873315250",
+                  "gruppe": "AKTORID"
+                }
+              ]
+            }
+          }
+        }
+        """
+    }
+
+    override fun executeRequest() {
+        pdlConsumer.hentIdenter("0")
+    }
 }
+
