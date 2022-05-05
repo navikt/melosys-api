1070fcccac7f4f3e90ddd91a91383ac8d77ca665
RuneLind <rulind@gmail.com>
2022-05-05 14:57:01 +0200

    Fjern SakConsumerTestBase

--- integrasjonstest/src/test/kotlin/no/nav/melosys/itest/token/SakConsumerIT.kt
+++ integrasjonstest/src/test/kotlin/no/nav/melosys/itest/token/SakConsumerIT.kt
@@ -2,17 +2,34 @@ package no.nav.melosys.itest.token
 
 import com.github.tomakehurst.wiremock.client.WireMock
 import com.github.tomakehurst.wiremock.matching.StringValuePattern
+import no.nav.melosys.integrasjon.reststs.RestStsClient
+import no.nav.melosys.integrasjon.reststs.StsRestTemplateProducer
 import no.nav.melosys.integrasjon.sak.SakConsumer
+import no.nav.melosys.integrasjon.sak.SakConsumerImpl
+import no.nav.melosys.integrasjon.sak.SakConsumerProducer
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.autoconfigure.web.reactive.function.client.WebClientAutoConfiguration
+import org.springframework.boot.test.autoconfigure.web.client.RestClientTest
 import org.springframework.test.web.client.MockRestServiceServer
 
+@RestClientTest(
+    value = [
+        StsRestTemplateProducer::class,
+        RestStsClient::class,
+        WebClientAutoConfiguration::class,
+
+        SakConsumerImpl::class,
+        SakConsumerProducer::class,
+    ],
+    properties = ["spring.profiles.active:itest-token"]
+)
 class SakConsumerIT(
     @Autowired private val sakConsumer: SakConsumer,
     @Autowired server: MockRestServiceServer,
     @Value("\${mockserver.port}") mockPort: Int,
-) : SakConsumerTestBase(server, mockPort, sakConsumer) {
+) : ConsumerTestBase<String>(server, mockPort) {
 
     @Test
     fun authorizationSkalKommeFraSystem() {
@@ -46,5 +63,11 @@ class SakConsumerIT(
         executeRequest()
     }
 
+    override fun getMockData(): String {
+        return "{}"
+    }
 
+    override fun executeRequest() {
+        sakConsumer.hentSak(1L)
+    }
 }
