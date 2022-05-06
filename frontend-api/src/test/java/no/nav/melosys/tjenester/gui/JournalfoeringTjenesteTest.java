cd5d16ffbd761e192893299e063cd7dc6e1dc9c2
Janita Johansen <janita.marielle.johansen@nav.no>
2022-05-06 08:03:40 +0200

    Oppdater skjema

--- frontend-api/src/test/java/no/nav/melosys/tjenester/gui/JournalfoeringTjenesteTest.java
+++ frontend-api/src/test/java/no/nav/melosys/tjenester/gui/JournalfoeringTjenesteTest.java
@@ -15,20 +15,18 @@ import no.nav.melosys.tjenester.gui.dto.journalforing.JournalpostDto;
 import org.jeasy.random.EasyRandom;
 import org.jeasy.random.EasyRandomParameters;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.ResponseEntity;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-@Disabled("Frem til Schema er oppdatert")
 @ExtendWith(MockitoExtension.class)
 class JournalfoeringTjenesteTest extends JsonSchemaTestParent {
     private static final Logger log = LoggerFactory.getLogger(JournalfoeringTjenesteTest.class);
@@ -56,21 +54,22 @@ class JournalfoeringTjenesteTest extends JsonSchemaTestParent {
     }
 
     @Test
-    void hentJournalpost_validerSchema() throws IOException {
+    void hentJournalpost_validerKallOgSchema() throws IOException {
         Journalpost journalpost = random.nextObject(Journalpost.class);
         journalpost.setAvsenderId(SAMPLE_ORGNR);
         when(journalføringService.hentJournalpost(anyString())).thenReturn(journalpost);
         when(journalføringService.finnBrukerIdent(journalpost)).thenReturn(Optional.of(SAMPLE_FNR));
 
-        ResponseEntity<JournalpostDto> response = tjeneste.hentJournalpostOpplysninger(anyString());
-        JournalpostDto journalpostDto = response.getBody();
+        JournalpostDto journalpostDto = tjeneste.hentJournalpostOpplysninger(anyString()).getBody();
 
+        verify(journalføringService).hentJournalpost(any());
         valider(journalpostDto, JOURNALFOERING_SCHEMA, log);
     }
 
     @Test
     void journalføringTilordne_validerKallOgSchema() throws IOException {
         JournalfoeringTilordneDto journalføringDto = random.nextObject(JournalfoeringTilordneDto.class);
+        journalføringDto.setVirksomhetOrgnr(null);
         journalføringDto.setBrukerID(SAMPLE_FNR);
         journalføringDto.setBehandlingstypeKode(Behandlingstyper.ENDRET_PERIODE.getKode());
 
@@ -84,6 +83,7 @@ class JournalfoeringTjenesteTest extends JsonSchemaTestParent {
     @Test
     void journalføringOpprett_validerKallOgSchema() throws IOException {
         JournalfoeringOpprettDto journalføringDto = random.nextObject(JournalfoeringOpprettDto.class);
+        journalføringDto.setVirksomhetOrgnr(null);
         journalføringDto.setBrukerID(SAMPLE_FNR);
         journalføringDto.setBehandlingstemaKode(Behandlingstema.ARBEID_FLERE_LAND.getKode());
         journalføringDto.setArbeidsgiverID(SAMPLE_ORGNR);
@@ -97,12 +97,34 @@ class JournalfoeringTjenesteTest extends JsonSchemaTestParent {
     }
 
     @Test
-    void journalføringOpprett_validerSchemaMedRepresentantIDNull() throws IOException {
+    void journalføringOpprett_validerKallOgSchemaMedRepresentantIDNull() throws IOException {
         JournalfoeringOpprettDto journalføringDto = random.nextObject(JournalfoeringOpprettDto.class);
+        journalføringDto.setVirksomhetOrgnr(null);
         journalføringDto.setBrukerID(SAMPLE_FNR);
         journalføringDto.setBehandlingstemaKode(Behandlingstema.ARBEID_FLERE_LAND.getKode());
         journalføringDto.setArbeidsgiverID(SAMPLE_ORGNR);
         journalføringDto.setRepresentantID(null);
+
+        tjeneste.journalførOgOpprettSak(journalføringDto);
+
+        verify(journalføringService).journalførOgOpprettSak(journalføringDto);
+        verify(oppgaveService).ferdigstillOppgave(journalføringDto.getOppgaveID());
+        valider(journalføringDto, JOURNALFOERING_OPPRETT_SCHEMA, log);
+    }
+
+    @Test
+    void journalføringOpprett_validerKallOgSchemaMedBrukerIDNull() throws IOException {
+        JournalfoeringOpprettDto journalføringDto = random.nextObject(JournalfoeringOpprettDto.class);
+        journalføringDto.setVirksomhetOrgnr(SAMPLE_ORGNR);
+        journalføringDto.setBrukerID(null);
+        journalføringDto.setBehandlingstemaKode(Behandlingstema.ARBEID_FLERE_LAND.getKode());
+        journalføringDto.setArbeidsgiverID(SAMPLE_ORGNR);
+        journalføringDto.setRepresentantID(SAMPLE_ORGNR);
+
+        tjeneste.journalførOgOpprettSak(journalføringDto);
+
+        verify(journalføringService).journalførOgOpprettSak(journalføringDto);
+        verify(oppgaveService).ferdigstillOppgave(journalføringDto.getOppgaveID());
         valider(journalføringDto, JOURNALFOERING_OPPRETT_SCHEMA, log);
     }
 
