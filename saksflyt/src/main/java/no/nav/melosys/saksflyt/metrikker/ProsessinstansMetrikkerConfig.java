650be05ff9e119616f9835277d62e14d347b2091
Isa Salem Hame <wanghattan@gmail.com>
2022-05-06 13:44:23 +0200

    Flytter metrikker for feilede pi per steg til gammel bønne

--- saksflyt/src/main/java/no/nav/melosys/saksflyt/metrikker/ProsessinstansMetrikkerConfig.java
+++ saksflyt/src/main/java/no/nav/melosys/saksflyt/metrikker/ProsessinstansMetrikkerConfig.java
@@ -17,27 +17,21 @@ public class ProsessinstansMetrikkerConfig {
                                                ProsessinstansStatusCache statusCache) {
         return registry -> {
             for (ProsessType prosessType : ProsessType.values()) {
-                String gaugeNavn = MetrikkerNavn.PROSESSINSTANSER + prosessType.getKode().toLowerCase() + ".feilet";
+                String gaugeNavnProsessinstansType = MetrikkerNavn.PROSESSINSTANSER + prosessType.getKode().toLowerCase() + ".feilet";
                 Gauge.builder(
-                    gaugeNavn,
+                    gaugeNavnProsessinstansType,
                     statusCache,
                     type -> statusCache.antallProsessinstanserFeilet(prosessType)
                 ).register(meterRegistry);
-            }
-        };
-    }
 
-    @Bean
-    public MeterBinder ProsessinstansFeiledeStegMetrikker(MeterRegistry meterRegistry,
-                                               ProsessinstansStatusCache statusCache) {
-        return registry -> {
-            for (ProsessSteg prosessSteg : ProsessSteg.values()) {
-                String gaugeNavn = MetrikkerNavn.PROSESSINSTANSER_STEG + prosessSteg.getKode().toLowerCase() + ".feilet";
-                Gauge.builder(
-                    gaugeNavn,
-                    statusCache,
-                    type -> statusCache.antallProsessinstanserFeiletPaSteg(prosessSteg)
-                ).register(meterRegistry);
+                for (ProsessSteg prosessSteg : ProsessSteg.values()) {
+                    String gaugeNavnProsessinstansSteg = MetrikkerNavn.PROSESSINSTANSER_STEG + prosessSteg.getKode().toLowerCase() + ".feilet";
+                    Gauge.builder(
+                        gaugeNavnProsessinstansSteg,
+                        statusCache,
+                        type -> statusCache.antallProsessinstanserFeiletPaSteg(prosessSteg)
+                    ).register(meterRegistry);
+                }
             }
         };
     }
