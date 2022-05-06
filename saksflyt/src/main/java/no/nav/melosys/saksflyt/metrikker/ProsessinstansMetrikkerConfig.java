c3ee03ad873c0ac40463eb1ef33666ec4cb73ba2
Isa Salem Hame <wanghattan@gmail.com>
2022-05-06 10:43:00 +0200

    Kaller på rktig metode når man henter antall prosessintanser feilet per steg

--- saksflyt/src/main/java/no/nav/melosys/saksflyt/metrikker/ProsessinstansMetrikkerConfig.java
+++ saksflyt/src/main/java/no/nav/melosys/saksflyt/metrikker/ProsessinstansMetrikkerConfig.java
@@ -3,6 +3,7 @@ package no.nav.melosys.saksflyt.metrikker;
 import io.micrometer.core.instrument.Gauge;
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.binder.MeterBinder;
+import no.nav.melosys.domain.saksflyt.ProsessSteg;
 import no.nav.melosys.domain.saksflyt.ProsessType;
 import no.nav.melosys.metrics.MetrikkerNavn;
 import org.springframework.context.annotation.Bean;
@@ -30,12 +31,12 @@ public class ProsessinstansMetrikkerConfig {
     public MeterBinder ProsessinstansFeiledeStegMetrikker(MeterRegistry meterRegistry,
                                                ProsessinstansStatusCache statusCache) {
         return registry -> {
-            for (ProsessType prosessType : ProsessType.values()) {
-                String gaugeNavn = MetrikkerNavn.PROSESSINSTANSER_STEG + prosessType.getKode().toLowerCase() + ".feilet";
+            for (ProsessSteg prosessSteg : ProsessSteg.values()) {
+                String gaugeNavn = MetrikkerNavn.PROSESSINSTANSER_STEG + prosessSteg.getKode().toLowerCase() + ".feilet";
                 Gauge.builder(
                     gaugeNavn,
                     statusCache,
-                    type -> statusCache.antallProsessinstanserFeilet(prosessType)
+                    type -> statusCache.antallProsessinstanserFeiletPaSteg(prosessSteg)
                 ).register(meterRegistry);
             }
         };
