2f870f2bacc4d547b8f18dabbea0dde0bfc0fa24
Fredrik Olsen Hauge <fredrik.olsen.hauge@nav.no>
2022-05-06 13:22:23 +0200

    Revertet sletting av gammmel doksys kode

--- service/src/main/java/no/nav/melosys/service/dokument/brev/BrevDataMapperRuter.java
+++ service/src/main/java/no/nav/melosys/service/dokument/brev/BrevDataMapperRuter.java
@@ -20,6 +20,7 @@ final class BrevDataMapperRuter {
             .put(Produserbaredokumenter.INNVILGELSE_YRKESAKTIV, InnvilgelsesbrevMapper.class)
             .put(Produserbaredokumenter.INNVILGELSE_YRKESAKTIV_FLERE_LAND, InnvilgelsesbrevFlereLandMapper.class)
             .put(Produserbaredokumenter.MELDING_FORVENTET_SAKSBEHANDLINGSTID, ForvaltningsmeldingMapper.class)
+            .put(Produserbaredokumenter.MELDING_HENLAGT_SAK, HenleggelsesbrevMapper.class)
             .put(Produserbaredokumenter.MELDING_MANGLENDE_OPPLYSNINGER, MangelbrevMapper.class)
             .put(Produserbaredokumenter.ORIENTERING_ANMODNING_UNNTAK, AnmodningUnntakMapper.class)
             .put(Produserbaredokumenter.ORIENTERING_UTPEKING_UTLAND, UtpekingAnnetLandMapper.class)
---
+++ service/src/test/java/no/nav/melosys/service/dokument/brev/mapper/HenleggelsesbrevMapperTest.java
@@ -0,0 +1,86 @@
+package no.nav.melosys.service.dokument.brev.mapper;
+
+import java.time.Instant;
+
+import no.nav.dok.brevdata.felles.v1.navfelles.NorskPostadresse;
+import no.nav.dok.melosysbrev._000072.HenleggelseGrunnKode;
+import no.nav.dok.melosysbrev.felles.melosys_felles.FellesType;
+import no.nav.dok.melosysbrev.felles.melosys_felles.MelosysNAVFelles;
+import no.nav.melosys.domain.Behandling;
+import no.nav.melosys.domain.Behandlingsresultat;
+import no.nav.melosys.domain.Fagsak;
+import no.nav.melosys.domain.kodeverk.Aktoersroller;
+import no.nav.melosys.domain.kodeverk.begrunnelser.Henleggelsesgrunner;
+import no.nav.melosys.service.dokument.brev.BrevDataMottattDato;
+import no.nav.melosys.service.dokument.brev.BrevbestillingRequest;
+import org.jeasy.random.EasyRandom;
+import org.junit.jupiter.api.Test;
+
+import static no.nav.melosys.service.dokument.brev.mapper.felles.FellesBrevtypeMappingTest.hentAlleVerdierFraKodeverk;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.jeasy.random.FieldPredicates.*;
+
+public class HenleggelsesbrevMapperTest {
+
+    private static final EasyRandom easyRandom = new EasyRandom(EasyRandomConfigurer.paramForDokProd()
+        .collectionSizeRange(0, 2)
+        .objectPoolSize(3)
+        .excludeField(named("lovvalgsperioder").or(named("avklartefakta").or(named("vilkaarsresultater"))))
+        .randomize(named("postnummer").and(ofType(String.class)).and(inClass(NorskPostadresse.class)), () -> "1234"));
+
+    @Test
+    public void mapTilBrevXmlGirIkkeTomXmlStreng() throws Exception {
+        testMapTilBrevXml();
+    }
+
+    private void testMapTilBrevXml() throws Exception {
+        testMapTilBrevXml(lagBehandlingsresultat());
+    }
+
+    private Behandlingsresultat lagBehandlingsresultat() {
+        return easyRandom.nextObject(Behandlingsresultat.class);
+    }
+
+    private void testMapTilBrevXml(Behandlingsresultat behandlingsresultat) throws Exception {
+        testMapTilBrevXml(lagBehandling(lagFagsak()), behandlingsresultat);
+    }
+
+    private Fagsak lagFagsak() {
+        return easyRandom.nextObject(Fagsak.class);
+    }
+
+    private Behandling lagBehandling(Fagsak fagsak) {
+        return easyRandom.nextObject(Behandling.class);
+    }
+
+    private void testMapTilBrevXml(Behandling behandling, Behandlingsresultat behandlingsresultat) throws Exception {
+        FellesType fellesType = lagFellesType();
+        MelosysNAVFelles navFelles = LagMelosysNAVFelles();
+        BrevbestillingRequest brevbestillingRequest = new BrevbestillingRequest.Builder()
+            .medMottaker(Aktoersroller.BRUKER)
+            .medBegrunnelseKode("ANNET")
+            .medFritekst("something")
+            .build();
+
+        BrevDataMottattDato brevdata = new BrevDataMottattDato("saksbehandler", brevbestillingRequest);
+        brevdata.initierendeJournalpostForsendelseMottattTidspunkt = Instant.now();
+
+        HenleggelsesbrevMapper instans = new HenleggelsesbrevMapper();
+        String resultat = instans.mapTilBrevXML(fellesType, navFelles, behandling, behandlingsresultat, brevdata);
+        assertThat(resultat).matches("(?s)\\<\\?xml version=\"\\d\\.\\d+\" .*>\n.*");
+    }
+
+    private MelosysNAVFelles LagMelosysNAVFelles() {
+        return easyRandom.nextObject(MelosysNAVFelles.class);
+    }
+
+    private FellesType lagFellesType() {
+        return easyRandom.nextObject(FellesType.class);
+    }
+
+    @Test
+    public void testHenleggelsegrunnKode() throws Exception {
+        hentAlleVerdierFraKodeverk(Henleggelsesgrunner.class)
+            .forEach(HenleggelseGrunnKode::fromValue);
+    }
+}
