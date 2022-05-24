1b41fa8ff11ab99ad073ca79611799d77980737c
Janita Johansen <janita.marielle.johansen@nav.no>
2022-05-24 14:16:33 +0200

    Glemt ubrukt import

--- service/src/main/java/no/nav/melosys/service/registeropplysninger/OrganisasjonOppslagSystemService.java
+++ service/src/main/java/no/nav/melosys/service/registeropplysninger/OrganisasjonOppslagSystemService.java
@@ -1,7 +1,6 @@
 package no.nav.melosys.service.registeropplysninger;
 
 import no.nav.melosys.integrasjon.ereg.EregFasade;
-import no.nav.melosys.service.behandling.BehandlingService;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
