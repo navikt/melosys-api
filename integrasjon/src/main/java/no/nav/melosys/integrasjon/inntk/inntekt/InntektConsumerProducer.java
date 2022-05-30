9581f5fbd9fa824306e68e31baf1b5f4fa6b1246
RuneLind <rulind@gmail.com>
2022-05-30 21:15:17 +0200

    Legg på StsLogin for InntektConsumerProducer

--- integrasjon/src/main/java/no/nav/melosys/integrasjon/inntk/inntekt/InntektConsumerProducer.java
+++ integrasjon/src/main/java/no/nav/melosys/integrasjon/inntk/inntekt/InntektConsumerProducer.java
@@ -3,36 +3,37 @@ package no.nav.melosys.integrasjon.inntk.inntekt;
 
 import no.nav.melosys.sikkerhet.sts.NAVSTSClient;
 import no.nav.melosys.sikkerhet.sts.StsConfigurationUtil;
+import no.nav.melosys.sikkerhet.sts.StsLogin;
 import no.nav.tjeneste.virksomhet.inntekt.v3.binding.InntektV3;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import static no.nav.melosys.sikkerhet.sts.NAVSTSClient.StsClientType.SYSTEM_SAML;
 
 
 @Configuration
 public class InntektConsumerProducer {
 
-    private InntektConsumerConfig config;
+    private final InntektConsumerConfig config;
+    private final StsLogin stsLogin;
 
-    public InntektConsumerProducer(InntektConsumerConfig config) {
+    public InntektConsumerProducer(InntektConsumerConfig config, StsLogin stsLogin) {
         this.config = config;
+        this.stsLogin = stsLogin;
     }
 
     @Bean
     InntektConsumer inntektConsumer() {
-        InntektV3 port = wrapWithSts(config.getPort(), SYSTEM_SAML);
+        InntektV3 port = wrapWithSts(config.getPort());
         return new InntektConsumerImpl(port);
     }
 
     @Bean
     InntektSelftestConsumer inntektSelftestConsumer() {
-        InntektV3 port = wrapWithSts(config.getPort(), SYSTEM_SAML);
+        InntektV3 port = wrapWithSts(config.getPort());
         return new InntektSelftestConsumerImpl(port, config.getEndpointUrl());
     }
 
-    private InntektV3 wrapWithSts(InntektV3 port, NAVSTSClient.StsClientType oidcTilSaml) {
-        return StsConfigurationUtil.wrapWithSts(port, oidcTilSaml);
+    private InntektV3 wrapWithSts(InntektV3 port) {
+        return StsConfigurationUtil.wrapWithSts(port, NAVSTSClient.StsClientType.SYSTEM_SAML, stsLogin);
     }
 
 }
