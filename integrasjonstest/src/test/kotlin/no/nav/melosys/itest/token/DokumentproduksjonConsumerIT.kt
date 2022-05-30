3e95c6d6248a05ba5b855a9d6cdde22aca264be1
RuneLind <rulind@gmail.com>
2022-05-30 12:47:31 +0200

    Legg på securityWireMockServer.resetAll()

--- integrasjonstest/src/test/kotlin/no/nav/melosys/itest/token/DokumentproduksjonConsumerIT.kt
+++ integrasjonstest/src/test/kotlin/no/nav/melosys/itest/token/DokumentproduksjonConsumerIT.kt
@@ -15,6 +15,7 @@ import no.nav.melosys.sikkerhet.sts.StsLogin
 import no.nav.tjeneste.virksomhet.dokumentproduksjon.v3.meldinger.ProduserIkkeredigerbartDokumentRequest
 import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
@@ -39,10 +40,11 @@ class DokumentproduksjonConsumerIT(
     @Autowired private val dokumentproduksjonConsumer: DokumentproduksjonConsumer,
     @Autowired server: MockRestServiceServer,
     @Value("\${mockserver.port}") mockPort: Int,
+    @Value("\${mockserver.security.port}") mockSecurityUrl: Int
 ) : ConsumerTestBase<String>(server, mockPort) {
 
     private val securityWireMockServer: WireMockServer =
-        WireMockServer(WireMockConfiguration.wireMockConfig().port(mockPort + 1))
+        WireMockServer(WireMockConfiguration.wireMockConfig().port(mockSecurityUrl))
 
     @BeforeAll
     fun beforeAll2() {
@@ -54,9 +56,9 @@ class DokumentproduksjonConsumerIT(
         securityWireMockServer.stop()
     }
 
-    @Test
-    fun test() {
-        dokumentproduksjonConsumer.produserIkkeredigerbartDokument(ProduserIkkeredigerbartDokumentRequest())
+    @BeforeEach
+    fun setup2() {
+        securityWireMockServer.resetAll()
     }
 
     override fun createWireMock(): MappingBuilder {
@@ -65,14 +67,12 @@ class DokumentproduksjonConsumerIT(
 
     @Test
     fun authorizationSkalKommeFraSystem() {
-        securityWireMockServer.start()
-
         securityWireMockServer.stubFor(
             WireMock.post("/SecurityTokenServiceProvider/")
                 .willReturn(
                     WireMock.aResponse()
                         .withStatus(200)
-                        .withHeader("Content-Type", "texy/xml")
+                        .withHeader("Content-Type", "text/xml")
                         .withBody(sts_response)
                 )
         )
@@ -80,29 +80,28 @@ class DokumentproduksjonConsumerIT(
         executeFromSystem {
             verifyHeaders(
                 mapOf<String, StringValuePattern>(
-//                    Pair("Authorization", WireMock.equalTo("Basic dGVzdDp0ZXN0")),
+                    Pair("SOAPAction", WireMock.equalTo("\"http://nav.no/tjeneste/virksomhet/dokumentproduksjon/v3/Dokumentproduksjon_v3/produserIkkeredigerbartDokumentRequest\"")),
                 )
             )
         }
     }
 
     @Test
-    fun authorizationSkalKommeFraBruker() {
+    fun  authorizationSkalKommeFraBruker() {
         securityWireMockServer.stubFor(
             WireMock.post("/SecurityTokenServiceProvider/")
                 .willReturn(
                     WireMock.aResponse()
                         .withStatus(200)
-                        .withHeader("Content-Type", "texy/xml")
+                        .withHeader("Content-Type", "text/xml")
                         .withBody(sts_response)
                 )
         )
 
-
         executeFromController {
             verifyHeaders(
                 mapOf<String, StringValuePattern>(
-//                    Pair("Authorization", WireMock.equalTo("Bearer --token-from-user--")),
+                    Pair("SOAPAction", WireMock.equalTo("\"http://nav.no/tjeneste/virksomhet/dokumentproduksjon/v3/Dokumentproduksjon_v3/produserIkkeredigerbartDokumentRequest\"")),
                 )
             )
         }
@@ -132,7 +131,7 @@ class DokumentproduksjonConsumerIT(
                     </ns3:produserIkkeredigerbartDokumentResponse>
                 </SOAP-ENV:Body>
             </SOAP-ENV:Envelope>
-        """
+        """.trimIndent()
     }
 
     override fun executeRequest() {
@@ -140,26 +139,26 @@ class DokumentproduksjonConsumerIT(
     }
 
     private val sts_response = """
-<?xml version="1.0" encoding="UTF-8"?>
-<soapenv:Envelope xmlns:wsa="http://www.w3.org/2005/08/addressing" xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/">
-    <soapenv:Header>
-        <wsa:MessageID>urn:uuid:5e22dd04-7a8e-494a-a05e-2fff16ecf883</wsa:MessageID>
-        <wsa:Action>http://docs.oasis-open.org/ws-sx/ws-trust/200512/RSTRC/IssueFinal</wsa:Action>
-        <wsa:To>http://www.w3.org/2005/08/addressing/anonymous</wsa:To>
-    </soapenv:Header>
-    <soapenv:Body>
-        <wst:RequestSecurityTokenResponseCollection xmlns:wst="http://docs.oasis-open.org/ws-sx/ws-trust/200512">
-            <wst:RequestSecurityTokenResponse Context="supportLater">
-             <wst:TokenType>http://docs.oasis-open.org/wss/oasis-wss-saml-token-profile-1.1#SAMLV2.0</wst:TokenType>
-                <wst:RequestedSecurityToken>
-                    <saml2:Assertion Version="2.0" ID="SAML-8d11de08-b17f-45ba-bd18-68098a4d28ce" IssueInstant="2018-09-06T10:28:45Z"
-                                     xmlns:saml2="urn:oasis:names:tc:SAML:2.0:assertion">
-                        <saml2:Issuer>SomeOne</saml2:Issuer>
-                    </saml2:Assertion>
-                </wst:RequestedSecurityToken>
-            </wst:RequestSecurityTokenResponse>
-        </wst:RequestSecurityTokenResponseCollection>
-    </soapenv:Body>
-</soapenv:Envelope>
-""".trimIndent()
+        <?xml version="1.0" encoding="UTF-8"?>
+        <soapenv:Envelope xmlns:wsa="http://www.w3.org/2005/08/addressing" xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/">
+            <soapenv:Header>
+                <wsa:MessageID>urn:uuid:5e22dd04-7a8e-494a-a05e-2fff16ecf883</wsa:MessageID>
+                <wsa:Action>http://docs.oasis-open.org/ws-sx/ws-trust/200512/RSTRC/IssueFinal</wsa:Action>
+                <wsa:To>http://www.w3.org/2005/08/addressing/anonymous</wsa:To>
+            </soapenv:Header>
+            <soapenv:Body>
+                <wst:RequestSecurityTokenResponseCollection xmlns:wst="http://docs.oasis-open.org/ws-sx/ws-trust/200512">
+                    <wst:RequestSecurityTokenResponse Context="supportLater">
+                     <wst:TokenType>http://docs.oasis-open.org/wss/oasis-wss-saml-token-profile-1.1#SAMLV2.0</wst:TokenType>
+                        <wst:RequestedSecurityToken>
+                            <saml2:Assertion Version="2.0" ID="SAML-8d11de08-b17f-45ba-bd18-68098a4d28ce" IssueInstant="2018-09-06T10:28:45Z"
+                                             xmlns:saml2="urn:oasis:names:tc:SAML:2.0:assertion">
+                                <saml2:Issuer>SomeOne</saml2:Issuer>
+                            </saml2:Assertion>
+                        </wst:RequestedSecurityToken>
+                    </wst:RequestSecurityTokenResponse>
+                </wst:RequestSecurityTokenResponseCollection>
+            </soapenv:Body>
+        </soapenv:Envelope>
+        """.trimIndent()
 }
