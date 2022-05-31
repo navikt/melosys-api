be12b26f0ab55d0bc506583ef8fd50514cc7625b
Francois Allix <32903762+frans-a@users.noreply.github.com>
2022-05-31 15:38:54 +0200

    Sett riktig trygdedekning ved mottak av A001
    Sett riktig trygdedekning ved mottak av anmodning om unntak, slik at
    lovvalsperioden som opprettes på bakgrunn av anmodningsperioden også får
     riktig dekning.

--- saksflyt/src/test/java/no/nav/melosys/saksflyt/steg/sed/OpprettAnmodningsperiodeFraSedTest.java
+++ saksflyt/src/test/java/no/nav/melosys/saksflyt/steg/sed/OpprettAnmodningsperiodeFraSedTest.java
@@ -1,14 +1,17 @@
 package no.nav.melosys.saksflyt.steg.sed;
 
 import java.time.LocalDate;
+import java.util.Collection;
 import java.util.Collections;
 
+import no.nav.melosys.domain.Anmodningsperiode;
 import no.nav.melosys.domain.Behandling;
 import no.nav.melosys.domain.Saksopplysning;
 import no.nav.melosys.domain.SaksopplysningType;
 import no.nav.melosys.domain.dokument.medlemskap.Periode;
 import no.nav.melosys.domain.dokument.sed.SedDokument;
 import no.nav.melosys.domain.kodeverk.Landkoder;
+import no.nav.melosys.domain.kodeverk.Trygdedekninger;
 import no.nav.melosys.domain.kodeverk.lovvalgsbestemmelser.Lovvalgbestemmelser_883_2004;
 import no.nav.melosys.domain.saksflyt.Prosessinstans;
 import no.nav.melosys.service.behandling.BehandlingService;
@@ -16,16 +19,19 @@ import no.nav.melosys.service.unntak.AnmodningsperiodeService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.mockito.ArgumentMatchers.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class OpprettAnmodningsperiodeFraSedTest {
-
     @Mock
     private AnmodningsperiodeService anmodningsperiodeService;
     @Mock
@@ -33,37 +39,72 @@ class OpprettAnmodningsperiodeFraSedTest {
 
     private OpprettAnmodningsperiodeFraSed opprettAnmodningsperiodeFraSed;
 
+    @Captor
+    private ArgumentCaptor<Collection<Anmodningsperiode>> argumentCaptor;
+
     @BeforeEach
     public void setup() {
-        opprettAnmodningsperiodeFraSed = new OpprettAnmodningsperiodeFraSed(anmodningsperiodeService, behandlingService);
+        opprettAnmodningsperiodeFraSed = new OpprettAnmodningsperiodeFraSed(anmodningsperiodeService,
+                                                                            behandlingService);
     }
 
     @Test
-    void utfør_medEksisterendeSedDokument_forventKall() {
+    void utfør_medSedHvorLovvalgslandErNorge_lagrerAnmodningsperiodeMedFullDekning() {
         Prosessinstans prosessinstans = new Prosessinstans();
         Behandling behandling = new Behandling();
         behandling.setId(1L);
         Saksopplysning saksopplysning = new Saksopplysning();
         saksopplysning.setType(SaksopplysningType.SEDOPPL);
-        saksopplysning.setDokument(lagSedDokument());
+        SedDokument sedDokument = lagSedDokument(Landkoder.NO);
+        saksopplysning.setDokument(sedDokument);
         behandling.setSaksopplysninger(Collections.singleton(saksopplysning));
         prosessinstans.setBehandling(behandling);
-
         when(behandlingService.hentBehandlingMedSaksopplysninger(anyLong())).thenReturn(behandling);
+
         opprettAnmodningsperiodeFraSed.utfør(prosessinstans);
 
-        verify(behandlingService).hentBehandlingMedSaksopplysninger(anyLong());
-        verify(anmodningsperiodeService).lagreAnmodningsperioder(eq(1L), any());
+        verify(anmodningsperiodeService).lagreAnmodningsperioder(eq(1L), argumentCaptor.capture());
+        assertThat(argumentCaptor.getValue()).contains(
+            lagForventetAnmodningsperiode(sedDokument, Trygdedekninger.FULL_DEKNING_EOSFO));
     }
 
-    private SedDokument lagSedDokument() {
+    @Test
+    void utfør_medSedHvorLovvalgslandIkkeErNorge_lagrerAnmodningsperiodeUtenDekning() {
+        Prosessinstans prosessinstans = new Prosessinstans();
+        Behandling behandling = new Behandling();
+        behandling.setId(1L);
+        Saksopplysning saksopplysning = new Saksopplysning();
+        saksopplysning.setType(SaksopplysningType.SEDOPPL);
+        SedDokument sedDokument = lagSedDokument(Landkoder.DE);
+        saksopplysning.setDokument(sedDokument);
+        behandling.setSaksopplysninger(Collections.singleton(saksopplysning));
+        prosessinstans.setBehandling(behandling);
+        when(behandlingService.hentBehandlingMedSaksopplysninger(anyLong())).thenReturn(behandling);
+
+        opprettAnmodningsperiodeFraSed.utfør(prosessinstans);
+
+        verify(anmodningsperiodeService).lagreAnmodningsperioder(eq(1L), argumentCaptor.capture());
+        assertThat(argumentCaptor.getValue()).contains(
+            lagForventetAnmodningsperiode(sedDokument, Trygdedekninger.UTEN_DEKNING));
+    }
+
+    private SedDokument lagSedDokument(Landkoder lovvalgslandKode) {
         SedDokument sedDokument = new SedDokument();
         sedDokument.setLovvalgsperiode(new Periode(LocalDate.now(), LocalDate.now().plusYears(1)));
         sedDokument.setLovvalgBestemmelse(Lovvalgbestemmelser_883_2004.FO_883_2004_ART16_1);
-        sedDokument.setLovvalgslandKode(Landkoder.DE);
+        sedDokument.setLovvalgslandKode(lovvalgslandKode);
         sedDokument.setUnntakFraLovvalgBestemmelse(Lovvalgbestemmelser_883_2004.FO_883_2004_ART12_1);
-        sedDokument.setUnntakFraLovvalgslandKode(Landkoder.NO);
+        sedDokument.setUnntakFraLovvalgslandKode(lovvalgslandKode == Landkoder.NO ? Landkoder.SE : Landkoder.NO);
 
         return sedDokument;
+    }
+
+    private static Anmodningsperiode lagForventetAnmodningsperiode(SedDokument sedDokument,
+                                                                   Trygdedekninger trygdedekning) {
+        return new Anmodningsperiode(sedDokument.getLovvalgsperiode().getFom(),
+                                     sedDokument.getLovvalgsperiode().getTom(), sedDokument.getLovvalgslandKode(),
+                                     sedDokument.getLovvalgBestemmelse(), null,
+                                     sedDokument.getUnntakFraLovvalgslandKode(),
+                                     sedDokument.getUnntakFraLovvalgBestemmelse(), trygdedekning);
     }
 }
