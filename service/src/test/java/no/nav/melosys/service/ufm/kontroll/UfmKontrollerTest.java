385362e0e22d9763cf7c03e6279f2f46c5f6a747
martonSkjevelandNav <marton.skjeveland@nav.no>
2022-06-01 09:43:09 +0200

    Unngår å bruke LocalDate.now() i test

--- service/src/test/java/no/nav/melosys/service/ufm/kontroll/UfmKontrollerTest.java
+++ service/src/test/java/no/nav/melosys/service/ufm/kontroll/UfmKontrollerTest.java
@@ -30,6 +30,8 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class UfmKontrollerTest {
+
+    private final LocalDate NOW = LocalDate.of(2022, 6, 20);
     @Test
     void feilIPeriode_erFeil_verifiserBegrunnelse() {
         assertThat(UfmKontroller.feilIPeriode(kontrollData(null, null))).isEqualTo(Kontroll_begrunnelser.FEIL_I_PERIODEN);
@@ -37,7 +39,7 @@ class UfmKontrollerTest {
 
     @Test
     void periodeErÅpen_erFeil_verifiserBegrunnelse() {
-        assertThat(UfmKontroller.periodeErÅpen(kontrollData(LocalDate.now(), null))).isEqualTo(Kontroll_begrunnelser.INGEN_SLUTTDATO);
+        assertThat(UfmKontroller.periodeErÅpen(kontrollData(NOW, null))).isEqualTo(Kontroll_begrunnelser.INGEN_SLUTTDATO);
     }
 
     @Test
@@ -52,7 +54,7 @@ class UfmKontrollerTest {
 
     @Test
     void periodeEldreEnn5År_erFeil_verifiserBegrunnelse() {
-        assertThat(UfmKontroller.periodeStarterFørFørsteJuni2012(kontrollData(LocalDate.now().minusYears(10), null))).isEqualTo(Kontroll_begrunnelser.PERIODE_FOR_GAMMEL);
+        assertThat(UfmKontroller.periodeStarterFørFørsteJuni2012(kontrollData(NOW.minusYears(10), null))).isEqualTo(Kontroll_begrunnelser.PERIODE_FOR_GAMMEL);
     }
 
     @Test
@@ -144,7 +146,7 @@ class UfmKontrollerTest {
     }
 
     private UfmKontrollData kontrollData() {
-        return kontrollData(LocalDate.now().plusMonths(15), LocalDate.now().plusYears(10));
+        return kontrollData(NOW.plusMonths(15), NOW.plusYears(10));
     }
 
     private UfmKontrollData kontrollData(LocalDate fom, LocalDate tom) {
@@ -164,13 +166,13 @@ class UfmKontrollerTest {
         sedDokument.setArbeidssteder(arbeidssteder);
 
         PersonDokument personDokument = new PersonDokument();
-        personDokument.setDødsdato(LocalDate.now());
+        personDokument.setDødsdato(NOW);
         personDokument.setBostedsadresse(new Bostedsadresse());
         personDokument.getBostedsadresse().setLand(new Land("NOR"));
 
         MedlemskapDokument medlemskapDokument = new MedlemskapDokument();
         Medlemsperiode medlemsperiode = new Medlemsperiode();
-        medlemsperiode.periode = new Periode(LocalDate.now(), LocalDate.now().plusYears(2));
+        medlemsperiode.periode = new Periode(NOW, NOW.plusYears(2));
         medlemsperiode.status = PeriodestatusMedl.UAVK.getKode();
         medlemskapDokument.getMedlemsperiode().add(medlemsperiode);
 
