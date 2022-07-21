10846cf7e12b2136ce168a7a693f9e2b8297bd9e
RuneLind <rulind@gmail.com>
2022-07-21 14:54:56 +0200

    Gjenbruk av OracleTestContainerBase

--- integrasjonstest/src/test/kotlin/no/nav/melosys/itest/ComponentTestBase.kt
+++ integrasjonstest/src/test/kotlin/no/nav/melosys/itest/ComponentTestBase.kt
@@ -6,12 +6,7 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.context.annotation.Import
 import org.springframework.test.context.ActiveProfiles
-import org.springframework.test.context.DynamicPropertyRegistry
-import org.springframework.test.context.DynamicPropertySource
-import org.testcontainers.containers.OracleContainer
-import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
-import org.testcontainers.utility.DockerImageName
 
 @Testcontainers
 @ActiveProfiles("test")
@@ -25,22 +20,4 @@ import org.testcontainers.utility.DockerImageName
     ComponentTestConfig::class
 )
 @EnableMockOAuth2Server
-class ComponentTestBase {
-    companion object {
-
-
-        @Container
-        var DB = OracleContainer(
-            DockerImageName.parse("ghcr.io/navikt/melosys-legacy-avhengigheter/oracle-xe:18.4.0-slim")
-                .asCompatibleSubstituteFor("gvenzl/oracle-xe")
-        )
-
-        @DynamicPropertySource
-        @JvmStatic
-        fun oracleProperties(registry: DynamicPropertyRegistry) {
-            registry.add("spring.datasource.url") { DB.jdbcUrl }
-            registry.add("spring.datasource.password") { DB.password }
-            registry.add("spring.datasource.username") { DB.username }
-        }
-    }
-}
+class ComponentTestBase : OracleTestContainerBase()
