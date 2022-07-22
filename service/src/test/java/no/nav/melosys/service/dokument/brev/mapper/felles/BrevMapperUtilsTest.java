c1598015e2b4e8f613901cd02a4f71dc04cf34b6
RuneLind <rulind@gmail.com>
2022-07-22 09:56:08 +0200

    Fjern println fra test

--- service/src/test/java/no/nav/melosys/service/dokument/brev/mapper/felles/BrevMapperUtilsTest.java
+++ service/src/test/java/no/nav/melosys/service/dokument/brev/mapper/felles/BrevMapperUtilsTest.java
@@ -13,8 +13,6 @@ class BrevMapperUtilsTest {
 
     @Test
     void convertToXMLGregorianCalendarRemoveTimezone() {
-        System.out.println("ZoneId.systemDefault()=" + ZoneId.systemDefault());
-
         LocalDate localDate = LocalDate.parse("2019-04-01");
         Instant april_1 = localDate.atStartOfDay(ZoneId.of("Europe/Paris")).toInstant();
         XMLGregorianCalendar xmlGregorianCalendar = BrevMapperUtils.convertToXMLGregorianCalendarRemoveTimezone(april_1);
