1e3ffb9a6ba48552940cf8919d57a5af866a9231
Ivan Skodje <ivan@tybo.no>
2022-07-26 11:32:59 +0200

    Tatt vekk Primary fra service som var unødvendig

--- service/src/main/java/no/nav/melosys/service/kontroll/feature/unntaksperiode/UnntaksperiodeKontrollService.java
+++ service/src/main/java/no/nav/melosys/service/kontroll/feature/unntaksperiode/UnntaksperiodeKontrollService.java
@@ -14,12 +14,10 @@ import no.nav.melosys.service.validering.Kontrollfeil;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Primary
 public class UnntaksperiodeKontrollService {
 
     private static final Logger log = LoggerFactory.getLogger(UnntaksperiodeKontrollService.class);
