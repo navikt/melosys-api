5a9569eacb6ea35dccb73ec73af200429dea4f91
RuneLind <rulind@gmail.com>
2022-07-27 17:45:21 +0200

    Legg @Retryable på metodene som også må åpnes
    Pga. https://github.com/spring-projects/spring-retry/issues/238

--- integrasjon/src/main/kotlin/no/nav/melosys/integrasjon/medl/MedlemskapRestConsumer.kt
+++ integrasjon/src/main/kotlin/no/nav/melosys/integrasjon/medl/MedlemskapRestConsumer.kt
@@ -6,12 +6,15 @@ import no.nav.melosys.integrasjon.medl.api.v1.MedlemskapsunntakForPut
 import no.nav.melosys.integrasjon.felles.RestConsumer
 import org.springframework.http.HttpMethod
 import org.springframework.http.MediaType
+import org.springframework.retry.annotation.Retryable
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.util.UriBuilder
 import java.time.LocalDate
 
 open class MedlemskapRestConsumer(private val webClient: WebClient) : RestConsumer {
-    fun hentPeriodeListe(fnr: String, fom: LocalDate, tom: LocalDate): List<MedlemskapsunntakForGet> {
+    var i: Int = 0
+    @Retryable
+    open fun hentPeriodeListe(fnr: String, fom: LocalDate, tom: LocalDate): List<MedlemskapsunntakForGet> {
         return hentMedlemskapsunntakForPeriode(fnr, fom, tom)!!.toList()
     }
 
@@ -35,16 +38,19 @@ open class MedlemskapRestConsumer(private val webClient: WebClient) : RestConsum
             .block()!!
     }
 
-    fun hentPeriode(periodeId: String?) = webClient.get()
+    @Retryable
+    open fun hentPeriode(periodeId: String?) = webClient.get()
         .uri("/{periodeId}?inkluderSporingsinfo={inkluderSporingsinfo}", periodeId, true)
         .accept(MediaType.APPLICATION_JSON)
         .retrieve()
         .bodyToMono(MedlemskapsunntakForGet::class.java)
         .block()!!
 
-    fun opprettPeriode(request: MedlemskapsunntakForPost) = utfør(request, HttpMethod.POST)
+    @Retryable
+    open fun opprettPeriode(request: MedlemskapsunntakForPost) = utfør(request, HttpMethod.POST)
 
-    fun oppdaterPeriode(request: MedlemskapsunntakForPut) = utfør(request, HttpMethod.PUT)
+    @Retryable
+    open fun oppdaterPeriode(request: MedlemskapsunntakForPut) = utfør(request, HttpMethod.PUT)
 
     private fun utfør(request: Any, method: HttpMethod) = webClient.method(method)
         .accept(MediaType.APPLICATION_JSON)
