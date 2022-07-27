c8213f15fc7f37a464847f3657daf38477d9dab0
RuneLind <rulind@gmail.com>
2022-07-27 21:06:23 +0200

    Fjerne unødvedig bruk av @Primary

--- integrasjon/src/main/kotlin/no/nav/melosys/integrasjon/medl/MedlemskapRestConsumerProducer.kt
+++ integrasjon/src/main/kotlin/no/nav/melosys/integrasjon/medl/MedlemskapRestConsumerProducer.kt
@@ -6,17 +6,15 @@ import no.nav.melosys.integrasjon.felles.WebClientConfig
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Primary
 import org.springframework.web.reactive.function.client.ClientRequest
 import org.springframework.web.reactive.function.client.ExchangeFilterFunction
 import org.springframework.web.reactive.function.client.WebClient
 import reactor.core.publisher.Mono
 
 @Configuration
-class MedlemskapRestConsumerProducer(@Value("\${medlemskap.rest.url}") private val url: String) : RestConsumer,
-    WebClientConfig {
+class MedlemskapRestConsumerProducer(@Value("\${medlemskap.rest.url}") private val url: String) :
+    RestConsumer, WebClientConfig {
     @Bean
-    @Primary
     fun medlemskapRestConsumer(
         webClientBuilder: WebClient.Builder,
         genericContextExchangeFilter: GenericContextExchangeFilter
