e64a54fa818b93c76cce14445927130f3daa95b6
RuneLind <rulind@gmail.com>
2022-07-27 12:55:19 +0200

    MedlemskapsunntakForPost til data class

--- integrasjon/src/main/kotlin/no/nav/melosys/integrasjon/medl/api/v1/MedlemskapsunntakForPost.kt
+++ integrasjon/src/main/kotlin/no/nav/melosys/integrasjon/medl/api/v1/MedlemskapsunntakForPost.kt
@@ -4,20 +4,20 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import java.time.LocalDate
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-class MedlemskapsunntakForPost {
-    var ident: String? = null
-    var fraOgMed: LocalDate? = null
-    var tilOgMed: LocalDate? = null
-    var status: String? = null
-    var statusaarsak: String? = null
-    var dekning: String? = null
-    var lovvalgsland: String? = null
-    var lovvalg: String? = null
-    var grunnlag: String? = null
+data class MedlemskapsunntakForPost(
+    var ident: String? = null,
+    var fraOgMed: LocalDate? = null,
+    var tilOgMed: LocalDate? = null,
+    var status: String? = null,
+    var statusaarsak: String? = null,
+    var dekning: String? = null,
+    var lovvalgsland: String? = null,
+    var lovvalg: String? = null,
+    var grunnlag: String? = null,
     var sporingsinformasjon: SporingsinformasjonForPost? = null
-
+) {
     @JsonIgnoreProperties(ignoreUnknown = true)
-    class SporingsinformasjonForPost {
+    data class SporingsinformasjonForPost(
         var kildedokument: String? = null
-    }
+    )
 }
