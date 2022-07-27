3385b0a98868bbc6debc97087257cfb89dc238fc
RuneLind <rulind@gmail.com>
2022-07-27 12:26:52 +0200

    Refaktoriser MedlemskapRestConsumer

--- integrasjon/src/main/kotlin/no/nav/melosys/integrasjon/medl/MedlemskapRestConsumer.kt
+++ integrasjon/src/main/kotlin/no/nav/melosys/integrasjon/medl/MedlemskapRestConsumer.kt
@@ -14,21 +14,20 @@ import java.time.LocalDate
 @Retryable
 class MedlemskapRestConsumer(private val webClient: WebClient) : RestConsumer {
     fun hentPeriodeListe(fnr: String, fom: LocalDate, tom: LocalDate): List<MedlemskapsunntakForGet> {
-        return hentPeriodeListe(fnr, fom, tom, "")!!.toList()
+        return hentMedlemskapsunntakForPeriode(fnr, fom, tom)!!.toList()
     }
 
-    private fun hentPeriodeListe(
+    private fun hentMedlemskapsunntakForPeriode(
         fnr: String,
         fom: LocalDate,
-        tom: LocalDate,
-        eksluderteKilder: String
+        tom: LocalDate
     ): Array<MedlemskapsunntakForGet>? {
         return webClient.get().uri("") { uriBuilder: UriBuilder ->
             uriBuilder
                 .queryParam("fraOgMed", fom)
                 .queryParam("tilOgMed", tom)
                 .queryParam("inkluderSporingsinfo", true)
-                .queryParam("ekskluderKilder", eksluderteKilder)
+                .queryParam("ekskluderKilder", "")
                 .build()
         }
             .header("Nav-Personident", fnr)
@@ -38,29 +37,29 @@ class MedlemskapRestConsumer(private val webClient: WebClient) : RestConsumer {
             .block()
     }
 
-    fun hentPeriode(periodeId: String?): MedlemskapsunntakForGet? {
+    fun hentPeriode(periodeId: String?): MedlemskapsunntakForGet {
         return webClient.get()
             .uri("/{periodeId}?inkluderSporingsinfo={inkluderSporingsinfo}", periodeId, true)
             .accept(MediaType.APPLICATION_JSON)
             .retrieve()
             .bodyToMono(MedlemskapsunntakForGet::class.java)
-            .block()
+            .block()!!
     }
 
-    fun opprettPeriode(request: MedlemskapsunntakForPost): MedlemskapsunntakForGet? {
+    fun opprettPeriode(request: MedlemskapsunntakForPost): MedlemskapsunntakForGet {
         return utfør(request, HttpMethod.POST)
     }
 
-    fun oppdaterPeriode(request: MedlemskapsunntakForPut): MedlemskapsunntakForGet? {
+    fun oppdaterPeriode(request: MedlemskapsunntakForPut): MedlemskapsunntakForGet {
         return utfør(request, HttpMethod.PUT)
     }
 
-    private fun utfør(request: Any, method: HttpMethod): MedlemskapsunntakForGet? {
+    private fun utfør(request: Any, method: HttpMethod): MedlemskapsunntakForGet {
         return webClient.method(method)
             .accept(MediaType.APPLICATION_JSON)
             .bodyValue(request)
             .retrieve()
             .bodyToMono(MedlemskapsunntakForGet::class.java)
-            .block()
+            .block()!!
     }
 }
