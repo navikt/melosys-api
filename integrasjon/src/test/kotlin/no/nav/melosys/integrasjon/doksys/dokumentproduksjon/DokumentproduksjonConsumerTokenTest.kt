7d70eeebb564dddeed7f14de755410dd48cc8739
RuneLind <rulind@gmail.com>
2022-07-28 17:02:59 +0200

    Ta med StsProdConfig for å se at wiring blir riktig

--- integrasjon/src/test/kotlin/no/nav/melosys/integrasjon/doksys/dokumentproduksjon/DokumentproduksjonConsumerTokenTest.kt
+++ integrasjon/src/test/kotlin/no/nav/melosys/integrasjon/doksys/dokumentproduksjon/DokumentproduksjonConsumerTokenTest.kt
@@ -20,7 +20,7 @@ import org.springframework.test.context.ActiveProfiles
         DokumentproduksjonConsumerProducer::class
     ]
 )
-@Import(StsLoginConfig::class, StsProdConfig::class)
+@Import(StsLoginConfig::class, StsProdConfig::class, StsProdConfig::class)
 @ActiveProfiles("wiremock-test")
 class DokumentproduksjonConsumerTokenTest(
     @Autowired private val dokumentproduksjonConsumer: DokumentproduksjonConsumer,
