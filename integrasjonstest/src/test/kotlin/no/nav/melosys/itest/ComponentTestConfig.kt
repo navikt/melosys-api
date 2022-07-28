d191f13182fab8e93d2f3a4adcd78e6432c404e9
RuneLind <rulind@gmail.com>
2022-07-28 17:04:36 +0200

    Fjren System property: spring.profiles.active

--- integrasjonstest/src/test/kotlin/no/nav/melosys/itest/ComponentTestConfig.kt
+++ integrasjonstest/src/test/kotlin/no/nav/melosys/itest/ComponentTestConfig.kt
@@ -28,7 +28,6 @@ class ComponentTestConfig {
     companion object {
         init {
             System.setProperty("kafkaPort", SocketUtils.findAvailableTcpPort(60000, 65535).toString())
-            System.setProperty("spring.profiles.active", "test")
         }
     }
 
