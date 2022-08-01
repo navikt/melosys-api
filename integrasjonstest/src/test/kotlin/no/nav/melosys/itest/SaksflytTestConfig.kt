aacc97376c1e2d8a1a9b6153ee021a19ffb9ec18
Francois Allix <32903762+frans-a@users.noreply.github.com>
2022-08-01 15:21:47 +0200

    Fiks Kafka testconfig

--- integrasjonstest/src/test/kotlin/no/nav/melosys/itest/SaksflytTestConfig.kt
+++ integrasjonstest/src/test/kotlin/no/nav/melosys/itest/SaksflytTestConfig.kt
@@ -2,18 +2,8 @@ package no.nav.melosys.itest
 
 import no.nav.melosys.melosysmock.config.GraphqlConfig
 import org.springframework.boot.SpringBootConfiguration
-import org.springframework.boot.autoconfigure.domain.EntityScan
-import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa
 import org.springframework.context.annotation.Import
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @SpringBootConfiguration
-@EnableJpaRepositories("no.nav.melosys.repository")
-@AutoConfigureDataJpa
-@EntityScan("no.nav.melosys.domain")
-@Import(GraphqlConfig::class)
-class SaksflytTestConfig {
-    init {
-        System.setProperty("kafkaPort", "0")
-    }
-}
+@Import(GraphqlConfig::class, KafkaTestConfig::class)
+class SaksflytTestConfig
