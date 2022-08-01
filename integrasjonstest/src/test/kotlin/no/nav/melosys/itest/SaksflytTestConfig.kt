2a868278a742b02bfcce669c8f1882bfd8e8010c
Francois Allix <32903762+frans-a@users.noreply.github.com>
2022-08-01 16:15:38 +0200

    SpringBootConfiguration ble plukket
    av BehandlingsresultatServiceIT

--- integrasjonstest/src/test/kotlin/no/nav/melosys/itest/SaksflytTestConfig.kt
+++ integrasjonstest/src/test/kotlin/no/nav/melosys/itest/SaksflytTestConfig.kt
@@ -1,9 +1,9 @@
 package no.nav.melosys.itest
 
 import no.nav.melosys.melosysmock.config.GraphqlConfig
-import org.springframework.boot.SpringBootConfiguration
+import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Import
 
-@SpringBootConfiguration
+@TestConfiguration
 @Import(GraphqlConfig::class, KafkaTestConfig::class)
 class SaksflytTestConfig
