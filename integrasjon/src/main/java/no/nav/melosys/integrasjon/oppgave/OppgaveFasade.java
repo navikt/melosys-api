3722ce0cc585748434b84d67a72d66d43c612432
Francois Allix <32903762+frans-a@users.noreply.github.com>
2022-08-09 10:22:16 +0200

    Fjern unødvendige kommentarer

--- integrasjon/src/main/java/no/nav/melosys/integrasjon/oppgave/OppgaveFasade.java
+++ integrasjon/src/main/java/no/nav/melosys/integrasjon/oppgave/OppgaveFasade.java
@@ -9,66 +9,29 @@ public interface OppgaveFasade {
 
     void feilregistrerOppgaver(Set<String> oppgaveIdSet);
 
-    /**
-     * Ferdigstiller en opprettet oppgave i Oppgave
-     */
     void ferdigstillOppgave(String oppgaveId);
 
-    /**
-     * Finner aktive og utildelte oppgaver som svarer til noen gitt kriterier.
-     * Oppgave sorterer oppgavene stigende etter frist.
-     */
     List<Oppgave> finnUtildelteOppgaverEtterFrist(String behandlingstype, String behandlingstema);
 
     void oppdaterOppgave(String oppgaveID, OppgaveOppdatering oppgaveOppdatering);
 
-    /**
-     * Finner Oppgaver basert på ansvarlig saksbehandler
-     * Oppgave sorterer oppgavene stigende etter frist.
-     */
-    Set<Oppgave> finnOppgaverMedAnsvarlig(String ansvarligId);
+    Set<Oppgave> finnOppgaverMedAnsvarlig(String ansvarligSaksbehandlerID);
 
-    /**
-     * Finner oppgaver relatert til en bruker.
-     * Oppgaver sorteres stigende etter frist.
-     */
     List<Oppgave> finnOppgaverMedAktørId(String aktørID);
 
-    /**
-     * Finner oppgaver relatert til en virksomhet.
-     * Oppgaver sorteres stigende etter frist.
-     */
     List<Oppgave> finnOppgaverMedOrgnr(String orgnr);
 
     List<Oppgave> finnÅpneOppgaverMedJournalpostID(String journalpostID);
 
-    /**
-     * Finner alle åpne oppgaver med gitt saksnummer.
-     */
     List<Oppgave> finnÅpneOppgaverMedSaksnummer(String saksnummer);
 
-    /**
-     * Finner alle oppgaver med gitt saksnummer.
-     */
     List<Oppgave> finnAvsluttetOppgaverMedSaksnummer(String saksnummer);
 
-    /**
-     * Hent oppgave fra Oppgave på en gitt oppgaveId
-     */
     Oppgave hentOppgave(String oppgaveId);
 
-    /**
-     * Oppretter en oppgave for Melosys i Oppgave og returnerer en unik oppgaveId
-     */
     String opprettOppgave(Oppgave oppgave);
 
-    /**
-     * Oppretter en oppgave for NAV Viken i Oppgave og returnerer en unik oppgaveId
-     */
     String opprettSensitivOppgave(Oppgave oppgave);
 
-    /**
-     * Legger tilbake en oppgave i Oppgave
-     */
     void leggTilbakeOppgave(String oppgaveId);
 }
