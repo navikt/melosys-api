bb1880371844d7176c6dd0839b04ee7b7cc1e49b
Marton Skjæveland <56435736+martonSkjevelandNav@users.noreply.github.com>
2022-08-15 14:04:06 +0200

    5220: FIX Sender hele oppgaveobjektet til oppgave-api for å patche status (#1322)
    * Sender hele oppgaveobjektet til oppgavea-pi for å patche status
    
    * Fjernet nå ikke brukt patch request
    
    * Bruker eksisterende oppdaterOppgave endepunkt fremfor patch

--- integrasjon/src/main/java/no/nav/melosys/integrasjon/oppgave/konsument/dto/OppgaveDto.java
+++ integrasjon/src/main/java/no/nav/melosys/integrasjon/oppgave/konsument/dto/OppgaveDto.java
@@ -4,6 +4,7 @@ import java.time.ZonedDateTime;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import no.nav.melosys.domain.oppgave.Oppgave;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class OppgaveDto extends OpprettOppgaveDto {
@@ -52,5 +53,30 @@ public class OppgaveDto extends OpprettOppgaveDto {
             ", status='" + status + '\'' +
             ", versjon=" + versjon +
             "} " + super.toString();
+    }
+
+    public static OppgaveDto av(Oppgave oppgave) {
+        var oppgaveDto = new OppgaveDto();
+        oppgaveDto.setId(oppgave.getOppgaveId());
+        oppgaveDto.setStatus(oppgave.getStatus());
+        oppgaveDto.setVersjon(oppgave.getVersjon());
+        oppgaveDto.setOpprettetTidspunkt(oppgave.getOpprettetTidspunkt());
+        oppgaveDto.setAktivDato(oppgave.getAktivDato());
+        oppgaveDto.setAktørId(oppgave.getAktørId());
+        oppgaveDto.setOrgnr(oppgave.getOrgnr());
+        oppgaveDto.setBehandlesAvApplikasjon(oppgave.getBehandlesAvApplikasjon().getKode());
+        oppgaveDto.setBehandlingstema(oppgave.getBehandlingstema());
+        oppgaveDto.setBehandlingstype(oppgave.getBehandlingstype());
+        oppgaveDto.setBeskrivelse(oppgave.getBeskrivelse());
+        oppgaveDto.setFristFerdigstillelse(oppgave.getFristFerdigstillelse());
+        oppgaveDto.setJournalpostId(oppgave.getJournalpostId());
+        oppgaveDto.setOppgavetype(oppgave.getOppgavetype().getKode());
+        oppgaveDto.setPrioritet(oppgave.getPrioritet().toString());
+        oppgaveDto.setSaksreferanse(oppgave.getSaksnummer());
+        oppgaveDto.setTema(oppgave.getTema().getKode());
+        oppgaveDto.setTemagruppe(oppgave.getTemagruppe());
+        oppgaveDto.setTildeltEnhetsnr(oppgave.getTildeltEnhetsnr());
+        oppgaveDto.setTilordnetRessurs(oppgave.getTilordnetRessurs());
+        return oppgaveDto;
     }
 }
