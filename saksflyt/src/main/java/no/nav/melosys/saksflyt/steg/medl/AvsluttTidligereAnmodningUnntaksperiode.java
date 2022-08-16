57ee1d97aac45536dabeb805014621c2c22a9e2f
Ivan Skodje <ivan@tybo.no>
2022-08-16 16:25:25 +0200

    Tatt vekk log

--- saksflyt/src/main/java/no/nav/melosys/saksflyt/steg/medl/AvsluttTidligereAnmodningUnntaksperiode.java
+++ saksflyt/src/main/java/no/nav/melosys/saksflyt/steg/medl/AvsluttTidligereAnmodningUnntaksperiode.java
@@ -6,15 +6,10 @@ import no.nav.melosys.domain.saksflyt.ProsessSteg;
 import no.nav.melosys.domain.saksflyt.Prosessinstans;
 import no.nav.melosys.saksflyt.steg.StegBehandler;
 import no.nav.melosys.service.medl.MedlPeriodeService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AvsluttTidligereAnmodningUnntaksperiode implements StegBehandler {
-
-    private static final Logger log = LoggerFactory.getLogger(AvsluttTidligereAnmodningUnntaksperiode.class);
-
     private final MedlPeriodeService medlPeriodeService;
 
     public AvsluttTidligereAnmodningUnntaksperiode(MedlPeriodeService medlPeriodeService) {
@@ -28,13 +23,10 @@ public class AvsluttTidligereAnmodningUnntaksperiode implements StegBehandler {
 
     @Override
     public void utfør(Prosessinstans prosessinstans) {
-        log.info("AvsluttTidligereAnmodningUnntaksperiode: start");
         Behandling nyBehandling = prosessinstans.getBehandling();
         if (erOppdatertA001(nyBehandling, prosessinstans)) {
             medlPeriodeService.avsluttTidligereAnmodningsperiode(nyBehandling);
-            log.info("Avsluttet tidligere periode for behandlingID {}", nyBehandling.getId());
         }
-        log.info("AvsluttTidligereAnmodningUnntaksperiode: slutt");
     }
 
     private boolean erOppdatertA001(Behandling behandling, Prosessinstans prosessinstans) {
