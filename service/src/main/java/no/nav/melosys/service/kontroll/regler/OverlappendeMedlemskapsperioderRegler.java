1f7db531e97db14bc9e2a0fcbcc0bca639cd6af4
Ivan Skodje <ivan@tybo.no>
2022-08-25 08:28:57 +0200

    Lagt til detaljerte logger i OverlappendeMedlemskapsperioderRegler for å feilsøke

--- service/src/main/java/no/nav/melosys/service/kontroll/regler/OverlappendeMedlemskapsperioderRegler.java
+++ service/src/main/java/no/nav/melosys/service/kontroll/regler/OverlappendeMedlemskapsperioderRegler.java
@@ -1,11 +1,17 @@
 package no.nav.melosys.service.kontroll.regler;
 
+import java.util.function.Predicate;
+
 import no.nav.melosys.domain.ErPeriode;
 import no.nav.melosys.domain.PeriodeOmLovvalg;
 import no.nav.melosys.domain.dokument.medlemskap.MedlemskapDokument;
+import no.nav.melosys.domain.dokument.medlemskap.Medlemsperiode;
 import no.nav.melosys.integrasjon.medl.PeriodestatusMedl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public final class OverlappendeMedlemskapsperioderRegler {
+    private static final Logger log = LoggerFactory.getLogger(OverlappendeMedlemskapsperioderRegler.class);
 
     public static boolean harOverlappendeMedlemsperiodeFraSed(MedlemskapDokument medlemskapDokument,
                                                               ErPeriode kontrollperiode) {
@@ -24,11 +30,25 @@ public final class OverlappendeMedlemskapsperioderRegler {
     public static boolean harOverlappendeMedlemsperiode(MedlemskapDokument medlemskapDokument,
                                                         PeriodeOmLovvalg kontrollperiode,
                                                         PeriodeOmLovvalg opprinneligPeriodeTilKontrollperiode) {
-        return medlemskapDokument.hentMedlemsperioderHvorKildeIkkeLånekassen().stream().anyMatch(
-            medlemsperiode -> !PeriodestatusMedl.AVST.getKode().equals(medlemsperiode.status)
-                && PeriodeRegler.periodeOverlapper(kontrollperiode, medlemsperiode.getPeriode())
-                && (kontrollperiode.erNyPeriodeForMedl() || kontrollperiode.harForskjelligMedlID(medlemsperiode.id))
-                && (opprinneligPeriodeTilKontrollperiode == null || opprinneligPeriodeTilKontrollperiode.harForskjelligMedlID(medlemsperiode.id)
-            ));
+
+        Predicate<Medlemsperiode> medlemsperiodePredicate = medlemsperiode -> {
+            boolean erIkkeAvvist = !PeriodestatusMedl.AVST.getKode().equals(medlemsperiode.status);
+            log.info("[harOverlappendeMedlemsperiode] erIkkeAvvist: {}", erIkkeAvvist);
+            boolean periodeOverlapper = PeriodeRegler.periodeOverlapper(kontrollperiode, medlemsperiode.getPeriode());
+            log.info("[harOverlappendeMedlemsperiode] periodeOverlapper: {}", periodeOverlapper);
+            boolean erNyPeriode = kontrollperiode.erNyPeriodeForMedl() || kontrollperiode.harForskjelligMedlID(medlemsperiode.id);
+            log.info("[harOverlappendeMedlemsperiode] erNyPeriode: {}", erNyPeriode);
+            boolean erOpprinneligPeriodeForskjellig = opprinneligPeriodeTilKontrollperiode == null || opprinneligPeriodeTilKontrollperiode.harForskjelligMedlID(medlemsperiode.id);
+            log.info("[harOverlappendeMedlemsperiode] erOpprinneligPeriodeForskjellig: {}", erOpprinneligPeriodeForskjellig);
+            boolean harOverlappendeMedlemsperiode = erIkkeAvvist
+                && periodeOverlapper
+                && erNyPeriode
+                && erOpprinneligPeriodeForskjellig;
+            log.info("[harOverlappendeMedlemsperiode] harOverlappendeMedlemsperiode: {}", harOverlappendeMedlemsperiode);
+            return harOverlappendeMedlemsperiode;
+        };
+        return medlemskapDokument.hentMedlemsperioderHvorKildeIkkeLånekassen()
+            .stream()
+            .anyMatch(medlemsperiodePredicate);
     }
 }
