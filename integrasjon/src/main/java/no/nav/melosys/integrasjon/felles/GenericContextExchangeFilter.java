d3e5e22f037d262b2d186db8df2921b9bc480ccd
Isa Salem Hame <wanghattan@gmail.com>
2022-09-06 08:26:53 +0200

    medl scope

--- integrasjon/src/main/java/no/nav/melosys/integrasjon/felles/GenericContextExchangeFilter.java
+++ integrasjon/src/main/java/no/nav/melosys/integrasjon/felles/GenericContextExchangeFilter.java
@@ -47,13 +47,14 @@ public class GenericContextExchangeFilter implements ExchangeFilterFunction {
             scope = "api://dev-fss.oppgavehandtering.oppgave-q1/.default";
         } else if (clientRequest.url().getHost().toString().contains("saf")) {
             scope = "api://dev-fss.teamdokumenthandtering.saf/.default";
+        } else if (clientRequest.url().getHost().toString().contains("medl")) {
+            scope = "api://dev-fss.team-rocket.medlemskap-medl-api-q1/.default";
         }
-
 
         String issuedToken = scope.isEmpty() ? oidcTokenString : azureADConsumer.hentToken(oidcTokenString, scope);
 
         System.out.println("Kaller på " + clientRequest.url().toString() + ". Scope for dette er: \"" + scope + "\"");
-        
+
         return exchangeFunction.exchange(
             ClientRequest.from(clientRequest)
                 .header(HttpHeaders.AUTHORIZATION, "Bearer " + issuedToken)
