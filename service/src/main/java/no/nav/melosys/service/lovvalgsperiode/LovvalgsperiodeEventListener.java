b09b2a0a85d4ad8945d0c4814efd20156c1066d2
Ivan Skodje <ivanskodje@protonmail.com>
2022-09-21 15:33:20 +0200

    Tatt vekk test-logg

--- service/src/main/java/no/nav/melosys/service/lovvalgsperiode/LovvalgsperiodeEventListener.java
+++ service/src/main/java/no/nav/melosys/service/lovvalgsperiode/LovvalgsperiodeEventListener.java
@@ -3,15 +3,12 @@ package no.nav.melosys.service.lovvalgsperiode;
 import no.nav.melosys.domain.LovvalgsperiodeLagreEvent;
 import no.nav.melosys.service.LovvalgsperiodeService;
 import no.nav.melosys.sikkerhet.context.ThreadLocalAccessInfo;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class LovvalgsperiodeEventListener {
 
-    private static final Logger log = LoggerFactory.getLogger(LovvalgsperiodeEventListener.class);
     private final LovvalgsperiodeService lovvalgsperiodeService;
 
     public LovvalgsperiodeEventListener(LovvalgsperiodeService lovvalgsperiodeService) {
@@ -20,8 +17,10 @@ public class LovvalgsperiodeEventListener {
 
     @EventListener
     public void lagreLovvalgsperiode(LovvalgsperiodeLagreEvent lovvalgsperiodeLagreEvent) {
-        log.debug("[Event] lagreLovvalgsperiode: {}", lovvalgsperiodeLagreEvent.toString());
-        ThreadLocalAccessInfo.executeProcess("lagreLovvalgsperiode", () -> lovvalgsperiodeService.lagreLovvalgsperioder(lovvalgsperiodeLagreEvent.getBehandlingId(),
-            lovvalgsperiodeLagreEvent.getLovvalgsperioder()));
+        ThreadLocalAccessInfo.executeProcess("lagreLovvalgsperiode", () ->
+            lovvalgsperiodeService.lagreLovvalgsperioder(
+                lovvalgsperiodeLagreEvent.getBehandlingId(),
+                lovvalgsperiodeLagreEvent.getLovvalgsperioder())
+        );
     }
 }
