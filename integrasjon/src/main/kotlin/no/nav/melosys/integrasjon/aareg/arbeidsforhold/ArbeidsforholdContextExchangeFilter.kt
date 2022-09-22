5bf26b71444a56460e2422b328ffe398d60e99eb
RuneLind <rulind@gmail.com>
2022-09-22 14:42:47 +0200

    Bruk GenericContextExchangeFilter for Arbeidsforhold

--- integrasjon/src/main/kotlin/no/nav/melosys/integrasjon/aareg/arbeidsforhold/ArbeidsforholdContextExchangeFilter.kt
+++ integrasjon/src/main/kotlin/no/nav/melosys/integrasjon/aareg/arbeidsforhold/ArbeidsforholdContextExchangeFilter.kt
@@ -1,33 +1,22 @@
 package no.nav.melosys.integrasjon.aareg.arbeidsforhold
 
+import no.nav.melosys.integrasjon.felles.GenericContextExchangeFilter
 import no.nav.melosys.integrasjon.reststs.RestStsClient
 import org.springframework.http.HttpHeaders
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.ClientRequest
-import org.springframework.web.reactive.function.client.ClientResponse
-import org.springframework.web.reactive.function.client.ExchangeFilterFunction
-import org.springframework.web.reactive.function.client.ExchangeFunction
-import reactor.core.publisher.Mono
-import java.util.function.Supplier
 
 @Component
-class ArbeidsforholdContextExchangeFilter(private val restStsClient: RestStsClient) : ExchangeFilterFunction {
-    override fun filter(
-        clientRequest: ClientRequest,
-        exchangeFunction: ExchangeFunction
-    ): Mono<ClientResponse> {
-        return exchangeFunction.exchange(
-            ClientRequest.from(clientRequest)
-                .header(HttpHeaders.AUTHORIZATION, getTokenSupplier(restStsClient).get())
-                .header(NAV_CONSUMER_TOKEN, restStsClient.bearerToken())
-                .build()
-        )
-    }
+class ArbeidsforholdContextExchangeFilter(restStsClient: RestStsClient) :
+    GenericContextExchangeFilter(restStsClient) {
 
-    private fun getTokenSupplier(restStsClient: RestStsClient): Supplier<String> {
-        // Om vi får lagt inn "0000-ga-aa-register-konsument" i sakbehandler token kan vi benytte dette når tilgjengelig
-        // https://nav-it.slack.com/archives/C01BSCJM127/p1649411252534409
-        return Supplier { restStsClient.bearerToken() }
+    override fun createClientRequest(clientRequest: ClientRequest): ClientRequest {
+        return ClientRequest.from(clientRequest)
+            // Om vi får lagt inn "0000-ga-aa-register-konsument" i sakbehandler token kan vi benytte dette når tilgjengelig
+            // https://nav-it.slack.com/archives/C01BSCJM127/p1649411252534409
+            .header(HttpHeaders.AUTHORIZATION, systemToken)
+            .header(NAV_CONSUMER_TOKEN, systemToken)
+            .build()
     }
 
     companion object {
