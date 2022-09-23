d214d5e8799fd440c78f24df9dadca7879e47272
Ivan Skodje <ivanskodje@protonmail.com>
2022-09-23 13:18:53 +0200

    SQL for å ta vekk prefix som er hardkodet til databasen

---
+++ app/src/main/resources/db/migration/melosysDB/V27__ta-vekk-prefix.sql
@@ -0,0 +1,7 @@
+UPDATE BEHANDLING_TEMA
+SET NAVN='Henvendelse eller forespørsel'
+WHERE NAVN = 'MED: Henvendelse eller forespørsel';
+
+UPDATE BEHANDLING_TEMA
+SET NAVN='Henvendelse eller forespørsel'
+WHERE NAVN = 'UFM: Henvendelse eller forespørsel';
