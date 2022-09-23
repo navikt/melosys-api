c1b6dc7cf2ff1f58fcc7977ee167551314a47c3b
Ivan Skodje <ivanskodje@protonmail.com>
2022-09-23 14:28:21 +0200

    Review tweaks

--- app/src/main/resources/db/migration/melosysDB/V27__ta-vekk-prefix.sql
+++ app/src/main/resources/db/migration/melosysDB/V27__ta-vekk-prefix.sql
@@ -1,7 +1,7 @@
 UPDATE BEHANDLING_TEMA
 SET NAVN='Henvendelse eller forespørsel'
-WHERE NAVN = 'MED: Henvendelse eller forespørsel';
+WHERE KODE = 'ØVRIGE_SED_MED';
 
 UPDATE BEHANDLING_TEMA
 SET NAVN='Henvendelse eller forespørsel'
-WHERE NAVN = 'UFM: Henvendelse eller forespørsel';
+WHERE KODE = 'ØVRIGE_SED_UFM';
