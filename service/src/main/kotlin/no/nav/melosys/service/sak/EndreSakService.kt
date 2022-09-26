8c0bd7ea1f457efbf153f213bb80749af51fcad4
Francois Allix <32903762+frans-a@users.noreply.github.com>
2022-09-26 14:11:04 +0200

    Validering + oppfrisking av saksopplysninger

--- service/src/main/kotlin/no/nav/melosys/service/sak/EndreSakService.kt
+++ service/src/main/kotlin/no/nav/melosys/service/sak/EndreSakService.kt
@@ -1,13 +1,45 @@
 package no.nav.melosys.service.sak
 
+import no.nav.melosys.domain.Fagsak
+import no.nav.melosys.domain.VedtakMetadataLagretEvent
 import no.nav.melosys.domain.kodeverk.Sakstemaer
 import no.nav.melosys.domain.kodeverk.Sakstyper
+import no.nav.melosys.exception.FunksjonellException
+import no.nav.melosys.service.behandlingsgrunnlag.BehandlingsgrunnlagService
+import no.nav.melosys.service.saksopplysninger.OppfriskSaksopplysningerService
+import org.springframework.context.ApplicationEventPublisher
 import org.springframework.stereotype.Service
+import javax.transaction.Transactional
 
 @Service
-class EndreSakService(private val fagsakService: FagsakService) {
+class EndreSakService(
+    private val fagsakService: FagsakService,
+    private val behandlingsgrunnlagService: BehandlingsgrunnlagService,
+    private val oppfriskSaksopplysningerService: OppfriskSaksopplysningerService,
+    private val applicationEventPublisher: ApplicationEventPublisher
+) {
+    @Transactional
     fun endre(saksnummer: String, sakstype: Sakstyper, sakstema: Sakstemaer) {
+        val fagsak = fagsakService.hentFagsak(saksnummer)
+        valider(fagsak, sakstype, sakstema)
+
+        // TODO søknad -> fra behandlingsgrunnlagService
         fagsakService.oppdaterSakstype(saksnummer, sakstype)
         fagsakService.oppdaterSakstema(saksnummer, sakstema)
+        oppfriskSaksopplysningerService.oppfriskSaksopplysning(fagsak.hentAktivBehandling().id, false)
+
+        // TODO oppdater oppgave
+        applicationEventPublisher.publishEvent(VedtakMetadataLagretEvent(1L))
+    }
+
+    private fun valider(
+        fagsak: Fagsak, sakstype: Sakstyper, sakstema: Sakstemaer
+    ) {
+        if (fagsak.type == sakstype && fagsak.tema == sakstema) {
+            throw FunksjonellException("Sak ${fagsak.saksnummer} har allerede type ${fagsak.type} og tema ${fagsak.tema}")
+        }
+        if (!fagsak.kanEndres()) {
+            throw FunksjonellException("Sak ${fagsak.saksnummer} kan ikke endres")
+        }
     }
 }
--- service/src/test/kotlin/no/nav/melosys/service/sak/EndreSakServiceTest.kt
+++ service/src/test/kotlin/no/nav/melosys/service/sak/EndreSakServiceTest.kt
@@ -1,14 +1,19 @@
 package no.nav.melosys.service.sak
 
+import io.mockk.every
 import io.mockk.impl.annotations.RelaxedMockK
 import io.mockk.junit5.MockKExtension
 import io.mockk.verify
 import no.nav.melosys.domain.kodeverk.Sakstemaer.TRYGDEAVGIFT
 import no.nav.melosys.domain.kodeverk.Sakstyper.EU_EOS
+import no.nav.melosys.service.SaksbehandlingDataFactory
+import no.nav.melosys.service.behandlingsgrunnlag.BehandlingsgrunnlagService
+import no.nav.melosys.service.saksopplysninger.OppfriskSaksopplysningerService
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.context.ApplicationEventPublisher
 
 @ExtendWith(MockKExtension::class)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -16,20 +21,35 @@ internal class EndreSakServiceTest {
     @RelaxedMockK
     lateinit var fagsakService: FagsakService
 
+    @RelaxedMockK
+    lateinit var behandlingsgrunnlagService: BehandlingsgrunnlagService
+
+    @RelaxedMockK
+    lateinit var oppfriskSaksopplysningerService: OppfriskSaksopplysningerService
+
+    @RelaxedMockK
+    lateinit var applicationEventPublisher: ApplicationEventPublisher
+
     lateinit var endreSakService: EndreSakService
 
     @BeforeEach
     fun setUp() {
-        endreSakService = EndreSakService(fagsakService)
+        endreSakService = EndreSakService(fagsakService, behandlingsgrunnlagService, oppfriskSaksopplysningerService, applicationEventPublisher)
     }
 
     @Test
-    fun endre() {
+    fun `endring av sak - oppdater type og tema, opprett ny søknad og oppfrisk saksopplysninger`() {
         val saksnummer = "MEL-123"
+        val fagsak = SaksbehandlingDataFactory.lagFagsak(saksnummer)
+        fagsak.behandlinger.add(SaksbehandlingDataFactory.lagBehandling(fagsak))
+        every { fagsakService.hentFagsak(saksnummer) } returns fagsak
 
         endreSakService.endre(saksnummer, EU_EOS, TRYGDEAVGIFT)
 
-        verify {fagsakService.oppdaterSakstype(saksnummer, EU_EOS)}
-        verify {fagsakService.oppdaterSakstema(saksnummer, TRYGDEAVGIFT)}
+        verify { fagsakService.oppdaterSakstype(saksnummer, EU_EOS) }
+        verify { fagsakService.oppdaterSakstema(saksnummer, TRYGDEAVGIFT) }
+        verify { oppfriskSaksopplysningerService.oppfriskSaksopplysning(fagsak.behandlinger[0].id, false) }
+        // event for å oppdatere oppgave
+        verify { applicationEventPublisher.publishEvent(any()) }
     }
 }
