39813073db75c3537edb2e6f8248a984c2b42c6e
RuneLind <rulind@gmail.com>
2022-09-28 15:18:17 +0200

    Refaktorering av SaksbehandlingRegler.harTomFlyt

--- service/src/main/kotlin/no/nav/melosys/service/saksbehandling/SaksbehandlingRegler.kt
+++ service/src/main/kotlin/no/nav/melosys/service/saksbehandling/SaksbehandlingRegler.kt
@@ -58,10 +58,8 @@ class SaksbehandlingRegler(private val behandlingsresultatRepository: Behandling
             sakstype: Sakstyper,
             behandlingstype: Behandlingstyper,
             behandlingstema: Behandlingstema
-        ): Boolean {
-            if (behandlingstype == Behandlingstyper.HENVENDELSE || behandlingstype == Behandlingstyper.KLAGE) return true
-
-            return when (behandlingstema) {
+        ): Boolean =
+            when (behandlingstema) {
                 ARBEID_KUN_NORGE,
                 IKKE_YRKESAKTIV,
                 PENSJONIST,
@@ -75,8 +73,7 @@ class SaksbehandlingRegler(private val behandlingsresultatRepository: Behandling
                 ANMODNING_OM_UNNTAK_HOVEDREGEL -> sakstype == Sakstyper.TRYGDEAVTALE
                 YRKESAKTIV -> sakstype == Sakstyper.FTRL
 
-                else -> return false
+                else -> behandlingstype == Behandlingstyper.HENVENDELSE || behandlingstype == Behandlingstyper.KLAGE
             }
-        }
     }
 }
