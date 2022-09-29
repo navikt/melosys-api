2c168fae46019ade03606da97ebd61d537d1686f
Ivan Skodje <ivanskodje@protonmail.com>
2022-09-29 15:22:24 +0200

    Lagt til ny logikk som sjekker sedDokument først, og deretter behandlingsgrunnlag.
    * Lagt på toggle

--- frontend-api/src/main/java/no/nav/melosys/tjenester/gui/sak/FagsakTjeneste.java
+++ frontend-api/src/main/java/no/nav/melosys/tjenester/gui/sak/FagsakTjeneste.java
@@ -233,7 +233,7 @@ public class FagsakTjeneste {
         return behandlingOversiktDto;
     }
 
-    private void setPeriodeOpplysninger(Behandling behandling, BehandlingOversiktDto behandlingOversiktDto) {
+    void setPeriodeOpplysninger(Behandling behandling, BehandlingOversiktDto behandlingOversiktDto) {
         if (unleash.isEnabled("melosys.behandle_alle_saker")) {
             var optionalSedDokument = saksopplysningerService.finnSedOpplysninger(behandling.getId());
 
