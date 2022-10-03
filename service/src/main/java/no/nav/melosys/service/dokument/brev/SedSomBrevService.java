38a1badb5141d2113e5e7379413c5709f50cf9d1
Eirik Bergande <eirik.bergande@nav.no>
2022-10-03 13:08:24 +0200

    tester

--- service/src/main/java/no/nav/melosys/service/dokument/brev/SedSomBrevService.java
+++ service/src/main/java/no/nav/melosys/service/dokument/brev/SedSomBrevService.java
@@ -3,6 +3,7 @@ package no.nav.melosys.service.dokument.brev;
 import java.util.Collections;
 import java.util.List;
 
+import no.finn.unleash.Unleash;
 import no.nav.melosys.domain.Behandling;
 import no.nav.melosys.domain.TemaFactory;
 import no.nav.melosys.domain.arkiv.FysiskDokument;
@@ -22,21 +23,23 @@ public class SedSomBrevService {
     private final JoarkFasade joarkFasade;
     private final PersondataFasade persondataFasade;
     private final UtenlandskMyndighetService utenlandskMyndighetService;
+    private final Unleash unleash;
 
     public SedSomBrevService(EessiService eessiService,
                              JoarkFasade joarkFasade,
                              PersondataFasade persondataFasade,
-                             UtenlandskMyndighetService utenlandskMyndighetService) {
+                             UtenlandskMyndighetService utenlandskMyndighetService,
+                             Unleash unleash) {
         this.eessiService = eessiService;
         this.joarkFasade = joarkFasade;
         this.persondataFasade = persondataFasade;
         this.utenlandskMyndighetService = utenlandskMyndighetService;
+        this.unleash = unleash;
     }
 
     public String lagJournalpostForSendingAvSedSomBrev(SedType sedType,
                                                        Landkoder mottakerland,
-                                                       Behandling behandling)
-        {
+                                                       Behandling behandling) {
         return lagJournalpostForSendingAvSedSomBrev(sedType, mottakerland, behandling, Collections.emptyList());
     }
 
@@ -49,8 +52,9 @@ public class SedSomBrevService {
         String institusjonID = utenlandskMyndighetService.lagInstitusjonsId(utenlandskMyndighet);
         String brukerFnr = persondataFasade.hentFolkeregisterident(fagsak.hentBrukersAktørID());
         byte[] sedPdf = eessiService.genererSedPdf(behandling.getId(), sedType);
-//        var tema = TemaFactory.fraBehandlingstema(behandling.getTema());
-        var tema = OppgaveFactory.utledTema(behandling.getFagsak().getTema());
+
+        boolean behandleAlleSakerEnabled = unleash.isEnabled("melosys.behandle_alle_saker");
+        var tema = behandleAlleSakerEnabled ? OppgaveFactory.utledTema(fagsak.getTema()) : TemaFactory.fraBehandlingstema(behandling.getTema());
 
         OpprettJournalpost opprettJournalpost = OpprettJournalpost.lagJournalpostForSendingAvSedSomBrev(
             fagsak.getSaksnummer(), brukerFnr, sedType, sedPdf, institusjonID,
---
+++ service/src/test/kotlin/no/nav/melosys/service/dokument/brev/SedSomBrevServiceTest.kt
@@ -0,0 +1,94 @@
+package no.nav.melosys.service.dokument.brev
+
+import io.kotest.matchers.shouldBe
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.impl.annotations.RelaxedMockK
+import io.mockk.junit5.MockKExtension
+import io.mockk.mockk
+import io.mockk.verify
+import no.finn.unleash.FakeUnleash
+import no.nav.melosys.domain.Behandling
+import no.nav.melosys.domain.Fagsak
+import no.nav.melosys.domain.Tema
+import no.nav.melosys.domain.UtenlandskMyndighet
+import no.nav.melosys.domain.eessi.SedType
+import no.nav.melosys.domain.kodeverk.Landkoder
+import no.nav.melosys.domain.kodeverk.Sakstemaer
+import no.nav.melosys.domain.kodeverk.behandlinger.Behandlingsstatus
+import no.nav.melosys.domain.kodeverk.behandlinger.Behandlingstema
+import no.nav.melosys.integrasjon.joark.JoarkFasade
+import no.nav.melosys.service.aktoer.UtenlandskMyndighetService
+import no.nav.melosys.service.dokument.sed.EessiService
+import no.nav.melosys.service.persondata.PersondataFasade
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import kotlin.test.assertEquals
+
+@ExtendWith(MockKExtension::class)
+class SedSomBrevServiceTest {
+
+    @RelaxedMockK
+    lateinit var eessiServiceMock: EessiService
+
+    @RelaxedMockK
+    lateinit var joarkFasadeMock: JoarkFasade
+
+    @MockK
+    lateinit var persondataFasadeMock: PersondataFasade
+
+    @MockK
+    lateinit var utenlandskMyndighetServiceMock: UtenlandskMyndighetService
+
+    val unleash = FakeUnleash()
+
+    lateinit var sedSomBrevService: SedSomBrevService
+
+    @BeforeEach
+    internal fun setUp() {
+        sedSomBrevService = SedSomBrevService(
+            eessiServiceMock,
+            joarkFasadeMock,
+            persondataFasadeMock,
+            utenlandskMyndighetServiceMock,
+            unleash
+        )
+    }
+
+    @Test
+    fun name() {
+        unleash.enable("melosys.behandle_alle_saker")
+        val fagsak: Fagsak = mockk<Fagsak>()
+        val behandling = Behandling()
+        behandling.id = 123
+        behandling.tema = Behandlingstema.YRKESAKTIV
+        behandling.status = Behandlingsstatus.UNDER_BEHANDLING
+        behandling.fagsak = fagsak
+
+        val utenlandskMyndighet: UtenlandskMyndighet = mockk<UtenlandskMyndighet>()
+
+        every { utenlandskMyndighetServiceMock.hentUtenlandskMyndighet(Landkoder.SE) } returns utenlandskMyndighet
+        every { utenlandskMyndighetServiceMock.lagInstitusjonsId(utenlandskMyndighet) } returns INSTITUSJONS_ID
+        every { fagsak.hentBrukersAktørID() } returns AKTØR_ID
+        every { persondataFasadeMock.hentFolkeregisterident(any()) } returns BRUKER_FNR
+        every { fagsak.getSaksnummer() } returns SAKSNUMMER
+        every { fagsak.tema } returns Sakstemaer.MEDLEMSKAP_LOVVALG
+
+        sedSomBrevService.lagJournalpostForSendingAvSedSomBrev(SedType.A002, Landkoder.SE, behandling, null)
+
+        verify {
+            joarkFasadeMock.opprettJournalpost(withArg {opprettJournalpost ->
+                opprettJournalpost.tema.shouldBe(Tema.MED.name)
+            }, true)
+        }
+    }
+
+    companion object {
+        val INSTITUSJONS_ID = "5544"
+        val BRUKER_FNR = "12345678922"
+        val AKTØR_ID = "551122"
+        val SAKSNUMMER= "789456"
+    }
+
+}
