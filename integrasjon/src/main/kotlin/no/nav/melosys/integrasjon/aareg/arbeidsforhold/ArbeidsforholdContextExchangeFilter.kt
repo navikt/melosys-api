a918619aabef20cc8df70eef4c1f175ad051c8e7
RuneLind <rulind@gmail.com>
2022-10-10 16:02:20 +0200

    Aareg skal ikke å bruke OAuth2AccessTokenService
    Siden tjenesten bare blir kalt med system token

--- integrasjon/src/main/kotlin/no/nav/melosys/integrasjon/aareg/arbeidsforhold/ArbeidsforholdContextExchangeFilter.kt
+++ integrasjon/src/main/kotlin/no/nav/melosys/integrasjon/aareg/arbeidsforhold/ArbeidsforholdContextExchangeFilter.kt
@@ -1,26 +1,34 @@
 package no.nav.melosys.integrasjon.aareg.arbeidsforhold
 
-import no.nav.melosys.integrasjon.felles.GenericContextExchangeFilter
 import no.nav.melosys.integrasjon.reststs.RestStsClient
-import no.nav.security.token.support.client.core.oauth2.OAuth2AccessTokenService
-import no.nav.security.token.support.client.spring.ClientConfigurationProperties
 import org.springframework.http.HttpHeaders
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.ClientRequest
+import org.springframework.web.reactive.function.client.ClientResponse
+import org.springframework.web.reactive.function.client.ExchangeFilterFunction
+import org.springframework.web.reactive.function.client.ExchangeFunction
+import reactor.core.publisher.Mono
+import java.util.function.Supplier
 
 @Component
-class ArbeidsforholdContextExchangeFilter(restStsClient: RestStsClient,
-                                          clientConfigurationProperties: ClientConfigurationProperties,
-                                          oAuth2AccessTokenService: OAuth2AccessTokenService
-) :
-    GenericContextExchangeFilter(restStsClient, clientConfigurationProperties, oAuth2AccessTokenService, "") {
+class ArbeidsforholdContextExchangeFilter(private val restStsClient: RestStsClient) : ExchangeFilterFunction {
+    override fun filter(
+        clientRequest: ClientRequest,
+        exchangeFunction: ExchangeFunction,
+    ): Mono<ClientResponse> {
+        return exchangeFunction.exchange(
+            ClientRequest.from(clientRequest)
+                .header(HttpHeaders.AUTHORIZATION, getTokenSupplier(restStsClient).get())
+                .header(NAV_CONSUMER_TOKEN, restStsClient.bearerToken())
+                .build()
+        )
+    }
 
-    override fun withClientRequestBuilder(clientRequestBuilder: ClientRequest.Builder): ClientRequest.Builder =
-        clientRequestBuilder
-            // Om vi får lagt inn "0000-ga-aa-register-konsument" i sakbehandler token kan vi benytte dette når tilgjengelig
-            // https://nav-it.slack.com/archives/C01BSCJM127/p1649411252534409
-            .header(HttpHeaders.AUTHORIZATION, systemToken)
-            .header(NAV_CONSUMER_TOKEN, systemToken)
+    private fun getTokenSupplier(restStsClient: RestStsClient): Supplier<String> {
+        // Om vi får lagt inn "0000-ga-aa-register-konsument" i sakbehandler token kan vi benytte dette når tilgjengelig
+        // https://nav-it.slack.com/archives/C01BSCJM127/p1649411252534409
+        return Supplier { restStsClient.bearerToken() }
+    }
 
     companion object {
         private const val NAV_CONSUMER_TOKEN = "Nav-Consumer-Token"
--- integrasjon/src/test/kotlin/no/nav/melosys/integrasjon/aareg/AaregConsumerTokenTest.kt
+++ integrasjon/src/test/kotlin/no/nav/melosys/integrasjon/aareg/AaregConsumerTokenTest.kt
@@ -5,28 +5,29 @@ import com.github.tomakehurst.wiremock.matching.StringValuePattern
 import no.nav.melosys.integrasjon.ConsumerWireMockTestBase
 import no.nav.melosys.integrasjon.aareg.arbeidsforhold.*
 import no.nav.melosys.integrasjon.reststs.RestTokenServiceClient
-import no.nav.melosys.integrasjon.reststs.StsRestTemplateProducer
+import no.nav.melosys.integrasjon.reststs.StsWebClientProducer
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.autoconfigure.web.client.RestTemplateAutoConfiguration
 import org.springframework.boot.test.autoconfigure.web.client.AutoConfigureWebClient
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.context.annotation.Import
 import org.springframework.test.context.ActiveProfiles
 
-@WebMvcTest(
-    value = [
-        StsRestTemplateProducer::class,
-        RestTokenServiceClient::class,
-
-        ArbeidsforholdRestConsumer::class,
-        ArbeidsforholdRestConsumerConfig::class,
-        ArbeidsforholdContextExchangeFilter::class
-    ]
+@Import(
+    StsWebClientProducer::class,
+    RestTokenServiceClient::class,
+    RestTemplateAutoConfiguration::class,
+    ArbeidsforholdRestConsumer::class,
+    ArbeidsforholdRestConsumerConfig::class,
+    ArbeidsforholdContextExchangeFilter::class
 )
+@WebMvcTest
 @ActiveProfiles("wiremock-test")
 @AutoConfigureWebClient
-class AaregConsumerTokenTest(
+private class AaregConsumerTokenTest(
     @Autowired private val arbeidsforholdRestConsumer: ArbeidsforholdRestConsumer,
     @Value("\${mockserver.port}") mockServiceUnderTestPort: Int,
     @Value("\${mockserver.security.port}") mockSecurityPort: Int
