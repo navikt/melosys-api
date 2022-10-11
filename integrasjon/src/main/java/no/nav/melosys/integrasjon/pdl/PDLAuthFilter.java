77de37fc57d188bd9ceede1a153aff2ccd901b9c
Isa Salem Hame <wanghattan@gmail.com>
2022-10-11 13:42:56 +0200

    Fikser PDLAuthFilter etter merge

--- integrasjon/src/main/java/no/nav/melosys/integrasjon/pdl/PDLAuthFilter.java
+++ integrasjon/src/main/java/no/nav/melosys/integrasjon/pdl/PDLAuthFilter.java
@@ -2,6 +2,9 @@ package no.nav.melosys.integrasjon.pdl;
 
 import no.nav.melosys.integrasjon.felles.GenericContextExchangeFilter;
 import no.nav.melosys.integrasjon.reststs.RestStsClient;
+import no.nav.security.token.support.client.core.ClientProperties;
+import no.nav.security.token.support.client.core.oauth2.OAuth2AccessTokenService;
+import no.nav.security.token.support.client.spring.ClientConfigurationProperties;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.ClientRequest;
 
@@ -9,8 +12,9 @@ import org.springframework.web.reactive.function.client.ClientRequest;
 public class PDLAuthFilter extends GenericContextExchangeFilter {
     private static final String NAV_CONSUMER_TOKEN = "Nav-Consumer-Token";
 
-    public PDLAuthFilter(RestStsClient restStsClient) {
-        super(restStsClient);
+    static String CLIENT_NAME = "pdl";
+    public PDLAuthFilter(RestStsClient restStsClient, ClientConfigurationProperties clientConfigurationProperties, OAuth2AccessTokenService oAuth2AccessTokenService) {
+        super(restStsClient, clientConfigurationProperties, oAuth2AccessTokenService, CLIENT_NAME);
     }
 
     @Override
