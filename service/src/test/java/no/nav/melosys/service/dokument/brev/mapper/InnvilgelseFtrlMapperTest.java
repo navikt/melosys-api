4fc0a970137fdabe92e20a052aa5ae37c0d63a97
Francois Allix <32903762+frans-a@users.noreply.github.com>
2022-10-18 14:45:06 +0200

    Nytt kodeverk trygdeavtaleland

--- service/src/test/java/no/nav/melosys/service/dokument/brev/mapper/InnvilgelseFtrlMapperTest.java
+++ service/src/test/java/no/nav/melosys/service/dokument/brev/mapper/InnvilgelseFtrlMapperTest.java
@@ -50,10 +50,9 @@ import static no.nav.melosys.domain.kodeverk.begrunnelser.folketrygdloven.Ftrl_2
 import static no.nav.melosys.domain.kodeverk.begrunnelser.folketrygdloven.Medfolgende_barn_begrunnelser_ftrl.IKKE_SOEKERS_BARN;
 import static no.nav.melosys.domain.kodeverk.begrunnelser.folketrygdloven.Medfolgende_ektefelle_samboer_begrunnelser_ftrl.IKKE_TRE_AV_FEM_SISTE_ÅR;
 import static no.nav.melosys.service.dokument.DokgenTestData.*;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.when;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 
 @ExtendWith(MockitoExtension.class)
@@ -131,7 +130,7 @@ class InnvilgelseFtrlMapperTest {
         assertThat(innvilgelseFtrl.getSaksbehandlerNavn()).isEqualTo(SAKSBEHANDLER_NAVN);
         assertThat(innvilgelseFtrl.getArbeidsgiverNavn()).isEqualTo(ARBEIDSGIVER_NAVN);
         assertThat(innvilgelseFtrl.getArbeidsland()).isEqualTo(Landkoder.AT.getBeskrivelse());
-        assertThat(innvilgelseFtrl.isTrygdeavtaleMedArbeidsland()).isFalse();
+        assertThat(innvilgelseFtrl.isTrygdeavtaleMedArbeidsland()).isTrue();
         assertThat(innvilgelseFtrl.getVurderingTrygdeavgift()).isNotNull();
         assertThat(innvilgelseFtrl.getVurderingTrygdeavgift().selvbetalende()).isFalse();
         assertThat(innvilgelseFtrl.getVurderingTrygdeavgift().representantNavn()).isEqualTo(REPRESENTANT_NAVN);
