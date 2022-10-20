919907ee4af6e87431dc1f38965a9ef38119e73d
Eirik Bergande <eirik.bergande@nav.no>
2022-10-20 12:46:10 +0200

    Endrer landkode til Land_ISO2 (trygdeavtale + eu) ifm brev

---
+++ domain/src/main/java/no/nav/melosys/domain/util/Land_ISO2.java
@@ -0,0 +1,69 @@
+package no.nav.melosys.domain.util;
+
+public enum Land {
+
+    AU("Australia"),
+    BA("Bosnia-Hercegovina"),
+    CA("Canada"),
+    CL("Chile"),
+    FR("Frankrike"),
+    GR("Hellas"),
+    IN("India"),
+    IL("Israel"),
+    IM("Isle of Man"),
+    IT("Italia"),
+    JE("Jersey"),
+    HR("Kroatia"),
+    LU("Luxembourg"),
+    ME("Montenegro"),
+    NL("Nederland"),
+    PT("Portugal"),
+    CA_QC("Quebec"),
+    RS("Serbia"),
+    SI("Slovenia"),
+    GB("Storbritannia"),
+    CH("Sveits"),
+    TR("Tyrkia"),
+    HU("Ungarn"),
+    US("USA"),
+    AT("Østerrike"),
+    BE("Belgia"),
+    BG("Bulgaria"),
+    DK("Danmark"),
+    EE("Estland"),
+    FI("Finland"),
+    FO("Færøyene"),
+    GL("Grønland"),
+    IE("Irland"),
+    IS("Island"),
+    CY("Kypros"),
+    LV("Latvia"),
+    LI("Liechtenstein"),
+    LT("Litauen"),
+    MT("Malta"),
+    NO("Norge"),
+    PL("Polen"),
+    RO("Romania"),
+    SK("Slovakia"),
+    ES("Spania"),
+    SJ("Svalbard og Jan Mayen"),
+    SE("Sverige"),
+    CZ("Tsjekkia"),
+    DE("Tyskland"),
+    AX("Åland");
+
+
+    private final String beskrivelse;
+
+    private Land(String beskrivelse) {
+        this.beskrivelse = beskrivelse;
+    }
+
+    public String getKode() {
+        return this.name();
+    }
+
+    public String getBeskrivelse() {
+        return this.beskrivelse;
+    }
+}
