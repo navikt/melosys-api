bf7f64d845e4c5a49d1f09299e93f5b15659a749
Janita Johansen <janita.marielle.johansen@nav.no>
2022-10-20 16:35:43 +0200

    Skiller OPPRETT_BEHANDLING_FOR_SAK i to ulike prosessflyter basert på om eksisterende behandling skal replikeres eller ikke: OPPRETT_REPLIKERT_BEHANDLING_FOR_SAK og OPPRETT_NY_BEHANDLING_FOR_SAK. Oppdaterer databasen. Skriver tester. SaksbehandlingRegler.kt sin skalTidligereBehandlingReplikeres tok inn fagsak så brukte denne til å finne sakstype og sakstema istedenfor å gå gjennom behandling. Flyttet validering ned i service-klassen

--- service/src/main/java/no/nav/melosys/service/sak/OpprettBehandlingForSak.java
+++ service/src/main/java/no/nav/melosys/service/sak/OpprettBehandlingForSak.java
@@ -2,6 +2,7 @@ package no.nav.melosys.service.sak;
 
 import no.nav.melosys.domain.Fagsak;
 import no.nav.melosys.exception.FunksjonellException;
+import no.nav.melosys.service.saksbehandling.SaksbehandlingRegler;
 import no.nav.melosys.service.saksflyt.ProsessinstansService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,19 +11,42 @@ import org.springframework.transaction.annotation.Transactional;
 public class OpprettBehandlingForSak {
     private final FagsakService fagsakService;
     private final ProsessinstansService prosessinstansService;
+    private final SaksbehandlingRegler saksbehandlingRegler;
 
-    public OpprettBehandlingForSak(FagsakService fagsakService, ProsessinstansService prosessinstansService) {
+    public OpprettBehandlingForSak(FagsakService fagsakService,
+                                   ProsessinstansService prosessinstansService,
+                                   SaksbehandlingRegler saksbehandlingRegler) {
         this.fagsakService = fagsakService;
         this.prosessinstansService = prosessinstansService;
+        this.saksbehandlingRegler = saksbehandlingRegler;
     }
 
     @Transactional
     public void opprettBehandling(String saksnummer, OpprettSakDto opprettSakDto) {
         Fagsak fagsak = fagsakService.hentFagsak(saksnummer);
 
-        if (fagsak.hentAktivBehandling() != null) {
-            throw new FunksjonellException(String.format("Det finnes allerede en aktiv behandling på fagsak %s", saksnummer));
+        valider(fagsak, opprettSakDto);
+
+        if (saksbehandlingRegler.skalTidligereBehandlingReplikeres(fagsak, opprettSakDto.getBehandlingstype(), opprettSakDto.getBehandlingstema())) {
+            prosessinstansService.opprettOgReplikerBehandlingForSak(saksnummer, opprettSakDto);
+        } else {
+            prosessinstansService.opprettNyBehandlingForSak(saksnummer, opprettSakDto);
         }
-        prosessinstansService.opprettNyBehandlingForSak(saksnummer, opprettSakDto);
+    }
+
+    private void valider(Fagsak fagsak, OpprettSakDto opprettSakDto) {
+        if (fagsak.hentAktivBehandling() != null) {
+            throw new FunksjonellException(String.format("Det finnes allerede en aktiv behandling på fagsak %s", fagsak.getSaksnummer()));
+        }
+
+        if (opprettSakDto.getBrukerID() == null && opprettSakDto.getVirksomhetOrgnr() == null) {
+            throw new FunksjonellException("BrukerID eller organisasjonsnummer trengs for å opprette en behandling.");
+        }
+        if (opprettSakDto.getBehandlingstema() == null) {
+            throw new FunksjonellException("Behandlingstema mangler");
+        }
+        if (opprettSakDto.getBehandlingstype() == null) {
+            throw new FunksjonellException("Behandlingstype mangler");
+        }
     }
 }
