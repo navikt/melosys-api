355778ae8e5624ef221cc6b7f5258cea052ef3e4
RuneLind <rulind@gmail.com>
2022-10-21 12:27:09 +0200

    Legg til MetricsTestConfig
    For å sjekke at metrics url blir riktig

---
+++ integrasjon/src/test/kotlin/no/nav/melosys/integrasjon/MetricsTestConfig.kt
@@ -0,0 +1,50 @@
+package no.nav.melosys.integrasjon
+
+import io.kotest.matchers.collections.shouldHaveSize
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.string.shouldContain
+import io.micrometer.core.instrument.simple.SimpleMeterRegistry
+import org.springframework.boot.actuate.metrics.AutoTimer
+import org.springframework.boot.actuate.metrics.web.reactive.client.DefaultWebClientExchangeTagsProvider
+import org.springframework.boot.actuate.metrics.web.reactive.client.MetricsWebClientFilterFunction
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.context.annotation.Bean
+import org.springframework.web.reactive.function.client.WebClient
+
+@TestConfiguration
+class MetricsTestConfig() {
+
+    @Bean
+    fun myBuilder(): WebClient.Builder {
+        val metricsWebClientFilterFunction = MetricsWebClientFilterFunction(
+            meterRegistry, DefaultWebClientExchangeTagsProvider(), "test", AutoTimer.ENABLED
+        )
+        return WebClient.builder().filters { it.add((metricsWebClientFilterFunction)) }
+    }
+
+    companion object {
+        var meterRegistry = SimpleMeterRegistry()
+        fun clearMeterRegistry() {
+            meterRegistry.clear()
+        }
+
+        fun checkMetricsUri(uri: String) {
+            meterRegistry.meters
+                .shouldHaveSize(1)
+                .first().id.apply {
+                    name.shouldBe("test")
+                    this.getTag("uri")
+                        .shouldBe(uri)
+                }
+        }
+        fun metricsUriShouldContainBrackets() {
+            meterRegistry.meters
+                .shouldHaveSize(1)
+                .first().id.apply {
+                    name.shouldBe("test")
+                    this.getTag("uri")
+                        .shouldContain("{")
+                }
+        }
+    }
+}
