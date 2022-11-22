1d6f8db2049813143e3a886287ea93b6197973d7
Marton Skjæveland <56435736+martonSkjevelandNav@users.noreply.github.com>
2022-11-22 10:02:56 +0100

    5455:SQL: Lagt til støtte for to gateadresselinjer for utenlandske myndigheter (#1485)
    Lagt til støtte for to gateadresselinjer for utenlandske myndigheter for å bruke i brevmottaker

---
+++ app/src/main/resources/db/migration/melosysDB/V39__utenlandsk_trygdemyndighet_adresselinje2.sql
@@ -0,0 +1,18 @@
+ALTER TABLE utenlandsk_myndighet
+    RENAME COLUMN
+        GATEADRESSE
+        TO
+        GATEADRESSE_1;
+
+ALTER TABLE utenlandsk_myndighet
+    ADD GATEADRESSE_2 VARCHAR2(99);
+
+UPDATE utenlandsk_myndighet
+set gateadresse_1 = 'North East England',
+    gateadresse_2 = 'HM Revenue and Customs'
+where id = 33;
+
+UPDATE utenlandsk_myndighet
+set gateadresse_1 = 'Social Security Administration',
+    gateadresse_2 = '4700 Annex Building, 6401 Security Blvd'
+where id = 34;
