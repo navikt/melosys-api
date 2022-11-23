6900aff3630a49d9b73090446911e6191c03d5e0
Janita Johansen <janita.marielle.johansen@nav.no>
2022-11-23 14:57:22 +0100

    Legger til ny tjeneste BehandlingsaarsakTjeneste som henter utledetMottaksdato

---
+++ frontend-api/src/main/java/no/nav/melosys/tjenester/gui/BehandlingsaarsakTjeneste.java
@@ -0,0 +1,41 @@
+package no.nav.melosys.tjenester.gui;
+
+import io.swagger.annotations.ApiOperation;
+import no.nav.melosys.service.behandling.BehandlingService;
+import no.nav.melosys.service.behandling.UtledMottaksdato;
+import no.nav.melosys.service.tilgang.Aksesskontroll;
+import no.nav.melosys.tjenester.gui.dto.MottaksdatoDto;
+import no.nav.security.token.support.core.api.Protected;
+import org.springframework.context.annotation.Scope;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.WebApplicationContext;
+
+@Protected
+@RestController
+@RequestMapping("/behandlingsaarsak")
+@Scope(value = WebApplicationContext.SCOPE_REQUEST)
+public class BehandlingsaarsakTjeneste {
+
+    private final Aksesskontroll aksesskontroll;
+    private final UtledMottaksdato utledMottaksdato;
+    private final BehandlingService behandlingService;
+
+    public BehandlingsaarsakTjeneste(Aksesskontroll aksesskontroll, UtledMottaksdato utledMottaksdato, BehandlingService behandlingService) {
+        this.aksesskontroll = aksesskontroll;
+        this.utledMottaksdato = utledMottaksdato;
+        this.behandlingService = behandlingService;
+    }
+
+    @GetMapping("/{behandlingID}/mottaksdato")
+    @ApiOperation(value = "Henter mottaksdato fra behandlingsårsak, eller bruk eksisterende logikk for å finne alternativ mottaksdato")
+    public ResponseEntity<MottaksdatoDto> hentMottaksdato(@PathVariable("behandlingID") long behandlingID) {
+        aksesskontroll.autoriser(behandlingID);
+
+        var behandling = behandlingService.hentBehandling(behandlingID);
+        return ResponseEntity.ok(new MottaksdatoDto(utledMottaksdato.getMottaksdato(behandling)));
+    }
+}
