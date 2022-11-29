62343ebd83ccda6c87a950f7426d9705f57615ee
Francois Allix <32903762+frans-a@users.noreply.github.com>
2022-11-29 16:57:35 +0100

    Glemte test

--- service/src/test/java/no/nav/melosys/service/behandling/BehandlingServiceTest.java
+++ service/src/test/java/no/nav/melosys/service/behandling/BehandlingServiceTest.java
@@ -123,7 +123,7 @@ class BehandlingServiceTest {
         behandlingService.endreBehandling(BEHANDLING_ID, BEHANDLING_TYPE, BEHANDLING_TEMA, BEHANDLING_STATUS, BEHANDLING_FRIST);
 
         verify(behandlingRepository, times(4)).save(behandlingCaptor.capture());
-        verify(applicationEventPublisher, never()).publishEvent(behandlingEventCaptor.capture());
+        verify(applicationEventPublisher).publishEvent(behandlingEventCaptor.capture());
 
         var lagredeBehandlinger = behandlingCaptor.getAllValues();
         assertThat(lagredeBehandlinger.get(0).getId()).isEqualTo(BEHANDLING_ID);
@@ -134,6 +134,10 @@ class BehandlingServiceTest {
         assertThat(lagredeBehandlinger.get(2).getBehandlingsfrist()).isEqualTo(BEHANDLING_FRIST);
         assertThat(lagredeBehandlinger.get(3).getId()).isEqualTo(BEHANDLING_ID);
         assertThat(lagredeBehandlinger.get(3).getTema()).isEqualTo(BEHANDLING_TEMA);
+
+        var behandlingEndretEvents = behandlingEventCaptor.getAllValues();
+        assertThat(behandlingEndretEvents.get(0).getBehandlingID()).isEqualTo(BEHANDLING_ID);
+        assertThat(((BehandlingEndretStatusEvent) behandlingEndretEvents.get(0)).getBehandlingsstatus()).isEqualTo(BEHANDLING_STATUS);
     }
 
     @Test
