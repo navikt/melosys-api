9fe8987052c7190f757cc96e70cffd4ae8155633
RuneLind <rulind@gmail.com>
2022-12-06 22:05:54 +0100

    Finn hvor kall kommer fra og Logg kun en gang

---
+++ config/src/main/kotlin/no/nav/melosys/featuretoggle/ByUserIdStrategy.kt
@@ -0,0 +1,47 @@
+package no.nav.melosys.featuretoggle
+
+import mu.KotlinLogging
+import no.finn.unleash.strategy.Strategy
+import no.nav.melosys.sikkerhet.context.SubjectHandler
+import no.nav.melosys.sikkerhet.context.ThreadLocalAccessInfo
+import java.util.*
+
+internal class ByUserIdStrategy : Strategy {
+    private val log = KotlinLogging.logger { }
+    private val uniqueLogMessages = Collections.synchronizedSet(HashSet<String>())
+
+    override fun getName(): String = "byUserId"
+
+    override fun isEnabled(parameters: Map<String, String>): Boolean {
+        val userIDs = parameters["user"]
+        if (userIDs.isNullOrBlank()) return false
+
+        val loggedInUserID = getLoggedInUserID()
+        if (loggedInUserID.isNullOrBlank()) {
+            logOnlyFirstMessage(
+                "Unleash byUserId Strategy brukes uten innlogget saksbehandler, blir kalt fra:\n${calledFrom()}"
+            )
+            return false
+        }
+
+        return userIDs.split(",").contains(loggedInUserID)
+    }
+
+    private fun logOnlyFirstMessage(msg: String) {
+        if (uniqueLogMessages.add(msg)) {
+            log.warn(msg)
+        }
+    }
+
+    private fun calledFrom(): String =
+        Thread.currentThread().stackTrace.let { stackTraceElements ->
+            val element = stackTraceElements.find {
+                it.fileName == "DefaultUnleash.java" && it.lineNumber == 93
+            } ?: return@let "Fant ikke unleash bruk i stacktrace"
+            val indexWhereCallToUnleashStarted = stackTraceElements.indexOf(element) + 1
+            stackTraceElements[indexWhereCallToUnleashStarted].toString()
+        }
+
+    private fun getLoggedInUserID(): String? =
+        SubjectHandler.getInstance()?.userID ?: ThreadLocalAccessInfo.getSaksbehandler()
+}
--- config/src/test/kotlin/no/nav/melosys/featuretoggle/ByUserIdStrategyTest.kt
+++ config/src/test/kotlin/no/nav/melosys/featuretoggle/ByUserIdStrategyTest.kt
@@ -1,11 +1,19 @@
 package no.nav.melosys.featuretoggle
 
+import ch.qos.logback.classic.Level
+import ch.qos.logback.classic.Logger
+import ch.qos.logback.classic.LoggerContext
+import ch.qos.logback.classic.spi.ILoggingEvent
+import ch.qos.logback.core.read.ListAppender
 import com.github.tomakehurst.wiremock.WireMockServer
 import com.github.tomakehurst.wiremock.client.WireMock
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration
 import com.google.gson.GsonBuilder
 import io.kotest.matchers.booleans.shouldBeFalse
 import io.kotest.matchers.booleans.shouldBeTrue
+import io.kotest.matchers.collections.shouldHaveSize
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.string.shouldContain
 import io.mockk.every
 import io.mockk.mockk
 import no.finn.unleash.ActivationStrategy
@@ -18,6 +26,7 @@ import no.nav.melosys.sikkerhet.context.SpringSubjectHandler
 import no.nav.melosys.sikkerhet.context.SubjectHandler
 import no.nav.melosys.sikkerhet.context.ThreadLocalAccessInfo
 import org.junit.jupiter.api.*
+import org.slf4j.LoggerFactory
 import org.springframework.http.HttpHeaders
 import org.springframework.http.MediaType
 import java.util.*
@@ -25,10 +34,16 @@ import java.util.*
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 internal class ByUserIdStrategyTest {
     private val wireMockServer = WireMockServer(WireMockConfiguration.wireMockConfig().dynamicPort())
+    private val listAppender = ListAppender<ILoggingEvent>()
 
     @BeforeAll
     fun beforeAll() {
         wireMockServer.start()
+        val logger = LoggerFactory.getLogger("no.nav.melosys.featuretoggle.ByUserIdStrategy") as Logger
+        listAppender.context = LoggerFactory.getILoggerFactory() as LoggerContext
+        logger.level = Level.WARN
+        logger.addAppender(listAppender)
+        listAppender.start()
     }
 
     @AfterAll
@@ -49,7 +64,7 @@ internal class ByUserIdStrategyTest {
         val byUserIdStrategy = ByUserIdStrategy()
 
 
-        val enabled = byUserIdStrategy.isEnabled(mapOf("user" to SAKSBEHANDLER))
+        val enabled = byUserIdStrategy.isEnabled(mapOf("user" to "Z1,$SAKSBEHANDLER,Z2"))
 
 
         enabled.shouldBeTrue()
@@ -83,9 +98,34 @@ internal class ByUserIdStrategyTest {
     @Test
     @Disabled("Ustabil når den kjøres i github actions")
     fun `unleash skal være enabled når brukerID retuneres fra unleash api`() {
+        val unleash = setUpUnleashMock()
+
+        val enabled = unleash.isEnabled("melosys.toggle")
+
+        enabled.shouldBeTrue()
+    }
+
+
+    @Test
+    fun `det skal logges hvor unleash isEnabled blir kalt fra`() {
+        val unleash = setUpUnleashMock(subjectHandlerReturnUserId = null)
+
+        repeat((0..10).count()) {
+            val enabled = unleash.isEnabled("melosys.toggle")
+            enabled.shouldBeFalse()
+        }
+
+        listAppender.list.shouldHaveSize(1)
+            .first().message.shouldContain(
+                "Unleash byUserId Strategy brukes uten innlogget saksbehandler, blir kalt fra:\n" +
+                    "no.nav.melosys.featuretoggle.ByUserIdStrategyTest.det skal logges hvor unleash isEnabled blir kalt fra"
+            )
+    }
+
+    private fun setUpUnleashMock(subjectHandlerReturnUserId: String? = SAKSBEHANDLER): DefaultUnleash {
         val subjectHandler: SubjectHandler = mockk<SpringSubjectHandler>()
         SubjectHandler.set(subjectHandler)
-        every { subjectHandler.userID } returns SAKSBEHANDLER
+        every { subjectHandler.userID } returns subjectHandlerReturnUserId
         wireMockServer.stubFor(
             WireMock.get("/client/features")
                 .willReturn(
@@ -106,18 +146,10 @@ internal class ByUserIdStrategyTest {
             .appName("melosys")
             .unleashAPI("http://localhost:" + wireMockServer.port())
             .build()
-        val unleash = DefaultUnleash(unleashConfig, ByUserIdStrategy())
-
-
-        val enabled = unleash.isEnabled("melosys.toggle")
-
-
-        enabled.shouldBeTrue()
-
-
+        return DefaultUnleash(unleashConfig, ByUserIdStrategy())
     }
 
-    fun createApiResponseJson(): String {
+    private fun createApiResponseJson(): String {
         val toggleCollection =
             ToggleCollection(
                 listOf(
