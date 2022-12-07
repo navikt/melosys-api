3ab87bee39b0935fffb5f5fdc365b5a16251d108
Rune Lind <rune.lind@nav.no>
2022-12-07 12:30:10 +0100

    Finn hvor toggle kall kommer fra og log kun en gang (#1556)
    Bedre logging når unleash ByUserIdStrategy blir forsøkt brukt uten innlogget saksbehandler.
    Finn hvor kallet kommer fra samt logge kun en gang pr. unik bruk av toggle.
    Irriterende at stacktrace er forskjellig lokalt og ved kjøring i github actions. Så ble en litt kreativ løsning som funker begge steder.

---
+++ config/src/main/kotlin/no/nav/melosys/featuretoggle/ByUserIdStrategy.kt
@@ -0,0 +1,52 @@
+package no.nav.melosys.featuretoggle
+
+import mu.KotlinLogging
+import no.finn.unleash.strategy.Strategy
+import no.nav.melosys.sikkerhet.context.SubjectHandler
+import no.nav.melosys.sikkerhet.context.ThreadLocalAccessInfo
+import java.util.*
+
+internal class ByUserIdStrategy : Strategy {
+    private val log = KotlinLogging.logger { }
+    private val uniqueLogMessages = Collections.synchronizedSet(HashSet<String>())
+
+    override fun getName(): String = "byUserId"
+
+    override fun isEnabled(parameters: Map<String, String>): Boolean {
+        val userIDs = parameters["user"]
+        if (userIDs.isNullOrBlank()) return false
+
+        val loggedInUserID = getLoggedInUserID()
+        if (loggedInUserID.isNullOrBlank()) {
+            logOnlyFirstMessage(
+                "Unleash byUserId Strategy brukes uten innlogget saksbehandler, blir kalt fra:\n${calledFrom()}"
+            )
+            return false
+        }
+
+        return userIDs.split(",").contains(loggedInUserID)
+    }
+
+    private fun logOnlyFirstMessage(msg: String) {
+        if (uniqueLogMessages.add(msg)) {
+            log.warn(msg)
+        }
+    }
+
+    private fun calledFrom(): String =
+        Thread.currentThread().stackTrace.let { stackTraceElements ->
+            val element = stackTraceElements.find {
+                it.toString().contains(STACK_TRACE_LINE_AFTER_UNLEASH_IS_ENABLED)
+            } ?: return@let "Fant ikke unleash bruk i stacktrace\n" + stackTraceElements.joinToString("\n")
+            val indexToLineAfterUnleashIsEnabledCall = stackTraceElements.indexOf(element)
+            stackTraceElements[indexToLineAfterUnleashIsEnabledCall - 1].toString()
+        }
+
+    private fun getLoggedInUserID(): String? =
+        SubjectHandler.getInstance()?.userID ?: ThreadLocalAccessInfo.getSaksbehandler()
+
+    companion object {
+        const val STACK_TRACE_LINE_AFTER_UNLEASH_IS_ENABLED =
+            "java.base/jdk.internal.reflect.NativeMethodAccessorImpl.invoke0"
+    }
+}
