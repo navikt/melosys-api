ebb25aff25cd5d5837adccb58608d5ba7cc0ef6f
Janita Johansen <janita.marielle.johansen@nav.no>
2022-12-13 11:48:34 +0100

    Endringer etter QA + kalling av event mens toggle er av

--- service/src/main/java/no/nav/melosys/service/behandling/BehandlingService.java
+++ service/src/main/java/no/nav/melosys/service/behandling/BehandlingService.java
@@ -1,10 +1,5 @@
 package no.nav.melosys.service.behandling;
 
-import java.lang.reflect.InvocationTargetException;
-import java.time.Instant;
-import java.time.LocalDate;
-import java.util.*;
-
 import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.Metrics;
 import no.finn.unleash.Unleash;
@@ -29,6 +24,11 @@ import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.lang.reflect.InvocationTargetException;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.util.*;
 
 import static no.nav.melosys.domain.kodeverk.behandlinger.Behandlingsstatus.*;
 import static no.nav.melosys.domain.kodeverk.behandlinger.Behandlingstema.ARBEID_FLERE_LAND;
@@ -129,29 +129,30 @@ public class BehandlingService {
     }
 
     @Transactional
-    public void endreBehandling(long behandlingID, Behandlingstyper type, Behandlingstema tema, Behandlingsstatus status, LocalDate mottaksdato) {
+    public void endreBehandling(long behandlingID, Behandlingstyper nyType, Behandlingstema nyTema, Behandlingsstatus nyStatus, LocalDate nyMottaksdato) {
         var behandling = hentBehandling(behandlingID);
         boolean behandlingErLåst = behandling.kanIkkeEndres();
 
         if (!behandling.erAktiv()) {
             throw new FunksjonellException("Behandlingen må være aktiv for å kunne endres");
         }
-        if (status != null && status != behandling.getStatus()) {
-            validerNyStatusMulig(behandling, status);
-            endreStatus(behandling, status);
+        if (nyStatus != null && nyStatus != behandling.getStatus()) {
+            validerNyStatusMulig(behandling, nyStatus);
+            endreStatus(behandling, nyStatus);
         }
-        if (type != null && type != behandling.getType() && !behandlingErLåst) {
-            validerNyTypeMulig(behandling, type);
-            endreType(behandling, type);
+        if (nyType != null && nyType != behandling.getType() && !behandlingErLåst) {
+            validerNyTypeMulig(behandling, nyType);
+            endreType(behandling, nyType);
         }
-        if (tema != null && tema != behandling.getTema() && !behandlingErLåst && validerNyTemaMulig(behandling, tema)) {
-            endreTema(behandling, tema);
+        if (nyTema != null && nyTema != behandling.getTema() && !behandlingErLåst && validerNyTemaMulig(behandling, nyTema)) {
+            endreTema(behandling, nyTema);
         }
-        var gammelMottaksdato = utledMottaksdato.getMottaksdato(behandling);
-        if (mottaksdato != null && !mottaksdato.equals(gammelMottaksdato)) {
-            endreMottaksdato(behandling, mottaksdato);
-        } else {
-            oppdaterBehandlingsfrist(behandling, gammelMottaksdato);
+        var nåværendeMottaksdato = utledMottaksdato.getMottaksdato(behandling);
+
+        oppdaterBehandlingsfrist(behandling, nyMottaksdato != null ? nyMottaksdato : nåværendeMottaksdato);
+
+        if (nyMottaksdato != null && !nyMottaksdato.equals(nåværendeMottaksdato)) {
+            endreMottaksdato(behandling, nyMottaksdato);
         }
     }
 
@@ -234,7 +235,6 @@ public class BehandlingService {
         } else {
             behandling.getMottatteOpplysninger().setMottaksdato(mottaksdato);
         }
-        oppdaterBehandlingsfrist(behandling, mottaksdato);
 
         behandlingRepository.save(behandling);
 
@@ -249,6 +249,10 @@ public class BehandlingService {
             : Behandling.utledFristForBehandlingtema(behandling.getTema()));
 
         behandlingRepository.save(behandling);
+
+        if (!unleash.isEnabled("melosys.behandle_alle_saker")) {
+            applicationEventPublisher.publishEvent(new BehandlingEndretAvSaksbehandlerEvent(behandling.getId(), behandling));
+        }
     }
 
     public List<Long> hentMedlemsperioder(long behandlingID) {
