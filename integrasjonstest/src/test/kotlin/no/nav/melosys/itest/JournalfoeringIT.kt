35d2e3e34691aed3d1e07bddbb7cdd16e85d7bae
martonSkjevelandNav <marton.skjeveland@nav.no>
2023-01-04 13:17:15 +0100

    Fjernet iTest som ikke funket med toggle disabled lenger

--- integrasjonstest/src/test/kotlin/no/nav/melosys/itest/JournalfoeringIT.kt
+++ integrasjonstest/src/test/kotlin/no/nav/melosys/itest/JournalfoeringIT.kt
@@ -56,49 +56,6 @@ class JournalfoeringIT(
         oAuthMockServer.stop()
     }
 
-
-    @Test
-    fun journalførOgOpprettSak_EU_EOS_prosesserKjørerAlleSteg() {
-        val journalfoeringOpprettDto = defaultJournalføringDto().apply {
-            fagsak.sakstype = Sakstyper.EU_EOS.kode
-            fagsak.sakstema = Sakstemaer.MEDLEMSKAP_LOVVALG.kode
-            behandlingstypeKode = Behandlingstyper.FØRSTEGANG.kode
-            behandlingstemaKode = Behandlingstema.UTSENDT_ARBEIDSTAKER.kode
-        }
-
-
-        val journalføringProsess = journalførOgVentTilProsesserErFerdige(
-            journalfoeringOpprettDto,
-            waitFor = ProsessType.JFR_NY_SAK_BRUKER,
-            alsoWaitForprosessType = listOf(ProsessType.OPPRETT_OG_DISTRIBUER_BREV)
-        )
-
-
-        val behandling = journalføringProsess.behandling
-        behandling.apply {
-            status.shouldBe(Behandlingsstatus.OPPRETTET)
-            type.shouldBe(Behandlingstyper.FØRSTEGANG)
-            tema.shouldBe(Behandlingstema.UTSENDT_ARBEIDSTAKER)
-        }
-        behandling.fagsak.apply {
-            type.shouldBe(Sakstyper.EU_EOS)
-            status.shouldBe(Saksstatuser.OPPRETTET)
-            registrertAv.shouldBe(Fagsystem.MELOSYS.toString())
-            tema.shouldBe(Sakstemaer.MEDLEMSKAP_LOVVALG)
-        }
-        behandling.mottatteOpplysninger.mottatteOpplysningerData.shouldBeInstanceOf<Soeknad>()
-            .shouldBeEqualToComparingFields(Soeknad().apply {
-                soeknadsland.apply {
-                    landkoder = listOf(Landkoder.IE.kode)
-                    erUkjenteEllerAlleEosLand = false
-                }
-                periode = Periode(
-                    periodeFOM,
-                    periodeTOM
-                )
-            }, FieldsEqualityCheckConfig(ignorePrivateFields = false))
-    }
-
     @Test
     fun journalførOgOpprettSakMedToggleBehandleAlleSaker_EU_EOS_prosesserKjørerAlleSteg() {
         unleash.enable("melosys.behandle_alle_saker")
