ab4da79a349d5700e8fc52f07bcdff84d2b26fc9
Eirik Bergande <eirik.bergande@nav.no>
2023-02-13 10:10:26 +0100

    Fixet test

--- service/src/test/java/no/nav/melosys/service/lovligekombinasjoner/LovligeKombinasjonerServiceTest.java
+++ service/src/test/java/no/nav/melosys/service/lovligekombinasjoner/LovligeKombinasjonerServiceTest.java
@@ -344,8 +344,8 @@ class LovligeKombinasjonerServiceTest {
     void hentMuligeBehandlingstyper_unntakA1AnmodningOmUnntakPåPapir_skalReturnereBehTypeFØRSTEGANG_NY_VURDERING_KLAGE() {
         Set<Behandlingstyper> muligeBehandlingstyper = lovligeKombinasjonerService.hentMuligeBehandlingstyper(Aktoersroller.BRUKER, EU_EOS, UNNTAK, A1_ANMODNING_OM_UNNTAK_PAPIR, null, null, null);
         assertThat(muligeBehandlingstyper)
-            .hasSize(3)
-            .containsExactlyInAnyOrder(FØRSTEGANG, NY_VURDERING, KLAGE);
+            .hasSize(4)
+            .containsExactly(FØRSTEGANG, NY_VURDERING, KLAGE, HENVENDELSE);
     }
 
     @Test
