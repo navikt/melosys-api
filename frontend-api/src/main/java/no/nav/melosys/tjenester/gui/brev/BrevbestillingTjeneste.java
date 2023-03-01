de01ede8b7bc405f8ab171375e8d3d3b43d00dc5
Janita Johansen <56385061+janitamarielle@users.noreply.github.com>
2023-03-01 12:52:34 +0100

    4835 Fjerner toggles (#1694)
    * Fjerner toggles og kode gjemt under. Fant også noen behandle-alle-saker tester som hadde sneket seg med.

--- frontend-api/src/main/java/no/nav/melosys/tjenester/gui/brev/BrevbestillingTjeneste.java
+++ frontend-api/src/main/java/no/nav/melosys/tjenester/gui/brev/BrevbestillingTjeneste.java
@@ -4,14 +4,10 @@ import java.util.List;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import no.finn.unleash.Unleash;
 import no.nav.melosys.domain.brev.NorskMyndighet;
-import no.nav.melosys.featuretoggle.ToggleName;
 import no.nav.melosys.service.brev.BrevbestillingFasade;
-import no.nav.melosys.service.brev.BrevbestillingServiceOld;
 import no.nav.melosys.service.dokument.brev.BrevbestillingDto;
 import no.nav.melosys.service.tilgang.Aksesskontroll;
-import no.nav.melosys.sikkerhet.context.SubjectHandler;
 import no.nav.melosys.tjenester.gui.BrevmalListeBygger;
 import no.nav.melosys.tjenester.gui.dto.brev.*;
 import no.nav.security.token.support.core.api.Protected;
@@ -29,21 +25,16 @@ import static org.springframework.http.MediaType.APPLICATION_PDF_VALUE;
 @RequestScope
 public class BrevbestillingTjeneste {
 
-    private final BrevbestillingServiceOld brevbestillingServiceOld;
     private final BrevmalListeBygger brevmalListeBygger;
     private final Aksesskontroll aksesskontroll;
     private final BrevbestillingFasade brevbestillingFasade;
-    private final Unleash unleash;
 
     public BrevbestillingTjeneste(BrevbestillingFasade brevbestillingFasade,
-                                  BrevbestillingServiceOld brevbestillingServiceOld,
                                   BrevmalListeBygger brevmalListeBygger,
-                                  Aksesskontroll aksesskontroll, Unleash unleash) {
+                                  Aksesskontroll aksesskontroll) {
         this.brevbestillingFasade = brevbestillingFasade;
-        this.brevbestillingServiceOld = brevbestillingServiceOld;
         this.brevmalListeBygger = brevmalListeBygger;
         this.aksesskontroll = aksesskontroll;
-        this.unleash = unleash;
     }
 
     @GetMapping(value = "/tilgjengelige-maler/{behandlingID}", produces = APPLICATION_JSON_VALUE)
@@ -59,17 +50,8 @@ public class BrevbestillingTjeneste {
                                                                    @RequestBody HentMuligeBrevmottakereRequest hentMuligeBrevmottakereRequest) {
         aksesskontroll.autoriser(behandlingID);
 
-        if (!unleash.isEnabled(ToggleName.MELOSYS_MEL_4835)) {
-            var gammelMuligeBrevmottakereDto = brevbestillingServiceOld.hentMuligeMottakere(hentMuligeBrevmottakereRequest.produserbartdokument(), behandlingID, hentMuligeBrevmottakereRequest.orgnr());
-            var hovedMottaker = MuligBrevmottaker.av(gammelMuligeBrevmottakereDto.getHovedMottaker());
-            var kopiMottakere = gammelMuligeBrevmottakereDto.getKopiMottakere().stream().map(MuligBrevmottaker::av).toList();
-            var fasteMottakere = gammelMuligeBrevmottakereDto.getFasteMottakere().stream().map(MuligBrevmottaker::av).toList();
-            return new HentMuligeBrevmottakereResponse(hovedMottaker, kopiMottakere, fasteMottakere);
-        }
-
         var hentMuligeMottakereRequestDto = hentMuligeBrevmottakereRequest.tilHentMuligeBrevmottakereRequestDto(behandlingID);
-        var hentMuligeMottakereResponseDto = brevbestillingFasade.hentMuligeMottakere(hentMuligeMottakereRequestDto);
-        return HentMuligeBrevmottakereResponse.av(hentMuligeMottakereResponseDto);
+        return HentMuligeBrevmottakereResponse.av(brevbestillingFasade.hentMuligeMottakere(hentMuligeMottakereRequestDto));
     }
 
     @PostMapping(value = "pdf/brev/utkast/{behandlingID}", consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_PDF_VALUE)
@@ -77,12 +59,6 @@ public class BrevbestillingTjeneste {
     public ResponseEntity<byte[]> produserUtkast(@PathVariable long behandlingID,
                                                  @RequestBody BrevbestillingRequest brevbestillingRequest) {
         aksesskontroll.autoriser(behandlingID);
-
-        if (!unleash.isEnabled(ToggleName.MELOSYS_MEL_4835)) {
-            BrevbestillingDto brevbestillingDto = brevbestillingRequest.tilBrevbestillingDto(SubjectHandler.getInstance().getUserID());
-            byte[] pdf = brevbestillingServiceOld.produserUtkast(behandlingID, brevbestillingDto);
-            return new ResponseEntity<>(pdf, genPdfHeaders("utkast_" + behandlingID), HttpStatus.OK);
-        }
 
         BrevbestillingDto brevbestillingDto = brevbestillingRequest.tilBrevbestillingDto();
         byte[] pdfInBytes = brevbestillingFasade.produserUtkast(behandlingID, brevbestillingDto);
@@ -94,12 +70,6 @@ public class BrevbestillingTjeneste {
     public void produserBrev(@PathVariable("behandlingID") long behandlingID,
                              @RequestBody BrevbestillingRequest brevbestillingRequest) {
         aksesskontroll.autoriser(behandlingID);
-
-        if (!unleash.isEnabled(ToggleName.MELOSYS_MEL_4835)) {
-            BrevbestillingDto brevbestillingDto = brevbestillingRequest.tilBrevbestillingDto(SubjectHandler.getInstance().getUserID());
-            brevbestillingServiceOld.produserBrev(behandlingID, brevbestillingDto);
-            return;
-        }
 
         BrevbestillingDto brevbestillingDto = brevbestillingRequest.tilBrevbestillingDto();
         brevbestillingFasade.produserBrev(behandlingID, brevbestillingDto);
--- service/src/main/java/no/nav/melosys/service/brev/BrevmalListeService.java
+++ service/src/main/java/no/nav/melosys/service/brev/BrevmalListeService.java
@@ -1,37 +1,14 @@
 package no.nav.melosys.service.brev;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
-import no.nav.melosys.domain.Behandling;
-import no.nav.melosys.domain.Kontaktopplysning;
-import no.nav.melosys.domain.brev.Mottaker;
-import no.nav.melosys.domain.brev.Postadresse;
-import no.nav.melosys.domain.dokument.organisasjon.OrganisasjonDokument;
-import no.nav.melosys.domain.kodeverk.Aktoersroller;
 import no.nav.melosys.domain.kodeverk.Mottakerroller;
-import no.nav.melosys.domain.kodeverk.Sakstemaer;
-import no.nav.melosys.domain.kodeverk.behandlinger.Behandlingstyper;
 import no.nav.melosys.domain.kodeverk.brev.Produserbaredokumenter;
-import no.nav.melosys.domain.person.Persondata;
-import no.nav.melosys.exception.FunksjonellException;
-import no.nav.melosys.integrasjon.dokgen.DokgenAdresseMapper;
-import no.nav.melosys.integrasjon.ereg.EregFasade;
-import no.nav.melosys.service.aktoer.KontaktopplysningService;
-import no.nav.melosys.service.behandling.BehandlingService;
 import no.nav.melosys.service.brev.bestilling.HentBrevAdresseTilMottakereService;
 import no.nav.melosys.service.brev.bestilling.HentMuligeProduserbaredokumenterService;
 import no.nav.melosys.service.brev.brevmalliste.BrevAdresse;
-import no.nav.melosys.service.dokument.BrevmottakerService;
-import no.nav.melosys.service.persondata.PersondataFasade;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import static java.util.Arrays.asList;
-import static java.util.Collections.emptyList;
-import static no.nav.melosys.domain.kodeverk.brev.Produserbaredokumenter.*;
-import static no.nav.melosys.integrasjon.dokgen.DokgenAdresseMapper.*;
+import java.util.List;
 
 @Service
 public class BrevmalListeService {
@@ -39,137 +16,18 @@ public class BrevmalListeService {
     private final HentMuligeProduserbaredokumenterService hentMuligeProduserbaredokumenterService;
     private final HentBrevAdresseTilMottakereService hentBrevAdresseTilMottakereService;
 
-    @Deprecated(since = "Ta vekk med melosys.MEL-4835.refactor1, erstattet av komponent")
-    private final BrevmottakerService brevmottakerService;
-
-    @Deprecated(since = "Ta vekk med melosys.MEL-4835.refactor1, erstattet av komponent")
-    private final BehandlingService behandlingService;
-
-    @Deprecated(since = "Ta vekk med melosys.MEL-4835.refactor1, erstattet av komponent")
-    private final PersondataFasade persondataFasade;
-
-    @Deprecated(since = "Ta vekk med melosys.MEL-4835.refactor1, erstattet av komponent")
-    private final KontaktopplysningService kontaktopplysningService;
-
-    @Deprecated(since = "Ta vekk med melosys.MEL-4835.refactor1, erstattet av komponent")
-    private final EregFasade eregFasade;
-
     public BrevmalListeService(HentMuligeProduserbaredokumenterService hentMuligeProduserbaredokumenterService,
-                               HentBrevAdresseTilMottakereService hentBrevAdresseTilMottakereService,
-                               BrevmottakerService brevmottakerService,
-                               BehandlingService behandlingService,
-                               PersondataFasade persondataFasade,
-                               KontaktopplysningService kontaktopplysningService,
-                               EregFasade eregFasade) {
+                               HentBrevAdresseTilMottakereService hentBrevAdresseTilMottakereService) {
         this.hentMuligeProduserbaredokumenterService = hentMuligeProduserbaredokumenterService;
         this.hentBrevAdresseTilMottakereService = hentBrevAdresseTilMottakereService;
-        this.brevmottakerService = brevmottakerService;
-        this.behandlingService = behandlingService;
-        this.persondataFasade = persondataFasade;
-        this.kontaktopplysningService = kontaktopplysningService;
-        this.eregFasade = eregFasade;
     }
 
     public List<Produserbaredokumenter> hentMuligeProduserbaredokumenter(long behandlingId, Mottakerroller rolle) {
         return hentMuligeProduserbaredokumenterService.hentMuligeProduserbaredokumenter(behandlingId, rolle);
     }
 
-    @Deprecated(since = "Ta vekk med melosys.MEL-4835.refactor1, erstattet av komponent")
-    @Transactional
-    public List<Produserbaredokumenter> hentMuligeProduserbaredokumenterGammel(long behandlingId, Mottakerroller rolle) {
-        Behandling behandling = behandlingService.hentBehandlingMedSaksopplysninger(behandlingId);
-
-        if (behandling.erInaktiv()) {
-            return emptyList();
-        }
-
-        return switch (rolle) {
-            case BRUKER -> {
-                List<Produserbaredokumenter> brevmaler = new ArrayList<>();
-                if (behandling.getFagsak().getTema() == Sakstemaer.MEDLEMSKAP_LOVVALG && behandling.getType() == Behandlingstyper.FØRSTEGANG) {
-                    brevmaler.add(MELDING_FORVENTET_SAKSBEHANDLINGSTID_SOKNAD);
-                }
-                brevmaler.addAll(asList(MANGELBREV_BRUKER, GENERELT_FRITEKSTBREV_BRUKER));
-                yield brevmaler;
-            }
-            case VIRKSOMHET -> Collections.singletonList(GENERELT_FRITEKSTBREV_VIRKSOMHET);
-            case ARBEIDSGIVER -> List.of(MANGELBREV_ARBEIDSGIVER, GENERELT_FRITEKSTBREV_ARBEIDSGIVER);
-            case ANNEN_ORGANISASJON -> behandling.getFagsak().getHovedpartRolle() == Aktoersroller.VIRKSOMHET
-                ? Collections.singletonList(GENERELT_FRITEKSTBREV_VIRKSOMHET)
-                : List.of(MANGELBREV_ARBEIDSGIVER, GENERELT_FRITEKSTBREV_ARBEIDSGIVER);
-            case UTENLANDSK_TRYGDEMYNDIGHET -> Collections.singletonList(UTENLANDSK_TRYGDEMYNDIGHET_FRITEKSTBREV);
-            case NORSK_MYNDIGHET -> Collections.singletonList(FRITEKSTBREV);
-            default -> throw new FunksjonellException("Rollen " + rolle + " kan ikke sende brev gjennom brevmenyen");
-        };
-    }
-
     @Transactional
     public List<BrevAdresse> hentBrevAdresseTilMottakere(long behandlingId, Mottakerroller rolle) {
         return hentBrevAdresseTilMottakereService.hentBrevAdresseTilMottakere(behandlingId, rolle);
-    }
-
-    @Deprecated(since = "Ta vekk med melosys.MEL-4835.refactor1, erstattet av komponent")
-    @Transactional
-    public List<BrevAdresse> hentBrevAdresseTilMottakereGammel(Mottakerroller rolle, long behandlingId) {
-        Behandling behandling = behandlingService.hentBehandlingMedSaksopplysninger(behandlingId);
-        var mottakere = brevmottakerService.avklarMottakere(null, Mottaker.medRolle(rolle), behandling, false, false);
-        List<BrevAdresse> brevAdresser = new ArrayList<>();
-
-        for (Mottaker mottaker : mottakere) {
-            brevAdresser.add(tilBrevAdresse(mottaker, behandling));
-        }
-        return brevAdresser;
-    }
-
-    @Deprecated(since = "Ta vekk med melosys.MEL-4835.refactor1, erstattet av komponent")
-    private BrevAdresse tilBrevAdresse(Mottaker mottaker, Behandling behandling) {
-        Persondata persondata = null;
-        Kontaktopplysning kontaktopplysning = null;
-        OrganisasjonDokument orgDokument = null;
-
-        switch (mottaker.getRolle()) {
-            case BRUKER: {
-                persondata = persondataFasade.hentPerson(behandling.getFagsak().hentBrukersAktørID());
-                break;
-            }
-            case FULLMEKTIG: {
-                if (mottaker.getPersonIdent() != null) {
-                    persondata = persondataFasade.hentPerson(mottaker.getPersonIdent());
-                    break;
-                }
-            }
-            case VIRKSOMHET, ARBEIDSGIVER: {
-                kontaktopplysning = kontaktopplysningService.hentKontaktopplysning(behandling.getFagsak().getSaksnummer(),
-                    mottaker.getOrgnr()).orElse(null);
-                String mottakerOrgnr = kontaktopplysning != null && kontaktopplysning.getKontaktOrgnr() != null ? kontaktopplysning.getKontaktOrgnr() : mottaker.getOrgnr();
-                orgDokument = (OrganisasjonDokument) eregFasade.hentOrganisasjon(mottakerOrgnr).getDokument();
-                break;
-            }
-            default:
-                throw new FunksjonellException("Mottakersrolle støttes ikke: " + mottaker.getRolle());
-        }
-
-        if (orgDokument == null && persondata == null) {
-            throw new FunksjonellException("Orgdata eller persondata forventes for å sende brev.");
-        }
-
-        return new BrevAdresse.Builder()
-            .medMottakerNavn(mapNavn(orgDokument, persondata))
-            .medOrgnr(orgDokument != null ? orgDokument.getOrgnummer() : null)
-            .medAdresselinjer(mapAdresselinjer(orgDokument, null, kontaktopplysning, persondata))
-            .medPostnr(mapPostnr(orgDokument, persondata))
-            .medPoststed(orgDokument != null ? DokgenAdresseMapper.mapPoststed(orgDokument) : mapPoststed(persondata))
-            .medRegion(mapRegionForAdresse(orgDokument, persondata))
-            .medLand(mapLandForAdresse(orgDokument, persondata))
-            .build();
-    }
-
-    @Deprecated(since = "Ta vekk med melosys.MEL-4835.refactor1, erstattet av komponent")
-    private String mapPoststed(Persondata persondata) {
-        Postadresse postadresse = persondata.hentGjeldendePostadresse();
-        if (postadresse == null) {
-            return null;
-        }
-        return postadresse.poststed();
     }
 }
