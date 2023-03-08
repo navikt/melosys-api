f0b3e1292cc308fde977c7f580b3f2a1143e96bf
Isa Salem Hame <wanghattan@gmail.com>
2023-03-08 22:28:48 +0100

    Konverterer enumklasse til Kotlin

--- service/src/main/java/no/nav/melosys/service/oppgave/OppgaveBehandlingstype.kt
+++ service/src/main/java/no/nav/melosys/service/oppgave/OppgaveBehandlingstype.kt
@@ -1,15 +1,5 @@
-package no.nav.melosys.service.oppgave;
+package no.nav.melosys.service.oppgave
 
-public enum OppgaveBehandlingstype {
-    EOS_LOVVALG_NORGE("ae0112");
-
-    private final String kode;
-
-    OppgaveBehandlingstype(String kode) {
-        this.kode = kode;
-    }
-
-    public String getKode() {
-        return kode;
-    }
+enum class OppgaveBehandlingstype(val kode: String) {
+    EOS_LOVVALG_NORGE("ae0112")
 }
