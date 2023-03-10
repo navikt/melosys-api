020f580fc309177f2bb4252d66259593190fc6ca
Thang Phan <thang.quoc.ly.phan@nav.no>
2023-03-10 12:19:07 +0100

    5241: Hent saksbehandler navn

--- service/src/main/java/no/nav/melosys/service/bruker/SaksbehandlerService.java
+++ service/src/main/java/no/nav/melosys/service/bruker/SaksbehandlerService.java
@@ -1,37 +1,26 @@
 package no.nav.melosys.service.bruker;
 
-import java.util.Objects;
 import java.util.Optional;
 
 import no.nav.melosys.exception.IkkeFunnetException;
-import no.nav.melosys.sikkerhet.context.SubjectHandler;
-import no.nav.melosys.sikkerhet.context.ThreadLocalAccessInfo;
+import no.nav.melosys.integrasjon.azuread.AzureAdService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SaksbehandlerService {
+    private AzureAdService azureAdService;
+
+    public SaksbehandlerService(AzureAdService azureAdService) {
+        this.azureAdService = azureAdService;
+    }
 
     public Optional<String> finnNavnForIdent(String ident) {
-        // Midlertidig løsning før vi legger inn støtte for å slå dette opp i azure ad
-        SubjectHandler instance = SubjectHandler.getInstance();
-        String userID = instance.getUserID();
-        if (userID != null) {
-            if (!Objects.equals(ident, userID)) {
-                return Optional.empty();
-            }
-            return Optional.of(instance.getUserName());
-        }
-
-        String saksbehandlerID = ThreadLocalAccessInfo.getSaksbehandler();
-        if (ident.equals(saksbehandlerID)) {
-            String saksbehandlerNavn = ThreadLocalAccessInfo.getSaksbehandlerNavn();
-            return Optional.of(saksbehandlerNavn);
-        }
-        return Optional.empty();
+        String saksbehandlerNavn = azureAdService.hentSaksbehandlerNavn(ident);
+        return Optional.of(saksbehandlerNavn);
     }
 
     public String hentNavnForIdent(String ident) {
         return finnNavnForIdent(ident)
-            .orElseThrow(() -> new IkkeFunnetException("Finner ikke navn for ident " + ident));
+            .orElseThrow(() -> new IkkeFunnetException("Finner ikke saksbehandler navn for ident " + ident));
     }
 }
