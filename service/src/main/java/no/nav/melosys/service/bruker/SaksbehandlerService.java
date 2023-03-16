074205d3c6f04a72bc454b80e015389cd1df8891
Thang Phan <thang.quoc.ly.phan@nav.no>
2023-03-16 19:22:35 +0100

    5241: Graph request feiler, i dev

--- service/src/main/java/no/nav/melosys/service/bruker/SaksbehandlerService.java
+++ service/src/main/java/no/nav/melosys/service/bruker/SaksbehandlerService.java
@@ -1,9 +1,12 @@
 package no.nav.melosys.service.bruker;
 
+import java.util.Objects;
 import java.util.Optional;
 
 import no.nav.melosys.exception.IkkeFunnetException;
 import no.nav.melosys.integrasjon.azuread.AzureAdService;
+import no.nav.melosys.sikkerhet.context.SubjectHandler;
+import no.nav.melosys.sikkerhet.context.ThreadLocalAccessInfo;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,8 +17,30 @@ public class SaksbehandlerService {
         this.azureAdService = azureAdService;
     }
 
+    public Optional<String> finnNavnForIdentFraToken(String ident) {
+        SubjectHandler instance = SubjectHandler.getInstance();
+        String userID = instance.getUserID();
+        if (userID != null) {
+            if (!Objects.equals(ident, userID)) {
+                return Optional.empty();
+            }
+            return Optional.of(instance.getUserName());
+        }
+
+        String saksbehandlerID = ThreadLocalAccessInfo.getSaksbehandler();
+        if (ident.equals(saksbehandlerID)) {
+            String saksbehandlerNavn = ThreadLocalAccessInfo.getSaksbehandlerNavn();
+            return Optional.of(saksbehandlerNavn);
+        }
+        return Optional.empty();
+    }
+
+    public String finnNavnForIdentFraAzure(String ident) {
+        return azureAdService.hentSaksbehandlerNavn(ident);
+    }
+
     public Optional<String> finnNavnForIdent(String ident) {
-        String saksbehandlerNavn = azureAdService.hentSaksbehandlerNavn(ident);
+        String saksbehandlerNavn = finnNavnForIdentFraToken(ident).orElseGet(() -> finnNavnForIdentFraAzure(ident));
         return Optional.of(saksbehandlerNavn);
     }
 
