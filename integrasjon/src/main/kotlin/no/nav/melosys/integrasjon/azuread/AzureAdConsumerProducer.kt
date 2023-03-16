eb9c31c0c63a0263c7828b2b1ec34be94a3b1cdb
Isa Salem Hame <isa.salem.hame@nav.no>
2023-03-16 21:55:58 +0100

    5241: Legger til proxy-innstillinger for Azure AD WebClient (#1719)
    * 5231: Legger til proxy-innstillinger for Azure AD WebClient
    
    * 5231: Ignorerer url for proxy hvis den ikke kan resolves av URI
    
    * 5231: Legger til http port i application-test
    
    * 5231: Gjør property optional

--- integrasjon/src/main/kotlin/no/nav/melosys/integrasjon/azuread/AzureAdConsumerProducer.kt
+++ integrasjon/src/main/kotlin/no/nav/melosys/integrasjon/azuread/AzureAdConsumerProducer.kt
@@ -7,24 +7,44 @@ import no.nav.melosys.integrasjon.felles.mdc.CorrelationIdOutgoingFilter
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.client.reactive.ReactorClientHttpConnector
 import org.springframework.web.reactive.function.client.WebClient
+import reactor.netty.http.client.HttpClient
+import reactor.netty.transport.ProxyProvider
+import reactor.netty.transport.ProxyProvider.TypeSpec
+import java.net.URI
 
 @Configuration
 class AzureAdConsumerProducer(
     @Value("\${microsoft.graph.rest.url}") private val url: String,
-    private val genericAuthFilterFactory: GenericAuthFilterFactory
+    @Value("\${http.proxy:#{null}}") private val httpProxy: String?,
+    private val genericAuthFilterFactory: GenericAuthFilterFactory,
 ) : CallIdAware, WebClientConfig {
 
     @Bean
     fun azureAdConsumer(
         webClientBuilder: WebClient.Builder, correlationIdOutgoingFilter: CorrelationIdOutgoingFilter,
     ): AzureAdConsumer {
+
         return AzureAdConsumer(
             webClientBuilder
                 .baseUrl(url)
                 .filter(genericAuthFilterFactory.getAzureFilter(CLIENT_NAME))
                 .filter(errorFilter("Kall mot microsoft graph feilet"))
-                .build(),
+                .apply {
+                    try {
+                        val proxyURI = URI(httpProxy!!)
+                        val httpClient = HttpClient.create()
+                            .proxy { proxy: TypeSpec ->
+                                proxy.type(ProxyProvider.Proxy.HTTP)
+                                    .host(proxyURI.host)
+                                    .port(proxyURI.port)
+                            }
+                        it.clientConnector(ReactorClientHttpConnector(httpClient))
+                    } catch (_: Exception) {
+                    }
+                }
+                .build()
         )
     }
 
