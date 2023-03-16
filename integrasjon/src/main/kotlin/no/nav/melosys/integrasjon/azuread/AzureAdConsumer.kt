153cdcddd9f34c88b33aafbe9a26202a304d9993
Thang Phan <thang.quoc.ly.phan@nav.no>
2023-03-16 14:17:10 +0100

    5241: Hent saksbehandler navn (#1714)
    * 5241: Hent saksbehandler navn
    
    * Fjern prirnln

---
+++ integrasjon/src/main/kotlin/no/nav/melosys/integrasjon/azuread/AzureAdConsumer.kt
@@ -0,0 +1,31 @@
+package no.nav.melosys.integrasjon.azuread
+
+import com.fasterxml.jackson.databind.JsonNode
+import no.nav.melosys.integrasjon.azuread.dto.DisplayNameDTO
+import org.springframework.http.MediaType
+import org.springframework.retry.annotation.Retryable
+import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.util.UriBuilder
+
+@Retryable
+open class AzureAdConsumer(
+    private val webClient: WebClient,
+) {
+
+    open fun hentSaksbehandlerNavn(ident: String): String? {
+        return webClient.get().uri("/users") { uriBuilder: UriBuilder ->
+            uriBuilder
+                .queryParam("\$filter", "mailnickname eq '$ident'")
+                .queryParam("\$select", "displayName")
+                .build()
+        }
+            .accept(MediaType.APPLICATION_JSON)
+            .retrieve()
+            .bodyToMono(JsonNode::class.java)
+            .map { jsonNode ->
+                val displayName = jsonNode["value"][0]["displayName"].asText()
+                DisplayNameDTO(displayName).displayName
+            }
+            .block()!!
+    }
+}
