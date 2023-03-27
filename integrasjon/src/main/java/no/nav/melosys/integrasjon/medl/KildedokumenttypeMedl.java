5e33e134dc40bd22d9feb67a6763a4948e069f6a
Marton Skjæveland <56435736+martonSkjevelandNav@users.noreply.github.com>
2023-03-27 11:37:04 +0200

    5687 Feiler mot medl med feil kode. DOKUMENT -> Dokument (#1729)

--- integrasjon/src/main/java/no/nav/melosys/integrasjon/medl/KildedokumenttypeMedl.java
+++ integrasjon/src/main/java/no/nav/melosys/integrasjon/medl/KildedokumenttypeMedl.java
@@ -3,7 +3,7 @@ package no.nav.melosys.integrasjon.medl;
 public enum KildedokumenttypeMedl {
     HENV_SOKNAD("Henv_Soknad"),
     SED("SED"),
-    DOKUMENT("DOKUMENT"),
+    DOKUMENT("Dokument"),
     A1("PortBlank_A1");
 
     final String kode;
