7b44ccfbcaa7d19c85249fb34abded788c85e851
Isa Salem Hame <wanghattan@gmail.com>
2023-03-30 10:06:53 +0200

    5684: Egen regelsett for trygdeavtale unntak

--- service/src/main/java/no/nav/melosys/service/kontroll/feature/ferdigbehandling/kontroll/FerdigbehandlingKontrollsett.java
+++ service/src/main/java/no/nav/melosys/service/kontroll/feature/ferdigbehandling/kontroll/FerdigbehandlingKontrollsett.java
@@ -5,16 +5,23 @@ import java.util.Set;
 import java.util.function.Function;
 
 import no.nav.melosys.domain.kodeverk.Sakstyper;
+import no.nav.melosys.domain.kodeverk.behandlinger.Behandlingstema;
 import no.nav.melosys.service.kontroll.feature.ferdigbehandling.data.FerdigbehandlingKontrollData;
 import no.nav.melosys.service.validering.Kontrollfeil;
 
 public class FerdigbehandlingKontrollsett {
 
-    public static Set<Function<FerdigbehandlingKontrollData, Kontrollfeil>> hentRegelsettForVedtak(Sakstyper sakstype) {
+    public static Set<Function<FerdigbehandlingKontrollData, Kontrollfeil>> hentRegelsettForVedtak(Sakstyper sakstype,
+                                                                                                   Behandlingstema behandlingstema) {
         return switch (sakstype) {
             case EU_EOS -> REGELSETT_EU_EOS;
             case FTRL -> Collections.emptySet();
-            case TRYGDEAVTALE -> REGELSETT_TRYGDEAVTALER;
+            case TRYGDEAVTALE -> {
+                if (behandlingstema.equals(Behandlingstema.REGISTRERING_UNNTAK)) {
+                    yield REGELSETT_TRYGDEAVTALE_UNNTAK;
+                }
+                yield REGELSETT_TRYGDEAVTALER;
+            }
         };
     }
     public static Set<Function<FerdigbehandlingKontrollData, Kontrollfeil>> hentRegelsettForAvslagOgHenleggelse() {
@@ -40,6 +47,11 @@ public class FerdigbehandlingKontrollsett {
         FerdigbehandlingKontroll::periodeManglerSluttdato,
         FerdigbehandlingKontroll::arbeidsstedManglerFelter,
         FerdigbehandlingKontroll::representantIUtlandetMangler
+    );
+
+    private static final Set<Function<FerdigbehandlingKontrollData, Kontrollfeil>> REGELSETT_TRYGDEAVTALE_UNNTAK = Set.of(
+        FerdigbehandlingKontroll::overlappendeMedlemsperiode,
+        FerdigbehandlingKontroll::periodeManglerSluttdato
     );
 
     private static final Set<Function<FerdigbehandlingKontrollData, Kontrollfeil>> REGELSETT_AVSLAG_HENLEGGELSE = Set.of(
