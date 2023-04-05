1638402b8f4f7a83e76f2be56f37bf666018ea97
RuneLind <rulind@gmail.com>
2023-04-05 08:16:52 +0200

    Implementert OppgaveBehandlingstemaNyMappingFactory
    Forsatt work in progress

--- service/src/main/java/no/nav/melosys/service/oppgave/OppgaveBehandlingstema.java
+++ service/src/main/java/no/nav/melosys/service/oppgave/OppgaveBehandlingstema.java
@@ -7,8 +7,24 @@ public enum OppgaveBehandlingstema {
     PENSJONIST_ELLER_UFORETRYGDET("ab0355"),
     YRKESAKTIV("ab0462"),
     ANMODNING_UNNTAK("ab0460"),
-    REGISTRERING_UNNTAK("ab0461");
-
+    REGISTRERING_UNNTAK("ab0461"),
+    AB0483("ab0483"),
+    AB0480("ab0480"),
+    AB0482("ab0482"),
+    AB0479("ab0479"),
+    AB0484("ab0484"),
+    AB0485("ab0485"),
+    AB0486("ab0486"),
+    AB0475("ab0475"),
+    AB0476("ab0476"),
+    AB0477("ab0477"),
+    AB0493("ab0493"),
+    AB0490("ab0490"),
+    AB0491("ab0491"),
+    AB0492("ab0492"),
+    AB0478("ab0478"),
+    AB0488("ab0488"),
+    AB0489("ab0489");
     private final String kode;
 
     OppgaveBehandlingstema(String kode) {
---
+++ service/src/test/kotlin/no/nav/melosys/service/oppgave/OppgaveFactoryNyMappingTest.kt
@@ -0,0 +1,123 @@
+package no.nav.melosys.service.oppgave
+
+import io.kotest.assertions.withClue
+import io.kotest.matchers.shouldBe
+import no.finn.unleash.FakeUnleash
+import no.nav.melosys.domain.Behandling
+import no.nav.melosys.domain.Fagsak
+import no.nav.melosys.domain.Tema
+import no.nav.melosys.domain.kodeverk.Oppgavetyper
+import no.nav.melosys.domain.kodeverk.Sakstemaer
+import no.nav.melosys.domain.kodeverk.Sakstyper
+import no.nav.melosys.domain.kodeverk.behandlinger.Behandlingstema
+import no.nav.melosys.domain.kodeverk.behandlinger.Behandlingstyper
+import no.nav.melosys.featuretoggle.ToggleName
+import org.junit.jupiter.api.Test
+import java.time.LocalDate
+
+class OppgaveFactoryNyMappingTest {
+
+    val oppgaveFactory = OppgaveFactory(FakeUnleash().apply { enable(ToggleName.NY_GOSYS_MAPPING) })
+
+    @Test
+    fun eueos_medlemskapLovvalg_standardEndretPeriode_flere_1() {
+        test(
+            sakstyper = listOf(Sakstyper.EU_EOS),
+            sakstemaer = listOf(Sakstemaer.MEDLEMSKAP_LOVVALG),
+            behandlingstyper = listOf(
+                Behandlingstyper.FØRSTEGANG, Behandlingstyper.NY_VURDERING, Behandlingstyper.ENDRET_PERIODE,
+            ),
+            melosysBehandlingstemaer = listOf(
+                Behandlingstema.UTSENDT_ARBEIDSTAKER,
+                Behandlingstema.UTSENDT_SELVSTENDIG,
+                Behandlingstema.ARBEID_TJENESTEPERSON_ELLER_FLY,
+                Behandlingstema.ARBEID_FLERE_LAND,
+                Behandlingstema.ARBEID_KUN_NORGE
+            ),
+            expectedBehandlingstema = OppgaveBehandlingstema.AB0483,
+            expectedBehandlingstype = null,
+            expectedTema = Tema.MED,
+            expectedOppgavetype = Oppgavetyper.BEH_SAK_MK
+        )
+    }
+
+    private fun test(
+        sakstyper: Collection<Sakstyper>,
+        sakstemaer: Collection<Sakstemaer>,
+        behandlingstyper: Collection<Behandlingstyper>,
+        melosysBehandlingstemaer: Collection<Behandlingstema>,
+        expectedBehandlingstema: OppgaveBehandlingstema,
+        expectedBehandlingstype: OppgaveBehandlingstype?,
+        expectedTema: Tema,
+        expectedOppgavetype: Oppgavetyper,
+        forventetBegrunnelse: String? = null
+    ) {
+        sakstyper.forEach { sakstype ->
+            sakstemaer.forEach { sakstema ->
+                behandlingstyper.forEach { behandlignstype ->
+                    melosysBehandlingstemaer.forEach { melosysBehandlingstema ->
+                        test(
+                            sakstype,
+                            sakstema,
+                            behandlignstype,
+                            melosysBehandlingstema,
+                            expectedBehandlingstema,
+                            expectedBehandlingstype,
+                            expectedTema,
+                            expectedOppgavetype,
+                            forventetBegrunnelse ?: melosysBehandlingstema.beskrivelse
+                        )
+                    }
+                }
+            }
+        }
+    }
+
+    private fun test(
+        sakstype: Sakstyper,
+        sakstema: Sakstemaer,
+        behandlingstype: Behandlingstyper,
+        melosysBehandlingstema: Behandlingstema,
+        expectedBehandlingstema: OppgaveBehandlingstema,
+        expectedBehandlingstype: OppgaveBehandlingstype?,
+        expectedTema: Tema,
+        expectedOppgavetype: Oppgavetyper,
+        forventetBegrunnelse: String
+    ) {
+        val behandling = Behandling().apply {
+            fagsak = Fagsak().apply {
+                type = sakstype
+                tema = sakstema
+
+            }
+            type = behandlingstype
+            tema = melosysBehandlingstema
+        }
+
+        val oppgave = oppgaveFactory.lagBehandlingsoppgave(behandling, LocalDate.now()).build()
+
+        withClue(
+            "\nsakstype:               $sakstype " +
+                    "\nsakstema:               $sakstema " +
+                    "\nbehandlingstype:        $behandlingstype " +
+                    "\nmelosysBehandlingstema: $melosysBehandlingstema"
+        ) {
+            withClue("oppgave.behandlingstema") {
+                oppgave.behandlingstema.shouldBe(expectedBehandlingstema.kode)
+            }
+            withClue("oppgave.behandlingstype") {
+                oppgave.behandlingstype.shouldBe(expectedBehandlingstype?.kode)
+            }
+            withClue("oppgave.tema") {
+                oppgave.tema.shouldBe(expectedTema)
+            }
+            withClue("oppgave.oppgavetype") {
+                oppgave.oppgavetype.shouldBe(expectedOppgavetype)
+            }
+            withClue("oppgave.beskrivelse") {
+                oppgave.beskrivelse.shouldBe(forventetBegrunnelse)
+            }
+        }
+    }
+    
+}
