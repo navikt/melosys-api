3a8748a313dd24830b0e25ffc6f0de1503025de3
RuneLind <rulind@gmail.com>
2023-04-26 21:50:26 +0200

    Legg til midlertidig BehandlingRepositoryForOppgaveMigrering

---
+++ repository/src/main/java/no/nav/melosys/repository/BehandlingRepositoryForOppgaveMigrering.java
@@ -0,0 +1,24 @@
+package no.nav.melosys.repository;
+
+import no.nav.melosys.domain.Behandling;
+import no.nav.melosys.domain.SakOgBehandlingDTO;
+import no.nav.melosys.domain.kodeverk.behandlinger.Behandlingsstatus;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.lang.Nullable;
+
+import java.util.Collection;
+import java.util.List;
+
+public interface BehandlingRepositoryForOppgaveMigrering extends CrudRepository<Behandling, Long> {
+    @EntityGraph(attributePaths = "saksopplysninger")
+    @Nullable
+    Behandling findWithSaksopplysningerById(Long behandlingID);
+
+    @Query("SELECT new no.nav.melosys.domain.SakOgBehandlingDTO(" +
+            "e.fagsak.saksnummer, e.id, e.fagsak.type, e.fagsak.tema, e.type, e.tema, e.status) " +
+            "FROM Behandling e WHERE e.status NOT IN (:excludedStatuses)")
+    Collection<SakOgBehandlingDTO> findSaksOgBehandlingTyperOgTeam(@Param("excludedStatuses") List<Behandlingsstatus> excludedStatuses);
+}
