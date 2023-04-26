a4bdfdfbb933c79f9ca99600a328f571f34a3733
RuneLind <rulind@gmail.com>
2023-04-26 21:40:23 +0200

    Rull tilbake Application og BehandlingRepository

--- app/src/main/java/no/nav/melosys/Application.java
+++ app/src/main/java/no/nav/melosys/Application.java
@@ -1,10 +1,7 @@
 package no.nav.melosys;
 
-import no.nav.melosys.service.oppgave.OppgaveMigrering;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -13,12 +10,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class Application {
 
-    private final OppgaveMigrering oppgaveMigrering;
-
-    public Application(OppgaveMigrering oppgaveMigrering) {
-        this.oppgaveMigrering = oppgaveMigrering;
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
@@ -26,11 +17,5 @@ public class Application {
     @GetMapping(path = { "/journalforing/**", "/sok/**" })
     public String forward() {
         return "forward:/";
-    }
-
-    @EventListener(ApplicationReadyEvent.class)
-    public void eventListenerExecute() {
-        System.out.println("############# - eventListenerExecute" );
-        oppgaveMigrering.go();
     }
 }
