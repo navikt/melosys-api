3a416812a9b36c9ba088e9717be258f415a1cbd3
RuneLind <rulind@gmail.com>
2023-04-28 14:25:37 +0200

    Legg til GyldigeKombinasjoner
    For bruk testing av oppgaveGosysMapping i første omgang

--- service/src/test/kotlin/no/nav/melosys/service/oppgave/OppgaveGosysMappingTest.kt
+++ service/src/test/kotlin/no/nav/melosys/service/oppgave/OppgaveGosysMappingTest.kt
@@ -1,6 +1,7 @@
 package no.nav.melosys.service.oppgave
 
 import io.kotest.matchers.collections.shouldHaveSize
+import no.nav.melosys.service.lovligekombinasjoner.GyldigeKombinasjoner
 import org.junit.jupiter.api.Test
 
 class OppgaveGosysMappingTest {
@@ -22,4 +23,19 @@ class OppgaveGosysMappingTest {
             }
         }
     }
+
+    // Finner nå 2 som ikke mapper mot gosys
+    @Test
+    fun `sjekk at gyldige melosys kombinasjoner funger når vi lager gosys oppgave`() {
+        //  Fant ikke oppgave mapping for sakstype:EU_EOS, sakstema:UNNTAK, behandlingstema:A1_ANMODNING_OM_UNNTAK_PAPIR, behandlingstype:KLAGE
+        //  Fant ikke oppgave mapping for sakstype:TRYGDEAVTALE, sakstema:UNNTAK, behandlingstema:ANMODNING_OM_UNNTAK_HOVEDREGEL, behandlingstype:KLAGE
+        GyldigeKombinasjoner.rows.forEach {
+            try {
+                oppgaveGosysMapping.finnOppgave(it.sakstype, it.sakstema, it.behandlingstema, it.behandlingstype)
+            } catch (e: Exception) {
+                println(e.message)
+            }
+        }
+    }
+
 }
