d2162bd5d823b91bf1ec0b94e0a7e6e29a3f1398
Marton Skjæveland <56435736+martonSkjevelandNav@users.noreply.github.com>
2023-05-08 13:10:01 +0200

    5879 Bruker lovvalgsland om mottatteOpplysninger er AnmodningEllerAttest i saksopplysninger (#1800)

--- domain/src/main/java/no/nav/melosys/domain/mottatteopplysninger/data/Soeknadsland.java
+++ domain/src/main/java/no/nav/melosys/domain/mottatteopplysninger/data/Soeknadsland.java
@@ -1,9 +1,11 @@
 package no.nav.melosys.domain.mottatteopplysninger.data;
 
+import no.nav.melosys.domain.kodeverk.Land_iso2;
 import no.nav.melosys.domain.kodeverk.Trygdeavtale_myndighetsland;
 import no.nav.melosys.exception.TekniskException;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Soeknadsland {
@@ -17,6 +19,10 @@ public class Soeknadsland {
     public Soeknadsland(List<String> landkoder, boolean erUkjenteEllerAlleEosLand) {
         this.landkoder = landkoder;
         this.erUkjenteEllerAlleEosLand = erUkjenteEllerAlleEosLand;
+    }
+
+    public static Soeknadsland av(Land_iso2... lovvalgsland) {
+        return new Soeknadsland(Arrays.stream(lovvalgsland).map(Land_iso2::getKode).toList(), false);
     }
 
     public boolean erGyldig() {
