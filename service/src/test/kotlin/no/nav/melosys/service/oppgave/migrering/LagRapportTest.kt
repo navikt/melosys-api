ccc0d0843ddaa2e6fefa8c176bcc7fd7e71782dc
RuneLind <rulind@gmail.com>
2023-05-19 12:12:50 +0200

    Legg på beskrivelse og warning

--- service/src/test/kotlin/no/nav/melosys/service/oppgave/migrering/LagRapportTest.kt
+++ service/src/test/kotlin/no/nav/melosys/service/oppgave/migrering/LagRapportTest.kt
@@ -1,19 +1,36 @@
 package no.nav.melosys.service.oppgave.migrering
 
 import no.finn.unleash.FakeUnleash
+import no.nav.melosys.domain.Behandling
+import no.nav.melosys.domain.Fagsak
+import no.nav.melosys.domain.Saksopplysning
+import no.nav.melosys.domain.SaksopplysningType
+import no.nav.melosys.domain.dokument.sed.SedDokument
+import no.nav.melosys.domain.eessi.SedType
+import no.nav.melosys.domain.kodeverk.Sakstemaer
+import no.nav.melosys.domain.kodeverk.Sakstyper
+import no.nav.melosys.domain.kodeverk.behandlinger.Behandlingstema
+import no.nav.melosys.domain.kodeverk.behandlinger.Behandlingstyper
+import no.nav.melosys.featuretoggle.ToggleName
+import no.nav.melosys.service.LoggingTestUtils
 import no.nav.melosys.service.lovligekombinasjoner.GyldigeKombinasjoner
+import no.nav.melosys.service.oppgave.OppgaveFactory
 import no.nav.melosys.service.oppgave.OppgaveGosysMapping
 import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import java.io.File
+import java.time.LocalDate
 
 @Disabled("brukes bare for å lage oppgave migrering html rapporter, fjerns fra git etter at migreing er utført")
 class LagRapportTest {
     private val oppgaveGosysMapping = OppgaveGosysMapping(FakeUnleash())
+    private val oppgaveFactory = OppgaveFactory(FakeUnleash().apply { enable(ToggleName.NY_GOSYS_MAPPING) })
 
     @Test
     fun `lag rapport hvor kombinasjoner er gruppert etter antall pr kombinasjon`() {
-        val oppgaveGosysMapping = OppgaveGosysMapping(FakeUnleash().apply { enable(OppgaveGosysMapping.NY_GOSYS_MAPPING_UNTAKK_FOR_MIGRERING) })
+        val oppgaveGosysMapping = OppgaveGosysMapping(
+            FakeUnleash().apply { enable(OppgaveGosysMapping.NY_GOSYS_MAPPING_UNTAKK_FOR_MIGRERING) }
+        )
         val migreringsRapport = Migrering.migreringsRapportFraJson("/Users/rune/div/jsonrapport-prod.json")
 
         document {
@@ -40,8 +57,13 @@ class LagRapportTest {
                     .sortedBy { it.second }.toList().reversed().forEach {
                         val sak = it.first
                         val oppgave: OppgaveGosysMapping.Oppgave =
-                            oppgaveGosysMapping.finnOppgave(sak.sakstype, sak.sakstema, sak.behandlingstema, sak.behandlingstype)
-                        val fargeFraRegelTuffet = when(oppgave.regelTruffet) {
+                            oppgaveGosysMapping.finnOppgave(
+                                sak.sakstype,
+                                sak.sakstema,
+                                sak.behandlingstema,
+                                sak.behandlingstype
+                            )
+                        val fargeFraRegelTuffet = when (oppgave.regelTruffet) {
                             OppgaveGosysMapping.Regel.FRA_TABELL -> "MintCream"
                             OppgaveGosysMapping.Regel.HENVENDELSE -> "MistyRose"
                             OppgaveGosysMapping.Regel.HENVENDELSE_OG_VIRKSOMHET -> "Orchid"
@@ -88,6 +110,8 @@ class LagRapportTest {
                     item(bc = "DarkSalmon") { "Tema" }
                     item(bc = "DarkSalmon") { "Oppgavetype" }
                     item(bc = "DarkSalmon") { "Beskrivelsesfelt" }
+                    item(bc = "DarkSalmon") { "Beskrivelse" }
+                    item(bc = "DarkSalmon") { "Warning" }
                 }
                 GyldigeKombinasjoner.rowsMelosysOgDatavarehus.sortedWith(
                     compareBy(
@@ -96,39 +120,56 @@ class LagRapportTest {
                         { it.behandlingstype },
                         { it.behandlingstema }
                     )
-                ).forEach {
-                    val oppgave: OppgaveGosysMapping.Oppgave? = try {
-                        oppgaveGosysMapping.finnOppgave(it.sakstype, it.sakstema, it.behandlingstema, it.behandlingstype)
-                    } catch (e: Exception) {
-                        println(e.message)
-                        null
-                    }
-                    val fargeFraRegelTuffet = when (oppgave?.regelTruffet) {
-                        OppgaveGosysMapping.Regel.FRA_TABELL -> "MintCream"
-                        OppgaveGosysMapping.Regel.HENVENDELSE -> "MistyRose"
-                        OppgaveGosysMapping.Regel.HENVENDELSE_OG_VIRKSOMHET -> "Orchid"
-                        else -> null
-                    }
-                    val fontType = when (oppgave?.regelTruffet) {
-                        OppgaveGosysMapping.Regel.FRA_TABELL -> Font.NORMAL
-                        OppgaveGosysMapping.Regel.HENVENDELSE -> Font.ITALIC
-                        OppgaveGosysMapping.Regel.HENVENDELSE_OG_VIRKSOMHET -> Font.STRONG
-                        else -> Font.NORMAL
-                    }
-                    val maglerOppgaveFarge = if (oppgave == null) "Red" else null
-                    val maglerOppgaveFont = if (oppgave == null) Font.STRONG else Font.NORMAL
-                    tr {
-                        item(fc = maglerOppgaveFarge, font = maglerOppgaveFont) { it.sakstype }
-                        item(fc = maglerOppgaveFarge, font = maglerOppgaveFont) { it.sakstema }
-                        item(fc = maglerOppgaveFarge, font = maglerOppgaveFont) { it.behandlingstype }
-                        item(fc = maglerOppgaveFarge, font = maglerOppgaveFont) { it.behandlingstema }
-                        item(fc = maglerOppgaveFarge, bc = fargeFraRegelTuffet, font = fontType) { oppgave?.regelTruffet?.beskrivelse }
-                        item(fc = maglerOppgaveFarge, font = maglerOppgaveFont) { it.regel.beskrivelse }
-                        item(fc = maglerOppgaveFarge, font = maglerOppgaveFont) { oppgave?.oppgaveBehandlingstema?.kode }
-                        item(fc = maglerOppgaveFarge, font = maglerOppgaveFont) { oppgave?.oppgaveBehandlingstema?.name }
-                        item(fc = maglerOppgaveFarge, font = maglerOppgaveFont) { oppgave?.tema }
-                        item(fc = maglerOppgaveFarge, font = maglerOppgaveFont) { oppgave?.oppgaveType }
-                        item(fc = maglerOppgaveFarge, font = maglerOppgaveFont) { oppgave?.beskrivelsefelt }
+                ).forEachIndexed { index, it ->
+                    LoggingTestUtils.withLogAppender<OppgaveFactory> { oppgaveFactoryLog ->
+                        val oppgave: OppgaveGosysMapping.Oppgave =
+                            oppgaveGosysMapping.finnOppgave(
+                                it.sakstype,
+                                it.sakstema,
+                                it.behandlingstema,
+                                it.behandlingstype
+                            )
+                        val sedType = when (oppgave.beskrivelsefelt) {
+                            OppgaveGosysMapping.Beskrivelsefelt.TOMT, OppgaveGosysMapping.Beskrivelsefelt.BEHANDLINGSTEMA, OppgaveGosysMapping.Beskrivelsefelt.A1_ANMODNING_OM_UNNTAK_PAPIR -> null
+                            OppgaveGosysMapping.Beskrivelsefelt.SED -> if (oppgave.oppgaveBehandlingstema?.kode == "ab0482") null else SedType.A003
+                            OppgaveGosysMapping.Beskrivelsefelt.SED_ELLER_TOMT -> if (index % 2 == 1) SedType.A004 else null
+                        }
+                        val behandling = lagBehandling(
+                            it.sakstype,
+                            it.sakstema,
+                            it.behandlingstema,
+                            it.behandlingstype,
+                            sedType
+                        )
+                        val resultatOppgave = oppgaveFactory.lagBehandlingsoppgave(
+                            behandling, LocalDate.now()
+                        ) { behandling.finnSedDokument().orElse(null) }.build()
+
+                        val fargeFraRegelTuffet = when (oppgave.regelTruffet) {
+                            OppgaveGosysMapping.Regel.FRA_TABELL -> "MintCream"
+                            OppgaveGosysMapping.Regel.HENVENDELSE -> "MistyRose"
+                            OppgaveGosysMapping.Regel.HENVENDELSE_OG_VIRKSOMHET -> "Orchid"
+                        }
+                        val fontType = when (oppgave.regelTruffet) {
+                            OppgaveGosysMapping.Regel.FRA_TABELL -> Font.NORMAL
+                            OppgaveGosysMapping.Regel.HENVENDELSE -> Font.ITALIC
+                            OppgaveGosysMapping.Regel.HENVENDELSE_OG_VIRKSOMHET -> Font.STRONG
+                        }
+                        tr {
+                            item { it.sakstype }
+                            item { it.sakstema }
+                            item { it.behandlingstype }
+                            item { it.behandlingstema }
+                            item(bc = fargeFraRegelTuffet, font = fontType) { oppgave.regelTruffet.beskrivelse }
+                            item { it.regel.beskrivelse }
+                            item { oppgave.oppgaveBehandlingstema?.kode }
+                            item { oppgave.oppgaveBehandlingstema?.name }
+                            item { oppgave.tema }
+                            item { oppgave.oppgaveType }
+                            item { oppgave.beskrivelsefelt }
+                            item { resultatOppgave.beskrivelse }
+                            item { oppgaveFactoryLog.list.joinToString(",") }
+                        }
                     }
                 }
             }
@@ -141,4 +182,32 @@ class LagRapportTest {
             }
         }
     }
+
+    private fun lagBehandling(
+        sakstype: Sakstyper,
+        sakstema: Sakstemaer,
+        behandlingstema: Behandlingstema,
+        behandlingstype: Behandlingstyper,
+        sedType: SedType? = null
+    ): Behandling {
+        return Behandling().apply {
+            id = 1
+            fagsak = Fagsak().apply {
+                saksnummer = "MEL-1"
+                type = sakstype
+                tema = sakstema
+            }
+            type = behandlingstype
+            tema = behandlingstema
+            if (sedType != null) {
+                saksopplysninger.add(Saksopplysning().apply {
+                    type = SaksopplysningType.SEDOPPL
+                    dokument = SedDokument().apply {
+                        setSedType(sedType)
+                    }
+                })
+            }
+        }
+    }
+
 }
