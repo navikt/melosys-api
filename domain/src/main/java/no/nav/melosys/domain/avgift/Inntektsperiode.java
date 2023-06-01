5e04f2449de2210e7b24005c821102664c5a6d52
Francois Allix <32903762+frans-a@users.noreply.github.com>
2023-06-01 10:11:59 +0200

    Riktig logikk for Trygdeavgiftmottaker kommer senere
    Se https://confluence.adeo.no/display/TEESSI/Lagring+av+informasjon+om+trygdeavgift+i+Melosys

--- domain/src/main/java/no/nav/melosys/domain/avgift/Inntektsperiode.java
+++ domain/src/main/java/no/nav/melosys/domain/avgift/Inntektsperiode.java
@@ -135,6 +135,7 @@ public class Inntektsperiode {
             + arbeidsgiversavgiftBetalesTilSkatt + ", trygdeavgiftBetalesTilSkatt=" + trygdeavgiftBetalesTilSkatt + '}';
     }
 
+    // FIXME Riktig logikk for Trygdeavgiftmottaker kommer i egen historie. Dette er for å fikse MELOSYS-5927
     public boolean trygdeavgiftBetalesBådeTilNavOgSkatt() {
         return isTrygdeavgiftBetalesTilSkatt() && !isArbeidsgiversavgiftBetalesTilSkatt() && !erSpesiellGruppe(getType());
     }
