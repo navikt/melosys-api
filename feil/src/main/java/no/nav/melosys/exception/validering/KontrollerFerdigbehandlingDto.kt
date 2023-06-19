c2a4c894fc8fb2ccec35e17c76bc19e5c45e12ce
Fredrik Olsen Hauge <fredrik.olsen.hauge@nav.no>
2023-06-19 09:49:16 +0200

    Konverterte KontrollerFerdigbehandlingDto til kotlin data class

--- feil/src/main/java/no/nav/melosys/exception/validering/KontrollerFerdigbehandlingDto.kt
+++ feil/src/main/java/no/nav/melosys/exception/validering/KontrollerFerdigbehandlingDto.kt
@@ -1,19 +1,3 @@
-package no.nav.melosys.exception.validering;
+package no.nav.melosys.exception.validering
 
-import java.util.List;
-
-public class KontrollerFerdigbehandlingDto {
-    private List<KontrollfeilDto> kontrollfeilList;
-
-    public KontrollerFerdigbehandlingDto(List<KontrollfeilDto> kontrollfeilList) {
-        this.kontrollfeilList = kontrollfeilList;
-    }
-
-    public List<KontrollfeilDto> getKontrollfeilList() {
-        return kontrollfeilList;
-    }
-
-    public void setKontrollfeilList(List<KontrollfeilDto> kontrollfeilList) {
-        this.kontrollfeilList = kontrollfeilList;
-    }
-}
+data class KontrollerFerdigbehandlingDto(var kontrollfeilList: List<KontrollfeilDto>)
