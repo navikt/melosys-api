4b66cb71549e7fa79fbc531d19caef85336d7b47
Francois Allix <32903762+frans-a@users.noreply.github.com>
2023-06-21 10:24:22 +0200

    Audit logger

---
+++ sikkerhet/src/main/kotlin/no/nav/melosys/sikkerhet/logging/AuditEvent.kt
@@ -0,0 +1,16 @@
+package no.nav.melosys.sikkerhet.logging
+
+data class AuditEvent(
+    val type: AuditEventType,
+    val sourceUserId: String,
+    val destinationUserId: String,
+    val message: String?,
+    val sourceProcessName: String?,
+)
+
+enum class AuditEventType() {
+    CREATE,
+    READ,
+    UPDATE,
+    DELETE,
+}
