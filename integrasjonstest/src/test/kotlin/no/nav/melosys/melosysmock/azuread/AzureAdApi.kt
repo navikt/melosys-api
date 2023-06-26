f2c546d748172c5687c86d17b7992a2e904d084a
RuneLind <rulind@gmail.com>
2023-06-26 11:16:02 +0200

    AzureAdApi må være @Unprotected for tester

--- integrasjonstest/src/test/kotlin/no/nav/melosys/melosysmock/azuread/AzureAdApi.kt
+++ integrasjonstest/src/test/kotlin/no/nav/melosys/melosysmock/azuread/AzureAdApi.kt
@@ -1,9 +1,11 @@
 package no.nav.melosys.melosysmock.azuread
 
+import no.nav.security.token.support.core.api.Unprotected
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/graph/v1.0")
+@Unprotected
 class AzureAdApi {
 
     @GetMapping("/users")
