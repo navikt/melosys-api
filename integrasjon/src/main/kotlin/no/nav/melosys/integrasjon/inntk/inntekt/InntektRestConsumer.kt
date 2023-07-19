d8f9c8ba3c1ee8b52634b113b659177f0a758dd1
RuneLind <rulind@gmail.com>
2023-07-19 18:00:31 +0200

    Trenger ikke CallIdAware

--- integrasjon/src/main/kotlin/no/nav/melosys/integrasjon/inntk/inntekt/InntektRestConsumer.kt
+++ integrasjon/src/main/kotlin/no/nav/melosys/integrasjon/inntk/inntekt/InntektRestConsumer.kt
@@ -1,14 +1,13 @@
 package no.nav.melosys.integrasjon.inntk.inntekt
 
 import no.nav.melosys.exception.TekniskException
-import no.nav.melosys.integrasjon.felles.CallIdAware
 import org.springframework.http.MediaType
 import org.springframework.retry.annotation.Retryable
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.bodyToMono
 
 @Retryable
-open class InntektRestConsumer(private val webClient: WebClient) : CallIdAware {
+open class InntektRestConsumer(private val webClient: WebClient) {
 
     fun hentInntektListe(inntektRequest: InntektRequest) : InntektResponse {
         return webClient.post()
