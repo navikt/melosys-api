d915ae4a181b46c4af6604e586f17d0ccdc10e8a
RuneLind <rulind@gmail.com>
2023-07-21 15:32:18 +0200

    Legg til InntektRestApi

---
+++ integrasjonstest/src/test/kotlin/no/nav/melosys/melosysmock/inntekt/InntektRestApi.kt
@@ -0,0 +1,21 @@
+package no.nav.melosys.melosysmock.inntekt
+
+import no.nav.melosys.integrasjon.inntk.inntekt.Aktoer
+import no.nav.melosys.integrasjon.inntk.inntekt.AktoerType
+import no.nav.melosys.integrasjon.inntk.inntekt.InntektRequest
+import no.nav.melosys.integrasjon.inntk.inntekt.InntektResponse
+import no.nav.security.token.support.core.api.Unprotected
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/inntektskomponenten/rs/api/v1")
+@Unprotected
+class InntektRestApi {
+    @PostMapping("/hentinntektliste")
+    fun hentInntekt(@RequestBody inntektRequest: InntektRequest): InntektResponse {
+        return InntektResponse(listOf(), Aktoer(inntektRequest.ident.identifikator, AktoerType.AKTOER_ID))
+    }
+}
