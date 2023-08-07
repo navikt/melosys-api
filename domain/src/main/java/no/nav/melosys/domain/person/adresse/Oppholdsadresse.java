977abe85c421976778f6b8f1e00e6bef85fcc1ee
Francois Allix <32903762+frans-a@users.noreply.github.com>
2023-08-07 09:47:06 +0200

    Postnummer kan være null

--- domain/src/main/java/no/nav/melosys/domain/person/adresse/Oppholdsadresse.java
+++ domain/src/main/java/no/nav/melosys/domain/person/adresse/Oppholdsadresse.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 import no.nav.melosys.domain.adresse.StrukturertAdresse;
+import org.apache.commons.lang3.StringUtils;
 
 public record Oppholdsadresse(
     StrukturertAdresse strukturertAdresse,
@@ -18,6 +19,6 @@ public record Oppholdsadresse(
 
     @Override
     public boolean harRegistrertAdresse() {
-        return strukturertAdresse != null && !strukturertAdresse.erTom() && !strukturertAdresse.getPostnummer().isBlank();
+        return strukturertAdresse != null && !strukturertAdresse.erTom() && !StringUtils.isBlank(strukturertAdresse.getPostnummer());
     }
 }
