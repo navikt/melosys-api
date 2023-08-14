c041500f01d3eeb86b246b34a48f341c50448854
Fredrik Olsen Hauge <fredrik.olsen.hauge@nav.no>
2023-08-14 14:53:49 +0200

    Nytt endepunkt for uthenting av trygdeavgiftmottaker.
    Endepunktet benyttes for å vise riktig tekst i ftrl vedtakssteg

---
+++ frontend-api/src/main/kotlin/no/nav/melosys/tjenester/gui/dto/trygdeavgift/TrygdeavgiftMottakerDto.kt
@@ -0,0 +1,5 @@
+package no.nav.melosys.tjenester.gui.dto.trygdeavgift
+
+import no.nav.melosys.domain.kodeverk.Trygdeavgiftmottaker
+
+data class TrygdeavgiftMottakerDto(val trygdeavgiftMottaker: Trygdeavgiftmottaker)
