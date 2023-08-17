a303288dbf31547ecf9e9be843a1a0791a121699
Isa Salem Hame <isa.salem.hame@nav.no>
2023-08-17 13:34:43 +0200

    5880: Fjern toggle for unntaksflyten (#1942)
    * 5880: Fjern toggle for unntaksflyten
    
    * Fjern unødvendig bruk av unleash
    
    * Fjern unødvendig bruk av unleash
    
    * Fikser tester etter fjerning av unleash
    
    * Fikser enda flere tester etter fjerning av unleash

--- config/src/main/java/no/nav/melosys/featuretoggle/ToggleName.java
+++ config/src/main/java/no/nav/melosys/featuretoggle/ToggleName.java
@@ -7,7 +7,5 @@ public class ToggleName {
 
     public static final String FOLKETRYGDEN_MVP = "melosys.folketrygden.mvp";
 
-    public static final String REGISTRERING_UNNTAK_FRA_MEDLEMSKAP = "melosys.registrering_unntak_fra_medlemskap";
-
     public static final String OPPDATER_BEHANDLINGSRESULTAT = "melosys.oppdater.behandlingsresultat";
 }
