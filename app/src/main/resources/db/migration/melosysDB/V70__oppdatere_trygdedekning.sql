1b9f2c0531962e9a9b685882abbb06c4f7c0e2c3
martonSkjevelandNav <marton.skjeveland@nav.no>
2023-08-17 14:12:38 +0200

    Oppdaterer trygdedekning i mottatteOpplysninger

--- app/src/main/resources/db/migration/melosysDB/V70__oppdatere_trygdedekning.sql
+++ app/src/main/resources/db/migration/melosysDB/V70__oppdatere_trygdedekning.sql
@@ -45,3 +45,34 @@ where trygde_dekning = 'HELSE_OG_PENSJONSDEL';
 update medlemskapsperiode
 set trygde_dekning = 'FTRL_2_9_FØRSTE_LEDD_C_ANDRE_LEDD_HELSE_PENSJON_SYKE_FORELDREPENGER'
 where trygde_dekning = 'HELSE_OG_PENSJONSDEL_MED_SYKE_OG_FORELDREPENGER';
+
+UPDATE MOTTATTEOPPLYSNINGER
+SET DATA = REPLACE(
+    REPLACE(
+        REPLACE(
+            REPLACE(
+                REPLACE(
+                    DATA,
+                    'HELSEDEL',
+                    'FTRL_2_9_FØRSTE_LEDD_A_HELSE'
+                    ),
+                'HELSEDEL_MED_SYKE_OG_FORELDREPENGER',
+                'FTRL_2_9_FØRSTE_LEDD_A_ANDRE_LEDD_HELSE_SYKE_FORELDREPENGER'
+                ),
+            'PENSJONSDEL',
+            'FTRL_2_9_FØRSTE_LEDD_B_PENSJON'
+            ),
+        'HELSE_OG_PENSJONSDEL',
+        'FTRL_2_9_FØRSTE_LEDD_C_HELSE_PENSJON'
+        ),
+    'HELSE_OG_PENSJONSDEL_MED_SYKE_OG_FORELDREPENGER',
+    'FTRL_2_9_FØRSTE_LEDD_C_ANDRE_LEDD_HELSE_PENSJON_SYKE_FORELDREPENGER'
+    )
+WHERE JSON_VALUE(DATA, '$.trygdedekning')
+          IN (
+              'HELSEDEL',
+              'HELSEDEL_MED_SYKE_OG_FORELDREPENGER',
+              'PENSJONSDEL',
+              'HELSE_OG_PENSJONSDEL',
+              'HELSE_OG_PENSJONSDEL_MED_SYKE_OG_FORELDREPENGER'
+          );
