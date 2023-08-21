e4e41bc076402d4581de67335aadbeda1cbea87c
Isa Salem Hame <wanghattan@gmail.com>
2023-08-21 09:38:09 +0200

    5974: Fjern toggle IKKE_YRKESAKTIV fra ToggleName

--- config/src/main/java/no/nav/melosys/featuretoggle/ToggleName.java
+++ config/src/main/java/no/nav/melosys/featuretoggle/ToggleName.java
@@ -3,8 +3,6 @@ package no.nav.melosys.featuretoggle;
 public class ToggleName {
     public static final String BEHANDLINGSTYPE_KLAGE = "melosys.behandlingstype.klage";
 
-    public static final String IKKEYRKESAKTIV_FLYT = "melosys.ikkeYrkesaktivForenkletFlyt";
-
     public static final String FOLKETRYGDEN_MVP = "melosys.folketrygden.mvp";
 
     public static final String OPPDATER_BEHANDLINGSRESULTAT = "melosys.oppdater.behandlingsresultat";
