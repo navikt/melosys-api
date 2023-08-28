71300adb946fea408d3be0712bddc11d026122bb
barstadEkstern <oystein.madsen.barstad@nav.no>
2023-08-28 12:27:10 +0200

    fjern unleash

--- service/src/test/java/no/nav/melosys/service/eessi/ArbeidFlereLandSedRuterTest.java
+++ service/src/test/java/no/nav/melosys/service/eessi/ArbeidFlereLandSedRuterTest.java
@@ -36,9 +36,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.*;
-import static org.mockito.Mockito.never;
 
 @ExtendWith(MockitoExtension.class)
 class ArbeidFlereLandSedRuterTest {
