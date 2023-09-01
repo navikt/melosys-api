8f07aac7252816d3af39f258a2d09a883c3dc346
Fredrik Olsen Hauge <45845087+fredrik-oh@users.noreply.github.com>
2023-09-01 12:17:22 +0200

    6008 TOGGLE La til periode på Inntektskilder i trygdeavgiftsgrunnlag. Aksept… (#1968)
    * 6008 La til periode på Inntektskilder i trygdeavgiftsgrunnlag. Aksepterer ukritisk periodene lagt inn av saksbehandler, validering løses i 6010
    
    * Fikset mangelfulle konstruktører i TrygdeavgiftsgrunnlagServiceTest
    
    ---------
    
    Co-authored-by: Fredrik Olsen Hauge <fredrik.olsen.hauge@nav.no>

--- frontend-api/src/main/kotlin/no/nav/melosys/tjenester/gui/dto/trygdeavgift/InntekskildeDto.kt
+++ frontend-api/src/main/kotlin/no/nav/melosys/tjenester/gui/dto/trygdeavgift/InntekskildeDto.kt
@@ -4,18 +4,23 @@ import no.nav.melosys.domain.avgift.Inntektsperiode
 import no.nav.melosys.domain.kodeverk.Inntektskildetype
 import no.nav.melosys.service.avgift.dto.InntektskildeRequest
 import java.math.BigDecimal
+import java.time.LocalDate
 
 data class InntekskildeDto(
     val type: Inntektskildetype,
     val arbeidsgiversavgiftBetales: Boolean,
-    val avgiftspliktigInntektMnd: BigDecimal?
+    val avgiftspliktigInntektMnd: BigDecimal?,
+    val fomDato: LocalDate,
+    val tomDato: LocalDate,
 ) {
     constructor(inntektsperiode: Inntektsperiode) : this(
         inntektsperiode.type,
         inntektsperiode.isArbeidsgiversavgiftBetalesTilSkatt,
-        inntektsperiode.avgiftspliktigInntektMnd?.verdi
+        inntektsperiode.avgiftspliktigInntektMnd?.verdi,
+        inntektsperiode.fomDato,
+        inntektsperiode.tomDato
     )
 
     fun tilRequest(): InntektskildeRequest =
-        InntektskildeRequest(type, arbeidsgiversavgiftBetales, avgiftspliktigInntektMnd)
+        InntektskildeRequest(type, arbeidsgiversavgiftBetales, avgiftspliktigInntektMnd, fomDato, tomDato)
 }
