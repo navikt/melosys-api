81249221addafb40bd6bc745379d336c2aa4ef80
Janita Johansen <janita.marielle.johansen@nav.no>
2023-09-05 14:13:41 +0200

    Ny tjeneste for avslag manglende opplysninger. Denne gjør alt det som ble gjort i prosessinstansene tidligere, men er nå synkront.

---
+++ frontend-api/src/main/java/no/nav/melosys/tjenester/gui/saksflyt/AvslagTjeneste.java
@@ -0,0 +1,39 @@
+package no.nav.melosys.tjenester.gui.saksflyt;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import no.nav.melosys.service.saksflyt.AvslagService;
+import no.nav.melosys.service.tilgang.Aksesskontroll;
+import no.nav.melosys.sikkerhet.context.SubjectHandler;
+import no.nav.melosys.tjenester.gui.dto.AvslagDto;
+import no.nav.security.token.support.core.api.Protected;
+import org.springframework.context.annotation.Scope;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.WebApplicationContext;
+
+@Protected
+@RestController
+@RequestMapping("/saksflyt/avslag")
+@Api(tags = {"saksflyt", "avslag"})
+@Scope(value = WebApplicationContext.SCOPE_REQUEST)
+public class AvslagTjeneste {
+
+    private final Aksesskontroll aksesskontroll;
+    private final AvslagService avslagService;
+
+    public AvslagTjeneste(Aksesskontroll aksesskontroll, AvslagService avslagService) {
+        this.aksesskontroll = aksesskontroll;
+        this.avslagService = avslagService;
+    }
+
+    @PostMapping("{behandlingID}/manglende-opplysninger")
+    @ApiOperation(value = "Avslår behandling pga manglende opplysninger")
+    public ResponseEntity<Void> fattVedtak(@PathVariable("behandlingID") long behandlingID,
+                                           @RequestBody AvslagDto avslagDto) {
+        aksesskontroll.autoriserSkriv(behandlingID);
+
+        avslagService.avslåPgaManglendeOpplysninger(behandlingID, avslagDto.getFritekst(), SubjectHandler.getInstance().getUserID());
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ frontend-api/src/main/kotlin/no/nav/melosys/tjenester/gui/dto/AvslagDto.kt
@@ -0,0 +1,3 @@
+package no.nav.melosys.tjenester.gui.dto
+
+data class AvslagDto(val fritekst: String)
