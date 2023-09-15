92cd0ee52e3edc42be11664c6bdafa4a6618c266
barstadEkstern <oystein.madsen.barstad@nav.no>
2023-09-15 12:12:53 +0200

    Review kommentarer

---
+++ saksflyt/src/main/java/no/nav/melosys/saksflyt/steg/sed/OpprettTidligereJournalpostForSak.kt
@@ -0,0 +1,45 @@
+package no.nav.melosys.saksflyt.steg.sed
+
+import no.finn.unleash.Unleash
+import no.nav.melosys.domain.eessi.melding.MelosysEessiMelding
+import no.nav.melosys.domain.saksflyt.ProsessDataKey
+import no.nav.melosys.domain.saksflyt.ProsessSteg
+import no.nav.melosys.domain.saksflyt.Prosessinstans
+import no.nav.melosys.featuretoggle.ToggleName
+import no.nav.melosys.integrasjon.joark.JoarkFasade
+import no.nav.melosys.saksflyt.steg.StegBehandler
+import no.nav.melosys.service.dokument.sed.EessiService
+import org.springframework.stereotype.Component
+import java.util.*
+
+
+@Component
+class OpprettTidligereJournalpostForSak(private val joarkFasade: JoarkFasade,
+                                        private val eessiService: EessiService,
+                                        private val unleash: Unleash) : StegBehandler {
+    override fun inngangsSteg(): ProsessSteg {
+        return ProsessSteg.OPPRETT_TIDLIGERE_JOURNALPOSTER_FOR_SAK
+    }
+
+    override fun utfør(prosessinstans: Prosessinstans) {
+        if (unleash.isEnabled(ToggleName.IKKE_JOURNALFOER_UTEN_PID)) {
+            finnEessiMelding(prosessinstans).ifPresent { melosysEessiMelding: MelosysEessiMelding ->
+                eessiService.opprettJournalpostForTidligereSed(
+                    melosysEessiMelding.rinaSaksnummer
+                )
+            }
+        }
+    }
+
+    private fun finnEessiMelding(prosessinstans: Prosessinstans): Optional<MelosysEessiMelding> {
+        val eessiMelding = prosessinstans.getData(ProsessDataKey.EESSI_MELDING, MelosysEessiMelding::class.java)
+        if (eessiMelding != null) {
+            return Optional.of(eessiMelding)
+        }
+        val journalpostID = prosessinstans.getData(ProsessDataKey.JOURNALPOST_ID)
+        return if (joarkFasade.hentJournalpost(journalpostID).mottaksKanalErEessi()) {
+            Optional.of(eessiService.hentSedTilknyttetJournalpost(journalpostID))
+        } else Optional.empty()
+    }
+}
+
---
+++ saksflyt/src/test/java/no/nav/melosys/saksflyt/steg/sed/OpprettTidligereJournalpostForSakTest.kt
@@ -0,0 +1,90 @@
+package no.nav.melosys.saksflyt.steg.sed;
+
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import no.finn.unleash.FakeUnleash
+import no.nav.melosys.domain.Behandling
+import no.nav.melosys.domain.Fagsak
+import no.nav.melosys.domain.arkiv.Journalpost
+import no.nav.melosys.domain.eessi.melding.MelosysEessiMelding
+import no.nav.melosys.domain.saksflyt.ProsessDataKey
+import no.nav.melosys.domain.saksflyt.Prosessinstans
+import no.nav.melosys.integrasjon.joark.JoarkFasade
+import no.nav.melosys.service.dokument.sed.EessiService
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.junit.jupiter.MockitoExtension
+
+@ExtendWith(MockitoExtension::class)
+class OpprettTidligereJournalpostForSakTest {
+
+    private val eessiService: EessiService = mockk()
+    private val joarkFasade: JoarkFasade = mockk()
+
+    private val fakeUnleash = FakeUnleash()
+
+    private lateinit var opprettTidligereJournalpostForSak: OpprettTidligereJournalpostForSak
+
+    private val JOURNALPOST_ID = "123"
+
+    @BeforeEach
+    fun setup() {
+        fakeUnleash.enableAll()
+        opprettTidligereJournalpostForSak = OpprettTidligereJournalpostForSak(joarkFasade, eessiService, fakeUnleash)
+    }
+
+    @Test
+    fun utfør_journalpostUtenEessiMelding_verifiserOpprettJournalposter() {
+        val prosessinstans = Prosessinstans()
+        prosessinstans.setData(ProsessDataKey.JOURNALPOST_ID, JOURNALPOST_ID)
+
+        val fagsak = Fagsak()
+        fagsak.gsakSaksnummer = 123L
+        val behandling = Behandling()
+        behandling.fagsak = fagsak
+        prosessinstans.behandling = behandling
+
+        val journalpost = Journalpost(JOURNALPOST_ID)
+        journalpost.mottaksKanal = "EESSI"
+        every { joarkFasade.hentJournalpost(JOURNALPOST_ID) } returns journalpost
+
+        val melosysEessiMelding = MelosysEessiMelding()
+        melosysEessiMelding.bucType = "LA_BUC_04"
+        melosysEessiMelding.rinaSaksnummer = "321323"
+        every { eessiService.hentSedTilknyttetJournalpost(JOURNALPOST_ID) } returns melosysEessiMelding
+        every { eessiService.opprettJournalpostForTidligereSed(any()) } returns Unit
+
+        opprettTidligereJournalpostForSak.utfør(prosessinstans)
+
+        verify(exactly = 1) {
+            eessiService.opprettJournalpostForTidligereSed(melosysEessiMelding.rinaSaksnummer)
+        }
+    }
+
+    @Test
+    fun utfør_journalpostMedEessimelding_verifiserOpprettJournalposter() {
+        val melosysEessiMelding = MelosysEessiMelding()
+        melosysEessiMelding.bucType = "LA_BUC_04"
+        melosysEessiMelding.rinaSaksnummer = "321323"
+        melosysEessiMelding.journalpostId = JOURNALPOST_ID
+
+        val prosessinstans = Prosessinstans()
+        prosessinstans.setData(ProsessDataKey.EESSI_MELDING, melosysEessiMelding)
+
+        val fagsak = Fagsak()
+        fagsak.gsakSaksnummer = 123L
+        val behandling = Behandling()
+        behandling.fagsak = fagsak
+        prosessinstans.behandling = behandling
+
+        every { eessiService.opprettJournalpostForTidligereSed(any()) } returns Unit
+
+        opprettTidligereJournalpostForSak.utfør(prosessinstans)
+
+        verify(exactly = 1) {
+            eessiService.opprettJournalpostForTidligereSed(melosysEessiMelding.rinaSaksnummer)
+        }
+    }
+}
