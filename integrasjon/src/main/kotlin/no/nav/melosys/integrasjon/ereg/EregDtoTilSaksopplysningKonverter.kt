18ecc8d8d4ddf3d18cb50ce81397f33f0f111a7a
RuneLind <rulind@gmail.com>
2023-09-19 17:22:20 +0200

    Bruk expression body

--- integrasjon/src/main/kotlin/no/nav/melosys/integrasjon/ereg/EregDtoTilSaksopplysningKonverter.kt
+++ integrasjon/src/main/kotlin/no/nav/melosys/integrasjon/ereg/EregDtoTilSaksopplysningKonverter.kt
@@ -11,72 +11,63 @@ import no.nav.melosys.domain.dokument.organisasjon.adresse.elektronisk.Telefonnu
 import no.nav.melosys.integrasjon.ereg.organisasjon.OrganisasjonResponse
 
 class EregDtoTilSaksopplysningKonverter {
-    fun lagSaksopplysning(organisasjon: OrganisasjonResponse.Organisasjon): Saksopplysning {
-        return Saksopplysning().apply {
-            dokument = OrganisasjonDokument().apply {
-                navn = listOf(organisasjon.navn?.sammensattnavn ?: "UKJENT")
-                organisasjonDetaljer = OrganisasjonsDetaljer().apply {
-                    orgnummer = organisasjon.organisasjonsnummer
-                    navn = tilNavn(organisasjon.organisasjonDetaljer?.navn)
-                    forretningsadresse = tilGeografiskAdresse(organisasjon.organisasjonDetaljer?.forretningsadresser)
-                    postadresse = tilGeografiskAdresse(organisasjon.organisasjonDetaljer?.postadresser)
-                    telefon = tilTelefon(organisasjon.organisasjonDetaljer?.telefonnummer)
-                    epostadresse = tilEpost(organisasjon.organisasjonDetaljer?.epostadresser)
-                    naering = organisasjon.organisasjonDetaljer?.naeringer?.map { it.naeringskode } ?: emptyList()
-                    opphoersdato = organisasjon.organisasjonDetaljer?.opphoersdato
-                }
+    fun lagSaksopplysning(organisasjon: OrganisasjonResponse.Organisasjon): Saksopplysning = Saksopplysning().apply {
+        dokument = OrganisasjonDokument().apply {
+            navn = listOf(organisasjon.navn?.sammensattnavn ?: "UKJENT")
+            organisasjonDetaljer = OrganisasjonsDetaljer().apply {
+                orgnummer = organisasjon.organisasjonsnummer
+                navn = tilNavn(organisasjon.organisasjonDetaljer?.navn)
+                forretningsadresse = tilGeografiskAdresse(organisasjon.organisasjonDetaljer?.forretningsadresser)
+                postadresse = tilGeografiskAdresse(organisasjon.organisasjonDetaljer?.postadresser)
+                telefon = tilTelefon(organisasjon.organisasjonDetaljer?.telefonnummer)
+                epostadresse = tilEpost(organisasjon.organisasjonDetaljer?.epostadresser)
+                naering = organisasjon.organisasjonDetaljer?.naeringer?.map { it.naeringskode } ?: emptyList()
+                opphoersdato = organisasjon.organisasjonDetaljer?.opphoersdato
             }
         }
     }
 
-    private fun tilNavn(navn: List<OrganisasjonResponse.Navn>?): List<Organisasjonsnavn> =
-        navn?.map {
-            Organisasjonsnavn().apply {
-                bruksperiode = it.bruksperiode.tilPeriode()
-                gyldighetsperiode = it.gyldighetsperiode.tilPeriode()
-                this.navn = listOf(it.sammensattnavn)
-                redigertNavn = it.sammensattnavn
-            }
-        } ?: emptyList()
+    private fun tilNavn(navn: List<OrganisasjonResponse.Navn>?): List<Organisasjonsnavn> = navn?.map {
+        Organisasjonsnavn().apply {
+            bruksperiode = it.bruksperiode.tilPeriode()
+            gyldighetsperiode = it.gyldighetsperiode.tilPeriode()
+            this.navn = listOf(it.sammensattnavn)
+            redigertNavn = it.sammensattnavn
+        }
+    } ?: emptyList()
 
-    private fun tilTelefon(telefonnummer: List<OrganisasjonResponse.Telefonnummer>?): List<Telefonnummer> {
-        return telefonnummer?.map {
-            Telefonnummer().apply {
-                identifikator = it.nummer
-                type = it.telefontype
-                retningsnummer = null // skal vi finne ut retningsnummer?
-                bruksperiode = it.bruksperiode.tilPeriode()
-                gyldighetsperiode = it.gyldighetsperiode.tilPeriode()
-            }
-        } ?: return emptyList()
-    }
+    private fun tilTelefon(telefonnummer: List<OrganisasjonResponse.Telefonnummer>?): List<Telefonnummer> = telefonnummer?.map {
+        Telefonnummer().apply {
+            identifikator = it.nummer
+            type = it.telefontype
+            retningsnummer = null // skal vi finne ut retningsnummer?
+            bruksperiode = it.bruksperiode.tilPeriode()
+            gyldighetsperiode = it.gyldighetsperiode.tilPeriode()
+        }
+    } ?: emptyList()
 
-    private fun tilEpost(epost: List<OrganisasjonResponse.Epostadresse>?): List<Epost> {
-        return epost?.map {
-            Epost().apply {
-                identifikator = it.adresse
-                bruksperiode = it.bruksperiode.tilPeriode()
-                gyldighetsperiode = it.gyldighetsperiode.tilPeriode()
-            }
-        } ?: return emptyList()
-    }
+    private fun tilEpost(epost: List<OrganisasjonResponse.Epostadresse>?): List<Epost> = epost?.map {
+        Epost().apply {
+            identifikator = it.adresse
+            bruksperiode = it.bruksperiode.tilPeriode()
+            gyldighetsperiode = it.gyldighetsperiode.tilPeriode()
+        }
+    } ?: emptyList()
 
-    private fun tilGeografiskAdresse(adresser: List<OrganisasjonResponse.Adresse>?): List<SemistrukturertAdresse> {
-        return adresser?.map {
-            SemistrukturertAdresse().apply {
-                bruksperiode = it.bruksperiode.tilPeriode()
-                gyldighetsperiode = it.gyldighetsperiode.tilPeriode()
-                landkode = it.landkode
-                adresselinje1 = it.adresselinje1
-                adresselinje2 = it.adresselinje2
-                adresselinje3 = it.adresselinje3
-                postnr = it.postnummer
-                poststed = it.poststed
-                kommunenr = it.kommunenummer
-                landkode = it.landkode
-            }
-        } ?: return emptyList()
-    }
+    private fun tilGeografiskAdresse(adresser: List<OrganisasjonResponse.Adresse>?): List<SemistrukturertAdresse> = adresser?.map {
+        SemistrukturertAdresse().apply {
+            bruksperiode = it.bruksperiode.tilPeriode()
+            gyldighetsperiode = it.gyldighetsperiode.tilPeriode()
+            landkode = it.landkode
+            adresselinje1 = it.adresselinje1
+            adresselinje2 = it.adresselinje2
+            adresselinje3 = it.adresselinje3
+            postnr = it.postnummer
+            poststed = it.poststed
+            kommunenr = it.kommunenummer
+            landkode = it.landkode
+        }
+    } ?: emptyList()
 
     private fun OrganisasjonResponse.Gyldighetsperiode.tilPeriode(): Periode = Periode(fom, tom)
 
