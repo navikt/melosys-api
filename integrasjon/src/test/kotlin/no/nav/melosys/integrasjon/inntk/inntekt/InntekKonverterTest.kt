a807c5647d60f285ce2eca5c4f9a418a16054a65
Rune Lind <rune.lind@nav.no>
2023-09-25 13:19:39 +0200

    6022 TOGGLE fiks mapping feil - svalbard kostoglosji (#2005)
    * Flytt json test data til egen test ressurs
    * Fiks feil i InntektKonverter og legg på test

---
+++ integrasjon/src/test/kotlin/no/nav/melosys/integrasjon/inntk/inntekt/InntekKonverterTest.kt
@@ -0,0 +1,87 @@
+package no.nav.melosys.integrasjon.inntk.inntekt
+
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import io.kotest.matchers.collections.shouldHaveSize
+import io.kotest.matchers.nulls.shouldNotBeNull
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.types.shouldBeTypeOf
+import no.nav.melosys.domain.dokument.inntekt.InntektDokument
+import no.nav.melosys.domain.dokument.inntekt.tillegsinfo.BonusFraForsvaret
+import no.nav.melosys.domain.dokument.inntekt.tillegsinfo.ReiseKostOgLosji
+import no.nav.melosys.domain.dokument.inntekt.tillegsinfo.Svalbardinntekt
+import no.nav.melosys.exception.IkkeFunnetException
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import java.math.BigDecimal
+import java.nio.charset.StandardCharsets
+import java.time.Year
+import java.time.YearMonth
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class InntekKonverterTest {
+
+    @Test
+    fun test() {
+        val inntektResponse = jacksonObjectMapper()
+            .registerModule(JavaTimeModule())
+            .readValue<InntektResponse>(hentRessurs("mock/inntekt/inntektConsumerResponse.json"))
+
+        val saksopplysning = InntektKonverter().lagSaksopplysning(inntektResponse)
+
+        saksopplysning.dokument
+            .shouldNotBeNull()
+            .shouldBeTypeOf<InntektDokument>()
+            .arbeidsInntektMaanedListe
+            .shouldHaveSize(3)
+            .toList()
+            .run {
+                get(0).run {
+                    aarMaaned.shouldBe(YearMonth.parse("2022-01"))
+                    arbeidsInntektInformasjon.inntektListe.shouldHaveSize(1)
+                        .first().run {
+                            beloep.shouldBe(BigDecimal(50000))
+                            tilleggsinformasjon
+                                .tilleggsinformasjonDetaljer
+                                .shouldBeTypeOf<BonusFraForsvaret>()
+                                .åretUtbetalingenGjelderFor.shouldBe(Year.of(1980))
+                        }
+                }
+
+                get(1).run {
+                    aarMaaned.shouldBe(YearMonth.parse("2022-02"))
+                    arbeidsInntektInformasjon.inntektListe.shouldHaveSize(1)
+                        .first().run {
+                            beloep.shouldBe(BigDecimal(50000))
+                            tilleggsinformasjon
+                                .tilleggsinformasjonDetaljer
+                                .shouldBeTypeOf<ReiseKostOgLosji>()
+                                .persontype.shouldBe("norskPendler")
+                        }
+                }
+                get(2).run {
+                    aarMaaned.shouldBe(YearMonth.parse("2022-03"))
+                    arbeidsInntektInformasjon.inntektListe.shouldHaveSize(1)
+                        .first().run {
+                            beloep.shouldBe(BigDecimal(50000))
+                            tilleggsinformasjon
+                                .tilleggsinformasjonDetaljer
+                                .shouldBeTypeOf<Svalbardinntekt>().run {
+                                    antallDager.shouldBe(40)
+                                    betaltTrygdeavgift.shouldBe(BigDecimal(50000))
+                                }
+
+
+                        }
+                }
+            }
+
+    }
+
+    fun hentRessurs(fil: String): String = InntekKonverterTest::class.java.classLoader.getResource(fil)
+        ?.readText(StandardCharsets.UTF_8) ?: throw IkkeFunnetException("Fant ikke $fil")
+}
+
+
+
