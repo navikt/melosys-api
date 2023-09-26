5e1c6fe14f23df9ab49cea18d30229185c6c9b14
RuneLind <rulind@gmail.com>
2023-09-26 14:19:51 +0200

    Konverter YtelseRegler til kotlin

---
+++ service/src/main/kotlin/no/nav/melosys/service/kontroll/regler/YtelseRegler.kt
@@ -0,0 +1,42 @@
+package no.nav.melosys.service.kontroll.regler
+
+import no.nav.melosys.domain.dokument.inntekt.InntektDokument
+import no.nav.melosys.domain.dokument.inntekt.inntektstype.YtelseFraOffentlige
+import java.time.LocalDate
+import java.time.YearMonth
+
+object YtelseRegler {
+    @JvmStatic
+    fun utbetaltYtelserFraOffentligIPeriode(inntektDokument: InntektDokument?, fom: LocalDate?, tom: LocalDate?): Boolean {
+        if (inntektDokument == null || inntektDokument.arbeidsInntektMaanedListe?.isEmpty() == true) {
+            return false
+        }
+        val fra = YearMonth.from(fom)
+        val til = if (tom != null) YearMonth.from(tom) else null
+        for (ytelseFraOffentlige in hentYtelseFraOffentlige(inntektDokument)) {
+            if (erUtbetaltIPeriode(ytelseFraOffentlige, fra, til)) {
+                return true
+            }
+        }
+        return false
+    }
+
+    private fun erUtbetaltIPeriode(ytelseFraOffentlige: YtelseFraOffentlige, fom: YearMonth, tom: YearMonth?): Boolean {
+        val utbetaltIPeriode = ytelseFraOffentlige.utbetaltIPeriode ?: return false
+
+        val tomNotNull = tom ?: fom.plusYears(2)
+        if (utbetaltIPeriode.isAfter(fom) && utbetaltIPeriode.isBefore(tomNotNull))
+            return true
+
+        return utbetaltIPeriode == fom || utbetaltIPeriode == tom
+    }
+
+    private fun hentYtelseFraOffentlige(inntektDokument: InntektDokument): Collection<YtelseFraOffentlige> {
+        return inntektDokument.arbeidsInntektMaanedListe
+            .asSequence()
+            .mapNotNull { it.arbeidsInntektInformasjon?.inntektListe }
+            .flatten()
+            .filterIsInstance<YtelseFraOffentlige>()
+            .toList()
+    }
+}
