909dc0fca18b6e3a3cf64a8c3676211a64d965b8
Thang Phan <thang.quoc.ly.phan@nav.no>
2023-09-27 09:29:30 +0200

    6137: DB TOGGLE Lagre fakturaserie referanse når man oppretter (#2006)
    * 6137: Lagre fakturaserie referanse når man oppretter
    
    Når man oppretter en ny behandling med type: Vurdering manglende innbetaling trygdeavgift
    må det lagres forrige fakturaserie referanse i behandlingsresultat slik
    at det kan hentes via frontend. Nå ser det ut som fakturainformasjon i
    frontend er tom pga det ikke har blitt lagret.
    
    https://jira.adeo.no/browse/MELOSYS-6138

---
+++ saksflyt/src/main/kotlin/no.nav.melosys.saksflyt/behandlingsresultat/LagreFakturaserieReferanse.kt
@@ -0,0 +1,30 @@
+package no.nav.melosys.saksflyt.faktureringskomponenten.behandlingsresultat
+
+import mu.KotlinLogging
+import no.nav.melosys.domain.saksflyt.ProsessDataKey
+import no.nav.melosys.domain.saksflyt.ProsessSteg
+import no.nav.melosys.domain.saksflyt.Prosessinstans
+import no.nav.melosys.saksflyt.steg.StegBehandler
+import no.nav.melosys.service.behandling.BehandlingsresultatService
+import org.springframework.stereotype.Component
+
+private val log = KotlinLogging.logger { }
+
+@Component
+class LagreFakturaserieReferanse(private val behandlingsresultatService: BehandlingsresultatService) : StegBehandler {
+
+    override fun inngangsSteg(): ProsessSteg {
+        return ProsessSteg.LAGRE_FAKTURASERIE_REFERANSE
+    }
+
+    override fun utfør(prosessinstans: Prosessinstans) {
+        val fakturaserieReferanse = prosessinstans.getData(ProsessDataKey.FAKTURASERIE_REFERANSE) ?: return
+        val behandlingsId = prosessinstans.behandling.id
+        val behandlingsresultat = behandlingsresultatService.hentBehandlingsresultat(behandlingsId)
+
+        behandlingsresultat.fakturaserieReferanse = fakturaserieReferanse
+        behandlingsresultatService.lagre(behandlingsresultat)
+
+        log.info("Lagret ny fakturaserie referanse {} på behandlingsresultat {}", fakturaserieReferanse, behandlingsId)
+    }
+}
