8592220a48c0f838dda31b06cf3b809bb820f1e1
barstadEkstern <oystein.madsen.barstad@nav.no>
2023-10-12 15:37:29 +0200

    wip

--- domain/src/test/kotlin/no/nav/melosys/domain/folketrygden/FastsattTrygdeavgiftTest.kt
+++ domain/src/test/kotlin/no/nav/melosys/domain/folketrygden/FastsattTrygdeavgiftTest.kt
@@ -2,24 +2,19 @@ package no.nav.melosys.domain.folketrygden
 
 import io.kotest.matchers.shouldBe
 import no.nav.melosys.domain.avgift.Inntektsperiode
-import no.nav.melosys.domain.avgift.SkatteforholdTilNorge
 import no.nav.melosys.domain.avgift.Trygdeavgiftsgrunnlag
 import no.nav.melosys.domain.kodeverk.Inntektskildetype
 import no.nav.melosys.domain.kodeverk.Inntektskildetype.MISJONÆR
-import no.nav.melosys.domain.kodeverk.Inntektskildetype.ARBEIDSINNTEKT_FRA_NORGE
-import no.nav.melosys.domain.kodeverk.Skatteplikttype
-
 import no.nav.melosys.domain.kodeverk.Trygdeavgiftmottaker.*
 import org.junit.jupiter.api.Test
 
 internal class FastsattTrygdeavgiftTest {
 
     @Test
-    fun `trygdeavgiftsmottaker skal være NAV hvis bruker ikke er skattepliktig og aga er false`() {
+    fun `trygdeavgiftsmottaker skal være NAV hvis ordinær trygdeavgift og aga er false`() {
         FastsattTrygdeavgift().apply {
             trygdeavgiftsgrunnlag = Trygdeavgiftsgrunnlag().apply {
-                skatteforholdTilNorge = mutableListOf(lagSkatteforholdsperiode(Skatteplikttype.IKKE_SKATTEPLIKTIG))
-                inntektsperioder = mutableListOf(lagInntektsperiode( false, ARBEIDSINNTEKT_FRA_NORGE))
+                inntektsperioder = mutableListOf(lagInntektsperiode(false, false))
             }
         }.run {
             trygdeavgiftMottaker.shouldBe(TRYGDEAVGIFT_BETALES_TIL_NAV)
@@ -27,11 +22,10 @@ internal class FastsattTrygdeavgiftTest {
     }
 
     @Test
-    fun `trygdeavgiftsmottaker skal være SKATT hvis bruker er skattepliktig og aga er true`() {
+    fun `trygdeavgiftsmottaker skal være SKATT hvis ordinær trygdeavgift og aga er true`() {
         FastsattTrygdeavgift().apply {
             trygdeavgiftsgrunnlag = Trygdeavgiftsgrunnlag().apply {
-                skatteforholdTilNorge = mutableListOf(lagSkatteforholdsperiode(Skatteplikttype.SKATTEPLIKTIG))
-                inntektsperioder = mutableListOf(lagInntektsperiode( true, ARBEIDSINNTEKT_FRA_NORGE))
+                inntektsperioder = mutableListOf(lagInntektsperiode(true, true))
             }
         }.run {
             trygdeavgiftMottaker.shouldBe(TRYGDEAVGIFT_BETALES_TIL_SKATT)
@@ -39,11 +33,10 @@ internal class FastsattTrygdeavgiftTest {
     }
 
     @Test
-    fun `trygdeavgiftsmottaker skal være SKATT hvis bruker er skattepliktig og aga er false, men type er MISJONÆR`() {
+    fun `trygdeavgiftsmottaker skal være SKATT hvis ordinær trygdeavgift er true og aga er false, men type er MISJONÆR`() {
         FastsattTrygdeavgift().apply {
             trygdeavgiftsgrunnlag = Trygdeavgiftsgrunnlag().apply {
-                skatteforholdTilNorge = mutableListOf(lagSkatteforholdsperiode(Skatteplikttype.SKATTEPLIKTIG))
-                inntektsperioder = mutableListOf(lagInntektsperiode( false, MISJONÆR))
+                inntektsperioder = mutableListOf(lagInntektsperiode(true, false, MISJONÆR))
             }
         }.run {
             trygdeavgiftMottaker.shouldBe(TRYGDEAVGIFT_BETALES_TIL_SKATT)
@@ -51,23 +44,10 @@ internal class FastsattTrygdeavgiftTest {
     }
 
     @Test
-    fun `trygdeavgiftsmottaker skal være NAV hvis bruker er ikke skattepliktig og aga er false, men type er MISJONÆR`() {
+    fun `trygdeavgiftsmottaker skal være NAV og SKATT hvis ordinær trygdeavgift og aga er en kombinasjon v1`() {
         FastsattTrygdeavgift().apply {
             trygdeavgiftsgrunnlag = Trygdeavgiftsgrunnlag().apply {
-                skatteforholdTilNorge = mutableListOf(lagSkatteforholdsperiode(Skatteplikttype.IKKE_SKATTEPLIKTIG))
-                inntektsperioder = mutableListOf(lagInntektsperiode( false, MISJONÆR))
-            }
-        }.run {
-            trygdeavgiftMottaker.shouldBe(TRYGDEAVGIFT_BETALES_TIL_NAV)
-        }
-    }
-
-    @Test
-    fun `trygdeavgiftsmottaker skal være NAV og SKATT hvis skatteplikt og aga er en kombinasjon v1`() {
-        FastsattTrygdeavgift().apply {
-            trygdeavgiftsgrunnlag = Trygdeavgiftsgrunnlag().apply {
-                skatteforholdTilNorge = mutableListOf(lagSkatteforholdsperiode(Skatteplikttype.SKATTEPLIKTIG))
-                inntektsperioder = mutableListOf(lagInntektsperiode( false, ARBEIDSINNTEKT_FRA_NORGE))
+                inntektsperioder = mutableListOf(lagInntektsperiode(true, false))
             }
         }.run {
             trygdeavgiftMottaker.shouldBe(TRYGDEAVGIFT_BETALES_TIL_NAV_OG_SKATT)
@@ -75,11 +55,10 @@ internal class FastsattTrygdeavgiftTest {
     }
 
     @Test
-    fun `trygdeavgiftsmottaker skal være NAV og SKATT hvis skatteplikt og aga er en kombinasjon v2`() {
+    fun `trygdeavgiftsmottaker skal være NAV og SKATT hvis ordinær trygdeavgift og aga er en kombinasjon v2`() {
         FastsattTrygdeavgift().apply {
             trygdeavgiftsgrunnlag = Trygdeavgiftsgrunnlag().apply {
-                skatteforholdTilNorge = mutableListOf(lagSkatteforholdsperiode(Skatteplikttype.IKKE_SKATTEPLIKTIG))
-                inntektsperioder = mutableListOf(lagInntektsperiode( true, ARBEIDSINNTEKT_FRA_NORGE))
+                inntektsperioder = mutableListOf(lagInntektsperiode(false, true))
             }
         }.run {
             trygdeavgiftMottaker.shouldBe(TRYGDEAVGIFT_BETALES_TIL_NAV_OG_SKATT)
@@ -87,46 +66,12 @@ internal class FastsattTrygdeavgiftTest {
     }
 
     @Test
-    fun `trygdeavgiftsmottaker skal være NAV og SKATT hvis skatteplikt og aga er en kombinasjon v3`() {
+    fun `trygdeavgiftsmottaker skal være NAV og SKATT hvis ordinær trygdeavgift og aga er en kombinasjon v3`() {
         FastsattTrygdeavgift().apply {
             trygdeavgiftsgrunnlag = Trygdeavgiftsgrunnlag().apply {
-                skatteforholdTilNorge = mutableListOf(lagSkatteforholdsperiode(Skatteplikttype.IKKE_SKATTEPLIKTIG),
-                    lagSkatteforholdsperiode(Skatteplikttype.SKATTEPLIKTIG))
                 inntektsperioder = mutableListOf(
-                    lagInntektsperiode( true, ARBEIDSINNTEKT_FRA_NORGE),
-                    lagInntektsperiode( true, ARBEIDSINNTEKT_FRA_NORGE)
-                )
-            }
-        }.run {
-            trygdeavgiftMottaker.shouldBe(TRYGDEAVGIFT_BETALES_TIL_NAV_OG_SKATT)
-        }
-    }
-
-    @Test
-    fun `trygdeavgiftsmottaker skal være NAV og SKATT hvis skatteplikt og aga er en kombinasjon v4`() {
-        FastsattTrygdeavgift().apply {
-            trygdeavgiftsgrunnlag = Trygdeavgiftsgrunnlag().apply {
-                skatteforholdTilNorge = mutableListOf(lagSkatteforholdsperiode(Skatteplikttype.IKKE_SKATTEPLIKTIG),
-                    lagSkatteforholdsperiode(Skatteplikttype.SKATTEPLIKTIG))
-                inntektsperioder = mutableListOf(
-                    lagInntektsperiode( true, ARBEIDSINNTEKT_FRA_NORGE),
-                    lagInntektsperiode( false, ARBEIDSINNTEKT_FRA_NORGE)
-                )
-            }
-        }.run {
-            trygdeavgiftMottaker.shouldBe(TRYGDEAVGIFT_BETALES_TIL_NAV_OG_SKATT)
-        }
-    }
-
-    @Test
-    fun `trygdeavgiftsmottaker skal være NAV og SKATT hvis skatteplikt og aga er en kombinasjon v5`() {
-        FastsattTrygdeavgift().apply {
-            trygdeavgiftsgrunnlag = Trygdeavgiftsgrunnlag().apply {
-                skatteforholdTilNorge = mutableListOf(lagSkatteforholdsperiode(Skatteplikttype.IKKE_SKATTEPLIKTIG),
-                    lagSkatteforholdsperiode(Skatteplikttype.SKATTEPLIKTIG))
-                inntektsperioder = mutableListOf(
-                    lagInntektsperiode( false, ARBEIDSINNTEKT_FRA_NORGE),
-                    lagInntektsperiode( false, ARBEIDSINNTEKT_FRA_NORGE)
+                    lagInntektsperiode(false, true),
+                    lagInntektsperiode(true, true)
                 )
             }
         }.run {
@@ -138,11 +83,9 @@ internal class FastsattTrygdeavgiftTest {
     fun `trygdeavgiftsmottaker skal være NAV og SKATT hvis det er flere innteksperioder med forskjellige mottakere`() {
         FastsattTrygdeavgift().apply {
             trygdeavgiftsgrunnlag = Trygdeavgiftsgrunnlag().apply {
-                skatteforholdTilNorge = mutableListOf(lagSkatteforholdsperiode(Skatteplikttype.SKATTEPLIKTIG),
-                    lagSkatteforholdsperiode(Skatteplikttype.SKATTEPLIKTIG))
                 inntektsperioder = mutableListOf(
-                    lagInntektsperiode( true, ARBEIDSINNTEKT_FRA_NORGE),
-                    lagInntektsperiode(false, ARBEIDSINNTEKT_FRA_NORGE)
+                    lagInntektsperiode(true, true),
+                    lagInntektsperiode(false, false)
                 )
             }
         }.run {
@@ -151,22 +94,15 @@ internal class FastsattTrygdeavgiftTest {
     }
 
     fun lagInntektsperiode(
+        ordinærTrygdeavgiftBetalesTilSkatt: Boolean,
         arbeidsgiversavgiftBetalesTilSkatt: Boolean,
-        inntektskildetype: Inntektskildetype
+        inntektskildetype: Inntektskildetype? = null
     ): Inntektsperiode {
         val inntektsperiode = Inntektsperiode().apply {
+            isOrdinærTrygdeavgiftBetalesTilSkatt = ordinærTrygdeavgiftBetalesTilSkatt
             isArbeidsgiversavgiftBetalesTilSkatt = arbeidsgiversavgiftBetalesTilSkatt
             type = inntektskildetype
         }
         return inntektsperiode
-    }
-
-    fun lagSkatteforholdsperiode(
-        skatteplikttype: Skatteplikttype
-    ): SkatteforholdTilNorge {
-        val skatteforholdTilNorge = SkatteforholdTilNorge().apply {
-            setSkatteplikttype(skatteplikttype)
-        }
-        return skatteforholdTilNorge
     }
 }
