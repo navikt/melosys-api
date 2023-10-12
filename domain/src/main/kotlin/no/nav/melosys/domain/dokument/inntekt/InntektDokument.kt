e2e0729913dc2b802cc272aca06354f57863d339
RuneLind <rulind@gmail.com>
2023-10-12 13:13:40 +0200

    arbeidsInntektInformasjon.inntektListe kan ikke være null

--- domain/src/main/kotlin/no/nav/melosys/domain/dokument/inntekt/InntektDokument.kt
+++ domain/src/main/kotlin/no/nav/melosys/domain/dokument/inntekt/InntektDokument.kt
@@ -2,12 +2,11 @@ package no.nav.melosys.domain.dokument.inntekt
 
 import no.nav.melosys.domain.dokument.SaksopplysningDokument
 
-class InntektDokument(var arbeidsInntektMaanedListe: List<ArbeidsInntektMaaned> = emptyList()) :
-    SaksopplysningDokument {
+class InntektDokument(var arbeidsInntektMaanedListe: List<ArbeidsInntektMaaned> = emptyList()) : SaksopplysningDokument {
 
     fun hentOrgnumre(): Set<String> {
         return arbeidsInntektMaanedListe
-            .mapNotNull { it.arbeidsInntektInformasjon.inntektListe }
+            .map { it.arbeidsInntektInformasjon.inntektListe }
             .flatten()
             .mapNotNull { it.virksomhetID }
             .toSet()
