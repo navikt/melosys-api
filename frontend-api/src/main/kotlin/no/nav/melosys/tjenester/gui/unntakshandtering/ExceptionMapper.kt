ee4037fbd8db952bf7b0d7757c8610353a245c11
Isa Salem Hame <wanghattan@gmail.com>
2023-10-17 09:08:20 +0200

    MELOSYS-6162: Ta med feilmelding fra konsument til klienten

--- frontend-api/src/main/kotlin/no/nav/melosys/tjenester/gui/unntakshandtering/ExceptionMapper.kt
+++ frontend-api/src/main/kotlin/no/nav/melosys/tjenester/gui/unntakshandtering/ExceptionMapper.kt
@@ -9,37 +9,47 @@ import no.nav.melosys.integrasjon.felles.mdc.MDCOperations
 import no.nav.security.token.support.spring.validation.interceptor.JwtTokenUnauthorizedException
 import org.slf4j.MDC
 import org.slf4j.event.Level
+import org.springframework.boot.configurationprocessor.json.JSONObject
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.reactive.function.client.WebClientResponseException
 import javax.servlet.http.HttpServletRequest
 
 private val log = KotlinLogging.logger { }
+
 @ControllerAdvice
 class ExceptionMapper {
+
     @ExceptionHandler(IkkeFunnetException::class)
-    fun håndter(e: IkkeFunnetException, request: HttpServletRequest): ResponseEntity<Map<String, Any?>> =
+    fun håndter(e: IkkeFunnetException, request: HttpServletRequest): ResponseEntity<Map<String, Any>> =
         håndter(e, request, HttpStatus.NOT_FOUND, Level.WARN)
 
     @ExceptionHandler(FunksjonellException::class)
-    fun håndter(e: FunksjonellException, request: HttpServletRequest): ResponseEntity<Map<String, Any?>> =
+    fun håndter(e: FunksjonellException, request: HttpServletRequest): ResponseEntity<Map<String, Any>> =
         håndter(e, request, HttpStatus.BAD_REQUEST, Level.WARN)
 
     @ExceptionHandler(SikkerhetsbegrensningException::class)
-    fun håndter(e: SikkerhetsbegrensningException, request: HttpServletRequest): ResponseEntity<Map<String, Any?>> =
+    fun håndter(e: SikkerhetsbegrensningException, request: HttpServletRequest): ResponseEntity<Map<String, Any>> =
         håndter(e, request, HttpStatus.FORBIDDEN, Level.WARN)
 
     @ExceptionHandler(JwtTokenUnauthorizedException::class)
-    fun håndter(e: JwtTokenUnauthorizedException, request: HttpServletRequest): ResponseEntity<Map<String, Any?>> =
+    fun håndter(e: JwtTokenUnauthorizedException, request: HttpServletRequest): ResponseEntity<Map<String, Any>> =
         håndter(e, request, HttpStatus.UNAUTHORIZED, Level.WARN)
 
     @ExceptionHandler(ValideringException::class)
-    fun håndter(e: ValideringException, request: HttpServletRequest): ResponseEntity<Map<String, Any?>> =
+    fun håndter(e: ValideringException, request: HttpServletRequest): ResponseEntity<Map<String, Any>> =
         håndter(e, request, HttpStatus.BAD_REQUEST, Level.INFO, e.feilkoder)
 
+    @ExceptionHandler(WebClientResponseException::class)
+    fun håndter(e: WebClientResponseException, request: HttpServletRequest): ResponseEntity<Map<String, Any>> {
+        val feilmeldingFraRespons = extractMessageFromJson(e.responseBodyAsString)
+        return håndter(e, request, HttpStatus.INTERNAL_SERVER_ERROR, Level.ERROR, listOfNotNull(feilmeldingFraRespons))
+    }
+
     @ExceptionHandler(Exception::class)
-    fun håndter(e: Exception, request: HttpServletRequest): ResponseEntity<Map<String, Any?>> =
+    fun håndter(e: Exception, request: HttpServletRequest): ResponseEntity<Map<String, Any>> =
         håndter(e, request, HttpStatus.INTERNAL_SERVER_ERROR, Level.ERROR)
 
     private fun håndter(
@@ -48,7 +58,7 @@ class ExceptionMapper {
         httpStatus: HttpStatus,
         loggnivå: Level,
         begrunnelser: Collection<*>? = emptyList<Any>()
-    ): ResponseEntity<Map<String, Any?>> {
+    ): ResponseEntity<Map<String, Any>> {
         val message = e.message ?: e.javaClass.simpleName
         val errorMessage =
             "API kall feilet: $message\nremoteHost:${request.remoteHost}\nrequestURI :${request.requestURI}"
@@ -59,7 +69,7 @@ class ExceptionMapper {
             log.warn(errorMessage, e)
         }
 
-        val entity = mutableMapOf<String, Any?>(
+        val entity = mutableMapOf<String, Any>(
             "status" to httpStatus.value(),
             "error" to httpStatus.reasonPhrase,
             "message" to message,
@@ -70,5 +80,15 @@ class ExceptionMapper {
             }
         }
         return ResponseEntity(entity, httpStatus)
+    }
+
+
+    private fun extractMessageFromJson(jsonString: String): String? {
+        return try {
+            val jsonObj = JSONObject(jsonString)
+            jsonObj.optString("message", null)
+        } catch (ex: Exception) {
+            null
+        }
     }
 }
