5073afd4edf4d7ab4aad2c2d09e8685d96dde316
RuneLind <rulind@gmail.com>
2023-10-24 20:06:12 +0200

    Legg til InntektType.Inntekt
    Trenger dette for lese inn historisk data som ble lagt inn med feil type

--- domain/src/main/kotlin/no/nav/melosys/domain/dokument/inntekt/InntektType.kt
+++ domain/src/main/kotlin/no/nav/melosys/domain/dokument/inntekt/InntektType.kt
@@ -2,6 +2,7 @@ package no.nav.melosys.domain.dokument.inntekt
 
 enum class InntektType {
     // Navn her er for bakoverkompatibilitet med det som allerede er i databasen
+    Inntekt,
     Loennsinntekt,
     Naeringsinntekt,
     PensjonEllerTrygd,
