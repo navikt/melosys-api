5679c610e6c6bb5f77cd095ea951e00aaf304e65
RuneLind <rulind@gmail.com>
2023-11-03 10:00:06 +0100

    Flytt tester til samme klasse

--- integrasjon/src/test/kotlin/no/nav/melosys/integrasjon/inntekt/InntektDocumentConvertTest.kt
+++ integrasjon/src/test/kotlin/no/nav/melosys/integrasjon/inntekt/InntektDocumentConvertTest.kt
@@ -5,10 +5,14 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 import io.kotest.assertions.json.shouldEqualJson
+import io.kotest.matchers.collections.shouldHaveSize
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.types.shouldBeInstanceOf
 import io.kotest.matchers.types.shouldBeTypeOf
 import no.nav.melosys.domain.dokument.DokumentView
 import no.nav.melosys.domain.dokument.SaksopplysningDokument
 import no.nav.melosys.domain.dokument.inntekt.InntektDokument
+import no.nav.melosys.domain.dokument.inntekt.InntektType
 import no.nav.melosys.domain.dokument.inntekt.tillegsinfo.TilleggsinformasjonDetaljer
 import no.nav.melosys.domain.jpa.SaksopplysningDokumentConverter
 import no.nav.melosys.domain.jpa.mixin.SaksopplysningDokumentMixIn
@@ -52,6 +56,67 @@ class InntektDocumentConvertTest {
         frontendExpectedDTOs.shouldEqualJson(forventetInntektDokumentrFrontEndJson)
     }
 
+    @Test
+    fun `sjekk at vi kan lese inn med type Inntekt`() {
+        val saksopplysningDokument: SaksopplysningDokument =
+            SaksopplysningDokumentConverter().convertToEntityAttribute(INNTEKTDOKUMENT_MED_TYPEI_NNTEKT)
+
+
+        saksopplysningDokument.shouldBeInstanceOf<InntektDokument>()
+            .arbeidsInntektMaanedListe.shouldHaveSize(1)
+            .first().arbeidsInntektInformasjon.inntektListe.first().type
+            .shouldBe(InntektType.Inntekt)
+    }
+
+    @Test
+    fun `sjekk at vi kan lese inn inntektListe uten kategori i tilleggsinformasjon`() {
+        val saksopplysningDokument: SaksopplysningDokument =
+            SaksopplysningDokumentConverter().convertToEntityAttribute(INNTEKT_DOKUMENT_UTEN_KATEGORI)
+
+
+        saksopplysningDokument.shouldBeInstanceOf<InntektDokument>()
+            .arbeidsInntektMaanedListe.shouldHaveSize(1)
+            .first().arbeidsInntektInformasjon.inntektListe.first().type
+            .shouldBe(InntektType.Inntekt)
+    }
+
+
     fun hentRessurs(fil: String): String = this::class.java.classLoader.getResource(fil)
         ?.readText(StandardCharsets.UTF_8) ?: throw IkkeFunnetException("Fant ikke $fil")
+
+    companion object {
+        private const val INNTEKTDOKUMENT_MED_TYPEI_NNTEKT = """{
+          "type" : "InntektDokument",
+          "arbeidsInntektMaanedListe" : [ {
+            "arbeidsInntektInformasjon" : {
+              "inntektListe" : [ {
+                "type" : "Inntekt",
+                "beloep" : 50000,
+                "fordel" : "kontantytelse",
+                "inntektskilde" : "A-ordningen",
+                "inntektsperiodetype" : "Maaned",
+                "inntektsstatus" : "LoependeInnrapportert",
+                "utbetaltIPeriode" : [ 2022, 1 ]
+              } ]
+            }
+          } ]}"""
+
+        private const val INNTEKT_DOKUMENT_UTEN_KATEGORI = """{
+          "type" : "InntektDokument",
+          "arbeidsInntektMaanedListe" : [ {
+            "arbeidsInntektInformasjon" : {
+              "inntektListe" : [ {
+                "type" : "Inntekt",
+                "beloep" : 50000,
+                "fordel" : "kontantytelse",
+                "inntektskilde" : "A-ordningen",
+                "inntektsperiodetype" : "Maaned",
+                "inntektsstatus" : "LoependeInnrapportert",
+                "utbetaltIPeriode" : [ 2022, 1 ],
+                    "tilleggsinformasjon" : {
+                }
+              } ]
+            }
+          } ]}"""
+    }
 }
