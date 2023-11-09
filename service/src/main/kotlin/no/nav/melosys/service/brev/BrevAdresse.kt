76f3cdc81e791b3eb0ab22e280613dddec9bbeaa
Janita Johansen <janita.marielle.johansen@nav.no>
2023-11-09 09:15:49 +0100

    Litt justering på BrevAdresse funksjoner

--- service/src/main/kotlin/no/nav/melosys/service/brev/BrevAdresse.kt
+++ service/src/main/kotlin/no/nav/melosys/service/brev/BrevAdresse.kt
@@ -2,7 +2,6 @@ package no.nav.melosys.service.brev
 
 import no.nav.melosys.domain.kodeverk.Land_iso2
 
-
 data class BrevAdresse(
     val mottakerNavn: String,
     val orgnr: String?,
@@ -12,11 +11,16 @@ data class BrevAdresse(
     val region: String?,
     val land: String?
 ) {
+    fun erUgyldig(): Boolean {
+        return isPostnrEmpty() || isAdresselinjerEmpty()
+    }
 
-    val isAdresselinjerEmpty: Boolean
-        get() = Land_iso2.NO.name != land && (adresselinjer == null || adresselinjer.stream().allMatch { obj: String -> obj.isBlank() })
-    val isPostnrEmpty: Boolean
-        get() = Land_iso2.NO.name == land && postnr.isNullOrBlank()
+    fun isAdresselinjerEmpty(): Boolean {
+        return Land_iso2.NO.name != land && (adresselinjer == null || adresselinjer.all { it.isBlank() })
+    }
 
+    fun isPostnrEmpty(): Boolean {
+        return Land_iso2.NO.name == land && postnr.isNullOrBlank()
+    }
 }
 
