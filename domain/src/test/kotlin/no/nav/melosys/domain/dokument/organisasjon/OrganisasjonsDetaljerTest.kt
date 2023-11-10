e9c68013bc7df7c9a34c7cc98f9f8aa9c6db5577
RuneLind <rulind@gmail.com>
2023-11-10 17:26:16 +0100

    Rydd i OrganisasjonsDetaljerTest

--- domain/src/test/kotlin/no/nav/melosys/domain/dokument/organisasjon/OrganisasjonsDetaljerTest.kt
+++ domain/src/test/kotlin/no/nav/melosys/domain/dokument/organisasjon/OrganisasjonsDetaljerTest.kt
@@ -1,102 +1,112 @@
 package no.nav.melosys.domain.dokument.organisasjon
 
+import io.kotest.matchers.nulls.shouldBeNull
+import io.kotest.matchers.shouldBe
 import no.nav.melosys.domain.dokument.felles.Periode
-import no.nav.melosys.domain.dokument.organisasjon.adresse.GeografiskAdresse
 import no.nav.melosys.domain.dokument.organisasjon.adresse.SemistrukturertAdresse
-import org.assertj.core.api.Assertions
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.mockito.Mockito
-import java.util.*
+import java.time.LocalDate
 
 class OrganisasjonsDetaljerTest {
-    private lateinit var adresse: SemistrukturertAdresse
-    private val linje1: String = "LINJE1  "
-    private val linje2: String = "LINJE2"
-    private val linje3: String = "LINJE3"
-    private val POSTNR: String = "postnummer"
-    private val POSTSTED: String = "poststed"
-    private val POSTSTED_UTLAND: String = "poststedUtland"
 
-    @BeforeEach
-    fun setUp() {
-        val periode = Mockito.mock(
-            Periode::class.java
-        )
-        Mockito.`when`(periode.erGyldig()).thenReturn(true)
-        adresse = SemistrukturertAdresse().apply {
-            adresselinje1 = linje1
-            adresselinje2 = linje2
-            adresselinje3 = linje3
-            postnr = POSTNR
-            poststed = POSTSTED
-            poststedUtland = POSTSTED_UTLAND
-            kommunenr = "kommunenr"
-            gyldighetsperiode = periode
+    private val linje1 = "LINJE1  "
+    private val linje2 = "LINJE2"
+    private val linje3 = "LINJE3"
+    private val POSTNR = "postnummer"
+    private val POSTSTED = "poststed"
+    private val POSTSTED_UTLAND = "poststedUtland"
+
+    @Test
+    fun `Konverter forretningsadresse til ustrukturert adresse`() {
+        val orgDetaljer = OrganisasjonsDetaljer().apply {
+            forretningsadresse = listOf(lagAdresse(landkode = "NO"))
+        }
+
+
+        val resultatAdresse = orgDetaljer.hentUstrukturertForretningsadresse()!!
+
+
+        resultatAdresse.run {
+            getAdresselinje(1).shouldBe(linje1)
+            getAdresselinje(2).shouldBe(linje2)
+            getAdresselinje(3).shouldBe(linje3)
+            getAdresselinje(4).shouldBe("$POSTNR $POSTSTED")
+            landkode.shouldBe("NO")
         }
     }
 
     @Test
-    fun testKonverterForretningsadresseTilUstrukturertAdresse() {
-        val landkode = "NO"
-        adresse.landkode = landkode
-        val orgDetaljer = OrganisasjonsDetaljer()
-        orgDetaljer.forretningsadresse = Arrays.asList<GeografiskAdresse?>(adresse)
+    fun `Konverter utenlandsk forretningsadresse til ustrukturert adresse`() {
+        val orgDetaljer = OrganisasjonsDetaljer().apply {
+            forretningsadresse = listOf(lagAdresse(landkode = "DK"))
+        }
+
         val resultatAdresse = orgDetaljer.hentUstrukturertForretningsadresse()!!
-        Assertions.assertThat(resultatAdresse.getAdresselinje(1)).isEqualTo(linje1)
-        Assertions.assertThat(resultatAdresse.getAdresselinje(2)).isEqualTo(linje2)
-        Assertions.assertThat(resultatAdresse.getAdresselinje(3)).isEqualTo(linje3)
-        Assertions.assertThat(resultatAdresse.getAdresselinje(4)).isEqualTo("$POSTNR $POSTSTED")
-        Assertions.assertThat(resultatAdresse.landkode).isEqualTo(landkode)
+
+        resultatAdresse.run {
+            getAdresselinje(1).shouldBe(linje1)
+            getAdresselinje(2).shouldBe(linje2)
+            getAdresselinje(3).shouldBe(linje3)
+            getAdresselinje(4).shouldBe(POSTSTED_UTLAND)
+            landkode.shouldBe("DK")
+        }
     }
 
     @Test
-    fun testKonverterUtenlandskForretningsadresseTilUstrukturertAdresse() {
-        val landkode = "DK"
-        adresse.landkode = landkode
-        val orgDetaljer = OrganisasjonsDetaljer()
-        orgDetaljer.forretningsadresse = Arrays.asList<GeografiskAdresse?>(adresse)
-        val resultatAdresse = orgDetaljer.hentUstrukturertForretningsadresse()!!
-        Assertions.assertThat(resultatAdresse.getAdresselinje(1)).isEqualTo(linje1)
-        Assertions.assertThat(resultatAdresse.getAdresselinje(2)).isEqualTo(linje2)
-        Assertions.assertThat(resultatAdresse.getAdresselinje(3)).isEqualTo(linje3)
-        Assertions.assertThat(resultatAdresse.getAdresselinje(4)).isEqualTo(POSTSTED_UTLAND)
-        Assertions.assertThat(resultatAdresse.landkode).isEqualTo(landkode)
-    }
+    fun `Konverter forretningsadresse til strukturert adresse`() {
+        val orgDetaljer = OrganisasjonsDetaljer().apply {
+            forretningsadresse = listOf(lagAdresse(landkode = "NO"))
+        }
 
-    @Test
-    fun testKonverterForretningsadresseTilStrukturertAdresse() {
-        val landkode = "NO"
-        adresse.landkode = landkode
-        val orgDetaljer = OrganisasjonsDetaljer()
-        orgDetaljer.forretningsadresse = mutableListOf(adresse)
         val resultatAdresse = orgDetaljer.hentStrukturertForretningsadresse()!!
-        Assertions.assertThat(resultatAdresse.gatenavn)
-            .isEqualTo(linje1.trim { it <= ' ' } + " " + linje2 + " " + linje3)
-        Assertions.assertThat(resultatAdresse.landkode).isEqualTo(landkode)
-        Assertions.assertThat(resultatAdresse.postnummer).isEqualTo(POSTNR)
-        // Ikke alltid poststed for norske registeradresser. Slåes opp med kodeverkservice ved behov
-        Assertions.assertThat(resultatAdresse.poststed).isEqualTo(POSTSTED)
+
+        resultatAdresse.run {
+            gatenavn.shouldBe(listOf(linje1.trim(), linje2, linje3).joinToString(" "))
+            landkode.shouldBe("NO")
+            postnummer.shouldBe(POSTNR)
+            poststed.shouldBe(POSTSTED)
+        }
     }
 
     @Test
-    fun testKonverterUtenlandskForretningsadresseTilStrukturertAdresse() {
-        val landkode = "DK"
-        adresse.landkode = landkode
-        val orgDetaljer = OrganisasjonsDetaljer()
-        orgDetaljer.forretningsadresse = mutableListOf(adresse)
+    fun `Konverter utenlandsk forretningsadresse til strukturert adresse`() {
+        val orgDetaljer = OrganisasjonsDetaljer().apply {
+            forretningsadresse = listOf(lagAdresse("DK"))
+        }
+
         val resultatAdresse = orgDetaljer.hentStrukturertForretningsadresse()!!
-        Assertions.assertThat(resultatAdresse.gatenavn)
-            .isEqualTo(linje1.trim { it <= ' ' } + " " + linje2 + " " + linje3)
-        Assertions.assertThat(resultatAdresse.landkode).isEqualTo(landkode)
-        Assertions.assertThat(resultatAdresse.postnummer).isEqualTo(POSTNR)
-        Assertions.assertThat(resultatAdresse.poststed).isEqualTo(POSTSTED_UTLAND)
+
+        resultatAdresse.run {
+            gatenavn.shouldBe(listOf(linje1.trim(), linje2, linje3).joinToString(" "))
+            landkode.shouldBe("DK")
+            postnummer.shouldBe(POSTNR)
+            poststed.shouldBe(POSTSTED_UTLAND)
+        }
     }
 
     @Test
-    fun testNullAdresse() {
-        val orgDetaljer = OrganisasjonsDetaljer()
+    fun `Test null adresse`() {
+        val orgDetaljer = OrganisasjonsDetaljer().apply {
+            forretningsadresse = listOf()
+        }
+
         val resultatAdresse = orgDetaljer.hentStrukturertForretningsadresse()
-        Assertions.assertThat(resultatAdresse).isNull()
+        resultatAdresse.shouldBeNull()
+    }
+
+    private fun lagAdresse(landkode: String = "") = SemistrukturertAdresse().apply {
+        adresselinje1 = linje1
+        adresselinje2 = linje2
+        adresselinje3 = linje3
+        postnr = POSTNR
+        poststed = POSTSTED
+        poststedUtland = POSTSTED_UTLAND
+        kommunenr = "kommunenr"
+        gyldighetsperiode = Periode(
+            LocalDate.now().minusDays(1),
+            LocalDate.now().plusDays(1)
+        )
+        this.landkode = landkode
     }
 }
+
