70f4764db33720075ac546c45e3a3f0ba5248a0e
RuneLind <rulind@gmail.com>
2023-11-11 12:33:30 +0100

    Rydd i Ereg4KonverteringTest

--- domain/src/test/kotlin/no/nav/melosys/domain/dokument/organisasjon/Ereg4KonverteringTest.kt
+++ domain/src/test/kotlin/no/nav/melosys/domain/dokument/organisasjon/Ereg4KonverteringTest.kt
@@ -1,104 +1,115 @@
 package no.nav.melosys.domain.dokument.organisasjon
 
+import io.kotest.matchers.nulls.shouldBeNull
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.string.shouldNotBeBlank
+import io.kotest.matchers.collections.shouldNotBeEmpty
+import io.kotest.matchers.nulls.shouldNotBeNull
+import io.kotest.matchers.string.shouldBeEmpty
 import no.nav.melosys.domain.Saksopplysning
 import no.nav.melosys.domain.SaksopplysningType
 import no.nav.melosys.domain.dokument.DokumentFactory
-import no.nav.melosys.domain.dokument.KonverteringTest
 import no.nav.melosys.domain.dokument.XsltTemplatesFactory
 import no.nav.melosys.domain.dokument.jaxb.JaxbConfig
 import no.nav.melosys.domain.dokument.organisasjon.adresse.Gateadresse
 import no.nav.melosys.domain.dokument.organisasjon.adresse.SemistrukturertAdresse
-import org.assertj.core.api.Assertions
+import no.nav.melosys.domain.dokument.KonverteringTest
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
-import java.io.IOException
 import java.time.LocalDate
-import java.util.*
-
-// Denne konverteringen testes også i DokumentFactoryTest, uten strukturert adresse
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-internal class Ereg4KonverteringTest : KonverteringTest {
-    @Test
-    @Throws(Exception::class)
-    fun testAdresse() {
-        val test = getSaksopplysning(EREG_4_0_MOCK)
+class Ereg4KonverteringTest : KonverteringTest { // Slettes når vi fjerner jaxb
 
-        // Test semistrukturert adresse...
-        val dokument = test!!.dokument as OrganisasjonDokument
-        val postadresse = dokument.organisasjonDetaljer!!.postadresse[0] as SemistrukturertAdresse
-        Assertions.assertThat(postadresse.adresselinje1).isEqualTo("Skuteviksbodene 1")
-        Assertions.assertThat(postadresse.postnr).isEqualTo("5035")
-        Assertions.assertThat(postadresse.kommunenr).isEqualTo("1201")
+    private lateinit var factory: DokumentFactory
 
-        // Test strukturert adresse...
-        val forretningsadresse = dokument.organisasjonDetaljer!!.forretningsadresse[0] as Gateadresse
-        Assertions.assertThat(forretningsadresse.gatenavn).isEqualTo("Gatenavn")
-        Assertions.assertThat(forretningsadresse.landkode).isEqualTo("NO")
-
-        // Test perioder:
-        // Forretningsadresse har ingen perioder satt...
-        Assertions.assertThat(forretningsadresse.bruksperiode).isNull()
-        Assertions.assertThat(forretningsadresse.gyldighetsperiode).isNull()
-        // Postadresse har fom-dato på begge periodene, men ikke tom-dato...
-        Assertions.assertThat(postadresse.gyldighetsperiode!!.fom).isEqualTo(LocalDate.of(2011, 9, 14))
-        Assertions.assertThat(postadresse.gyldighetsperiode!!.tom).isNull()
-        Assertions.assertThat(postadresse.bruksperiode!!.fom).isEqualTo(LocalDate.of(2015, 2, 23))
-        Assertions.assertThat(postadresse.bruksperiode!!.tom).isNull()
+    @BeforeAll
+    fun setUp() {
+        val marshaller = JaxbConfig.getJaxb2Marshaller()
+        val xsltTemplatesFactory = XsltTemplatesFactory()
+        factory = DokumentFactory(marshaller, xsltTemplatesFactory)
     }
 
     @Test
-    @Throws(IOException::class)
-    fun testJuridiskEnhet() {
-        val saksopplysning = getSaksopplysning(EREG_4_0_MOCK)
-        val dokument = saksopplysning!!.dokument as OrganisasjonDokument
-        Assertions.assertThat(dokument.sektorkode).isNotBlank()
-        Assertions.assertThat(dokument.organisasjonDetaljer!!.naering).isNotEmpty()
-        Assertions.assertThat(dokument.oppstartsdato).isNull()
-        Assertions.assertThat(dokument.enhetstype).isNotBlank()
+    fun `Adresse test`() {
+        val dokument = getSaksopplysning(EREG_4_0_MOCK).dokument as OrganisasjonDokument
+
+
+        dokument.organisasjonDetaljer.shouldNotBeNull().run {
+            val postadresse = postadresse[0] as SemistrukturertAdresse
+            postadresse.run {
+                adresselinje1.shouldBe("Skuteviksbodene 1")
+                postnr.shouldBe("5035")
+                kommunenr.shouldBe("1201")
+                gyldighetsperiode.shouldNotBeNull().fom.shouldBe(LocalDate.of(2011, 9, 14))
+                gyldighetsperiode.shouldNotBeNull().tom.shouldBeNull()
+                bruksperiode!!.fom.shouldBe(LocalDate.of(2015, 2, 23))
+                bruksperiode!!.tom.shouldBeNull()
+            }
+
+            val forretningsadresse = forretningsadresse[0] as Gateadresse
+            forretningsadresse.run {
+                gatenavn.shouldBe("Gatenavn")
+                landkode.shouldBe("NO")
+                bruksperiode.shouldBeNull()
+                gyldighetsperiode.shouldBeNull()
+            }
+        }
     }
 
     @Test
-    @Throws(IOException::class)
-    fun testOrgledd() {
-        val ressurs = "organisasjon/974652366.xml"
-        val saksopplysning = getSaksopplysning(ressurs)
-        val dokument = saksopplysning!!.getDokument() as OrganisasjonDokument
-        Assertions.assertThat(dokument.sektorkode).isNotBlank()
-        Assertions.assertThat(dokument.organisasjonDetaljer!!.naering).isNotEmpty()
-        Assertions.assertThat(dokument.oppstartsdato).isNull()
-        Assertions.assertThat(dokument.enhetstype).isEmpty()
+    fun `Juridiskenhet test`() {
+        val dokument = getSaksopplysning(EREG_4_0_MOCK).dokument as OrganisasjonDokument
+
+
+        dokument.run {
+            sektorkode.shouldNotBeBlank()
+            organisasjonDetaljer.shouldNotBeNull()
+                .naering.shouldNotBeEmpty()
+            oppstartsdato.shouldBeNull()
+            enhetstype.shouldNotBeBlank()
+        }
     }
 
     @Test
-    @Throws(IOException::class)
-    fun testVirksomhet() {
-        val ressurs = "organisasjon/975270211.xml"
-        val saksopplysning = getSaksopplysning(ressurs)
-        val dokument = saksopplysning!!.dokument as OrganisasjonDokument
-        Assertions.assertThat(dokument.sektorkode).isEmpty()
-        Assertions.assertThat(dokument.organisasjonDetaljer!!.naering).isNotEmpty()
-        Assertions.assertThat(dokument.oppstartsdato).isNotNull()
-        Assertions.assertThat(dokument.enhetstype).isEmpty()
+    fun `Orgledd test`() {
+        val dokument = getSaksopplysning(ORGLEDD_RESSURS).dokument as OrganisasjonDokument
+
+
+        dokument.run {
+            sektorkode.shouldNotBeBlank()
+            organisasjonDetaljer.shouldNotBeNull()
+                .naering.shouldNotBeEmpty()
+            oppstartsdato.shouldBeNull()
+            enhetstype.shouldBeEmpty()
+        }
     }
 
-    @Throws(IOException::class)
-    override fun getSaksopplysning(ressurs: String?): Saksopplysning? {
-        val kilde = javaClass.getClassLoader().getResourceAsStream(ressurs)
-        Objects.requireNonNull(kilde)
+    @Test
+    fun `Virksomhet test`() {
+        val dokument = getSaksopplysning(VIRKSOMHET_RESSURS).dokument as OrganisasjonDokument
+
+
+        dokument.run {
+            sektorkode.shouldBeEmpty()
+            organisasjonDetaljer.shouldNotBeNull()
+                .naering.shouldNotBeEmpty()
+            oppstartsdato.shouldNotBeNull()
+            enhetstype.shouldBeEmpty()
+        }
+    }
+
+    override fun getSaksopplysning(ressurs: String?): Saksopplysning {
+        val kilde = javaClass.getClassLoader()
+            .getResourceAsStream(ressurs)
+            .shouldNotBeNull()
         return konverter(kilde, factory, SaksopplysningType.ORG, "4.0")
     }
 
     companion object {
         private const val EREG_4_0_MOCK: String = "organisasjon/org_med_strukturert_adresse.xml"
-        private var factory: DokumentFactory? = null
-        @BeforeAll
-        @JvmStatic
-        fun setUp() {
-            val marshaller = JaxbConfig.getJaxb2Marshaller()
-            val xsltTemplatesFactory = XsltTemplatesFactory()
-            factory = DokumentFactory(marshaller, xsltTemplatesFactory)
-        }
+        private const val ORGLEDD_RESSURS: String = "organisasjon/974652366.xml"
+        private const val VIRKSOMHET_RESSURS: String = "organisasjon/975270211.xml"
     }
 }
