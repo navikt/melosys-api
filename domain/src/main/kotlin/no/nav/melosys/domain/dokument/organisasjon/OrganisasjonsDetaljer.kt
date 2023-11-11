560fc43d60f86e0f28662bee22ba58fb9507b598
RuneLind <rulind@gmail.com>
2023-11-11 11:49:04 +0100

    Rydd i OrganisasjonsDetaljer

--- domain/src/main/kotlin/no/nav/melosys/domain/dokument/organisasjon/OrganisasjonsDetaljer.kt
+++ domain/src/main/kotlin/no/nav/melosys/domain/dokument/organisasjon/OrganisasjonsDetaljer.kt
@@ -7,7 +7,6 @@ import no.nav.melosys.domain.dokument.organisasjon.adresse.GeografiskAdresse
 import no.nav.melosys.domain.dokument.organisasjon.adresse.SemistrukturertAdresse
 import no.nav.melosys.domain.dokument.organisasjon.adresse.elektronisk.Epost
 import no.nav.melosys.domain.dokument.organisasjon.adresse.elektronisk.Telefonnummer
-import org.springframework.util.StringUtils
 import java.time.LocalDate
 import javax.xml.bind.annotation.XmlAccessType
 import javax.xml.bind.annotation.XmlAccessorType
@@ -45,76 +44,36 @@ open class OrganisasjonsDetaljer { // Needs to be open because of mocking : TODO
         return konverterTilUstrukturertAdresse(adresse)
     }
 
-    private fun hentFørsteGyldigeForretningsadresse(): GeografiskAdresse? {
-        return hentFørsteGyldigeAdresse(forretningsadresse)
-    }
+    private fun hentFørsteGyldigeForretningsadresse(): GeografiskAdresse? = hentFørsteGyldigeAdresse(forretningsadresse)
 
-    private fun hentFørsteGyldigePostadresse(): GeografiskAdresse? {
-        return hentFørsteGyldigeAdresse(postadresse)
-    }
+    private fun hentFørsteGyldigePostadresse(): GeografiskAdresse? = hentFørsteGyldigeAdresse(postadresse)
 
-    private fun hentFørsteGyldigeAdresse(adresser: List<GeografiskAdresse>): GeografiskAdresse? {
-        for (adresse in adresser) {
-            val gyldighetsperiode = adresse.gyldighetsperiode
-            if (gyldighetsperiode!!.erGyldig()) {
-                return adresse
-            }
-        }
-        return null
-    }
+    private fun hentFørsteGyldigeAdresse(adresser: List<GeografiskAdresse>): GeografiskAdresse? =
+        adresser.firstOrNull { it.gyldighetsperiode?.erGyldig() == true }
 
     private fun konverterTilUstrukturertAdresse(adresse: GeografiskAdresse?): UstrukturertAdresse? {
-        if (adresse == null) {
-            return null
-        }
-        val ustrukturertAdresse: UstrukturertAdresse?
-        ustrukturertAdresse = if (adresse is SemistrukturertAdresse) {
-            val sAdresse = adresse as SemistrukturertAdresse?
-            UstrukturertAdresse.av(sAdresse)
-        } else {
-            // Enhetsregistret har bare SemistrukturertAdresser
-            throw IllegalArgumentException("Adresse ikke støttet " + adresse.javaClass.getSimpleName())
-        }
-        return ustrukturertAdresse
+        if (adresse == null) return null
+        require(adresse is SemistrukturertAdresse) { "Adresse ikke støttet " + adresse.javaClass.getSimpleName() }
+        return UstrukturertAdresse.av(adresse)
     }
 
     private fun konverterTilStrukturertAdresse(adresse: GeografiskAdresse?): StrukturertAdresse? {
-        if (adresse == null) {
-            return null
-        }
-        val strukturertAdresse = StrukturertAdresse()
-        if (adresse is SemistrukturertAdresse) {
-            val sAdresse = adresse as SemistrukturertAdresse?
-            val stringBuilder = StringBuilder()
-            if (sAdresse!!.adresselinje1 != null) {
-                stringBuilder.append(sAdresse.adresselinje1)
-            }
-            if (sAdresse.adresselinje2 != null) {
-                stringBuilder.append(" ")
-                stringBuilder.append(adresse.adresselinje2)
-            }
-            if (sAdresse.adresselinje3 != null) {
-                stringBuilder.append(" ")
-                stringBuilder.append(sAdresse.adresselinje3)
-            }
-            val adresseLinje = stringBuilder.toString()
-            strukturertAdresse.gatenavn = adresseLinje.replace("\\s+".toRegex(), " ")
-            strukturertAdresse.landkode = sAdresse.landkode
-            strukturertAdresse.postnummer = sAdresse.postnr
-            if (sAdresse.erUtenlandsk()) {
-                strukturertAdresse.poststed =
-                    if (StringUtils.isEmpty(sAdresse.poststedUtland)) sAdresse.poststed else sAdresse.poststedUtland
-                // Utenlandsk adresse kan ha postnummer som en del av poststed
-                if (strukturertAdresse.postnummer == null) {
-                    strukturertAdresse.postnummer = " "
-                }
+        if (adresse == null) return null
+        require(adresse is SemistrukturertAdresse) { "Adresse ikke støttet ${adresse.javaClass.simpleName}" }
+
+        val adresseLinje = listOfNotNull(adresse.adresselinje1, adresse.adresselinje2, adresse.adresselinje3)
+            .joinToString(" ")
+            .replace("\\s+".toRegex(), " ")
+
+        return StrukturertAdresse().apply {
+            gatenavn = adresseLinje
+            landkode = adresse.landkode
+            postnummer = adresse.postnr ?: " "
+            poststed = if (adresse.erUtenlandsk()) {
+                adresse.poststedUtland?.ifEmpty { adresse.poststed ?: "" }
             } else {
-                strukturertAdresse.poststed = if (sAdresse.poststed == null) "" else sAdresse.poststed
+                adresse.poststed ?: ""
             }
-        } else {
-            // Enhetsregistret har bare SemistrukturertAdresser
-            throw IllegalArgumentException("GeografiskAdresse ikke støttet " + adresse.javaClass.getSimpleName())
         }
-        return strukturertAdresse
     }
 }
