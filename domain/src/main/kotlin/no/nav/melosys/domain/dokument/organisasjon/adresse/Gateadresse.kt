5cb0ec9c0e6cc9a9c3874164c98d0c0904b33783
RuneLind <rulind@gmail.com>
2023-11-16 15:13:26 +0100

    Fiks kommentar

--- domain/src/main/kotlin/no/nav/melosys/domain/dokument/organisasjon/adresse/Gateadresse.kt
+++ domain/src/main/kotlin/no/nav/melosys/domain/dokument/organisasjon/adresse/Gateadresse.kt
@@ -4,12 +4,12 @@ package no.nav.melosys.domain.dokument.organisasjon.adresse
  * En geografisk adresse som angir geografisk plassering i veiadresse form. Vil brukes om adresser i Norge.
  */
 class Gateadresse : StrukturertAdresse() {
-    var poststed: String? = null // hadde xmk required = true
+    var poststed: String? = null // hadde xml required = true - TODO: se om vi kan bruke non-null
     var bolignummer: String? = null
     var kommunenummer: String? = null
     var gatenummer: Int? = null
 
-    var gatenavn: String? = null // hadde xmk required = true
+    var gatenavn: String? = null // hadde xml required = true - TODO: se om vi kan bruke non-null
     var husnummer: Int? = null
     var husbokstav: String? = null
 
