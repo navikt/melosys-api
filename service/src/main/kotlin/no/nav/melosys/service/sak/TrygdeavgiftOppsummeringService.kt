b52d66f9d1ed250f8f63abb7cd9b21e5bf90bf9c
Janita Johansen <56385061+janitamarielle@users.noreply.github.com>
2023-11-30 14:52:25 +0100

    6235 TOGGLE Forslag til nytt endepunkt (#2111)
    https://jira.adeo.no/browse/MELOSYS-6235
    
    TOGGLE siden dette ligger bak fullmektig-trygdeavgift toggle
    
    ---------
    
    Co-authored-by: Marton Skjæveland <marton.skjeveland@nav.no>

---
+++ service/src/main/kotlin/no/nav/melosys/service/sak/TrygdeavgiftOppsummeringService.kt
@@ -0,0 +1,27 @@
+package no.nav.melosys.service.sak
+
+import no.nav.melosys.domain.avgift.Trygdeavgiftsperiode
+import no.nav.melosys.service.behandling.BehandlingsresultatService
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+class TrygdeavgiftOppsummeringService(
+    private val fagsakService: FagsakService,
+    private val behandlingsresultatService: BehandlingsresultatService,
+) {
+    @Transactional
+    fun harFagsakBehandlingerMedTrygdeavgift(saksnummer: String): Boolean {
+        return fagsakService.hentFagsak(saksnummer)
+            .behandlinger
+            .map { behandling -> behandlingsresultatService.hentBehandlingsresultat(behandling.id) }
+            .any {
+                it.medlemAvFolketrygden?.fastsattTrygdeavgift?.trygdeavgiftsperioder?.filter { trygdeavgiftsperiodeHarAvgift(it) }
+                    ?.isNotEmpty() ?: false
+            }
+    }
+
+    private fun trygdeavgiftsperiodeHarAvgift(trygdeavgiftsperiode: Trygdeavgiftsperiode?): Boolean {
+        return (trygdeavgiftsperiode != null) && (trygdeavgiftsperiode.trygdeavgiftsbeløpMd != null) && trygdeavgiftsperiode.harAvgift()
+    }
+}
---
+++ service/src/test/kotlin/no/nav/melosys/service/sak/TrygdeavgiftOppsummeringServiceTest.kt
@@ -0,0 +1,106 @@
+package no.nav.melosys.service.sak
+
+import io.kotest.matchers.booleans.shouldBeFalse
+import io.kotest.matchers.booleans.shouldBeTrue
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.junit5.MockKExtension
+import no.nav.melosys.domain.Behandling
+import no.nav.melosys.domain.Behandlingsresultat
+import no.nav.melosys.domain.Fagsak
+import no.nav.melosys.domain.avgift.Penger
+import no.nav.melosys.domain.avgift.Trygdeavgiftsperiode
+import no.nav.melosys.domain.folketrygden.FastsattTrygdeavgift
+import no.nav.melosys.domain.folketrygden.MedlemAvFolketrygden
+import no.nav.melosys.service.behandling.BehandlingsresultatService
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import java.math.BigDecimal
+
+@ExtendWith(MockKExtension::class)
+class TrygdeavgiftOppsummeringServiceTest {
+
+    private val SAKSNUMMER = "MEL-123"
+    private val BEHANDLING_ID = 123L
+
+    @MockK
+    private lateinit var fagsakService: FagsakService
+
+    @MockK
+    private lateinit var behandlingsresultatService: BehandlingsresultatService
+
+    private lateinit var trygdeavgiftOppsummeringService: TrygdeavgiftOppsummeringService
+
+    private lateinit var fagsak: Fagsak
+    private lateinit var behandlingsresultat: Behandlingsresultat
+
+    @BeforeEach
+    fun setup() {
+        trygdeavgiftOppsummeringService = TrygdeavgiftOppsummeringService(fagsakService, behandlingsresultatService)
+        fagsak = Fagsak().apply { behandlinger.add(Behandling().apply { id = BEHANDLING_ID }) }
+        behandlingsresultat = Behandlingsresultat()
+        every { fagsakService.hentFagsak(SAKSNUMMER) }.returns(fagsak)
+        every { behandlingsresultatService.hentBehandlingsresultat(BEHANDLING_ID) }.returns(behandlingsresultat)
+    }
+
+    @Test
+    fun harFagsakBehandlingerMedTrygdeavgift_harIkkeMedlemAvFolketrygden_returnererFalse() {
+        behandlingsresultat.apply { medlemAvFolketrygden = null }
+
+
+        trygdeavgiftOppsummeringService.harFagsakBehandlingerMedTrygdeavgift(SAKSNUMMER).shouldBeFalse()
+    }
+
+    @Test
+    fun harFagsakBehandlingerMedTrygdeavgift_harTrygedavgiftsperiodeMenDenHarIkkeSattMndBeløp_returnererFalse() {
+        val trygdeavgiftsperiode = Trygdeavgiftsperiode().apply { trygdeavgiftsbeløpMd = null }
+        behandlingsresultat.apply {
+            medlemAvFolketrygden = MedlemAvFolketrygden().apply {
+                fastsattTrygdeavgift = FastsattTrygdeavgift().apply { trygdeavgiftsperioder.add(trygdeavgiftsperiode) }
+            }
+        }
+
+
+        trygdeavgiftOppsummeringService.harFagsakBehandlingerMedTrygdeavgift(SAKSNUMMER).shouldBeFalse()
+    }
+
+    @Test
+    fun harFagsakBehandlingerMedTrygdeavgift_harTrygedavgiftsperiodeMenDenHarIkkeAvgift_returnererFalse() {
+        val trygdeavgiftsperiode = Trygdeavgiftsperiode().apply {
+            trygdeavgiftsbeløpMd = Penger(0.0)
+            trygdesats = BigDecimal.ZERO
+        }
+        behandlingsresultat.apply {
+            medlemAvFolketrygden = MedlemAvFolketrygden().apply {
+                fastsattTrygdeavgift = FastsattTrygdeavgift().apply { trygdeavgiftsperioder.add(trygdeavgiftsperiode) }
+            }
+        }
+
+
+        trygdeavgiftOppsummeringService.harFagsakBehandlingerMedTrygdeavgift(SAKSNUMMER).shouldBeFalse()
+    }
+
+    @Test
+    fun harFagsakBehandlingerMedTrygdeavgift_harTrygedavgiftsperioderBådeMedOgUtenAvgift_returnererTrue() {
+        val trygdeavgiftsperiode1 = Trygdeavgiftsperiode().apply {
+            trygdeavgiftsbeløpMd = Penger(0.0)
+            trygdesats = BigDecimal.ZERO
+        }
+        val trygdeavgiftsperiode2 = Trygdeavgiftsperiode().apply {
+            trygdeavgiftsbeløpMd = Penger(2345.56)
+            trygdesats = BigDecimal(3.56)
+        }
+        behandlingsresultat.apply {
+            medlemAvFolketrygden = MedlemAvFolketrygden().apply {
+                fastsattTrygdeavgift = FastsattTrygdeavgift().apply {
+                    trygdeavgiftsperioder.add(trygdeavgiftsperiode1)
+                    trygdeavgiftsperioder.add(trygdeavgiftsperiode2)
+                }
+            }
+        }
+
+
+        trygdeavgiftOppsummeringService.harFagsakBehandlingerMedTrygdeavgift(SAKSNUMMER).shouldBeTrue()
+    }
+}
