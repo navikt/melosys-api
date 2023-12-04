235200b76d4c8d0371974eeca342274cf0e307b3
barstadEkstern <oystein.madsen.barstad@nav.no>
2023-12-04 18:40:10 +0100

    fjern kommentar

--- service/src/main/java/no/nav/melosys/service/kontroll/feature/ferdigbehandling/kontroll/KontrolldataFeilType.kt
+++ service/src/main/java/no/nav/melosys/service/kontroll/feature/ferdigbehandling/kontroll/KontrolldataFeilType.kt
@@ -1,6 +1,5 @@
 package no.nav.melosys.service.kontroll.feature.ferdigbehandling.kontroll
 
-//Enum class that contains either advarsel or feil
 enum class KontrolldataFeilType {
     ADVARSEL,
     FEIL
