3dec707acc9703613197239c64c2e9ce9d0ca794
Isa Salem Hame <isa.salem.hame@nav.no>
2023-12-06 12:18:33 +0100

    5781: Endre request for OIDC-token (#2129)
    mock-oauth2-server krevde POST request og client_credentials og scope i
    body. Rest STS tar imot begge deler. Da går vi for POST og params i
    body, sånn at det passer for begge.
    
    Se https://github.com/navikt/gandalf

--- integrasjonstest/src/test/kotlin/no/nav/melosys/melosysmock/reststs/RestStsApi.kt
+++ integrasjonstest/src/test/kotlin/no/nav/melosys/melosysmock/reststs/RestStsApi.kt
@@ -2,6 +2,7 @@ package no.nav.melosys.melosysmock.reststs
 
 import no.nav.security.token.support.core.api.Unprotected
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/rest")
 @Unprotected
 class RestStsApi {
-    @GetMapping("/v1/sts/token")
+    @PostMapping("/v1/sts/token")
     fun getCredentials() : Map<String, Any> =
         mapOf("access_token" to "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c", "expires_in" to 100000)
 }
