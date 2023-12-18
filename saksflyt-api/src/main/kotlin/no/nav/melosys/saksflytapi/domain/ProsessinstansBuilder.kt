232505933663dda1d3a59a5b85381af33d804d22
RuneLind <rulind@gmail.com>
2023-12-18 19:06:40 +0100

    Rydd i ProsessinstansBuilder etter konverteing til Kotlin

--- saksflyt-api/src/main/kotlin/no/nav/melosys/saksflytapi/domain/ProsessinstansBuilder.kt
+++ saksflyt-api/src/main/kotlin/no/nav/melosys/saksflytapi/domain/ProsessinstansBuilder.kt
@@ -6,71 +6,44 @@ import no.nav.melosys.domain.eessi.melding.MelosysEessiMelding
 import org.apache.commons.lang3.StringUtils
 import org.springframework.util.CollectionUtils
 
-class ProsessinstansBuilder {
-    private var type: ProsessType? = null
-    private var behandling: Behandling? = null
-    private var begrunnelseFritekst: String? = null
-    private var vedleggTilSed: Set<DokumentReferanse?>? = null
-    private var ytterligereInformasjonTilSed: String? = null
-    private var eessiMottakere: Set<String?>? = null
+class ProsessinstansBuilder(
+    private var type: ProsessType? = null,
+    private var behandling: Behandling? = null,
+    private var begrunnelseFritekst: String? = null,
+    private var vedleggTilSed: Set<DokumentReferanse?>? = null,
+    private var ytterligereInformasjonTilSed: String? = null,
+    private var eessiMottakere: Set<String?>? = null,
     private var eessiMelding: MelosysEessiMelding? = null
+) {
+    fun medType(type: ProsessType?): ProsessinstansBuilder = apply { this.type = type }
+    fun medBehandling(behandling: Behandling?): ProsessinstansBuilder = apply { this.behandling = behandling }
+    fun medBegrunnelseFritekst(fritekst: String?): ProsessinstansBuilder = apply { this.begrunnelseFritekst = fritekst }
+    fun medVedleggTilSed(vedlegg: Set<DokumentReferanse?>?): ProsessinstansBuilder = apply { this.vedleggTilSed = vedlegg }
+    fun medYtterligereinformasjonSed(ytterligereInformasjonSed: String?): ProsessinstansBuilder =
+        apply { this.ytterligereInformasjonTilSed = ytterligereInformasjonSed }
 
-    fun medType(type: ProsessType?): ProsessinstansBuilder {
-        this.type = type
-        return this
-    }
+    fun medEessiMottakere(eessiMottakere: Set<String?>?): ProsessinstansBuilder = apply { this.eessiMottakere = eessiMottakere }
+    fun medEessiMelding(eessiMelding: MelosysEessiMelding?): ProsessinstansBuilder = apply { this.eessiMelding = eessiMelding }
 
-    fun medBehandling(behandling: Behandling?): ProsessinstansBuilder {
-        this.behandling = behandling
-        return this
-    }
+    fun build(): Prosessinstans = Prosessinstans().apply {
+        behandling = this@ProsessinstansBuilder.behandling
+        type = this@ProsessinstansBuilder.type
 
-    fun medBegrunnelseFritekst(fritekst: String?): ProsessinstansBuilder {
-        this.begrunnelseFritekst = fritekst
-        return this
-    }
-
-    fun medVedleggTilSed(vedlegg: Set<DokumentReferanse?>?): ProsessinstansBuilder {
-        this.vedleggTilSed = vedlegg
-        return this
-    }
-
-    fun medYtterligereinformasjonSed(ytterligereInformasjonSed: String?): ProsessinstansBuilder {
-        this.ytterligereInformasjonTilSed = ytterligereInformasjonSed
-        return this
-    }
-
-    fun medEessiMottakere(eessiMottakere: Set<String?>?): ProsessinstansBuilder {
-        this.eessiMottakere = eessiMottakere
-        return this
-    }
-
-    fun medEessiMelding(eessiMelding: MelosysEessiMelding?): ProsessinstansBuilder {
-        this.eessiMelding = eessiMelding
-        return this
-    }
-
-    fun build(): Prosessinstans {
-        val prosessinstans = Prosessinstans()
-        prosessinstans.behandling = behandling
-        prosessinstans.type = type
-
-        if (StringUtils.isNotEmpty(begrunnelseFritekst)) {
-            prosessinstans.setData(ProsessDataKey.BEGRUNNELSE_FRITEKST, begrunnelseFritekst)
+        if (begrunnelseFritekst.isNullOrEmpty()) {
+            setData(ProsessDataKey.BEGRUNNELSE_FRITEKST, begrunnelseFritekst)
         }
         if (!CollectionUtils.isEmpty(vedleggTilSed)) {
-            prosessinstans.setData(ProsessDataKey.VEDLEGG_SED, vedleggTilSed)
+            setData(ProsessDataKey.VEDLEGG_SED, vedleggTilSed)
         }
         if (StringUtils.isNotEmpty(ytterligereInformasjonTilSed)) {
-            prosessinstans.setData(ProsessDataKey.YTTERLIGERE_INFO_SED, ytterligereInformasjonTilSed)
+            setData(ProsessDataKey.YTTERLIGERE_INFO_SED, ytterligereInformasjonTilSed)
         }
         if (!CollectionUtils.isEmpty(eessiMottakere)) {
-            prosessinstans.setData(ProsessDataKey.EESSI_MOTTAKERE, eessiMottakere)
+            setData(ProsessDataKey.EESSI_MOTTAKERE, eessiMottakere)
         }
-        if (eessiMelding != null) {
-            prosessinstans.setData(ProsessDataKey.EESSI_MELDING, eessiMelding)
-            prosessinstans.låsReferanse = eessiMelding!!.lagUnikIdentifikator()
+        eessiMelding?.let {
+            setData(ProsessDataKey.EESSI_MELDING, it)
+            låsReferanse = it.lagUnikIdentifikator()
         }
-        return prosessinstans
     }
 }
