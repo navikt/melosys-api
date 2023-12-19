0a16b1acfccdf9065367657a68f8f47011f2d598
Thang Phan <thang.quoc.ly.phan@nav.no>
2023-12-19 11:29:47 +0100

    Filtrering av adresser i kontroll

--- service/src/main/java/no/nav/melosys/service/kontroll/regler/PersonRegler.java
+++ service/src/main/java/no/nav/melosys/service/kontroll/regler/PersonRegler.java
@@ -3,9 +3,11 @@ package no.nav.melosys.service.kontroll.regler;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Predicate;
 
 import no.nav.melosys.domain.dokument.person.adresse.BostedsadressePeriode;
 import no.nav.melosys.domain.mottatteopplysninger.MottatteOpplysningerData;
+import no.nav.melosys.domain.oppgave.Oppgave;
 import no.nav.melosys.domain.person.Persondata;
 import no.nav.melosys.domain.person.adresse.Bostedsadresse;
 
@@ -24,17 +26,31 @@ public final class PersonRegler {
             && NORGE_ISO2_LANDKODE.equals(bostedsadresseOptional.get().strukturertAdresse().getLandkode());
     }
 
-    public static boolean personBosattINorgeIPeriode(List<BostedsadressePeriode> bostedsadressePerioder, LocalDate periodeFra, LocalDate periodeTil) {
-        return bostedsadressePerioder
+    public static boolean personBosattINorgeIPeriode(List<BostedsadressePeriode> bostedsadressePerioder, Optional<Bostedsadresse> bostedsadresseOptional, LocalDate periodeFra, LocalDate periodeTil) {
+        var erGyldigBostedsadresse = bostedsadresseOptional.filter(bostedsadresse -> bostedsadresse.strukturertAdresse().getLandkode() != null &&
+            NORGE_ISO2_LANDKODE.equals(bostedsadresse.strukturertAdresse().getLandkode()) &&
+            filtrerAdressePeriode(bostedsadresse.gyldigFraOgMed(), bostedsadresse.gyldigTilOgMed(), periodeFra, periodeTil)).isPresent();
+
+        return erGyldigBostedsadresse || bostedsadressePerioder
             .stream()
             .anyMatch(a ->
                 a.bostedsadresse.tilStrukturertAdresse().getLandkode().equals(NORGE_ISO2_LANDKODE) &&
-                    ((a.periode.getFom().isBefore(periodeFra) || a.periode.getFom().isEqual(periodeFra)) &&
-                        (a.periode.getTom().isAfter(periodeFra) || a.periode.getTom().isEqual(periodeFra))) ||
-                    ((a.periode.getFom().isBefore(periodeTil) || a.periode.getFom().isEqual(periodeTil)) &&
-                        (a.periode.getTom().isAfter(periodeTil) || a.periode.getTom().isEqual(periodeTil)))
+                    filtrerAdressePeriode(a.periode.getFom(), a.periode.getTom(), periodeFra, periodeTil)
             );
+    }
 
+    private static boolean filtrerAdressePeriode(LocalDate adresseGyldigFom, LocalDate adresseGyldigTom, LocalDate periodeFra, LocalDate periodeTil) {
+        var fom = adresseGyldigFom;
+        var tom = adresseGyldigTom != null ? adresseGyldigTom : LocalDate.now().plusYears(10);
+
+        if(fom == null){
+            return false;
+        }
+
+        return ((fom.isBefore(periodeFra) || fom.isEqual(periodeFra)) &&
+            (tom.isAfter(periodeFra) || tom.isEqual(periodeFra))) ||
+            ((fom.isBefore(periodeTil) || fom.isEqual(periodeTil)) &&
+                (tom.isAfter(periodeTil) || tom.isEqual(periodeTil)));
     }
 
     public static boolean harRegistrertAdresse(Persondata persondata, MottatteOpplysningerData mottatteOpplysningerData) {
