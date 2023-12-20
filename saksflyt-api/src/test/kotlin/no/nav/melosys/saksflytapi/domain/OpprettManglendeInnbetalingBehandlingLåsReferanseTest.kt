d52c04d3ab411d474a7fb6965af631703d3485d6
RuneLind <rulind@gmail.com>
2023-12-20 14:17:19 +0100

    Fiks navn i test

--- saksflyt-api/src/test/kotlin/no/nav/melosys/saksflytapi/domain/OpprettManglendeInnbetalingBehandlingLåsReferanseTest.kt
+++ saksflyt-api/src/test/kotlin/no/nav/melosys/saksflytapi/domain/OpprettManglendeInnbetalingBehandlingLåsReferanseTest.kt
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test
 class OpprettManglendeInnbetalingBehandlingLåsReferanseTest {
 
     @Test
-    fun initierSedLåsReferanse_gyldigReferanseString_verifiserParsing() {
+    fun initierLåsReferanse_gyldigReferanseString_verifiserParsing() {
         OpprettManglendeInnbetalingBehandlingLåsReferanse(PREFIX + "_" + FAKTURASERIE_REFERANSE + "_" + FAKTURANUMMER).apply {
             fakturaserieReferanse shouldBe FAKTURASERIE_REFERANSE
             fakturanummer shouldBe FAKTURANUMMER
@@ -15,7 +15,7 @@ class OpprettManglendeInnbetalingBehandlingLåsReferanseTest {
     }
 
     @Test
-    fun initierSedLåsReferanse_ugyldigReferanseString_kasterException() {
+    fun initierLåsReferanse_ugyldigReferanseString_kasterException() {
         shouldThrow<IllegalArgumentException> {
             OpprettManglendeInnbetalingBehandlingLåsReferanse(FAKTURASERIE_REFERANSE)
         }.message.shouldBe("$FAKTURASERIE_REFERANSE er ikke gyldig OpprettManglendeInnbetalingBehandling-referanse")
