d15dd7ec6ea82edaadf8ee1114d814958ab0e3e8
Janita Johansen <janita.marielle.johansen@nav.no>
2023-12-20 07:05:04 +0100

    Database-endringer

--- domain/src/main/java/no/nav/melosys/domain/Aktoer.java
+++ domain/src/main/java/no/nav/melosys/domain/Aktoer.java
@@ -7,7 +7,6 @@ import javax.persistence.*;
 import no.nav.melosys.domain.kodeverk.Aktoersroller;
 import no.nav.melosys.domain.kodeverk.Fullmaktstype;
 import no.nav.melosys.domain.kodeverk.Land_iso2;
-import no.nav.melosys.domain.kodeverk.Representerer;
 import no.nav.melosys.exception.TekniskException;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
@@ -46,10 +45,6 @@ public class Aktoer extends RegistreringsInfo {
 
     @Column(name = "utenlandsk_person_id")
     private String utenlandskPersonId;
-
-    @Enumerated(EnumType.STRING)
-    @Column(name = "representerer")
-    private Representerer representerer;
 
     @OneToMany(mappedBy = "aktoer", cascade = CascadeType.ALL, fetch = FetchType.EAGER, orphanRemoval = true)
     private Set<Fullmakt> fullmakter = new HashSet<>(1);
