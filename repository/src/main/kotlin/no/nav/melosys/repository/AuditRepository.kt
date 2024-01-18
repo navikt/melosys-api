26c3b0116653173e07dff36fd8ae9afc75357d56
Francois <francois.allix@nav.no>
2024-01-18 09:14:52 +0100

    6244 endringshistorikk API, ikke i bruk ennå (#2172)
    - hent gyldige aktører på et gitt tidspunkt
    
    - utkast for visning av historikk backend,

--- repository/src/main/kotlin/no/nav/melosys/repository/AuditRepository.kt
+++ repository/src/main/kotlin/no/nav/melosys/repository/AuditRepository.kt
@@ -1,6 +1,5 @@
 package no.nav.melosys.repository
 
-import org.hibernate.envers.AuditReader
 import org.hibernate.envers.AuditReaderFactory
 import org.hibernate.envers.DefaultRevisionEntity
 import org.hibernate.envers.RevisionType
@@ -8,17 +7,21 @@ import org.hibernate.envers.query.AuditEntity
 import org.springframework.stereotype.Repository
 import org.springframework.transaction.annotation.Transactional
 import org.springframework.util.StringUtils
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZoneId
 import java.util.*
 import javax.persistence.EntityManager
 import javax.persistence.PersistenceContext
 
+internal const val EUROPE_OSLO = "Europe/Oslo"
+
 @Repository
 class AuditRepository {
     @PersistenceContext
-    private val entityManager: EntityManager? = null
+    private lateinit var entityManager: EntityManager
 
-    private val auditReader: AuditReader
-        get() = AuditReaderFactory.get(entityManager)
+    private val auditReader get() = AuditReaderFactory.get(entityManager)
 
     @Transactional(readOnly = true)
     fun <T> getRevisions(tClass: Class<T>, propertiesMap: Map<String, Any>): List<EntityRevision<T>> {
@@ -28,11 +31,30 @@ class AuditRepository {
             require(!(!StringUtils.hasText(key) || Objects.isNull(value))) { "Invalid property in map: key or value is null or empty." }
         }
 
-        val auditQuery = auditReader.createQuery()
-            .forRevisionsOfEntity(tClass, false, true)
+        val auditQuery = auditReader.createQuery().forRevisionsOfEntity(tClass, false, true)
         for ((key, value) in propertiesMap) {
             auditQuery.add(AuditEntity.property(key).eq(value))
         }
+
+        val resultList = auditQuery.resultList as List<Array<Any>>
+        return resultList.map { array -> EntityRevision(array[0] as T, array[1] as DefaultRevisionEntity, array[2] as RevisionType) }
+    }
+
+    @Transactional(readOnly = true)
+    fun <T> getRevisionsBeforeOrAtDate(tClass: Class<T>, propertiesMap: Map<String, Any>, dateTime: LocalDateTime): List<EntityRevision<T>> {
+        require(propertiesMap.isNotEmpty()) { "Invalid params." }
+
+        for ((key, value) in propertiesMap) {
+            require(!(!StringUtils.hasText(key) || Objects.isNull(value))) { "Invalid property in map: key or value is null or empty." }
+        }
+
+        val auditQuery = auditReader.createQuery().forRevisionsOfEntity(tClass, false, false)
+        for ((key, value) in propertiesMap) {
+            auditQuery.add(AuditEntity.property(key).eq(value))
+        }
+
+        val timestamp = dateTime.atZone(ZoneId.of(EUROPE_OSLO)).toInstant().toEpochMilli()
+        auditQuery.add(AuditEntity.revisionProperty("timestamp").le(timestamp))
 
         val resultList = auditQuery.resultList as List<Array<Any>>
         return resultList.map { array -> EntityRevision(array[0] as T, array[1] as DefaultRevisionEntity, array[2] as RevisionType) }
@@ -43,4 +65,7 @@ data class EntityRevision<T>(
     val entity: T,
     val revisionInfo: DefaultRevisionEntity,
     val revisionType: RevisionType
-)
+) {
+    val revisionLocalDateTime : LocalDateTime
+        get() = Instant.ofEpochMilli(this.revisionInfo.timestamp).atZone(ZoneId.of(EUROPE_OSLO)).toLocalDateTime()
+}
