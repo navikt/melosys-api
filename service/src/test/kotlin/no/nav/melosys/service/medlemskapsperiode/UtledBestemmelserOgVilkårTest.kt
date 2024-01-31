14a5a6bca41cd0c27418b8ac12b503ed1c87e6de
Isa Salem Hame <isa.salem.hame@nav.no>
2024-01-31 13:16:49 +0100

    6368 DWEB TOGGLE: Legg til nye vilkår for § 2.7 og 2.8 (#2203)
    * Nye vilkår for § 2.7 og 2.8
    * Begrunnelser for § 2.7
    * https://github.com/navikt/melosys-web/pull/2185
    * https://github.com/navikt/melosys-kodeverk/pull/368 
    
    https://jira.adeo.no/browse/MELOSYS-6368

--- service/src/test/kotlin/no/nav/melosys/service/medlemskapsperiode/UtledBestemmelserOgVilkårTest.kt
+++ service/src/test/kotlin/no/nav/melosys/service/medlemskapsperiode/UtledBestemmelserOgVilkårTest.kt
@@ -1,31 +1,57 @@
 package no.nav.melosys.service.medlemskapsperiode
 
-import io.kotest.matchers.collections.shouldBeEmpty
-import io.kotest.matchers.collections.shouldContain
-import io.kotest.matchers.collections.shouldNotBeEmpty
-import io.kotest.matchers.maps.shouldBeEmpty
-import io.kotest.matchers.maps.shouldHaveKeys
-import io.kotest.matchers.maps.shouldNotBeEmpty
-import io.kotest.matchers.maps.shouldNotHaveKeys
+import io.getunleash.FakeUnleash
+import io.kotest.matchers.collections.*
+import io.kotest.matchers.maps.*
 import io.kotest.matchers.nulls.shouldNotBeNull
 import no.nav.melosys.domain.kodeverk.Folketrygdloven_kap2_bestemmelser
 import no.nav.melosys.domain.kodeverk.Vilkaar
+import no.nav.melosys.domain.kodeverk.begrunnelser.folketrygdloven.Ftrl_2_7_begrunnelser
 import no.nav.melosys.domain.kodeverk.begrunnelser.folketrygdloven.Ftrl_2_8_naer_tilknytning_norge_begrunnelser
 import no.nav.melosys.domain.kodeverk.behandlinger.Behandlingstema
+import no.nav.melosys.featuretoggle.ToggleName
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
 class UtledBestemmelserOgVilkårTest {
 
-    private val utledBestemmelserOgVilkår = UtledBestemmelserOgVilkår()
+    val fakeUnleash = FakeUnleash()
+
+    lateinit var utledBestemmelserOgVilkår: UtledBestemmelserOgVilkår
+
+    @BeforeEach
+    fun setUp() {
+        utledBestemmelserOgVilkår = UtledBestemmelserOgVilkår(fakeUnleash)
+    }
 
     @Test
     fun hentStøttede_behandlingstemaYRKESAKTIV_returnererStøttede() {
-        utledBestemmelserOgVilkår.hentStøttedeBestemmelserOgVilkår(Behandlingstema.YRKESAKTIV)
-            .shouldHaveKeys(
+        utledBestemmelserOgVilkår.hentStøttedeBestemmelserOgVilkår(Behandlingstema.YRKESAKTIV).run {
+            shouldHaveKeys(
                 Folketrygdloven_kap2_bestemmelser.FTRL_KAP2_2_8_FØRSTE_LEDD_A,
-                Folketrygdloven_kap2_bestemmelser.FTRL_KAP2_2_8_ANDRE_LEDD
+                Folketrygdloven_kap2_bestemmelser.FTRL_KAP2_2_8_ANDRE_LEDD,
             )
+            shouldHaveSize(2)
+        }
+
     }
+
+    @Test
+    fun hentStøttede_behandlingstemaYRKESAKTIV_returnererStøttedeMedToggle() {
+        fakeUnleash.enable(ToggleName.MELOSYS_FOLKETRYGDEN_2_7)
+
+        utledBestemmelserOgVilkår.hentStøttedeBestemmelserOgVilkår(Behandlingstema.YRKESAKTIV).run {
+            shouldHaveKeys(
+                Folketrygdloven_kap2_bestemmelser.FTRL_KAP2_2_8_FØRSTE_LEDD_A,
+                Folketrygdloven_kap2_bestemmelser.FTRL_KAP2_2_8_ANDRE_LEDD,
+                Folketrygdloven_kap2_bestemmelser.FTRL_KAP2_2_7_FØRSTE_LEDD,
+                Folketrygdloven_kap2_bestemmelser.FTRL_KAP2_2_7A
+            )
+            shouldHaveSize(4)
+        }
+
+    }
+
 
     @Test
     fun hentStøttede_behandlingstemaIKKE_YRKESAKTIV_returnererFærre() {
@@ -34,12 +60,27 @@ class UtledBestemmelserOgVilkårTest {
     }
 
     @Test
+    fun hentStøttede_behandlingstemaIKKE_YRKESAKTIV_returnererFærreMedToggle() {
+        fakeUnleash.enable(ToggleName.MELOSYS_FOLKETRYGDEN_2_7)
+
+        utledBestemmelserOgVilkår.hentStøttedeBestemmelserOgVilkår(Behandlingstema.IKKE_YRKESAKTIV).run {
+            shouldHaveKeys(
+                Folketrygdloven_kap2_bestemmelser.FTRL_KAP2_2_8_ANDRE_LEDD,
+                Folketrygdloven_kap2_bestemmelser.FTRL_KAP2_2_7_FØRSTE_LEDD
+            )
+            shouldHaveSize(2)
+        }
+    }
+
+    @Test
     fun hentStøttede_ikkeRelevantBehandlingstema_returnererDefault() {
-        utledBestemmelserOgVilkår.hentStøttedeBestemmelserOgVilkår(Behandlingstema.ARBEID_TJENESTEPERSON_ELLER_FLY)
-            .shouldHaveKeys(
+        utledBestemmelserOgVilkår.hentStøttedeBestemmelserOgVilkår(Behandlingstema.ARBEID_TJENESTEPERSON_ELLER_FLY).run {
+            shouldHaveKeys(
                 Folketrygdloven_kap2_bestemmelser.FTRL_KAP2_2_8_FØRSTE_LEDD_A,
                 Folketrygdloven_kap2_bestemmelser.FTRL_KAP2_2_8_ANDRE_LEDD
             )
+            shouldHaveSize(2)
+        }
     }
 
     @Test
@@ -60,12 +101,35 @@ class UtledBestemmelserOgVilkårTest {
     }
 
     @Test
+    fun hentIkkeStøttede_behandlingstemaYRKESAKTIV_returnererIkkeStøttedeMedToggle() {
+        fakeUnleash.enableAll()
+        utledBestemmelserOgVilkår.hentIkkeStøttedeBestemmelserOgVilkår(Behandlingstema.YRKESAKTIV).run {
+            shouldNotBeEmpty()
+            shouldNotHaveKeys(
+                Folketrygdloven_kap2_bestemmelser.FTRL_KAP2_2_8_FØRSTE_LEDD_A,
+                Folketrygdloven_kap2_bestemmelser.FTRL_KAP2_2_8_ANDRE_LEDD,
+                Folketrygdloven_kap2_bestemmelser.FTRL_KAP2_2_7_FØRSTE_LEDD,
+                Folketrygdloven_kap2_bestemmelser.FTRL_KAP2_2_7A
+            )
+        }
+
+    }
+
+    @Test
     fun hentBegrunnelserForVilkår_vilkårErFTRL_2_8_NÆR_TILKNYTNING_NORGE_returnererBegrunnelser() {
         utledBestemmelserOgVilkår.hentBegrunnelserForVilkår(Vilkaar.FTRL_2_8_NÆR_TILKNYTNING_NORGE)
             .shouldNotBeNull()
             .shouldNotBeEmpty()
             .shouldContain(Ftrl_2_8_naer_tilknytning_norge_begrunnelser.ANSATT_I_MULTINASJONALT_SELSKAP.kode)
+    }
 
+
+    @Test
+    fun hentBegrunnelserForVilkår_vilkårErFTRL_2_7_RIMELIGHETSVURDERING_returnererBegrunnelser() {
+        utledBestemmelserOgVilkår.hentBegrunnelserForVilkår(Vilkaar.FTRL_2_7_RIMELIGHETSVURDERING)
+            .shouldNotBeNull()
+            .shouldNotBeEmpty()
+            .shouldContainAll(Ftrl_2_7_begrunnelser.values().map { it.kode })
     }
 
     @Test
