02cf7e22b6e9d771a11e06a659dc5b31b4430f00
barstadEkstern <oystein.madsen.barstad@nav.no>
2024-02-13 13:16:11 +0100

    kommentarer

--- domain/src/main/kotlin/no/nav/melosys/domain/mottatteopplysninger/data/arbeidssteder/LuftfartBase.kt
+++ domain/src/main/kotlin/no/nav/melosys/domain/mottatteopplysninger/data/arbeidssteder/LuftfartBase.kt
@@ -3,15 +3,8 @@ package no.nav.melosys.domain.mottatteopplysninger.data.arbeidssteder
 import no.nav.melosys.domain.kodeverk.Flyvningstyper
 
 
-class LuftfartBase {
-    var hjemmebaseNavn: String? = null
-    var hjemmebaseLand: String? = null
+data class LuftfartBase(
+    var hjemmebaseNavn: String? = null,
+    var hjemmebaseLand: String? = null,
     var typeFlyvninger: Flyvningstyper? = null
-
-    constructor()
-    constructor(hjemmebaseNavn: String?, hjemmebaseLand: String?, flyvningstype: Flyvningstyper?) {
-        this.hjemmebaseNavn = hjemmebaseNavn
-        this.hjemmebaseLand = hjemmebaseLand
-        typeFlyvninger = flyvningstype
-    }
-}
+)
