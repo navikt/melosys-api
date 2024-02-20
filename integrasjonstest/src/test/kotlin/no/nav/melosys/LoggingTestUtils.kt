ca8af69c6006a7303e542671cd7d3c4702902993
RuneLind <rulind@gmail.com>
2024-02-20 15:48:38 +0100

    Fjern ubrukt import

--- integrasjonstest/src/test/kotlin/no/nav/melosys/LoggingTestUtils.kt
+++ integrasjonstest/src/test/kotlin/no/nav/melosys/LoggingTestUtils.kt
@@ -4,7 +4,6 @@ import ch.qos.logback.classic.Logger
 import ch.qos.logback.classic.spi.ILoggingEvent
 import ch.qos.logback.core.read.ListAppender
 import org.slf4j.LoggerFactory
-import java.lang.reflect.Type
 
 object LoggingTestUtils {
     inline fun <reified T> captureLog(block: () -> Unit): List<ILoggingEvent> {
