2a61c66e33a911417c688afbfa72ea553db194ab
Francois Allix <32903762+frans-a@users.noreply.github.com>
2024-02-26 09:34:09 +0100

    Fjern unødvendig @Autowired

--- service/src/main/kotlin/no/nav/melosys/service/avklartefakta/AvklartFamilieRelasjonTypeService.kt
+++ service/src/main/kotlin/no/nav/melosys/service/avklartefakta/AvklartFamilieRelasjonTypeService.kt
@@ -2,11 +2,10 @@ package no.nav.melosys.service.avklartefakta
 
 import no.nav.melosys.domain.kodeverk.Avklartefaktatyper
 import no.nav.melosys.domain.kodeverk.Ikkeyrkesaktivrelasjontype
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
 @Service
-class AvklartFamilieRelasjonTypeService(@Autowired private val avklartefaktaService: AvklartefaktaService) {
+class AvklartFamilieRelasjonTypeService(private val avklartefaktaService: AvklartefaktaService) {
     fun lagreFamilierelasjonstypeSomAvklarteFakta(behandlingID: Long, ikkeyrkesaktivrelasjontype: Ikkeyrkesaktivrelasjontype) {
         avklartefaktaService.slettAvklarteFakta(behandlingID, Avklartefaktatyper.IKKE_YRKESAKTIV_RELASJON)
         avklartefaktaService.leggTilAvklarteFakta(
