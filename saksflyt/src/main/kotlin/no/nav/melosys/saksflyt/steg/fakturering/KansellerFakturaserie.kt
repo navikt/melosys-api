fe84ef1347a703475a8fe37536b1570b49fce32b
Isa Salem Hame <wanghattan@gmail.com>
2024-02-27 15:39:10 +0100

    6479: Kanseller fakturaserie bruker siste behandling med fakturaseriereferanse

--- saksflyt/src/main/kotlin/no/nav/melosys/saksflyt/steg/fakturering/KansellerFakturaserie.kt
+++ saksflyt/src/main/kotlin/no/nav/melosys/saksflyt/steg/fakturering/KansellerFakturaserie.kt
@@ -1,14 +1,12 @@
 package no.nav.melosys.saksflyt.steg.fakturering
 
 import mu.KotlinLogging
-import no.nav.melosys.domain.Behandling
 import no.nav.melosys.integrasjon.faktureringskomponenten.FaktureringskomponentenConsumer
 import no.nav.melosys.saksflyt.steg.StegBehandler
 import no.nav.melosys.saksflytapi.domain.ProsessDataKey
 import no.nav.melosys.saksflytapi.domain.ProsessSteg
 import no.nav.melosys.saksflytapi.domain.Prosessinstans
 import no.nav.melosys.service.behandling.BehandlingsresultatService
-import no.nav.melosys.service.sak.TrygdeavgiftOppsummeringService
 import org.springframework.stereotype.Component
 
 private val log = KotlinLogging.logger { }
@@ -17,7 +15,6 @@ private val log = KotlinLogging.logger { }
 class KansellerFakturaserie(
     private val behandlingsresultatService: BehandlingsresultatService,
     private val faktureringskomponentenConsumer: FaktureringskomponentenConsumer,
-    private val trygdeavgiftOppsummeringService: TrygdeavgiftOppsummeringService
 ) : StegBehandler {
     override fun inngangsSteg(): ProsessSteg {
         return ProsessSteg.KANSELLER_FAKTURASERIE
@@ -25,23 +22,26 @@ class KansellerFakturaserie(
 
     override fun utfør(prosessinstans: Prosessinstans) {
         val behandling = prosessinstans.behandling
-        val behandlingsId = behandling.id
         val saksbehandlerIdent = prosessinstans.getData(ProsessDataKey.SAKSBEHANDLER)
-        val behandlingsresultat = behandlingsresultatService.hentBehandlingsresultat(behandlingsId)
+        val fagsak = behandling.fagsak
+        val sisteBehandlingMedFakturaserieReferanse =
+            fagsak.hentBehandlingerSortertSynkendePåRegistrertDato().firstOrNull { hentFakturaserieReferanse(it.id) != null }
 
-        if (harOpprinneligBehandlingMedTrygdeavgift(behandling)) {
-            log.info("Kansellerer fakturaserie for behandling: $behandlingsId med fakturaseriereferanse: ${behandlingsresultat.fakturaserieReferanse}")
+        if (sisteBehandlingMedFakturaserieReferanse != null) {
+            val behandlingID = sisteBehandlingMedFakturaserieReferanse.id
+            val behandlingsresultat = behandlingsresultatService.hentBehandlingsresultat(behandlingID)
+            log.info("Kansellerer fakturaserie for behandling: $behandlingID med fakturaseriereferanse: ${behandlingsresultat.fakturaserieReferanse}")
             val fakturaserieResponse =
                 faktureringskomponentenConsumer.kansellerFakturaserie(behandlingsresultat.fakturaserieReferanse, saksbehandlerIdent)
             behandlingsresultat.fakturaserieReferanse = fakturaserieResponse.fakturaserieReferanse
             behandlingsresultatService.lagre(behandlingsresultat)
         } else {
-            log.info { "Finner ingen fakturaseriereferanse for behandling: $behandlingsId" }
+            log.info { "Finner ingen fakturaseriereferanse for sak: ${fagsak.saksnummer}" }
         }
     }
 
-    private fun harOpprinneligBehandlingMedTrygdeavgift(behandling: Behandling): Boolean =
-        behandling.opprinneligBehandling?.let {
-            trygdeavgiftOppsummeringService.harTrygdeavgiftOgBestiltFaktura(behandlingsresultatService.hentBehandlingsresultat(it.id))
-        } ?: false
+    private fun hentFakturaserieReferanse(behandlingID: Long): String? {
+        val behandlingsresultat = behandlingsresultatService.hentBehandlingsresultat(behandlingID)
+        return behandlingsresultat.fakturaserieReferanse
+    }
 }
--- saksflyt/src/test/kotlin/no/nav/melosys/saksflyt/steg/faktureringskomponenten/KansellerFakturaserieTest.kt
+++ saksflyt/src/test/kotlin/no/nav/melosys/saksflyt/steg/faktureringskomponenten/KansellerFakturaserieTest.kt
@@ -7,6 +7,8 @@ import io.mockk.junit5.MockKExtension
 import io.mockk.verify
 import no.nav.melosys.domain.Behandling
 import no.nav.melosys.domain.Behandlingsresultat
+import no.nav.melosys.domain.Fagsak
+import no.nav.melosys.domain.kodeverk.behandlinger.Behandlingstyper
 import no.nav.melosys.integrasjon.faktureringskomponenten.FaktureringskomponentenConsumer
 import no.nav.melosys.integrasjon.faktureringskomponenten.NyFakturaserieResponseDto
 import no.nav.melosys.saksflyt.steg.fakturering.KansellerFakturaserie
@@ -16,6 +18,7 @@ import no.nav.melosys.service.behandling.BehandlingsresultatService
 import no.nav.melosys.service.sak.TrygdeavgiftOppsummeringService
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
+import java.time.Instant
 
 @ExtendWith(MockKExtension::class)
 class KansellerFakturaserieTest {
@@ -23,10 +26,13 @@ class KansellerFakturaserieTest {
 
     @RelaxedMockK
     lateinit var behandlingsresultatService: BehandlingsresultatService
+
     @RelaxedMockK
     lateinit var faktureringskomponentenConsumer: FaktureringskomponentenConsumer
+
     @RelaxedMockK
     lateinit var trygdeavgiftOppsummeringService: TrygdeavgiftOppsummeringService
+
     @InjectMockKs
     lateinit var kansellerFakturaserie: KansellerFakturaserie
 
@@ -35,34 +41,109 @@ class KansellerFakturaserieTest {
         val behandlingId = 123L
         val opprinneligBehandlingId = 456L
         val fakturaReferanse = "FADKFOGMV123"
-        val nyFakturaReferanse = "456FRKVLFVS"
         val SAKSBEHANDLER_IDENT = "S123456"
 
+        val opprinneligBehandling = Behandling().apply {
+            id = opprinneligBehandlingId
+            registrertDato = Instant.now().minusSeconds(1333337)
+        }
+        val behandling = Behandling().apply {
+            id = behandlingId
+            this.opprinneligBehandling = opprinneligBehandling
+            registrertDato = Instant.now()
+        }
+
+        val fagsak = Fagsak().apply {
+            saksnummer = "123"
+            behandlinger = mutableListOf(
+                behandling,
+                opprinneligBehandling
+            )
+        }
+        behandling.fagsak = fagsak
+
         val prosessinstans = Prosessinstans().apply {
-            this.behandling = Behandling().apply {
-                id = behandlingId
-                opprinneligBehandling = Behandling().apply {
-                    id = opprinneligBehandlingId
-                }
-            }
+            this.behandling = behandling
             setData(ProsessDataKey.SAKSBEHANDLER, SAKSBEHANDLER_IDENT)
         }
-        val behandlingsresultat = Behandlingsresultat().apply {
+        val behandlingsresultatOpprinneligBehandling = Behandlingsresultat().apply {
             id = behandlingId
             fakturaserieReferanse = fakturaReferanse
         }
 
-        val nyFakturaserieResponseDto = NyFakturaserieResponseDto(nyFakturaReferanse)
+        val nyFakturaserieResponseDto = NyFakturaserieResponseDto(fakturaReferanse)
 
-        every { behandlingsresultatService.hentBehandlingsresultat(behandlingId) } returns behandlingsresultat
-        every { behandlingsresultatService.hentBehandlingsresultat(opprinneligBehandlingId) } returns behandlingsresultat
+        every { behandlingsresultatService.hentBehandlingsresultat(behandlingId) } returns Behandlingsresultat().apply { id = behandlingId }
+        every { behandlingsresultatService.hentBehandlingsresultat(opprinneligBehandlingId) } returns behandlingsresultatOpprinneligBehandling
         every { faktureringskomponentenConsumer.kansellerFakturaserie(fakturaReferanse, SAKSBEHANDLER_IDENT) } returns nyFakturaserieResponseDto
-        every { trygdeavgiftOppsummeringService.harTrygdeavgiftOgBestiltFaktura(behandlingsresultat) } returns true
+        every { trygdeavgiftOppsummeringService.harTrygdeavgiftOgBestiltFaktura(behandlingsresultatOpprinneligBehandling) } returns true
 
 
         kansellerFakturaserie.utfør(prosessinstans)
 
-        verify { behandlingsresultatService.lagre(behandlingsresultat) }
+        verify { behandlingsresultatService.lagre(behandlingsresultatOpprinneligBehandling) }
+        verify { faktureringskomponentenConsumer.kansellerFakturaserie(fakturaReferanse, SAKSBEHANDLER_IDENT) }
+    }
+
+
+    @Test
+    fun `kanseller fakturaserie med flere behandlinger med forskjellige typer`() {
+        val opprinneligBehandlingId = 123L
+        val behandlingHenvendelseId = 333L;
+        val nyesteBehandlingId = 456L
+        val fakturaReferanse = "FADKFOGMV123"
+        val SAKSBEHANDLER_IDENT = "S123456"
+
+        val opprinneligBehandling = Behandling().apply {
+            id = opprinneligBehandlingId
+            type = Behandlingstyper.FØRSTEGANG
+            registrertDato = Instant.now().minusSeconds(1333337)
+        }
+
+        val behandlingHenvendelse = Behandling().apply {
+            id = behandlingHenvendelseId
+            type = Behandlingstyper.HENVENDELSE
+            registrertDato = Instant.now().minusSeconds(133337)
+        }
+
+        val nyesteBehandlingUtenFakturaserieReferanse = Behandling().apply {
+            id = nyesteBehandlingId
+            registrertDato = Instant.now()
+            type = Behandlingstyper.NY_VURDERING
+        }
+
+        val fagsak = Fagsak().apply {
+            saksnummer = "123"
+            behandlinger = mutableListOf(
+                nyesteBehandlingUtenFakturaserieReferanse,
+                opprinneligBehandling,
+                behandlingHenvendelse
+            )
+        }
+        nyesteBehandlingUtenFakturaserieReferanse.fagsak = fagsak
+
+        val prosessinstans = Prosessinstans().apply {
+            this.behandling = nyesteBehandlingUtenFakturaserieReferanse
+            setData(ProsessDataKey.SAKSBEHANDLER, SAKSBEHANDLER_IDENT)
+        }
+        val behandlingsresultatOpprinneligBehandling = Behandlingsresultat().apply {
+            id = opprinneligBehandlingId
+            fakturaserieReferanse = fakturaReferanse
+        }
+
+        val nyFakturaserieResponseDto = NyFakturaserieResponseDto(fakturaReferanse)
+
+        every { behandlingsresultatService.hentBehandlingsresultat(nyesteBehandlingId) } returns Behandlingsresultat().apply { id = nyesteBehandlingId }
+        every { behandlingsresultatService.hentBehandlingsresultat(behandlingHenvendelseId) } returns Behandlingsresultat().apply { id = behandlingHenvendelseId }
+        every { behandlingsresultatService.hentBehandlingsresultat(opprinneligBehandlingId) } returns behandlingsresultatOpprinneligBehandling
+        every { faktureringskomponentenConsumer.kansellerFakturaserie(fakturaReferanse, SAKSBEHANDLER_IDENT) } returns nyFakturaserieResponseDto
+        every { trygdeavgiftOppsummeringService.harTrygdeavgiftOgBestiltFaktura(behandlingsresultatOpprinneligBehandling) } returns true
+
+
+        kansellerFakturaserie.utfør(prosessinstans)
+
+        verify { behandlingsresultatService.lagre(behandlingsresultatOpprinneligBehandling) }
+        verify { faktureringskomponentenConsumer.kansellerFakturaserie(fakturaReferanse, SAKSBEHANDLER_IDENT) }
     }
 
 }
