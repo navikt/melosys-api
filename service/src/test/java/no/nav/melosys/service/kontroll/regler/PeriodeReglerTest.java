6f0b11f89ec8d4f086cf34bc27faf52a1f613de5
Janita Johansen <janita.marielle.johansen@nav.no>
2024-02-29 09:07:00 +0100

    Setter fast dato der man sjekker periode. Der man sjekker relativ dato til nå, beholdes LocalDate.now().

--- service/src/test/java/no/nav/melosys/service/kontroll/regler/PeriodeReglerTest.java
+++ service/src/test/java/no/nav/melosys/service/kontroll/regler/PeriodeReglerTest.java
@@ -9,69 +9,71 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class PeriodeReglerTest {
 
+    private final static LocalDate DATO = LocalDate.parse("2024-01-01");
+
     @Test
     void feilIPeriode_erGyldigPeriode_false() {
-        assertThat(PeriodeRegler.feilIPeriode(LocalDate.now().plusMonths(6), LocalDate.now().plusYears(2))).isFalse();
+        assertThat(PeriodeRegler.feilIPeriode(DATO.plusMonths(6), DATO.plusYears(2))).isFalse();
     }
 
     @Test
     void feilIPeriode_tomFørFom_true() {
-        assertThat(PeriodeRegler.feilIPeriode(LocalDate.now().plusYears(1), LocalDate.now())).isTrue();
+        assertThat(PeriodeRegler.feilIPeriode(DATO.plusYears(1), DATO)).isTrue();
     }
 
     @Test
     void periodeErÅpen_ingenTilDato_true() {
-        assertThat(PeriodeRegler.periodeErÅpen(LocalDate.now().plusMonths(6), null)).isTrue();
+        assertThat(PeriodeRegler.periodeErÅpen(DATO.plusMonths(6), null)).isTrue();
     }
 
     @Test
     void periodeErÅpen_ikkeÅpen_false() {
-        assertThat(PeriodeRegler.periodeErÅpen(LocalDate.now().plusMonths(6), LocalDate.now().plusYears(1))).isFalse();
+        assertThat(PeriodeRegler.periodeErÅpen(DATO.plusMonths(6), DATO.plusYears(1))).isFalse();
     }
 
     @Test
     void periodeOver24Mnd_periodeOver24Mnd_true() {
-        assertThat(PeriodeRegler.periodeOver24Måneder(LocalDate.now(), LocalDate.now().plusYears(3))).isTrue();
+        assertThat(PeriodeRegler.periodeOver24Måneder(DATO, DATO.plusYears(3))).isTrue();
     }
 
     @Test
-    void periodeoOver24Mnd_periode24Mnd_true() {
-        assertThat(PeriodeRegler.periodeOver24Måneder(LocalDate.now(), LocalDate.now().plusMonths(24))).isTrue();
+    void periodeOver24Mnd_periode24Mnd_true() {
+        assertThat(PeriodeRegler.periodeOver24Måneder(DATO, DATO.plusMonths(24))).isTrue();
     }
 
     @Test
-    void periodeoOver24Mnd_periode23Mnd_false() {
-        assertThat(PeriodeRegler.periodeOver24Måneder(LocalDate.now(), LocalDate.now().plusMonths(23))).isFalse();
+    void periodeOver24Mnd_periode23Mnd_false() {
+        assertThat(PeriodeRegler.periodeOver24Måneder(DATO, DATO.plusMonths(23))).isFalse();
     }
 
     @Test
     void periodeOver24Mnd_periode14Mnd_false() {
-        assertThat(PeriodeRegler.periodeOver24Måneder(LocalDate.now(), LocalDate.now().plusMonths(14))).isFalse();
+        assertThat(PeriodeRegler.periodeOver24Måneder(DATO, DATO.plusMonths(14))).isFalse();
     }
 
     @Test
     void periodeOver3År_periode2År_false() {
-        assertThat(PeriodeRegler.periodeOver3År(LocalDate.now(), LocalDate.now().plusYears(2).plusMonths(11))).isFalse();
+        assertThat(PeriodeRegler.periodeOver3År(DATO, DATO.plusYears(2).plusMonths(11))).isFalse();
     }
 
     @Test
     void periodeOver3År_periode3År_true() {
-        assertThat(PeriodeRegler.periodeOver3År(LocalDate.now(), LocalDate.now().plusYears(3))).isTrue();
+        assertThat(PeriodeRegler.periodeOver3År(DATO, DATO.plusYears(3))).isTrue();
     }
 
     @Test
     void periodeOver3År_periode4År_true() {
-        assertThat(PeriodeRegler.periodeOver3År(LocalDate.now(), LocalDate.now().plusYears(4))).isTrue();
+        assertThat(PeriodeRegler.periodeOver3År(DATO, DATO.plusYears(4))).isTrue();
     }
 
     @Test
     void periodeOver5År_periode5År_true() {
-        assertThat(PeriodeRegler.periodeOver5År(LocalDate.now(), LocalDate.now().plusYears(5))).isTrue();
+        assertThat(PeriodeRegler.periodeOver5År(DATO, DATO.plusYears(5))).isTrue();
     }
 
     @Test
     void periodeOver5År_periode4År_false() {
-        assertThat(PeriodeRegler.periodeOver5År(LocalDate.now(), LocalDate.now().plusYears(4).plusMonths(11))).isFalse();
+        assertThat(PeriodeRegler.periodeOver5År(DATO, DATO.plusYears(4).plusMonths(11))).isFalse();
     }
 
     @Test
@@ -107,39 +109,39 @@ class PeriodeReglerTest {
 
     @Test
     void periodeOver1ÅrFremITid_periodeOm2År_true() {
-        assertThat(PeriodeRegler.datoOver1ÅrFremITid(LocalDate.now().plusYears(2))).isTrue();
+        assertThat(PeriodeRegler.datoOver1ÅrFremITid(DATO.plusYears(2))).isTrue();
     }
 
     @Test
     void periodeOver1ÅrFremITid_periodeNå_false() {
-        assertThat(PeriodeRegler.datoOver1ÅrFremITid(LocalDate.now())).isFalse();
+        assertThat(PeriodeRegler.datoOver1ÅrFremITid(DATO)).isFalse();
     }
 
     @Test
     void periodeErLik_periodeLik_true() {
         assertThat(
-            PeriodeRegler.periodeErLik(LocalDate.now(), LocalDate.now(), LocalDate.now(), LocalDate.now())
+            PeriodeRegler.periodeErLik(DATO, DATO, DATO, DATO)
         ).isTrue();
     }
 
     @Test
     void periodeErLik_periodeIkkeLik_false() {
         assertThat(
-            PeriodeRegler.periodeErLik(LocalDate.now(), LocalDate.now().plusYears(1), LocalDate.now(), LocalDate.now())
+            PeriodeRegler.periodeErLik(DATO, DATO.plusYears(1), DATO, DATO)
         ).isFalse();
     }
 
     @Test
     void periodeErLik_tomErNullPeriodeLik_true() {
         assertThat(
-            PeriodeRegler.periodeErLik(LocalDate.now(), null, LocalDate.now(), null)
+            PeriodeRegler.periodeErLik(DATO, null, DATO, null)
         ).isTrue();
     }
 
     @Test
     void periodeErLik_tom2ErNullPeriodeIkkeLik_false() {
         assertThat(
-            PeriodeRegler.periodeErLik(LocalDate.now(), LocalDate.now(), LocalDate.now(), null)
+            PeriodeRegler.periodeErLik(DATO, DATO, DATO, null)
         ).isFalse();
     }
 
@@ -155,6 +157,6 @@ class PeriodeReglerTest {
 
     @Test
     void periodeOver12Måneder_periode12Måneder_true() {
-        assertThat(PeriodeRegler.periodeOver12Måneder(LocalDate.now(), LocalDate.now().plusMonths(12))).isTrue();
+        assertThat(PeriodeRegler.periodeOver12Måneder(DATO, DATO.plusMonths(12))).isTrue();
     }
 }
