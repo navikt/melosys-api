e244d3a8cfcfefe79c93c3d4f3ab3ce08927e262
RuneLind <rulind@gmail.com>
2024-03-07 13:04:54 +0100

    Fjern eksperimentering i AwaitUtil

--- integrasjonstest/src/test/kotlin/no/nav/melosys/AwaitUtil.kt
+++ integrasjonstest/src/test/kotlin/no/nav/melosys/AwaitUtil.kt
@@ -2,8 +2,6 @@ package no.nav.melosys
 
 import ch.qos.logback.classic.Level
 import ch.qos.logback.classic.spi.ILoggingEvent
-import io.kotest.assertions.withClue
-import io.kotest.matchers.shouldBe
 import no.nav.melosys.exception.TekniskException
 import org.awaitility.core.ConditionFactory
 import org.awaitility.core.ConditionTimeoutException
@@ -17,20 +15,6 @@ object AwaitUtil {
         LoggingTestUtils.withLogCapture { logEvents ->
             await.throwOnLogError(logEvents).block(logEvents)
         }
-
-    internal fun <T> ConditionFactory.untilMatching(
-        waitFor: T,
-        clueMessages: (e: Exception) -> String? = { e -> e.message },
-        getCurrent: () -> T?
-    ) {
-        try {
-            until { getCurrent() == waitFor }
-        } catch (e: ConditionTimeoutException) {
-            withClue(clueMessages(e)) {
-                getCurrent() shouldBe waitFor
-            }
-        }
-    }
 
     fun ConditionFactory.onTimeout(onTimeout: (e: ConditionTimeoutException) -> Unit) = apply {
         threadLocalOnTimeoutLambda.set(onTimeout)
@@ -50,38 +34,6 @@ object AwaitUtil {
     fun ConditionFactory.throwOnLogError(logEvents: List<ILoggingEvent>): ConditionFactory = this.conditionEvaluationListener {
         logEvents.firstOrNull { it.level == Level.ERROR }?.let {
             throw TekniskException("Fant log entry med level error: ${it.formattedMessage}")
-        }
-    }
-
-    fun ConditionFactory.untilBuilder() = AwaitUntilBuilder(this)
-
-    class AwaitUntilBuilder(private val conditionFactory: ConditionFactory) {
-        private var waitFor: () -> Boolean = { throw IllegalStateException("waitFor not set") }
-        private var assertIfTimeout: (e: ConditionTimeoutException) -> Unit = { e -> throw e }
-        fun until(block: () -> Boolean) = apply {
-            waitFor = block
-        }
-
-        fun onTimeout(block: (e: ConditionTimeoutException) -> Unit) = apply {
-            assertIfTimeout = block
-        }
-
-        fun onTimeoutWithExceptionClue(block: () -> Unit) = apply {
-            assertIfTimeout = { e ->
-                withClue(e.message) {
-                    block()
-                }
-            }
-        }
-
-        fun execute() {
-            try {
-                conditionFactory.until {
-                    waitFor()
-                }
-            } catch (e: ConditionTimeoutException) {
-                assertIfTimeout(e)
-            }
         }
     }
 }
--- integrasjonstest/src/test/kotlin/no/nav/melosys/AwaitUtilTest.kt
+++ integrasjonstest/src/test/kotlin/no/nav/melosys/AwaitUtilTest.kt
@@ -7,9 +7,7 @@ import io.kotest.matchers.shouldBe
 import mu.KotlinLogging
 import no.nav.melosys.AwaitUtil.awaitWithFailOnLogErrors
 import no.nav.melosys.AwaitUtil.onTimeout
-import no.nav.melosys.AwaitUtil.untilBuilder
 import no.nav.melosys.AwaitUtil.throwOnLogError
-import no.nav.melosys.AwaitUtil.untilMatching
 import no.nav.melosys.AwaitUtil.waitFor
 import no.nav.melosys.LoggingTestUtils.withLogCapture
 import no.nav.melosys.exception.TekniskException
@@ -58,51 +56,6 @@ class AwaitUtilTest {
     }
 
     @Test
-    fun `untilMatching skal sammenlikne waitFor med string`() {
-        await.untilMatching(
-            waitFor = "last log messge"
-        ) {
-            "last log messge"
-        }
-    }
-
-
-    @Test
-    fun `await builder skal returnere verdi`() {
-        var i = 0
-            await.atMost(Duration.ofMillis(100))
-                .pollInterval(Duration.ofMillis(1))
-                .pollDelay(Duration.ofMillis(1))
-                .untilBuilder()
-                .until { i++ >= 10 }
-                .onTimeout {
-                    withClue("i > 10") {
-                        i shouldBe 10
-                    }
-                }.execute()
-    }
-
-    @Test
-    fun `await builder skal kjøre assert ved timeout`() {
-        shouldThrow<AssertionFailedError> {
-            var i = 0
-            await.atMost(Duration.ofMillis(2))
-                .pollInterval(Duration.ofMillis(1))
-                .pollDelay(Duration.ofMillis(1))
-                .untilBuilder()
-                .until { i++.toString() == "hei" }
-                .onTimeout {
-                    withClue("$i.toString() == \"hei\"") {
-                        i.toString() shouldBe "hei"
-                    }
-                }.execute()
-        }.message.shouldBe(
-            "1.toString() == \"hei\"\n" +
-                "expected:<\"hei\"> but was:<\"1\">"
-        )
-    }
-
-    @Test
     fun `await builder skal kjøre assert ved timeout - uten builder`() {
         shouldThrow<AssertionFailedError> {
             var i = 0
@@ -118,33 +71,6 @@ class AwaitUtilTest {
         }.message.shouldBe(
             "tall kan ikke være 1 - Condition with no.nav.melosys.AwaitUtil was not fulfilled within 2 milliseconds.\n" +
                 "expected:<\"hei\"> but was:<\"1\">"
-        )
-    }
-
-    @Test
-    fun `untilMatching skal gi assert på forskjellig resultat ved timeout`() {
-        shouldThrow<AssertionFailedError> {
-            await.atMost(Duration.ofMillis(101)).untilMatching("waiting for message") {
-                "last log messge"
-            }
-        }.message.shouldBe(
-            "Condition with no.nav.melosys.AwaitUtil was not fulfilled within 101 milliseconds.\n" +
-                "expected:<\"waiting for message\"> but was:<\"last log messge\">"
-        )
-    }
-
-    @Test
-    fun `untilMatching skal gi assert på forskjellig resultat ved timeout bruk withClue`() {
-        shouldThrow<AssertionFailedError> {
-            await.atMost(Duration.ofMillis(101)).untilMatching(
-                waitFor = "waiting for message",
-                clueMessages = { e -> "Did not match last log message: ${e.message}" }
-            ) {
-                "last log message"
-            }
-        }.message.shouldBe(
-            "Did not match last log message: Condition with no.nav.melosys.AwaitUtil was not fulfilled within 101 milliseconds.\n" +
-                "expected:<\"waiting for message\"> but was:<\"last log message\">"
         )
     }
 }
