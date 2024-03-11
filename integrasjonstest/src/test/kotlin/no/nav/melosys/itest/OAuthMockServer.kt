2013022f91c25dc0fc4172b7650cc9d880c624cd
Eirik Bergande <eirik.bergande@nav.no>
2024-03-11 13:52:22 +0100

    fjerner unødvendig mock

--- integrasjonstest/src/test/kotlin/no/nav/melosys/itest/OAuthMockServer.kt
+++ integrasjonstest/src/test/kotlin/no/nav/melosys/itest/OAuthMockServer.kt
@@ -3,20 +3,8 @@ package no.nav.melosys.itest
 import com.github.tomakehurst.wiremock.WireMockServer
 import com.github.tomakehurst.wiremock.client.WireMock
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration
-import com.nimbusds.jwt.JWT
-import com.nimbusds.jwt.JWTClaimsSet
-import com.nimbusds.jwt.PlainJWT
-import com.ninjasquad.springmockk.MockkBean
-import io.mockk.every
-import io.mockk.justRun
 import no.nav.security.token.support.client.spring.oauth2.EnableOAuth2Client
-import no.nav.security.token.support.core.context.TokenValidationContext
-import no.nav.security.token.support.core.context.TokenValidationContextHolder
-import no.nav.security.token.support.core.jwt.JwtToken
 import org.springframework.beans.factory.annotation.Value
-import java.time.LocalDateTime
-import java.time.ZoneId
-import java.util.*
 
 @EnableOAuth2Client
 class OAuthMockServer(
@@ -25,12 +13,8 @@ class OAuthMockServer(
     private val azureMockServer: WireMockServer =
         WireMockServer(WireMockConfiguration.wireMockConfig().port(mockSecurityPort))
 
-    @MockkBean()
-    private lateinit var tokenValidationContextHolder: TokenValidationContextHolder
 
     fun start() {
-        every { tokenValidationContextHolder.tokenValidationContext } returns tokenValidationContext("sub1")
-        justRun { tokenValidationContextHolder.setTokenValidationContext(any())}
         azureMockServer.start()
 
         azureMockServer.stubFor(
@@ -50,22 +34,6 @@ class OAuthMockServer(
                     )
             )
         )
-    }
-
-    fun tokenValidationContext(sub: String): TokenValidationContext {
-        val expiry = LocalDateTime.now().atZone(ZoneId.systemDefault()).plusSeconds(60).toInstant()
-        val jwt: JWT = PlainJWT(
-            JWTClaimsSet.Builder()
-                .subject(sub)
-                .audience("thisapi")
-                .issuer("someIssuer")
-                .expirationTime(Date.from(expiry))
-                .claim("jti", UUID.randomUUID().toString())
-                .build()
-        )
-        val map: MutableMap<String, JwtToken> = HashMap()
-        map["issuer1"] = JwtToken(jwt.serialize())
-        return TokenValidationContext(map)
     }
 
     fun stop() {
