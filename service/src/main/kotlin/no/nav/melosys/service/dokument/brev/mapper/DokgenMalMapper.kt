7a4f7555739ff4e3648afb3aa6f0ad9417a28929
Janita Johansen <janita.marielle.johansen@nav.no>
2024-03-20 13:15:50 +0100

    Legger til medlemskapstype mot dokgen

--- service/src/main/kotlin/no/nav/melosys/service/dokument/brev/mapper/DokgenMalMapper.kt
+++ service/src/main/kotlin/no/nav/melosys/service/dokument/brev/mapper/DokgenMalMapper.kt
@@ -198,7 +198,9 @@ class DokgenMalMapper(
             Produserbaredokumenter.IKKE_YRKESAKTIV_FRIVILLIG_FTRL -> innvilgelseFtrlMapper.mapIkkeYrkesaktivFrivillig(brevbestilling as InnvilgelseFtrlIkkeYrkesaktivFrivilligBrevbestilling)
 
             Produserbaredokumenter.VARSELBREV_MANGLENDE_INNBETALING -> VarselbrevManglendeInnbetaling(
-                brevbestilling as VarselbrevManglendeInnbetalingBrevbestilling
+                brevbestilling as VarselbrevManglendeInnbetalingBrevbestilling,
+                dokgenMapperDatahenter.hentBehandlingsresultat(brevbestilling.behandling.opprinneligBehandling.id)?.medlemAvFolketrygden?.medlemskapsperioder?.firstOrNull()?.medlemskapstype
+                    ?: throw FunksjonellException("Forventer at behandling som tilhører varselbrevet har en opprinnelig behandling med medlemskapsperioder")
             )
 
             Produserbaredokumenter.VEDTAK_OPPHOERT_MEDLEMSKAP -> VedtakOpphoertMedlemskap(brevbestilling as VedtakOpphoertMedlemskapBrevbestilling)
