88403a339b39af4ec2ea051ec72a24bb1d90faf8
RuneLind <rulind@gmail.com>
2024-04-23 19:23:05 +0200

    Konverter KafkaConsumerConfig til kotlin og rename til KafkaConfig

--- service/src/test/java/no/nav/melosys/service/kafka/KafkaConsumerConfigTest.java
+++ service/src/test/java/no/nav/melosys/service/kafka/KafkaConsumerConfigTest.java
@@ -20,7 +20,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @JsonTest
 @ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = {KafkaConsumerConfig.class, KafkaProperties.class})
+@ContextConfiguration(classes = {KafkaConfig.class, KafkaProperties.class})
 class KafkaConsumerConfigTest {
 
     @Autowired
