3057541c9775f41de5879d2b915d56914fe6ad5d
RuneLind <rulind@gmail.com>
2024-04-23 14:33:04 +0200

    Legg til KafkaFakturaBestiltProducer

---
+++ saksflyt/src/main/kotlin/no/nav/melosys/saksflyt/steg/melding/KafkaMelosysHendelseProducer.kt
@@ -0,0 +1,43 @@
+package no.nav.melosys.saksflyt.steg.melding
+
+import mu.KotlinLogging
+import no.nav.melosys.config.MDCOperations
+import no.nav.melosys.exception.TekniskException
+import no.nav.melosys.service.hendelser.MelosysHendelse
+import org.apache.kafka.clients.producer.ProducerRecord
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.stereotype.Component
+import java.util.concurrent.TimeUnit
+
+private val log = KotlinLogging.logger { }
+
+@Component
+class KafkaFakturaBestiltProducer(
+    @Value("\$kafka.aiven.melosys-hendelse.topic") private val topicName: String,
+    @Qualifier("melosysHendelse") @Autowired private val kafkaTemplate: KafkaTemplate<String, MelosysHendelse>
+) {
+    fun produserBestillingsmelding(melosysHendelse: MelosysHendelse) {
+        val hendelseRecord = ProducerRecord<String, MelosysHendelse>(topicName, melosysHendelse)
+        hendelseRecord.headers().add(MDCOperations.CORRELATION_ID, MDCOperations.getCorrelationId().encodeToByteArray())
+        val future = kafkaTemplate.send(hendelseRecord)
+
+        try {
+            val sendeResultat = future[15L, TimeUnit.SECONDS]
+            log.info(
+                "Melding sendt på topic $topicName " +
+                    "for melosysHendelse ${melosysHendelse.melding}\n" +
+                    "Offset: ${sendeResultat.recordMetadata.offset()} "
+            )
+        } catch (e: InterruptedException) {
+            Thread.currentThread().interrupt()
+            throw TekniskException("Avbrutt ved sending av melding om faktura bestilt for hendelse ${melosysHendelse.melding}")
+        } catch (e: Exception) {
+            throw TekniskException(
+                "Kunne ikke sende melding om faktura bestilt for hendelse ${melosysHendelse.melding}", e
+            )
+        }
+    }
+}
