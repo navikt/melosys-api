8c4c969914f8d20218373cb00588f0fc945aa912
Francois Allix <32903762+frans-a@users.noreply.github.com>
2024-05-22 13:26:18 +0200

    GET response ligner det som skal vises

--- frontend-api/src/main/kotlin/no/nav/melosys/tjenester/gui/aarsavregning/ÅrsavregningTjeneste.kt
+++ frontend-api/src/main/kotlin/no/nav/melosys/tjenester/gui/aarsavregning/ÅrsavregningTjeneste.kt
@@ -1,6 +1,9 @@
 package no.nav.melosys.tjenester.gui.aarsavregning
 
 import io.swagger.annotations.Api
+import no.nav.melosys.domain.kodeverk.Inntektskildetype
+import no.nav.melosys.domain.kodeverk.Skatteplikttype
+import no.nav.melosys.domain.kodeverk.Trygdedekninger
 import no.nav.melosys.service.sak.ÅrsavregningService
 import no.nav.security.token.support.core.api.Protected
 import org.springframework.http.ResponseEntity
@@ -8,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.time.LocalDate
 
 @Protected
 @RestController
@@ -17,15 +21,116 @@ class ÅrsavregningTjeneste(
     private val årsavregningService: ÅrsavregningService,
 ) {
     @GetMapping("/{avregningID}")
-    fun hentAvregning(@PathVariable("avregningID") behandlingID: Long): ResponseEntity<ÅrsavregningDto> {
+    fun hentAvregning(@PathVariable("avregningID") avregningID: Long): ResponseEntity<ÅrsavregningResponse> {
+        // TODO val årsavregnig = årsavregningService.hentÅrsavregnig(avregningID)
         return ResponseEntity.ok(
-            ÅrsavregningDto(
-                aar = 2023
+            ÅrsavregningResponse(
+                aar = 2023,
+                forskuddsvisFakturertAvgift = FakturertAvgift(
+                    skatteforholdsperioder = listOf(
+                        Skatteforholdsperiode(LocalDate.of(2023, 1, 1), LocalDate.of(2023, 7, 31), Skatteplikttype.SKATTEPLIKTIG),
+                        Skatteforholdsperiode(LocalDate.of(2023, 8, 1), LocalDate.of(2023, 12, 31), Skatteplikttype.IKKE_SKATTEPLIKTIG)
+                    ),
+                    medlemskapsperioder = listOf(
+                        Medlemskapsperiode(LocalDate.of(2023, 1, 1), LocalDate.of(2023, 7, 31), Trygdedekninger.FTRL_2_9_FØRSTE_LEDD_B_PENSJON),
+                        Medlemskapsperiode(
+                            LocalDate.of(2023, 8, 1),
+                            LocalDate.of(2023, 12, 31),
+                            Trygdedekninger.FTRL_2_9_FØRSTE_LEDD_C_ANDRE_LEDD_HELSE_PENSJON_SYKE_FORELDREPENGER
+                        )
+                    ),
+                    trygdeavgiftsperioder = listOf(
+                        Trygdeavgiftsperiode(
+                            LocalDate.of(2023, 1, 1), LocalDate.of(2023, 7, 31), Inntektskildetype.ARBEIDSINNTEKT_FRA_NORGE, true, 40000, 0.0, 0
+                        ), Trygdeavgiftsperiode(
+                            LocalDate.of(2023, 8, 1), LocalDate.of(2023, 12, 31), Inntektskildetype.INNTEKT_FRA_UTLANDET, false, 15000, 42.2, 6330
+                        )
+                    ),
+                    totalInntektPerMd = 690000,
+                    totalAvgiftPerMd = 127020
+                ),
+                avvikFunnet = false,
+                endeligAvgift = EndeligAvgift(
+                    skatteforholdsperioder = listOf(
+                        Skatteforholdsperiode(LocalDate.of(2023, 1, 1), LocalDate.of(2023, 7, 31), Skatteplikttype.SKATTEPLIKTIG),
+                        Skatteforholdsperiode(LocalDate.of(2023, 8, 1), LocalDate.of(2023, 12, 31), Skatteplikttype.IKKE_SKATTEPLIKTIG)
+                    ),
+                    inntektskperioder = listOf(
+                        Inntektsperiode(LocalDate.of(2023, 1, 1), LocalDate.of(2023, 7, 31), Inntektskildetype.ARBEIDSINNTEKT_FRA_NORGE, true, 95000)
+                    ),
+                    trygdeavgiftsperioder = listOf(
+                        Trygdeavgiftsperiode(
+                            LocalDate.of(2023, 1, 1), LocalDate.of(2023, 7, 31), Inntektskildetype.ARBEIDSINNTEKT_FRA_NORGE, true, 40000, 0.0, 0
+                        ), Trygdeavgiftsperiode(
+                            LocalDate.of(2023, 8, 1), LocalDate.of(2023, 12, 31), Inntektskildetype.INNTEKT_FRA_UTLANDET, false, 15000, 42.2, 6330
+                        )
+                    ),
+                    totalInntektPerMd = 690000,
+                    totalAvgiftPerMd = 127020
+
+                ),
+                avregning = Avregning(
+                    nyttTotalbeloep = 24280,
+                    tidligereFakturertBeloep = 21170,
+                    tilFaktureringBeloep = 3110
+                )
             )
         )
     }
 }
 
-data class ÅrsavregningDto(
+data class ÅrsavregningResponse(
     val aar: Int,
+    val forskuddsvisFakturertAvgift: FakturertAvgift,
+    val avvikFunnet: Boolean?,
+    val endeligAvgift: EndeligAvgift?,
+    val avregning: Avregning?
+)
+
+data class FakturertAvgift(
+    val skatteforholdsperioder: List<Skatteforholdsperiode>,
+    val medlemskapsperioder: List<Medlemskapsperiode>,
+    val trygdeavgiftsperioder: List<Trygdeavgiftsperiode>,
+    val totalInntektPerMd: Int,
+    val totalAvgiftPerMd: Int
+)
+
+data class Skatteforholdsperiode(
+    val fom: LocalDate, val tom: LocalDate, val skatteplikttype: Skatteplikttype
+)
+
+data class Medlemskapsperiode(
+    val fom: LocalDate, val tom: LocalDate, val trygdedekning: Trygdedekninger
+)
+
+data class Trygdeavgiftsperiode(
+    val fom: LocalDate,
+    val tom: LocalDate,
+    val inntektskildetype: Inntektskildetype,
+    val arbeidsgiversavgiftBetales: Boolean,
+    val inntektPerMd: Int,
+    val avgiftssats: Double,
+    val avgiftPerMd: Int
+)
+
+data class EndeligAvgift(
+    val skatteforholdsperioder: List<Skatteforholdsperiode>,
+    val inntektskperioder: List<Inntektsperiode>,
+    val trygdeavgiftsperioder: List<Trygdeavgiftsperiode>,
+    val totalInntektPerMd: Int,
+    val totalAvgiftPerMd: Int
+)
+
+data class Inntektsperiode(
+    val fom: LocalDate,
+    val tom: LocalDate,
+    val type: Inntektskildetype,
+    val arbeidsgiversavgiftBetales: Boolean,
+    val inntektPerMd: Int
+)
+
+data class Avregning(
+    val nyttTotalbeloep: Int,
+    val tidligereFakturertBeloep: Int,
+    val tilFaktureringBeloep: Int,
 )
--- frontend-api/src/test/kotlin/no/nav/melosys/tjenester/gui/aarsavregning/ÅrsavregningTjenesteTest.kt
+++ frontend-api/src/test/kotlin/no/nav/melosys/tjenester/gui/aarsavregning/ÅrsavregningTjenesteTest.kt
@@ -10,6 +10,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 @WebMvcTest(controllers = [ÅrsavregningTjeneste::class])
@@ -24,9 +25,113 @@ internal class ÅrsavregningTjenesteTest {
     fun `hent avregning basert på ID`() {
         every { årsavregningService.hentÅrsavregnig(any()) } returns Aarsavregning()
 
+
+        val expectedJson = """{
+  "aar": 2023,
+  "forskuddsvisFakturertAvgift": {
+    "skatteforholdsperioder": [
+      {
+        "fom": "2023-01-01",
+        "tom": "2023-07-31",
+        "skatteplikttype": "SKATTEPLIKTIG"
+      },
+      {
+        "fom": "2023-08-01",
+        "tom": "2023-12-31",
+        "skatteplikttype": "IKKE_SKATTEPLIKTIG"
+      }
+    ],
+    "medlemskapsperioder": [
+      {
+        "fom": "2023-01-01",
+        "tom": "2023-07-31",
+        "trygdedekning": "FTRL_2_9_FØRSTE_LEDD_B_PENSJON"
+      },
+      {
+        "fom": "2023-08-01",
+        "tom": "2023-12-31",
+        "trygdedekning": "FTRL_2_9_FØRSTE_LEDD_C_ANDRE_LEDD_HELSE_PENSJON_SYKE_FORELDREPENGER"
+      }
+    ],
+    "trygdeavgiftsperioder": [
+      {
+        "fom": "2023-01-01",
+        "tom": "2023-07-31",
+        "inntektskildetype": "ARBEIDSINNTEKT_FRA_NORGE",
+        "arbeidsgiversavgiftBetales": true,
+        "inntektPerMd": 40000,
+        "avgiftssats": 0.0,
+        "avgiftPerMd": 0
+      },
+      {
+        "fom": "2023-08-01",
+        "tom": "2023-12-31",
+        "inntektskildetype": "INNTEKT_FRA_UTLANDET",
+        "arbeidsgiversavgiftBetales": false,
+        "inntektPerMd": 15000,
+        "avgiftssats": 42.2,
+        "avgiftPerMd": 6330
+      }
+    ],
+    "totalInntektPerMd": 690000,
+    "totalAvgiftPerMd": 127020
+  },
+  "avvikFunnet": false,
+  "endeligAvgift": {
+    "skatteforholdsperioder": [
+      {
+        "fom": "2023-01-01",
+        "tom": "2023-07-31",
+        "skatteplikttype": "SKATTEPLIKTIG"
+      },
+      {
+        "fom": "2023-08-01",
+        "tom": "2023-12-31",
+        "skatteplikttype": "IKKE_SKATTEPLIKTIG"
+      }
+    ],
+    "inntektskperioder": [
+      {
+        "fom": "2023-01-01",
+        "tom": "2023-07-31",
+        "type": "ARBEIDSINNTEKT_FRA_NORGE",
+        "arbeidsgiversavgiftBetales": true,
+        "inntektPerMd": 95000
+      }
+    ],
+    "trygdeavgiftsperioder": [
+      {
+        "fom": "2023-01-01",
+        "tom": "2023-07-31",
+        "inntektskildetype": "ARBEIDSINNTEKT_FRA_NORGE",
+        "arbeidsgiversavgiftBetales": true,
+        "inntektPerMd": 40000,
+        "avgiftssats": 0.0,
+        "avgiftPerMd": 0
+      },
+      {
+        "fom": "2023-08-01",
+        "tom": "2023-12-31",
+        "inntektskildetype": "INNTEKT_FRA_UTLANDET",
+        "arbeidsgiversavgiftBetales": false,
+        "inntektPerMd": 15000,
+        "avgiftssats": 42.2,
+        "avgiftPerMd": 6330
+      }
+    ],
+    "totalInntektPerMd": 690000,
+    "totalAvgiftPerMd": 127020
+  },
+  "avregning": {
+    "nyttTotalbeloep": 24280,
+    "tidligereFakturertBeloep": 21170,
+    "tilFaktureringBeloep": 3110
+  }
+}"""
+
         mockMvc.perform(
             MockMvcRequestBuilders.get("$BASE_URL/{avregningID}", 1).contentType(MediaType.APPLICATION_JSON)
-        ).andExpect(status().isOk())
+        ).andExpect(status().isOk()).andExpect(content().json(expectedJson, true))
     }
 
     companion object {
