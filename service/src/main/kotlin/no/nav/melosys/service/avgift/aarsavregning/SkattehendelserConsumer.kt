ee5e4bfeda4b71d6717ac74f7b5c7f7b5e93a3c9
Eirik Bergande <eirik.bergande@nav.no>
2024-05-27 15:16:41 +0200

    toggle på konsumer

--- service/src/main/kotlin/no/nav/melosys/service/avgift/aarsavregning/SkattehendelserConsumer.kt
+++ service/src/main/kotlin/no/nav/melosys/service/avgift/aarsavregning/SkattehendelserConsumer.kt
@@ -1,18 +1,23 @@
 package no.nav.melosys.service.avgift.aarsavregning
 
+import io.getunleash.Unleash
 import mu.KotlinLogging
 import no.nav.melosys.domain.avgift.aarsavregning.Skattehendelse
+import no.nav.melosys.featuretoggle.ToggleName
 import no.nav.melosys.saksflytapi.ProsessinstansService
 import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Profile
 import org.springframework.kafka.annotation.KafkaListener
+import org.springframework.stereotype.Service
 
 private val log = KotlinLogging.logger { }
 
 @Profile("!local-q1 & !local-q2")
+@Service
 class SkattehendelserConsumer(
-    @Autowired private val prosessinstansService: ProsessinstansService
+    @Autowired private val prosessinstansService: ProsessinstansService,
+    @Autowired private val unleash: Unleash
 ) {
 
     @KafkaListener(
@@ -21,8 +26,11 @@ class SkattehendelserConsumer(
         containerFactory = "aivenSkattehendelserListenerContainerFactory"
     )
     fun lesSkattehendelser(consumerRecord: ConsumerRecord<String, Skattehendelse>) {
-        val skattehendelseMelding = consumerRecord.value()
-        prosessinstansService.opprettArsavregningsBehandlingProsessflyt(skattehendelseMelding)
+        if (unleash.isEnabled(ToggleName.MELOSYS_SKATTEHENDELSE_CONSUMER)) {
+            prosessinstansService.opprettArsavregningsBehandlingProsessflyt(consumerRecord.value())
+        } else {
+            log.info { "Skattehendelsemelding med key: ${consumerRecord.key()}" }
+        }
     }
 
 }
