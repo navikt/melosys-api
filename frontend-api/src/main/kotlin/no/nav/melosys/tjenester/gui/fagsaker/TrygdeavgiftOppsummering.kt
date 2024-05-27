9739c66104002b35aecc68ba499b9bf2dbe1ddda
Francois <francois.allix@nav.no>
2024-05-27 13:59:27 +0200

    6528 aarsavregning api (#2375)
    https://jira.adeo.no/browse/MELOSYS-6528
    
    Utkast (som kan merges) med statisk json for å diskutere om dette er et
    bra api for årsavregninger

--- frontend-api/src/main/kotlin/no/nav/melosys/tjenester/gui/fagsaker/TrygdeavgiftOppsummering.kt
+++
@@ -1,3 +0,0 @@
-package no.nav.melosys.tjenester.gui.fagsaker
-
-data class TrygdeavgiftOppsummering(val harBehandlingMedTrygdeavgift: Boolean)
--- service/src/main/kotlin/no/nav/melosys/service/sak/ÅrsavregningService.kt
+++ service/src/main/kotlin/no/nav/melosys/service/sak/ÅrsavregningService.kt
@@ -1,17 +1,58 @@
 package no.nav.melosys.service.sak
 
+import no.nav.melosys.domain.Medlemskapsperiode
+import no.nav.melosys.domain.avgift.Inntektsperiode
+import no.nav.melosys.domain.avgift.SkatteforholdTilNorge
+import no.nav.melosys.domain.avgift.Trygdeavgiftsperiode
+import no.nav.melosys.exception.IkkeFunnetException
 import no.nav.melosys.integrasjon.faktureringskomponenten.FaktureringskomponentenConsumer
 import no.nav.melosys.integrasjon.faktureringskomponenten.dto.BeregnTotalBeløpDto
+import no.nav.melosys.repository.AarsavregningRepository
 import no.nav.melosys.sikkerhet.context.SubjectHandler
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 import java.math.BigDecimal
 
 @Service
-class ÅrsavregningService (
-    private val faktureringskomponentenConsumer: FaktureringskomponentenConsumer
+class ÅrsavregningService(
+    private val faktureringskomponentenConsumer: FaktureringskomponentenConsumer,
+    private val aarsavregningRepository: AarsavregningRepository,
 ) {
     fun beregnTotalTrygdeavgiftForPeriode(beregnTotalBeløpDto: BeregnTotalBeløpDto): BigDecimal {
         val saksbehandlerIdent = SubjectHandler.getInstance().getUserID()
         return faktureringskomponentenConsumer.hentTotalTrygdeavgiftForPeriode(beregnTotalBeløpDto, saksbehandlerIdent)
     }
+
+    @Transactional(readOnly = true)
+    fun hentÅrsavregning(avregningID: Long): Årsavregning {
+        val aarsavregning = aarsavregningRepository.findById(avregningID).orElseThrow { IkkeFunnetException("Fant ikke årsavregning $avregningID") }
+        return Årsavregning(
+            aar = aarsavregning.aar,
+            tidligereAvgift = null, // TODO mer mapping kommer
+            endeligAvgift = null,
+            tidligereFakturertBeloep = aarsavregning.tidligereFakturertBeloep,
+            nyttTotalbeloep = aarsavregning.nyttTotalbeloep,
+            tilFaktureringBeloep = aarsavregning.tilFaktureringBeloep
+        )
+    }
 }
+
+data class Årsavregning(
+    val aar: Int,
+    val tidligereAvgift: Trygdeavgift?,
+    val endeligAvgift: Trygdeavgift?,
+    val tidligereFakturertBeloep: BigDecimal?,
+    val nyttTotalbeloep: BigDecimal?,
+    val tilFaktureringBeloep: BigDecimal?,
+)
+
+data class Trygdeavgift(
+    val grunnlag: Trygdeavgiftsgrunnlag,
+    val avgiftsperioder: List<Trygdeavgiftsperiode>
+)
+
+data class Trygdeavgiftsgrunnlag(
+    val medlemskapsperioder: List<Medlemskapsperiode>,
+    val skatteforholdsperioder: List<SkatteforholdTilNorge>,
+    val innteksperioder: List<Inntektsperiode>
+)
