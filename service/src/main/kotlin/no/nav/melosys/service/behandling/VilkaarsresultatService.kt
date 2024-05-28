8d3b457fa79443dbbbf47bf55a6ed0315dcd87b9
Janita Johansen <janita.marielle.johansen@nav.no>
2024-05-28 14:21:27 +0200

    Skriver VilkaarsresultatService og test om til kotlin

---
+++ service/src/main/kotlin/no/nav/melosys/service/behandling/VilkaarsresultatService.kt
@@ -0,0 +1,150 @@
+package no.nav.melosys.service.behandling
+
+import no.nav.melosys.domain.Behandlingsresultat
+import no.nav.melosys.domain.VilkaarBegrunnelse
+import no.nav.melosys.domain.Vilkaarsresultat
+import no.nav.melosys.domain.kodeverk.Kodeverk
+import no.nav.melosys.domain.kodeverk.Vilkaar
+import no.nav.melosys.exception.FunksjonellException
+import no.nav.melosys.exception.IkkeFunnetException
+import no.nav.melosys.repository.BehandlingsresultatRepository
+import no.nav.melosys.service.saksbehandling.SaksbehandlingRegler
+import no.nav.melosys.service.vilkaar.VilkaarDto
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import java.util.*
+
+
+@Service
+class VilkaarsresultatService(
+    private val behandlingsresultatRepository: BehandlingsresultatRepository,
+    private val saksbehandlingRegler: SaksbehandlingRegler
+) {
+    fun hentBehandlingsresultat(behandlingsid: Long): Behandlingsresultat =
+        behandlingsresultatRepository.findById(behandlingsid)
+            .orElseThrow { IkkeFunnetException(BehandlingsresultatService.KAN_IKKE_FINNE_BEHANDLINGSRESULTAT + behandlingsid) }
+
+    @Transactional(readOnly = true)
+    fun hentVilkaar(behandlingID: Long): List<VilkaarDto> =
+        hentBehandlingsresultat(behandlingID).vilkaarsresultater.map {
+            VilkaarDto().apply {
+                vilkaar = it.vilkaar.kode
+                isOppfylt = it.isOppfylt
+                begrunnelseKoder = it.begrunnelser.map { it.kode }.toSet()
+                begrunnelseFritekst = it.begrunnelseFritekst
+                begrunnelseFritekstEngelsk = it.begrunnelseFritekstEessi
+            }
+        }
+
+    @Transactional(readOnly = true)
+    fun finnVilkaarsresultat(behandlingID: Long, vilkaar: Vilkaar): Optional<Vilkaarsresultat> =
+        Optional.ofNullable(hentBehandlingsresultat(behandlingID).vilkaarsresultater.firstOrNull { it.vilkaar == vilkaar })
+
+    @Transactional(readOnly = true)
+    fun oppfyllerVilkaar(behandlingID: Long, vilkaar: Vilkaar): Boolean =
+        hentBehandlingsresultat(behandlingID).vilkaarsresultater
+            .any { vilkaar == it.vilkaar && it.isOppfylt }
+
+    @Transactional(readOnly = true)
+    fun harVilkaarForArtikkel12(behandlingID: Long): Boolean {
+        val art121Vilkaar = finnVilkaarsresultat(behandlingID, Vilkaar.FO_883_2004_ART12_1)
+        val art122Vilkaar = finnVilkaarsresultat(behandlingID, Vilkaar.FO_883_2004_ART12_2)
+        return art121Vilkaar.isPresent || art122Vilkaar.isPresent
+    }
+
+    @Transactional(readOnly = true)
+    fun harVilkaarForArtikkel16(behandlingID: Long): Boolean {
+        val art161Vilkaar = finnVilkaarsresultat(behandlingID, Vilkaar.FO_883_2004_ART16_1)
+        return art161Vilkaar.isPresent
+    }
+
+    @Transactional
+    fun registrerVilkår(behandlingID: Long, vilkaarDtoer: List<VilkaarDto>) {
+        validerVilkår(vilkaarDtoer)
+        val behandlingsresultat = hentBehandlingsresultat(behandlingID)
+        tømVilkårsresultatFraBehandlingsresultat(behandlingID)
+        // Flush fordi vi potensielt legger til samme vilkåret igjen. INSERT kommer før DELETE i Hibernate, som skaper UNIQUE constraint problemer uten flush.
+        behandlingsresultatRepository.saveAndFlush(behandlingsresultat)
+        for (vilkaarDto in vilkaarDtoer) {
+            val vilkaarsresultat = lagVilkaarsresultat(
+                behandlingsresultat,
+                Vilkaar.valueOf(vilkaarDto.vilkaar),
+                vilkaarDto.isOppfylt,
+                vilkaarDto.begrunnelseKoder,
+                vilkaarDto.begrunnelseFritekst,
+                vilkaarDto.begrunnelseFritekstEngelsk
+            )
+            behandlingsresultat.vilkaarsresultater.add(vilkaarsresultat)
+        }
+        behandlingsresultatRepository.save(behandlingsresultat)
+    }
+
+    @Transactional
+    fun tømVilkårsresultatFraBehandlingsresultat(behandlingID: Long) {
+        val behandlingsresultat = hentBehandlingsresultat(behandlingID)
+        val behandling = behandlingsresultat.behandling
+        if (behandling.fagsak.erSakstypeEøs() && !saksbehandlingRegler.harIngenFlyt(behandling)) {
+            behandlingsresultat.vilkaarsresultater.removeIf { it.vilkaar !in IMMUTABLE_VILKAAR }
+        } else {
+            behandlingsresultat.vilkaarsresultater.clear()
+        }
+        behandlingsresultatRepository.saveAndFlush(behandlingsresultat)
+    }
+
+    private fun validerVilkår(vilkaarDtoer: List<VilkaarDto>) {
+        val nyeVilkår = vilkaarDtoer.map { it.vilkaar }
+        for (immutableVilkår in IMMUTABLE_VILKAAR) {
+            if (immutableVilkår.kode in nyeVilkår) {
+                throw FunksjonellException("Kan ikke endre vilkår $immutableVilkår")
+            }
+        }
+    }
+
+    @Transactional
+    fun oppdaterVilkaarsresultat(
+        behandlingID: Long,
+        vilkaar: Vilkaar,
+        oppfylt: Boolean,
+        begrunnelseKoder: Set<Kodeverk>
+    ) {
+        val behandlingsresultat = hentBehandlingsresultat(behandlingID)
+        behandlingsresultat.vilkaarsresultater.clear()
+        // Flush fordi vi potensielt legger til samme vilkåret igjen. INSERT kommer før DELETE i Hibernate, som skaper UNIQUE constraint problemer uten flush.
+        behandlingsresultatRepository.saveAndFlush(behandlingsresultat)
+        val vilkaarsresultat = lagVilkaarsresultat(
+            behandlingsresultat,
+            vilkaar,
+            oppfylt,
+            begrunnelseKoder.map { it.kode }.toSet()
+        )
+        behandlingsresultat.vilkaarsresultater.add(vilkaarsresultat)
+        behandlingsresultatRepository.save(behandlingsresultat)
+    }
+
+    private fun lagVilkaarsresultat(
+        behandlingsresultat: Behandlingsresultat,
+        vilkaar: Vilkaar,
+        oppfylt: Boolean,
+        begrunnelseKoder: Set<String>,
+        begrunnelseFritekst: String? = null,
+        begrunnelseFritekstEngelsk: String? = null
+    ): Vilkaarsresultat =
+        Vilkaarsresultat().apply {
+            this.behandlingsresultat = behandlingsresultat
+            this.vilkaar = vilkaar
+            this.isOppfylt = oppfylt
+            this.begrunnelser = begrunnelseKoder.map { lagBegrunnelse(this, it) }.toSet()
+            this.begrunnelseFritekst = begrunnelseFritekst
+            this.begrunnelseFritekstEessi = begrunnelseFritekstEngelsk
+        }
+
+    private fun lagBegrunnelse(vilkaarsresultat: Vilkaarsresultat, begrunnelseKode: String) = VilkaarBegrunnelse().apply {
+        this.vilkaarsresultat = vilkaarsresultat
+        this.kode = begrunnelseKode
+    }
+
+    companion object {
+        val IMMUTABLE_VILKAAR: Collection<Vilkaar> = setOf(Vilkaar.FO_883_2004_INNGANGSVILKAAR)
+    }
+}
+
---
+++ service/src/test/kotlin/no/nav/melosys/service/behandling/VilkaarsresultatServiceTest.kt
@@ -0,0 +1,195 @@
+package no.nav.melosys.service.behandling
+
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.matchers.collections.shouldBeEmpty
+import io.kotest.matchers.collections.shouldHaveSize
+import io.kotest.matchers.nulls.shouldNotBeNull
+import io.kotest.matchers.shouldBe
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.impl.annotations.RelaxedMockK
+import io.mockk.junit5.MockKExtension
+import io.mockk.verify
+import no.nav.melosys.domain.Behandling
+import no.nav.melosys.domain.Behandlingsresultat
+import no.nav.melosys.domain.FagsakTestFactory
+import no.nav.melosys.domain.Vilkaarsresultat
+import no.nav.melosys.domain.kodeverk.Sakstyper
+import no.nav.melosys.domain.kodeverk.Vilkaar
+import no.nav.melosys.domain.kodeverk.begrunnelser.Art12_1_begrunnelser
+import no.nav.melosys.domain.kodeverk.behandlinger.Behandlingstema
+import no.nav.melosys.domain.kodeverk.behandlinger.Behandlingstyper
+import no.nav.melosys.exception.FunksjonellException
+import no.nav.melosys.repository.BehandlingsresultatRepository
+import no.nav.melosys.service.saksbehandling.SaksbehandlingRegler
+import no.nav.melosys.service.vilkaar.VilkaarDto
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import java.util.*
+
+
+@ExtendWith(MockKExtension::class)
+internal class VilkaarsresultatServiceTest {
+    private val BEHANDLING_ID = 1L
+
+    @RelaxedMockK
+    private lateinit var saksbehandlingRegler: SaksbehandlingRegler
+
+    @MockK
+    private lateinit var behandlingsresultatRepo: BehandlingsresultatRepository
+
+    private lateinit var vilkaarsresultatService: VilkaarsresultatService
+
+    @BeforeEach
+    fun setUp() {
+        vilkaarsresultatService = VilkaarsresultatService(behandlingsresultatRepo, saksbehandlingRegler)
+    }
+
+    @Test
+    fun hentVilkaar() {
+        val behandlingsresultat = lagBehandlingsresultat().apply {
+            vilkaarsresultater = setOf(
+                Vilkaarsresultat().apply {
+                    vilkaar = Vilkaar.ART12_1_FORUTGAAENDE_MEDLEMSKAP
+                    isOppfylt = true
+                    begrunnelser = setOf()
+                    begrunnelseFritekst = "begrunnelse"
+                }
+            )
+        }
+        every { behandlingsresultatRepo.findById(BEHANDLING_ID) } returns Optional.of(behandlingsresultat)
+
+
+        val response = vilkaarsresultatService.hentVilkaar(BEHANDLING_ID)
+
+
+        response
+            .shouldNotBeNull()
+            .shouldHaveSize(1)
+            .single().vilkaar.shouldBe(Vilkaar.ART12_1_FORUTGAAENDE_MEDLEMSKAP.kode)
+    }
+
+    @Test
+    fun registrerVilkår() {
+        val behandlingsresultat = lagBehandlingsresultat()
+        val vilkaarDto = VilkaarDto().apply {
+            vilkaar = Vilkaar.FO_883_2004_ART12_1.kode
+            begrunnelseKoder = setOf(Art12_1_begrunnelser.ERSTATTER_ANNEN.kode)
+        }
+        every { behandlingsresultatRepo.findById(BEHANDLING_ID) } returns Optional.of(behandlingsresultat)
+        every { behandlingsresultatRepo.saveAndFlush(any()) } returnsArgument 0
+        every { behandlingsresultatRepo.save(any()) } returnsArgument 0
+        behandlingsresultat.vilkaarsresultater.shouldBeEmpty()
+
+
+        vilkaarsresultatService.registrerVilkår(BEHANDLING_ID, listOf(vilkaarDto))
+
+
+        verify { behandlingsresultatRepo.save(behandlingsresultat) }
+        behandlingsresultat.vilkaarsresultater
+            .shouldHaveSize(1)
+            .single().run {
+                vilkaar.shouldBe(Vilkaar.FO_883_2004_ART12_1)
+                begrunnelser.shouldHaveSize(1).single().kode.shouldBe(Art12_1_begrunnelser.ERSTATTER_ANNEN.kode)
+            }
+    }
+
+    @Test
+    fun registrer_inngangsvilkår_feiler() {
+        val vilkaarDto = VilkaarDto().apply { vilkaar = Vilkaar.FO_883_2004_INNGANGSVILKAAR.kode }
+
+        shouldThrow<FunksjonellException> { vilkaarsresultatService.registrerVilkår(BEHANDLING_ID, listOf(vilkaarDto)) }
+            .message.shouldBe("Kan ikke endre vilkår " + Vilkaar.FO_883_2004_INNGANGSVILKAAR)
+    }
+
+    @Test
+    fun tømVilkårsresultatFraBehandlingsresultat_sakstypeIkkeEøs_sletterAlleVilkår() {
+        val behandlingsresultat = Behandlingsresultat().apply {
+            id = BEHANDLING_ID
+            behandling = Behandling().apply { fagsak = FagsakTestFactory.builder().type(Sakstyper.FTRL).build() }
+            vilkaarsresultater = mutableSetOf(Vilkaarsresultat().apply { id = BEHANDLING_ID })
+        }
+        every { behandlingsresultatRepo.findById(BEHANDLING_ID) } returns Optional.of(behandlingsresultat)
+        every { behandlingsresultatRepo.saveAndFlush(any()) } returnsArgument 0
+
+
+        vilkaarsresultatService.tømVilkårsresultatFraBehandlingsresultat(BEHANDLING_ID)
+
+
+        verify { behandlingsresultatRepo.saveAndFlush(behandlingsresultat) }
+        behandlingsresultat.vilkaarsresultater.shouldBeEmpty()
+    }
+
+    @Test
+    fun tømVilkårsresultatFraBehandlingsresultat_sakstypeEøsMenIngenFlyt_sletterAlleVilkår() {
+        val behandlingsresultat = Behandlingsresultat().apply {
+            id = BEHANDLING_ID
+            behandling = Behandling().apply {
+                id = BEHANDLING_ID
+                fagsak = FagsakTestFactory.lagFagsak()
+                tema = Behandlingstema.UTSENDT_ARBEIDSTAKER
+                type = Behandlingstyper.HENVENDELSE
+            }
+            vilkaarsresultater = mutableSetOf(Vilkaarsresultat().apply { id = BEHANDLING_ID })
+        }
+        every { saksbehandlingRegler.harIngenFlyt(behandlingsresultat.behandling) } returns true
+        every { behandlingsresultatRepo.findById(BEHANDLING_ID) } returns Optional.of(behandlingsresultat)
+        every { behandlingsresultatRepo.saveAndFlush(any()) } returnsArgument 0
+
+
+        vilkaarsresultatService.tømVilkårsresultatFraBehandlingsresultat(BEHANDLING_ID)
+
+
+        verify { behandlingsresultatRepo.saveAndFlush(behandlingsresultat) }
+        behandlingsresultat.vilkaarsresultater.shouldBeEmpty()
+    }
+
+    @Test
+    fun tømVilkårsresultatFraBehandlingsresultat_sakstypeEøsOgHarFlyt_sletterIkkeInngangsvilkår() {
+        val behandlingsresultat = Behandlingsresultat().apply {
+            id = BEHANDLING_ID
+            behandling = Behandling().apply {
+                id = BEHANDLING_ID
+                fagsak = FagsakTestFactory.lagFagsak()
+                tema = Behandlingstema.UTSENDT_ARBEIDSTAKER
+                type = Behandlingstyper.FØRSTEGANG
+            }
+            vilkaarsresultater = mutableSetOf(
+                Vilkaarsresultat().apply {
+                    id = BEHANDLING_ID
+                    vilkaar = Vilkaar.FO_883_2004_INNGANGSVILKAAR
+                },
+                Vilkaarsresultat().apply {
+                    id = 2L
+                    vilkaar = Vilkaar.FO_883_2004_ART12_1
+                }
+            )
+        }
+
+        every { behandlingsresultatRepo.findById(BEHANDLING_ID) } returns Optional.of(behandlingsresultat)
+        every { behandlingsresultatRepo.saveAndFlush(any()) } returnsArgument 0
+
+
+        vilkaarsresultatService.tømVilkårsresultatFraBehandlingsresultat(BEHANDLING_ID)
+
+
+        verify { behandlingsresultatRepo.saveAndFlush(behandlingsresultat) }
+        behandlingsresultat.vilkaarsresultater
+            .shouldHaveSize(1)
+            .single().run {
+                id.shouldBe(BEHANDLING_ID)
+                vilkaar.shouldBe(Vilkaar.FO_883_2004_INNGANGSVILKAAR)
+            }
+    }
+
+    private fun lagBehandlingsresultat(): Behandlingsresultat =
+        Behandlingsresultat().apply {
+            id = BEHANDLING_ID
+            behandling = Behandling().apply {
+                fagsak = FagsakTestFactory.lagFagsak()
+            }
+        }
+
+}
+
