9468d7403f791877eb4da4525ffa5229e3650147
Isa Salem Hame <wanghattan@gmail.com>
2024-06-03 09:31:15 +0200

    6631 FIX: La skattteforhold- og inntektsperioder være distinct i DTO

--- frontend-api/src/main/kotlin/no/nav/melosys/tjenester/gui/dto/trygdeavgift/TrygdeavgiftsgrunnlagDto.kt
+++ frontend-api/src/main/kotlin/no/nav/melosys/tjenester/gui/dto/trygdeavgift/TrygdeavgiftsgrunnlagDto.kt
@@ -9,8 +9,8 @@ data class TrygdeavgiftsgrunnlagDto(
 ) {
 
     constructor(trygdeavgiftsperiode: Set<Trygdeavgiftsperiode>) : this(
-        trygdeavgiftsperiode.map { SkatteforholdTilNorgeDto(it.grunnlagSkatteforholdTilNorge) }.sortedWith(compareBy { it.tomDato }).toList(),
-        trygdeavgiftsperiode.map { InntekskildeDto(it.grunnlagInntekstperiode) }.sortedWith(compareBy { it.tomDato }).toList()
+        trygdeavgiftsperiode.map { SkatteforholdTilNorgeDto(it.grunnlagSkatteforholdTilNorge) }.sortedWith(compareBy { it.tomDato }).distinct(),
+        trygdeavgiftsperiode.map { InntekskildeDto(it.grunnlagInntekstperiode) }.sortedWith(compareBy { it.tomDato }).distinct()
     )
 
     fun tilRequest(): OppdaterTrygdeavgiftsgrunnlagRequest =
