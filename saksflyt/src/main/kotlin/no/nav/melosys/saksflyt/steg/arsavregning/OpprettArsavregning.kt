16ac13fb6ee7a5c5a76f0ebb68283efc070c8934
RuneLind <rulind@gmail.com>
2024-06-07 12:09:28 +0200

    Legg til OpprettArsavregningTest skall

--- saksflyt/src/main/kotlin/no/nav/melosys/saksflyt/steg/arsavregning/OpprettArsavregning.kt
+++ saksflyt/src/main/kotlin/no/nav/melosys/saksflyt/steg/arsavregning/OpprettArsavregning.kt
@@ -74,7 +74,7 @@ class OpprettArsavregning(
             null,
             null,
             LocalDate.now(),
-            Behandlingsaarsaktyper.ANNET, // TODO: Her kommer en ny type
+            Behandlingsaarsaktyper.ANNET, // TODO: Her kommer en ny type SKATTEHENDELSE
             null
         ).also { nyBehandling ->
             val behandlingsresultat = behandslingsresultatService.hentBehandlingsresultat(nyBehandling.id)
---
+++ saksflyt/src/test/kotlin/no/nav/melosys/saksflyt/steg/arsavregning/OpprettArsavregningTest.kt
@@ -0,0 +1,64 @@
+package no.nav.melosys.saksflyt.steg.arsavregning
+
+import io.mockk.impl.annotations.MockK
+import io.mockk.junit5.MockKExtension
+import no.nav.melosys.service.LovvalgsperiodeService
+import no.nav.melosys.service.behandling.BehandlingService
+import no.nav.melosys.service.behandling.BehandlingsresultatService
+import no.nav.melosys.service.ftrl.medlemskapsperiode.MedlemskapsperiodeService
+import no.nav.melosys.service.persondata.PersondataService
+import no.nav.melosys.service.sak.FagsakService
+import no.nav.melosys.service.sak.TrygdeavgiftOppsummeringService
+import no.nav.melosys.service.sak.ÅrsavregningService
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+
+
+@ExtendWith(MockKExtension::class)
+class OpprettArsavregningTest {
+    @MockK
+    private lateinit var fagsakService: FagsakService
+
+    @MockK
+    private lateinit var behandlingService: BehandlingService
+
+    @MockK
+    private lateinit var behandslingsresultatService: BehandlingsresultatService
+
+    @MockK
+    private lateinit var lovvalgsperiodeService: LovvalgsperiodeService
+
+    @MockK
+    private lateinit var trygdeavgiftOppsummeringService: TrygdeavgiftOppsummeringService
+
+    @MockK
+    private lateinit var medlemskapsperiodeService: MedlemskapsperiodeService
+
+    @MockK
+    private lateinit var persondataService: PersondataService
+
+    @MockK
+    private lateinit var oppretteÅrsavregning: ÅrsavregningService
+
+
+    @BeforeEach
+    fun setUp() {
+        val opprettArsavregning = OpprettArsavregning(
+            fagsakService,
+            persondataService,
+            trygdeavgiftOppsummeringService,
+            behandlingService,
+            lovvalgsperiodeService,
+            medlemskapsperiodeService,
+            behandslingsresultatService,
+            oppretteÅrsavregning
+        )
+    }
+
+
+    @Test
+    fun test() {
+
+    }
+}
