4c0ff53860940989303e309c192494b339352737
RuneLind <rulind@gmail.com>
2024-06-11 15:13:07 +0200

    Reverter OpprettOppgave slik at vi feiler om det ikke finnes behandling

--- saksflyt/src/main/kotlin/no/nav/melosys/saksflyt/steg/oppgave/OpprettOppgave.kt
+++ saksflyt/src/main/kotlin/no/nav/melosys/saksflyt/steg/oppgave/OpprettOppgave.kt
@@ -1,24 +1,16 @@
 package no.nav.melosys.saksflyt.steg.oppgave
 
-import mu.KotlinLogging
 import no.nav.melosys.saksflyt.steg.StegBehandler
 import no.nav.melosys.saksflytapi.domain.ProsessSteg
 import no.nav.melosys.saksflytapi.domain.Prosessinstans
 import no.nav.melosys.service.oppgave.OppgaveService
 import org.springframework.stereotype.Component
 
-private val log = KotlinLogging.logger { }
-
 @Component
 class OpprettOppgave(private val oppgaveService: OppgaveService) : StegBehandler {
     override fun inngangsSteg(): ProsessSteg = ProsessSteg.OPPRETT_OPPGAVE
 
     override fun utfør(prosessinstans: Prosessinstans) {
-        if(prosessinstans.behandling == null) {
-            log.warn("Prosessinstans ${prosessinstans.id} har ingen behandling, kan ikke opprette oppgave")
-            return
-        }
-
         oppgaveService.opprettEllerGjenbrukBehandlingsoppgave(
             prosessinstans.behandling,
             prosessinstans.hentJournalpostID(),
