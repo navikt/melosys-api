0517dd53dee61d35c786d70a29474882037f4ff0
barstadEkstern <oystein.madsen.barstad@nav.no>
2024-06-26 16:13:48 +0200

    testing

--- service/src/main/kotlin/no/nav/melosys/service/eessi/AdminInnvalideringSedRuter.kt
+++ service/src/main/kotlin/no/nav/melosys/service/eessi/AdminInnvalideringSedRuter.kt
@@ -62,6 +62,7 @@ class AdminInnvalideringSedRuter(
                 if (aktivBehandlingErInvalidert && (sistAktiveBehandling.erRegisteringAvUnntak() || sistAktiveBehandling.erAnmodningOmUnntak())) {
                     annullerSakOgBehandling(sistAktiveBehandling)
                     behandlingsresultatService.oppdaterBehandlingsresultattype(sistAktiveBehandling.id, Behandlingsresultattyper.HENLEGGELSE)
+                    oppgaveService.ferdigstillOppgaveMedSaksnummer(sistAktiveBehandling.fagsak.saksnummer)
                 } else {
                     oppgaveService.opprettEllerGjenbrukBehandlingsoppgave(
                         sistAktiveBehandling,
