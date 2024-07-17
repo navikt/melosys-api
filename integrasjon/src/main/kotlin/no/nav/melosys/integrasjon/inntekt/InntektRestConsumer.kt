35a4d57f2d569e6d77d86e7f8518f09a357666b9
Francois Allix <32903762+frans-a@users.noreply.github.com>
2024-07-17 18:25:08 +0200

    Config for å åpne for Retryable

--- integrasjon/src/main/kotlin/no/nav/melosys/integrasjon/inntekt/InntektRestConsumer.kt
+++ integrasjon/src/main/kotlin/no/nav/melosys/integrasjon/inntekt/InntektRestConsumer.kt
@@ -7,10 +7,8 @@ import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.bodyToMono
 
 @Retryable
-open class InntektRestConsumer(private val webClient: WebClient) {
-    // Metoder må være open for at retry skal funke og at webClient ikke skal bli null
-    // https://github.com/spring-projects/spring-framework/issues/26729
-    open fun hentInntektListe(inntektRequest: InntektRequest) : InntektResponse {
+class InntektRestConsumer(private val webClient: WebClient) {
+    fun hentInntektListe(inntektRequest: InntektRequest) : InntektResponse {
         return webClient.post()
             .uri("/hentinntektliste")
             .accept(MediaType.APPLICATION_JSON)
