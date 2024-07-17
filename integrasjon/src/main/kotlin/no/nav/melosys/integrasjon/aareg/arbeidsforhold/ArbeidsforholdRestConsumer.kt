0dca9c0659f2b9a3e2b09c8cae571826768a9522
Francois Allix <32903762+frans-a@users.noreply.github.com>
2024-07-17 17:10:21 +0200

    til kotlin

---
+++ integrasjon/src/main/kotlin/no/nav/melosys/integrasjon/aareg/arbeidsforhold/ArbeidsforholdRestConsumer.kt
@@ -0,0 +1,38 @@
+package no.nav.melosys.integrasjon.aareg.arbeidsforhold
+
+import no.nav.melosys.integrasjon.felles.WebClientConfig
+import org.springframework.http.MediaType
+import org.springframework.retry.annotation.Retryable
+import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.bodyToMono
+import org.springframework.web.util.UriBuilder
+
+@Retryable
+class ArbeidsforholdRestConsumer(private val webClient: WebClient) : WebClientConfig {
+    fun finnArbeidsforholdPrArbeidstaker(
+        fnr: String,
+        arbeidsforholdQuery: ArbeidsforholdQuery
+    ): ArbeidsforholdResponse {
+        return ArbeidsforholdResponse(hentArbeidsforhold(fnr, arbeidsforholdQuery))
+    }
+
+    private fun hentArbeidsforhold(
+        fnr: String,
+        arbeidsforholdQuery: ArbeidsforholdQuery
+    ): List<ArbeidsforholdResponse.Arbeidsforhold> {
+        return webClient.get().uri("") { uriBuilder: UriBuilder ->
+            uriBuilder
+                .queryParam("regelverk", arbeidsforholdQuery.regelverk)
+                .queryParamIfPresent("arbeidsforholdType", arbeidsforholdQuery.arbeidsforholdType)
+                .queryParamIfPresent("ansettelsesperiodeFom", arbeidsforholdQuery.ansettelsesperiodeFom)
+                .queryParamIfPresent("ansettelsesperiodeTom", arbeidsforholdQuery.ansettelsesperiodeTom)
+                .build()
+        } // Om vi ønsker å se request med mer detaljer i grafana må vi gjøre det samme som er gjort i MedlemskapRestConsumer
+            // Nå ser vi bare request på host
+            .header("Nav-Personident", fnr)
+            .accept(MediaType.APPLICATION_JSON)
+            .retrieve()
+            .bodyToMono<List<ArbeidsforholdResponse.Arbeidsforhold>>()
+            .block()!!
+    }
+}
