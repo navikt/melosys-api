854edb030367558f088664fd49108f7f47dc21d8
Francois Allix <32903762+frans-a@users.noreply.github.com>
2024-07-18 13:24:09 +0200

    QA

--- integrasjon/src/main/kotlin/no/nav/melosys/integrasjon/aareg/arbeidsforhold/ArbeidsforholdResponse.kt
+++ integrasjon/src/main/kotlin/no/nav/melosys/integrasjon/aareg/arbeidsforhold/ArbeidsforholdResponse.kt
@@ -13,8 +13,7 @@ import java.util.*
 
 class ArbeidsforholdResponse(val arbeidsforhold: List<Arbeidsforhold>) {
     fun tilSaksopplysning(): String {
-        val objectMapper = ObjectMapper()
-        objectMapper.registerModule(JavaTimeModule())
+        val objectMapper = ObjectMapper().apply { registerModule(JavaTimeModule()) }
         try {
             // serialize to json or find out if we want to convert to xml doc?
             return objectMapper.writeValueAsString(arbeidsforhold)
