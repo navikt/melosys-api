6cc9745daa20089390a3b059dad08734c955310e
Francois <francois.allix@nav.no>
2024-07-19 12:14:23 +0200

    6681 aareg kotlin (#2455)
    Aareg kode til Kotlin

---
+++ integrasjon/src/main/kotlin/no/nav/melosys/integrasjon/aareg/AaregService.kt
@@ -0,0 +1,38 @@
+package no.nav.melosys.integrasjon.aareg
+
+import no.nav.melosys.domain.Saksopplysning
+import no.nav.melosys.domain.SaksopplysningKildesystem
+import no.nav.melosys.domain.SaksopplysningType
+import no.nav.melosys.integrasjon.aareg.arbeidsforhold.ArbeidsforholdKonverter
+import no.nav.melosys.integrasjon.aareg.arbeidsforhold.ArbeidsforholdQuery
+import no.nav.melosys.integrasjon.aareg.arbeidsforhold.ArbeidsforholdRestConsumer
+import no.nav.melosys.integrasjon.kodeverk.KodeOppslag
+import org.springframework.stereotype.Service
+import java.time.LocalDate
+
+@Service
+class AaregService(
+    private val arbeidsforholdRestConsumer: ArbeidsforholdRestConsumer,
+    private val kodeOppslag: KodeOppslag
+) : AaregFasade {
+    override fun finnArbeidsforholdPrArbeidstaker(ident: String, fom: LocalDate?, tom: LocalDate?): Saksopplysning {
+        val arbeidsforholdQuery = ArbeidsforholdQuery(
+            regelverk = ArbeidsforholdQuery.Regelverk.A_ORDNINGEN,
+            arbeidsforholdType = ArbeidsforholdQuery.ArbeidsforholdType.ALLE,
+            ansettelsesperiodeFom = fom,
+            ansettelsesperiodeTom = tom
+        )
+
+        val response = arbeidsforholdRestConsumer.finnArbeidsforholdPrArbeidstaker(ident, arbeidsforholdQuery)
+
+        return ArbeidsforholdKonverter(response, kodeOppslag).createSaksopplysning().apply {
+            leggTilKildesystemOgMottattDokument(SaksopplysningKildesystem.AAREG, response.tilSaksopplysning())
+            type = SaksopplysningType.ARBFORH
+            versjon = ARBEIDSFORHOLD_REST_VERSJON
+        }
+    }
+
+    companion object {
+        private const val ARBEIDSFORHOLD_REST_VERSJON = "REST 1.0"
+    }
+}
---
+++ integrasjon/src/main/kotlin/no/nav/melosys/integrasjon/aareg/arbeidsforhold/ArbeidsforholdRestConsumer.kt
@@ -0,0 +1,36 @@
+package no.nav.melosys.integrasjon.aareg.arbeidsforhold
+
+import no.nav.melosys.exception.TekniskException
+import no.nav.melosys.integrasjon.felles.WebClientConfig
+import org.springframework.http.MediaType
+import org.springframework.retry.annotation.Retryable
+import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.bodyToMono
+import org.springframework.web.util.UriBuilder
+import java.util.*
+
+@Retryable
+open class ArbeidsforholdRestConsumer(private val webClient: WebClient) : WebClientConfig {
+    open fun finnArbeidsforholdPrArbeidstaker(
+        fnr: String,
+        arbeidsforholdQuery: ArbeidsforholdQuery
+    ): ArbeidsforholdResponse = ArbeidsforholdResponse(hentArbeidsforhold(fnr, arbeidsforholdQuery))
+
+    private fun hentArbeidsforhold(
+        fnr: String,
+        arbeidsforholdQuery: ArbeidsforholdQuery
+    ): List<ArbeidsforholdResponse.Arbeidsforhold> = webClient.get().uri("") { uriBuilder: UriBuilder ->
+        uriBuilder
+            .queryParam("regelverk", arbeidsforholdQuery.regelverk)
+            .queryParamIfPresent("arbeidsforholdType", Optional.ofNullable(arbeidsforholdQuery.arbeidsforholdType.queryParam))
+            .queryParamIfPresent("ansettelsesperiodeFom", Optional.ofNullable(arbeidsforholdQuery.ansettelsesperiodeFom))
+            .queryParamIfPresent("ansettelsesperiodeTom", Optional.ofNullable(arbeidsforholdQuery.ansettelsesperiodeTom))
+            .build()
+    } // Om vi ønsker å se request med mer detaljer i grafana må vi gjøre det samme som er gjort i MedlemskapRestConsumer
+        // Nå ser vi bare request på host
+        .header("Nav-Personident", fnr)
+        .accept(MediaType.APPLICATION_JSON)
+        .retrieve()
+        .bodyToMono<List<ArbeidsforholdResponse.Arbeidsforhold>>()
+        .block() ?: throw TekniskException("ArbeidsforholdResponse er null")
+}
