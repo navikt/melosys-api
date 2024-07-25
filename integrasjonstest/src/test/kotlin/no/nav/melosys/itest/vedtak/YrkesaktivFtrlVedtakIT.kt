2900d0d2ce9bd08c68a71614f003999cf55036c0
Isa Salem Hame <isa.salem.hame@nav.no>
2024-07-25 11:44:09 +0200

    Rett på en test etter renaming

--- integrasjonstest/src/test/kotlin/no/nav/melosys/itest/vedtak/YrkesaktivFtrlVedtakIT.kt
+++ integrasjonstest/src/test/kotlin/no/nav/melosys/itest/vedtak/YrkesaktivFtrlVedtakIT.kt
@@ -337,7 +337,7 @@ class YrkesaktivFtrlVedtakIT(
                         .run {
                             behandlingsResultRepository.findById(id)
                                 .shouldBePresent()
-                                .aarsavregning
+                                .Årsavregning
                                 .shouldNotBeNull()
                                 .run {
                                     aar shouldBe 2023
