79a1a8f0dd2d2e173a9bceda7c006f1cbd3b4950
Isa Salem Hame <isa.salem.hame@nav.no>
2024-08-02 11:08:36 +0200

    VAKT: Legg til ny adresse for utenlandsk myndighet, Bosnia

---
+++ app/src/main/resources/db/migration/melosysDB/V115__utenlandsk_myndighet_bosnia_ny_adresse.sql
@@ -0,0 +1,4 @@
+INSERT INTO utenlandsk_myndighet (id, institusjonskode, navn, gateadresse_1, gateadresse_2, postnummer, poststed, land, landkode)
+VALUES(48, null, 'Ministry of Labor, War Veterans and Disabled Persons'' Protection', 'Trg Republike Srpske 1, Lamela A', null, '78000', 'Banja Luka', 'BOSNIA-HERCEGOVINA', 'BA');
+
+INSERT INTO utenlandsk_myndighet_pref(utenlandsk_myndighet_id, preferanse_id) VALUES(48, 1);
