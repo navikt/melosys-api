31198726269f7f554a6708dc02a4f7fb1c3ab003
Thomas.Dolplads <thomas.dolplads@twoday.com>
2024-08-21 08:22:04 +0200

    GenericContextExchangeFilter::getCorrectToken feiler pga verken system eller user token

--- integrasjon/src/main/kotlin/no/nav/melosys/integrasjon/kodeverk/impl/KodeverkConsumerImpl.kt
+++ integrasjon/src/main/kotlin/no/nav/melosys/integrasjon/kodeverk/impl/KodeverkConsumerImpl.kt
@@ -2,13 +2,16 @@ package no.nav.melosys.integrasjon.kodeverk.impl
 
 import no.nav.melosys.integrasjon.felles.CallIdAware
 import no.nav.melosys.integrasjon.kodeverk.impl.dto.FellesKodeverkDto
+import no.nav.melosys.sikkerhet.context.ThreadLocalAccessInfo
 import org.springframework.web.reactive.function.client.WebClient
 import java.time.LocalDate
+import java.util.*
 
 class KodeverkConsumerImpl internal constructor(private val webClient: WebClient) : CallIdAware {
 
     fun hentKodeverk(navn: String): FellesKodeverkDto {
-        return webClient
+        val requestId = UUID.randomUUID();
+        val kodeverkRequest = webClient
             .get()
             .uri { uriBuilder ->
                 uriBuilder
@@ -20,7 +23,12 @@ class KodeverkConsumerImpl internal constructor(private val webClient: WebClient
             }
             .retrieve()
             .bodyToMono(FellesKodeverkDto::class.java)
-            .block() ?: throw RuntimeException("Feilet å hente felles-kodeverk")
+
+        ThreadLocalAccessInfo.beforeExecuteProcess(requestId, "hent felles-kodeverk")
+        val fellesKodeverkDto = kodeverkRequest.block() ?: throw RuntimeException("Feilet å hente felles-kodeverk")
+        ThreadLocalAccessInfo.afterExecuteProcess(requestId)
+
+        return fellesKodeverkDto
     }
 
     companion object {
