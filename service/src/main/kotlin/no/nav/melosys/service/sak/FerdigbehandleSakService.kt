e13b8a4f9f1a56c35b421862c214fcbe24f8dfb7
Thomas.Dolplads <thomas.dolplads@twoday.com>
2024-08-26 15:39:48 +0200

    benytt behandlingId som erstatning for sakId pga flere aktive behandlinger

--- service/src/main/kotlin/no/nav/melosys/service/sak/FerdigbehandleSakService.kt
+++ service/src/main/kotlin/no/nav/melosys/service/sak/FerdigbehandleSakService.kt
@@ -2,6 +2,8 @@ package no.nav.melosys.service.sak
 
 import no.nav.melosys.domain.kodeverk.Saksstatuser
 import no.nav.melosys.domain.kodeverk.behandlinger.Behandlingsresultattyper
+import no.nav.melosys.domain.kodeverk.behandlinger.Behandlingstyper
+import no.nav.melosys.service.behandling.BehandlingService
 import no.nav.melosys.service.behandling.BehandlingsresultatService
 import no.nav.melosys.service.oppgave.OppgaveService
 import org.springframework.stereotype.Service
@@ -10,9 +12,11 @@ import org.springframework.transaction.annotation.Transactional
 @Service
 class FerdigbehandleSakService(
     private val fagsakService: FagsakService,
+    private val behandlingService: BehandlingService,
     private val behandlingsresultatService: BehandlingsresultatService,
-    private val oppgaveService: OppgaveService,
+    private val oppgaveService: OppgaveService
 ) {
+    @Deprecated("erstattet av med metode med behandlingId")
     @Transactional
     fun ferdigbehandleSak(saksnummer: String) {
         val fagsak = fagsakService.hentFagsak(saksnummer)
@@ -22,5 +26,25 @@ class FerdigbehandleSakService(
         fagsakService.avsluttFagsakOgBehandling(fagsak, behandling, nyStatus)
         behandlingsresultatService.oppdaterBehandlingsresultattype(behandling.id, Behandlingsresultattyper.FERDIGBEHANDLET)
         oppgaveService.ferdigstillOppgaveMedBehandlingID(behandling.id)
+    }
+
+    @Transactional
+    fun ferdigbehandle(behandlingId: Long) {
+        val behandling = behandlingService.hentBehandling(behandlingId)
+        val fagsak = fagsakService.hentFagsak(behandling.fagsak.saksnummer)
+
+        if (behandling.type == Behandlingstyper.ÅRSAVREGNING) {
+            behandlingsresultatService.tømBehandlingsresultat(behandlingId)
+        }
+
+        behandlingsresultatService.oppdaterBehandlingsresultattype(behandlingId, Behandlingsresultattyper.FERDIGBEHANDLET)
+
+        if (fagsak.erEnesteBehandling(behandlingId)) {
+            fagsakService.avsluttFagsakOgBehandling(fagsak, behandling, Saksstatuser.AVSLUTTET)
+        } else {
+            behandlingService.avsluttBehandling(behandlingId)
+        }
+
+        oppgaveService.ferdigstillOppgaveMedBehandlingID(behandlingId)
     }
 }
--- service/src/test/kotlin/no/nav/melosys/service/sak/FerdigbehandleSakServiceTest.kt
+++ service/src/test/kotlin/no/nav/melosys/service/sak/FerdigbehandleSakServiceTest.kt
@@ -6,9 +6,12 @@ import io.mockk.junit5.MockKExtension
 import io.mockk.verify
 import no.nav.melosys.domain.Behandling
 import no.nav.melosys.domain.FagsakTestFactory
+import no.nav.melosys.domain.FagsakTestFactory.BEHANDLING_ID
 import no.nav.melosys.domain.kodeverk.Saksstatuser
 import no.nav.melosys.domain.kodeverk.behandlinger.Behandlingsresultattyper
+import no.nav.melosys.domain.kodeverk.behandlinger.Behandlingstyper
 import no.nav.melosys.saksflytapi.ProsessinstansService
+import no.nav.melosys.service.behandling.BehandlingService
 import no.nav.melosys.service.behandling.BehandlingsresultatService
 import no.nav.melosys.service.oppgave.OppgaveService
 import org.junit.jupiter.api.BeforeEach
@@ -19,13 +22,16 @@ import org.junit.jupiter.api.extension.ExtendWith
 class FerdigbehandleSakServiceTest {
 
     @RelaxedMockK
+    lateinit var fagsakService: FagsakService
+
+    @RelaxedMockK
+    lateinit var behandlingService: BehandlingService
+
+    @RelaxedMockK
     lateinit var behandlingsresultatService: BehandlingsresultatService
 
     @RelaxedMockK
     lateinit var oppgaveService: OppgaveService
-
-    @RelaxedMockK
-    lateinit var fagsakService: FagsakService
 
     @RelaxedMockK
     lateinit var prosessinstansService: ProsessinstansService
@@ -34,22 +40,25 @@ class FerdigbehandleSakServiceTest {
 
     @BeforeEach
     fun setup() {
-        ferdigbehandleSakService = FerdigbehandleSakService(fagsakService, behandlingsresultatService, oppgaveService)
+        ferdigbehandleSakService = FerdigbehandleSakService(fagsakService, behandlingService, behandlingsresultatService, oppgaveService)
     }
 
     @Test
-    fun ferdigbehandleSak_saksstatusOPPRETTET_lagrerKorrekt() {
+    fun `ferdigbehandle med kun en behandling setter behandlingsresultat til FERDIGBEHANDLET og avslutter fagsak`() {
         val fagsak = FagsakTestFactory.lagFagsak()
-        val behandlingID : Long = 1
+        val behandlingID: Long = BEHANDLING_ID
         val behandling = Behandling().apply {
             id = behandlingID
             this.fagsak = fagsak
         }
+
         fagsak.behandlinger.add(behandling)
+
+        every { behandlingService.hentBehandling(behandlingID) } returns behandling
         every { fagsakService.hentFagsak(FagsakTestFactory.SAKSNUMMER) } returns fagsak
 
 
-        ferdigbehandleSakService.ferdigbehandleSak(FagsakTestFactory.SAKSNUMMER)
+        ferdigbehandleSakService.ferdigbehandle(BEHANDLING_ID)
 
 
         verify { fagsakService.avsluttFagsakOgBehandling(fagsak, behandling, Saksstatuser.AVSLUTTET) }
@@ -58,25 +67,72 @@ class FerdigbehandleSakServiceTest {
     }
 
     @Test
-    fun ferdigbehandleSak_saksstatusAnnetEnnOPPRETTET_lagrerKorrekt() {
-        val fagsak = FagsakTestFactory.builder().apply {
-            status = Saksstatuser.LOVVALG_AVKLART
-        }.build()
-        val behandlingID: Long = 1
-        val behandling = Behandling().apply {
+    fun `ferdigbehandle med flere behandlinger setter behandlingsresultat til FERDIGBEHANDLET og endrer ikke fagsak`() {
+        val fagsak = FagsakTestFactory.lagFagsak()
+        val behandlingID: Long = BEHANDLING_ID
+        val førstegangsBehandling = Behandling().apply {
+            id = 123
+            this.fagsak = fagsak
+        }
+        val annengangsBehandling = Behandling().apply {
             id = behandlingID
             this.fagsak = fagsak
-
         }
-        fagsak.behandlinger.add(behandling)
+
+        fagsak.behandlinger.add(førstegangsBehandling)
+        fagsak.behandlinger.add(annengangsBehandling)
+
+        every { behandlingService.hentBehandling(behandlingID) } returns annengangsBehandling
         every { fagsakService.hentFagsak(FagsakTestFactory.SAKSNUMMER) } returns fagsak
 
 
-        ferdigbehandleSakService.ferdigbehandleSak(FagsakTestFactory.SAKSNUMMER)
+        ferdigbehandleSakService.ferdigbehandle(BEHANDLING_ID)
 
 
-        verify { fagsakService.avsluttFagsakOgBehandling(fagsak, behandling, Saksstatuser.LOVVALG_AVKLART) }
-        verify { behandlingsresultatService.oppdaterBehandlingsresultattype(behandlingID, Behandlingsresultattyper.FERDIGBEHANDLET) }
+        verify(exactly = 0) { fagsakService.avsluttFagsakOgBehandling(fagsak, any(), any()) }
+        verify { behandlingsresultatService.oppdaterBehandlingsresultattype(BEHANDLING_ID, Behandlingsresultattyper.FERDIGBEHANDLET) }
         verify { oppgaveService.ferdigstillOppgaveMedBehandlingID(behandlingID) }
+    }
+
+    @Test
+    fun `ferdigbehandle med årsavregning tømmer behandlingsresultat`() {
+        val fagsak = FagsakTestFactory.lagFagsak()
+        val behandlingID: Long = BEHANDLING_ID
+        val behandling = Behandling().apply {
+            id = behandlingID
+            type = Behandlingstyper.ÅRSAVREGNING
+            this.fagsak = fagsak
+        }
+
+        fagsak.behandlinger.add(behandling)
+
+        every { behandlingService.hentBehandling(behandlingID) } returns behandling
+        every { fagsakService.hentFagsak(FagsakTestFactory.SAKSNUMMER) } returns fagsak
+
+
+        ferdigbehandleSakService.ferdigbehandle(BEHANDLING_ID)
+
+        verify { behandlingsresultatService.tømBehandlingsresultat(BEHANDLING_ID) }
+    }
+
+    @Test
+    fun `ferdigbehandle ikke årsavregning tømmer ikke behandlingsresultat`() {
+        val fagsak = FagsakTestFactory.lagFagsak()
+        val behandlingID: Long = BEHANDLING_ID
+        val behandling = Behandling().apply {
+            id = behandlingID
+            type = Behandlingstyper.FØRSTEGANG
+            this.fagsak = fagsak
+        }
+
+        fagsak.behandlinger.add(behandling)
+
+        every { behandlingService.hentBehandling(behandlingID) } returns behandling
+        every { fagsakService.hentFagsak(FagsakTestFactory.SAKSNUMMER) } returns fagsak
+
+
+        ferdigbehandleSakService.ferdigbehandle(BEHANDLING_ID)
+
+        verify(exactly = 0) { behandlingsresultatService.tømBehandlingsresultat(any()) }
     }
 }
