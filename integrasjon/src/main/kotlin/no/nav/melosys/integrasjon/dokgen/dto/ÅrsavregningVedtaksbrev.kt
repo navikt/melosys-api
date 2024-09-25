25955fc0e5a42d1a159920fa1c2606f8e6b9fda8
Francois Allix <32903762+frans-a@users.noreply.github.com>
2024-09-25 14:09:15 +0200

    Fjern ubrukt felt

--- integrasjon/src/main/kotlin/no/nav/melosys/integrasjon/dokgen/dto/ÅrsavregningVedtaksbrev.kt
+++ integrasjon/src/main/kotlin/no/nav/melosys/integrasjon/dokgen/dto/ÅrsavregningVedtaksbrev.kt
@@ -1,17 +1,10 @@
 package no.nav.melosys.integrasjon.dokgen.dto
 
-import com.fasterxml.jackson.annotation.JsonFormat
-import com.fasterxml.jackson.databind.annotation.JsonSerialize
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer
 import no.nav.melosys.domain.brev.ÅrsavregningVedtakBrevBestilling
 import no.nav.melosys.domain.kodeverk.Mottakerroller
-import java.time.LocalDate
 
 class ÅrsavregningVedtaksbrev(
     brevBestilling: ÅrsavregningVedtakBrevBestilling,
-    @JsonSerialize(using = LocalDateSerializer::class)
-    @JsonFormat(shape = JsonFormat.Shape.STRING)
-    val datoMottatt: LocalDate?,
     val innledningFritekst: String?,
     val begrunnelseFritekst: String?,
 ) : DokgenDto(brevBestilling, Mottakerroller.BRUKER) {
@@ -19,7 +12,6 @@ class ÅrsavregningVedtaksbrev(
         brevBestilling: ÅrsavregningVedtakBrevBestilling,
     ) : this(
         brevBestilling = brevBestilling,
-        datoMottatt = instantTilLocalDate(brevBestilling.forsendelseMottatt),
         innledningFritekst = brevBestilling.innledningFritekstAarsavregning,
         begrunnelseFritekst = brevBestilling.begrunnelseFritekstAarsavregning
     )
