eac0558c3806350179d1de38046ebcf3baf6b76e
RuneLind <rulind@gmail.com>
2024-10-03 14:07:26 +0200

    Legg på lag behandling test i SkattehendelserConsumerTest

--- service/src/test/kotlin/no/nav/melosys/service/avgift/aarsavregning/SkattehendelserConsumerTest.kt
+++ service/src/test/kotlin/no/nav/melosys/service/avgift/aarsavregning/SkattehendelserConsumerTest.kt
@@ -5,28 +5,28 @@ import io.kotest.matchers.shouldBe
 import io.mockk.*
 import io.mockk.impl.annotations.MockK
 import io.mockk.junit5.MockKExtension
-import no.nav.melosys.domain.*
+import no.nav.melosys.domain.Behandling
+import no.nav.melosys.domain.Behandlingsresultat
+import no.nav.melosys.domain.Fagsak
 import no.nav.melosys.domain.avgift.aarsavregning.Skattehendelse
 import no.nav.melosys.domain.avgift.Årsavregning
-import no.nav.melosys.domain.dokument.person.PersonDokument
 import no.nav.melosys.domain.kodeverk.Aktoersroller
 import no.nav.melosys.domain.kodeverk.Saksstatuser
 import no.nav.melosys.domain.kodeverk.Sakstemaer
 import no.nav.melosys.domain.kodeverk.Sakstyper
 import no.nav.melosys.domain.kodeverk.behandlinger.Behandlingsresultattyper
 import no.nav.melosys.domain.kodeverk.behandlinger.Behandlingsstatus
-import no.nav.melosys.domain.kodeverk.behandlinger.Behandlingstema
 import no.nav.melosys.domain.kodeverk.behandlinger.Behandlingstyper
 import no.nav.melosys.saksflytapi.ProsessinstansService
 import no.nav.melosys.service.avgift.TrygdeavgiftService
 import no.nav.melosys.service.behandling.BehandlingService
 import no.nav.melosys.service.behandling.BehandlingsresultatService
-import no.nav.melosys.service.persondata.PersondataService
 import no.nav.melosys.service.sak.FagsakService
 import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
+import java.util.*
 
 
 @ExtendWith(MockKExtension::class)
@@ -48,25 +48,61 @@ class SkattehendelserConsumerTest {
     private lateinit var behandslingsresultatService: BehandlingsresultatService
 
     @MockK
-    private lateinit var persondataService: PersondataService
-
-    @MockK
     private lateinit var årsavregningService: ÅrsavregningService
 
     private lateinit var skattehendelserConsumer: SkattehendelserConsumer
+
+    private val unleash = FakeUnleash().apply { enableAll() }
 
 
     @BeforeEach
     fun setUp() {
         skattehendelserConsumer = SkattehendelserConsumer(
-            mockk(),
-            FakeUnleash().apply { enableAll() },
+            prosessinstansService,
+            unleash,
             fagsakService,
             trygdeavgiftService,
             behandlingService,
             behandslingsresultatService,
             årsavregningService
         )
+    }
+
+    @Test
+    fun `lag behandling ved skatteoppgjør hendelse når vi har fagsak behandlinger med trygdeavgift`() {
+        val behandling = lagBehandling {
+            status = Behandlingsstatus.AVSLUTTET
+        }
+        val fagsak = lagFagsak {
+            this.leggTilBehandling(behandling)
+        }
+        val behandlingsresultat = Behandlingsresultat().apply {
+            this.behandling = behandling
+        }
+
+        every { trygdeavgiftService.harFagsakBehandlingerMedTrygdeavgift(SAKSNUMMER) } returns true
+        every { fagsakService.hentFagsakerMedAktør(Aktoersroller.BRUKER, AKTØR_ID) } returns listOf(fagsak)
+        every {
+            årsavregningService.hentSisteBehandlingsresultatMedInnvilgetMedlemskapsperiodeOgAvgiftsgrunnlag(
+                SAKSNUMMER,
+                GJELDER_ÅR
+            )
+        } returns behandlingsresultat
+        every { prosessinstansService.opprettArsavregningsBehandlingProsessflyt(any(), any()) } returns mockk<UUID>()
+
+
+        skattehendelserConsumer.lesSkattehendelser(
+            ConsumerRecord(
+                "topic", 1, 1, "key", Skattehendelse(
+                    gjelderPeriode = GJELDER_ÅR.toString(),
+                    identifikator = AKTØR_ID,
+                    hendelsetype = "ny"
+                )
+            )
+        )
+
+
+        verify { prosessinstansService.opprettArsavregningsBehandlingProsessflyt(SAKSNUMMER, GJELDER_ÅR.toString()) }
     }
 
 
@@ -90,38 +126,22 @@ class SkattehendelserConsumerTest {
         }
 
         every { trygdeavgiftService.harFagsakBehandlingerMedTrygdeavgift(SAKSNUMMER) } returns true
-        every { persondataService.hentAktørIdForIdent(any()) } returns AKTØR_ID
         every { fagsakService.hentFagsakerMedAktør(Aktoersroller.BRUKER, AKTØR_ID) } returns listOf(fagsak)
-        every { fagsakService.hentFagsak(SAKSNUMMER) } returns fagsak
-        every {
-            årsavregningService.hentSisteBehandlingsresultatMedInnvilgetMedlemskapsperiodeOgAvgiftsgrunnlag(
-                fagsak.saksnummer,
-                any()
-            )
-        } returns behandlingsresultat
-
         every { behandslingsresultatService.hentBehandlingsresultat(behandling.id) } returns behandlingsresultat
-        every { årsavregningService.opprettÅrsavregning(any(), any()) } returns ÅrsavregningModel(
-            2023,
-            null,
-            emptyList(),
-            null,
-            emptyList(),
-            null,
-            null,
-            null
-        )
         val behandlingSlot = slot<Behandling>()
         every { behandlingService.lagre(capture(behandlingSlot)) } returns Unit
 
-        val consumerRecord = ConsumerRecord(
-            "topic", 1, 1, "key", Skattehendelse(
-                gjelderPeriode = GJELDER_ÅR.toString(),
-                identifikator = AKTØR_ID,
-                hendelsetype = "ny"
+
+        skattehendelserConsumer.lesSkattehendelser(
+            ConsumerRecord(
+                "topic", 1, 1, "key", Skattehendelse(
+                    gjelderPeriode = GJELDER_ÅR.toString(),
+                    identifikator = AKTØR_ID,
+                    hendelsetype = "ny"
+                )
             )
         )
-        skattehendelserConsumer.lesSkattehendelser(consumerRecord)
+
 
         verify(exactly = 0) { prosessinstansService.opprettArsavregningsBehandlingProsessflyt(any(), any()) }
         verify { behandlingService.lagre(behandling) }
@@ -134,7 +154,6 @@ class SkattehendelserConsumerTest {
         val fagsak = lagFagsak()
 
         every { trygdeavgiftService.harFagsakBehandlingerMedTrygdeavgift(SAKSNUMMER) } returns true
-        every { persondataService.hentAktørIdForIdent(any()) } returns AKTØR_ID
         every { fagsakService.hentFagsakerMedAktør(Aktoersroller.BRUKER, AKTØR_ID) } returns listOf(fagsak)
         every { fagsakService.hentFagsak(SAKSNUMMER) } returns fagsak
         every {
@@ -144,74 +163,37 @@ class SkattehendelserConsumerTest {
             )
         } returns null
 
-        val consumerRecord = ConsumerRecord(
-            "topic", 1, 1, "key", Skattehendelse(
-                gjelderPeriode = GJELDER_ÅR.toString(),
-                identifikator = AKTØR_ID,
-                hendelsetype = "ny"
+
+        skattehendelserConsumer.lesSkattehendelser(
+            ConsumerRecord(
+                "topic", 1, 1, "key", Skattehendelse(
+                    gjelderPeriode = GJELDER_ÅR.toString(),
+                    identifikator = AKTØR_ID,
+                    hendelsetype = "ny"
+                )
             )
         )
-        skattehendelserConsumer.lesSkattehendelser(consumerRecord)
 
 
-        verify(exactly = 0) { prosessinstansService.opprettArsavregningsBehandlingProsessflyt(any(), any()) }
-        verify(exactly = 0) { behandlingService.nyBehandling(any(), any(), any(), any(), any(), any(), any(), any(), any()) }
-        verify(exactly = 0) { årsavregningService.opprettÅrsavregning(any(), any()) }
+        verify { prosessinstansService wasNot Called }
+        verify { behandlingService wasNot Called }
     }
 
 
-    private fun lagFagsak(block: Fagsak.() -> Unit = {}): Fagsak = Fagsak(
+    private fun lagFagsak(block: Fagsak.() -> Unit = {}) = Fagsak(
         SAKSNUMMER,
         123L,
         Sakstyper.EU_EOS,
         Sakstemaer.MEDLEMSKAP_LOVVALG,
         Saksstatuser.OPPRETTET,
-        mutableSetOf(lagBruker()),
+        mutableSetOf(),
         mutableListOf()
     ).apply {
         block()
     }
 
-    fun lagBruker(): Aktoer {
-        val aktoer = Aktoer()
-        aktoer.rolle = Aktoersroller.BRUKER
-        aktoer.aktørId = "aktørID"
-        return aktoer
-    }
-
-    private fun lagBehandling(block: Behandling.() -> Unit = {}): Behandling = lagBehandling().apply {
+    private fun lagBehandling(block: Behandling.() -> Unit = {}) = Behandling().apply {
         block()
-    }
-
-    fun lagBehandling(): Behandling {
-        val behandling = Behandling()
-        behandling.id = 1L
-        behandling.type = Behandlingstyper.FØRSTEGANG
-        behandling.tema = Behandlingstema.UTSENDT_ARBEIDSTAKER
-        behandling.fagsak = lagFagsak()
-        behandling.saksopplysninger = setOf(lagPersonopplysning())
-        return behandling
-    }
-
-    fun lagPersonopplysning(): Saksopplysning {
-        val saksopplysning = Saksopplysning()
-        saksopplysning.type = SaksopplysningType.PERSOPL
-        val personDokument = PersonDokument()
-        personDokument.fnr = "99887766554"
-        saksopplysning.dokument = personDokument
-        return saksopplysning
-    }
-
-    fun lagFagsak(saksnummer: String?): Fagsak {
-        return Fagsak(
-            saksnummer!!,
-            123L,
-            Sakstyper.EU_EOS,
-            Sakstemaer.MEDLEMSKAP_LOVVALG,
-            Saksstatuser.OPPRETTET,
-            mutableSetOf(lagBruker()),
-            mutableListOf()
-        )
     }
 
     companion object {
