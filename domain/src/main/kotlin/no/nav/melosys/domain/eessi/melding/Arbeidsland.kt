d5dccc840a5a182ab6ffa950f92243947a23a2bd
RuneLind <rulind@gmail.com>
2024-10-22 14:37:51 +0200

    Takle null i arbeidssted liste når vi motar kafka MelosysEessiMelding

--- domain/src/main/kotlin/no/nav/melosys/domain/eessi/melding/Arbeidsland.kt
+++ domain/src/main/kotlin/no/nav/melosys/domain/eessi/melding/Arbeidsland.kt
@@ -5,6 +5,6 @@ import com.fasterxml.jackson.annotation.JsonProperty
 
 data class Arbeidsland @JsonCreator(mode = JsonCreator.Mode.PROPERTIES) constructor(
     @JsonProperty("land") val land: String,
-    @JsonProperty("arbeidssted") val arbeidssted: List<Arbeidssted>,
+    @JsonProperty("arbeidssted") val arbeidssted: List<Arbeidssted?> = emptyList(),
 ) {
 }
