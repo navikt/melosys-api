fea320dd298c33e105fceaf28b3aed80617a60ea
RuneLind <rulind@gmail.com>
2024-10-28 20:10:14 +0100

    Legg til test for gyldig json

--- integrasjonstest/src/test/kotlin/no/nav/melosys/itest/EessiMeldingConsumerIT.kt
+++ integrasjonstest/src/test/kotlin/no/nav/melosys/itest/EessiMeldingConsumerIT.kt
@@ -1,11 +1,13 @@
 package no.nav.melosys.itest
 
 import io.kotest.matchers.collections.shouldHaveSize
+import io.kotest.matchers.nulls.shouldNotBeNull
 import io.kotest.matchers.shouldBe
-import io.kotest.matchers.string.shouldContain
 import io.mockk.every
 import io.mockk.mockk
+import io.mockk.slot
 import no.nav.melosys.LoggingTestUtils
+import no.nav.melosys.domain.eessi.melding.MelosysEessiMelding
 import no.nav.melosys.integrasjon.kafka.KafkaConfig
 import no.nav.melosys.saksflytapi.ProsessinstansService
 import no.nav.melosys.service.eessi.kafka.EessiMeldingConsumer
@@ -20,6 +22,7 @@ import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Bean
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.kafka.test.context.EmbeddedKafka
+import org.springframework.test.annotation.DirtiesContext
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.ContextConfiguration
 import java.util.UUID
@@ -36,45 +39,71 @@ import java.util.concurrent.TimeUnit
     classes = [
         JacksonAutoConfiguration::class,
         KafkaAutoConfiguration::class,
-        EessiMeldingConsumerIT.Config::class,
+        EessiMeldingConsumerIT.TestConfig::class,
         EessiMeldingConsumer::class,
         KafkaTestConfig::class,
         KafkaConfig::class
     ]
 )
 class EessiMeldingConsumerIT(
-    @Autowired @Qualifier("melosysEessiMeldingString") private val melosysEessiMeldingKafkaTemplateString: KafkaTemplate<String, String>
+    @Autowired @Qualifier("melosysEessiMeldingString") private val melosysEessiMeldingKafkaTemplateString: KafkaTemplate<String, String>,
+    @Autowired private val testConfig: TestConfig
 ) {
 
     @TestConfiguration
-    class Config {
+    class TestConfig {
+        private val melosysEessiMeldingSlot = slot<MelosysEessiMelding>()
+
+        fun capturedMelosysEessiMelding() = melosysEessiMeldingSlot.captured
+
+        fun isCaptured() = melosysEessiMeldingSlot.isCaptured
+
         @Bean
         fun prosessinstansService() = mockk<ProsessinstansService>(relaxed = true).also {
-            every { it.opprettProsessinstansSedMottak(any()) } returns mockk<UUID>()
+            every { it.opprettProsessinstansSedMottak(capture(melosysEessiMeldingSlot)) } returns mockk<UUID>()
         }
     }
 
-    val jsonMelosysEessiMeldingMedFeil = """
+    @Test
+    @DirtiesContext
+    fun `mottak av gyldig json på kafka kø skal fungere`() {
+        val jsonMelosysEessiMelding = """
+            {
+                "sedId": "sedId"
+            }
+        """
+        melosysEessiMeldingKafkaTemplateString.send("teammelosys.eessi.v1-local", jsonMelosysEessiMelding)
+
+        await.atMost(5, TimeUnit.SECONDS).until {
+            testConfig.isCaptured()
+        }
+        testConfig.capturedMelosysEessiMelding().sedId shouldBe "sedId"
+    }
+
+    @Test
+    @DirtiesContext
+    fun `mottak av ugyldig json på kafka kø skal feile å logge`() {
+        val jsonMelosysEessiMeldingMedFeil = """
             {
                 "sedId": "sedId",
                 "avsender": []
             }
         """
 
-    @Test
-    fun `mottak av ugyldig json på kafka kø skal feile å logge`() {
         melosysEessiMeldingKafkaTemplateString.send("teammelosys.eessi.v1-local", jsonMelosysEessiMeldingMedFeil)
 
         LoggingTestUtils.withLogCapture { logs ->
             await.atMost(5, TimeUnit.SECONDS).untilAsserted {
                 logs.shouldHaveSize(2)
             }
-
-            logs.map { it.formattedMessage }.run {
-                get(0) shouldBe  ("Failed to deserialize message on topic teammelosys.eessi.v1-local: $jsonMelosysEessiMeldingMedFeil")
-                get(1) shouldContain ("Consumer exception")
+            logs.run {
+                get(0).formattedMessage shouldBe "Failed to deserialize message on topic teammelosys.eessi.v1-local: $jsonMelosysEessiMeldingMedFeil"
+                get(1).run {
+                    formattedMessage shouldBe "Consumer exception"
+                    throwableProxy.shouldNotBeNull()
+                        .message shouldBe "Stopped container"
+                }
             }
         }
     }
-
 }
