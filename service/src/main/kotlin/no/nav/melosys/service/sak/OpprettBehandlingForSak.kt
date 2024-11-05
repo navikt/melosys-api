ffa74211659ef297928142da114b24e64d7ca8d4
Thang Phan <thang.quoc.ly.phan@nav.no>
2024-11-05 13:32:28 +0100

    Rydder opp

--- service/src/main/kotlin/no/nav/melosys/service/sak/OpprettBehandlingForSak.kt
+++ service/src/main/kotlin/no/nav/melosys/service/sak/OpprettBehandlingForSak.kt
@@ -36,7 +36,7 @@ class OpprettBehandlingForSak(
             behandlingstype!!
         )
 
-        if (sistBehandling.erAktiv() && !sistBehandling.erÅrsavregning() && behandlingstype != Behandlingstyper.ÅRSAVREGNING) {
+        if (sistBehandling.erAktiv() && behandlingstype != Behandlingstyper.ÅRSAVREGNING) {
             behandlingService.avsluttBehandling(sistBehandling.id)
         }
 
--- service/src/main/kotlin/no/nav/melosys/service/saksbehandling/SaksbehandlingRegler.kt
+++ service/src/main/kotlin/no/nav/melosys/service/saksbehandling/SaksbehandlingRegler.kt
@@ -42,7 +42,7 @@ class SaksbehandlingRegler(private val behandlingsresultatRepository: Behandling
         behandlinger
             .filter { it.erInaktiv() }
             .filter { !harIngenFlyt(it) }
-            .filter { !it.erÅrsavregning() }
+            .filterNot { it.erÅrsavregning() }
             .firstOrNull {
                 val behandlingsresultat = behandlingsresultatRepository.findById(it.id)
                 behandlingstyperSomKanReplikeres.contains(it.type)
