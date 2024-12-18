097b57e08d09f642ddf33fd703e52946285981db
Isa Salem Hame <isa.salem.hame@nav.no>
2024-12-18 06:37:54 +0100

    Send EFTA brev ved vedtak

--- config/src/main/kotlin/no/nav/melosys/featuretoggle/ToggleName.kt
+++ config/src/main/kotlin/no/nav/melosys/featuretoggle/ToggleName.kt
@@ -7,6 +7,7 @@ object ToggleName {
     const val MELOSYS_SEND_MELDING_OM_VEDTAK = "melosys.send_melding_om_vedtak"
     const val MELOSYS_KONVENSJON_EFTA_LAND_OG_STORBRITANNIA = "melosys.konvensjon.efta.land.og.storbritannia"
     const val MELOSYS_ARBEID_KUN_NORGE = "melosys.arbeid.kun.norge"
+    const val MELOSYS_11_3_A_NORGE_ER_UTPEKT = "melosys.11_3_a_Norge_er_utpekt"
     const val MELOSYS_HINDRE_JOURNALFOERING_AV_FERDIGSTILTE_JOURNALPOSTER = "melosys.hindre-journalfoering-av-ferdigstilte-journalposter"
     const val MELOSYS_SKATTEHENDELSE_CONSUMER = "melosys.skattehendelse.consumer"
     const val MELOSYS_ÅRSAVREGNING = "melosys.arsavregning"
