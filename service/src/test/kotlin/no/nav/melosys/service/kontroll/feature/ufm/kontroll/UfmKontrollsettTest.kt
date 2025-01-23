a5f06eebe9e2f93ad3984f2199f4b0276db33e93
Thang Phan <thang.quoc.ly.phan@nav.no>
2025-01-23 15:44:55 +0100

    6395 Fjerner CDM 4.3 toggle

--- service/src/test/kotlin/no/nav/melosys/service/kontroll/feature/ufm/kontroll/UfmKontrollsettTest.kt
+++ service/src/test/kotlin/no/nav/melosys/service/kontroll/feature/ufm/kontroll/UfmKontrollsettTest.kt
@@ -10,63 +10,35 @@ class UfmKontrollsettTest {
     @Test
     fun hentKontrollerA001_verifiserKontroller() {
         val kontroller =
-            UfmKontrollsett.hentRegelsettForSedType(SedType.A001, false)
+            UfmKontrollsett.hentRegelsettForSedType(SedType.A001)
         Assertions.assertThat(kontroller).hasSize(10)
     }
 
     @Test
     fun hentKontrollerA003_verifiserKontroller() {
         val kontroller =
-            UfmKontrollsett.hentRegelsettForSedType(SedType.A003, false)
+            UfmKontrollsett.hentRegelsettForSedType(SedType.A003)
         Assertions.assertThat(kontroller).hasSize(11)
     }
 
     @Test
     fun hentKontrollerA009_verifiserKontroller() {
         val kontroller =
-            UfmKontrollsett.hentRegelsettForSedType(SedType.A009, false)
+            UfmKontrollsett.hentRegelsettForSedType(SedType.A009)
         Assertions.assertThat(kontroller).hasSize(10)
     }
 
     @Test
     fun hentKontrollerA010_verifiserKontroller() {
         val kontroller =
-            UfmKontrollsett.hentRegelsettForSedType(SedType.A010, false)
-        Assertions.assertThat(kontroller).hasSize(10)
-    }
-
-    @Test
-    fun hentKontrollerA001_CDM4_3_verifiserKontroller() {
-        val kontroller =
-            UfmKontrollsett.hentRegelsettForSedType(SedType.A001, true)
-        Assertions.assertThat(kontroller).hasSize(10)
-    }
-
-    @Test
-    fun hentKontrollerA003_CDM4_3_verifiserKontroller() {
-        val kontroller =
-            UfmKontrollsett.hentRegelsettForSedType(SedType.A003, true)
-        Assertions.assertThat(kontroller).hasSize(11)
-    }
-
-    @Test
-    fun hentKontrollerA009_CDM4_3_verifiserKontroller() {
-        val kontroller =
-            UfmKontrollsett.hentRegelsettForSedType(SedType.A009, true)
-        Assertions.assertThat(kontroller).hasSize(10)
-    }
-
-    @Test
-    fun hentKontrollerA010_CDM4_3_verifiserKontroller() {
-        val kontroller =
-            UfmKontrollsett.hentRegelsettForSedType(SedType.A010, true)
+            UfmKontrollsett.hentRegelsettForSedType(SedType.A010)
         Assertions.assertThat(kontroller).hasSize(10)
     }
 
     @Test
     fun hentKontrollerA008_verifiserIngenKontroller() {
         Assertions.assertThatExceptionOfType(UnsupportedOperationException::class.java)
-            .isThrownBy { UfmKontrollsett.hentRegelsettForSedType(SedType.A008, false) }
+            .isThrownBy { UfmKontrollsett.hentRegelsettForSedType(SedType.A008) }
             .withMessageContaining("A008 er ikke støttet")
     }
 }
