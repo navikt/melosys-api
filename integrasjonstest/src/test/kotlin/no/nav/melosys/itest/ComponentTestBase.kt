ee3f5cf292f7f6590a3108e3772dff4c6ef67eea
RuneLind <rulind@gmail.com>
2025-01-27 20:45:20 +0100

    Flytt ComponentTestConfig ting inn i ComponentTestBase

--- integrasjonstest/src/test/kotlin/no/nav/melosys/itest/ComponentTestBase.kt
+++ integrasjonstest/src/test/kotlin/no/nav/melosys/itest/ComponentTestBase.kt
@@ -2,6 +2,8 @@ package no.nav.melosys.itest
 
 import io.getunleash.FakeUnleash
 import no.nav.melosys.Application
+import no.nav.melosys.melosysmock.config.GraphqlConfig
+import no.nav.melosys.melosysmock.config.SoapConfig
 import no.nav.melosys.melosysmock.medl.MedlRepo
 import no.nav.melosys.melosysmock.melosyseessi.MelosysEessiRepo
 import no.nav.melosys.melosysmock.sak.SakRepo
@@ -10,7 +12,10 @@ import org.junit.jupiter.api.AfterEach
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Import
+import org.springframework.context.annotation.Primary
 import org.springframework.kafka.test.context.EmbeddedKafka
 import org.springframework.test.annotation.DirtiesContext
 import org.springframework.test.context.ActiveProfiles
@@ -27,12 +32,18 @@ import org.springframework.test.context.ActiveProfiles
         "teammelosys.fattetvedtak.v1-local", "teammelosys.manglende-fakturabetaling-local", "teammelosys.melosys-hendelse-local"],
     brokerProperties = ["offsets.topic.replication.factor=1", "transaction.state.log.replication.factor=1", "transaction.state.log.min.isr=1"]
 )
-@Import(
-    ComponentTestConfig::class
-)
+@Import(GraphqlConfig::class, SoapConfig::class, KafkaTestConfig::class, ComponentTestBase.TestConfig::class)
 @DirtiesContext
 @EnableMockOAuth2Server
 class ComponentTestBase : OracleTestContainerBase() {
+
+    @TestConfiguration
+    class TestConfig {
+        // TODO: vi kan fjerne denne og bruke FeatureToggleConfig som bruker enable all som vi vil så på etter rydding
+        @Primary
+        @Bean
+        fun fakeUnleash(): FakeUnleash = FakeUnleash()
+    }
 
     @Autowired
     protected lateinit var unleash: FakeUnleash
