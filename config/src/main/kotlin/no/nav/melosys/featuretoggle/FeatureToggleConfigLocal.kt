22716bad2d9e7b98a82fb39ee561e3de3c14e956
RuneLind <rulind@gmail.com>
2025-01-28 15:42:56 +0100

    Del FeatureToggleConfig i 3 konfig klasser
    Så vi slipper og ha test kode i prod

---
+++ config/src/main/kotlin/no/nav/melosys/featuretoggle/FeatureToggleConfigLocal.kt
@@ -0,0 +1,18 @@
+package no.nav.melosys.featuretoggle
+
+import io.getunleash.Unleash
+import mu.KotlinLogging
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
+
+private val log = KotlinLogging.logger { }
+
+@Configuration
+@Profile("!nais & !test")
+class FeatureToggleConfigLocal {
+    @Bean
+    fun unleash(): Unleash = LocalUnleash().apply { enableAll() }.also {
+        log.info { "FeatureToggleConfigLocal er aktivert med ${it.javaClass.simpleName}" }
+    }
+}
