04aca81340465b64064513b541e78abe4121a089
Thang Phan <thang.quoc.ly.phan@nav.no>
2025-01-30 10:00:31 +0100

    NO_JIRA DUNLEASH melosys.ftrl.yrkesskadefordel fjern toggle

--- config/src/main/kotlin/no/nav/melosys/featuretoggle/ToggleName.kt
+++ config/src/main/kotlin/no/nav/melosys/featuretoggle/ToggleName.kt
@@ -2,7 +2,6 @@ package no.nav.melosys.featuretoggle
 
 object ToggleName {
     const val BEHANDLINGSTYPE_KLAGE = "melosys.behandlingstype.klage"
-    const val MELOSYS_FTRL_YRKESSKADEFORDEL = "melosys.ftrl.yrkesskadefordel"
     const val MELOSYS_SEND_MELDING_OM_VEDTAK = "melosys.send_melding_om_vedtak"
     const val MELOSYS_11_3_A_NORGE_ER_UTPEKT = "melosys.11_3_a_Norge_er_utpekt"
     const val MELOSYS_HINDRE_JOURNALFOERING_AV_FERDIGSTILTE_JOURNALPOSTER = "melosys.hindre-journalfoering-av-ferdigstilte-journalposter"
