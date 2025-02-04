f8e44825fc7d70f887581e1f4618030e1c25d3d1
Eirik Bergande <eirik.bergande@nav.no>
2025-02-04 15:49:08 +0100

    6959 qa

--- service/src/test/kotlin/no/nav/melosys/service/avgift/satsendring/Testconfig.kt
+++ service/src/test/kotlin/no/nav/melosys/service/avgift/satsendring/Testconfig.kt
@@ -2,10 +2,8 @@ package no.nav.melosys.service.avgift.satsendring
 
 import org.springframework.boot.SpringBootConfiguration
 import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
 
 @SpringBootConfiguration
-@Configuration
 class TestConfig {
     @Bean
     fun satsendringAdminController(satsendringFinner: SatsendringFinner): SatsendringAdminController {
