a8500ab7f69ef75b6141d2aa9e789a63949cb422
Thang Phan <thang.quoc.ly.phan@nav.no>
2025-02-13 14:47:43 +0100

    7106 TOGGLE Vilkår og bestemmelser for pensjonister

---
+++ service/src/main/kotlin/no/nav/melosys/service/ftrl/bestemmelse/vilkaar/VilkårForBestemmelsePensjonist.kt
@@ -0,0 +1,122 @@
+package no.nav.melosys.service.ftrl.bestemmelse.vilkaar
+
+import no.nav.melosys.domain.kodeverk.Avklartefaktatyper
+import no.nav.melosys.domain.kodeverk.Bestemmelse
+import no.nav.melosys.domain.kodeverk.Folketrygdloven_kap2_bestemmelser.*
+import no.nav.melosys.domain.kodeverk.Ikkeyrkesaktivrelasjontype
+import no.nav.melosys.domain.kodeverk.Land_iso2
+import no.nav.melosys.domain.kodeverk.Vilkaar.*
+import no.nav.melosys.domain.mottatteopplysninger.data.Soeknadsland
+import no.nav.melosys.exception.FunksjonellException
+import no.nav.melosys.service.mottatteopplysninger.MottatteOpplysningerService
+import org.springframework.stereotype.Component
+
+@Component
+class VilkårForBestemmelsePensjonist(val mottatteOpplysningerService: MottatteOpplysningerService) {
+    fun hentVilkår(
+        bestemmelse: Bestemmelse,
+        avklarteFakta: Map<Avklartefaktatyper, String>,
+        behandlingID: Long?
+    ): List<Vilkår> {
+        return when (bestemmelse) {
+            FTRL_KAP2_2_1 -> ftrlKap2_1VilkårForBehandling(behandlingID)
+
+            FTRL_KAP2_2_5_ANDRE_LEDD -> ftrlKap2_5VilkårForAvklarteFakta(avklarteFakta)
+
+            FTRL_KAP2_2_7_FØRSTE_LEDD -> listOf(
+                Vilkår(FTRL_2_1A_TRYGDEKOORDINGERING),
+                Vilkår(FTRL_2_7_IKKE_PLIKTIG_MEDLEM),
+                Vilkår(FTRL_2_7_RIMELIGHETSVURDERING)
+            )
+
+            FTRL_KAP2_2_7_FJERDE_LEDD -> listOf(
+                Vilkår(FTRL_2_1A_TRYGDEKOORDINGERING),
+                Vilkår(FTRL_2_7_FORSØRGET_FAMILIEMEDLEM),
+                Vilkår(FTRL_2_7_INGEN_SÆRLIGE_GRUNNER_TALER_IMOT)
+            )
+
+            FTRL_KAP2_2_8_FØRSTE_LEDD_D -> listOf(
+                Vilkår(FTRL_2_1A_TRYGDEKOORDINGERING),
+                Vilkår(FTRL_2_8_PENSJON_UFØRETRYGD_FOLKETRYGDEN),
+                Vilkår(FTRL_2_8_PENSJONIST_TRETTI_ÅR_TRYGDETID),
+                Vilkår(FTRL_2_8_PENSJONIST_TI_ÅR_TRYGDETID_FØR_SØKNADSTIDSPUNKT),
+                Vilkår(FTRL_2_8_NÆR_TILKNYTNING_NORGE)
+            )
+
+            FTRL_KAP2_2_8_ANDRE_LEDD -> listOf(
+                Vilkår(FTRL_2_1A_TRYGDEKOORDINGERING),
+                Vilkår(FTRL_FORUTGÅENDE_TRYGDETID),
+                Vilkår(FTRL_2_8_NÆR_TILKNYTNING_NORGE)
+            )
+
+            FTRL_KAP2_2_8_FJERDE_LEDD -> listOf(
+                Vilkår(FTRL_2_1A_TRYGDEKOORDINGERING),
+                Vilkår(FTRL_2_8_FORSØRGET_FAMILIEMEDLEM),
+                Vilkår(FTRL_FORUTGÅENDE_TRYGDETID),
+                Vilkår(FTRL_2_8_NÆR_TILKNYTNING_NORGE),
+            )
+
+            else -> emptyList()
+        }
+    }
+
+    private fun ftrlKap2_1VilkårForBehandling(behandlingID: Long?): List<Vilkår> {
+        if (behandlingID == null) {
+            throw FunksjonellException("BehandlingID trengs for å avgjøre land for ftrlKap2_1")
+        }
+        val mottatteOpplysninger = mottatteOpplysningerService.hentMottatteOpplysninger(behandlingID)
+        val vilkårForLand = ftrlKap2_1VilkårForLand(mottatteOpplysninger.mottatteOpplysningerData?.soeknadsland)
+
+        return vilkårForLand + Vilkår(FTRL_2_1_LOVLIG_OPPHOLD)
+    }
+
+    private fun ftrlKap2_1VilkårForLand(søknadsland: Soeknadsland?): List<Vilkår> {
+        if (søknadsland == null) {
+            return emptyList()
+        }
+
+        // Ett eller flere land utenfor Norge. Vi legger alltid til grunn at det gjelder opphold i utlandet ved flere land.
+        return if (søknadsland.landkoder.any { it != Land_iso2.NO.toString() } || søknadsland.isFlereLandUkjentHvilke) {
+            listOf(
+                Vilkår(FTRL_2_1_BOSATT_NORGE_FORUT),
+                Vilkår(FTRL_2_1_OPPHOLD_UNDER_12MND)
+            )
+        } else {
+            listOf(
+                Vilkår(FTRL_2_1_BOSATT_NORGE)
+            )
+        }
+    }
+
+    private fun ftrlKap2_5VilkårForAvklarteFakta(avklarteFakta: Map<Avklartefaktatyper, String>): List<Vilkår> {
+        val familieRelasjon = hentFamilieRelasjonFraFakta(avklarteFakta)
+        return when (familieRelasjon) {
+            Ikkeyrkesaktivrelasjontype.BARN_2_5_ANDRE_LEDD -> listOf(
+                Vilkår(FTRL_2_5_MEDFØLGENDE_A_E),
+                Vilkår(FTRL_2_5_FORSØRGET_FAMILIEMEDLEM)
+            )
+
+            Ikkeyrkesaktivrelasjontype.EKTEFELLE_2_5_ANDRE_LEDD_A_TIL_B -> listOf(
+                Vilkår(FTRL_2_5_MEDFØLGENDE_A_E, defaultOppfylt = true),
+                Vilkår(FTRL_2_5_FORSØRGET_FAMILIEMEDLEM),
+                Vilkår(FTRL_2_5_NORSK_STATSBORGER_EØS_BORGER)
+            )
+
+            Ikkeyrkesaktivrelasjontype.EKTEFELLE_2_5_ANDRE_LEDD_C_TIL_E -> listOf(
+                Vilkår(FTRL_2_5_MEDFØLGENDE_A_E, defaultOppfylt = true),
+                Vilkår(FTRL_2_5_FORSØRGET_FAMILIEMEDLEM),
+                Vilkår(FTRL_FORUTGÅENDE_TRYGDETID)
+            )
+
+            else -> emptyList()
+        }
+    }
+
+    private fun hentFamilieRelasjonFraFakta(avklarteFakta: Map<Avklartefaktatyper, String>): Ikkeyrkesaktivrelasjontype {
+        val avklarteFamilieRelasjon = avklarteFakta[Avklartefaktatyper.IKKE_YRKESAKTIV_RELASJON]
+        if (avklarteFamilieRelasjon == null || !Ikkeyrkesaktivrelasjontype.values().any { it.name == avklarteFamilieRelasjon }) {
+            throw FunksjonellException("FamilieRelasjon $avklarteFamilieRelasjon er ugyldig")
+        }
+        return Ikkeyrkesaktivrelasjontype.valueOf(avklarteFamilieRelasjon)
+    }
+}
---
+++ service/src/test/kotlin/no/nav/melosys/service/ftrl/bestemmelse/vilkaar/VilkårForBestemmelsePensjonistTest.kt
@@ -0,0 +1,280 @@
+package no.nav.melosys.service.ftrl.bestemmelse.vilkaar
+
+import io.kotest.matchers.collections.shouldContainExactly
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.junit5.MockKExtension
+import no.nav.melosys.domain.kodeverk.*
+import no.nav.melosys.domain.kodeverk.Vilkaar.*
+import no.nav.melosys.domain.mottatteopplysninger.MottatteOpplysninger
+import no.nav.melosys.domain.mottatteopplysninger.SøknadNorgeEllerUtenforEØS
+import no.nav.melosys.domain.mottatteopplysninger.data.Soeknadsland
+import no.nav.melosys.service.mottatteopplysninger.MottatteOpplysningerService
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+
+@ExtendWith(MockKExtension::class)
+class VilkårForBestemmelsePensjonistTest {
+    @MockK
+    private lateinit var mottatteOpplysningerService: MottatteOpplysningerService
+
+    private lateinit var vilkårForBestemmelse: VilkårForBestemmelsePensjonist
+
+    @BeforeEach
+    fun setUp() {
+        vilkårForBestemmelse = VilkårForBestemmelsePensjonist(mottatteOpplysningerService)
+    }
+
+    @Test
+    fun `vilkår for FTRL_KAP2_2_1, ett eller flere land utenfor Norge`() {
+        val mottatteOpplysninger =
+            MottatteOpplysninger().apply {
+                mottatteOpplysningerData =
+                    SøknadNorgeEllerUtenforEØS().apply { soeknadsland = Soeknadsland().apply { landkoder = listOf(
+                        Land_iso2.NO.toString(), "AB", "PR") } }
+            }
+        every {mottatteOpplysningerService.hentMottatteOpplysninger(1L)} returns mottatteOpplysninger
+
+
+        val vilkår = vilkårForBestemmelse.hentVilkår(
+            Folketrygdloven_kap2_bestemmelser.FTRL_KAP2_2_1,
+            mapOf(Avklartefaktatyper.IKKE_YRKESAKTIV_FTRL_2_1_OPPHOLD to Ikkeyrkesaktivoppholdtype.MIDLERTIDIG_2_1_FJERDE_LEDD.name),
+            1L
+        )
+
+
+        vilkår.shouldContainExactly(
+            Vilkår(
+                FTRL_2_1_BOSATT_NORGE_FORUT
+            ),
+            Vilkår(
+                FTRL_2_1_OPPHOLD_UNDER_12MND
+            ),
+            Vilkår(
+                FTRL_2_1_LOVLIG_OPPHOLD
+            ),
+        )
+    }
+
+    @Test
+    fun `vilkår for FTRL_KAP2_2_1, kun Norge`() {
+        val mottatteOpplysninger =
+            MottatteOpplysninger().apply {
+                mottatteOpplysningerData =
+                    SøknadNorgeEllerUtenforEØS().apply { soeknadsland = Soeknadsland().apply { landkoder = listOf(
+                        Land_iso2.NO.toString()) } }
+            }
+        every {mottatteOpplysningerService.hentMottatteOpplysninger(1L)} returns mottatteOpplysninger
+
+
+        val vilkår = vilkårForBestemmelse.hentVilkår(
+            Folketrygdloven_kap2_bestemmelser.FTRL_KAP2_2_1,
+            emptyMap(),
+            1L
+        )
+
+
+        vilkår.shouldContainExactly(
+            Vilkår(
+                FTRL_2_1_BOSATT_NORGE
+            ),
+            Vilkår(
+                FTRL_2_1_LOVLIG_OPPHOLD
+            ),
+        )
+    }
+
+    @Test
+    fun `vilkår for FTRL_KAP2_2_5_ANDRE_LEDD, barn`() {
+        val vilkår = vilkårForBestemmelse.hentVilkår(
+            Folketrygdloven_kap2_bestemmelser.FTRL_KAP2_2_5_ANDRE_LEDD,
+            mapOf(Avklartefaktatyper.IKKE_YRKESAKTIV_RELASJON to Ikkeyrkesaktivrelasjontype.BARN_2_5_ANDRE_LEDD.name),
+            1L
+        )
+
+
+        vilkår.shouldContainExactly(
+            Vilkår(
+                FTRL_2_5_MEDFØLGENDE_A_E,
+            ),
+            Vilkår(
+                FTRL_2_5_FORSØRGET_FAMILIEMEDLEM
+            ),
+        )
+    }
+
+    @Test
+    fun `vilkår for FTRL_KAP2_2_5_ANDRE_LEDD, ektefelle a til b`() {
+        val vilkår = vilkårForBestemmelse.hentVilkår(
+            Folketrygdloven_kap2_bestemmelser.FTRL_KAP2_2_5_ANDRE_LEDD,
+            mapOf(Avklartefaktatyper.IKKE_YRKESAKTIV_RELASJON to Ikkeyrkesaktivrelasjontype.EKTEFELLE_2_5_ANDRE_LEDD_A_TIL_B.name),
+            1L
+        )
+
+
+        vilkår.shouldContainExactly(
+            Vilkår(
+                FTRL_2_5_MEDFØLGENDE_A_E,
+                defaultOppfylt = true
+            ),
+            Vilkår(
+                FTRL_2_5_FORSØRGET_FAMILIEMEDLEM
+            ),
+            Vilkår(
+                FTRL_2_5_NORSK_STATSBORGER_EØS_BORGER
+            ),
+        )
+    }
+
+    @Test
+    fun `vilkår for FTRL_KAP2_2_5_ANDRE_LEDD, ektefelle c til e`() {
+        val vilkår = vilkårForBestemmelse.hentVilkår(
+            Folketrygdloven_kap2_bestemmelser.FTRL_KAP2_2_5_ANDRE_LEDD,
+            mapOf(Avklartefaktatyper.IKKE_YRKESAKTIV_RELASJON to Ikkeyrkesaktivrelasjontype.EKTEFELLE_2_5_ANDRE_LEDD_C_TIL_E.name),
+            1L
+        )
+
+
+        vilkår.shouldContainExactly(
+            Vilkår(
+                FTRL_2_5_MEDFØLGENDE_A_E,
+                defaultOppfylt = true
+            ),
+            Vilkår(
+                FTRL_2_5_FORSØRGET_FAMILIEMEDLEM
+            ),
+            Vilkår(
+                FTRL_FORUTGÅENDE_TRYGDETID
+            ),
+        )
+    }
+
+    @Test
+    fun `vilkår for FTRL_KAP2_2_7_FØRSTE_LEDD`() {
+        val vilkår = vilkårForBestemmelse.hentVilkår(
+            Folketrygdloven_kap2_bestemmelser.FTRL_KAP2_2_7_FØRSTE_LEDD,
+            emptyMap(),
+            1L
+        )
+
+
+        vilkår.shouldContainExactly(
+            Vilkår(
+                FTRL_2_1A_TRYGDEKOORDINGERING
+            ),
+            Vilkår(
+                FTRL_2_7_IKKE_PLIKTIG_MEDLEM
+            ),
+            Vilkår(
+                FTRL_2_7_RIMELIGHETSVURDERING
+            ),
+        )
+    }
+
+    @Test
+    fun `vilkår for FTRL_KAP2_2_7_FJERDE_LEDD`() {
+        val vilkår = vilkårForBestemmelse.hentVilkår(
+            Folketrygdloven_kap2_bestemmelser.FTRL_KAP2_2_7_FJERDE_LEDD,
+            emptyMap(),
+            1L
+        )
+
+
+        vilkår.shouldContainExactly(
+            Vilkår(
+                FTRL_2_1A_TRYGDEKOORDINGERING
+            ),
+            Vilkår(
+                FTRL_2_7_FORSØRGET_FAMILIEMEDLEM
+            ),
+            Vilkår(
+                FTRL_2_7_INGEN_SÆRLIGE_GRUNNER_TALER_IMOT
+            ),
+        )
+    }
+
+    @Test
+    fun `vilkår for FTRL_KAP2_2_8_FØRSTE_LEDD_D`() {
+        val vilkår = vilkårForBestemmelse.hentVilkår(
+            Folketrygdloven_kap2_bestemmelser.FTRL_KAP2_2_8_FØRSTE_LEDD_D,
+            emptyMap(),
+            1L
+        )
+
+
+        vilkår.shouldContainExactly(
+            Vilkår(FTRL_2_1A_TRYGDEKOORDINGERING),
+            Vilkår(FTRL_2_8_PENSJON_UFØRETRYGD_FOLKETRYGDEN),
+            Vilkår(FTRL_2_8_PENSJONIST_TRETTI_ÅR_TRYGDETID),
+            Vilkår(FTRL_2_8_PENSJONIST_TI_ÅR_TRYGDETID_FØR_SØKNADSTIDSPUNKT),
+            Vilkår(FTRL_2_8_NÆR_TILKNYTNING_NORGE)
+        )
+    }
+
+    @Test
+    fun `vilkår for FTRL_KAP2_2_8_ANDRE_LEDD, særlig grunn`() {
+        val vilkår = vilkårForBestemmelse.hentVilkår(
+            Folketrygdloven_kap2_bestemmelser.FTRL_KAP2_2_8_ANDRE_LEDD,
+            emptyMap(),
+            1L
+        )
+
+
+        vilkår.shouldContainExactly(
+            Vilkår(
+                FTRL_2_1A_TRYGDEKOORDINGERING
+            ),
+            Vilkår(
+                FTRL_FORUTGÅENDE_TRYGDETID
+            ),
+            Vilkår(
+                FTRL_2_8_NÆR_TILKNYTNING_NORGE
+            ),
+        )
+    }
+
+    @Test
+    fun `vilkår for FTRL_KAP2_2_8_FJERDE_LEDD, barn`() {
+        val vilkår = vilkårForBestemmelse.hentVilkår(
+            Folketrygdloven_kap2_bestemmelser.FTRL_KAP2_2_8_FJERDE_LEDD,
+            mapOf(Avklartefaktatyper.IKKE_YRKESAKTIV_RELASJON to Ikkeyrkesaktivrelasjontype.BARN_2_8_FJERDE_LEDD.name),
+            1L
+        )
+
+
+        vilkår.shouldContainExactly(
+            Vilkår(
+                FTRL_2_1A_TRYGDEKOORDINGERING
+            ),
+            Vilkår(
+                FTRL_2_8_FORSØRGET_FAMILIEMEDLEM
+            ),
+        )
+    }
+
+    @Test
+    fun `vilkår for FTRL_KAP2_2_8_FJERDE_LEDD, ektefelle`() {
+        val vilkår = vilkårForBestemmelse.hentVilkår(
+            Folketrygdloven_kap2_bestemmelser.FTRL_KAP2_2_8_FJERDE_LEDD,
+            mapOf(Avklartefaktatyper.IKKE_YRKESAKTIV_RELASJON to Ikkeyrkesaktivrelasjontype.EKTEFELLE_2_8_FJERDE_LEDD.name),
+            1L
+        )
+
+
+        vilkår.shouldContainExactly(
+            Vilkår(
+                FTRL_2_1A_TRYGDEKOORDINGERING
+            ),
+            Vilkår(
+                FTRL_2_8_FORSØRGET_FAMILIEMEDLEM
+            ),
+            Vilkår(
+                FTRL_FORUTGÅENDE_TRYGDETID
+            ),
+            Vilkår(
+                FTRL_2_8_NÆR_TILKNYTNING_NORGE
+            ),
+        )
+    }
+}
