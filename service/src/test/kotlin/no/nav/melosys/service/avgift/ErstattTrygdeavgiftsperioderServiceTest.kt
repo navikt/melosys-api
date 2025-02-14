6bc02357b2a98f5990bf2748709186bb49f6d300
Thomas.Dolplads <thomas.dolplads@twoday.com>
2025-02-14 10:42:43 +0100

    ny test for erstatting av trygdeavgiftsperioder

--- service/src/test/kotlin/no/nav/melosys/service/avgift/ErstattTrygdeavgiftsperioderServiceTest.kt
+++ service/src/test/kotlin/no/nav/melosys/service/avgift/ErstattTrygdeavgiftsperioderServiceTest.kt
@@ -1,18 +1,18 @@
 package no.nav.melosys.service.avgift
 
-import io.mockk.MockKAnnotations
+import io.kotest.matchers.equals.shouldBeEqual
+import io.kotest.matchers.shouldBe
 import io.mockk.every
 import io.mockk.impl.annotations.InjectMockKs
 import io.mockk.impl.annotations.MockK
 import io.mockk.junit5.MockKExtension
 import io.mockk.mockk
-import io.mockk.verify
 import no.nav.melosys.domain.Behandling
 import no.nav.melosys.domain.Behandlingsresultat
 import no.nav.melosys.domain.Medlemskapsperiode
 import no.nav.melosys.domain.avgift.Trygdeavgiftsperiode
+import no.nav.melosys.domain.kodeverk.Trygdeavgift_typer
 import no.nav.melosys.service.behandling.BehandlingsresultatService
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
@@ -25,34 +25,41 @@ class ErstattTrygdeavgiftsperioderServiceTest() {
     @InjectMockKs
     private lateinit var erstattTrygdeavgiftsperioderService: ErstattTrygdeavgiftsperioderService
 
-    @BeforeEach
-    fun setUp() {
-        MockKAnnotations.init(this) // Initializes the mocks
-    }
-
     @Nested
     inner class ErstattTrygdeavgiftsperioderTest {
         @Test
         fun `erstatter eksisterende Trygdeavgiftsperioder`() {
-            val id = 1L
+            val eksisterendeId = 1L
+            val nyId = 2L
+            val medlId = 3L
 
-            val medlemskap = Medlemskapsperiode().apply { this.id = id }
+            val eksisterendeTrygdeavgiftsperiodeMock = mockk<Trygdeavgiftsperiode>(relaxed = true).apply {
+                every { id } returns eksisterendeId
+                every { grunnlagMedlemskapsperiode?.id } returns medlId
+            }
+            val nyTrygdeavgiftsperiodeMock = mockk<Trygdeavgiftsperiode>(relaxed = true).apply {
+                every { id } returns nyId
+                every { grunnlagMedlemskapsperiode?.id } returns medlId
+            }
+
+            val medlemskap = Medlemskapsperiode().apply { this.id = medlId }
+            medlemskap.addTrygdeavgiftsperiode(eksisterendeTrygdeavgiftsperiodeMock)
+
             val behandlingsresultat = Behandlingsresultat().apply {
                 behandling = Behandling()
                 medlemskapsperioder = listOf(medlemskap)
             }
 
-            val trygdeavgiftsperiodeMock = mockk<Trygdeavgiftsperiode>(relaxed = true).apply {
-                every { grunnlagMedlemskapsperiode?.id } returns id
-            }
-
             every { behandlingsresultatService.hentBehandlingsresultat(any()) } returns behandlingsresultat
 
+            val nyeTrygdeavgiftsperioder = listOf(nyTrygdeavgiftsperiodeMock)
 
-            erstattTrygdeavgiftsperioderService.erstattTrygdeavgiftsperioder(1337L, listOf(trygdeavgiftsperiodeMock))
+
+            erstattTrygdeavgiftsperioderService.erstattTrygdeavgiftsperioder(1337L, nyeTrygdeavgiftsperioder)
 
 
-            verify { medlemskap.addTrygdeavgiftsperiode(trygdeavgiftsperiodeMock) }
+            behandlingsresultat.trygdeavgiftType shouldBeEqual Trygdeavgift_typer.FORELØPIG
+            medlemskap.trygdeavgiftsperioder shouldBe nyeTrygdeavgiftsperioder
         }
 
         @Test
