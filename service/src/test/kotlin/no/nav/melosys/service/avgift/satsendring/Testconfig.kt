a3ae1f5ea95cec97d0eeee7805707c6c5bdd60c9
Eirik Bergande <eirik.bergande@nav.no>
2025-02-26 12:39:08 +0100

    7154 TOGGLE Endepunkt for å opprette satsendring prosess (#2709)
    https://jira.adeo.no/browse/MELOSYS-7154

--- service/src/test/kotlin/no/nav/melosys/service/avgift/satsendring/Testconfig.kt
+++ service/src/test/kotlin/no/nav/melosys/service/avgift/satsendring/Testconfig.kt
@@ -1,12 +1,18 @@
 package no.nav.melosys.service.avgift.satsendring
 
+import no.nav.melosys.saksflytapi.ProsessinstansService
+import no.nav.melosys.service.behandling.BehandlingService
 import org.springframework.boot.SpringBootConfiguration
 import org.springframework.context.annotation.Bean
 
 @SpringBootConfiguration
 class TestConfig {
     @Bean
-    fun satsendringAdminController(satsendringFinner: SatsendringFinner): SatsendringAdminController {
-        return SatsendringAdminController(satsendringFinner, "Dummy")
-    }
+    fun satsendringAdminController(
+        satsendringFinner: SatsendringFinner,
+        behandlingService: BehandlingService,
+        prosessinstansService: ProsessinstansService
+    ) =
+        SatsendringAdminController(satsendringFinner, behandlingService, prosessinstansService, "Dummy")
+
 }
