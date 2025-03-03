dc18c28b94372b96bb3fdcd7274aff98c2303cb5
Isa Salem Hame <isa.salem.hame@nav.no>
2025-03-03 15:45:26 +0100

    7129: Endre frontendtittel for StandardvedleggType (#2732)

--- domain/src/main/java/no/nav/melosys/domain/brev/StandardvedleggType.kt
+++ domain/src/main/java/no/nav/melosys/domain/brev/StandardvedleggType.kt
@@ -8,11 +8,11 @@ enum class StandardvedleggType(
     VIKTIG_INFORMASJON_RETTIGHETER_PLIKTER_AVSLAG(
         malnavn = "info_om_rettigheter_avslag",
         journalføringstittel = "Viktig informasjon om rettigheter og plikter",
-        frontendTittel = "Avslag: Rettigheter og plikter"
+        frontendTittel = "Avslag: Viktig informasjon om rettigheter og plikter"
     ),
     VIKTIG_INFORMASJON_RETTIGHETER_PLIKTER_INNVILGELSE(
         malnavn = "info_om_rettigheter_innvilgelse",
         journalføringstittel = "Viktig informasjon om rettigheter og plikter",
-        frontendTittel = "Innvilgelse: Rettigheter og plikter"
+        frontendTittel = "Innvilgelse: Viktig informasjon om rettigheter og plikter"
     )
 }
