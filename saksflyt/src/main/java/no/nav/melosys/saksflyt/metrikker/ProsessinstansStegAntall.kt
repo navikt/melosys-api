670d7a70524018b8877bece58c6d2501139a7960
RuneLind <rulind@gmail.com>
2025-03-03 11:56:55 +0100

    sistFullfortSteg må kunne være null

--- saksflyt/src/main/java/no/nav/melosys/saksflyt/metrikker/ProsessinstansStegAntall.kt
+++ saksflyt/src/main/java/no/nav/melosys/saksflyt/metrikker/ProsessinstansStegAntall.kt
@@ -5,7 +5,7 @@ import no.nav.melosys.saksflytapi.domain.ProsessSteg
 import no.nav.melosys.saksflytapi.domain.ProsessType
 
 data class ProsessinstansStegAntall(
-    val sistFullfortSteg: ProsessSteg,
+    val sistFullfortSteg: ProsessSteg?,
     val prosessType: ProsessType,
     val prosessStatus: ProsessStatus,
     val antall: Long
