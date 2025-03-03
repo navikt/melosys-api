d675eec7573a962368b4ab0fc92efe462ff5636e
Thomas.Dolplads <thomas.dolplads@twoday.com>
2025-03-03 12:11:55 +0100

    kast exception når vi ikke har beregnet faktureringsbeløp

--- service/src/main/kotlin/no/nav/melosys/service/dokument/brev/mapper/ÅrsavregningVedtakMapper.kt
+++ service/src/main/kotlin/no/nav/melosys/service/dokument/brev/mapper/ÅrsavregningVedtakMapper.kt
@@ -13,7 +13,6 @@ import no.nav.melosys.service.avgift.aarsavregning.totalbeloep.TotalbeløpBeregn
 import no.nav.melosys.service.avgift.aarsavregning.ÅrsavregningKonstanter
 import no.nav.melosys.service.avgift.aarsavregning.ÅrsavregningService
 import org.springframework.stereotype.Component
-import java.math.BigDecimal
 
 @Component
 class ÅrsavregningVedtakMapper(
@@ -39,7 +38,7 @@ class ÅrsavregningVedtakMapper(
             endeligTrygdeavgiftTotalbeløp = årsavregningModel.nyttTotalbeloep
                 ?: throw FunksjonellException("Nytt totalbeløp finnes ikke for behandling $behandlingsId"),
             forskuddsvisFakturertTrygdeavgiftTotalbeløp = årsavregningModel.totaltTidligereFakturertBeloep(),
-            differansebeløp = årsavregningModel.tilFaktureringBeloep ?: BigDecimal.ZERO,
+            differansebeløp = årsavregningModel.tilFaktureringBeloep ?: throw FunksjonellException("Til faktureringsbeløpet er ikke blitt kalkulert"),
             minimumsbeløpForFakturering = ÅrsavregningKonstanter.MINIMUM_BELØP_FAKTURERING.beløp,
             harGrunnlagKunFraMelosys = årsavregningModel.harGrunnlagKunFraMelosys(),
             pliktigMedlemskap = årsavregningModel.tidligereGrunnlag?.medlemskapsperioder?.all { it.medlemskapstyper == Medlemskapstyper.PLIKTIG }
