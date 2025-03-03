4a8ec129ef4166ba7b254676f5ff3ea0c2e70ed9
ck-hui <Chak.Kwan.Hui@nav.no>
2025-03-03 12:34:53 +0100

    Test - WIP

--- integrasjonstest/src/test/kotlin/no/nav/melosys/itest/vedtak/PensjonistFtrlVedtakIT.kt
+++ integrasjonstest/src/test/kotlin/no/nav/melosys/itest/vedtak/PensjonistFtrlVedtakIT.kt
@@ -1,47 +1,41 @@
 package no.nav.melosys.itest.vedtak
 
 import com.github.tomakehurst.wiremock.client.WireMock
+import io.kotest.assertions.withClue
 import io.kotest.matchers.collections.shouldHaveSize
 import io.kotest.matchers.nulls.shouldNotBeNull
 import io.kotest.matchers.optional.shouldBePresent
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.types.shouldBeInstanceOf
+import no.nav.melosys.domain.Behandlingsmaate
 import no.nav.melosys.domain.avgift.Inntektsperiode
 import no.nav.melosys.domain.avgift.Penger
 import no.nav.melosys.domain.avgift.SkatteforholdTilNorge
 import no.nav.melosys.domain.avgift.Trygdeavgiftsperiode
-import no.nav.melosys.domain.kodeverk.Avklartefaktatyper
-import no.nav.melosys.domain.kodeverk.Folketrygdloven_kap2_bestemmelser
-import no.nav.melosys.domain.kodeverk.Inntektskildetype
-import no.nav.melosys.domain.kodeverk.InnvilgelsesResultat
-import no.nav.melosys.domain.kodeverk.Mottatteopplysningertyper
-import no.nav.melosys.domain.kodeverk.Sakstemaer
-import no.nav.melosys.domain.kodeverk.Sakstyper
-import no.nav.melosys.domain.kodeverk.Skatteplikttype
-import no.nav.melosys.domain.kodeverk.Trygdedekninger
-import no.nav.melosys.domain.kodeverk.Vedtakstyper
-import no.nav.melosys.domain.kodeverk.Vilkaar
-import no.nav.melosys.domain.kodeverk.behandlinger.Behandlingsaarsaktyper
-import no.nav.melosys.domain.kodeverk.behandlinger.Behandlingsresultattyper
-import no.nav.melosys.domain.kodeverk.behandlinger.Behandlingstema
-import no.nav.melosys.domain.kodeverk.behandlinger.Behandlingstyper
+import no.nav.melosys.domain.kodeverk.*
+import no.nav.melosys.domain.kodeverk.behandlinger.*
 import no.nav.melosys.domain.manglendebetaling.Betalingsstatus
 import no.nav.melosys.domain.manglendebetaling.ManglendeFakturabetalingMelding
 import no.nav.melosys.domain.mottatteopplysninger.SøknadNorgeEllerUtenforEØS
 import no.nav.melosys.domain.mottatteopplysninger.data.Periode
 import no.nav.melosys.domain.mottatteopplysninger.data.Soeknadsland
+import no.nav.melosys.integrasjon.hendelser.VedtakHendelseMelding
 import no.nav.melosys.integrasjon.trygdeavgift.dto.DatoPeriodeDto
-import no.nav.melosys.itest.AvgiftFaktureringTestBase
 import no.nav.melosys.itest.MelosysHendelseKafkaConsumer
+import no.nav.melosys.itest.AvgiftFaktureringTestBase
+import no.nav.melosys.repository.BehandlingRepository
 import no.nav.melosys.repository.FagsakRepository
 import no.nav.melosys.saksflytapi.domain.ProsessType
 import no.nav.melosys.service.avgift.TrygdeavgiftsberegningService
 import no.nav.melosys.service.avklartefakta.AvklartefaktaDto
 import no.nav.melosys.service.avklartefakta.AvklartefaktaService
+import no.nav.melosys.service.behandling.BehandlingsresultatService
 import no.nav.melosys.service.behandling.VilkaarsresultatService
 import no.nav.melosys.service.ftrl.medlemskapsperiode.MedlemskapsperiodeService
 import no.nav.melosys.service.ftrl.medlemskapsperiode.OpprettForslagMedlemskapsperiodeService
 import no.nav.melosys.service.mottatteopplysninger.MottatteOpplysningerService
+import no.nav.melosys.service.sak.OpprettBehandlingForSak
+import no.nav.melosys.service.sak.OpprettSakDto
 import no.nav.melosys.service.saksopplysninger.OppfriskSaksopplysningerService
 import no.nav.melosys.service.vedtak.FattVedtakRequest
 import no.nav.melosys.service.vedtak.VedtaksfattingFasade
@@ -54,18 +48,21 @@ import org.springframework.kafka.core.KafkaTemplate
 import java.time.LocalDate
 
 class PensjonistFtrlVedtakIT(
-    @Autowired private val fagsakRepository: FagsakRepository,
-    @Autowired private val mottatteOpplysningerService: MottatteOpplysningerService,
     @Autowired private val avklartefaktaService: AvklartefaktaService,
+    @Autowired private val fagsakRepository: FagsakRepository,
+    @Autowired private val behandlingsresultatService: BehandlingsresultatService,
+    @Autowired private val behandlingRepository: BehandlingRepository,
+    @Autowired private val mottatteOpplysningerService: MottatteOpplysningerService,
     @Autowired private val vilkaarsresultatService: VilkaarsresultatService,
     @Autowired private val medlemskapsperiodeService: MedlemskapsperiodeService,
     @Autowired private val opprettForslagMedlemskapsperiodeService: OpprettForslagMedlemskapsperiodeService,
     @Autowired private val oppfriskSaksopplysningerService: OppfriskSaksopplysningerService,
     @Autowired private val vedtaksfattingFasade: VedtaksfattingFasade,
+    @Autowired private val opprettBehandlingForSak: OpprettBehandlingForSak,
     @Autowired private val trygdeavgiftsberegningService: TrygdeavgiftsberegningService,
     @Autowired @Qualifier("manglendeFakturabetalingMelding") private val manglendeFakturabetalingMeldingTemplate: KafkaTemplate<String, ManglendeFakturabetalingMelding>,
     @Autowired private val melosysHendelseKafkaConsumer: MelosysHendelseKafkaConsumer,
-) : AvgiftFaktureringTestBase(TrygdeavgiftsberegningTransformer()){
+) : AvgiftFaktureringTestBase(TrygdeavgiftsberegningTransformer()) {
 
     private val kafkaTopic = "teammelosys.manglende-fakturabetaling-local"
     override val fakturaserieReferanse: String = "01J17B5NTTDYKFB5DZTSSQEHJ0"
@@ -75,10 +72,115 @@ class PensjonistFtrlVedtakIT(
         melosysHendelseKafkaConsumer.clear()
     }
 
+    @Test
+    fun `Pensjonist vedtak - FTRL - opprett fakturaserie for førstegangsbehandling og kanseller fakturaserie i ny vurdering`() {
+        val saksnummer = lagFørstegangsbehandling(Skatteplikttype.IKKE_SKATTEPLIKTIG, false)
+
+        val behandlingsId = executeAndWait(
+            mapOf(
+                ProsessType.OPPRETT_REPLIKERT_BEHANDLING_FOR_SAK to 1
+            )
+        ) {
+            opprettBehandlingForSak.opprettBehandling(
+                saksnummer,
+                lagOpprettSakDto()
+            )
+        }.behandling.id
+
+        val periode = DatoPeriodeDto(LocalDate.of(2023, 1, 1), LocalDate.of(2023, 2, 1))
+
+
+        val skattefordholdsperioder = listOf(
+            SkatteforholdTilNorge().apply {
+                fomDato = periode.fom
+                tomDato = periode.tom
+                skatteplikttype = Skatteplikttype.SKATTEPLIKTIG
+            }
+        )
+        val inntektsforholdsperioder = listOf(
+            Inntektsperiode().apply {
+                fomDato = periode.fom
+                tomDato = periode.tom
+                type = Inntektskildetype.PENSJON_UFØRETRYGD
+                isArbeidsgiversavgiftBetalesTilSkatt = true
+                avgiftspliktigMndInntekt = Penger(10000.toBigDecimal())
+            }
+        )
+
+        trygdeavgiftsberegningService.beregnOgLagreTrygdeavgift(behandlingsId, skattefordholdsperioder, inntektsforholdsperioder)
+
+        val vedtakRequest = FattVedtakRequest.Builder()
+            .medBehandlingsresultatType(Behandlingsresultattyper.MEDLEM_I_FOLKETRYGDEN)
+            .medVedtakstype(Vedtakstyper.ENDRINGSVEDTAK)
+            .medBestillersId("komponent test")
+            .build()
+
+        executeAndWait(
+            mapOf(
+                ProsessType.IVERKSETT_VEDTAK_FTRL to 1,
+                ProsessType.OPPRETT_OG_DISTRIBUER_BREV to 1
+            )
+        ) {
+            vedtaksfattingFasade.fattVedtak(behandlingsId, vedtakRequest)
+        }
+
+
+        behandlingsresultatService.hentBehandlingsresultat(behandlingsId).run {
+            type shouldBe Behandlingsresultattyper.MEDLEM_I_FOLKETRYGDEN
+            behandlingsmåte shouldBe Behandlingsmaate.MANUELT
+            fastsattAvLand shouldBe Land_iso2.NO
+        }
+        behandlingRepository.findById(behandlingsId)
+            .shouldBePresent().run {
+                withClue("Behandlingsstatus skal være AVSLUTTET") {
+                    status shouldBe Behandlingsstatus.AVSLUTTET
+                }
+                fagsak.run {
+                    withClue("Saksstatus skal være LOVVALG_AVKLART") {
+                        status shouldBe Saksstatuser.LOVVALG_AVKLART
+                    }
+                }
+            }
+
+        melosysHendelseKafkaConsumer.melosysHendelser.shouldHaveSize(2)
+            .last().value()
+            .melding.shouldBeInstanceOf<VedtakHendelseMelding>()
+            .shouldBe(
+                VedtakHendelseMelding(
+                    folkeregisterIdent = "30056928150",
+                    sakstype = Sakstyper.FTRL,
+                    sakstema = Sakstemaer.MEDLEMSKAP_LOVVALG,
+                    behandligsresultatType = Behandlingsresultattyper.MEDLEM_I_FOLKETRYGDEN,
+                    vedtakstype = Vedtakstyper.ENDRINGSVEDTAK,
+                    medlemskapsperioder = listOf(
+                        no.nav.melosys.integrasjon.hendelser.Periode(
+                            LocalDate.of(2023, 1, 1),
+                            LocalDate.of(2023, 2, 1),
+                            InnvilgelsesResultat.INNVILGET
+                        )
+                    ),
+                    lovvalgsperioder = listOf()
+                )
+            )
+
+        mockServer.verify(1, WireMock.postRequestedFor(WireMock.urlEqualTo("/fakturaserier")))
+        mockServer.verify(1, WireMock.deleteRequestedFor(WireMock.urlEqualTo("/fakturaserier/$fakturaserieReferanse")))
+    }
 
     @Test
-    fun `Oppretter riktig oppgave ved manglende innbetaling for pensjonister`() {
+    fun `Håndtere manglende innbetaling i sak som allerede har en åpen behandling`() {
         val saksnummer = lagFørstegangsbehandling(Skatteplikttype.IKKE_SKATTEPLIKTIG, false)
+
+        val behandlingID = executeAndWait(
+            mapOf(
+                ProsessType.OPPRETT_REPLIKERT_BEHANDLING_FOR_SAK to 1
+            )
+        ) {
+            opprettBehandlingForSak.opprettBehandling(
+                saksnummer,
+                lagOpprettSakDto()
+            )
+        }.behandling.id
 
         executeAndWait(
             mapOf(
@@ -97,7 +199,7 @@ class PensjonistFtrlVedtakIT(
 
         fagsakRepository.findBySaksnummer(saksnummer)
             .shouldBePresent().run {
-                behandlinger.shouldHaveSize(1)
+                behandlinger.shouldHaveSize(2)
                 finnAktivBehandlingIkkeÅrsavregning().shouldNotBeNull().run {
                     tema shouldBe Behandlingstema.PENSJONIST
                     type shouldBe Behandlingstyper.MANGLENDE_INNBETALING_TRYGDEAVGIFT
@@ -105,10 +207,36 @@ class PensjonistFtrlVedtakIT(
                 }
             }
 
+        behandlingsresultatService.hentBehandlingsresultat(behandlingID).run {
+            type shouldBe Behandlingsresultattyper.IKKE_FASTSATT
+            behandlingsmåte shouldBe Behandlingsmaate.MANUELT
+            fastsattAvLand shouldBe Land_iso2.NO
+        }
+        behandlingRepository.findById(behandlingID).shouldBePresent()
+            .run {
+                withClue("Behandlingsstatus skal være OPPRETTET") {
+                    status shouldBe Behandlingsstatus.OPPRETTET
+                }
+                fagsak.apply {
+                    withClue("Saksstatus skal være LOVVALG_AVKLART") {
+                        status shouldBe Saksstatuser.LOVVALG_AVKLART
+                    }
+                }
+            }
+
         mockServer.verify(
             1,
             WireMock.postRequestedFor(WireMock.urlEqualTo("/api/v1/mal/varsel_manglende_innbetaling/lag-pdf?somKopi=false&utkast=false"))
         )
+    }
+
+    private fun lagOpprettSakDto(): OpprettSakDto {
+        val opprettsakdto = OpprettSakDto()
+        opprettsakdto.behandlingstema = Behandlingstema.PENSJONIST
+        opprettsakdto.behandlingstype = Behandlingstyper.NY_VURDERING
+        opprettsakdto.mottaksdato = LocalDate.now()
+        opprettsakdto.behandlingsaarsakType = Behandlingsaarsaktyper.SØKNAD
+        return opprettsakdto
     }
 
     fun lagFørstegangsbehandling(skatteplikttype: Skatteplikttype, arbeidsgiversavgiftBetales: Boolean): String {
@@ -129,6 +257,7 @@ class PensjonistFtrlVedtakIT(
             mottatteOpplysningerService.hentEllerOpprettMottatteOpplysninger(behandling.id, true)
                 .shouldNotBeNull()
                 .apply {
+                    type shouldBe Mottatteopplysningertyper.SØKNAD_YRKESAKTIVE_NORGE_ELLER_UTENFOR_EØS
                     mottatteOpplysningerData
                         .shouldBeInstanceOf<SøknadNorgeEllerUtenforEØS>()
                         .apply {
@@ -137,7 +266,7 @@ class PensjonistFtrlVedtakIT(
                                 LocalDate.of(2023, 2, 1),
                             )
                             soeknadsland = Soeknadsland(listOf("AF"), false)
-                            trygdedekning = Trygdedekninger.FULL_DEKNING_FTRL
+                            trygdedekning = Trygdedekninger.FTRL_2_9_FØRSTE_LEDD_A_HELSE
                         }
                 }
         mottatteOpplysningerService.oppdaterMottatteOpplysninger(behandling.id, mottatteOpplysninger.mottatteOpplysningerData.toJsonNode)
@@ -170,13 +299,19 @@ class PensjonistFtrlVedtakIT(
         avklartefaktaService.lagreAvklarteFakta(behandling.id, setOf(yrkesgruppe, virksomhet, yrkesaktivitet))
 
         val vilkår = listOf(VilkaarDto().apply {
-            vilkaar = Vilkaar.FTRL_2_1_BOSATT_NORGE_FORUT.kode
+            vilkaar = Vilkaar.FTRL_2_1A_TRYGDEKOORDINGERING.kode
             isOppfylt = true
         }, VilkaarDto().apply {
-            vilkaar = Vilkaar.FTRL_2_1_LOVLIG_OPPHOLD.kode
+            vilkaar = Vilkaar.FTRL_2_8_NÆR_TILKNYTNING_NORGE.kode
             isOppfylt = true
         }, VilkaarDto().apply {
-            vilkaar = Vilkaar.FTRL_2_1_OPPHOLD_UNDER_12MND.kode
+            vilkaar = Vilkaar.FTRL_2_8_PENSJONIST_TRETTI_ÅR_TRYGDETID.kode
+            isOppfylt = true
+        },VilkaarDto().apply {
+            vilkaar = Vilkaar.FTRL_2_8_PENSJON_UFØRETRYGD_FOLKETRYGDEN.kode
+            isOppfylt = true
+        },VilkaarDto().apply {
+            vilkaar = Vilkaar.FTRL_2_8_PENSJONIST_TI_ÅR_TRYGDETID_FØR_SØKNADSTIDSPUNKT.kode
             isOppfylt = true
         })
         vilkaarsresultatService.registrerVilkår(behandling.id, vilkår)
@@ -187,7 +322,6 @@ class PensjonistFtrlVedtakIT(
             .medBehandlingsresultatType(Behandlingsresultattyper.MEDLEM_I_FOLKETRYGDEN)
             .medVedtakstype(Vedtakstyper.FØRSTEGANGSVEDTAK)
             .medBestillersId("komponent test")
-            .medFritekstSed("Fritekst")
             .build()
 
         executeAndWait(
@@ -209,7 +343,7 @@ class PensjonistFtrlVedtakIT(
     private fun setupTrygdeavgiftBeregning(behandlingId: Long, skatteplikttype: Skatteplikttype, arbeidsgiversavgiftBetales: Boolean) {
         val medlemskapsperiodeId = opprettForslagMedlemskapsperiodeService.opprettForslagPåMedlemskapsperioder(
             behandlingId,
-            Folketrygdloven_kap2_bestemmelser.FTRL_KAP2_2_1
+            Folketrygdloven_kap2_bestemmelser.FTRL_KAP2_2_8_FØRSTE_LEDD_D
         ).single().id
 
         val medlemskapsperiode = medlemskapsperiodeService.oppdaterMedlemskapsperiode(
@@ -218,8 +352,8 @@ class PensjonistFtrlVedtakIT(
             LocalDate.of(2023, 1, 1),
             LocalDate.of(2023, 2, 1),
             InnvilgelsesResultat.INNVILGET,
-            Trygdedekninger.FULL_DEKNING_FTRL,
-            Folketrygdloven_kap2_bestemmelser.FTRL_KAP2_2_1
+            Trygdedekninger.FTRL_2_9_FØRSTE_LEDD_A_HELSE,
+            Folketrygdloven_kap2_bestemmelser.FTRL_KAP2_2_8_FØRSTE_LEDD_D
         )
         val periode = DatoPeriodeDto(LocalDate.of(2023, 1, 1), LocalDate.of(2023, 2, 1))
         val skattefordholdsperioder = listOf(
@@ -233,9 +367,9 @@ class PensjonistFtrlVedtakIT(
             Inntektsperiode().apply {
                 fomDato = periode.fom
                 tomDato = periode.tom
-                this.type = Inntektskildetype.UFØRETRYGD
+                this.type = Inntektskildetype.PENSJON_UFØRETRYGD
                 isArbeidsgiversavgiftBetalesTilSkatt = arbeidsgiversavgiftBetales
-                avgiftspliktigMndInntekt = Penger(1000.toBigDecimal())
+                avgiftspliktigMndInntekt = Penger(10000.toBigDecimal())
                 avgiftspliktigTotalinntekt = Penger(10000.toBigDecimal())
             }
         )
@@ -252,7 +386,7 @@ class PensjonistFtrlVedtakIT(
         val inntektsperiode = Inntektsperiode().apply {
             fomDato = LocalDate.of(2023, 1, 1)
             tomDato = LocalDate.of(2023, 2, 1)
-            type = Inntektskildetype.UFØRETRYGD
+            type = Inntektskildetype.PENSJON_UFØRETRYGD
             isArbeidsgiversavgiftBetalesTilSkatt = arbeidsgiversavgiftBetales
             avgiftspliktigMndInntekt = Penger(10000.toBigDecimal(), "nok")
         }
@@ -272,5 +406,4 @@ class PensjonistFtrlVedtakIT(
 
         medlemskapsperiode.trygdeavgiftsperioder = trygdeavgiftsperioder
     }
-
 }
