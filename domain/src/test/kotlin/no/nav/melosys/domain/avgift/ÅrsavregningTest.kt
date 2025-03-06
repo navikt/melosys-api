c856ae56e93aaded690032641b9cf42643916a20
thomasdolplads <thomas.dolplads@twoday.com>
2025-03-06 15:00:29 +0100

    7172 TOGGLE Beregn tilfaktureringsbeloep (#2736)
    [MELOSYS-7172](https://jira.adeo.no/browse/MELOSYS-7172)
    
    - Beregn tilfaktureringsbeloepet under oppdatering av årsavregning selv
    om vi ikke tidligere har foretatt faktureringer

---
+++ domain/src/test/kotlin/no/nav/melosys/domain/avgift/ÅrsavregningTest.kt
@@ -0,0 +1,58 @@
+package no.nav.melosys.domain.avgift
+
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+import java.math.BigDecimal
+
+class ÅrsavregningTest {
+
+    @Test
+    fun `beregnTilFaktureringsBeloep uten tidligere fakturert beløp benytter totalbeløp`() {
+        val årsavregning = Årsavregning().apply {
+            nyttTotalbeloep = BigDecimal(1000)
+        }
+
+        årsavregning.beregnTilFaktureringsBeloep()
+
+        årsavregning.tilFaktureringBeloep shouldBe BigDecimal(1000)
+    }
+
+    @Test
+    fun `beregnTilFaktureringsBeloep med tidligere fakturert beløp fra avgiftssystemet trekker fra tidligere fakturert beløp fra avgiftssystemet`() {
+        val årsavregning = Årsavregning().apply {
+            nyttTotalbeloep = BigDecimal(1000)
+            harDeltGrunnlag = true
+            tidligereFakturertBeloepAvgiftssystem = BigDecimal(200)
+        }
+
+        årsavregning.beregnTilFaktureringsBeloep()
+
+        årsavregning.tilFaktureringBeloep shouldBe BigDecimal(800)
+    }
+
+    @Test
+    fun `beregnTilFaktureringsBeloep med tidligere fakturert beløp trekker fra tidligere fakturert beløp`() {
+        val årsavregning = Årsavregning().apply {
+            nyttTotalbeloep = BigDecimal(1000)
+            tidligereFakturertBeloep = BigDecimal(200)
+        }
+
+        årsavregning.beregnTilFaktureringsBeloep()
+
+        årsavregning.tilFaktureringBeloep shouldBe BigDecimal(800)
+    }
+
+    @Test
+    fun `beregnTilFaktureringsBeloep med tidligere fakturert beløp og tidligere fakturert beløp fra avgiftssystemet trekker fra begge`() {
+        val årsavregning = Årsavregning().apply {
+            nyttTotalbeloep = BigDecimal(1000)
+            harDeltGrunnlag = true
+            tidligereFakturertBeloepAvgiftssystem = BigDecimal(200)
+            tidligereFakturertBeloep = BigDecimal(200)
+        }
+
+        årsavregning.beregnTilFaktureringsBeloep()
+
+        årsavregning.tilFaktureringBeloep shouldBe BigDecimal(600)
+    }
+}
