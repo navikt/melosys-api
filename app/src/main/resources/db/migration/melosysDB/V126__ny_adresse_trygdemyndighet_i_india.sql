ef259b7e37ef3e77d7537114e7266f9dd59e67c3
ck-hui <Chak.Kwan.Hui@nav.no>
2025-04-07 09:57:39 +0200

    Script som oppdaterer adresse

---
+++ app/src/main/resources/db/migration/melosysDB/V126__ny_adresse_trygdemyndighet_i_india.sql
@@ -0,0 +1,7 @@
+UPDATE utenlandsk_myndighet
+SET
+    NAVN = 'Employees Provident Fund Organisation Head Office',
+    GATEADRESSE_1 = 'Plate A Ground Floor, Office Block-II, East Kidwai Nagar',
+    POSTSTED = 'New Dehli - 110023'
+WHERE
+    LAND = 'INDIA';
