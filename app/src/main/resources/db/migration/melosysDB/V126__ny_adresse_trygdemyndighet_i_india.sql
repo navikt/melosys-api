49cb6f31ae3296b0fb156fe01616a8924311d5c2
ck-hui <Chak.Kwan.Hui@nav.no>
2025-04-07 11:44:52 +0200

    Flyway for oppdatering av adresse til India

--- app/src/main/resources/db/migration/melosysDB/V126__ny_adresse_trygdemyndighet_i_india.sql
+++ app/src/main/resources/db/migration/melosysDB/V126__ny_adresse_trygdemyndighet_i_india.sql
@@ -1,7 +1,8 @@
 UPDATE utenlandsk_myndighet
 SET
     NAVN = 'Employees Provident Fund Organisation Head Office',
-    GATEADRESSE_1 = 'Plate A Ground Floor, Office Block-II, East Kidwai Nagar',
+    GATEADRESSE_1 = 'Plate A, Ground Floor, Block II, East Kidwai Nagar',
+    GATEADRESSE_2 = NULL,
     POSTSTED = 'New Dehli - 110023'
 WHERE
     LAND = 'INDIA';
