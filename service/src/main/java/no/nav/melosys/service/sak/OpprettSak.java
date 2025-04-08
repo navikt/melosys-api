096ee3349e608518356f5450dddca2ed20d60a50
Thomas.Dolplads <thomas.dolplads@twoday.com>
2025-04-08 11:11:24 +0200

    rydd bort oppretting av sak hvor vi knytter til eksisterende oppgave

--- service/src/main/java/no/nav/melosys/service/sak/OpprettSak.java
+++ service/src/main/java/no/nav/melosys/service/sak/OpprettSak.java
@@ -1,28 +1,13 @@
 package no.nav.melosys.service.sak;
 
-import java.time.LocalDate;
-import java.time.ZoneId;
-import java.util.Optional;
-
-import no.nav.melosys.domain.Fagsak;
-import no.nav.melosys.domain.arkiv.Journalpost;
-import no.nav.melosys.domain.arkiv.Journalposttype;
-import no.nav.melosys.domain.eessi.melding.MelosysEessiMelding;
-import no.nav.melosys.domain.kodeverk.Oppgavetyper;
-import no.nav.melosys.domain.kodeverk.Sakstemaer;
 import no.nav.melosys.domain.kodeverk.Sakstyper;
 import no.nav.melosys.domain.kodeverk.behandlinger.Behandlingsaarsaktyper;
 import no.nav.melosys.domain.kodeverk.behandlinger.Behandlingstema;
-import no.nav.melosys.domain.kodeverk.behandlinger.Behandlingstyper;
-import no.nav.melosys.domain.oppgave.Oppgave;
 import no.nav.melosys.exception.FunksjonellException;
 import no.nav.melosys.saksflytapi.ProsessinstansService;
 import no.nav.melosys.service.dokument.sed.EessiService;
-import no.nav.melosys.service.journalforing.JournalfoeringService;
-import no.nav.melosys.service.journalforing.UtledBehandlingsaarsak;
 import no.nav.melosys.service.journalforing.dto.PeriodeDto;
 import no.nav.melosys.service.lovligekombinasjoner.LovligeKombinasjonerSaksbehandlingService;
-import no.nav.melosys.service.oppgave.OppgaveService;
 import no.nav.melosys.service.saksbehandling.SaksbehandlingRegler;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.context.annotation.Lazy;
@@ -31,8 +16,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class OpprettSak {
-    private final JournalfoeringService journalfoeringService;
-    private final OppgaveService oppgaveService;
     private final ProsessinstansService prosessinstansService;
     private final SaksbehandlingRegler saksbehandlingRegler;
     private final FagsakService fagsakService;
@@ -40,56 +23,14 @@ public class OpprettSak {
 
     private final LovligeKombinasjonerSaksbehandlingService lovligeKombinasjonerSaksbehandlingService;
 
-    public OpprettSak(JournalfoeringService journalfoeringService, OppgaveService oppgaveService,
-                      @Lazy ProsessinstansService prosessinstansService,
+    public OpprettSak(@Lazy ProsessinstansService prosessinstansService,
                       SaksbehandlingRegler saksbehandlingRegler,
                       FagsakService fagsakService, EessiService eessiService, LovligeKombinasjonerSaksbehandlingService lovligeKombinasjonerSaksbehandlingService) {
-        this.journalfoeringService = journalfoeringService;
-        this.oppgaveService = oppgaveService;
         this.prosessinstansService = prosessinstansService;
         this.saksbehandlingRegler = saksbehandlingRegler;
         this.fagsakService = fagsakService;
         this.eessiService = eessiService;
         this.lovligeKombinasjonerSaksbehandlingService = lovligeKombinasjonerSaksbehandlingService;
-    }
-
-    @Transactional
-    public void opprettNySakOgBehandlingFraOppgave(OpprettSakDto opprettSakDto) {
-        if (StringUtils.isEmpty(opprettSakDto.getOppgaveID())) {
-            throw new FunksjonellException("OppgaveID mangler.");
-        }
-        validerOpprettSakDto(opprettSakDto);
-
-        final Oppgave oppgave = oppgaveService.hentOppgaveMedOppgaveID(opprettSakDto.getOppgaveID());
-        validerOppgave(oppgave);
-
-        Journalpost journalpost = journalfoeringService.hentJournalpost(oppgave.getJournalpostId());
-        validerJournalpost(journalpost);
-
-        opprettSakDto.setBehandlingsaarsakType(getBehandlingsaarsakType(journalpost, opprettSakDto));
-        opprettSakDto.setMottaksdato(LocalDate.ofInstant(journalpost.getForsendelseMottatt(), ZoneId.systemDefault()));
-
-        Sakstyper sakstype = opprettSakDto.getSakstype();
-        if (sakstype == Sakstyper.EU_EOS) {
-            prosessinstansService.opprettProsessinstansNySakEØS(
-                oppgave.getJournalpostId(),
-                opprettSakDto.tilOpprettSakRequest()
-            );
-        } else if (sakstype == Sakstyper.FTRL || sakstype == Sakstyper.TRYGDEAVTALE) {
-            prosessinstansService.opprettProsessinstansNySakFTRLTrygdeavtale(
-                oppgave.getJournalpostId(),
-                opprettSakDto.tilOpprettSakRequest()
-            );
-        } else {
-            throw new FunksjonellException("Sakstype %s støttes ikke".formatted(sakstype));
-        }
-    }
-
-    private static Behandlingsaarsaktyper getBehandlingsaarsakType(Journalpost journalpost, OpprettSakDto opprettSakDto) {
-        Sakstemaer sakstema = opprettSakDto.getSakstema();
-        Behandlingstema behandlingstema = opprettSakDto.getBehandlingstema();
-        Behandlingstyper behandlingstype = opprettSakDto.getBehandlingstype();
-        return UtledBehandlingsaarsak.utledÅrsaktype(journalpost, sakstema, behandlingstema, behandlingstype);
     }
 
     @Transactional
@@ -149,49 +90,5 @@ public class OpprettSak {
         if (periodeDto.getTom() != null && periodeDto.getFom().isAfter(periodeDto.getTom())) {
             throw new FunksjonellException("Fra og med dato kan ikke være etter til og med dato.");
         }
-    }
-
-    private void validerOppgave(Oppgave oppgave) {
-        if (!nySakKanOpprettesFraOppgavetype(oppgave.getOppgavetype())) {
-            throw new FunksjonellException("Ny sak kan ikke opprettes på bakgrunn av oppgave med type: " + oppgave.getOppgavetype().getBeskrivelse());
-        }
-
-        if (StringUtils.isEmpty(oppgave.getJournalpostId())) {
-            throw new FunksjonellException("Ny sak kan ikke opprettes fordi oppgave " + oppgave.getOppgaveId() + " mangler journalpost.");
-        }
-    }
-
-    private static boolean nySakKanOpprettesFraOppgavetype(Oppgavetyper oppgavetype) {
-        return oppgavetype == Oppgavetyper.BEH_SAK_MK
-            || oppgavetype == Oppgavetyper.BEH_SAK
-            || oppgavetype == Oppgavetyper.BEH_SED
-            || oppgavetype == Oppgavetyper.VUR
-            || oppgavetype == Oppgavetyper.VURD_HENV
-            || oppgavetype == Oppgavetyper.VURD_MAN_INNB;
-    }
-
-    private void validerJournalpost(Journalpost journalpost) {
-        if (journalpost.getJournalposttype() == Journalposttype.UT) {
-            throw new FunksjonellException("Ny sak kan ikke opprettes fra utgående journalposter siden brev refererer til mottaksdato.");
-        }
-        validerSedTilknytning(journalpost);
-    }
-
-    private void validerSedTilknytning(Journalpost journalpost) {
-        Optional<Fagsak> optionalFagsak = finnSakTilknyttetSedJournalpost(journalpost);
-        if (optionalFagsak.isPresent()) {
-            throw new FunksjonellException(
-                "SED-en som er tilknyttet Gosys-oppgaven du har valgt er allerede koblet til %s".formatted(
-                    optionalFagsak.get().getSaksnummer()));
-        }
-    }
-
-    private Optional<Fagsak> finnSakTilknyttetSedJournalpost(Journalpost journalpost) {
-        if (!journalpost.mottaksKanalErEessi()) {
-            return Optional.empty();
-        }
-        MelosysEessiMelding melosysEessiMelding = eessiService.hentSedTilknyttetJournalpost(journalpost.getJournalpostId());
-        Optional<Long> tilknyttetArkivsak = eessiService.finnSakForRinasaksnummer(melosysEessiMelding.getRinaSaksnummer());
-        return tilknyttetArkivsak.flatMap(fagsakService::finnFagsakFraArkivsakID);
     }
 }
