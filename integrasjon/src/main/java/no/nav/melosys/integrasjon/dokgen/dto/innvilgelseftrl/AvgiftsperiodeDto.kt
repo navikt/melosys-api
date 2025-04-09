f6b741095fe5bbeca31f14e3b0fd9c8d0586a0fe
Thang Phan <thang.quoc.ly.phan@nav.no>
2025-04-09 10:02:20 +0200

    Fjern skatteplikt avgdto

--- integrasjon/src/main/java/no/nav/melosys/integrasjon/dokgen/dto/innvilgelseftrl/AvgiftsperiodeDto.kt
+++ integrasjon/src/main/java/no/nav/melosys/integrasjon/dokgen/dto/innvilgelseftrl/AvgiftsperiodeDto.kt
@@ -3,7 +3,6 @@ package no.nav.melosys.integrasjon.dokgen.dto.innvilgelseftrl
 import com.fasterxml.jackson.databind.annotation.JsonSerialize
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer
 import no.nav.melosys.domain.kodeverk.Inntektskildetype
-import no.nav.melosys.domain.kodeverk.Skatteplikttype
 import java.math.BigDecimal
 import java.time.LocalDate
 
@@ -14,5 +13,4 @@ data class AvgiftsperiodeDto(
     val avgiftPerMd: BigDecimal,
     val inntektskildetype: Inntektskildetype,
     val avgiftspliktigInntektPerMd: BigDecimal,
-    val skatteplikt: Boolean? = false
 )
