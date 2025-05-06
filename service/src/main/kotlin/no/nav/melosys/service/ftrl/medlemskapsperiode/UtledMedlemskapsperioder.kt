85fd254652e093a4df51ad7349de343473c62818
CK-Hui <chak.kwan.hui@nav.no>
2025-05-06 09:58:09 +0200

    7254 G4P Forslag til perioder når sluttdato er ukjent - Pensjonist (#2790)
    https://jira.adeo.no/browse/MELOSYS-7254

--- service/src/main/kotlin/no/nav/melosys/service/ftrl/medlemskapsperiode/UtledMedlemskapsperioder.kt
+++ service/src/main/kotlin/no/nav/melosys/service/ftrl/medlemskapsperiode/UtledMedlemskapsperioder.kt
@@ -119,7 +119,7 @@ object UtledMedlemskapsperioder {
                 )
             )
             //Scenario 2 og 3
-            søknadsperiode.fom.isBefore(enMånedFørMottaksdato) && søknadsperiode.tom.isAfter(mottaksdato) -> {
+            søknadsperiode.fom.isBefore(enMånedFørMottaksdato) && (søknadsperiode.tom == null || søknadsperiode.tom.isAfter(mottaksdato)) -> {
                 val erScenario2 = Trygdedekninger.FTRL_2_7_TREDJE_LEDD_B_HELSE_SYKE_FORELDREPENGER == trygdedekning
                 val erScenario3 = Trygdedekninger.FULL_DEKNING_FTRL == trygdedekning
 
@@ -239,8 +239,9 @@ object UtledMedlemskapsperioder {
                     emptySet()
                 }
             }
+
             // Scenario 3 og 4
-            søknadsperiode.fom.isBefore(enMånedFørMottaksdato) && !søknadsperiode.tom.isBefore(mottaksdato) -> {
+            søknadsperiode.fom.isBefore(enMånedFørMottaksdato) && (søknadsperiode.tom == null || !søknadsperiode.tom.isBefore(mottaksdato)) -> {
                 val scenario3 =
                     listOf(
                         Trygdedekninger.FTRL_2_9_FØRSTE_LEDD_A_HELSE,
@@ -254,15 +255,15 @@ object UtledMedlemskapsperioder {
                         Trygdedekninger.FTRL_2_9_FØRSTE_LEDD_B_PENSJON
                     ).contains(trygdedekning)
 
-                val førstePeriode = Periode(søknadsperiode.fom, mottaksdato.minusDays(1))
-                val andrePeriode = Periode(mottaksdato, søknadsperiode.tom)
+
+                val splittetPeriode = splitPeriode(søknadsperiode, grunnlag.mottaksdatoSøknadNotNull)
 
                 return if (scenario3) setOf(
-                    lagPeriode(førstePeriode, Trygdedekninger.FTRL_2_9_FØRSTE_LEDD_A_HELSE, InnvilgelsesResultat.AVSLAATT, grunnlag.bestemmelse),
-                    lagPeriode(andrePeriode, Trygdedekninger.FTRL_2_9_FØRSTE_LEDD_A_HELSE, InnvilgelsesResultat.INNVILGET, grunnlag.bestemmelse)
+                    lagPeriode(splittetPeriode.first, Trygdedekninger.FTRL_2_9_FØRSTE_LEDD_A_HELSE, InnvilgelsesResultat.AVSLAATT, grunnlag.bestemmelse),
+                    lagPeriode(splittetPeriode.second, Trygdedekninger.FTRL_2_9_FØRSTE_LEDD_A_HELSE, InnvilgelsesResultat.INNVILGET, grunnlag.bestemmelse)
                 ) else if (scenario4) setOf(
-                    lagPeriode(førstePeriode, Trygdedekninger.FTRL_2_9_FØRSTE_LEDD_A_HELSE, InnvilgelsesResultat.AVSLAATT, grunnlag.bestemmelse),
-                    lagPeriode(andrePeriode, Trygdedekninger.FTRL_2_9_FØRSTE_LEDD_A_HELSE, InnvilgelsesResultat.INNVILGET, grunnlag.bestemmelse),
+                    lagPeriode(splittetPeriode.first, Trygdedekninger.FTRL_2_9_FØRSTE_LEDD_A_HELSE, InnvilgelsesResultat.AVSLAATT, grunnlag.bestemmelse),
+                    lagPeriode(splittetPeriode.second, Trygdedekninger.FTRL_2_9_FØRSTE_LEDD_A_HELSE, InnvilgelsesResultat.INNVILGET, grunnlag.bestemmelse),
                     lagPeriode(søknadsperiode, Trygdedekninger.FTRL_2_9_FØRSTE_LEDD_B_PENSJON, InnvilgelsesResultat.AVSLAATT, grunnlag.bestemmelse)
                 ) else emptySet()
 
