a89895f2e371f0b06dd6317eb9f3576a5dbe31df
Francois Allix <francois.allix@nav.no>
2025-06-25 16:48:42 +0200

    Fiks test etter @Protected

--- integrasjonstest/src/test/kotlin/no/nav/melosys/itest/SatsendringAdminControllerIT.kt
+++ integrasjonstest/src/test/kotlin/no/nav/melosys/itest/SatsendringAdminControllerIT.kt
@@ -1,6 +1,7 @@
 package no.nav.melosys.itest
 
 import no.nav.melosys.tjenester.gui.config.ApiKeyInterceptor.Companion.API_KEY_HEADER
+import no.nav.security.mock.oauth2.MockOAuth2Server
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -9,13 +10,28 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+import wiremock.com.google.common.net.HttpHeaders.AUTHORIZATION
 
 @AutoConfigureMockMvc
 class SatsendringAdminControllerIT(
-    @Autowired private val mockMvc: MockMvc
+    @Autowired private val mockMvc: MockMvc,
+    @Autowired var mockOAuth2Server: MockOAuth2Server
 ) : ComponentTestBase() {
 
     private val testYear = 2025
+
+    private fun hentBearerToken(): String {
+        return mockOAuth2Server.issueToken(
+            issuerId = "issuer1",
+            subject = "testbruker",
+            audience = "dumbdumb",
+            claims = mapOf(
+                "oid" to "test-oid",
+                "azp" to "test-azp",
+                "NAVident" to "test123"
+            )
+        ).serialize()
+    }
 
     @BeforeEach
     fun setup() {
@@ -30,6 +46,7 @@ class SatsendringAdminControllerIT(
         mockMvc.perform(
             MockMvcRequestBuilders.post("/admin/satsendringer/${testYear}")
                 .header(API_KEY_HEADER, "dummy")
+                .header(AUTHORIZATION, "Bearer ${hentBearerToken()}")
         ).andExpect(MockMvcResultMatchers.status().isAccepted)
     }
 }
