b6f2cbfd10c39914c213755162c0e1558d656091
RuneLind <rulind@gmail.com>
2025-07-26 12:27:43 +0200

    Rydd i BehandlingTestBuilder

--- domain/src/test/kotlin/no.nav.melosys.domain/BehandlingTestBuilder.kt
+++ domain/src/test/kotlin/no.nav.melosys.domain/BehandlingTestBuilder.kt
@@ -11,6 +11,18 @@ import java.time.LocalDate
  */
 object BehandlingTestBuilder {
 
+    /**
+     * Oppretter en Behandling med fornuftige standardverdier for alle påkrevde felt.
+     * Overstyr kun de feltene du trenger for din spesifikke test.
+     *
+     * Eksempel:
+     * ```
+    BehandlingTestBuilder.builderWithDefaults()
+        .medId(1L)
+        .medStatus(Behandlingsstatus.UNDER_BEHANDLING)
+        .build();
+     * ```
+     */
     @JvmStatic
     fun builderWithDefaults() = Behandling.Builder().apply {
         // Sett standardverdier for alle påkrevde felt
@@ -30,6 +42,18 @@ object BehandlingTestBuilder {
     }
 }
 
-// Utvidelsefunksjon for enda renere syntaks i tester
+
+/**
+ * Oppretter en Behandling med fornuftige standardverdier for alle påkrevde felt.
+ * Overstyr kun de feltene du trenger for din spesifikke test.
+ *
+ * Eksempel:
+ * ```
+ * val behandling = Behandling.buildForTest {
+ *     status = Behandlingsstatus.AVSLUTTET
+ *     tema = Behandlingstema.YRKESAKTIV
+ * }
+ * ```
+ */
 fun Behandling.Companion.buildForTest(init: Behandling.Builder.() -> Unit = {}): Behandling =
     BehandlingTestBuilder.builderWithDefaults().apply(init).build()
