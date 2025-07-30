51c8565d89585f9fd6d367697795faa2f5942e3f
Thang Phan <thang.quoc.ly.phan@nav.no>
2025-07-30 12:51:44 +0200

    Legger til try catch

--- integrasjonstest/src/test/kotlin/no/nav/melosys/itest/vedtak/TrygdeavgiftsberegningTransformerForEøsPensjonist.kt
+++ integrasjonstest/src/test/kotlin/no/nav/melosys/itest/vedtak/TrygdeavgiftsberegningTransformerForEøsPensjonist.kt
@@ -15,6 +15,7 @@ import java.util.*
  */
 class TrygdeavgiftsberegningTransformerForEøsPensjonist : ResponseTransformerV2 {
     override fun transform(response: Response?, serveEvent: ServeEvent?): Response {
+
         val mapper = jacksonObjectMapper().registerModule(JavaTimeModule())
 
         if (serveEvent?.request?.url != "/api/v2/eos-pensjonist/beregn") {
@@ -23,39 +24,45 @@ class TrygdeavgiftsberegningTransformerForEøsPensjonist : ResponseTransformerV2
 
         val requestBody = mapper.readTree(serveEvent.request?.bodyAsString)
 
-        val helseutgiftDekkesPeriodeFom = requestBody["helseutgiftDekkesPeriode"]["periode"]["fom"]
-        val helseutgiftDekkesPeriodeTom = requestBody["helseutgiftDekkesPeriode"]["periode"]["tom"]
+        try {
+            val helseutgiftDekkesPeriodeFom = requestBody["helseutgiftDekkesPeriode"]["periode"]["fom"]
+            val helseutgiftDekkesPeriodeTom = requestBody["helseutgiftDekkesPeriode"]["periode"]["tom"]
 
-        val mappetDatoFom = LocalDate.of(helseutgiftDekkesPeriodeFom[0].asInt(), helseutgiftDekkesPeriodeFom[1].asInt(), helseutgiftDekkesPeriodeFom[2].asInt())
-        val mappetDatoTom = LocalDate.of(helseutgiftDekkesPeriodeTom[0].asInt(), helseutgiftDekkesPeriodeTom[1].asInt(), helseutgiftDekkesPeriodeTom[2].asInt())
-        val skatteforholdsperioderUuid = requestBody["skatteforholdsperioder"][0]["id"].asText()
-        val inntektsperioderUuid = requestBody["inntektsperioder"][0]["id"].asText()
+            val mappetDatoFom = LocalDate.of(helseutgiftDekkesPeriodeFom[0].asInt(), helseutgiftDekkesPeriodeFom[1].asInt(), helseutgiftDekkesPeriodeFom[2].asInt())
+            val mappetDatoTom = LocalDate.of(helseutgiftDekkesPeriodeTom[0].asInt(), helseutgiftDekkesPeriodeTom[1].asInt(), helseutgiftDekkesPeriodeTom[2].asInt())
+            val skatteforholdsperioderUuid = requestBody["skatteforholdsperioder"][0]["id"].asText()
+            val inntektsperioderUuid = requestBody["inntektsperioder"][0]["id"].asText()
 
-        val skatteforhold = requestBody["skatteforholdsperioder"][0]["skatteforhold"].asText()
-        val sats = if (skatteforhold == "IKKE_SKATTEPLIKTIG") 6.8.toBigDecimal() else 0.toBigDecimal()
-        val månedsavgift = if (skatteforhold == "IKKE_SKATTEPLIKTIG") PengerDto(
-            1000.toBigDecimal(),
-            NOK
-        ) else PengerDto(0.toBigDecimal(), NOK)
+            val skatteforhold = requestBody["skatteforholdsperioder"][0]["skatteforhold"].asText()
+            val sats = if (skatteforhold == "IKKE_SKATTEPLIKTIG") 6.8.toBigDecimal() else 0.toBigDecimal()
+            val månedsavgift = if (skatteforhold == "IKKE_SKATTEPLIKTIG") PengerDto(
+                1000.toBigDecimal(),
+                NOK
+            ) else PengerDto(0.toBigDecimal(), NOK)
 
-        val responsBodyFraTrygdeavgiftsberegning = listOf(
-            EøsPensjonistTrygdeavgiftsberegningResponse(
-                TrygdeavgiftsperiodeDto(
-                    DatoPeriodeDto(LocalDate.of(2023, 1, 1), LocalDate.of(2023, 2, 1)),
-                    sats,
-                    månedsavgift
-                ),
-                EøsPensjonistTrygdeavgiftsgrunnlagDto(
-                    DatoPeriodeDto(fom = mappetDatoFom, tom = mappetDatoTom),
-                    UUID.fromString(skatteforholdsperioderUuid),
-                    UUID.fromString(inntektsperioderUuid)
+            val responsBodyFraTrygdeavgiftsberegning = listOf(
+                EøsPensjonistTrygdeavgiftsberegningResponse(
+                    TrygdeavgiftsperiodeDto(
+                        DatoPeriodeDto(LocalDate.of(2023, 1, 1), LocalDate.of(2023, 2, 1)),
+                        sats,
+                        månedsavgift
+                    ),
+                    EøsPensjonistTrygdeavgiftsgrunnlagDto(
+                        DatoPeriodeDto(fom = mappetDatoFom, tom = mappetDatoTom),
+                        UUID.fromString(skatteforholdsperioderUuid),
+                        UUID.fromString(inntektsperioderUuid)
+                    )
                 )
             )
-        )
 
-        return Response.Builder.like(response)
-            .body(mapper.writeValueAsString(responsBodyFraTrygdeavgiftsberegning))
-            .build()
+            return Response.Builder.like(response)
+                .body(mapper.writeValueAsString(responsBodyFraTrygdeavgiftsberegning))
+                .build()
+        } catch (exception: Exception ) {
+            println(exception)
+        }
+
+        return Response.Builder().build()
     }
 
 
