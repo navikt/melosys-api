0f323106e1bd13b8465d34752c0ca7496a1a778a
Øyvind Nygard <oyvinyg@gmail.com>
2025-08-05 11:21:06 +0200

    6670 Endret fra .properties-format til .yml (#2918)

--- app/src/main/java/no/nav/melosys/ApplicationConfig.java
+++ app/src/main/java/no/nav/melosys/ApplicationConfig.java
@@ -4,12 +4,10 @@ import no.nav.security.token.support.spring.api.EnableJwtTokenValidation;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.retry.annotation.EnableRetry;
 
 @Configuration
 @ServletComponentScan("no.nav.melosys.integrasjon.felles")
-@PropertySource(value = "classpath:service.properties", encoding = "utf-8")
 @EnableJwtTokenValidation(ignore = {"org.springframework", "springfox.documentation", "org.springdoc"})
 @EnableCaching
 @EnableRetry
