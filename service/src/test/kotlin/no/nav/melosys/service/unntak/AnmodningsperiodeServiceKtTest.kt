69d76351621f09884edbf971b8e5683b86b005fc
Isa Salem Hame <isa.salem.hame@nav.no>
2025-08-07 17:45:25 +0200

    Legg til ny test: AnmodningsperiodeServiceKtTest.kt

--- service/src/test/kotlin/no/nav/melosys/service/unntak/AnmodningsperiodeServiceKtTest.kt
+++ service/src/test/kotlin/no/nav/melosys/service/unntak/AnmodningsperiodeServiceKtTest.kt
@@ -1,7 +1,8 @@
 package no.nav.melosys.service.unntak
 
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.matchers.booleans.shouldBeTrue
 import io.kotest.matchers.shouldBe
-import io.kotest.matchers.shouldNotBe
 import io.mockk.*
 import no.nav.melosys.domain.Anmodningsperiode
 import no.nav.melosys.domain.AnmodningsperiodeSvar
@@ -17,13 +18,9 @@ import no.nav.melosys.service.LovvalgsperiodeService
 import no.nav.melosys.service.behandling.BehandlingsresultatService
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertThrows
-import org.junit.jupiter.api.extension.ExtendWith
-import org.mockito.junit.jupiter.MockitoExtension
 import java.time.LocalDate
 import java.util.*
 
-@ExtendWith(MockitoExtension::class)
 class AnmodningsperiodeServiceKtTest {
 
     companion object {
@@ -31,24 +28,26 @@ class AnmodningsperiodeServiceKtTest {
         private const val BEHANDLINGS_ID = 22L
     }
 
-    private val anmodningsperiodeRepository: AnmodningsperiodeRepository = mockk()
-    private val behandlingsresultatService: BehandlingsresultatService = mockk()
-    private val lovvalgsperiodeService: LovvalgsperiodeService = mockk()
-    private val anmodningsperiodeSvarRepository: AnmodningsperiodeSvarRepository = mockk()
+    private val anmodningsperiodeRepository = mockk<AnmodningsperiodeRepository>()
+    private val behandlingsresultatService = mockk<BehandlingsresultatService>()
+    private val lovvalgsperiodeService = mockk<LovvalgsperiodeService>()
+    private val anmodningsperiodeSvarRepository = mockk<AnmodningsperiodeSvarRepository>()
 
     private lateinit var anmodningsperiodeService: AnmodningsperiodeService
 
     @BeforeEach
     fun setUp() {
         anmodningsperiodeService = AnmodningsperiodeService(
-            anmodningsperiodeRepository, lovvalgsperiodeService,
-            anmodningsperiodeSvarRepository, behandlingsresultatService
+            anmodningsperiodeRepository, 
+            lovvalgsperiodeService,
+            anmodningsperiodeSvarRepository, 
+            behandlingsresultatService
         )
     }
 
     @Test
     fun `hentAnmodningsperiode`() {
-        every { anmodningsperiodeRepository.findById(any()) } returns Optional.empty()
+        every { anmodningsperiodeRepository.findById(ANMODNINGSPERIODE_ID) } returns Optional.empty()
 
         anmodningsperiodeService.finnAnmodningsperiode(ANMODNINGSPERIODE_ID)
         
@@ -57,7 +56,7 @@ class AnmodningsperiodeServiceKtTest {
 
     @Test
     fun `hentAnmodningsperioder`() {
-        every { anmodningsperiodeRepository.findByBehandlingsresultatId(any()) } returns emptyList()
+        every { anmodningsperiodeRepository.findByBehandlingsresultatId(BEHANDLINGS_ID) } returns emptyList()
 
         anmodningsperiodeService.hentAnmodningsperioder(BEHANDLINGS_ID)
         
@@ -65,182 +64,185 @@ class AnmodningsperiodeServiceKtTest {
     }
 
     @Test
-    fun `lagreAnmodningsperioder ingenSvarRegistrert mottarLagredePerioder`() {
+    fun `lagreAnmodningsperioder - ingen svar registrert - mottar lagrede perioder`() {
+        // Arrange
         val anmodningsperiode = lagAnmodningsperiode()
         val anmodningperioder = listOf(anmodningsperiode)
         val behandlingsresultat = Behandlingsresultat()
 
         every { anmodningsperiodeRepository.findByBehandlingsresultatId(BEHANDLINGS_ID) } returns listOf(anmodningsperiode)
         every { behandlingsresultatService.hentBehandlingsresultat(BEHANDLINGS_ID) } returns behandlingsresultat
-        every { anmodningsperiodeRepository.deleteByBehandlingsresultat(any()) } just Runs
-        every { anmodningsperiodeRepository.flush() } just Runs
-        every { anmodningsperiodeRepository.saveAll(any<Collection<Anmodningsperiode>>()) } returns anmodningperioder
+        every { anmodningsperiodeRepository.saveAll(anmodningperioder) } returns anmodningperioder
 
+        // Act
         anmodningsperiodeService.lagreAnmodningsperioder(BEHANDLINGS_ID, anmodningperioder)
 
+        // Assert
         verify { anmodningsperiodeRepository.saveAll(anmodningperioder) }
         anmodningsperiode.behandlingsresultat shouldBe behandlingsresultat
     }
 
     @Test
-    fun `lagreAnmodningsperioder svarErRegistrert forventFunksjonellException`() {
-        val anmodningsperiode = lagAnmodningsperiode().apply {
-            anmodningsperiodeSvar = AnmodningsperiodeSvar()
-        }
-
+    fun `lagreAnmodningsperioder - svar er registrert - forvent funksjonell exception`() {
+        // Arrange
+        val anmodningsperiode = lagAnmodningsperiode()
         every { anmodningsperiodeRepository.findByBehandlingsresultatId(BEHANDLINGS_ID) } returns listOf(anmodningsperiode)
+        anmodningsperiode.anmodningsperiodeSvar = AnmodningsperiodeSvar()
 
-        val exception = assertThrows<FunksjonellException> {
+        // Act & Assert
+        val exception = shouldThrow<FunksjonellException> {
             anmodningsperiodeService.lagreAnmodningsperioder(BEHANDLINGS_ID, listOf(anmodningsperiode))
         }
-
-        exception.message shouldNotBe null
-        exception.message?.contains("svar er registrert") shouldBe true
+        exception.message shouldBe "svar er registrert"
     }
 
     @Test
-    fun `lagreAnmodningsperiodeSvar svarErInnvilgelse lagrerAnmodningsperiodeSvarOgLovvalgsperiode`() {
-        val anmodningsperiode = mockAnmodningsperiodeIdPåFindById()
-
+    fun `lagreAnmodningsperiodeSvar - svar er innvilgelse - lagrer anmodningsperiode svar og lovvalgsperiode`() {
+        // Arrange
+        val anmodningsperiode = mockAnmodningsperiodeIdPaaFindById()
         val svar = AnmodningsperiodeSvar().apply {
             anmodningsperiodeSvarType = Anmodningsperiodesvartyper.INNVILGELSE
-            setAnmodningsperiode(anmodningsperiode)
+            this.anmodningsperiode = anmodningsperiode
         }
 
-        every { anmodningsperiodeSvarRepository.save(any()) } returns svar
-        every { lovvalgsperiodeService.lagreLovvalgsperioder(any(), any()) } just Runs
+        every { anmodningsperiodeSvarRepository.save(any<AnmodningsperiodeSvar>()) } returns svar
+        every { lovvalgsperiodeService.lagreLovvalgsperioder(eq(BEHANDLINGS_ID), any()) } returns emptyList()
 
+        // Act
         anmodningsperiodeService.lagreAnmodningsperiodeSvarMedLovvalgsperiode(ANMODNINGSPERIODE_ID, svar)
 
+        // Assert
         verify { anmodningsperiodeSvarRepository.save(any<AnmodningsperiodeSvar>()) }
         verify { lovvalgsperiodeService.lagreLovvalgsperioder(eq(BEHANDLINGS_ID), any()) }
     }
 
     @Test
-    fun `lagreAnmodningsperiodeSvar svarErAvslag lagrerAnmodningsperiodeSvarOgLovvalgsperiode`() {
-        val anmodningsperiode = mockAnmodningsperiodeIdPåFindById()
-
+    fun `lagreAnmodningsperiodeSvar - svar er avslag - lagrer anmodningsperiode svar og lovvalgsperiode`() {
+        // Arrange
+        val anmodningsperiode = mockAnmodningsperiodeIdPaaFindById()
         val svar = AnmodningsperiodeSvar().apply {
             anmodningsperiodeSvarType = Anmodningsperiodesvartyper.AVSLAG
-            setAnmodningsperiode(anmodningsperiode)
+            this.anmodningsperiode = anmodningsperiode
         }
 
-        every { anmodningsperiodeSvarRepository.save(any()) } returns svar
-        every { lovvalgsperiodeService.lagreLovvalgsperioder(any(), any()) } just Runs
+        every { anmodningsperiodeSvarRepository.save(svar) } returns svar
+        every { lovvalgsperiodeService.lagreLovvalgsperioder(eq(BEHANDLINGS_ID), any()) } returns emptyList()
 
+        // Act
         anmodningsperiodeService.lagreAnmodningsperiodeSvarMedLovvalgsperiode(ANMODNINGSPERIODE_ID, svar)
 
+        // Assert
         verify { anmodningsperiodeSvarRepository.save(svar) }
         verify { lovvalgsperiodeService.lagreLovvalgsperioder(eq(BEHANDLINGS_ID), any()) }
     }
 
     @Test
-    fun `lagreAnmodningsperiodeSvar svarErDelvisInnvilgelseIngenPeriode forventFunksjonellException`() {
-        val anmodningsperiode = mockAnmodningsperiodeIdPåFindById()
-
+    fun `lagreAnmodningsperiodeSvar - svar er delvis innvilgelse ingen periode - forvent funksjonell exception`() {
+        // Arrange
+        val anmodningsperiode = mockAnmodningsperiodeIdPaaFindById()
         val svar = AnmodningsperiodeSvar().apply {
             anmodningsperiodeSvarType = Anmodningsperiodesvartyper.DELVIS_INNVILGELSE
-            setAnmodningsperiode(anmodningsperiode)
+            this.anmodningsperiode = anmodningsperiode
         }
 
-        val exception = assertThrows<FunksjonellException> {
+        // Act & Assert
+        val exception = shouldThrow<FunksjonellException> {
             anmodningsperiodeService.lagreAnmodningsperiodeSvarMedLovvalgsperiode(ANMODNINGSPERIODE_ID, svar)
         }
-
-        exception.message shouldNotBe null
-        exception.message?.contains("Periode må være fyllt ut ved ${Anmodningsperiodesvartyper.DELVIS_INNVILGELSE}") shouldBe true
+        exception.message shouldBe "Periode må være fyllt ut ved ${Anmodningsperiodesvartyper.DELVIS_INNVILGELSE}"
     }
 
     @Test
-    fun `lagreAnmodningsperiodeSvar manglerBehandlingsresultat forventFunksjonellException`() {
-        val anmodningsperiode = mockAnmodningsperiodeIdPåFindById().apply {
-            behandlingsresultat = null
-        }
+    fun `lagreAnmodningsperiodeSvar - mangler behandlingsresultat - forvent funksjonell exception`() {
+        // Arrange
+        val anmodningsperiode = mockAnmodningsperiodeIdPaaFindById()
+        anmodningsperiode.behandlingsresultat = null
 
         val svar = AnmodningsperiodeSvar().apply {
             anmodningsperiodeSvarType = Anmodningsperiodesvartyper.AVSLAG
-            setAnmodningsperiode(anmodningsperiode)
+            this.anmodningsperiode = anmodningsperiode
         }
 
-        val exception = assertThrows<IllegalStateException> {
+        // Act & Assert
+        val exception = shouldThrow<IllegalStateException> {
             anmodningsperiodeService.lagreAnmodningsperiodeSvarMedLovvalgsperiode(ANMODNINGSPERIODE_ID, svar)
         }
-
-        exception.message shouldNotBe null
-        exception.message?.contains(Anmodningsperiode.FEIL_VED_HENT_BEHANDLINGSRESULTAT_ID.format(ANMODNINGSPERIODE_ID)) shouldBe true
+        exception.message shouldBe Anmodningsperiode.FEIL_VED_HENT_BEHANDLINGSRESULTAT_ID.format(ANMODNINGSPERIODE_ID)
     }
 
     @Test
-    fun `lagreAnmodningsperiodeSvar svarManglerType forventFunksjonellException`() {
-        mockAnmodningsperiodeIdPåFindById()
+    fun `lagreAnmodningsperiodeSvar - svar mangler type - forvent funksjonell exception`() {
+        // Arrange
+        mockAnmodningsperiodeIdPaaFindById()
         val svar = AnmodningsperiodeSvar()
 
-        val exception = assertThrows<FunksjonellException> {
+        // Act & Assert
+        val exception = shouldThrow<FunksjonellException> {
             anmodningsperiodeService.lagreAnmodningsperiodeSvarMedLovvalgsperiode(ANMODNINGSPERIODE_ID, svar)
         }
-
-        exception.message shouldNotBe null
-        exception.message?.contains("Må spesifiseres svarType for svar på anmodningsperiode") shouldBe true
+        exception.message shouldBe "Må spesifiseres svarType for svar på anmodningsperiode"
     }
 
     @Test
-    fun `lagreAnmodningsperiodeSvar ugyldigPeriodeForDelvisInnvilgelse forventFunksjonellException`() {
-        mockAnmodningsperiodeIdPåFindById()
+    fun `lagreAnmodningsperiodeSvar - ugyldig periode for delvis innvilgelse - forvent funksjonell exception`() {
+        // Arrange
+        mockAnmodningsperiodeIdPaaFindById()
         val svar = AnmodningsperiodeSvar().apply {
             anmodningsperiodeSvarType = Anmodningsperiodesvartyper.DELVIS_INNVILGELSE
             innvilgetFom = LocalDate.now()
             innvilgetTom = LocalDate.now().minusYears(2)
         }
 
-        val exception = assertThrows<FunksjonellException> {
+        // Act & Assert
+        val exception = shouldThrow<FunksjonellException> {
             anmodningsperiodeService.lagreAnmodningsperiodeSvarMedLovvalgsperiode(ANMODNINGSPERIODE_ID, svar)
         }
-
-        exception.message shouldNotBe null
-        exception.message?.contains("Periode er ikke gyldig") shouldBe true
+        exception.message shouldBe "Periode er ikke gyldig"
     }
 
     @Test
-    fun `oppdaterAnmodningsperiodeSendtForBehandling verifiserOppdatert`() {
+    fun `oppdaterAnmodningsperiodeSendtForBehandling - verifiser oppdatert`() {
+        // Arrange
         val anmodningsperiode = Anmodningsperiode()
-        
-        every { anmodningsperiodeRepository.findByBehandlingsresultatId(any()) } returns listOf(anmodningsperiode)
-        every { anmodningsperiodeRepository.save(any()) } returns anmodningsperiode
+        every { anmodningsperiodeRepository.findByBehandlingsresultatId(any<Long>()) } returns listOf(anmodningsperiode)
+        every { anmodningsperiodeRepository.save(anmodningsperiode) } returns anmodningsperiode
 
+        // Act
         anmodningsperiodeService.oppdaterAnmodningsperiodeSendtForBehandling(1L)
 
-        anmodningsperiode.erSendtUtland() shouldBe true
+        // Assert
+        anmodningsperiode.erSendtUtland().shouldBeTrue()
         verify { anmodningsperiodeRepository.save(anmodningsperiode) }
     }
 
     @Test
-    fun `oppdaterAnmodetAvForBehandling erIkkeSattFraFør oppdateres`() {
+    fun `oppdaterAnmodetAvForBehandling - er ikke satt fra før - oppdateres`() {
+        // Arrange
         val anmodetAv = "MEG"
         val anmodningsperiode = Anmodningsperiode()
-        
-        every { anmodningsperiodeRepository.findByBehandlingsresultatId(any()) } returns listOf(anmodningsperiode)
-        every { anmodningsperiodeRepository.save(any()) } returns anmodningsperiode
+        every { anmodningsperiodeRepository.findByBehandlingsresultatId(any<Long>()) } returns listOf(anmodningsperiode)
+        every { anmodningsperiodeRepository.save(anmodningsperiode) } returns anmodningsperiode
 
+        // Act
         anmodningsperiodeService.oppdaterAnmodetAvForBehandling(1L, anmodetAv)
 
+        // Assert
         anmodningsperiode.anmodetAv shouldBe anmodetAv
         verify { anmodningsperiodeRepository.save(anmodningsperiode) }
     }
 
     @Test
-    fun `oppdaterAnmodetAvForBehandling erSattFraFør kasterException`() {
-        val anmodningsperiode = Anmodningsperiode().apply {
-            anmodetAv = "DEG"
-        }
-        
-        every { anmodningsperiodeRepository.findByBehandlingsresultatId(any()) } returns listOf(anmodningsperiode)
+    fun `oppdaterAnmodetAvForBehandling - er satt fra før - kaster exception`() {
+        // Arrange
+        val anmodningsperiode = Anmodningsperiode()
+        anmodningsperiode.anmodetAv = "DEG"
+        every { anmodningsperiodeRepository.findByBehandlingsresultatId(any<Long>()) } returns listOf(anmodningsperiode)
 
-        val exception = assertThrows<FunksjonellException> {
+        // Act & Assert
+        val exception = shouldThrow<FunksjonellException> {
             anmodningsperiodeService.oppdaterAnmodetAvForBehandling(1L, "MEG")
         }
-
-        exception.message shouldNotBe null
-        exception.message?.contains("allerede anmodet av DEG") shouldBe true
+        exception.message shouldBe "allerede anmodet av DEG"
     }
 
     private fun lagAnmodningsperiode(): Anmodningsperiode {
@@ -249,18 +251,15 @@ class AnmodningsperiodeServiceKtTest {
             Land_iso2.NO,
             Lovvalgbestemmelser_883_2004.FO_883_2004_ART16_1,
             null, Land_iso2.SE, Lovvalgbestemmelser_883_2004.FO_883_2004_ART13_1A, Trygdedekninger.FULL_DEKNING_EOSFO
-        ).apply {
-            id = ANMODNINGSPERIODE_ID
-        }
-        
-        val behandlingsresultat = Behandlingsresultat().apply {
-            id = BEHANDLINGS_ID
-        }
+        )
+        anmodningsperiode.id = ANMODNINGSPERIODE_ID
+        val behandlingsresultat = Behandlingsresultat()
+        behandlingsresultat.id = BEHANDLINGS_ID
         anmodningsperiode.behandlingsresultat = behandlingsresultat
         return anmodningsperiode
     }
 
-    private fun mockAnmodningsperiodeIdPåFindById(): Anmodningsperiode {
+    private fun mockAnmodningsperiodeIdPaaFindById(): Anmodningsperiode {
         val anmodningsperiode = lagAnmodningsperiode()
         every { anmodningsperiodeRepository.findById(ANMODNINGSPERIODE_ID) } returns Optional.of(anmodningsperiode)
         return anmodningsperiode
