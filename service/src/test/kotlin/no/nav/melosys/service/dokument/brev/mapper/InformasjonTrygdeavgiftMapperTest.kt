d81708ca2871f68d827152d8cf2efe63a7d25e2c
Francois Allix <francois.allix@nav.no>
2025-08-08 15:00:14 +0200

    Fjern ubrukt mock i test

--- service/src/test/kotlin/no/nav/melosys/service/dokument/brev/mapper/InformasjonTrygdeavgiftMapperTest.kt
+++ service/src/test/kotlin/no/nav/melosys/service/dokument/brev/mapper/InformasjonTrygdeavgiftMapperTest.kt
@@ -7,29 +7,21 @@ import io.mockk.every
 import io.mockk.impl.annotations.MockK
 import io.mockk.junit5.MockKExtension
 import no.nav.melosys.domain.Behandlingsresultat
-import no.nav.melosys.domain.Medlemskapsperiode
 import no.nav.melosys.domain.avgift.Inntektsperiode
 import no.nav.melosys.domain.avgift.Penger
 import no.nav.melosys.domain.avgift.SkatteforholdTilNorge
 import no.nav.melosys.domain.avgift.Trygdeavgiftsperiode
 import no.nav.melosys.domain.brev.DokgenBrevbestilling
-import no.nav.melosys.domain.brev.InnvilgelseFtrlYrkesaktivFrivilligBrevbestilling
 import no.nav.melosys.domain.helseutgiftdekkesperiode.HelseutgiftDekkesPeriode
 import no.nav.melosys.domain.kodeverk.*
 import no.nav.melosys.service.avgift.TrygdeavgiftMottakerService
 import no.nav.melosys.service.avgift.TrygdeavgiftsberegningService
-import no.nav.melosys.service.behandling.BehandlingsresultatService
 import no.nav.melosys.service.dokument.DokgenTestData
-import no.nav.melosys.service.dokument.brev.mapper.InnvilgelseFtrlPensjonistMapperTest.Companion.BEGRUNNELSE_FRITEKST
-import no.nav.melosys.service.dokument.brev.mapper.InnvilgelseFtrlPensjonistMapperTest.Companion.Case
-import no.nav.melosys.service.dokument.brev.mapper.InnvilgelseFtrlPensjonistMapperTest.Companion.INNLEDNING_FRITEKST
 import no.nav.melosys.service.dokument.brev.mapper.InnvilgelseFtrlPensjonistMapperTest.Companion.SAKSBEHANDLER_NAVN
-import no.nav.melosys.service.dokument.brev.mapper.InnvilgelseFtrlPensjonistMapperTest.Companion.TRYGDEAVGIFT_FRITEKST
 import no.nav.melosys.service.helseutgiftdekkesperiode.HelseutgiftDekkesPeriodeService
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
-import org.mockito.kotlin.anyArray
 import java.math.BigDecimal
 import java.time.Instant
 import java.time.LocalDate
@@ -39,9 +31,6 @@ internal class InformasjonTrygdeavgiftMapperTest {
 
     @MockK
     private lateinit var mockDokgenMapperDatahenter: DokgenMapperDatahenter
-
-    @MockK
-    private lateinit var mockBehandlingsresultatService: BehandlingsresultatService
 
     @MockK
     private lateinit var mockTrygdeavgiftsberegningService: TrygdeavgiftsberegningService
@@ -73,7 +62,7 @@ internal class InformasjonTrygdeavgiftMapperTest {
         every { mockHelseutgiftDekkesPeriodeService.hentHelseutgiftDekkesPeriode(any()) } returns behandlingsresultat.helseutgiftDekkesPeriode
         every { mockTrygdeavgiftMottakerService.getTrygdeavgiftMottaker(any<List<Trygdeavgiftsperiode>>()) } returns Trygdeavgiftmottaker.TRYGDEAVGIFT_BETALES_TIL_NAV
         every { mockDokgenMapperDatahenter.hentBehandlingsresultat(ofType()) } returns behandlingsresultat
-        every { mockBehandlingsresultatService.hentBehandlingsresultat(ofType()) } returns behandlingsresultat
+
 
         informasjonTrygdeavgiftMapper.mapInformasjonTrygdeavgift(lagBrevbestilling()).shouldNotBeNull().apply {
             fomDato shouldBe behandlingsresultat.helseutgiftDekkesPeriode.fomDato
