54bbb109778d98a5a65022acb0de6a9168ed6386
Francois Allix <francois.allix@nav.no>
2025-08-12 17:08:45 +0200

    `org.springframework.http` for header- og mediatype-konstanter

--- integrasjon/src/main/kotlin/no/nav/melosys/integrasjon/sak/SakConsumerConfig.kt
+++ integrasjon/src/main/kotlin/no/nav/melosys/integrasjon/sak/SakConsumerConfig.kt
@@ -1,12 +1,12 @@
 package no.nav.melosys.integrasjon.sak
 
-import jakarta.ws.rs.core.HttpHeaders
-import jakarta.ws.rs.core.MediaType
 import no.nav.melosys.integrasjon.felles.WebClientConfig
 import no.nav.melosys.integrasjon.felles.mdc.CorrelationIdOutgoingFilter
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpHeaders
+import org.springframework.http.MediaType
 import org.springframework.web.reactive.function.client.WebClient
 
 @Configuration
@@ -20,7 +20,7 @@ class SakConsumerConfig(
     ): SakConsumerInterface {
         val webClient = webClientBuilder
             .baseUrl(url)
-            .defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON)
+            .defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
             .filter(correlationIdOutgoingFilter)
             .filter(errorFilter("Kall mot Sak API feilet"))
             .build()
--- integrasjon/src/main/kotlin/no/nav/melosys/integrasjon/sak/WebClientSakConsumer.kt
+++ integrasjon/src/main/kotlin/no/nav/melosys/integrasjon/sak/WebClientSakConsumer.kt
@@ -1,10 +1,10 @@
 package no.nav.melosys.integrasjon.sak
 
-import jakarta.ws.rs.core.HttpHeaders
 import no.nav.melosys.exception.TekniskException
 import no.nav.melosys.integrasjon.felles.BasicAuthAware
 import no.nav.melosys.sikkerhet.context.SubjectHandler
 import no.nav.melosys.sikkerhet.context.ThreadLocalAccessInfo
+import org.springframework.http.HttpHeaders
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.bodyToMono
 
