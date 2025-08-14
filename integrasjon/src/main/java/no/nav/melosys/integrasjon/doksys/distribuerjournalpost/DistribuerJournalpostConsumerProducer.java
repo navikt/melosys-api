6e98278fc21f1113d9cd08c696ebb7ace5541fc9
oyvinyg <oyvinyg@gmail.com>
2025-08-14 14:02:11 +0200

    Sette headers og feilhåndtering i bean

--- integrasjon/src/main/java/no/nav/melosys/integrasjon/doksys/distribuerjournalpost/DistribuerJournalpostConsumerProducer.java
+++ integrasjon/src/main/java/no/nav/melosys/integrasjon/doksys/distribuerjournalpost/DistribuerJournalpostConsumerProducer.java
@@ -3,19 +3,23 @@ package no.nav.melosys.integrasjon.doksys.distribuerjournalpost;
 import io.getunleash.Unleash;
 import no.nav.melosys.integrasjon.felles.GenericAuthFilterFactory;
 import no.nav.melosys.integrasjon.felles.SystemContextClientRequestInterceptor;
+import no.nav.melosys.integrasjon.felles.WebClientConfig;
 import no.nav.melosys.integrasjon.felles.mdc.CorrelationIdOutgoingFilter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.util.DefaultUriBuilderFactory;
 
+import java.util.List;
+
 import static no.nav.melosys.featuretoggle.ToggleName.MELOSYS_DISTJOURNALPOST_BRUK_WEBCLIENT_MED_AD_TOKEN;
 
 @Configuration
-public class DistribuerJournalpostConsumerProducer {
+public class DistribuerJournalpostConsumerProducer implements WebClientConfig {
 
     @Bean
     public DistribuerJournalpostConsumer distribuerJournalpostConsumer(
@@ -32,8 +36,13 @@ public class DistribuerJournalpostConsumerProducer {
             return new DistribuerJournalpostConsumerWebClientImpl(
                 webclientBuilder
                     .baseUrl(url)
+                    .defaultHeaders(httpHeaders -> {
+                        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+                        httpHeaders.setAccept(List.of(MediaType.APPLICATION_JSON));
+                    })
                     .filter(genericAuthFilterFactory.getAzureFilter("dokdistfordeling"))
                     .filter(correlationIdOutgoingFilter)
+                    .filter(errorFilter("Kall mot distribuer journalpost feilet."))
                     .build()
             );
         }
