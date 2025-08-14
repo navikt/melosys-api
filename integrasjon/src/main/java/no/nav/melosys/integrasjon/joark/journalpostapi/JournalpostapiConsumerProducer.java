420ab1ed398facc2134f0c3290acc9d931c51860
oyvinyg <oyvinyg@gmail.com>
2025-08-14 09:37:15 +0200

    Bruke kotlin for ny implementasjon og sette opp default headers og feilhåndtering i bean

--- integrasjon/src/main/java/no/nav/melosys/integrasjon/joark/journalpostapi/JournalpostapiConsumerProducer.java
+++ integrasjon/src/main/java/no/nav/melosys/integrasjon/joark/journalpostapi/JournalpostapiConsumerProducer.java
@@ -1,14 +1,18 @@
 package no.nav.melosys.integrasjon.joark.journalpostapi;
 
+import java.util.Collections;
+
 import io.getunleash.Unleash;
 import no.nav.melosys.integrasjon.felles.GenericAuthFilterFactory;
 import no.nav.melosys.integrasjon.felles.SystemContextClientRequestInterceptor;
+import no.nav.melosys.integrasjon.felles.WebClientConfig;
 import no.nav.melosys.integrasjon.felles.mdc.CorrelationIdOutgoingFilter;
 import no.nav.melosys.integrasjon.felles.mdc.CorrelationIdOutgoingInterceptor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -17,7 +21,7 @@ import org.springframework.web.util.DefaultUriBuilderFactory;
 import static no.nav.melosys.featuretoggle.ToggleName.MELOSYS_DOKARKIV_BRUK_WEBCLIENT_MED_AD_TOKEN;
 
 @Configuration
-public class JournalpostapiConsumerProducer {
+public class JournalpostapiConsumerProducer implements WebClientConfig {
 
     @Bean
     public JournalpostapiConsumer journalpostapiConsumer(
@@ -37,6 +41,11 @@ public class JournalpostapiConsumerProducer {
                     .baseUrl(url)
                     .filter(genericAuthFilterFactory.getAzureFilter("dokarkiv"))
                     .filter(correlationIdOutgoingFilter)
+                    .filter(errorFilter("Kall mot journalpostapi feilet."))
+                    .defaultHeaders( httpHeaders -> {
+                        httpHeaders.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+                        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+                    })
                     .build()
             );
         }
