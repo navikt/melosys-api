42ad7d6c58ae1da00deb02bd18bf36b0cd5b0974
barstadEkstern <oystein.madsen.barstad@nav.no>
2025-09-08 09:25:57 +0200

    glemte å lage query.

--- repository/src/main/java/no/nav/melosys/repository/BehandlingRepository.java
+++ repository/src/main/java/no/nav/melosys/repository/BehandlingRepository.java
@@ -5,6 +5,7 @@ import java.util.Collection;
 import no.nav.melosys.domain.Behandling;
 import no.nav.melosys.domain.kodeverk.behandlinger.Behandlingsstatus;
 import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.lang.Nullable;
 
@@ -14,6 +15,7 @@ public interface BehandlingRepository extends CrudRepository<Behandling, Long> {
     @Nullable
     Behandling findWithSaksopplysningerById(Long behandlingID);
 
+    @Query("SELECT b.id FROM Behandling b WHERE b.status = :status")
     Collection<Long> findIdsByStatus(Behandlingsstatus behandlingsstatus);
 
 }
