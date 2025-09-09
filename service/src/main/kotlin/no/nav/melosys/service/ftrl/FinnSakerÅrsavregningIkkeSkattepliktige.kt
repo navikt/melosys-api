70e23d99cda692aec3073f0ef14477b1debd6529
Rune Lind <rune.lind@nav.no>
2025-09-09 12:40:23 +0200

    7504 G4P Implementerer filtering og dryrun for ikke-skattepliktige saker i årsavregning (#3001)
    Legger til funksjonalitet for å finne og filtrere saker for årsavregning av ikke-skattepliktige personer. Inkluderer mulighet for dryrun-kjøring med JSON-rapport for testing før faktisk kjøring.
    
    - Ny service for å finne FTRL-fagsaker som ikke er skattepliktige
    - Controller med REST-endepunkt for dryrun og filtrering
    - Forbedret filtering på hvilke saker som skal prosesseres
    - JSON-rapportering for dryrun-resultater

---
+++ service/src/main/kotlin/no/nav/melosys/service/ftrl/FinnSakerÅrsavregningIkkeSkattepliktige.kt
@@ -0,0 +1,162 @@
+package no.nav.melosys.service.ftrl
+
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import mu.KotlinLogging
+import no.nav.melosys.domain.Behandling
+import no.nav.melosys.domain.Fagsak
+import no.nav.melosys.service.JobMonitor
+import no.nav.melosys.sikkerhet.context.ThreadLocalAccessInfo
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.CrudRepository
+import org.springframework.data.repository.query.Param
+import org.springframework.scheduling.annotation.Async
+import org.springframework.stereotype.Component
+import org.springframework.transaction.annotation.Transactional
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.util.*
+
+private val log = KotlinLogging.logger { }
+
+@Component
+class FinnSakerÅrsavregningIkkeSkattepliktige(
+    private val sakerForÅrsavregningRepository: SakerÅrsavregningIkkeSkattepliktigeRepository
+) {
+    val sakerFunnet: MutableList<SakMedBehandlinger> = mutableListOf()
+
+    private val jobMonitor = JobMonitor(
+        jobName = "FinnSakerForÅrsavregningIkkeSkattepliktige",
+        stats = JobStatus()
+    )
+
+    fun sakerFunnetJsonString(): String = jacksonObjectMapper()
+        .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
+        .registerModule(JavaTimeModule())
+        .valueToTree<JsonNode>(sakerFunnet.map { it.toMap() }).toPrettyString()
+
+    @Async("taskExecutor")
+    @Transactional(readOnly = true)
+    fun finnSakerAsynkront(dryrun: Boolean, antallFeilFørStopAvJob: Int, saksnummer: String?, fomDato: LocalDate, tomDato: LocalDate) {
+        require(antallFeilFørStopAvJob >= 0) { "antallFeilFørStopAvJob må være 0 eller positiv" }
+        finnSaker(dryrun, antallFeilFørStopAvJob, fomDato, tomDato)
+    }
+
+    @Synchronized
+    @Transactional(readOnly = true)
+    fun finnSaker(
+        dryrun: Boolean,
+        antallFeilFørStopAvJob: Int = 0,
+        fomDato: LocalDate,
+        tomDato: LocalDate
+    ) = runAsSystem {
+        log.info {
+            "Starter søk etter saker for årsavregning ikke-skattepliktige " +
+                "\n dryrun: $dryrun" +
+                "\n medlemskapsperiode fom: $fomDato" +
+                "\n medlemskapsperiode tom: $tomDato"
+        }
+        jobMonitor.execute(antallFeilFørStopAvJob) {
+            finnSakerMedBehandlinger(fomDato, tomDato)
+                .onEach { antallFunnet++ }
+                .forEach {
+                    if (jobMonitor.shouldStop) return@execute
+                    sakerFunnet.add(it)
+                    antallProsessert++
+                    if (dryrun) return@forEach
+                    // TODO: bruk prosessinstansService.opprettArsavregningsBehandlingProsessflyt
+                }
+        }
+    }
+
+    private fun finnSakerMedBehandlinger(fomDato: LocalDate, tomDato: LocalDate): List<SakMedBehandlinger> =
+        sakerForÅrsavregningRepository.finnFTRLBehandlinger(fomDato = fomDato, tomDato = tomDato)
+            .groupBy { it.fagsak }
+            .map { (fagsak, behandlinger) ->
+                SakMedBehandlinger(fagsak, behandlinger.sortedByDescending { it.endretDato })
+            }.also { jobMonitor.stats.dbQueryStoppedAt = LocalDateTime.now() }
+
+    private fun <T> runAsSystem(prosessSteg: String = "finnSakerHvorÅrsavregningSkalOpprettes", block: () -> T): T {
+        val processId = UUID.randomUUID()
+        ThreadLocalAccessInfo.beforeExecuteProcess(processId, prosessSteg)
+        return try {
+            block()
+        } finally {
+            ThreadLocalAccessInfo.afterExecuteProcess(processId)
+        }
+    }
+
+    fun status() = jobMonitor.status()
+
+    inner class JobStatus(
+        @Volatile var antallFunnet: Int = 0,
+        @Volatile var antallProsessert: Int = 0,
+        @Volatile var dbQueryStoppedAt: LocalDateTime? = null,
+    ) : JobMonitor.Stats {
+        override fun reset() {
+            antallFunnet = 0
+            antallProsessert = 0
+            dbQueryStoppedAt = null
+        }
+
+        override fun asMap(): Map<String, Any?> = mapOf(
+            "dbQueryRuntime" to jobMonitor.durationUntil(dbQueryStoppedAt),
+            "antallFunnet" to antallFunnet,
+            "antallProsessert" to antallProsessert,
+        )
+    }
+
+    data class SakMedBehandlinger(
+        val sak: Fagsak,
+        val behandlinger: List<Behandling>,
+    ) {
+        fun toMap(): Map<String, Any?> = sak.toMap(true)
+
+        private fun Behandling.toMap(inkluderFagsak: Boolean = true): Map<String, Any?> = mapOf(
+            "id" to id,
+            "status" to status.name,
+            "type" to type.name,
+            "tema" to tema.name,
+            "registrertDato" to registrertDato,
+            "endretDato" to endretDato,
+            "fagsak" to if (inkluderFagsak) fagsak.toMap(inkluderBehandlinger = false) else null
+        ).filterValues { it != null }
+
+        private fun Fagsak.toMap(inkluderBehandlinger: Boolean = true) = mapOf(
+            "saksnummer" to saksnummer,
+            "gsakSaksnummer" to gsakSaksnummer,
+            "type" to type.name,
+            "tema" to tema.name,
+            "status" to status.name,
+            "betalingsvalg" to betalingsvalg?.name,
+            "behandlinger" to if (inkluderBehandlinger) this.behandlinger.map { behandling -> behandling.toMap(false) } else null
+        ).filterValues { it != null }
+    }
+
+}
+
+interface SakerÅrsavregningIkkeSkattepliktigeRepository : CrudRepository<Behandling, Long> {
+    @Query(
+        """
+        select distinct b
+            FROM Behandlingsresultat br
+            JOIN br.behandling b
+            JOIN br.medlemskapsperioder mp
+            JOIN br.vedtakMetadata vm
+            JOIN b.fagsak f
+            JOIN mp.trygdeavgiftsperioder tap
+            JOIN tap.grunnlagSkatteforholdTilNorge stn
+        where f.type = 'FTRL'
+            and mp.fom >= :fomDato
+            and mp.tom < :tomDato
+            and f.status = 'LOVVALG_AVKLART'
+            and stn.skatteplikttype = 'IKKE_SKATTEPLIKTIG'
+        """
+    )
+    fun finnFTRLBehandlinger(
+        @Param("fomDato") fomDato: LocalDate,
+        @Param("tomDato") tomDato: LocalDate,
+    ): List<Behandling>
+}
---
+++ service/src/main/kotlin/no/nav/melosys/service/ftrl/FinnSakerÅrsavregningIkkeSkattepliktigeController.kt
@@ -0,0 +1,47 @@
+package no.nav.melosys.service.ftrl
+
+import mu.KotlinLogging
+import no.nav.security.token.support.core.api.Protected
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+import java.time.LocalDate
+
+private val log = KotlinLogging.logger { }
+
+@Protected
+@RestController
+@RequestMapping("/admin/ftrl/finn-saker-for-årsavregning-ikke-skattepliktige")
+class FinnSakerÅrsavregningIkkeSkattepliktigeController(
+    private val finnSakerÅrsavregningIkkeSkattepliktige: FinnSakerÅrsavregningIkkeSkattepliktige
+) {
+
+    @PostMapping("/finn")
+    fun finnPersonerOgSendVedtakMeldinger(
+        @RequestParam(required = true, defaultValue = "true") fomDato: LocalDate,
+        @RequestParam(required = true, defaultValue = "true") tomDato: LocalDate,
+        @RequestParam(required = false, defaultValue = "true") dryrun: Boolean,
+        @RequestParam(required = false, defaultValue = "0") antallFeilFørStopAvJob: Int,
+        @RequestParam(required = false) saksnummer: String?,
+    ): ResponseEntity<Unit> {
+        log.info(
+            "finnSakerÅrsavregningIkkeSkattepliktige - dryrun $dryrun, " +
+                "antallFeilFørStopAvJob: $antallFeilFørStopAvJob saksnummer: $saksnummer"
+        )
+
+        finnSakerÅrsavregningIkkeSkattepliktige.finnSakerAsynkront(dryrun, antallFeilFørStopAvJob, saksnummer, fomDato, tomDato)
+
+        return ResponseEntity.noContent().build()
+    }
+
+    @GetMapping("/status")
+    fun status(): ResponseEntity<Map<String, Any?>> =
+        ResponseEntity<Map<String, Any?>>(finnSakerÅrsavregningIkkeSkattepliktige.status(), HttpStatus.OK)
+
+    @GetMapping("/jsonrapport", produces = [MediaType.APPLICATION_JSON_VALUE])
+    fun jsonrapport(): ResponseEntity<String> {
+        return ResponseEntity(finnSakerÅrsavregningIkkeSkattepliktige.sakerFunnetJsonString(), HttpStatus.OK)
+    }
+
+}
