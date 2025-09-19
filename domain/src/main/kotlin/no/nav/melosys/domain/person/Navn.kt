57d5ea5fa7d8afec35b729d112fa83c36ddeaccf
RuneLind <rulind@gmail.com>
2025-09-19 14:44:25 +0200

    Fjerne metoder som kun ble brukt i test
    Fjern testen også

--- domain/src/main/kotlin/no/nav/melosys/domain/person/Navn.kt
+++ domain/src/main/kotlin/no/nav/melosys/domain/person/Navn.kt
@@ -8,37 +8,9 @@ data class Navn(
     val mellomnavn: String?,
     val etternavn: String?
 ) {
-    fun tilSammensattNavn(): String {
-        return (etternavn + leggTilMellomnavn() + " " + fornavn).trim()
-    }
+    fun tilSammensattNavn(): String = (etternavn + leggTilMellomnavn() + " " + fornavn).trim()
 
-    private fun leggTilMellomnavn(): String {
-        return if (mellomnavn == null) "" else " $mellomnavn"
-    }
+    private fun leggTilMellomnavn(): String = if (mellomnavn == null) "" else " $mellomnavn"
 
-    fun harLiktFornavn(navn: String): Boolean {
-        return nonNull(fornavn) && fornavn == navn
-    }
-
-    companion object {
-        // A B C -> C, A B
-        @JvmStatic
-        fun navnEtternavnFørst(fulltNavnEtternavnSist: String): String {
-            val splittetNavn = fulltNavnEtternavnSist.split(" ")
-            val etternavn = splittetNavn[splittetNavn.size - 1]
-            val forOgMellomnavn = splittetNavn.take(splittetNavn.size - 1).joinToString(" ")
-            return "$etternavn, $forOgMellomnavn"
-        }
-
-        // C, A B -> A B C
-        @JvmStatic
-        fun navnEtternavnSist(fulltNavnEtternavnFørst: String): String {
-            if (!fulltNavnEtternavnFørst.contains(",")) return fulltNavnEtternavnFørst
-
-            val splittetNavn = fulltNavnEtternavnFørst.split(" ")
-            val etternavn = splittetNavn[0].substring(0, splittetNavn[0].length - 1)
-            val forOgMellomnavn = splittetNavn.drop(1).joinToString(" ")
-            return "$forOgMellomnavn $etternavn"
-        }
-    }
+    fun harLiktFornavn(navn: String): Boolean = nonNull(fornavn) && fornavn == navn
 }
