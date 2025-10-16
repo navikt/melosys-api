65b072f27e0b84bc33aa7bcc8bd5d07508da313c
RuneLind <rulind@gmail.com>
2025-10-16 12:51:05 +0200

    Log kun pensjonsopptjeningHendelse når vi ikke er i prod

--- integrasjon/src/main/kotlin/no/nav/melosys/integrasjon/hendelser/KafkaMelosysPoppHendelseProducer.kt
+++ integrasjon/src/main/kotlin/no/nav/melosys/integrasjon/hendelser/KafkaMelosysPoppHendelseProducer.kt
@@ -1,5 +1,7 @@
 package no.nav.melosys.integrasjon.hendelser
 
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.ObjectMapper
 import mu.KotlinLogging
 import no.nav.melosys.config.MDCOperations
 import no.nav.melosys.exception.TekniskException
@@ -8,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.core.env.Environment
 import org.springframework.stereotype.Component
 import java.util.concurrent.TimeUnit
 
@@ -16,18 +19,19 @@ private val log = KotlinLogging.logger { }
 @Component
 class KafkaPensjonsopptjeningHendelseProducer(
     @Value("\${kafka.aiven.melosys-popp-hendelser.topic}") private val topicName: String,
-    @Qualifier("melosysPoppHendelse") @Autowired private val kafkaTemplate: KafkaTemplate<String, PensjonsopptjeningHendelse>
+    @Qualifier("melosysPoppHendelse") @Autowired private val kafkaTemplate: KafkaTemplate<String, PensjonsopptjeningHendelse>,
+    @Autowired private val objectMapper: ObjectMapper,
+    private val environment: Environment
 ) {
     fun sendPensjonsopptjeningHendelse(pensjonsopptjeningHendelse: PensjonsopptjeningHendelse) {
         val hendelseRecord = ProducerRecord<String, PensjonsopptjeningHendelse>(topicName, pensjonsopptjeningHendelse)
         hendelseRecord.headers().add(MDCOperations.CORRELATION_ID, MDCOperations.getCorrelationId().encodeToByteArray())
-        val future = kafkaTemplate.send(hendelseRecord)
-
+        val completableFuture = kafkaTemplate.send(hendelseRecord)
         try {
-            val sendeResultat = future[15L, TimeUnit.SECONDS]
+            val resultat = completableFuture[15L, TimeUnit.SECONDS]
             log.info(
-                "PensjonsopptjeningHendelse sendt på topic $topicName med offset: ${sendeResultat.recordMetadata.offset()} \n" +
-                    "data: $pensjonsopptjeningHendelse"
+                "PensjonsopptjeningHendelse sendt på topic $topicName med offset: ${resultat.recordMetadata.offset()}" +
+                    formatHendelseDataForLogging(pensjonsopptjeningHendelse)
             )
         } catch (e: InterruptedException) {
             Thread.currentThread().interrupt()
@@ -36,4 +40,14 @@ class KafkaPensjonsopptjeningHendelseProducer(
             throw TekniskException("Kunne ikke sende pensjonsopptjeningHendelse $pensjonsopptjeningHendelse", e)
         }
     }
+
+    private fun formatHendelseDataForLogging(
+        pensjonsopptjeningHendelse: PensjonsopptjeningHendelse
+    ): String = when {
+        environment.activeProfiles.contains("nais") -> ""
+        else -> "\ndata: ${pensjonsopptjeningHendelse.toJsonNode().toPrettyString()}"
+    }
+
+    private fun Any.toJsonNode(): JsonNode = objectMapper.valueToTree(this)
+
 }
