1e43a248dbff4a7793ed53fbea013d7195999db2
RuneLind <rulind@gmail.com>
2025-10-19 21:20:57 +0200

    Bruk kotest istenden for json

--- saksflyt/src/test/kotlin/no/nav/melosys/saksflyt/DSLTest.kt
+++ saksflyt/src/test/kotlin/no/nav/melosys/saksflyt/DSLTest.kt
@@ -11,6 +11,7 @@ import io.kotest.matchers.types.shouldBeInstanceOf
 import no.nav.melosys.domain.Behandling
 import no.nav.melosys.domain.Behandlingsresultat
 import no.nav.melosys.domain.Fagsak
+import no.nav.melosys.domain.FagsakTestFactory
 import no.nav.melosys.domain.SaksopplysningType
 import no.nav.melosys.domain.avgift.Penger
 import no.nav.melosys.domain.avgift.Trygdeavgiftsperiode
@@ -32,6 +33,7 @@ import no.nav.melosys.domain.kodeverk.Skatteplikttype
 import no.nav.melosys.domain.medlemskapsperiode
 import no.nav.melosys.domain.dokument.organisasjon.OrganisasjonDokument
 import no.nav.melosys.domain.dokument.person.PersonDokument
+import no.nav.melosys.domain.kodeverk.Aktoersroller
 import no.nav.melosys.domain.mottatteopplysninger.AnmodningEllerAttest
 import no.nav.melosys.domain.mottatteopplysninger.Soeknad
 import no.nav.melosys.domain.mottatteopplysninger.anmodningEllerAttest
@@ -59,27 +61,18 @@ class DSLTest {
         }
 
         fagsak.run {
-            behandlinger[0].fagsak shouldBe fagsak
-            behandlinger[1].fagsak shouldBe fagsak
-            toMap().toJsonString() shouldEqualJson """
-            {
-              "saksnummer" : "MEL-test",
-              "type" : "EU_EOS",
-              "tema" : "TRYGDEAVGIFT",
-              "status" : "OPPRETTET",
-              "aktører" : [ ],
-              "behandlinger" : [ {
-                "id" : 1,
-                "status" : "UNDER_BEHANDLING",
-                "type" : "FØRSTEGANG",
-                "tema" : "REGISTRERING_UNNTAK_NORSK_TRYGD_UTSTASJONERING"
-              }, {
-                "id" : 2,
-                "status" : "UNDER_BEHANDLING",
-                "type" : "FØRSTEGANG",
-                "tema" : "REGISTRERING_UNNTAK_NORSK_TRYGD_UTSTASJONERING"
-              } ]
-            }"""
+            saksnummer shouldBe FagsakTestFactory.SAKSNUMMER
+            tema shouldBe Sakstemaer.TRYGDEAVGIFT
+            behandlinger.shouldHaveSize(2).run {
+                elementAt(0).also { behandling ->
+                    behandling.id shouldBe 1L
+                    behandling.fagsak shouldBe fagsak
+                }
+                elementAt(1).also { behandling ->
+                    behandling.id shouldBe 2L
+                    behandling.fagsak shouldBe fagsak
+                }
+            }
         }
     }
 
@@ -93,35 +86,27 @@ class DSLTest {
         }
 
         behandling.run {
-            fagsak shouldBe behandling.fagsak
-            toMap().toJsonString() shouldEqualJson """
-            {
-              "id" : 0,
-              "status" : "UNDER_BEHANDLING",
-              "type" : "FØRSTEGANG",
-              "tema" : "REGISTRERING_UNNTAK_NORSK_TRYGD_UTSTASJONERING",
-              "fagsak" : {
-                "saksnummer" : "MEL-test",
-                "type" : "EU_EOS",
-                "tema" : "MEDLEMSKAP_LOVVALG",
-                "status" : "OPPRETTET",
-                "aktører" : [ {
-                  "aktørId" : "12345678901",
-                  "rolle" : "BRUKER"
-                }, {
-                  "rolle" : "VIRKSOMHET",
-                  "orgnr" : "123456789"
-                } ]
-              }
+            fagsak.run {
+                saksnummer shouldBe FagsakTestFactory.SAKSNUMMER
+                aktører.shouldHaveSize(2).run {
+                    elementAt(0).run {
+                        aktørId shouldBe FagsakTestFactory.BRUKER_AKTØR_ID
+                        rolle.name shouldBe Aktoersroller.BRUKER.name
+                    }
+                    elementAt(1).run {
+                        orgnr shouldBe FagsakTestFactory.ORGNR
+                        rolle.name shouldBe Aktoersroller.VIRKSOMHET.name
+                    }
+                }
             }
-            """
         }
     }
 
     @Test
     fun `Prosessinstans med behandling og data`() {
+        val prosessInstansID = UUID.fromString("da6a548b-59a8-4f19-9788-434254728307")
         val prosessinstans = Prosessinstans.forTest {
-            id = UUID.fromString("da6a548b-59a8-4f19-9788-434254728307")
+            id = prosessInstansID
             behandling {
                 fagsak {
                     medBruker()
@@ -131,31 +116,18 @@ class DSLTest {
         }
 
         prosessinstans.run {
-            toMap().toJsonString() shouldEqualJson """
-            {
-              "id" : "da6a548b-59a8-4f19-9788-434254728307",
-              "status" : "KLAR",
-              "behandling" : {
-                "id" : 0,
-                "status" : "UNDER_BEHANDLING",
-                "type" : "FØRSTEGANG",
-                "tema" : "REGISTRERING_UNNTAK_NORSK_TRYGD_UTSTASJONERING",
-                "fagsak" : {
-                  "saksnummer" : "MEL-test",
-                  "type" : "EU_EOS",
-                  "tema" : "MEDLEMSKAP_LOVVALG",
-                  "status" : "OPPRETTET",
-                  "aktører" : [ {
-                    "aktørId" : "12345678901",
-                    "rolle" : "BRUKER"
-                  } ]
+            id shouldBe prosessInstansID
+            getData(ProsessDataKey.SAKSBEHANDLER) shouldBe "Z123456"
+            hentBehandling.run {
+                fagsak.run {
+                    saksnummer shouldBe FagsakTestFactory.SAKSNUMMER
+                    aktører shouldHaveSize 1
+                    aktører.elementAt(0).run {
+                        aktørId shouldBe FagsakTestFactory.BRUKER_AKTØR_ID
+                        rolle.name shouldBe Aktoersroller.BRUKER.name
+                    }
                 }
-              },
-              "data" : [ {
-                "key" : "saksbehandler",
-                "value" : "Z123456"
-              } ]
-            }"""
+            }
         }
     }
 
@@ -396,60 +368,4 @@ class DSLTest {
             }
         }
     }
-
-
-    private fun Behandling.toMap(inkluderFagsak: Boolean = true): Map<String, Any?> = mapOf(
-        "id" to id,
-        "status" to status.name,
-        "type" to type.name,
-        "tema" to tema.name,
-        "fagsak" to if (inkluderFagsak) fagsak.toMap(inkluderBehandlinger = false) else null
-    ).filterValues { it != null }
-
-    private fun Fagsak.toMap(inkluderBehandlinger: Boolean = true) = mapOf(
-        "saksnummer" to saksnummer,
-        "gsakSaksnummer" to gsakSaksnummer,
-        "type" to type.name,
-        "tema" to tema.name,
-        "status" to status.name,
-        "betalingsvalg" to betalingsvalg?.name,
-        "aktører" to this.aktører.map { aktør ->
-            mapOf(
-                "aktørId" to aktør.aktørId,
-                "rolle" to aktør.rolle?.name,
-                "orgnr" to aktør.orgnr
-            ).filterValues { it != null }
-        },
-        "behandlinger" to if (inkluderBehandlinger) this.behandlinger.map { behandling -> behandling.toMap(false) } else null
-    ).filterValues { it != null }
-
-    private fun Prosessinstans.toMap() = mapOf(
-        "id" to id,
-        "status" to status.name,
-        "behandling" to hentBehandling.toMap() + mapOf(
-            "fagsak" to hentBehandling.fagsak.toMap(inkluderBehandlinger = false)
-        ),
-        "data" to this.getData().map { (key, value) ->
-            mapOf(
-                "key" to key,
-                "value" to value
-            )
-        }
-    )
-
-    private fun Any.toJsonString() = objectMapper
-        .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
-        .registerModule(JavaTimeModule())
-        .writerWithDefaultPrettyPrinter()
-        .writeValueAsString(this)
-
-    private fun Any.printJson() {
-        println(this.toJsonString())
-    }
-
-    companion object {
-        private val objectMapper = jacksonObjectMapper()
-    }
-
 }
-
