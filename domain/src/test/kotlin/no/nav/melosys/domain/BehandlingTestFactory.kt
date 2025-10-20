c32240d460a31100eb1539e6cdb5ae94e5df4972
RuneLind <rulind@gmail.com>
2025-10-20 19:29:40 +0200

    La behandling ha default id=1

--- domain/src/test/kotlin/no/nav/melosys/domain/BehandlingTestFactory.kt
+++ domain/src/test/kotlin/no/nav/melosys/domain/BehandlingTestFactory.kt
@@ -69,6 +69,7 @@ fun BehandlingTestFactory.BehandlingTestBuilder.saksopplysning(init: Saksopplysn
  * Test-verktøy for å opprette Behandling-instanser med standardverdier.
  */
 object BehandlingTestFactory {
+    const val BEHANDLING_ID = 1L
 
     /**
      * @MelosysTestDsl forhindrer at man kan aksessere properties fra ytre scopes,
@@ -91,6 +92,7 @@ object BehandlingTestFactory {
      */
     @JvmStatic
     fun builderWithDefaults() = BehandlingTestBuilder().apply {
+        id = BEHANDLING_ID
         // Sett standardverdier for alle påkrevde felt
         fagsak = FagsakTestFactory.lagFagsak()
 
