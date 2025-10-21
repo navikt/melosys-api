68defb4a0f9c57011aaeba12cf58f61c6051f9db
RuneLind <rulind@gmail.com>
2025-10-21 21:53:37 +0200

    Med KDoc for PensjonsopptjeningHendelse

--- integrasjon/src/main/kotlin/no/nav/melosys/integrasjon/hendelser/PensjonsopptjeningHendelse.kt
+++ integrasjon/src/main/kotlin/no/nav/melosys/integrasjon/hendelser/PensjonsopptjeningHendelse.kt
@@ -3,6 +3,22 @@ package no.nav.melosys.integrasjon.hendelser
 import java.time.LocalDateTime
 import java.util.UUID
 
+/**
+ * Hendelse som sendes til POPP (Pensjon Opptjening) for å rapportere pensjonsgivende inntekt
+ * fra årsavregning av trygdeavgift for personer som ikke er skattepliktige til Norge.
+ *
+ * Denne hendelsen sendes etter at et årsavregningsvedtak er fattet for FTRL-saker
+ * hvor bruker ikke er skattepliktig til Norge.
+ *
+ * @property hendelsesId Unik ID for hendelsen, generert deterministisk basert på behandlingID og inntektsår for idempotens
+ * @property correlationId Sporings-ID for hendelsen
+ * @property fnr Fødselsnummer/D-nummer for personen hendelsen gjelder
+ * @property pgi Pensjonsgivende inntekt i kroner (brutto årsbeløp)
+ * @property inntektsAr Hvilket år inntekten gjelder for
+ * @property fastsattTidspunkt Tidspunkt når vedtaket ble fattet
+ * @property endringstype Type endring (ny inntekt, oppdatering eller fjerning)
+ * @property melosysBehandlingID ID på Melosys-behandlingen som ligger til grunn for vedtaket
+ */
 data class PensjonsopptjeningHendelse(
     val hendelsesId: String,
     val correlationId: String,
@@ -13,10 +29,18 @@ data class PensjonsopptjeningHendelse(
     val endringstype: Endringstype,
     val melosysBehandlingID: String
 ) {
+    /**
+     * Type endring for pensjonsopptjening.
+     */
     enum class Endringstype {
-        NY_INNTEKT,  // Pensjonsgivende inntekt fra første vedtak for dette året
-        OPPDATERING, // Nytt vedtak med forskjellig beløp for samme år
-        FJERNING     // Bruker har ikke betalt, PGI skal fjernes (PGI = 0)
+        /** Pensjonsgivende inntekt fra første vedtak for dette året */
+        NY_INNTEKT,
+
+        /** Nytt vedtak med forskjellig beløp for samme år */
+        OPPDATERING,
+
+        /** Bruker har ikke betalt, PGI skal fjernes (PGI = 0) */
+        FJERNING
     }
 
     companion object {
