6aae0a2020a808464f568269df9732a22e1fdb6e
Øystein Barstad <111733896+barstadEkstern@users.noreply.github.com>
2025-10-28 12:28:29 +0100

    VAKT NOJIRA Lag admin endepunkt for endring av aktør id (#3053)
    Dette oppstår ofte i vakt. Lager derfor ett endepunkt slik att vi
    slipper å bruke DB

---
+++ integrasjonstest/src/test/kotlin/no/nav/melosys/itest/EndreAktoerIdIT.kt
@@ -0,0 +1,129 @@
+package no.nav.melosys.itest
+
+import io.kotest.matchers.shouldBe
+import io.mockk.every
+import io.mockk.mockk
+import no.nav.melosys.Application
+import no.nav.melosys.domain.Aktoer
+import no.nav.melosys.domain.Fagsak
+import no.nav.melosys.domain.kodeverk.Aktoersroller
+import no.nav.melosys.domain.kodeverk.Saksstatuser
+import no.nav.melosys.domain.kodeverk.Sakstemaer
+import no.nav.melosys.domain.kodeverk.Sakstyper
+import no.nav.melosys.repository.AktoerRepository
+import no.nav.melosys.repository.FagsakRepository
+import no.nav.melosys.service.tilgang.Aksesskontroll
+import no.nav.melosys.sikkerhet.context.SpringSubjectHandler
+import no.nav.melosys.sikkerhet.context.SubjectHandler
+import no.nav.security.mock.oauth2.MockOAuth2Server
+import no.nav.security.token.support.spring.SpringTokenValidationContextHolder
+import no.nav.security.token.support.spring.test.EnableMockOAuth2Server
+import org.junit.jupiter.api.AfterEach
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Primary
+import org.springframework.kafka.test.context.EmbeddedKafka
+import org.springframework.test.annotation.DirtiesContext
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+import org.springframework.transaction.annotation.Transactional
+import kotlin.test.Test
+
+@ActiveProfiles("test")
+@SpringBootTest(
+    classes = [Application::class, EndreAktoerIdIT.TestConfig::class],
+    webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT
+)
+@EmbeddedKafka
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@DirtiesContext
+@EnableMockOAuth2Server
+@AutoConfigureMockMvc
+class EndreAktoerIdIT(
+    @Autowired var mockMvc: MockMvc,
+    @Autowired var mockOAuth2Server: MockOAuth2Server,
+    @Autowired var fagsakRepository: FagsakRepository,
+    @Autowired var aktoerRepository: AktoerRepository
+) : OracleTestContainerBase() {
+
+    @TestConfiguration
+    class TestConfig {
+        @Bean
+        @Primary
+        fun aksesskontroll(): Aksesskontroll = mockk(relaxed = true)
+    }
+
+    @AfterEach
+    fun resetSubjectHandler() {
+        SubjectHandler.set(SpringSubjectHandler(SpringTokenValidationContextHolder()))
+    }
+
+    private fun hentBearerToken(): String {
+        return mockOAuth2Server.issueToken(
+            issuerId = "issuer1",
+            subject = "testbruker",
+            audience = "dumbdumb",
+            claims = mapOf(
+                "oid" to "test-oid",
+                "azp" to "test-azp",
+                "NAVident" to "test123"
+            )
+        ).serialize()
+    }
+
+    @Test
+    @Transactional
+    fun `endreAktoerId skal oppdatere aktørId på eksisterende aktør i fagsak`() {
+        val subjectHandler: SubjectHandler = mockk<SpringSubjectHandler>()
+        SubjectHandler.set(subjectHandler)
+        every { subjectHandler.userID } returns "Z123456"
+
+        val saksnummer = "MEL-123"
+        val gammelAktoerid = "1111111111111"
+        val nyAktoerid = "2222222222222"
+
+        val fagsak = Fagsak(
+            saksnummer = saksnummer,
+            type = Sakstyper.FTRL,
+            tema = Sakstemaer.MEDLEMSKAP_LOVVALG,
+            status = Saksstatuser.OPPRETTET
+        )
+        fagsakRepository.save(fagsak)
+
+        val brukerAktor = Aktoer().apply {
+            rolle = Aktoersroller.BRUKER
+            aktørId = gammelAktoerid
+            setFagsak(fagsak)
+        }
+        aktoerRepository.save(brukerAktor)
+        fagsak.aktører.add(brukerAktor)
+        fagsakRepository.save(fagsak)
+
+        mockMvc.perform(
+            MockMvcRequestBuilders.put("/admin/fagsaker/$saksnummer/endreAktoerId/$nyAktoerid")
+                .header("X-MELOSYS-ADMIN-APIKEY", "dummy")
+                .header("Authorization", "Bearer ${hentBearerToken()}")
+        ).andExpect(MockMvcResultMatchers.status().isOk)
+
+        // Hent den oppdaterte aktøren
+        val oppdatertAktor = aktoerRepository.findById(brukerAktor.id).get()
+
+        // Samme aktør men med ny aktørId
+        oppdatertAktor.aktørId shouldBe nyAktoerid
+        oppdatertAktor.fagsak?.saksnummer shouldBe saksnummer
+        oppdatertAktor.id shouldBe brukerAktor.id
+
+        // Fagsak skal være oppdatert og ha kun EN bruker aktør
+        val oppdatertFagsak = fagsakRepository.findById(saksnummer).get()
+        val fagsakBrukerAktorer = aktoerRepository.findByFagsakAndRolle(oppdatertFagsak, Aktoersroller.BRUKER)
+        fagsakBrukerAktorer.size shouldBe 1
+        fagsakBrukerAktorer.first().aktørId shouldBe nyAktoerid
+        fagsakBrukerAktorer.first().id shouldBe brukerAktor.id
+    }
+}
