7454dbeb4c9cf5f8855d3ca333e4637d15b8d499
RuneLind <rulind@gmail.com>
2025-10-29 17:05:44 +0100

    Fiks på class OpprettÅrsavregningModelBehandlingTest

--- saksflyt/src/test/kotlin/no/nav/melosys/saksflyt/steg/arsavregning/OpprettÅrsavregningModelBehandlingTest.kt
+++ saksflyt/src/test/kotlin/no/nav/melosys/saksflyt/steg/arsavregning/OpprettÅrsavregningModelBehandlingTest.kt
@@ -13,14 +13,15 @@ import no.nav.melosys.domain.Behandling
 import no.nav.melosys.domain.Behandlingsaarsak
 import no.nav.melosys.domain.Behandlingsresultat
 import no.nav.melosys.domain.Fagsak
-import no.nav.melosys.domain.FagsakTestFactory
+import no.nav.melosys.domain.SaksopplysningType
 import no.nav.melosys.domain.fagsak
 import no.nav.melosys.domain.forTest
-import no.nav.melosys.domain.kodeverk.Aktoersroller
 import no.nav.melosys.domain.kodeverk.Saksstatuser
 import no.nav.melosys.domain.kodeverk.Sakstemaer
 import no.nav.melosys.domain.kodeverk.Sakstyper
 import no.nav.melosys.domain.kodeverk.behandlinger.*
+import no.nav.melosys.domain.personDokument
+import no.nav.melosys.domain.saksopplysning
 import no.nav.melosys.saksflyt.TestdataFactory.lagPersonopplysning
 import no.nav.melosys.saksflytapi.domain.ProsessDataKey
 import no.nav.melosys.saksflytapi.domain.Prosessinstans
@@ -31,7 +32,6 @@ import no.nav.melosys.service.avgift.aarsavregning.ÅrsavregningService
 import no.nav.melosys.service.behandling.BehandlingService
 import no.nav.melosys.service.behandling.BehandlingsresultatService
 import no.nav.melosys.service.mottatteopplysninger.MottatteOpplysningerService
-import no.nav.melosys.service.persondata.PersondataService
 import no.nav.melosys.service.sak.FagsakService
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -48,9 +48,6 @@ class OpprettÅrsavregningModelBehandlingTest {
 
     @MockK
     private lateinit var behandslingsresultatService: BehandlingsresultatService
-
-    @MockK
-    private lateinit var persondataService: PersondataService
 
     @MockK
     private lateinit var årsavregningService: ÅrsavregningService
@@ -77,7 +74,6 @@ class OpprettÅrsavregningModelBehandlingTest {
         }
 
         val eksisterendeÅrsavregningsBehandling = lagBehandling {
-            id = 1
             type = Behandlingstyper.ÅRSAVREGNING
         }
 
@@ -92,7 +88,7 @@ class OpprettÅrsavregningModelBehandlingTest {
                 behandling = eksisterendeÅrsavregningsBehandling
                 id = 2
                 type = Behandlingsresultattyper.IKKE_FASTSATT
-            }.id!!, GJELDER_ÅR)
+            }.hentId(), GJELDER_ÅR)
         }
 
         prosessinstans.hentBehandling.run {
@@ -108,7 +104,6 @@ class OpprettÅrsavregningModelBehandlingTest {
         }
 
         val eksisterendeÅrsavregningsBehandling = lagBehandling {
-            id = 1
             type = Behandlingstyper.ÅRSAVREGNING
         }
 
@@ -145,9 +140,12 @@ class OpprettÅrsavregningModelBehandlingTest {
     }
 
     private fun setupMock(eksisterendeÅrsavregningsBehandling: Behandling, behandlingsaarsaktyper: Behandlingsaarsaktyper) {
-        val fagsak = lagFagsak()
-        every { persondataService.hentAktørIdForIdent(any()) } returns AKTØR_ID
-        every { fagsakService.hentFagsakerMedAktør(Aktoersroller.BRUKER, AKTØR_ID) } returns listOf(fagsak)
+        val fagsak = Fagsak.forTest {
+            type = Sakstyper.EU_EOS
+            tema = Sakstemaer.MEDLEMSKAP_LOVVALG
+            status = Saksstatuser.OPPRETTET
+            medBruker()
+        }
         every { fagsakService.hentFagsak(SAKSNUMMER) } returns fagsak
         every {
             årsavregningService.hentGjeldendeBehandlingsresultaterForÅrsavregning(
@@ -155,14 +153,12 @@ class OpprettÅrsavregningModelBehandlingTest {
                 GJELDER_ÅR
             )
         } returns GjeldendeBehandlingsresultaterForÅrsavregning(
-            sisteBehandlingsresultatMedAvgift = Behandlingsresultat().apply {
+            sisteBehandlingsresultatMedAvgift = Behandlingsresultat.forTest {
                 behandling = eksisterendeÅrsavregningsBehandling
-                id = 2
                 type = Behandlingsresultattyper.IKKE_FASTSATT
             },
-            sisteBehandlingsresultatMedMedlemskapsperiode = Behandlingsresultat().apply {
+            sisteBehandlingsresultatMedMedlemskapsperiode = Behandlingsresultat.forTest {
                 behandling = eksisterendeÅrsavregningsBehandling
-                id = 2
                 type = Behandlingsresultattyper.IKKE_FASTSATT
             },
         )
@@ -190,10 +186,9 @@ class OpprettÅrsavregningModelBehandlingTest {
             saksopplysninger = mutableSetOf(lagPersonopplysning())
         }
 
-        every { behandslingsresultatService.hentBehandlingsresultat(eksisterendeÅrsavregningsBehandling.id) } returns Behandlingsresultat().apply {
-            this.behandling = eksisterendeÅrsavregningsBehandling
-            this.id = 2
-            this.type = Behandlingsresultattyper.IKKE_FASTSATT
+        every { behandslingsresultatService.hentBehandlingsresultat(eksisterendeÅrsavregningsBehandling.id) } returns Behandlingsresultat.forTest {
+            behandling = eksisterendeÅrsavregningsBehandling
+            type = Behandlingsresultattyper.IKKE_FASTSATT
         }
         every { årsavregningService.opprettÅrsavregning(any(), any()) } returns ÅrsavregningModel(
             årsavregningID = 112,
@@ -209,27 +204,22 @@ class OpprettÅrsavregningModelBehandlingTest {
     private fun lagBehandling(block: Behandling.Builder.() -> Unit = {}): Behandling = Behandling.forTest {
         type = Behandlingstyper.FØRSTEGANG
         tema = Behandlingstema.UTSENDT_ARBEIDSTAKER
-        saksopplysninger = mutableSetOf(lagPersonopplysning())
-        block()
-    }
-
-    private fun lagFagsak(block: FagsakTestFactory.Builder.() -> Unit = {}): Fagsak = Fagsak.forTest {
-        type = Sakstyper.EU_EOS
-        tema = Sakstemaer.MEDLEMSKAP_LOVVALG
-        status = Saksstatuser.OPPRETTET
-        medBruker()
+        saksopplysning {
+            type = SaksopplysningType.PERSOPL
+            personDokument {
+                fnr = "99887766554"
+            }
+        }
         block()
     }
 
     private fun lagProsessInstans(block: Prosessinstans.Builder.() -> Unit = {}): Prosessinstans = Prosessinstans.forTest {
         medData(ProsessDataKey.GJELDER_ÅR, GJELDER_ÅR)
-        medData(ProsessDataKey.AKTØR_ID, AKTØR_ID)
         medData(ProsessDataKey.SAKSNUMMER, SAKSNUMMER)
         block()
     }
 
     companion object {
-        const val AKTØR_ID = "456789123"
         const val GJELDER_ÅR = 2023
         const val SAKSNUMMER = "MEL-test"
     }
