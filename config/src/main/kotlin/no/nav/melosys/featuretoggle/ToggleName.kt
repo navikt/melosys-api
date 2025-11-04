aa6e1fecb317de3499f61d4c9303c093e6ba1d53
Isa Salem Hame <isa.salem.hame@nav.no>
2025-11-04 16:55:27 +0100

    Toggle sortering

--- config/src/main/kotlin/no/nav/melosys/featuretoggle/ToggleName.kt
+++ config/src/main/kotlin/no/nav/melosys/featuretoggle/ToggleName.kt
@@ -13,4 +13,5 @@ object ToggleName {
     const val STANDARDVEDLEGG_EGET_VEDLEGG_AVTALELAND = "standardvedlegg_eget_vedlegg_avtaleland"
     const val MELOSYS_FAKTURERINGSKOMPONENTEN_IKKE_TIDLIGERE_PERIODER = "melosys.faktureringskomponenten.ikke-tidligere-perioder"
     const val MELOSYS_SEND_POPP_HENDELSE = "melosys.send_popp_hendelse"
+    const val MELOSYS_SORTER_SOK_PA_REDIGERINGSDATO = "melosys.sorter_sok_pa_redigeringsdato"
 }
