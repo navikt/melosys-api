002ade8c68ec5c02bd23fae52e0df8f84a7c609d
Francois Allix <francois.allix@nav.no>
2025-11-12 12:23:18 +0100

    Forenkling og reduser støy i produksjonslogger

--- service/src/main/kotlin/no/nav/melosys/service/behandling/jobb/AvsluttArt13BehandlingService.kt
+++ service/src/main/kotlin/no/nav/melosys/service/behandling/jobb/AvsluttArt13BehandlingService.kt
@@ -68,13 +68,17 @@ class AvsluttArt13BehandlingService(
     }
 
     private fun toMndHarPassertSidenSaksbehandling(behandling: Behandling, behandlingsresultat: Behandlingsresultat): Boolean {
-        log.info { "Sjekker om 2 måneder har passert siden saksbehandling for behandling ${behandling.id}" }
-        val behandlingKanResultereIVedtak = behandling.kanResultereIVedtak()
-        log.info { "Behandling ${behandling.id} kan resultere i vedtak: $behandlingKanResultereIVedtak" }
-        val erUtpekingUtenVedtak = erUtpekingUtenVedtak(behandlingsresultat)
-        log.info { "Behandling ${behandling.id} er utpeking uten vedtak: $erUtpekingUtenVedtak" }
+        log.debug { "Sjekker om 2 måneder har passert siden saksbehandling for behandling ${behandling.id}" }
 
-        if (behandlingKanResultereIVedtak && !erUtpekingUtenVedtak) {
+        val erUtpekingUtenVedtak = erUtpekingUtenVedtak(behandlingsresultat)
+        val skalHaVedtak = behandling.kanResultereIVedtak() && !erUtpekingUtenVedtak
+
+        log.debug {
+            "Behandling ${behandling.id}: kanResultereIVedtak=${behandling.kanResultereIVedtak()}, " +
+            "erUtpekingUtenVedtak=$erUtpekingUtenVedtak"
+        }
+
+        if (skalHaVedtak) {
             if (!behandlingsresultat.harVedtak()) { //TODO dette kommer aldri til å skje
                 throw FunksjonellException(
                     "Behandling ${behandling.id} har ikke et vedtak og status kan da ikke settes til AVSLUTTET"
