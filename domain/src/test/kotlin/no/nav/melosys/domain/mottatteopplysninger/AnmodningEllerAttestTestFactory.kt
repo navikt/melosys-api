8f9a8f396950f614dc696fd60152c9eb1234c797
RuneLind <rulind@gmail.com>
2026-01-03 12:20:45 +0100

    Refactor OpprettLovvalgsperiodeServiceTest and TrygdeavtaleServiceTest to forTest DSL

--- domain/src/test/kotlin/no/nav/melosys/domain/mottatteopplysninger/AnmodningEllerAttestTestFactory.kt
+++ domain/src/test/kotlin/no/nav/melosys/domain/mottatteopplysninger/AnmodningEllerAttestTestFactory.kt
@@ -2,7 +2,9 @@ package no.nav.melosys.domain.mottatteopplysninger
 
 import no.nav.melosys.domain.MelosysTestDsl
 import no.nav.melosys.domain.kodeverk.Land_iso2
+import no.nav.melosys.domain.mottatteopplysninger.data.Periode
 import no.nav.melosys.domain.mottatteopplysninger.data.Soeknadsland
+import java.time.LocalDate
 
 fun anmodningEllerAttestForTest(init: AnmodningEllerAttestTestFactory.Builder.() -> Unit = {}): AnmodningEllerAttest =
     AnmodningEllerAttestTestFactory.Builder().apply(init).build()
@@ -16,9 +18,19 @@ object AnmodningEllerAttestTestFactory {
         var avsenderland: Land_iso2? = AVSENDERLAND
         var lovvalgsland: Land_iso2? = LOVVALGSLAND
 
+        // Periode support (inherited from MottatteOpplysningerData)
+        private var periodeFom: LocalDate? = null
+        private var periodeTom: LocalDate? = null
+
         // Soeknadsland support (inherited from MottatteOpplysningerData)
         private var soeknadslandkoder: List<String> = emptyList()
         private var flereLandUkjentHvilke: Boolean = false
+
+        /** Set periode with fom and tom dates */
+        fun periode(fom: LocalDate, tom: LocalDate? = null) = apply {
+            periodeFom = fom
+            periodeTom = tom
+        }
 
         /** Set søknadsland med landkoder */
         fun soeknadsland(vararg landkoder: String, flereLandUkjent: Boolean = false) = apply {
@@ -35,6 +47,11 @@ object AnmodningEllerAttestTestFactory {
         fun build(): AnmodningEllerAttest = AnmodningEllerAttest().apply {
             this.avsenderland = this@Builder.avsenderland
             this.lovvalgsland = this@Builder.lovvalgsland
+
+            // Set periode if fom is provided
+            if (this@Builder.periodeFom != null) {
+                this.periode = Periode(this@Builder.periodeFom, this@Builder.periodeTom)
+            }
 
             // Set soeknadsland if any landkoder were provided
             if (this@Builder.soeknadslandkoder.isNotEmpty() || this@Builder.flereLandUkjentHvilke) {
