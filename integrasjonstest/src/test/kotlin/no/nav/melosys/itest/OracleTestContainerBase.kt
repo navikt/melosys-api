78c86d4ca3fb72cf385bd3dad243735a95cfb0cd
RuneLind <rulind@gmail.com>
2026-01-19 16:56:05 +0100

    Bruk eksplisitt liste over tabeller som skal tømmes
    Unngår å tømme kodeverk/statiske data-tabeller som FAGSAK_TEMA

--- integrasjonstest/src/test/kotlin/no/nav/melosys/itest/OracleTestContainerBase.kt
+++ integrasjonstest/src/test/kotlin/no/nav/melosys/itest/OracleTestContainerBase.kt
@@ -23,45 +23,36 @@ open class OracleTestContainerBase {
      * - Hver test starter med en ren database
      * - Man kan inspisere databasen etter en test for feilsøking
      *
-     * Ekskluderer Flyway-tabeller og kodeverk/statiske data-tabeller.
+     * Bruker eksplisitt liste over tabeller som skal tømmes for å unngå
+     * å tømme kodeverk/statiske data-tabeller.
      */
     @BeforeEach
     fun truncateAllTables() {
-        val tablesToTruncate = jdbcTemplate.queryForList(
-            """
-            SELECT table_name FROM user_tables
-            WHERE table_name NOT LIKE 'flyway%'
-              AND table_name NOT IN (
-                'KODEVERK', 'KODEVERK_RELASJON', 'KODEVERK_RELASJON_VERDI',
-                'BEHANDLINGSAARSAK_TYPE', 'BEHANDLINGSTYPE', 'BEHANDLINGSTEMA',
-                'BEHANDLINGSRESULTATTYPE', 'FAGSAK_STATUS', 'FAGSAK_TYPE',
-                'LOVVALGSBESTEMMELSE', 'PROSESS_STEG', 'PROSESS_TYPE',
-                'SAKSTYPE', 'UTENLANDSK_MYNDIGHET', 'PREFERANSE'
-              )
-            """,
-            String::class.java
+        // Eksplisitt liste over tabeller som inneholder testdata (ikke kodeverk/statiske data)
+        val tablesToTruncate = listOf(
+            "PROSESSINSTANS_HENDELSER", "PROSESSINSTANS",  // Saksflyt først pga FK
+            "AARSAVREGNING", "TRYGDEAVGIFTSPERIODE", "SKATTEFORHOLD_TIL_NORGE", "INNTEKTSPERIODE",
+            "HELSEUTGIFT_DEKKES_PERIODE", "MEDLEMSKAPSPERIODE", "LOVVALG_PERIODE", "UTPEKINGSPERIODE",
+            "VILKAARSRESULTAT", "VILKAAR_BEGRUNNELSE", "AVKLARTEFAKTA", "AVKLARTEFAKTA_REGISTRERING",
+            "VEDTAK_METADATA", "BEHANDLINGSRES_BEGRUNNELSE", "KONTROLLRESULTAT", "UTKAST_BREV",
+            "SAKSOPPLYSNING_KILDE", "SAKSOPPLYSNING", "MOTTATTEOPPLYSNINGER",
+            "ANMODNINGSPERIODE_SVAR", "ANMODNINGSPERIODE",
+            "TIDLIGERE_MEDLEMSPERIODE", "KONTAKTOPPLYSNING", "FULLMAKT",
+            "BEHANDLINGSNOTAT", "BEHANDLINGSAARSAK", "OPPGAVE_TILBAKELEGGING",
+            "BEHANDLINGSRESULTAT", "BEHANDLING", "AKTOER", "FAGSAK"
         )
-
-        if (tablesToTruncate.isEmpty()) {
-            return
-        }
 
         // Deaktiver alle fremmednøkkel-constraints
-        val constraints = jdbcTemplate.queryForList(
-            """
-            SELECT constraint_name, table_name FROM user_constraints
-            WHERE constraint_type = 'R' AND status = 'ENABLED'
-            """,
-            mapOf<String, Any>()
-        )
+        data class Constraint(val name: String, val tableName: String)
+        val constraints = jdbcTemplate.query(
+            "SELECT constraint_name, table_name FROM user_constraints WHERE constraint_type = 'R' AND status = 'ENABLED'"
+        ) { rs, _ -> Constraint(rs.getString("CONSTRAINT_NAME"), rs.getString("TABLE_NAME")) }
 
-        constraints.forEach { row ->
-            val constraintName = row["CONSTRAINT_NAME"] as String
-            val tableName = row["TABLE_NAME"] as String
+        constraints.forEach { constraint ->
             try {
-                jdbcTemplate.execute("ALTER TABLE $tableName DISABLE CONSTRAINT $constraintName")
+                jdbcTemplate.execute("ALTER TABLE ${constraint.tableName} DISABLE CONSTRAINT ${constraint.name}")
             } catch (e: Exception) {
-                log.warn { "Kunne ikke deaktivere constraint $constraintName på $tableName: ${e.message}" }
+                log.warn { "Kunne ikke deaktivere constraint ${constraint.name} på ${constraint.tableName}: ${e.message}" }
             }
         }
 
@@ -75,13 +66,11 @@ open class OracleTestContainerBase {
         }
 
         // Reaktiver alle fremmednøkkel-constraints
-        constraints.forEach { row ->
-            val constraintName = row["CONSTRAINT_NAME"] as String
-            val tableName = row["TABLE_NAME"] as String
+        constraints.forEach { constraint ->
             try {
-                jdbcTemplate.execute("ALTER TABLE $tableName ENABLE CONSTRAINT $constraintName")
+                jdbcTemplate.execute("ALTER TABLE ${constraint.tableName} ENABLE CONSTRAINT ${constraint.name}")
             } catch (e: Exception) {
-                log.warn { "Kunne ikke reaktivere constraint $constraintName på $tableName: ${e.message}" }
+                log.warn { "Kunne ikke reaktivere constraint ${constraint.name} på ${constraint.tableName}: ${e.message}" }
             }
         }
     }
