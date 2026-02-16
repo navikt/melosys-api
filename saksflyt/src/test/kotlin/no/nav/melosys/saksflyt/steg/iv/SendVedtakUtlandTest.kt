5af4b2f7957a9816027b41e086db0cf35e35adfc
Thang Phan <thang.quoc.ly.phan@nav.no>
2026-02-16 11:12:37 +0100

    7885 Ikke send SED uten mottakerinstutisjoner når det er 11_3_b flyten (#3207)
    https://jira.adeo.no/browse/MELOSYS-7885
    
    Dette påvirker kun 11_3_b flyten. Dersom det ikke finnes
    mottakerinstitusjoner, skal ikke det sendes brev/sed til utlandet.
    
    ---------
    
    Co-authored-by: Francois <francois.allix@nav.no>

--- saksflyt/src/test/kotlin/no/nav/melosys/saksflyt/steg/iv/SendVedtakUtlandTest.kt
+++ saksflyt/src/test/kotlin/no/nav/melosys/saksflyt/steg/iv/SendVedtakUtlandTest.kt
@@ -136,6 +136,27 @@ class SendVedtakUtlandTest {
     }
 
     @Test
+    fun `utfør skal ikke sende SED når artikkel 11_3B og ingen mottakerinstitusjoner`() {
+        val behandlingsresultat = lagBehandlingsresultat {
+            lovvalgsperioder.clear()
+            lovvalgsperiode {
+                bestemmelse = Lovvalgbestemmelser_883_2004.FO_883_2004_ART11_3B
+                lovvalgsland = Land_iso2.NO
+                innvilgelsesresultat = InnvilgelsesResultat.INNVILGET
+            }
+        }
+        val prosessinstans = lagProsessinstans()
+        every { behandlingsresultatService.hentBehandlingsresultat(any()) } returns behandlingsresultat
+        every { behandlingsresultatService.hentBehandlingsresultatMedAvklartefakta(any()) } returns behandlingsresultat
+
+
+        sendVedtakUtland.utfør(prosessinstans)
+
+
+        verify(exactly = 0) { eessiService.opprettOgSendSed(any(), any(), any(), any(), any(), any()) }
+    }
+
+    @Test
     fun `utfør skal sende SED for artikkel 11 når suksessfull og status er oppdatert resultat`() {
         val behandlingsresultat = lagBehandlingsresultat {
             lovvalgsperioder.clear()
