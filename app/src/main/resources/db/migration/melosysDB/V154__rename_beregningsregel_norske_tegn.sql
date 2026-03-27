662ab6b6b6654bedd55b6e5f842eb5f87dbfa108
RuneLind <rulind@gmail.com>
2026-03-27 12:55:40 +0100

    Bruk norske tegn i Avgiftsberegningsregel-enum
    Rename ORDINAER til ORDINÆR og MINSTEBELOEP til MINSTEBELØP i enum, alle referanser i 9 filer, og Flyway V154 for eksisterende data. Fjerner string-replace-workaround i TrygdeavgiftsberegningService. Legger også til takeIf-fallback for tom grunnlagListe fra beregningsservicen.
    
    melosys-trygdeavgift-beregning bruker nå norske tegn i beregningsregel-responsen (ORDINÆR, MINSTEBELØP). Enum-verdiene i melosys-api må matche for at valueOf() skal fungere uten workarounds.

---
+++ app/src/main/resources/db/migration/melosysDB/V154__rename_beregningsregel_norske_tegn.sql
@@ -0,0 +1,2 @@
+UPDATE trygdeavgiftsperiode SET beregningstype = 'ORDINÆR' WHERE beregningstype = 'ORDINAER';
+UPDATE trygdeavgiftsperiode SET beregningstype = 'MINSTEBELØP' WHERE beregningstype = 'MINSTEBELOEP';
