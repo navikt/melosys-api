9baf0bf2342e541992635f40d3a0695121335bed
Francois Allix <francois.allix@nav.no>
2026-05-11 13:32:20 +0200

    Endre beregningsregel-felt til å bruke enum Avgiftsberegningsregel

--- integrasjon/src/main/kotlin/no/nav/melosys/integrasjon/dokgen/dto/InformasjonTrygdeavgift.kt
+++ integrasjon/src/main/kotlin/no/nav/melosys/integrasjon/dokgen/dto/InformasjonTrygdeavgift.kt
@@ -1,12 +1,13 @@
 package no.nav.melosys.integrasjon.dokgen.dto
 
 import com.fasterxml.jackson.annotation.JsonFormat
-import tools.jackson.databind.annotation.JsonSerialize
-import tools.jackson.databind.ext.javatime.ser.LocalDateSerializer
+import no.nav.melosys.domain.avgift.Avgiftsberegningsregel
 import no.nav.melosys.domain.brev.DokgenBrevbestilling
 import no.nav.melosys.domain.kodeverk.Betalingstype
 import no.nav.melosys.domain.kodeverk.Mottakerroller
 import no.nav.melosys.domain.kodeverk.Trygdeavgiftmottaker
+import tools.jackson.databind.annotation.JsonSerialize
+import tools.jackson.databind.ext.javatime.ser.LocalDateSerializer
 import java.math.BigDecimal
 import java.time.LocalDate
 
@@ -41,5 +42,5 @@ data class AvgiftsperiodeEøsPensjonist(
     val avgiftspliktigInntektPerMd: BigDecimal,
     val inntektskilde: String,
     val skatteplikt: Boolean,
-    val beregningsregel: String
+    val beregningsregel: Avgiftsberegningsregel
 )
