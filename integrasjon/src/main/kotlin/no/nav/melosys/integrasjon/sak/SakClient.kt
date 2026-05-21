804d63c898caefc8eb96004960ec7befc1d9fd58
Eirik Bergande <eirik.bergande@nav.no>
2026-05-21 11:50:38 +0200

    logging for test

--- integrasjon/src/main/kotlin/no/nav/melosys/integrasjon/sak/SakClient.kt
+++ integrasjon/src/main/kotlin/no/nav/melosys/integrasjon/sak/SakClient.kt
@@ -1,9 +1,12 @@
 package no.nav.melosys.integrasjon.sak
 
+import mu.KotlinLogging
 import no.nav.melosys.exception.TekniskException
 import no.nav.melosys.sikkerhet.context.SubjectHandler
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.bodyToMono
+
+private val log = KotlinLogging.logger { }
 
 class SakClient(
     private val webClient: WebClient
@@ -12,11 +15,17 @@ class SakClient(
     override fun opprettSak(sakDto: SakDto): SakDto {
         sakDto.opprettetAv = SubjectHandler.getInstance().getUserID()
 
-        return webClient
+        log.info { "Kaller Sak API for opprettelse av sak med saksnummer ${sakDto.saksnummer} og tema ${sakDto.tema}" }
+
+        val response = webClient
             .post()
             .bodyValue(sakDto)
             .retrieve()
             .bodyToMono<SakDto>()
             .block() ?: throw TekniskException("Mangler respons fra Sak")
+
+        log.info { "Opprettet sak i Sak API med id ${response.id} for saksnummer ${sakDto.saksnummer}" }
+
+        return response
     }
 }
