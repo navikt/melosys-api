d455b924f438554a9e8116e9b12f9cb239e4aee0
Francois Allix <francois.allix@nav.no>
2026-06-23 13:14:38 +0200

    Fjern inntektskilde fra FTRL-brevmaler pga. dokgen-oppslag via inntektskildetype

--- integrasjon/src/main/kotlin/no/nav/melosys/integrasjon/dokgen/dto/innvilgelseftrl/AvgiftsperiodeDto.kt
+++ integrasjon/src/main/kotlin/no/nav/melosys/integrasjon/dokgen/dto/innvilgelseftrl/AvgiftsperiodeDto.kt
@@ -13,7 +13,6 @@ data class AvgiftsperiodeDto(
     val avgiftssats: BigDecimal?,
     val avgiftPerMd: BigDecimal,
     val inntektskildetype: Inntektskildetype,
-    val inntektskilde: String,
     val avgiftspliktigInntektPerMd: BigDecimal,
     val beregningsregel: Avgiftsberegningsregel
 )
