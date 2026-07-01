3f762bd3dc2ca9ec206ee4a214ed9cc59cde2d8d
Francois Allix <francois.allix@nav.no>
2026-07-01 14:57:34 +0200

    7988 Fjern ubrukt import i testbase
    io.mockk.every ble stående ubrukt etter at fallback-mocken i setup() ble
    fjernet til fordel for eksplisitte per-test-stubs av
    hentBehandlingsresultatMedTrygdeavgiftsperioder.

--- service/src/test/kotlin/no/nav/melosys/service/avgift/aarsavregning/ÅrsavregningServiceTestBase.kt
+++ service/src/test/kotlin/no/nav/melosys/service/avgift/aarsavregning/ÅrsavregningServiceTestBase.kt
@@ -1,6 +1,5 @@
 package no.nav.melosys.service.avgift.aarsavregning
 
-import io.mockk.every
 import io.mockk.impl.annotations.RelaxedMockK
 import io.mockk.junit5.MockKExtension
 import no.nav.melosys.domain.*
