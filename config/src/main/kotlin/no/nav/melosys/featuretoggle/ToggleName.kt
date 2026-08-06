1829f4e1852bd1c3639b8e0b908a227d133a1cc5
Thang Phan <thang.quoc.ly.phan@nav.no>
2026-08-06 13:14:28 +0200

    8192 DWEB Fjern feature toggle for 11-3a Norge er utpekt (#3441)
    Fjerner den permanente feature-togglen `melosys.11_3_a_Norge_er_utpekt`
    fra api-koden og forenkler brevmapper- og vedtaksbrevlogikken slik at
    den alltid oppfører seg som påslått.
    
    Togglen har vært påslått i produksjon over lang tid og regnes som
    permanent. Å fjerne den reduserer død kode og gjør logikken enklere å
    vedlikeholde.
    [MELOSYS-8192](https://jira.adeo.no/browse/MELOSYS-8192)
    
    - Fjernet `MELOSYS_11_3_A_NORGE_ER_UTPEKT` fra `ToggleName.kt`
    - Forenklet betingelser i `InnvilgelseEftaStorbritanniaMapper.kt` og
    `SendVedtaksbrevInnland.java`
    - Fjernet tilhørende Unleash-avhengighet og `FakeUnleash` i berørte
    klasser/tester
    
    ## Testing
    - [x] Enhetstester (`InnvilgelseEftaStorbritanniaMapperTest` 8 grønne,
    `SendVedtaksbrevInnlandTest` 27 grønne)
    - [ ] Manuell testing lokalt

--- config/src/main/kotlin/no/nav/melosys/featuretoggle/ToggleName.kt
+++ config/src/main/kotlin/no/nav/melosys/featuretoggle/ToggleName.kt
@@ -3,7 +3,6 @@ package no.nav.melosys.featuretoggle
 object ToggleName {
     const val BEHANDLINGSTYPE_KLAGE = "melosys.behandlingstype.klage"
     const val MELOSYS_SEND_MELDING_OM_VEDTAK = "melosys.send_melding_om_vedtak"
-    const val MELOSYS_11_3_A_NORGE_ER_UTPEKT = "melosys.11_3_a_Norge_er_utpekt"
     const val MELOSYS_SKATTEHENDELSE_CONSUMER = "melosys.skattehendelse.consumer"
     const val MELOSYS_ÅRSAVREGNING = "melosys.arsavregning"
     const val MELOSYS_ÅRSAVREGNING_UTEN_FLYT = "melosys.arsavregning.uten.flyt"
