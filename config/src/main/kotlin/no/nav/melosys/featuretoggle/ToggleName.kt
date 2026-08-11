c3424f54aecbe5120936577c62fdcd084bb9e068
Thang Phan <thang.quoc.ly.phan@nav.no>
2026-08-11 13:58:35 +0200

    8204 Fjern feature toggle melosys.cdm-4-4
    Fjerner feature toggle melosys.cdm-4-4 slik at CDM 4.4-oppførsel
    alltid er aktiv. Dødt CDM 4.3-kode, erCdm44-felt og nå-ubrukte
    unleash-parametre er fjernet. Kosovo-statsborgerskap bevares alltid.
    https://nav.atlassian.net/browse/MELOSYS-8204

--- config/src/main/kotlin/no/nav/melosys/featuretoggle/ToggleName.kt
+++ config/src/main/kotlin/no/nav/melosys/featuretoggle/ToggleName.kt
@@ -14,7 +14,6 @@ object ToggleName {
     const val MELOSYS_SEND_POPP_HENDELSE = "melosys.send_popp_hendelse"
     const val MELOSYS_SORTER_SOK_PA_REDIGERINGSDATO = "melosys.sorter_sok_pa_redigeringsdato"
     const val MELOSYS_BRUK_OPPRETT_BUC_OG_SED_V2 = "melosys.bruk_opprett_buc_og_sed_v2"
-    const val MELOSYS_CDM_4_4 = "melosys.cdm-4-4"
     const val MELOSYS_OPPGAVE_NØKKELORD = "melosys.oppgave_nokkelord"
     const val MELOSYS_TEKSTBLOKKER = "melosys.tekstblokker"
     const val MELOSYS_TEKSTBLOKKER_DYNAMISK_PLACEHOLDER = "melosys.tekstblokker.dynamisk-placeholder"
